e5f35c16dea9bce6a2e6c3809f91a929ad3a27f5
이준규 <ljk6463@gmail.com>
2019-03-23 00:45:11 +0900

    기능명세작성, 기능분리 전,기본 로직

---
+++ src/main/java/Baseball.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.nio.Buffer;
+
+public class Baseball {
+
+   public static void main(String[] args) throws IOException {
+
+      String com;
+      String usr;
+
+      int[] st = {0, 0, 0};
+      int[] ball = {0, 0, 0};
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+      while(true){
+         int tmp = (int)(Math.random() * 1000) +1;
+         com = String.valueOf(tmp);
+         boolean gameover = false;
+
+         bw.write(String.valueOf("숫자를 입력해주세요 : "));
+         //st,ball 을 계속 생성해버리면 주소가 다른 객체가 메모리에 계속 차임.
+         //선언은 한번만하고, 나중에 void clear()를 만들어서 배열 청소해줄 것.
+         while(true){
+            usr = br.readLine();
+            for (int i=0;i<com.length();++i){
+               if (com.charAt(i) == usr.charAt(i)){
+                  st[i] = 1;
+               }
+            }
+            int sum=0;
+            for (int i=0;i<st.length;i++){
+               sum += st[i];
+            }
+            if (sum==3){
+               gameover = true;
+               break;
+            }
+            for (int i=0;i<com.length();++i){
+               if(st[i]==1) continue;
+               else if (com.contains(String.valueOf(usr.charAt(i)))){
+                  ball[i] = 1;
+               }
+            }
+
+            int stsum=0;
+            int ballsum=0;
+
+            for (int i=0;i<usr.length();i++){
+               stsum += st[i];
+               ballsum += ball[i];
+            }
+            if (stsum!=0) {
+               if (ballsum!=0){
+                  bw.write(stsum + " 스트라이크 " + ballsum + "볼");
+               }
+               else{
+                  bw.write(stsum + " 스트라이크");
+               }
+            }
+            else {
+               if (ballsum!=0){
+                  bw.write(ballsum+"볼");
+               }
+               else{
+                  bw.write("낫싱");
+               }
+            }
+
+
+
+
+         }
+
+         if(gameover==true){
+            bw.write("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            bw.write("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            int choose = Integer.parseInt(br.readLine());
+            if (choose == 2) break;
+            else {
+               //void clear()
+               for (int i=0;i<usr.length();i++){
+                  st[i] = 0;
+                  ball[i]=0;
+               }
+            }
+         }
+
+         //if 3 st 2while break;
+         // -> 3개의 숫자를 모두 맞히셨습니다!! 게임 종료
+         //gameover -> yes
+         //choose -> 1,2
+         //if choose ==2 break;
+
+
+
+      }
+
+      bw.flush();
+      bw.close();
+      br.close();
+
+   }
+
+
+}
