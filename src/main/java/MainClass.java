db315d33a9cc4ec1969d5cad65e959177b0baa41
hyojaekim <hj00686@naver.com>
2019-03-22 22:13:51 +0900

    create test Commit!

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,6 +2,7 @@ public class MainClass {
 
     public void runGame(){
         System.out.println("test runGame!");
+        System.out.println("test runGame!");
     }
     public static void main(String[] args){
         MainClass mc = new MainClass();
