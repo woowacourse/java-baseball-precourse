66d91016fce0d3a1fe70d39500d5679b6b9c3987
je941004 <je941004@naver.com>
2019-03-23 00:29:25 +0900

    #1 make main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+
+  public static void main(String args[]) {
+    boolean gameContinue = true;
+
+    while (gameContinue) {
+      gameContinue = playBaseball();
+    }
+  }
+
+  public static int[] createRandomNumber() {
+
+    return null;
+  }
+
+  public static int[] Input() {
+
+    return null;
+  }
+
+  public static boolean playBaseball() {
+
+    return true;
+  }
+
+  public static int countBall(int[] computerNum, int[] userNum) {
+
+    return 0;
+  }
+
+  public static int countStrike(int[] computerNum, int[] userNum) {
+
+    return 0;
+  }
+
+  public static void output(int strike, int ball) {
+
+  }
+
+  public static void output() {
+
+  }
+
+  public static boolean isRegame() {
+
+    return true;
+  }
+
+  public static boolean isValid(int[] inputNum) {
+
+    return true;
+  }
+}
