5e872292c37dbc5946d670a0ddc85686dc3f564e
chowhr1231 <thddbswo14@gmail.com>
2019-03-22 21:37:18 +0900

    [ADD] Create checkAnswer function

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,23 +1,49 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 class Game {
-    private String[] answer = new String[3];
+    private char[] answer = new char[3];
 
     public Game() {
         answer = generateAnswer();
     }
 
-    private String[] generateAnswer() {
-        String[] str = new String[3];
+    private char[] generateAnswer() {
+        char[] str = new char[3];
         Random random = new Random();
+        int[] randomCheck = new int[9];
 
-        for (int i = 0; i < str.length; i++) {
+        int i=0;
+        while(i < str.length){
             int num = random.nextInt(9) + 1;
-            str[i] = String.valueOf(num);
+
+            if(randomCheck[num - 1] != 1){
+                str[i] = (char) (num + '0');
+                randomCheck[num-1]++;
+                i++;
+            }
         }
 
         return str;
+    }
+
+    public int[] CheckAnswer(String[] question){
+        int[] bs = {0, 0};
+
+        for(int i=0; i < answer.length; i++){
+            if (!String.valueOf(answer).contains(question[i])) {
+                continue;
+            }
+
+            if(answer[i] == question[i].charAt(0)){
+                bs[1]++; // strike 증가
+            }else{
+                bs[0]++; // ball 증가
+            }
+        }
+
+        return bs;
     }
 }
 
@@ -25,12 +51,13 @@ public class Baseball {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Game game = new Game();
-        boolean flag = false; // 정답인지 확인을 하기 위해
         String[] numArr = new String[3];
+        int[] checkNum = new int[2];
+        boolean flag = false; // 정답인지 확인을 하기 위해
 
         while(!flag){
             numArr = input.next().split("");
-
+            checkNum = game.CheckAnswer(numArr);
         }
     }
 }
