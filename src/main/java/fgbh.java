f9942a013a29314d6795930433165208758cb387
yeeunlee <mikalee618@gmail.com>
2019-03-22 17:29:21 +0900

    print test

---
+++ src/main/java/fgbh.java
@@ -0,0 +1,13 @@
+public class fgbh{
+
+    public static void main(String[] arg){
+
+        String[] args = new String[2];
+        args[0] = "HELLO";
+        args[1] = "WORLD";
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+    }
+}
