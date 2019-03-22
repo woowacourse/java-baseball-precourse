94a274213dc9a2c0153dd32327367fae00157f7f
hyojaekim <hj00686@naver.com>
2019-03-22 22:11:11 +0900

    create runGame()

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+
+    public void runGame(){
+        System.out.println("test runGame!");
+    }
+    public static void main(String[] args){
+        MainClass mc = new MainClass();
+        mc.runGame();
+    }
+}
