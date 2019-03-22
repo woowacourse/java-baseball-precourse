37f5d4f915161ece1ae45dcb953ef999be283a9c
hyojaekim <hj00686@naver.com>
2019-03-22 22:14:52 +0900

    test commit2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,6 +3,7 @@ public class MainClass {
     public void runGame(){
         System.out.println("test runGame!");
         System.out.println("test runGame!");
+        System.out.println("test!!");
     }
     public static void main(String[] args){
         MainClass mc = new MainClass();
