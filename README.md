289d0cfc3fbf7097993746298de4875b983ce6bc
hyojaekim <hj00686@naver.com>
2019-03-23 12:11:34 +0900

    Create Method printMenu

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
     * 스트라이크 초기화 및 확인 후 Up (resetStrike, setCheckStrike, setUpStrike)
     * 볼 초기화 및 확인 후 Up (resetBall, setCheckBall, setUpBall)
     * 결과 출력하기 (printResult)
+
+* 메뉴 출력(printMenu)
+    * "1" 입력 시 새로운 게임 시작
+    * "2" 입력 시 게임 종료
+    * "그 외 입력" 다시 메뉴 출력
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,21 +1,44 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainClass {
+    public void printMenu(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Scanner sc = new Scanner(System.in);
+        try{
+            int menuNum = sc.nextInt();
+            switch (menuNum){
+                case 1 :
+                    runGame();
+                    break;
+                case 2 :
+                    break;
+                default:
+                     System.out.println("잘못된 입력값 입니다. 다시 입력하세요.");
+                     printMenu();
+                     break;
+            }
+        }catch (InputMismatchException e) {
+            System.out.println("잘못된 입력값 입니다. 다시 입력하세요.");
+            printMenu();
+        }
+    }
 
     public void runGame(){
         RandomNumber rn = new RandomNumber();
         Baseball b = new Baseball();
         rn.setRandomNum();
+        System.out.println(rn.getRandA() + ", " +rn.getRandB() + ", " +rn.getRandC());
         while(true){
             b.setInputNum();
             b.setCheckStrike(rn);
             b.setCheckBall(rn);
             if(b.printResult()){
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                printMenu();
                 break;
             }
         }
-
     }
     public static void main(String[] args){
         MainClass mc = new MainClass();
