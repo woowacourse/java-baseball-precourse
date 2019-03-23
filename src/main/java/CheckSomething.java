3bc4930d81a9fba74c31c54f83b31d9d91ace3ed
jinwook <jin942002@naver.com>
2019-03-23 20:00:34 +0900

    checkNumbers function implemented.

--- src/main/java/CheckSomething.java
+++ src/main/java/CheckSomething.java
@@ -7,14 +7,20 @@ public class CheckSomething {
             return true;
 
     }
-    /*
+
     boolean checkNumbers(String input) {
         boolean flag =  true;
         for (int i = 0; i < input.length(); i++) {
-            if (checkNumber(input[i])
+            if (checkNumber(input.charAt(i)) == false) {
+                flag = false;
+                break;
+            }
         }
+        if (flag == false)
+            return false;
+        else
+            return true;
     }
-    */
 
     boolean checkNumber(char character) {
         if (character >= '1' && character <= '9')
--- src/main/java/FirstWeek.java
+++ src/main/java/FirstWeek.java
@@ -3,13 +3,34 @@ public class FirstWeek {
 
     public static void main (String[] args) {
         boolean bool;
-        String s = "hello world!";
+        String s = "hello";
         CheckSomething check = new CheckSomething();
-        bool = check.checkLength(s, 10);
-        bool = check.checkNumber('3');
 
+        bool = check.checkLength(s, 5);
+        System.out.println("is hello is 5 word?");
         System.out.println(bool);
-        System.out.println("hello world!");
+        System.out.println();
+
+        bool = check.checkNumber('3');
+        System.out.println("is '3' is number?");
+        System.out.println(bool);
+        System.out.println();
+
+        bool  = check.checkNumber('a');
+        System.out.println("is 'a' is number?");
+        System.out.println(bool);
+        System.out.println();
+
+        bool  = check.checkNumbers("12833");
+        System.out.println("is 12833 is number?");
+        System.out.println(bool);
+        System.out.println();
+
+        bool  = check.checkNumbers("300aa3");
+        System.out.println("is 300a3 is number?");
+        System.out.println(bool);
+        System.out.println();
+
 
     }
 }
