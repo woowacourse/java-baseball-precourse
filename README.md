5e2c4f2b48d83a30e558093472337b6879a42adc
hyojaekim <hj00686@naver.com>
2019-03-23 11:12:19 +0900

    Create Method isCorrectNum & Update error check

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 * 게임 실행(runGame)
     * 랜덤 숫자 생성(setRandomNum, getter, setter, makeRandomNum)
     * 유저 숫자 입력(setInputNum)
-    * 유저 숫자 자릿수 분리(setDivUserNum)
+    * 유저 숫자 자릿수 분리(setDivUserNum, setter, getter)
+    * 유저가 입력한 값 오류 확인(isCorrectNum)
--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,16 +1,51 @@
 import java.util.Scanner;
 
 public class Baseball {
+    private int userNumA;
+    private int userNumB;
+    private int userNumC;
 
     public void setInputNum(){
         Scanner sc = new Scanner(System.in);
         System.out.print("숫자를 입력해주세요 : ");
         int userNum = sc.nextInt();
-        setDivUserNum(userNum);
+        if(userNum < 123 || userNum > 987) {
+            System.out.println("잘못된 입력값 입니다!!");
+            setInputNum();
+        }else{
+            setDivUserNum(userNum);
+        }
     }
 
     public void setDivUserNum(int num){
         int a = num/100, b = (num - (a*100))/10, c = num - ((a*100) + (b*10));
+        if(isCorrectNum(a, b, c)){
+            setUserNumA(a);
+            setUserNumB(b);
+            setUserNumC(c);
+        }else{
+            System.out.println("잘못된 입력값 입니다!!");
+            setInputNum();
+        }
+    }
+
+    public boolean isCorrectNum(int a, int b, int c){
+        if(a == 0 || b == 0 || c == 0 || a == b || a == c || b == c) return false;
+        else return true;
+    }
+
+    public void setUserNumA(int n){ this.userNumA = n; }
+    public void setUserNumB(int n){ this.userNumB = n; }
+    public void setUserNumC(int n){ this.userNumC = n; }
+
+    public int getUserNumA(){
+        return this.userNumA;
+    }
+    public int getUserNumB(){
+        return this.userNumB;
+    }
+    public int getUserNumC(){
+        return this.userNumC;
     }
 
 }
