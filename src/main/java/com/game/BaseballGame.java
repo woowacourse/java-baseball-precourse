08c57f1cdb7e2045e01d5ce76ccdcac52462cd75
Jinho <wootechco.temp@gmail.com>
2019-03-23 22:53:46 +0900

    2번 구현 완료

--- src/main/java/com/game/BaseballGame.java
+++ src/main/java/com/game/BaseballGame.java
@@ -6,6 +6,28 @@ public class BaseballGame {
     }
 
     /**
+     * result[0] = 스트라이크 값
+     * result[1] = 볼 값
+     */
+    int[] compareNumber(final int[] com, final int[] user) {
+        int[] check = new int[10];
+        for (int i = 0; i < 3; ++i) {
+            check[com[i]] = i + 1;
+        }
+
+        int[] result = new int[2];
+        for (int i = 0; i < 3; ++i) {
+            if (check[user[i]] == (i + 1)) {
+                ++result[0];
+            } else if (check[user[i]] != 0) {
+                ++result[1];
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * 1~9까지 중복 없이 3개의 숫자을 배열로 반환
      */
     int[] getRandomNum() {
