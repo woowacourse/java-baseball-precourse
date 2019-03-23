cd76172000cd542c9cf00ab023ddacbba1ea9323
hyojaekim <hj00686@naver.com>
2019-03-23 10:57:17 +0900

    Create Method setDivUserNum

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * 게임 실행(runGame)
     * 랜덤 숫자 생성(setRandomNum, getter, setter, makeRandomNum)
     * 유저 숫자 입력(setInputNum)
+    * 유저 숫자 자릿수 분리(setDivUserNum)
--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -6,6 +6,11 @@ public class Baseball {
         Scanner sc = new Scanner(System.in);
         System.out.print("숫자를 입력해주세요 : ");
         int userNum = sc.nextInt();
+        setDivUserNum(userNum);
+    }
+
+    public void setDivUserNum(int num){
+        int a = num/100, b = (num - (a*100))/10, c = num - ((a*100) + (b*10));
     }
 
 }
