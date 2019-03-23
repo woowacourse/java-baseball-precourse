d50daa9da5e1546cfec992457b87244003060f19
DoWon Lee <gch01410@ajou.ac.kr>
2019-03-23 13:06:05 +0900

    first commit

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,25 @@
+/*
+ * @(#)BaseballGame.java       0.1 2019/03/23
+ */
+
+/**
+ * 숫자야구게임을 진행하는 클래스
+ *
+ * @version     0.1 2019/03/23
+ * @author      이도원
+ */
+public class BaseballGame {
+
+    /**
+     * BaseballGame 클래스의 생성자
+     */
+    public BaseballGame() {
+    }
+
+    /**
+     * 숫자야구게임을 진행하는 메소드
+     */
+    public void start() {
+        System.out.print("게임을 시작합니다!\n");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)Main.java       0.1 2019/03/23
+ */
+
+/**
+ * 숫자야구게임을 테스트하는 클래스
+ *
+ * @version     0.1 2019/03/23
+ * @author      이도원
+ */
+public class Main {
+
+    /**
+     * 숫자야구게임을 실행하는 메소드
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
+    }
+}
