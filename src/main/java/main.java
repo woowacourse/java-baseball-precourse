d37f3ec31b1ad55beaec9527ccd0731ace7a5c95
Sangwon <junothyme@gmail.com>
2019-03-23 10:21:11 +0900

    기본 진행 화면 기능 추가

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args){
+        int choice;
+        int humanNum;
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.print("숫자를 입력해주세요: ");
+            humanNum = sc.nextInt();
+        }
+
+    }
+}
