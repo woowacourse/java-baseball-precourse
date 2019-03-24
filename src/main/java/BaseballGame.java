30dd6cb6f6fb708bfadcc6d598cdb5df7c1030a2
changhoi <0417kch@naver.com>
2019-03-24 22:55:32 +0900

    전체 게임 끝내거나 다시하기 구조

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class BaseballGame {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(true) {
+            boolean win = false;
+            while(!win) {
+
+            }
+
+            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+            int re = stdIn.nextInt();
+            if (re == 2)
+                break;
+        }
+    }
 }
