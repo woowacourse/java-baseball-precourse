1b937927f939fddb93ff69b1a0226eb3cdfff86e
changhoi <0417kch@naver.com>
2019-03-24 23:28:36 +0900

    결과 보여주는 함수 구현

--- README.md
+++ README.md
@@ -7,5 +7,6 @@
 - [x] 1 ~ 9 사이의 난수 3자리를 배열로 저장하는 함수 - `makeArray`
 - [x] 사용자가 입력한 숫자를 배열로 저장하는 함수 - `inputArray`
 - [x] strike, ball 세는 함수 - `countStrike`, `countBall`
-- [ ] 결과 보여주는 함수
+- [x] 결과 보여주는 함수 - `printResult`
 - [ ] 3 스트라이크인지 아닌지 확인하는 함수 - `isWin`
+- [ ] 자바 코드 컨벤션 확인
--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -44,6 +44,18 @@ public class BaseballGame {
         return count;
     }
 
+    static void printResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + " 볼");
+        } else if (ball == 0) {
+            System.out.println(strike + " 스트라이크");
+        } else {
+            System.out.println(strike + " 스트라이크 " + ball + " 볼");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         while(true) {
@@ -58,6 +70,7 @@ public class BaseballGame {
                 int ball = countBall(answer, arr);
                 ball -= strike;
 
+                printResult(strike, ball);
 
                 win = isWin();
             }
