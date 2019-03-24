a7aa42e3be11552160b5c7a6ccf794d804f6f35f
강연욱 <fiasboc23@gmail.com>
2019-03-24 12:24:30 +0900

    Baseball.java
    created main

---
+++ src/main/java/Baseball.java
@@ -0,0 +1,14 @@
+package src.main.java;
+
+public class Baseball {
+	
+	public void game() {
+		System.out.println("make main");
+	}
+	
+	public static void main(String[] args) {
+		
+		Baseball bb_obj = new Baseball();
+		bb_obj.game();
+	}
+}
