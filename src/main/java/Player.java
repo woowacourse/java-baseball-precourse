9d07cb50a7399b7d8d7128cdeb4da52dbc994c38
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-03-25 04:59:45 +0900

    Player 클래스 구현

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,6 +5,8 @@
  * All rights reserved.
  */
 
+import java.util.Scanner;
+
 /**
  * Player 클래스는 숫자야구게임의 유저를 대표합니다.
  * 주 역할은 정답을 알고 있는 컴퓨터를 상대로 숫자를 추측하는 것입니다.
@@ -18,13 +20,43 @@ public class Player {
 
     /** 플레이어가 컴퓨터를 상대로 정답을 추측하는 메소드입니다. 추측한 정답은 guess 멤버변수에 저장됩니다. */
     public void makeGuess() {
+        Scanner rd = new Scanner(System.in);
+        while(true) {
+            int intGuess = rd.nextInt();
+            int intArrGuess[] = {intGuess / 100, (intGuess / 10) % 10, intGuess % 10};  // e.g., 432 -> {4, 3, 2}
+            boolean isThreeDigits = intGuess / 1000 == 0 && intGuess / 100 == 1;
+            boolean isDifferent = intArrGuess[0] != intArrGuess[1] && intArrGuess[0] != intArrGuess[2]
+                                                                   && intArrGuess[1] != intArrGuess[2];
+            boolean isThereZero = intArrGuess[0] == 0 || intArrGuess[1] == 0 || intArrGuess[2] == 0;
+            if (isThreeDigits && isDifferent && !isThereZero) {
+                this.guess = intArrGuess;
+                break;
+            }
+            System.out.println("[ERROR] 잘못된 입력입니다. 1 이상 9 이하의 서로 다른 3 개의 수를 입력해주십시오." +
+                    " e.g., 729");
+        }
     }
 
     /** 플레이어가 클래스 외부로 자신의 추측을 알려주는 메소드입니다. */
     public int[] tellGuess() {
+        return this.guess;
     }
 
     /** 플레이어가 자신이 게임을 지속하기를 원하는지 아닌지를 알려주는 메소드입니다. */
     public boolean wantToEnd() {
+        Scanner rd = new Scanner(System.in);
+        int userInput;
+
+        while(true) {
+            userInput = rd.nextInt();
+            if (userInput == 1) {
+                return true;
+            } else if (userInput == 2) {
+                return false;
+            } else {
+                System.out.println("[ERROR] 잘못된 입력입니다. 1 혹은 2 만 입력하실 수 있습니다." +
+                        " 다시 입력해 주십시오.");
+            }
+        }
     }
 }
