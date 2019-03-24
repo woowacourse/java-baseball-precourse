ecf2e85d5c77f0a9aed7523ccc761226a038d96a
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-03-24 19:56:05 +0900

    입력값 저장 컨테이너 Set->List 수정

--- src/main/java/com/github/seokhyoenchoi/game/baseball/BaseballGame.java
+++ src/main/java/com/github/seokhyoenchoi/game/baseball/BaseballGame.java
@@ -1,5 +1,6 @@
 package com.github.seokhyoenchoi.game.baseball;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -16,13 +17,13 @@ public class BaseballGame {
 	/**
 	 * @desc 난수를 저장할 Set
 	 */
-	private Set<Integer> baseballNums;
+	private List<Integer> baseballNums;
 	
 	
 	/**
 	 * @desc Test용. 모든 기능 구현 후 제거
 	 */
-	public Set<Integer> getNums(){
+	public List<Integer> getNums(){
 		return baseballNums;
 	}
 	
@@ -30,7 +31,7 @@ public class BaseballGame {
 	 * @desc Set에 난수 Setting
 	 */
 	public BaseballGame() {
-		baseballNums = new HashSet<>(3);
+		baseballNums = new ArrayList<>();
 
 		while (baseballNums.size() < 3) {
 			int randomNumber = (int) (Math.random() * 9 + 1);
@@ -40,5 +41,6 @@ public class BaseballGame {
 			}
 		}
 	}
+	
 
 }
