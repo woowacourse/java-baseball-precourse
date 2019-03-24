13342774ab95633cecbb6e8c4124bae6c0c212bd
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-03-25 04:27:23 +0900

    Player 클래스의 public interfaces 선언 (주석 추가)

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -13,14 +13,18 @@
  * @author        김유민
  */
 public class Player {
+    /** 플레이어의 추측이 저장되는 멤버변수입니다. */
     private int guess[];
 
+    /** 플레이어가 컴퓨터를 상대로 정답을 추측하는 메소드입니다. 추측한 정답은 guess 멤버변수에 저장됩니다. */
     public void makeGuess() {
     }
 
+    /** 플레이어가 클래스 외부로 자신의 추측을 알려주는 메소드입니다. */
     public int[] tellGuess() {
     }
 
+    /** 플레이어가 자신이 게임을 지속하기를 원하는지 아닌지를 알려주는 메소드입니다. */
     public boolean wantToEnd() {
     }
 }
