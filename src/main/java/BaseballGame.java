a45068e9b4cd4a3e88bace36c8f19ff31185130e
Jo Jinsik <kunggom@gmail.com>
2019-03-25 03:17:10 +0900

    getIntArray() 함수 작성 및 동작 테스트. IDE 환경에서는 테스트가 안 된다. cmd 창에서 수동으로 테스트.

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.Console;
 
 public class BaseballGame {
     // 1에서 9 사이의 서로 다른 임의의 정수 3개를 생성하여 배열로 반환.
@@ -18,10 +19,39 @@ public class BaseballGame {
         return Arrays.copyOfRange(arr, 0, 3);
     }
 
+    // 지정된 길이만큼의 숫자열을 입력받아 각각 배열에 넣어 반환하는 함수.
+    // 사용자 입력을 유도하는 메시지 또한 출력한다.
+    // 입력 오류시 오류 메시지를 출력한 후 0으로 초기화된 배열을 반환한다.
+    private static int[] getIntArray(int length, String msg) {
+        int[] arr = new int[length];
+        Console console = System.console();
+
+        try {
+            String inputData = console.readLine(msg);
+
+            if (inputData.length() != length)
+                throw new Exception("오류: 잘못된 길이의 입력입니다.");
+
+            for (int i = 0, j; i < inputData.length(); i++) {
+                j = inputData.charAt(i) - '0'; // type casting (char -> int)
+                if (j < 0 || j > 9) {
+                    throw new Exception("오류: 잘못된 숫자 입력입니다.");
+                } else {
+                    arr[i] = j;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new int[length];
+        }
+
+        return arr;
+    }
+
     // 메인 함수.
     public static void main(String[] args) {
-        // randIntArray 동작 테스트
-        int[] array1 = randIntArray();
+        // getIntArray 동작 테스트
+        int[] array1 = getIntArray(3, "3개 : ");
         for (int i: array1) {
             System.out.println(i);
         }
