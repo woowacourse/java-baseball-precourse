601e62d3f7c32444c4fe3a7124d9ddce653d3e8b
changhoi <0417kch@naver.com>
2019-03-24 23:23:08 +0900

    strike, ball 세는 함수 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 public class BaseballGame {
     static boolean isWin() {
@@ -15,12 +16,48 @@ public class BaseballGame {
         return arr;
     }
 
+    static int[] inputArray(int val) {
+        int[] arr = new int[3];
+        for (int i = 2; i >= 0; i--) {
+            arr[i] = val % 10;
+            val /= 10;
+        }
+        return arr;
+    }
+
+    static int countStrike(int[] answer, int[] arr) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (arr[i] == answer[i])
+                count++;
+        }
+        return count;
+    }
+
+    static int countBall(int[] answer, int[] arr) {
+        Arrays.sort(answer);
+        int count = 0;
+        for (int i : arr) {
+            if (Arrays.binarySearch(answer, i) >= 0)
+                count++;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         while(true) {
-            int[] arr = makeArray();
+            int[] answer = makeArray();
             boolean win = false;
             while(!win) {
+                System.out.print("숫자를 입력해주세요 : ");
+                int val = stdIn.nextInt();
+                int[] arr = inputArray(val);
+
+                int strike = countStrike(answer, arr);
+                int ball = countBall(answer, arr);
+                ball -= strike;
+
 
                 win = isWin();
             }
