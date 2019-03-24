d385db3cad81034c52b3247bf18935f282f870f6
Sangwon <junothyme@gmail.com>
2019-03-24 10:11:21 +0900

    컴퓨터 객체 생성

---
+++ src/main/java/Computer.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+
+public class Computer {
+    private int[] comNumArray;
+    private int randomNum;
+
+    public Computer() {
+        this.comNumArray = new int[3];
+    }
+
+    public int[] getComNumArray(){
+        return comNumArray;
+    }
+}
