c32552e55cb5e536f88a78eddcb40176f94bed65
YuSeonChoi <marine8743@naver.com>
2019-03-25 03:29:07 +0900

    모든 기능 구현 완료

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -10,23 +10,28 @@ public class Baseball {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
-
-        createNumber();
-        inputNumber();
-        comNum = resave(num);
-        check(inputNum,comNum);
-        repeat();
-
-//		printNumber();
+        newStart();
+        printNumber();    // 컴퓨터의 숫자 값을 알려주는 메소드
     }
 
     static void createNumber() {
+
+        removeNumber();
+
         while(true) {
             int random = (int)(Math.random() * 9 + 1);
             num.add(random);
             if(num.size() == 3)
                 break;
+        }
+    }
+
+    static void removeNumber() {
+        Iterator<Integer> iterator = num.iterator();
+
+        while(iterator.hasNext()) {
+            Integer element = iterator.next();
+            iterator.remove();
         }
     }
 
@@ -78,35 +83,67 @@ public class Baseball {
     static void check(int[] input, int[] com) {
         int strike = 0;
         int ball = 0;
+        int number = 0;
+
+
         for(int i=0; i<3; i++) {
             if(input[i] == com[i])
-                strike++;
+                strike+=1;
         }
-        if((input[0]==com[1]) || input[0] == com[2])
-            ball++;
-        if(input[1]==com[2])
-            ball++;
+        if((input[0]==com[1]) || (input[0] == com[2])) {
+            System.out.println("ball이 증가하였습니다.");
+            ball+=1;
+        }
+        if((input[1]==com[0]) || (input[1] == com[2])) {
+            System.out.println("ball이 증가하였습니다.");
+            ball+=1;
+        }
+        if((input[2]==com[0]) || (input[2] == com[1])) {
+            System.out.println("ball이 증가하였습니다.");
+            ball+=1;
+        }
+
 
         if(strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            Scanner scan = new Scanner(System.in);
+            number = scan.nextInt();
+            if(number == 1)
+                newStart();
+            else {
+                System.out.println("프로램을 종료합니다.");
+                System.exit(0);
+            }
+
         }
-        else if((strike > 0) && (ball > 0)) {
+        if((strike > 0) && (ball > 0)) {
             System.out.println(strike + "스트라이크" + ball + "볼");
+            repeat();
         }
-        else if(strike > 0) {
+        if(strike > 0) {
             System.out.println(strike + "스트라이크");
+            repeat();
         }
-        else if(ball > 0) {
+        if(ball > 0) {
             System.out.println(ball + "볼");
+            repeat();
         }
         else
         {
             System.out.println("0 스트라이크 0볼입니다.");
+            repeat();
         }
     }
 
     static void repeat() {
+        inputNumber();
+        comNum = resave(num);
+        check(inputNum,comNum);
+    }
+
+    static void newStart() {
+        createNumber();
         inputNumber();
         comNum = resave(num);
         check(inputNum,comNum);
