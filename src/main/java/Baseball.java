b743609361cfdd5be989a3875425457603eee970
강연욱 <fiasboc23@gmail.com>
2019-03-24 13:11:11 +0900

    Baseball.java
    created function : createNum()

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,9 +1,42 @@
 package src.main.java;
 
+import java.util.ArrayList;
+
 public class Baseball {
 	
+	/**
+	 * 1부터 9까지 랜덤한 숫자 3개 생성
+	 * 생성된 3개 숫자는 서로 다른 수
+	 * @param randNum
+	 */
+	public void createNum(int[] randNum) {
+		int cnt, temp;
+		ArrayList num_list = new ArrayList();
+		
+		cnt = 0;
+		while(cnt <= 2) {
+			// 1부터 9까지
+			temp = (int) (Math.random() * 9) + 1;
+			
+			// 중복 생성된 숫자 판별
+			if(num_list.contains(temp))
+				continue;
+			
+			randNum[cnt] = temp;
+			num_list.add(temp);
+			cnt++;
+		}
+	}
+
 	public void game() {
-		System.out.println("make main");
+		
+		int[] answer = new int[3];
+		
+		createNum(answer);
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.println(answer[i]);
+		}
 	}
 	
 	public static void main(String[] args) {
