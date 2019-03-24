b4932427f1f1829f55afcb95fbf4f51f3ae90ef3
changhoi <0417kch@naver.com>
2019-03-24 22:59:31 +0900

    맞추기 전까지 계속 숫자 입력하게 하는 구조

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class BaseballGame {
+    static boolean isWin() {
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
         while(true) {
             boolean win = false;
             while(!win) {
 
+                win = isWin();
             }
 
             System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
