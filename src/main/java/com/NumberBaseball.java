7b7f3dfa7e030f2c0a71ba9a15a4c7569995e9cf
김성훈 <seonghun127@gmail.com>
2019-03-24 20:11:39 +0900

    기능목록 4 구현

--- src/main/java/com/NumberBaseball.java
+++ src/main/java/com/NumberBaseball.java
@@ -12,6 +12,10 @@ public class NumberBaseball{
     static HashMap<Integer, Integer> user = new HashMap<>();
     static Scanner sc = new Scanner(System.in);
 
+    /**
+     * 이 클래스의 main메소드로서 기능 메소드 구현 후 테스트용으로 사용
+     * @param args
+     */
     public static void main(String[] args){
         setComNumber();         // 난수 발생
 
@@ -26,7 +30,10 @@ public class NumberBaseball{
                 .forEach(System.out::print);    // 입력된 숫자 출력
 
         StrikeBall sb = calc(new StrikeBall(0, 0));     // 컴퓨터의 난수와 사용자 입력 숫자 비교
-        System.out.println("strike is " + sb.strike + " ball is "+sb.ball);     // 비교 후 갱신된 strike, ball 출력
+
+        printResult(sb);            // StrikeBall 객체에 들어있는 결과 값을 출력
+
+        guessComNumber();           // 난수 발생을 제외한 위 과정을 반복 수행
     }
 
     /**
@@ -75,5 +82,57 @@ public class NumberBaseball{
             }
         }
         return strikeBall;
+    }
+
+    /**
+     * calc 메소드의 결과로 나온 StrikeBall 객체를 받아서 비교 결과를 출력하는 메소드
+     * @param strikeBall
+     */
+    static void printResult(StrikeBall strikeBall){
+        String result = "";
+        String s = "";
+
+        if(strikeBall.strike != 0){
+            s = strikeBall.strike + " 스트라이크 ";
+            result += s;
+        }
+        if(strikeBall.ball != 0){
+            s = strikeBall.ball + " 볼 ";
+            result += s;
+        }
+        if(result.isEmpty()){
+            result = "낫싱";
+        }
+
+        System.out.println(result);
+    }
+
+    /**
+     * 컴퓨터 난수와 사용자 입력 수가 같은지를 확인하는 메소드
+     * 정답을 맞혔는지 확인하기 위해 구현
+     * @return
+     */
+    static boolean compare(){
+        for(int i=0; i<3; i++){
+            if(com.get(i) != user.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 사용자의 입력, 두 수 비교, 결과 출력, 두 수가 같은지 여부를 확인하는 메소드들을묶어논 메소드
+     * do while로 묶어서 정답을 맞힐때까지 반복하게 해준다.
+     */
+    static void guessComNumber(){
+        do{
+            /* 사용자의 입력 */
+            setUserNumber();
+
+            /* 두 숫자 비교 후 결과 출력*/
+            printResult(calc(new StrikeBall(0, 0)));
+
+        }while(!compare());     // 두 수가 같은지 여부를 확인
     }
 }
