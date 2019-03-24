2cf9ba937fea7356b10da0845b88f6df211df6d7
Sangwon <junothyme@gmail.com>
2019-03-24 10:21:58 +0900

    게임 객체 생성

---
+++ src/main/java/Game.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Game {
+    private int strkes;
+    private int balls;
+
+    public Game(){
+        this.strkes = 0;
+        this.balls = 0;
+    }
+
+}
