58440424ddd96e4b4c05652803dbbf4cfffc681e
강연욱 <fiasboc23@gmail.com>
2019-03-24 19:03:54 +0900

    Baseball.java
    created function : compareNum()
    created function : isStrike()
    created function : isBall()

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,19 +1,53 @@
 package src.main.java;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Baseball {
 	
+	//strike 여부
+	public boolean isStrike(int a, int b) {
+		return a == b;
+	}
+
+	//ball 여부
+	public boolean isBall(int[] answer, int[] input, int digit) {
+		
+		for(int i = 0; i < 3; i++) {
+			
+			//strike에서 이미 판별
+			if(i == digit) continue;
+			
+			if(input[digit] == answer[i]) return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * strike, ball 개수 확인
+	 * @param answer	정답
+	 * @param input 	사용자 입력
+	 * @param ball_cnt	[0] : strike, [1] : ball 
+	 */
+	public void compareNum(int[] answer, int[] input, int[] ball_cnt) {
+		
+		for(int i = 0; i < 3; i++) {
+			if(isStrike(answer[i], input[i])) ball_cnt[0]++;
+			else if(isBall(answer, input, i)) ball_cnt[1]++;
+		}
+		
+	}
+	
 	/**
 	 * 3자리 수를 각각의 자리수에 따라 분리한다
 	 * ex) 123 -> 1, 2, 3
-	 * @param num 3자리 수
-	 * @return res 자리 수에 따른 분리된 수
+	 * @param num 	3자리 수
+	 * @return res 	자리 수에 따른 분리된 수
 	 */
 	public int[] splitNum(int num) {
+		
 		int[] res = new int[3];
 		
 		res[0] = num / 100;
@@ -32,6 +66,7 @@ public class Baseball {
 	 * @return
 	 */
 	public int[] input() {
+		
 		String str;
 		int input_num;
 		int[] split_num = new int[3];
@@ -42,6 +77,7 @@ public class Baseball {
 		str = sc.nextLine();
 		input_num = Integer.parseInt(str);
 		
+		//입력한 숫자를 분리
 		split_num = splitNum(input_num);
 		
 		return split_num;
@@ -53,6 +89,7 @@ public class Baseball {
 	 * @param randNum
 	 */
 	public void createNum(int[] randNum) {
+		
 		int cnt, temp;
 		ArrayList num_list = new ArrayList();
 		
@@ -62,8 +99,7 @@ public class Baseball {
 			temp = (int) (Math.random() * 9) + 1;
 			
 			// 중복 판별
-			if(num_list.contains(temp))
-				continue;
+			if(num_list.contains(temp)) continue;
 			
 			randNum[cnt] = temp;
 			num_list.add(temp);
@@ -73,21 +109,20 @@ public class Baseball {
 
 	public void game() {
 		
-		int strike, ball;
 		int[] answer = new int[3];
-		int[] num = new int[3];
+		int[] input_num = new int[3];
+		int[] ball_cnt = new int[2];
 		
 		createNum(answer);
 		
 		while(true) {
-			strike = 0;
-			ball = 0;
 			
-			num = input();
+			input_num = input();
 			
-			for(int i = 0; i < 3; i++) {
-				System.out.println(num[i]);
-			}
+			//strike, ball 개수 세기
+			ball_cnt[0] = 0;
+			ball_cnt[1] = 0;
+			compareNum(answer, input_num, ball_cnt);
 			
 			break;
 		}
