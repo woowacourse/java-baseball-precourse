a79c76b721ca2e4f7b1bb835c57cc7191fe4b101
woung717 <woung717@gmail.com>
2019-03-25 21:22:22 +0900

    게임을 진행하는 텍스트 인터페이스 - gameStart()

--- src/main/java/woowatechcourse/woung717/baseball/BaseballDriver.java
+++ src/main/java/woowatechcourse/woung717/baseball/BaseballDriver.java
@@ -10,6 +10,8 @@
 
 package woowatechcourse.woung717.baseball;
 
+import java.util.Scanner;
+
 public class BaseballDriver {
     static boolean isValid(String s) {
         StringBuilder newString = new StringBuilder();
@@ -17,6 +19,32 @@ public class BaseballDriver {
         s.chars().distinct().forEach(c -> newString.append((char) c));
 
         return s.equals(newString.toString()) && s.matches("^[1-9]{3}$");
+    }
+
+    static void gameStart() {
+        final int STRIKE_SCORE = 3;
+        Baseball game = new Baseball(new BaseballAnswer());
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            // System.out.print(game.answer);
+            System.out.println("> 숫자를 입력해 주세요: ");
+            String query = scan.nextLine();
+
+            if (!isValid(query)) {
+                System.out.println("서로 다른 1~9로 구성된 3자리 숫자를 입력해주세요. ");
+                continue;
+            }
+
+            Baseball.Result turnResult = game.getResponse(query);
+
+            System.out.println(turnResult.getMessage());
+
+            if (turnResult.getnStrikes() == STRIKE_SCORE) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
     }
 
     public static void main(String[] args) {
