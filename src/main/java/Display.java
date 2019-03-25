be52dad57bd9defe42081b1dd3b668cbd4219fe8
HeavyRyan92 <heavyryan92@gmail.com>
2019-03-26 03:07:38 +0900

    matchDisplay(int[] baseballResult) 메소드  - 비교 결과를 저장하고 있는 배열을 매개변수로 받아 결과 출력

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -6,9 +6,37 @@
  */
 public class Display {
 
-    /*사용자 입력을 요구하는 문구를 출력하는 메소드 */
+    /* 사용자 입력을 요구하는 문구를 출력하는 메소드 */
     public static void inputDisplay() {
-        System.out.print("숫자를 입력해주세요 : ");  //사용자 입력 문구 출력
+
+        System.out.print("숫자를 입력해주세요 : ");  // 사용자 입력 문구 출력
+    }
+
+    /*비교 결과 출력, 비교 결과가 저장되있는 배열을 매개변수로 받는다*/
+    public static void matchDisplay(int[] baseballResult){
+
+        if((baseballResult[0] == 0) && (baseballResult[1] == 0)){
+
+            /* 0스트라이크 0볼 일때 */
+            System.out.println(" ");
+
+        } else if((baseballResult[0] != 0) && (baseballResult[1] == 0)){
+
+            /* 1스트라이크 이상  0볼 일때 */
+            System.out.println(baseballResult[0] + " 스트라이크");
+
+        } else if ((baseballResult[0] == 0) && (baseballResult[1] != 0)) {
+
+            /* 0스트라이크 1볼 이상 일때 */
+            System.out.println(baseballResult[1] + " 볼");
+
+        } else if((baseballResult[0] != 0) && (baseballResult[1]) != 0){
+
+            /* 1스트라이크 이상 1볼 이상 일때 */
+            System.out.println(baseballResult[0] + " 스트라이크" + baseballResult[1] + " 볼");
+
+        }
+
     }
 
 }
