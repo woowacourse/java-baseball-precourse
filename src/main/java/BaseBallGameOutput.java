017b9aff1f55633935dc96940d54bd3276fc0dff
Leehaewon <leehaewon68@gmail.com>
2019-03-26 02:54:55 +0900

    add BaseBallGameOutput Class

---
+++ src/main/java/BaseBallGameOutput.java
@@ -0,0 +1,18 @@
+public class BaseBallGameOutput {
+    public void printResult(BaseBallGameResult baseBallGameResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if(baseBallGameResult.getStrike() == 3) {
+            stringBuilder.append(baseBallGameResult.getStrike() + " 스트라이크 ");
+            stringBuilder.append("\n3개의 숫자를 모두 맞히셨습니다.");
+
+        } else if(baseBallGameResult.getStrike() != 0) {
+            stringBuilder.append(baseBallGameResult.getStrike() + " 스트라이크 ");
+        }
+
+        if(baseBallGameResult.getBall() != 0) {
+            stringBuilder.append(baseBallGameResult.getBall() + " 볼");
+        }
+
+        System.out.println(stringBuilder);
+    }
+} 
