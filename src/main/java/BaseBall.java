4e780e4990b5edafb9c77aac9b236ed23ff2e317
andole87 <andole87@gmail.com>
2019-03-25 17:56:15 +0900

    getRandomNumber 메서드 생성

---
+++ src/main/java/BaseBall.java
@@ -0,0 +1,17 @@
+/*
+ * BaseBall 클래스는 야구게임의 기본 규칙과 로직을 담는다.
+ * 메서드별 @param, @return 표시한다.
+ */
+import java.util.Random;
+
+public class BaseBall {
+    /**
+     * @return  3자리 랜덤 숫자
+     */
+    int getRandomNumber() {
+        int result;
+        Random rd = new Random();
+        result = rd.nextInt(899) + 100;
+        return result;
+    }
+}
