96de6865e9a1580ecbc13b03c0e49c258ae61c6f
Leehaewon <leehaewon68@gmail.com>
2019-03-26 02:50:38 +0900

    add BaseBallGameResult Class, checking strike count

---
+++ src/main/java/BaseBallGameResult.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class BaseBallGameResult {
+    public static final int BASEBALL_NUMBER_COUNTING_SIZE = 3;
+    private int strike = 0;
+    private int ball = 0;
+    private List<BaseNumber> computerBaseBallNumber;
+    private List<BaseNumber> userBaseBallNumber;
+
+    public BaseBallGameResult(List<BaseNumber> userBaseBallNumber, List<BaseNumber> computerBaseBallNumber) {
+        this.userBaseBallNumber = userBaseBallNumber;
+        this.computerBaseBallNumber = computerBaseBallNumber;
+        checkStrike();
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    private void checkStrike() {
+        for(int i = 0; i < BASEBALL_NUMBER_COUNTING_SIZE; i++) {
+            if (userBaseBallNumber.get(i).getNumber() == computerBaseBallNumber.get(i).getNumber())
+                strike++;
+        }
+    }
+} 
