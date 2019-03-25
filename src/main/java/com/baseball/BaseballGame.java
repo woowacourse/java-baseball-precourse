e6f010c3315e9bca7da45cd2b345e4e6c99f6b0e
이영현 <leey9494@icloud.com>
2019-03-25 20:21:23 +0900

    나의 수 3개 입력

--- src/main/java/com/baseball/BaseballGame.java
+++ src/main/java/com/baseball/BaseballGame.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class BaseballGame {
 
-    public void run(){
+    public void run() {
         while (round());
     }
 
@@ -16,7 +16,37 @@ public class BaseballGame {
             System.out.print(a);
         }
 
+        System.out.println();
+
+        while (true) {
+            int[] myNum;
+
+            System.out.print("숫자를 입력해주세요 : ");
+            myNum = getMyNumbers();
+
+            for(int b : myNum) {
+                System.out.print(b);
+            }
+
+            break;
+        }
+
         return false;
+    }
+
+    /**
+     * 내가 입력하는 숫자 메소드
+     * @return
+     */
+    private int[] getMyNumbers() {
+        int[] myNumber = new int[3];
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < myNumber.length; i++) {
+            myNumber[i] = scan.nextInt();
+        }
+
+        return myNumber;
     }
 
     /**
