11a812685a9e3ecf7df324a03d466484a229ebf8
heebg2 <heebg2@naver.com>
2019-03-25 22:29:44 +0900

    기능 1 : 컴퓨터 정답 생성 - 1~9 사이의 3자리수 정답 생성하는 함수 추가 (makeAnswer()) - 변수 이름 변경

--- src/main/java/baseball.java
+++ src/main/java/baseball.java
@@ -12,16 +12,32 @@ public class baseball {
 	}
 	
 	public static boolean playGame() {
-		System.out.println("숫자를 입력해주세요 : ");
+		int answer = makeAnswer();
+		
+		System.out.print("숫자를 입력해주세요 : ");
+		System.out.println();
 		
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 		
 		Scanner sc = new Scanner(System.in);
-		int ansInt = sc.nextInt();
-		boolean answer = ansInt == 2 ? false : true;
+		int resInt = sc.nextInt();
+		boolean res = resInt == 2 ? false : true;
 		
-		return answer;
+		return res;
+	}
+	
+	public static int makeAnswer() {
+		int num[] = new int[3];
+		int number = 0;
+		Random rand = new Random();
+		for(int i = 0 ; i < num.length ; i++) {
+			num[i] = rand.nextInt(9) + 1;
+			number += num[i]*Math.pow(10, i);
+		}
+		//System.out.println(number);
+		
+		return number;
 	}
 
 }
