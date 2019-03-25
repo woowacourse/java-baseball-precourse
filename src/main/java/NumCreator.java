e20c599b6bf8cfe41160c2722a5235997057296b
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-03-25 15:00:07 +0900

    [feature_create_3_num] create three different numbers

--- src/main/java/NumCreator.java
+++ src/main/java/NumCreator.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 public class NumCreator {
     // TODO
     public static String getNum(){
-        return "test";
+        Random rand = new Random();
+        while(true){
+            int num = rand.nextInt(900)+100;
+            String input = String.valueOf(num);
+            if((input.charAt(0) == input.charAt(1))
+                    || (input.charAt(1) == input.charAt(2))
+                    || (input.charAt(2) == input.charAt(0))){
+                continue;
+            }
+            return input;
+        }
     }
 }
