e768cf58c76ad4164098f8a581e8cab08608db9a
andole87 <andole87@gmail.com>
2019-03-25 20:08:00 +0900

    Scores 클래스 생성, 생성자 생성

---
+++ src/main/java/Scores.java
@@ -0,0 +1,13 @@
+/*
+ * 야구게임의 점수를 담는 클래스.
+ */
+
+public class Scores {
+    private int strike;
+    private int ball;
+
+    Scores(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
