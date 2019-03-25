78bf0321f5aca610a06d04aa893e5bfa9f315644
parkjunghun <go9018@naver.com>
2019-03-25 12:36:26 +0900

    add : 컴퓨터 클래스 생성 , 중복되지않는 세자리 랜덤숫자 생성

---
+++ src/main/java/Computer.java
@@ -0,0 +1,38 @@
+public class Computer {
+
+    private int[] computerNum;
+
+    Computer() {
+        computerNum = new int[3];
+        saveComputerNums();
+    }
+
+    private void saveComputerNums() {
+        boolean flag = true;
+        int second, third;
+        computerNum[0] = makeRandom();
+
+
+        while (flag) {
+
+            second = makeRandom();
+            third = makeRandom();
+
+            if (computerNum[0] != second && computerNum[0] != third && second != third) {
+                computerNum[1] = second;
+                computerNum[2] = third;
+                flag = false;
+            }
+        }
+    }
+
+    private int makeRandom() {
+        return (int) (Math.random() * 9) + 1;
+    }
+
+
+    int[] getComputerNum() {
+        return computerNum;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Computer computer = new Computer();
+        computer.getComputerNum();
+
+
+
+
     }
 }
 
