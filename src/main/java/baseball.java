32d2ec63eb616e0edc76ea57e4481861863dc596
heebg2 <heebg2@naver.com>
2019-03-25 22:59:40 +0900

    기능 3 : 사용자로부터 숫자 입력받기

--- src/main/java/baseball.java
+++ src/main/java/baseball.java
@@ -32,7 +32,8 @@ public class baseball {
 		int answer = makeAnswer();
 		
 		System.out.print("숫자를 입력해주세요 : ");
-		System.out.println();
+		Scanner num = new Scanner(System.in);
+		int number = num.nextInt();
 		
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
