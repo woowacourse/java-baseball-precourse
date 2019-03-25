a591e6c3e025b878795a11587ee21f3e4a04493f
woung717 <woung717@gmail.com>
2019-03-25 18:18:44 +0900

    입력으로 부터 스트라이크인 숫자 찾기 - Baseball 클래스, getStrikes()

---
+++ src/main/java/woowatechcourse/woung717/baseball/Baseball.java
@@ -0,0 +1,35 @@
+/*
+ * Baseball class
+ *
+ * v0.1
+ *
+ * 2019.03.24
+ *
+ * Copyrights
+ */
+
+package woowatechcourse.woung717.baseball;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class Baseball {
+    private String answer;
+
+    public Baseball(BaseballAnswer ans) {
+        this.answer = ans.getAnswer();
+    }
+
+    private Set<String> getStrikes(String query) {
+        Set<String> strikeNumbers = new HashSet<>();
+
+        for (int i = 0; i < 3; i++) {
+            if (this.answer.charAt(i) == query.charAt(i)) {
+                strikeNumbers.add(this.answer.substring(i, i + 1));
+            }
+        }
+
+        return strikeNumbers;
+    }
+
+}
