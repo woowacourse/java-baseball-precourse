89ac518ca35cbc9a5dbd832973eb80c158c87688
이영현 <leey9494@icloud.com>
2019-03-25 20:39:39 +0900

    게임 종료 , 재시작 중 선택 / 게임 종료 또는 재시작

--- src/main/java/com/baseball/BaseballGame.java
+++ src/main/java/com/baseball/BaseballGame.java
@@ -33,11 +33,23 @@ public class BaseballGame {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
-
-            break;
         }
 
-        return false;
+        return isReplay();
+    }
+
+    /**
+     * 라운드 재시작 확인 메소드
+     * @return
+     */
+    private boolean isReplay() {
+        int num = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        num = scan.nextInt();
+
+        return num == 1;
     }
 
     /**
