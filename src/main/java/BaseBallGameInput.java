4d46f6e48dbe09b6d22c257f1c7d3727966be392
Leehaewon <leehaewon68@gmail.com>
2019-03-26 02:59:17 +0900

    modify BaseBallNumberClass, add askGameRestartOrExit function(BaseBallGameInput)

--- src/main/java/BaseBallGameInput.java
+++ src/main/java/BaseBallGameInput.java
@@ -2,4 +2,8 @@ public class BaseBallGameInput {
     public void askBaseBallNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    public void askGameRestartOrExit() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 } 
--- src/main/java/BaseBallNumberGame.java
+++ src/main/java/BaseBallNumberGame.java
@@ -3,13 +3,27 @@ import java.util.Scanner;
 
 public class BaseBallNumberGame {
     public static void main(String[] args) {
+        int GAME_START = 1;
         BaseBallGameInput baseBallGameInput = new BaseBallGameInput();
+        BaseBallGameOutput baseBallOutput = new BaseBallGameOutput();
         Scanner scanner = new Scanner(System.in);
+        int gameProgressNum = GAME_START;
+        BaseBallNumberGenerator baseBallNumberGenerator = new BaseBallNumberGenerator();
+        List<BaseNumber> computerBaseBallNumber = baseBallNumberGenerator.getBaseBallNumbers();
 
-        baseBallGameInput.askBaseBallNumbers();
-        int inputBaseBallNumber = scanner.nextInt();
-        BaseBallNumber baseBallNumber = new BaseBallNumber(inputBaseBallNumber);
-        List<BaseNumber> userBaseBallNumber = baseBallNumber.getBaseBallNumbers();
-
+        while(gameProgressNum == GAME_START) {
+            baseBallGameInput.askBaseBallNumbers();
+            int inputBaseBallNumber = scanner.nextInt();
+            BaseBallNumber baseBallNumber = new BaseBallNumber(inputBaseBallNumber);
+            List<BaseNumber> userBaseBallNumber = baseBallNumber.getBaseBallNumbers();
+            BaseBallGameResult baseBallResult = new BaseBallGameResult(userBaseBallNumber, computerBaseBallNumber);
+            baseBallOutput.printResult(baseBallResult);
+            if(baseBallResult.getStrike() == 3) {
+                baseBallGameInput.askGameRestartOrExit();
+                gameProgressNum = scanner.nextInt();
+                baseBallNumberGenerator = new BaseBallNumberGenerator();
+                computerBaseBallNumber = baseBallNumberGenerator.getBaseBallNumbers();
+            }
+        }
     }
 } 
