3fb68e35e790bba98512e2c19a146ec11fd2b764
HeavyRyan92 <heavyryan92@gmail.com>
2019-03-25 20:53:42 +0900

    Number 클래스  - 주석 수정
    create RandomNumber() 메소드
     - 주석 수정
    
    Display 클래스
     - baseballGame에 필요한 문구 출력 클래스 추가
    
    inputDisplay()
     - 사용자 입력 문구 추가

---
+++ src/main/java/Display.java
@@ -0,0 +1,14 @@
+/**
+ * DIsplay 클래스는 baseballGame에 필요한 문구를 출력하는 클래스 입니다.
+ *
+ * @version         1.0 2019년 3월 25일
+ * @author          이중혁
+ */
+public class Display {
+
+    /*사용자 입력을 요구하는 문구를 출력하는 메소드 */
+    public static void inputDisplay() {
+        System.out.print("숫자를 입력해주세요 : ");  //사용자 입력 문구 출력
+    }
+
+}
--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -1,21 +1,18 @@
 import java.util.Random;
 
 /**
- * Number 클래스는 baseballGame에 필요한 숫자 생성과 입력을 받는 class
+ * Number 클래스는 baseballGame에 필요한 숫자 생성과 입력을 받는 클래스 입니다.
  *
- * @version         1.0 2019년 5월 21일
+ * @version         1.0 2019년 3월 25일
  * @author          이중혁
  */
 public class Number {
 
-    /**
-     * 난수 생성 메소드
-     * @return int형
-     */
+    /*100~999까지의 난수를 생성하는 메소드*/
     public static int createRandomNumber(){
-        Random createNumber=new Random();   //난수 생성을 쓰기위한 인스턴스 생성
-        int randomNumber = createNumber.nextInt(900)+100;   //100~999까지 난수를 생성하여 randomNumber에 저장
+        Random createNumber=new Random();   //난수 인스턴스 생성
+        int randomNumber = createNumber.nextInt(900)+100;   //100~999까지 난수 저장
 
-        return randomNumber;    //생성된 randomNumber 반환
+        return randomNumber;
     }
 }
