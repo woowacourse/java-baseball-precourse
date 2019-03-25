5fe6ff48dc0295a906af44f51385795fdbbff8b6
men7627 <men1219@hanmail.net>
2019-03-26 01:25:49 +0900

    Create Menu class and printMenu Method

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ *@(#)Main.java           1.0 2019/03/26
+ *Copyright (c) 2019 Hyogeon Kim.
+ *Number Baseball Game, Java, Mungyeong, KOREA
+ */
+
+/*
+ * main class
+ * @version               1.0 2019년 03월 26일
+ * @author                김효건
+ */
+public class Main {
+    /*메뉴 출력*/
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.printMenu();
+    }
+}
---
+++ src/main/java/Menu.java
@@ -0,0 +1,19 @@
+/*
+ *@(#)Menu.java           1.0 2019/03/26
+ *Copyright (c) 2019 Hyogeon Kim.
+ *Number Baseball Game, Java, Mungyeong, KOREA
+ */
+
+/*
+ * 야구게임 메뉴(게임실행/게임종료)에 대한 출력/선택의 역할을 수행하는 클래스
+ * @version               1.0 2019년 03월 26일
+ * @author                김효건
+ */
+public class Menu {
+    /*메뉴 출력 구현*/
+    public void printMenu(){
+        System.out.println("게임 시작 (1)");
+        System.out.println("게임 종료 (2)");
+    }
+    /*게임 시작,종료에 대한 메뉴를 출력*/
+}
