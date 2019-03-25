965eccae5a3a72599c1e8132013dbb0b7867526b
Sunho Van <vsh123@naver.com>
2019-03-25 22:03:10 +0900

    기능 목록 1 - Main.java 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+/*
+ * @(#)Main.java        0.1 2019/03/25
+ *
+ *
+ */
+
+
+/**
+ * BaseballGame 실행을 위한 Main클래스 입니다.
+ *
+ * @version         0.1 2019년 3월 25일
+ * @author          반선호
+ */
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
