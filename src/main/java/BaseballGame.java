dc54b8fea1cfc0409d4471d47941a15b085dffcb
changhoi <0417kch@naver.com>
2019-03-26 00:08:48 +0900

    컨벤션을 읽고 주석을 정리함

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,14 +1,34 @@
+/*
+ * BaseballGame
+ *
+ * 1.0.0
+ *
+ * 2019.03.25
+ *
+ * MIT
+ */
+
 import java.util.Scanner;
 import java.util.Random;
 import java.util.Arrays;
 
+/**
+ * 숫자 야구 게임을 구현한 클래스 입니다.
+ *
+ * @version 1.0.0
+ * @author changhoi
+ */
+
 public class BaseballGame {
-    static boolean isWin(int strike) {
+
+    /* 게임을 이겼는지 졌는지를 판단해주는 함수입니다. */
+     static boolean isWin(int strike) {
         if(strike == 3) return true;
         return false;
     }
 
-    static int[] makeArray() {
+    /* 컴퓨터가 임의로 3개의 1~9의 숫자를 생성해 저장한 배열을 리턴하는 함수입니다. */
+     static int[] makeArray() {
         Random rand = new Random(System.currentTimeMillis());
         int[] arr = new int[3];
         for (int i = 0; i < 3; i++) {
@@ -17,6 +37,7 @@ public class BaseballGame {
         return arr;
     }
 
+    /* 사용자가 입력한 세 자리 숫자를 각 자리 별로 나누어 저장한 배열을 리턴하는 함수입니다. */
     static int[] inputArray(int val) {
         int[] arr = new int[3];
         for (int i = 2; i >= 0; i--) {
@@ -26,6 +47,7 @@ public class BaseballGame {
         return arr;
     }
 
+    /* 스트라이크의 수를 리턴하는 함수입니다. */
     static int countStrike(int[] answer, int[] arr) {
         int count = 0;
         for (int i = 0; i < 3; i++) {
@@ -35,6 +57,10 @@ public class BaseballGame {
         return count;
     }
 
+    /**
+     *  볼의 수를 리턴하는 함수입니다. 다만 스트라이크의 수를 포함한 수를 리턴합니다.
+     *  배열을 정렬하기 때문에 countStrike() 함수 이후 사용해야 합니다.
+     */
     static int countBall(int[] answer, int[] arr) {
         Arrays.sort(answer);
         int count = 0;
@@ -45,6 +71,7 @@ public class BaseballGame {
         return count;
     }
 
+    /* 스트라이크와 볼 결과를 표시해주는 함수입니다. */
     static void printResult(int strike, int ball) {
         if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
