0da73e359a1b6faf2f6b6162b5a66d9a2f6c2ca5
skygl <jupiter9501@gmal.com>
2019-03-25 16:37:07 +0900

    Implement 난수발생 메소드

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,23 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class BaseballGame {
 
-    public BaseballGame(){
+    List<Integer> answer;
+    Random random;
+
+
+    public BaseballGame() {
+    }
+
+    public void init() {
+        answer = new ArrayList<>();
+        random = new Random();
+    }
+
+    public void start() {
+        setAnswer();
+    }
+
+    public void reset() {
 
     }
 
-    public void init(){
+    public void close() {
 
     }
 
-    public void start(){
-
-    }
-
-    public void reset(){
-
-    }
-
-    public void close(){
-
+    private void setAnswer() {
+        do {
+            int i = random.nextInt(9) + 1;
+            if (!answer.contains(i))
+                answer.add(i);
+        } while (answer.size() < 3);
     }
 
 }
