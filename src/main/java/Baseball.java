1e66789c633bbfc1399c4cdf14674e31cd7d7307
os94 <ss1511@naver.com>
2019-03-25 22:42:17 +0900

    add baseballGame Fn

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * Baseball Class
  * @version 1.0
@@ -13,10 +15,38 @@ public class Baseball {
         while (restart) {
             restart = baseballGame();
         }
-        System.out.println("게임을 종료합니다.");
     }
 
     public static boolean baseballGame() {
-        return false;
+        Scanner sc = new Scanner(System.in);
+        int key = 0;
+        int num = 0;
+        int nextGame = 0;
+        boolean result = false;
+
+        // generateKey()
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            num = sc.nextInt();
+            if(matchKey(key, num)) {
+                break;
+            }
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        nextGame = sc.nextInt();
+        if(nextGame == 1) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public static boolean matchKey(int key, int num) {
+        return true;
     }
 }
