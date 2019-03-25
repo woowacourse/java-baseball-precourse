10c43405762b222fd50fb8743f21cd2cd2d19be4
choihz <coiio@naver.com>
2019-03-25 20:28:41 +0900

    main() 수정하여 완성

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class BaseballGame {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        boolean reGame;
+
+        do {
+            doGame();
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int answer = scan.nextInt();
+
+            if (answer == 1) {
+                reGame = true;
+            } else {
+                reGame = false;
+            }
+        } while (reGame);
+
+        scan.close();
     }
 
     int[] makeComNum() {
