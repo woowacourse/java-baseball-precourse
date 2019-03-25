71730f9ca555a4cb569fe3eafc8277fec0fd84bd
andole87 <andole87@gmail.com>
2019-03-25 20:17:36 +0900

    Main 클래스, main 메서드 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * 패키지의 메인 메서드를 가진 클래스.
+ * PlayBall 인스턴스를 얻은 뒤 playGame()을 호출하는 것으로 프로그램을 시작한다.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        PlayBall pb = new PlayBall();
+        pb.playGame();
+    }
+}
