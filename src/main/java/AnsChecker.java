36c95d3eb7bdc79c9e477743116f4d12c8303f91
LeeJaeilHan <wodlf1212@hanyang.ac.kr>
2019-03-25 14:18:29 +0900

    [feature_game_interface] modify for indent depth

--- src/main/java/AnsChecker.java
+++ src/main/java/AnsChecker.java
@@ -1,6 +1,7 @@
 public class AnsChecker {
     // TODO
-    public static boolean checkAns(String input, String target){
-        return true;
+    public static void checkAns(String input, String target){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         while(true){
             String input = InputResolver.getInput();
-            if(AnsChecker.checkAns(input,target)){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                if(CmdResolver.resolveCmd()) break;
-            }
+            AnsChecker.checkAns(input,target);
+            if(CmdResolver.resolveCmd()) break;
         }
     }
 }
