9910efd54a3410f6d2b586129327ecc3f042430e
이인권 <hjun4512@gmail.com>
2019-03-26 18:48:06 +0900

    구현 4 - 플레이어의 수가 컴퓨터의 난수 와 맞는지 점수를 내는 함수 구현 (스트라이크, 볼, 낫싱)

--- src/main/java/Feature.java
+++ src/main/java/Feature.java
@@ -1,2 +1,41 @@
+import java.util.HashMap;
+
 public class Feature {
+
+    // 클래스 안에 클래스 holder를 두는 Singleton
+    private Feature(){ }
+    private static class Featureholder{ public static final Feature INSTANCE = new Feature();}
+    public static Feature getInstance(){ return Feature.Featureholder.INSTANCE; }
+
+    //플레이어와 컴퓨터의 점수를 내기위한 체크 함수
+    public int checkNum(HashMap hashMap, String[] playerNum){
+        int strike = 0, ball = 0, nothing=0;
+
+        for(int i = 0 ; i < playerNum.length ; i++){
+            if(!hashMap.containsValue(Integer.parseInt(playerNum[i]))){
+                nothing++;
+            } else if((int)hashMap.get(i) == Integer.parseInt(playerNum[i])){
+                strike++;
+            } else{
+                ball++;
+            }
+        }
+
+        printScore(strike, ball, nothing);
+        return strike;
+    }
+
+    // 점수 출력문
+    private void printScore(int strike, int ball, int nothing){
+        if(nothing == 3){
+            System.out.println("낫싱");
+        } else if(strike == 3){
+            System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
+        } else if(ball == 0 || strike == 0){
+            if(ball == 0) System.out.println(strike + " 스트라이크");
+            if(strike == 0) System.out.println(ball + " 볼");
+        } else{
+            System.out.println(strike + " 스트라이크, " + ball + " 볼");
+        }
+    }
 }
--- src/main/java/PlayGame.java
+++ src/main/java/PlayGame.java
@@ -5,19 +5,21 @@ import java.util.HashMap;
 
 public class PlayGame {
 
-    private HashMap hashMap;
-    private String playerNum[];
-
     public void startGame() throws IOException {
+
+        HashMap hashMap;
+        String[] playerNum;
+        int gameResult;
+
         System.out.println("숫자 야구게임을 시작합니다.");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         hashMap = Number.getInstance().comNum();
-        playerNum = Number.getInstance().playerNum(br);
 
-        for (Object data : playerNum) {
-            System.out.print(data);
-        }
-        System.out.println("");
+        do {
+            playerNum = Number.getInstance().playerNum(br);
+            gameResult = Feature.getInstance().checkNum(hashMap, playerNum);
+        }while(gameResult != 3);
+
     }
 }
