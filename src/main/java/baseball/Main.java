c3f63e9c2bd07956bd22557ffbc98fb0710f9884
hongduk kim <2012103911@khu.ac.kr>
2019-03-27 02:44:48 +0900

    숫자를 비교하여 스트라이크와 볼을 세는 기능

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -22,20 +22,20 @@ import java.util.stream.Stream;
  * 3. 입력 받은 수가 세자리 수 인지 확인하는 기능
  */
 public class Main {
-    static int strike = 0;
-    static int ball = 0;
     public static void main(String args[]) {
+        int strike = 0;
         Main myTest = new Main();
 
-        int a[] = myTest.randNumGenerator();    //randNumGenerator함수로부터 랜덤 값을 받아온다
+        int comnum[] = myTest.randNumGenerator();    //randNumGenerator함수로부터 랜덤 값을 받아온다
         //테스트용 출력
 //        for (int i = 0; i<a.length; i++) {
 //            System.out.print(a[i]);
 //        }
         while(strike < 3) {  //스트라이크 3이 안되면 반복
-            int b = myTest.numReader();     //numReader로 부터 값을 입력 받음
-            myTest.checkNum(b);
-            System.out.println(b);     //Test용 출력
+            int a = myTest.numReader();     //numReader로 부터 값을 입력 받음
+            int usernum[] = myTest.checkNum(a);
+            int[] count = myTest.compNum(comnum, usernum);
+
         }
     }
 
@@ -70,19 +70,34 @@ public class Main {
         return sc.nextInt();//입력받은 숫자를 반환한다.
     }
     // checkNum은 사용자로 입력받은 숫자가 3자리 수인지, 양의 정수인지 판별해 준다.
-    private void checkNum(int number){
-        //정수를 배열 형태로 바꾸어줌
-        if(number < 0){
+    private int[] checkNum(int number){
+        int[] a = {-1};
+        if(number < 0){ //입력 받은 수가 양수인지 확인한다.
             System.out.println("음수는 입력할 수 없습니다");
-            return;
+            return a;
         }
+        //입력받은 수를 배열 형태로 바꾸어준다
         int[] digits = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
-        for (int i = 0; i < digits.length; i++) {
+        for (int i = 0; i < digits.length; i++) {    //입력 받은 수가 세자리 수인지 확인해준다.
             if (digits.length >= 4 || digits.length <= 2) {
                 System.out.println("세자리 수를 입력하세요");
-                return;
+                return digits;
             }
         }
-        return;
+        return a;
+    }
+    // compNum 은 사용자가 만든 숫자와 컴퓨터가 만든 숫자를 비교하여 ball 과 strike를 세는 역할을 한다.
+    private int[] compNum(int[] comnum, int[] usernum){
+        int[] count = {0,0};//strike, ball 카운트이다.
+        for (int i = 0; i<3; i++){
+            if (comnum[i] == usernum[i]){
+                count[0] += 1;//strike 카운트를 하나 늘린다.
+            }
+            else if (usernum[i] == comnum[1] || usernum[i] == comnum[2] || usernum[i] == comnum[0]){
+                count[1] +=1; //ball count를 하나 늘린다.
+            }
+
+        }
+        return count;
     }
 }
