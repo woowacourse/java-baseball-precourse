17b39b355e2cc1e340fee9b719636bb464647827
jihunoh <ohji100629@naver.com>
2019-03-27 01:03:33 +0900

    add : 2-ii 기능구현

--- src/main/java/com/github/offMomySon/resource/Computer.java
+++ src/main/java/com/github/offMomySon/resource/Computer.java
@@ -25,17 +25,31 @@ public class Computer {
 
     /**
      * @desc : 서로다른 1~9인 정수 3개를 만드는 함수
+     * 이미 존재하는 수를 만들거나, 유효하지 않은 수이면 다시 만든다.
      */
     public void create_randnum() {
         while(numsMap.size()< 3){
             int new_number = (int)(Math.random()*10) +1;
 
+            if(is_keyValid(new_number) == false)
+                continue;
             numsMap.put(new_number, new_number);
         }
 
         for( Integer num : numsMap.keySet()){
             numsList.add(num);
         }
+    }
+
+    /**
+     * @desc : 새로 만들어진 수가 중복하지 않으며, 유효한 수인지 검사
+     * @param : 새로 만들어진 수
+     */
+    public boolean is_keyValid(int new_number){
+        if( new_number>= 10  || numsMap.containsKey(new_number))
+            return false;
+        else
+            return true;
     }
 
     /**
