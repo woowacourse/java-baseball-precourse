eca79cc9a1d87be4d3907e65887a0b8437db02d7
hoonha <sosqsz2@naver.com>
2019-03-27 04:40:56 +0900

    사용자 숫자 입력

--- src/main/java/baseball.java
+++ src/main/java/baseball.java
@@ -10,11 +10,17 @@ public class baseball {
     /* 숫자 자릿수 선언 */
     private static final int DIGIT = 3;
 
+    /* 사용자 입력 변수 선언 */
+    private static String user_Number = "";
+
     /* 중복 숫자를 줄이는 방법 LinkedHashSet 사용 입력 순서로 추가 */
     private static Set<String> com = new LinkedHashSet<>();
 
     /* 컴퓨터 Set 숫자를 저장할 배열 */
     private static String[] com_Array = new String[DIGIT];
+
+    /* 사용사 숫자 저장하는 배열 */
+    private static String[] user_Array = new String[DIGIT];
 
 
     /* 컴퓨터 임의의 3자리의 수를 선택하는 메소드 */
@@ -37,6 +43,17 @@ public class baseball {
         while(iterator.hasNext()){
             com_Array[k++] = iterator.next();
         }
+    }
+
+    /* 사용자 숫자 입력 메소드*/
+    private static void getUser_Input_Number(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("숫자를 입력해주세요 : ");
+        user_Number =  sc.nextLine();
+
+        user_Array = user_Number.split("");
+
     }
 
     public static void main(String[] args){
