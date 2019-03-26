c6d53d61387023073bb5faaec3a69e2dec5db2e2
Soojin Roh <rsj3112@gmail.com>
2019-03-26 15:06:42 +0900

    Baseball.java 시작주석 작성 및 접근제한자 변경

--- src/main/java/com/woowa/precourse/Baseball.java
+++ src/main/java/com/woowa/precourse/Baseball.java
@@ -1,16 +1,25 @@
+/*
+ * @Baseball.java        1.00 2019/03/26
+ * 
+ * Copyright(c) 2019         Soojin Roh
+ * All rights reserved.
+ * 
+ * BaseballGame.java에서 작성할 추상 method를 구현한 interface입니다.
+ */
+
 package com.woowa.precourse;
 
 public interface Baseball {
 
-    public int[] makeThreeRandomNumbers();
-    public int[] inputUserNumbers(); 
-    public boolean checkInputNumbers(String inputNumber);
-    public boolean checkDuplicate(int[] intArray, int index);
-    public int[] convertStringToIntArray(String inputNumber); 
-    public boolean checkNumberOrNot(String inputNumber); 
-    public boolean checkInputNumberLength(String inputNumber); 
-    public int[] checkStrikeOrBall(int[] comNumbers, int[] userNumbers);
-    public void runGame(); 
-    public void reGame();
+    abstract int[] makeThreeRandomNumbers();
+    abstract int[] inputUserNumbers(); 
+    abstract boolean checkInputNumbers(String inputNumber);
+    abstract boolean checkDuplicate(int[] intArray, int index);
+    abstract int[] convertStringToIntArray(String inputNumber); 
+    abstract boolean checkNumberOrNot(String inputNumber); 
+    abstract boolean checkInputNumberLength(String inputNumber); 
+    abstract int[] checkStrikeOrBall(int[] comNumbers, int[] userNumbers);
+    abstract void runGame(); 
+    abstract void reGame();
 
 }
