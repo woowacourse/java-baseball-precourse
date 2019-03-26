0504846d4f12ac501dc474dd4738650d7c264b42
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-03-26 17:17:14 +0900

    사용자 입력 기능 추가

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,6 +3,8 @@
  *
  */
 
+import java.util.Scanner;
+
 /**
  * 게임의 진행과 입력을 담당하는 클래스
  */
@@ -27,6 +29,26 @@ public class Player {
 	 */
 	private void play() {
 		String randomNumber = NumberMaker.makeRandomNumber();
+		String userInput = null;
+
+		System.out.print("숫자를 입력해주세요: ");
+		userInput = getInput();
+
 		System.out.println(randomNumber);
+		System.out.println(userInput);
+	}
+
+	/**
+	 * 문자열을 입력받아 리턴하는 함수
+	 * @return input
+	 */
+	private String getInput() {
+		String input = null;
+		Scanner scanner = new Scanner(System.in);
+
+		input = scanner.nextLine();
+		scanner.close();
+
+		return input;
 	}
 }
