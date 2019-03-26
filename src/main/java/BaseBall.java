cdd527959582d2e427caa425a4d9d515ec099056
YUN, SUNGHO <itlockit@gmail.com>
2019-03-26 13:56:06 +0900

    Output strike & ball , Remove Computer Duplicate Numbers
    Output a strike and ball to match the user's input.
    And change the code so that computer doesn't have duplicate numbers.

--- src/main/java/BaseBall.java
+++ src/main/java/BaseBall.java
@@ -12,8 +12,10 @@ import java.util.regex.Matcher;
 public class BaseBall{
     private int numbers[] = new int[3];
     private Computer computer;
+    int compNumbers[];
     private Scanner scanner;
     private boolean isGamePlay = true;
+    private boolean isGameFinish = false;
     private Pattern p = Pattern.compile("[1-9]{3}");
     BaseBall(){
         scanner = new Scanner(System.in);
@@ -22,15 +24,54 @@ public class BaseBall{
     }
     public void init(){
         computer = new Computer();
+        compNumbers = computer.getNumbers();
     }
     public void play(){
         while(isGamePlay) {
+            int strike = 0;
+            int ball = 0;
             System.out.print("숫자를 입력해주세요 : ");
             String userInput = scanner.nextLine();
             Matcher m = p.matcher(userInput);
-            if(m.matches()){
-                // correct input
+            if(!m.matches()){
+                // if wrong input restart game
+                continue;
+            }
+            int userNumbers[] = new int[3];
+            userNumbers[0] = Integer.parseInt(userInput.substring(0,1));
+            userNumbers[1] = Integer.parseInt(userInput.substring(1,2));
+            userNumbers[2] = Integer.parseInt(userInput.substring(2,3));
+            strike = checkStrike(userNumbers, compNumbers);
+            ball = checkBall(userNumbers,compNumbers);
+            if(strike != 0 && strike != 3 && ball == 0){
+                System.out.println(strike + "스트라이크 ");
+            } else if(ball != 0){
+                System.out.print(strike + "스트라이크 ");
+                System.out.println(ball + "볼");
+            } else if(strike == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
             }
         }
+    }
+    public int checkStrike(int[] userNumbers, int[]compNumbers){
+        int count = 0;
+        for(int i = 0 ; i < 3 ; i++){
+            if(userNumbers[i] == compNumbers[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+    public int checkBall(int[] userNumbers, int[]compNumbers){
+        int count = 0;
+        for(int i = 0 ; i < 3 ; i++){
+            if(userNumbers[i] == compNumbers[(i+1)%3]){
+                count++;
+            }
+            if(userNumbers[i] == compNumbers[(i+2)%3]){
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -11,10 +11,13 @@ public class Computer {
     private int numbers[] = new int[3];
     Computer(){
         Random random = new Random();
-        // Enter a 3-digit integer.
-        for(int i = 0 ; i < 3 ; i++){
-            numbers[i] = random.nextInt(9) + 1;
-        }
+        // Enter first digit
+        numbers[0] = random.nextInt(9) + 1;
+        // Second number random until it doesn't overlap with the first number
+        while((numbers[1]  = random.nextInt(9) + 1) == numbers[0]);
+        // Third number random until it doesn't overlap with the first and second number
+        while((numbers[2]  = random.nextInt(9) + 1) == numbers[1] ||
+                (numbers[2]  = random.nextInt(9) + 1) == numbers[0]);
     }
     int[] getNumbers(){
         return this.numbers ;
