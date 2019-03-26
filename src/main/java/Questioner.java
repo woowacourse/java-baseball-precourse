4e1a2bd7e184a35600f5e833b9480dbd148ef869
hyperpace <cnk9109@gmail.com>
2019-03-27 00:22:37 +0900

    add method setNumber()

--- src/main/java/Questioner.java
+++ src/main/java/Questioner.java
@@ -4,6 +4,8 @@
  * Released under the MIT license
  */
 
+import java.util.Random;
+
 /**
  * Questioner 클래스는 숫자야구게임의 문제(서로 다른 3자리 수)를 만들고 소유한다.
  *
@@ -20,5 +22,38 @@ public class Questioner {
 	 */
 	public Questioner() {
 		this.number = new int[3]; // 3자리 수
+	}
+
+	/**
+	 * 문제(서로 다른 3자리 수)를 만든다.
+	 * @return 문제(서로다른 3자리수 배열)
+	 * */
+	public int[] setNumber() {
+
+		Random rand = new Random(); // 랜덤숫자를 반환을 위한 rand 객체
+		for(int i = 0; i < this.number.length; i++) {
+
+			int num;
+
+			do {
+
+				int bound = 9;
+				int addNum = 1;
+				num = rand.nextInt(bound) + addNum; // 1~9사이의 랜덤 숫자
+
+				/* 중복되는 숫자가 존재하는지 확인한다. */
+			} while ( checkReduplicate(num, this.number, i) );
+
+			this.number[i] = num; // 중복되지 않은 숫자를 자리에 넣는다.
+		}
+		return this.number;
+	}
+
+	private boolean checkReduplicate(int num, int[] number, int index) {
+		boolean isReduplicate = false;
+
+		// 중복 검사
+
+		return isReduplicate;
 	}
 }
