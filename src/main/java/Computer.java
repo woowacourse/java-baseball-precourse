894e280ab8d2842f530bc73f91c228c3ba157e5c
Junmo Han <wnsah052@naver.com>
2019-03-27 01:52:30 +0900

    < Computer class > 게임이 시작할 때 메세지를 출력하는 기능

---
+++ src/main/java/Computer.java
@@ -0,0 +1,24 @@
+/*
+ * @(#)Computer.java		2019/3/27
+ * 
+ * Copyright (c) 2019 Junmo Han.
+ * All rights reserved.
+ */
+
+/**
+ * Computer 클래스는 숫자 야구 게임을 진행하면서 사용자의 입력이 필요하지 않은 메소드들을 모아 놓은 클래스입니다. 
+ * 
+ * @version			1.00 2019년 3월 27일
+ * @author 			한준모
+ */
+
+public class Computer {
+
+	/** 
+	 * 게임이 시작할 때 메세지를 출력하는 메소드입니다.
+	 */
+	public void printMessage() {
+		System.out.print("숫자를 입력해주세요 : ");
+	}
+	
+}
