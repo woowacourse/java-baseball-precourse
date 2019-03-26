b1c29dfe29639983014178ea84ab9dfa9d40ea08
Sangwon <junothyme@gmail.com>
2019-03-26 16:11:39 +0900

    자바 코딩 규칙 적용 - Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,38 @@
 import java.util.Scanner;
 
-public class main {
+/**
+ * 메인 화면 구현 클래스
+ * @version 2.0
+ * @date 2019.03.23
+ * @author 이상원
+ */
+public class Main {
 
     public static void main(String[] args){
+        /*사용자로부터 받은 숫자를 저장하는 변수*/
         int humanNum;
+
         Human human = new Human();
+
         Computer com = new Computer();
+
         Game game = new Game();
+
+        /*3 스트라이크 이후 게임을 지속하는지 확인하는 변수*/
+        boolean continueFlag = true;
+
+        /*3 스트라이크를 확인하는 변수*/
+        int finishedFlag = 0;
+
+
         com.makeRandomNum();
         com.showNum();
+
         Scanner sc = new Scanner(System.in);
-        boolean continueFlag = true;
-        int finishedFlag = 0;
+
         while(continueFlag){
             System.out.print("숫자를 입력해주세요: ");
+
             humanNum = sc.nextInt();
             human.setInputNum(humanNum);
             if(human.isValid() != true){
@@ -32,6 +51,5 @@ public class main {
                 continue;
             }
         }
-
     }
 }
