a00e5188d3873a419d0568e4009ab4d8eadc21fd
이인권 <hjun4512@gmail.com>
2019-03-26 18:57:07 +0900

    구현 5 - 3자리수 맞추고 새게임 아니면 종료를 물어보는 함수 구현

--- src/main/java/PlayGame.java
+++ src/main/java/PlayGame.java
@@ -14,12 +14,26 @@ public class PlayGame {
         System.out.println("숫자 야구게임을 시작합니다.");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        hashMap = Number.getInstance().comNum();
+        do{
+            hashMap = Number.getInstance().comNum();
+            do {
+                playerNum = Number.getInstance().playerNum(br);
+                gameResult = Feature.getInstance().checkNum(hashMap, playerNum);
+            }while(gameResult != 3);
 
-        do {
-            playerNum = Number.getInstance().playerNum(br);
-            gameResult = Feature.getInstance().checkNum(hashMap, playerNum);
-        }while(gameResult != 3);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
+        }while(isExit(br));
+
+    }
+
+    // 새게임, 종료 물어보는 함수
+    public boolean isExit(BufferedReader br) throws IOException {
+        String exitNum = br.readLine();
+        if (exitNum.equals("1")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
