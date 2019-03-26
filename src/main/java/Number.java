6c19fc0204cb5d27d0161a269b8d9e647479f8f3
이인권 <hjun4512@gmail.com>
2019-03-26 18:34:35 +0900

    구현 2 - 1 ~ 9 까지 3자리 컴퓨터 난수 생성 및 난수 중복처리 함수 구현

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -1,2 +1,40 @@
+import java.util.HashMap;
+
 public class Number {
+
+    private HashMap<Integer,Integer> hashMap;
+
+    // 클래스 안에 클래스 holder를 두는 Singleton
+    private Number(){ hashMap = new HashMap<>(); }
+    private static class NumberHolder{ public static final Number INSTANCE = new Number();}
+    public static Number getInstance(){ return NumberHolder.INSTANCE; }
+
+    //컴퓨터 3자리 난수 생성 함수
+    public HashMap comNum(){
+
+        if(!hashMap.isEmpty()){
+            hashMap.clear();
+        }
+
+        for(int i = 0 ; i < 3 ; i ++){
+            int num = (int) (Math.random() * 9) + 1;
+            if(isOverLap(hashMap,num)){
+                i -= 1;
+                continue;
+            }
+            hashMap.put(i,num);
+        }
+        return hashMap;
+    }
+
+    //컴퓨터 3자리난수 중복처리 함수
+    private boolean isOverLap(HashMap hashMap, int num){
+        for(Object data : hashMap.values()){
+            if((int)data == num){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/PlayGame.java
+++ src/main/java/PlayGame.java
@@ -1,6 +1,18 @@
+import java.util.HashMap;
+
 public class PlayGame {
+
+    private HashMap hashMap;
 
     public void startGame() {
         System.out.println("숫자 야구게임을 시작합니다.");
+
+        hashMap = Number.getInstance().comNum();
+        System.out.print("컴퓨터 숫자 : ");
+        for (Object data : hashMap.values()) {
+            System.out.print(data);
+        }
+        System.out.println("");
+
     }
 }
