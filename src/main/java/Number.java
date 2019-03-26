3842261b808e9e69a7cbd68010705019cb474087
men7627 <men1219@hanmail.net>
2019-03-26 18:11:35 +0900

    Create Number class, member variables and constructor

---
+++ src/main/java/Number.java
@@ -0,0 +1,10 @@
+public class Number {
+    private int first;
+    private int second;
+    private int third;
+    public Number(int first,int second, int third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
