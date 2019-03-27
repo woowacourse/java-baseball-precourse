7822a797e2a3e4c8a3cc6564fd0303edc690ced7
seongchanchoi <chsch911028@gmail.com>
2019-03-27 20:43:16 +0900

    ADD FUNCTION: 사용자에게 메세지를 보여주는 기능

---
+++ src/main/java/BaseBallGame.java
@@ -0,0 +1,20 @@
+/**
+ * BaseBallGame
+ *
+ * @version 1.00 2019.03.27
+ * @author 최성찬
+ *
+ */
+
+public class BaseBallGame {
+
+    /** 클래스 변수 초기화 */
+    public BaseBallGame(){
+
+    }
+
+    /** 사용자에게 메세지를 보여주는 기능 */
+    public void showMessage(String msg) {
+        System.out.println(msg);
+    }
+}
