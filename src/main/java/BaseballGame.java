7f553b8fda1381b2f1a7980aaf5af0a6064d7499
blueLibertas <jinhok7890@gmail.com>
2019-03-27 23:02:50 +0900

    no message

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,11 +1,20 @@
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BaseballGame {
     public static void main(String[] args) {
     int[] computerNum = new int[3];
+    int[] playerNum = new int[3];
+
+    //get 3 different random numbers
     computerNum = fillArray(computerNum);
     // System.out.print(Arrays.toString(computerNum));
+
+    //get input from user
+    playerNum = getInput(playerNum);
+    //System.out.print(Arrays.toString(playerNum));
+
 
     }
 
@@ -25,5 +34,24 @@ public class BaseballGame {
     private static int getNewNumber() {
         Random randomNumber = new Random();
         return randomNumber.nextInt(9) + 1;
+    }
+
+    private static int[] getInput(int[] myArray) {
+        Scanner myScanner = new Scanner(System.in);
+        boolean correctInput = false;
+        int temp = -1;
+        while (!correctInput) {
+            System.out.print("숫자를 입력해주세요: ");
+            temp = myScanner.nextInt();
+            if (100 <= temp & temp <= 999 )
+                correctInput = true;
+            else
+                System.out.println("잘못된 값 입니다");
+        }
+        myArray[0] = temp/100;
+        temp = temp%100;
+        myArray[1] = temp/10;
+        myArray[2] = temp%10;
+        return myArray;
     }
 }
