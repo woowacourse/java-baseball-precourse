db197d5eb181d59c24f0eeee86a45113eaabf6d0
hjbc0921 <hjbc0921@gmail.com>
2019-03-27 20:12:59 +0900

    Constants.java : 게임 관련 상수 정의

---
+++ src/main/java/com/woowacourse/baseball/util/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * Constants.java
+ * version 1.0
+ * 2019.03.28
+ * Copyright (c) 2019 Hyunji Choi
+ * This program is made available under the terms of the MIT License.
+ */
+
+package com.woowacourse.baseball.util;
+
+/**
+ * Constants hold final int used across classes.
+ */
+public class Constants {
+
+    /* Length of the number to guess. */
+    public static final int NUMBER_LENGTH  = 3;
+
+    /* Range of digits */
+    public static final int NUMBER_LOWER_BOUND = 1;
+    public static final int NUMBER_UPPER_BOUND = 9;
+}
