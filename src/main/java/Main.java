85b5ad9ffbd41b7765bfdb7bd4b1133ef399b67a
EternalFete <comath@gmail.com>
2019-03-27 21:52:55 +0900

    Step 2-1. add test code for Step 2.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,11 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             System.out.println((baseball.computer[i]));
         }
+
+        baseball.getUserInput();
+
+        for (int j = 0; j < 3; j++) {
+            System.out.println(baseball.user[j]);
+        }
     }
 }
