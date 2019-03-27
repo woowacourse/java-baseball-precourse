beb554c2c8ae41d3fd129201f8aba65ec3a8d4fd
Yookibaek <psykibaek@gmail.com>
2019-03-27 20:28:42 +0900

    javadoc생성 및 문서주석 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,32 +6,36 @@ import java.util.Scanner;
 public class Main {
 
     /**
-     * 야구게임 1판 실행한다
+     * 숫자 야구게임 실행 함수 <br>
+     * 진행은 아래와 같다.<br>
+     * 1. 컴퓨터의 랜덤숫자를 생성 - createComputerAnswer() <br>
+     * 2. 유저의 예측 숫자를 입력받는다 - getUserPredict(String inputStr)  <br>
+     * 3. Strike갯수와, Ball갯수를 구한다 - getStrikeCount(String str1, String str2), getBallCount(String str1, String str2) <br>
+     * 4. strike가 3개가 아니라면, 2로 돌아간다.
      */
     public static void startGame(){
         String computerAnswer = createComputerAnswer(); // 컴퓨터의 정답
 
         while (true){
-            String inputStr = getUserPredict();                             // 사용자의 예측 정답
-            int strikeCount = getStrikeCount(computerAnswer, inputStr);     // Strike 개수
-            int ballCount = getBallCount(computerAnswer, inputStr);         // Ball 개수
-
+            String inputStr = getUserPredict();                             // 사용자로부터 받은 INPUT
+            int strikeCount = getStrikeCount(computerAnswer, inputStr);     // Strike 갯수
+            int ballCount = getBallCount(computerAnswer, inputStr);         // Ball 갯수
 
             printStatements(strikeCount, ballCount);                        // strike, ball 에따른 응답 출력
-            if(strikeCount == 3){                                           // strike가 3일경우, 게임 종료
+            if(strikeCount == 3){                                           // strike가 3개일 경우, 게임 종료
                 break;
             }
         }
     }
 
     /**
-     * 컴퓨터의 3자릿수 넘버를 랜덤으로 생성해 반환
-     *
-     * @return String 3자릿수 랜덤넘버 스트링
+     * 컴퓨터의 3자릿수 랜덤넘버를 생성하는 함수 <br>
+     * <br>
+     * @return 생성된 3자릿수 랜덤넘버 문자열
      */
     private static String createComputerAnswer(){
-        List leftNumList = new LinkedList();  // 중복숫자를 피하기 위해, 출력가능한 숫자 보관 List
-        String answer = "";                   // 반환 answer
+        List leftNumList = new LinkedList();  // 중복숫자를 피하기 위해, 1~9까지 숫자 List 생성(곂치지않게 하기위함)
+        String answer = "";                   // 컴퓨터 Answer 문자열
 
         // leftNumList에 1~9까지의 숫자 삽입
         for (int i = 1; i < 10; i++) {
@@ -54,13 +58,13 @@ public class Main {
     }
 
     /**
-     * 유저로부터 입력받은 3자릿수 String이 조건을 모두 만족하는지 체크
-     * 조건1: 각자릿수는 1~9 사이 일 것
-     * 조건2: 입력 String이 3자릿수 일 것
-     * 조건3: 중복되는 숫자가없을 것
-     *
+     * 유저로부터 입력받은 3자릿수 문자열이 다음의 조건을 모두 만족하는지 체크 <br>
+     * 조건1: 각자릿수는 1~9 사이 일 것 <br>
+     * 조건2: 입력 String이 3자릿수 일 것 <br>
+     * 조건3: 중복되는 숫자가없을 것 <br>
+     * <br>
      * @param inputStr      체크할 String
-     * @return boolean      모든조건을 만족하는지 여부
+     * @return boolean      모든조건 만족 여부
      */
     public static boolean checkUserInput(String inputStr){
 
@@ -106,10 +110,9 @@ public class Main {
     }
 
     /**
-     * 유저의 predict를 입력받아 반환한다. predict가 조건을 만족하지않으면, 재입력을 요청한다.
-     * 그렇기때문에, 반환되는 String은 모든 조건을 만족하는함을 보장한다.
-     *
-     * @return String       입력받은 유저의 3자리 predict 넘버
+     * 유저의 예측문자열을 입력받는다. 문자열이 조건을 만족하지않으면, 재입력을 요청한다. <br>
+     * <br>
+     * @return String 유저로부터 입력받은 3자리 랜덤넘버
      */
     public static String getUserPredict(){
         Scanner sc = new Scanner(System.in);
@@ -129,9 +132,9 @@ public class Main {
     }
 
     /**
-     * String을 2개 입력받아 Strike가 몇개인지 반환
-     * @param str1          String 1
-     * @param str2          String 2
+     * 문자열을 2개 입력받아 Strike가 몇개인지 반환. 입력문자열의 순서는 상관 없다. <br>
+     * @param str1          문자열 1(문자열 2와 순서 상관 없음)
+     * @param str2          문자열 2(문자열 1과 순서 상관 없음)
      * @return int          Strike 개수 반환
      */
     public static int getStrikeCount(String str1, String str2){
@@ -147,9 +150,10 @@ public class Main {
     }
 
     /**
-     * String을 2개 입력받아 Ball이 몇개인지 반환
-     * @param str1          String 1
-     * @param str2          String 2
+     * String을 2개 입력받아 Ball이 몇개인지 반환 <br>
+     * <br>
+     * @param str1          문자열 1(문자열 2와 순서 상관 없음)
+     * @param str2          문자열 2(문자열 1과 순서 상관 없음)
      * @return int          ball 개수 반환
      */
     public static int getBallCount(String str1, String str2){
@@ -182,10 +186,10 @@ public class Main {
     }
 
     /**
-     * ball, strike 갯수에 따른 반응을 출력
-     *
-     * @param strikeCount   strike 갯수
-     * @param ballCount     ball 갯수
+     * ball, strike 갯수에 따른 반응을 출력 <br>
+     * <br>
+     * @param strikeCount   strike 갯수 <br>
+     * @param ballCount     ball 갯수 <br>
      */
     public static void printStatements(int strikeCount, int ballCount){
 
@@ -208,9 +212,9 @@ public class Main {
     }
 
     /**
-     * 유저에게 1 혹은 2를 입력받아, 재시작을원하는지 혹은 아닌지를 반환
-     *
-     * @return boolean      재시작 여부 반환
+     * 유저에게 1 혹은 2를 입력받아, 재시작을원하는지 혹은 아닌지를 반환 <br>
+     * <br>
+     * @return boolean      재시작 여부 반환 <br>
      */
     public static boolean getRestartAnswer(){
         boolean isRestart = false;
