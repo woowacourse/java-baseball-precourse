98bdeddbe602e2fb02382305a892b7cf40767817
Yujin Kim <99stella24@naver.com>
2019-03-27 12:15:21 +0900

    get3메서드 추가

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Baseball {
     public static List<Integer> getNum3(){
@@ -11,9 +8,18 @@ public class Baseball {
         Collections.sort(subList);
         return subList;
     }
+    public static List<Integer> get3(){
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int a = num/100;
+        num%=100;
+        List<Integer> thisList = new ArrayList<Integer>(Arrays.asList(a, num/10, num%10));
+        return thisList;
+    }
 
     public static void main(String args[]){
         List<Integer> baseballNum = getNum3();
+        List<Integer> yourNum = get3();
         return;
     }
 }
