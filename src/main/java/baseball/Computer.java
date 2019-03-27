49a9758d719665837ec9cedb0a1324f0ecfc7048
hyun <jhs695@naver.com>
2019-03-27 15:00:19 +0900

    Computer class 볼 , 스트라이크 카운터 기능 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.stream.IntStream;
+
 public class Computer {
     private int num[];
 
@@ -17,6 +19,18 @@ public class Computer {
         while ((num[0] == num[2]) || (num[1] == num[2])){
             num[2] = (int)(Math.random()*9 + 1);
         }
+    }
+
+    private int countBall(int[] userNum){
+        return IntStream.range(0,3)
+                .map(i -> (num[i] == userNum[(i+1)%3]) || (num[i] == userNum[(i+2)%3]) ? 1:0)
+                .sum();
+    }
+
+    private int countStrike(int[] userNum){
+        return IntStream.range(0,3)
+                .map(i -> (num[i] == userNum[i]) ? 1:0)
+                .sum();
     }
 
 }
