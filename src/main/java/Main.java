e68f277b703f590a6a5db947e35b81433b613d6b
EternalFete <comath@gmail.com>
2019-03-27 21:12:20 +0900

    Step 1-1. add Main Class and main method. add test code for Step 1.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Baseball baseball = new Baseball();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println((baseball.computer[i]));
+        }
+    }
+}
