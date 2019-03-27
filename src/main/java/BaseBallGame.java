1c48d6d42fd926e424b220a5fa05c9f60f8624d0
seongchanchoi <chsch911028@gmail.com>
2019-03-27 20:46:27 +0900

    ADD FUNCTION: 사용자 입력을 받아 3자리 수 저장하는 기능

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 /**
  * BaseBallGame
  *
@@ -10,12 +14,18 @@ public class BaseBallGame {
 
     /** 임의의 3자리 수 */
     private int[] num;
+    /** 사용자 입력 3자리 수 */
+    private int[] userNum;
+    /** 사용자 입력을 받을 객체 */
+    private BufferedReader br;
 
     /** 클래스 변수 초기화 */
     public BaseBallGame(){
         this.num = new int[3];
+        this.userNum = new int[3];
+        this.br = new BufferedReader(new InputStreamReader(System.in));
     }
-    
+
     /** 임의의 3수를 저장 */
     public void setRandomNum() {
         for(int i=0; i<3; i++) {
@@ -36,6 +46,38 @@ public class BaseBallGame {
             if(this.num[i] == value) { res = true; break; }
         }
         return res;
+    }
+
+    /** 사용자 입력을 받아 3개의 수 저장 */
+    public boolean setUserNum() {
+        String userInput = null;
+        boolean res = false;
+
+        try {
+            this.showMessage("숫자를 입력해주세요");
+            userInput = this.br.readLine();
+            if(userInput.length() != 3) {
+                this.showMessage("유요하지 않은 입력 입니다.");
+            }else {
+                this.setUserNumArr(userInput);
+                res = true;
+            }
+        }catch(Exception e) {
+            this.showMessage("오류가 발생했습니다.");
+        }
+        return res;
+    }
+
+    /**
+     * 3자리 문자열 수를 배열로 저장
+     * @param userInput 3자리 수를 표현하는 문자열
+     */
+    public void setUserNumArr(String userInput) {
+
+        String[] arr = userInput.split("");
+        for(int i=0; i<3; i++) {
+            this.userNum[i] = Integer.parseInt(arr[i]);
+        }
     }
 
     /** 사용자에게 메세지를 보여주는 기능 */
