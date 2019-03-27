98ab0f330b88e84451cb8d472ab19677b3f1f4ba
usang <usang>
2019-03-27 16:05:00 +0900

    Main 클래스 생성 및 주석추가
    게임실행을 위한 Main 클래스를 생성하고 inputCheck(String str) 메소드 명을 isValidInput(String str)로 수정

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,3 +1,16 @@
+/*
+ * 이 클래스틑 Baseball 게임에 대한 클래스로 게임 진행을 위한 메소드와 인스턴스가 있다
+ * README.MD 에 작성한 기능별로 메소드가 있다
+ *
+ * 클래스 이름    Baseball
+ *
+ * 버전 정보     1.0
+ *
+ * 날짜    2019/03/27
+ *
+ * 저작권   권유상
+ */
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -41,7 +54,7 @@ public class Baseball {
         int random_number;
         Random random = new Random();
 
-        // 전체 숫자의 길이가 3일때 까지 1~9까지 숫자를 하나씩 생성
+        /* 전체 숫자의 길이가 3일때 까지 1~9까지 숫자를 하나씩 생성 */
         do {
             random_number = random.nextInt(8) + 1;
             // 중복 확인
@@ -62,7 +75,7 @@ public class Baseball {
             System.out.print("숫자를 입력해 주세요: ");
             Scanner scan = new Scanner(System.in);
             ret = scan.nextLine();
-            if(inputCheck(ret)) {
+            if(isValidInput(ret)) {
                 break;
             }
             else {
@@ -73,13 +86,14 @@ public class Baseball {
     }
 
     // 기능 3 - 2
-    private boolean inputCheck(String str){
-        // 3자리수가 아니면 false
+    private boolean isValidInput(String str){
+
+        /* 3자리수가 아니면 false */
         if(str.length() != 3) {
             return false;
         }
 
-        // 임시 string 변수 temp에 str의 숫자를 하나씩 옮기면서 중복확인
+        /* 임시 string 변수 temp에 str의 숫자를 하나씩 옮기면서 중복확인 */
         String temp = "";
         for(int i = 0; i < 3; i++) {
             if(temp.contains(String.valueOf(str.charAt(i)))) {
@@ -96,7 +110,7 @@ public class Baseball {
     //기능 4
     private void strikeBallCheck(String answer, String number) {
 
-        // 맞힐 숫자(answer)와 입력한 숫자(number)를 한자리씩 비교하며 strike와 ball의 개수를 count up.
+        /* 맞힐 숫자(answer)와 입력한 숫자(number)를 한자리씩 비교하며 strike와 ball의 개수를 count up */
         for(int i = 0; i < 3; i++) {
             if(answer.charAt(i) == number.charAt(i)) {
                 strike_num++;
---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+/*
+ * 이 클래스는 Baseball 게임을 실행하기 위한 클래스이다
+ * Baseball 객체를 생성하고 runGame() 메소드를 통해 실행한다
+ *
+ * 클래스 이름    Main
+ *
+ * 버전 정보     1.0
+ *
+ * 날짜    2019/03/27
+ *
+ * 저작권   권유상
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Baseball baseball = new Baseball();
+        baseball.runGame();
+    }
+
+}
