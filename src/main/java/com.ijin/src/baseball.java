5e7d4dc23bfde4f50e9e054a39dbac03108c4a6a
483759 <483759@naver.com>
2019-03-28 00:54:41 +0900

    RandomSave()메소드  인자 수정, UserInput()메소드 1차 구현

--- src/main/java/com.ijin/src/baseball.java
+++ src/main/java/com.ijin/src/baseball.java
@@ -1,14 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
 public class baseball {
-    int num[];
-    void RandomSave() {
+    public static void RandomSave(int[] num) {
+        Random random = new Random();
         for (int i = 0; i < 3; i++)
-            num[i] = ((int) Math.random() * 10) % 9 + 1;
+            num[i] = random.nextInt(10)+1;
+    }
+
+    public static int UserInput(){
+        int us_input;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        us_input = sc.nextInt();
+        return us_input;
     }
 
     public static void main(String[] args){
+        int num[] = new int[4];
+        int user_num;
         baseball b = new baseball();
-        b.RandomSave();
-        for(int i=0;i<3;i++)
-            System.out.println("%d", b.num[i]);
+        b.RandomSave(num);
+        /*
+        for(int i=0;i<3;i++){
+            System.out.print(num[i] + " ");
+        }
+        */
+        user_num = UserInput();
+        System.out.println(user_num);
     }
 }
