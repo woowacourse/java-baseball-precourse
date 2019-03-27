3e6f32cd9d746604bca92d7605a3221d9031585a
SUNGBUMPARK <psbum1609@gmail.com>
2019-03-27 20:20:28 +0900

    Add a function to create a random number
    먼저 3자리 숫자를 저장할 computerBaseballNumber 배열을 생성하고
    생성자에서 크기 3으로 초기화한다.
    
    makeRandomNumber() 메서드에서 서로 다른 3자리의 랜덤 숫자를 생성한다. 랜덤
    숫자 배열이 정확히 생성되었는지 출력으로 확인하는 코드를 임의로
    삽입하였다.
    
    checkOverlapNumber() 메서드는 배열과 현재 인덱스를 전달받으며 해당 배열의
    처음부터 현재 인덱스까지 중복이 있는지 검사한다.

--- src/main/java/com/codemcd/baseballgame/Main.java
+++ src/main/java/com/codemcd/baseballgame/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         NumberBaseball newGame = new NumberBaseball();
+        newGame.makeRandomNumber();
 
         System.out.println("게임이 종료되었습니다.");
 
--- src/main/java/com/codemcd/baseballgame/NumberBaseball.java
+++ src/main/java/com/codemcd/baseballgame/NumberBaseball.java
@@ -1,8 +1,43 @@
 package com.codemcd.baseballgame;
 
+import java.util.Random;
+
 public class NumberBaseball {
+
+    private int[] computerBaseballNumber;
 
     NumberBaseball() {
         System.out.println("숫자 야구 게임 시작!");
+
+        computerBaseballNumber = new int[3];
+    }
+
+    private static int checkOverlapNumber(int[] checkNumberArray, int currentIndex) {
+        for (int i = 0; i < currentIndex; ++i) {
+            if (checkNumberArray[currentIndex] == checkNumberArray[i]) {
+                return -1;
+            }
+        }
+
+        return 1;
+    }
+
+    public void makeRandomNumber() {
+
+        Random random = new Random();
+
+        /*
+         * for 문의 i 값을 증가시키는 조건은 현재 숫자가 이전의 숫자들과 비교했을 때
+         * 중복이 없는 경우이다.
+         */
+        for (int i = 0; i < 3; i += checkOverlapNumber(computerBaseballNumber, i)) {
+            computerBaseballNumber[i] = random.nextInt(9) + 1;
+        }
+
+        /* 기능 확인을 위해 잠시 추가해둠 */
+        for (int i = 0; i < 3; ++i) {
+            System.out.print(computerBaseballNumber[i]);
+        }
+        System.out.println();
     }
 }
