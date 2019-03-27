901aad97503236aafa50dd241240df44146bb55e
ponyong <ponyong@naver.com>
2019-03-28 00:51:40 +0900

    Create strike count method ( 2.6 )

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,22 +1,34 @@
 
 public class Computer {
-	public int targetnum = 0;
+	
+	public int[] target_num = new int[3];
 	
 	public void create_num() {
-		int num1, num2, num3;
 		
 		while(true) {
 			// 임의의 세 숫자를 랜덤함수 를 통해 생성 (1 ~ 9)
-			num1 = (int) (Math.random() * 9 + 1);
-			num2 = (int) (Math.random() * 9 + 1);
-			num3 = (int) (Math.random() * 9 + 1);
+			for(int i = 0; i < target_num.length; i++) {
+				target_num[i] = (int) (Math.random() * 9 + 1);
+			}
 			
 			// 세 숫자가 중복이 되지 않을 때 까지 반복 후 만족할 경우 루프 탈출
-			if ( num1 != num2 && num2 != num3 && num3 != num1) {
+			if ( target_num[0] != target_num[1] && target_num[1] != target_num[2] 
+					&& target_num[2] != target_num[1]) {
 				break;
 			}
 		}
-		// 세 숫자를 각 자리수로 하여 타겟넘버 설정
-		this.targetnum = (num1 * 100) + (num2 * 10) + num3;
+	}
+	
+	// 타겟 넘버를 받아 스트라이크 횟수를 카운트 합니다. 
+	public int count_s(int[] input_num) {
+		int s_cnt = 0;
+		
+		// 입력 숫자와 각 자리를 비교하여 같은 숫자가 있을 경우 스트라이크 카운터를 증가시킨다. 
+		for(int i = 0; i < target_num.length; i++) {
+			if( target_num[i] == input_num[i]) {
+				s_cnt++;
+			}
+		}
+		return s_cnt;
 	}
 }
