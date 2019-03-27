d3fe69d09c888d02d93907ed45a7105c3635493e
Yujin Kim <99stella24@naver.com>
2019-03-27 21:09:26 +0900

    refactoring

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,69 +1,89 @@
 import java.util.*;
 
 public class Baseball {
-    public static List<Integer> getNum3(){
-        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        Collections.shuffle(arr);
-        List<Integer> subList = arr.subList(0, 3);
-        return subList;
-    }
-    public static List<Integer> get3(){
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        List<Integer> thisList = new ArrayList<Integer>(Arrays.asList(num/100, num%100/10, num%10));
-        return thisList;
-    }
 
-    public static int strikeNum(List<Integer> bN, List<Integer> yN){
-        int strike = 0;
-        if(bN.get(0)==yN.get(0)) strike++;
-        if(bN.get(1)==yN.get(1)) strike++;
-        if(bN.get(2)==yN.get(2)) strike++;
-        return strike;
-    }
+  private static List<Integer> getNum3() {
+    ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    Collections.shuffle(arr);
+    return arr.subList(0, 3);
+  }
 
-    public static int ballNum(List<Integer> bN, List<Integer> yN){
-        int ball = 0;
-        if(bN.get(0)==yN.get(1)||bN.get(0)==yN.get(2)) ball++;
-        if(bN.get(1)==yN.get(0)||bN.get(1)==yN.get(2)) ball++;
-        if(bN.get(2)==yN.get(0)||bN.get(2)==yN.get(1)) ball++;
-        return ball;
-    }
+  private static List<Integer> get3() {
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    return new ArrayList<Integer>(
+        Arrays.asList(num / 100, num % 100 / 10, num % 10));
+  }
 
-    public static boolean tellAns(int s, int b){
-        if(s==0&&b==0) System.out.println("낫싱");
-        else if(b==0) System.out.printf("%d 스트라이크\n", s);
-        else if(s==0) System.out.printf("%d 볼\n", b);
-        else System.out.printf("%d 스트라이크 %d 볼\n", s, b);
-        boolean isEnd = false;
-        if(s==3){
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            isEnd = true;
-        }
-        return isEnd;
+  private static int strikeNum(List<Integer> bN, List<Integer> yN) {
+    int strike = 0;
+    if (bN.get(0).equals(yN.get(0))) {
+      strike++;
     }
+    if (bN.get(1).equals(yN.get(1))) {
+      strike++;
+    }
+    if (bN.get(2).equals(yN.get(2))) {
+      strike++;
+    }
+    return strike;
+  }
 
-    public static boolean toContinue(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        boolean cont = true;
-        if(num!=1) cont = false;
-        return cont;
+  private static int ballNum(List<Integer> bN, List<Integer> yN) {
+    int ball = 0;
+    if (bN.get(0).equals(yN.get(1)) || bN.get(0).equals(yN.get(2))) {
+      ball++;
     }
-    public static void main(String args[]){
-        boolean cont = true;
-        while(cont){
-            List<Integer> baseballNum = getNum3();
-            boolean end = false;
-            while(!end) {
-                List<Integer> yourNum = get3();
-                int strike = strikeNum(baseballNum, yourNum);
-                int ball = ballNum(baseballNum, yourNum);
-                end = tellAns(strike, ball);
-            }
-            cont = toContinue();
-        }
-        return;
+    if (bN.get(1).equals(yN.get(0)) || bN.get(1).equals(yN.get(2))) {
+      ball++;
     }
+    if (bN.get(2).equals(yN.get(0)) || bN.get(2).equals(yN.get(1))) {
+      ball++;
+    }
+    return ball;
+  }
+
+  private static boolean tellAns(int s, int b) {
+    if (s == 0 && b == 0) {
+      System.out.println("낫싱");
+    } else if (b == 0) {
+      System.out.printf("%d 스트라이크\n", s);
+    } else if (s == 0) {
+      System.out.printf("%d 볼\n", b);
+    } else {
+      System.out.printf("%d 스트라이크 %d 볼\n", s, b);
+    }
+    boolean isEnd = false;
+    if (s == 3) {
+      System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      isEnd = true;
+    }
+    return isEnd;
+  }
+
+  private static boolean toContinue() {
+    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    boolean cont = true;
+    if (num != 1) {
+      cont = false;
+    }
+    return cont;
+  }
+
+  public static void main(String args[]) {
+    boolean cont = true;
+    while (cont) {
+      List<Integer> baseballNum = getNum3();
+      boolean end = false;
+      while (!end) {
+        List<Integer> yourNum = get3();
+        int strike = strikeNum(baseballNum, yourNum);
+        int ball = ballNum(baseballNum, yourNum);
+        end = tellAns(strike, ball);
+      }
+      cont = toContinue();
+    }
+  }
 }
