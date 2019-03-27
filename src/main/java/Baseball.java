0c70d432cc46a490435bd0e1dda864be71e68c05
yk1028 <wongeunsong94@gmail.com>
2019-03-28 00:55:46 +0900

    게임을 완전히 종료 또는 재시작 시키는 기능 구현

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -7,21 +7,50 @@
  * 
  * Created by Wongeun Song
  */
+
+import java.util.Scanner;
+
 public class Baseball {
-	public static void main(String[] args) {
-		Baseball baseball = new Baseball();
+	public void playBaseball() {
+		do {
+			this.playGame();
+		} while(this.endOrRestart());
+	}
+	
+	public void playGame() {
 		Computer computer = new Computer();
 		Player player = new Player();
 		Result result;
 		
 		computer.generateNumbers();
 		do {
-			baseball.inputPlayerNumbers(player);
-			result = baseball.compareNumbers(computer.numbers, player.numbers);
+			this.inputPlayerNumbers(player);
+			result = this.compareNumbers(computer.numbers, player.numbers);
 			result.print();
 		} while (!result.isCollect());
 
 		System.out.println("3개의 숫자를 맞추셧습니다! 게임종료");
+	}
+	
+	public void inputPlayerNumbers(Player player) {
+		do {
+			player.inputNumbers();
+			
+			if (player.isCollectLength()) {
+				player.convertStringToCharacter();
+			} else {
+				System.out.println("1-9까지 서로 다른 세개의 수를 입력하세요.");
+				continue;
+			}
+			
+			if (player.isNumber() && (!player.hasDuplicated())) {
+				player.convertCharacterToInt();
+			} else {
+				System.out.println("1-9까지 서로 다른 세개의 수를 입력하세요.");
+				continue;
+			}
+			break;
+		} while(true);
 	}
 	
 	public Result compareNumbers(int[] computerNumbers, int[] playerNumbers) {
@@ -54,24 +83,20 @@ public class Baseball {
 		return result;
 	}
 	
-	public void inputPlayerNumbers(Player player) {
-		do {
-			player.inputNumbers();
+	public boolean endOrRestart() {
+		Scanner scan = new Scanner(System.in);
+		int select;
+		
+		while(true) {
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			select = scan.nextInt();
 			
-			if (player.isCollectLength()) {
-				player.convertStringToCharacter();
-			} else {
-				System.out.println("1-9까지 서로 다른 세개의 수를 입력하세요.");
-				continue;
+			if (select == 1) {
+				return true;
+			} else if (select == 2) {
+				return false;
 			}
-			
-			if (player.isNumber() && (!player.hasDuplicated())) {
-				player.convertCharacterToInt();
-			} else {
-				System.out.println("1-9까지 서로 다른 세개의 수를 입력하세요.");
-				continue;
-			}
-			break;
-		} while(true);
+		}
 	}
+	
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Main
+ * 
+ * version 1.0
+ * 
+ * 2019. 3. 27
+ * 
+ * Created by Wongeun Song
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		Baseball baseball = new Baseball();
+		
+		baseball.playBaseball();
+	}
+}
