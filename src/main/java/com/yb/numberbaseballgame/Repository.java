306acd3a0738d16b04b56981967c3cba2c866466
youngbae10000 <youngbae10000@gmail.com>
2019-03-27 18:45:29 +0900

    기능 구현 1. 2~8까지의 함수를 모아둔[Service] interface, [Repository class]

---
+++ src/main/java/com/yb/numberbaseballgame/Repository.java
@@ -0,0 +1,29 @@
+﻿/*
+ * @(#)Repository.java        1.8.0_191 2019/03/27
+ * 
+ * Copyright (c) 2019 Youngbae Son
+ * ComputerScience, ProgrammingLanguage, Java, Busan, KOREA
+ * All rights reserved.
+ * */
+
+package com.yb.numberbaseballgame;
+
+import java.util.Scanner;
+
+/*
+ * 3. 컴퓨터 난수 생성 : createNumber()
+ * 4. 게임 가이드 메세지 출력 : guideMessage()
+ * 5. 사용자의 값 입력 : input()
+ * 6. 1~9이외 값, 중복 검사확인 : userNumCheck()
+ * 7. 스트라이크, 볼인지 검사 : playGame()
+ * 8. X 스트라이크 X 볼 : printBallCount()
+ * 9. run() : 3 -> 4 -> 5 -> 6-> 7-> 8->  9.계속  or 종료
+ * 
+ * @author 손영배
+ * */
+
+public class Repository implements Service {
+
+	
+
+}
---
+++ src/main/java/com/yb/numberbaseballgame/Service.java
@@ -0,0 +1,23 @@
+/*
+ * @(#)Service.java        1.8.0_191 2019/03/27				
+ * 
+ * Copyright (c) 2019 Youngbae Son
+ * ComputerScience, ProgrammingLanguage, Java, Busan, KOREA
+ * All rights reserved.
+ * */
+
+package com.yb.numberbaseballgame;
+
+/*
+ * 인터페이스 파일
+ * 
+ * 게임에 필요한 기능을 모두 담아둔 클래스
+ * 
+ * @author 손영배
+ * */
+
+public interface Service {
+
+	
+}
+ 
