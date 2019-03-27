74fdad65d25a827f884c14bb2f66239f33efc41e
NotYet <wiseoldster@gmail.com>
2019-03-28 00:35:41 +0900

    기능 작성 : 컴퓨터 방어용 랜덤 숫자 배열 생성 리턴 (int[] defenseArr)

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -1,5 +1,18 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class NumberBaseball {
-	
-
+	static int[] defenseArr() {
+		Set<Integer> set = new LinkedHashSet<>();
+		int[] arr = new int[] {0,0,0};
+		while(set.size()<3) {
+			set.add((int)(Math.random()*9)+1);
+		}
+		Iterator<Integer> it = set.iterator();
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = it.next();
+		}
+		return arr;
+	}
 }
--- src/test/java/PlayTestGame.java
+++ src/test/java/PlayTestGame.java
@@ -2,5 +2,9 @@
 public class PlayTestGame {
 	public static void main(String[] args) {
 		NumberBaseball nb = new NumberBaseball();
+		int[] test = nb.defenseArr();
+		for (int i = 0; i < 3; i++) {
+			System.out.print(test[i]);
+		}
 	}
 }
