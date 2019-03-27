12b171caae8fba44ec4f529cf4f2a204030c31c7
hyun <jhs695@naver.com>
2019-03-27 15:11:10 +0900

    Computer class 볼 , 스트라이크 별로 메세지 작성 기능 , 메세지 관련 기능 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,6 +4,16 @@ import java.util.stream.IntStream;
 
 public class Computer {
     private int num[];
+    private String message = "";
+
+    Computer(){
+        init();
+    }
+
+    private void init(){
+        makeRandomNumber();
+        message = "숫자를 입력해주세요 : ";
+    }
 
     private void makeRandomNumber(){
         num = new int[3];
@@ -21,6 +31,26 @@ public class Computer {
         }
     }
 
+    private void checkNum(int[] userNum){
+        int strike = countStrike(userNum);
+        int ball = countBall(userNum);
+
+        if (ball == 0 && strike == 0){
+            message = "낫싱";
+        }
+        if (ball >= 1) {
+            message = ball + " 볼 ";
+        }
+        if (strike >= 1){
+            message = strike + " 스트라이크 " + message;
+        }
+        message += "\n" + "숫자를 입력해주세요 : ";
+        if (strike == 3){
+            message = strike + " 스트라이크 " + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ";
+        }
+
+    }
+
     private int countBall(int[] userNum){
         return IntStream.range(0,3)
                 .map(i -> (num[i] == userNum[(i+1)%3]) || (num[i] == userNum[(i+2)%3]) ? 1:0)
@@ -31,6 +61,16 @@ public class Computer {
         return IntStream.range(0,3)
                 .map(i -> (num[i] == userNum[i]) ? 1:0)
                 .sum();
+    }
+
+    public String sendMessage(){
+        return message;
+    }
+
+    public void readMessage(int[] message){
+        if (message.length == 3){
+            checkNum(message);
+        }
     }
 
 }
--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,8 +1,18 @@
 package baseball;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Computer computer = new Computer();
+        User user = new User();
 
+        boolean gameState = true;
+
+        while (gameState){
+            user.readMessage(computer.sendMessage());
+            computer.readMessage(user.sendMessage());
+        }
     }
 }
