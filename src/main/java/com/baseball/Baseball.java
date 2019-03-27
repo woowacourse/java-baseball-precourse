f0e2ee5e7a896328a1d700649ce65c0b50892a4d
Kwanwoo Kang <kkw200@naver.com>
2019-03-27 17:09:47 +0900

    리팩토링 2

--- src/main/java/com/baseball/Baseball.java
+++ src/main/java/com/baseball/Baseball.java
@@ -1,14 +1,11 @@
 /*
-* Baseball
-*
-* Ver 1.0
-*
-* Mar 25 2019
-*
 * Copyright 2019 KKW
+*
+* Baseball Ver 1.0
 */
 
-package baseball;
+
+package com.baseball;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
@@ -22,17 +19,20 @@ public class Baseball {
 
 
 class Game {
-    private Scanner scanner = new Scanner(System.in);
-    private int[] myNumbers = new int[3];
-    private int[] yourNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private final Scanner scanner = new Scanner(System.in);
+    private final int[] myNumbers = new int[3];
+    private final int[] yourNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     Game() {
         initGame();
     }
 
+    /**
+    * 1 ~ 9를 담은 배열을 Fisher-Yates 알고리즘으로 섞어서 앞의 3자리를 컴퓨터 숫자로 사용
+    */
     private void initGame() {
-        for (int i = 0; i < 8; i++) {
-            final int j = (int) (Math.random() * (8 - i)) + 1;
+        for (int i = 0; i < yourNumbers.length - 1; i++) {
+            final int j = (int) (Math.random() * (yourNumbers.length - i)) + i;
             final int temp = yourNumbers[i];
             yourNumbers[i] = yourNumbers[j];
             yourNumbers[j] = temp;
@@ -41,37 +41,44 @@ class Game {
     }
 
     private void numbersInput() {
-        int input = 0;
+        final int input;
 
         System.out.println("숫자를 입력해주세요 : ");
         try {
             input = scanner.nextInt();
-            myNumbers[0] = input / 100;
-            myNumbers[1] = input % 100 / 10;
-            myNumbers[2] = input % 10;
-            if (input < 123 || input > 987
-                    || myNumbers[0] == myNumbers[1]
-                    || myNumbers[1] == myNumbers[2]
-                    || myNumbers[2] == myNumbers[0]) {
-                System.out.println("잘못된 입력입니다.");
-                numbersInput();
-            } else {
-                play();
-            }
         } catch (InputMismatchException e) {
             scanner.next();
             System.out.println("잘못된 입력입니다.");
             numbersInput();
+            return;
+        }
+        myNumbers[0] = input / 100;
+        myNumbers[1] = input % 100 / 10;
+        myNumbers[2] = input % 10;
+        if (input < 123
+                || input > 987
+                || myNumbers[0] == myNumbers[1]
+                || myNumbers[1] == myNumbers[2]
+                || myNumbers[2] == myNumbers[0]) {
+            System.out.println("잘못된 입력입니다.");
+            numbersInput();
+        } else {
+            play();
         }
     }
 
+    /**
+     * 사용자가 택한 세 자리 수와 생성된 수를 각각 (1번쨰, 1번째), (1번쨰, 2번째), ..., (3번째, 3번째) 자리끼리
+     * 총 9번 비교하여 해를 구한다.
+     */
     private void play() {
+        final int CYCLE = myNumbers.length;
         int strike = 0;
         int ball = 0;
 
-        for (int i = 0; i < 9; i++) {
-            if (myNumbers[i / 3] == yourNumbers[i % 3]) {
-                int temp = (i / 3 == i % 3) ? strike++ : ball++;
+        for (int i = 0; i < yourNumbers.length; i++) {
+            if (myNumbers[i / CYCLE] == yourNumbers[i % CYCLE]) {
+                int temp = (i / CYCLE == i % CYCLE) ? strike++ : ball++;
             }
         }
         if (strike + ball == 0) {
@@ -92,7 +99,7 @@ class Game {
     }
 
     private void commandInput() {
-        int input = 0;
+        final int input;
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         try {
