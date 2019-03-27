227abf13456422bd3a003a8357b8e9339049aed6
전성모 <smjeon@jeonseongmoui-MacBook-Pro.local>
2019-03-27 23:11:05 +0900

    Update README.md and add startBaseball

---
+++ src/main/java/Baseball.java
@@ -0,0 +1,29 @@
+/**
+ * Baseball class는 숫자 야구 게임을 위한 class이다.
+ * last modified: 2019.03.27
+ * author: SM Jeon
+ */
+
+
+import java.util.*;
+
+public class Baseball {
+    public static void main(String[] args) {
+            startBaseball();
+    }
+
+    /*
+     * Method for start baseball game
+     * Generate Random Number, check strike and ball
+     */
+    private static void startBaseball() {
+        String comNum = "";         // Computer number(Random Number)
+        String userNum;             // User number (Input)
+        boolean inputFlag = true;   // for check 3 strike
+        comNum = "123";             // generate Random Number
+        while (inputFlag) {
+            userNum = "123";        // Receive Input
+            inputFlag = false;      // check 3 strike
+        }
+    }
+}
