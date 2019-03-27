487a7ccf3505b878482016b58f7394cfb05d574e
DiceMono <46372339+DiceMono@users.noreply.github.com>
2019-03-27 20:04:33 +0900

    #2 숫자 입력 및 형변환 메서드

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,23 +1,25 @@
 /*
  * BaseballGame
  * 
- * 2019.03.25
+ * 2019.03.27
  * 
  * 이 프로그램은 누구든 수정가능합니다.
  */
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 /**
  * 숫자 야구 게임!
  *
- * @version 1.00 2019.03.25
+ * @version 1.00 2019.03.27
  * @author 김형선
  */
 
 public class BaseballGame {
 	/* 숫자 야구 게임 */
 	static Random random = new Random();
+	static Scanner scan = new Scanner(System.in);
 
 	/* 문제 생성  메서드
 	 * 첫 번째 for문은 1~9 정수를 포함하는 ArrayList를 생성한다.
@@ -31,18 +33,35 @@ public class BaseballGame {
 		}
 		int[] answer = new int[3];
 		int count = -1;
-		for(int i=9; i>6; i--) {
+		for(int i=nums.size(); i>6; i--) {
 			count++;
 			int RanIndex = random.nextInt(i);
 			answer[count] = nums.remove(RanIndex);
 		}
-		return answer;	
+		return answer;
 	}
+	
+	/* 사용자에게 3자리 수를 입력받아 int[]로 반환하는 메서드
+	 * ex) 입력값 : 315-> return {3, 1, 5} */
+	public static int[] inputNums() {
+		int[] playerNums = new int[3];
+		System.out.println("숫자를 입력해 주세요 :");
+		String inputStr = scan.next();
+		for(int i=0; i<3; i++) {
+			playerNums[i] = inputStr.charAt(i) - '0';
+		}
+		return playerNums;
+	}
+	
 	/* 테스트 코드 */
 	public static void main(String[] args) {
 		int[] answer = createProblem();
 		for (int i : answer) {
 			System.out.println(i);
+		}
+		int[] input = inputNums();
+		for (int j : input) {
+			System.out.println(j);
 		}
 	}
 }
