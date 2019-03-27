53545f41cb4565e3de7bb3c45dfed1bc0a7c492c
U-Yeon <leanner@naver.com>
2019-03-27 23:37:56 +0900

    1) main 함수의 준비

---
+++ src/main/java/NumberBaseball.java
@@ -0,0 +1,17 @@
+/**
+ *NumberBaseball.class
+ *
+ * @author ÃÖ¿©Áø
+ *
+ */
+import java.util.Scanner;
+
+
+public class NumberBaseball {
+	public static Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		int userNum[] = new int[3];
+		int comNum[] = new int[3];
+	}
+
+}
