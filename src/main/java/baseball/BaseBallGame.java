0bfec4fe6716ba4a2d878ad5bda632f762b26ad7
DaejunBae <dpudpu11@gmail.com>
2019-03-27 22:21:25 +0900

    Add 6. 게임 클리어 확인, 클리어시 재시작

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,6 +1,10 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class BaseBallGame {
+    private final static int RESTART = 1;
+    private final static int END = 2;
     private final int DIGIT_LENGTH;
     private Computer computer;
     private Player player;
@@ -15,7 +19,9 @@ public class BaseBallGame {
         computer = new Computer(DIGIT_LENGTH);
         player = new Player(DIGIT_LENGTH);
 
-        start();
+        do {
+            start();
+        } while (restart());
     }
 
     private void start() {
@@ -27,8 +33,9 @@ public class BaseBallGame {
             compareDigits(computer.getDigits(), player.getDigits());
 
             System.out.println(getResult());
-        } while (true);
+        } while (!isGameClear());
 
+        System.out.println(DIGIT_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public void compareDigits(String comNums, String playerNums) {
@@ -78,4 +85,23 @@ public class BaseBallGame {
         return (ball == 0) ? "" : ball + " 볼";
     }
 
+    private boolean isGameClear() {
+        return strike == DIGIT_LENGTH;
+    }
+
+    private boolean restart() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            int command = sc.nextInt();
+            if (command == RESTART)
+                return true;
+            else if (command == END)
+                return false;
+
+            System.out.println("잘못 입력하셨습니다.");
+        }
+    }
 }
