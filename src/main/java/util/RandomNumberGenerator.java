a57d156e749580a0d75b2bf96d669280cfb069f0
Delf-Lee <ttjs2008@naver.com>
2019-03-27 22:41:27 +0900

    난수 생성기 반환값 수정

--- src/main/java/util/RandomNumberGenerator.java
+++ src/main/java/util/RandomNumberGenerator.java
@@ -1,6 +1,7 @@
 package util;
 
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
@@ -11,23 +12,38 @@ public class RandomNumberGenerator {
     private static Random r = new Random();
 
     /**
-     *
-     * @param n 생성할 난수의 수
-     * @param limit 생성할 난수의 한계
-     * @return 0과 limit 사이에서 중복되지 않는 크기가 n인 난수(정수) 배열
+     * @param n   생성할 난수의 수
+     * @param min 생성 난수의 최솟값
+     * @param max 생성 난수의 최댓값
+     * @return min과 max 사이에서 중복되지 않는 크기가 n인 난수(정수)가 담겨있는 RandomNumbers 객체
+     * @see util.RandomNumberGenerator.RandomNumbers
      */
-    public static int[] getUniqueNumbers(int n, int limit) {
-        Set<Integer> set = new HashSet<>(n);
+    public static RandomNumbers getUniqueNumbers(int n, int min, int max) {
+        Set<Integer> set = new LinkedHashSet<>();
         while (set.size() < n) {
-            set.add(r.nextInt(limit));
+            set.add(r.nextInt(max - min) + min);
         }
 
-        int index = 0;
-        int[] result = new int[n];
-        for (int c : set) {
-            result[index++] = c;
+        return new RandomNumbers(set);
+    }
+
+
+    /**
+     * 생성된 난수를 저장하고 있는 객체. 임의의 값으로 반환 가능하다.
+     */
+    public static class RandomNumbers<E> {
+        private Collection<E> numbers;
+
+        RandomNumbers(Collection numbers) {
+            this.numbers = numbers;
         }
-        // return set.stream().mapToInt(Number::intValue).toArray(); // 스트림을 사용할 경우
-        return result;
+
+        public String byString() {
+            StringBuilder sb = new StringBuilder();
+            for (E e : numbers) {
+                sb.append(e);
+            }
+            return sb.toString();
+        }
     }
 }
--- src/test/java/util/RandomNumberGeneratorTest.java
+++ src/test/java/util/RandomNumberGeneratorTest.java
@@ -15,13 +15,11 @@ public class RandomNumberGeneratorTest {
     @Test
     public void 일정_범위_내의_중복되지_않는_난수를_N개를_생성한다() {
         int size = 3;
-        int limit = 10;
-
-        int[] result = RandomNumberGenerator.getUniqueNumbers(size, limit);
+        String result = RandomNumberGenerator.getUniqueNumbers(size, 1, 9).byString();
 
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < result.length; i++) {
-            set.add(result[i]);
+        for (int i = 0; i < result.length(); i++) {
+            set.add(Character.getNumericValue(result.charAt(i)));
         }
         assertThat(set).hasSize(size);
     }
