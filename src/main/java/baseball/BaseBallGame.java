283ba9f487ea44aa89065c0b9035e4c5d306ba1a
DaejunBae <dpudpu11@gmail.com>
2019-03-27 22:17:30 +0900

    Add 4. 컴퓨터, 플레이어 숫자 비교
     - 같은 인덱스 중복 - 스트라이크 ++
     - 다른 인덱스 중복 - 볼 ++

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -4,6 +4,8 @@ public class BaseBallGame {
     private final int DIGIT_LENGTH;
     private Computer computer;
     private Player player;
+    private int ball;
+    private int strike;
 
     public BaseBallGame(int digitLength) {
         this.DIGIT_LENGTH = digitLength;
@@ -19,10 +21,40 @@ public class BaseBallGame {
     private void start() {
         computer.generateDigits();
 
-        do{
+        do {
             player.inputDigits();
 
-        }while(true);
+            compareDigits(computer.getDigits(), player.getDigits());
 
+        } while (true);
+
+    }
+
+    public void compareDigits(String comNums, String playerNums) {
+        initBallAndStrike();
+
+        for (int i = 0; i < DIGIT_LENGTH; i++) {
+            if (matchSameIndex(comNums, playerNums, i))
+                strike++;
+            else if (matchDifferentIndex(comNums, playerNums, i))
+                ball++;
+        }
+    }
+
+    private void initBallAndStrike() {
+        ball = 0;
+        strike = 0;
+    }
+
+    private boolean matchSameIndex(String comNums, String playerNums, int index) {
+        return comNums.charAt(index) == playerNums.charAt(index);
+    }
+
+    private boolean matchDifferentIndex(String comNums, String playerNums, int index) {
+        for (int i = 0; i < DIGIT_LENGTH; i++) {
+            if (i != index && playerNums.charAt(index) == comNums.charAt(i))
+                return true;
+        }
+        return false;
     }
 }
