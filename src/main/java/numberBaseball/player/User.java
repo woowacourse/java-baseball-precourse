2096dc701fe35c32ab5d086c89c6aa275745341b
deocksoo <dusol92@snu.ac.kr>
2019-03-28 15:25:32 +0900

    [ADD] User Class의 guessThreeNumber 함수를 구현합니다
    User로부터 추측한 숫자를 입력받아 PlayerBot에게 넘겨주기 위한 함수입니다.
    User에게 입력을 받을 때, 문제 조건에 맞지 않는 input이 들어오는 경우를 체크하여
    다시 숫자를 입력받기 위해 hasInvalid 함수를 선언하였습니다.

--- src/main/java/numberBaseball/player/User.java
+++ src/main/java/numberBaseball/player/User.java
@@ -1,12 +1,31 @@
 package numberBaseball.player;
 
+import java.util.Scanner;
+
 public class User implements Player {
     public void setThreeNumber() {
 
     }
 
+    public boolean hasInvalid(String guessed) {
+        return false;
+    }
+
     public int[] guessThreeNumber() {
-        return new int[3];
+        Scanner sc = new Scanner(System.in);
+        int [] guessed = {0, 0, 0};
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String guessedStr = sc.nextLine().replaceAll("\\s+", "");
+        while(hasInvalid(guessedStr)) {
+            System.out.print("숫자를 다시 입력해주세요 : ");
+            guessedStr = sc.nextLine().replaceAll("\\s+", "");
+        }
+
+        for (int i =0; i < guessedStr.length(); i++) {
+            guessed[i] = Character.getNumericValue(guessedStr.charAt(i));
+        }
+        return guessed;
     }
 
     public void answerHintOf(int[] guessed) {
