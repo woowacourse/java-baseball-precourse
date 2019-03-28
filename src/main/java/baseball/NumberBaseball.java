6e2dda5fd82b974f89e5f43db03358e14f2dc001
stopsilver13 <stopsilver13>
2019-03-28 22:35:26 +0900

    Judge strike and ball

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -20,12 +20,14 @@ public class NumberBaseball {
         HashSet<Integer> numbers = new HashSet<>();
 
         // 1~9 서로 다른 세 숫자 선정
-        while (numbers.size()!=3) {
-            numbers.add((int)(Math.random() * 9 + 1));
+        while (numbers.size() != 3) {
+            numbers.add((int) (Math.random() * 9 + 1));
         }
 
         ArrayList<Integer> correctAnswer = new ArrayList<>(numbers);
         Collections.shuffle(correctAnswer);
+
+        System.out.println(correctAnswer);
 
         // 사용자가 유추한 정답 입력
         String result = "";
@@ -33,9 +35,40 @@ public class NumberBaseball {
         while (!result.equals("3스트라이크")) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("숫자를 입력해주세요 : ");
-            int userAnswer = scanner.nextInt();
+            int input = scanner.nextInt();
 
-            result = "3스트라이크";
+            ArrayList<Integer> userAnswer = new ArrayList<>();
+
+            while (input != 0) {
+                userAnswer.add(0, input % 10);
+                input /= 10;
+            }
+
+            // 스트라이크 & 볼 판단
+            int ball = 0;
+            int strike = 0;
+            int index = 0;
+
+            for (int e : userAnswer) {
+                if (userAnswer.get(index).equals(correctAnswer.get(index))) {
+                    strike++;
+                } else if (correctAnswer.contains(e)) {
+                    ball++;
+                }
+                index++;
+            }
+
+            if (ball == 0 && strike == 0) {
+                result = "낫싱";
+            } else if (strike == 0) {
+                result = ball + "볼";
+            } else if (ball == 0) {
+                result = strike + "스트라이크";
+            } else {
+                result = strike + "스트라이크 " + ball + "볼";
+            }
+
+            System.out.println(result);
         }
     }
 }
