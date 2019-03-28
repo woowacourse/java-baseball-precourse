468c0a248dcb6e5f99653a473682675afde7a171
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2019-03-28 22:42:19 +0900

    makeanswer

--- src/main/java/baseball.java
+++ src/main/java/baseball.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class baseball{
+
+    public static int makeanswer(){
+        int check_dup[] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // 서로 다른 세 자리수를 고르기 위해 중복 체크
+        int answer=0;
+        for(int i=1; i<=3; i++){
+            int randnum = baseball.getrand(check_dup); // check_dup 배열을 기준으로 중복되지 않은 숫자를 하나 생성.
+            answer = answer*10 + randnum; // 현재까지 나온 결과값에 추가.
+            check_dup[randnum]=1; // 방금 추가된 숫자의 사용 여부를 체크.
+        }
+        return answer;
+    }
     public static void baseball_game(){
         int answer, state=1;
         answer = baseball.makeanswer(); // 컴퓨터의 정답
