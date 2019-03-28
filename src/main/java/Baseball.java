0a729362ac44b76cee915ef8438865a1fd7a8491
HongJow <wlghlplp@ajou.ac.kr>
2019-03-28 22:19:47 +0900

    branch done

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -59,7 +59,7 @@ public class Baseball {
         while(true) {
             game.play();
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
             int dec = sc.nextInt();
             if(dec == 1)
                 continue;
