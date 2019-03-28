7d4c6c9d41a93cbfdab6d47fbf76a267cf5cbdd9
stopsilver13 <stopsilver13>
2019-03-28 23:21:30 +0900

    Divide main method

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -16,21 +16,71 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class NumberBaseball {
+    private static ArrayList<Integer> setAnswer() {
+        HashSet<Integer> numbers = new HashSet<>();
+
+        // 1~9 서로 다른 세 숫자 선정
+        while (numbers.size() != 3) {
+            numbers.add((int) (Math.random() * 9 + 1));
+        }
+
+        ArrayList<Integer> correctAnswer = new ArrayList<>(numbers);
+        Collections.shuffle(correctAnswer);
+
+        System.out.println(correctAnswer);
+
+        return correctAnswer;
+    }
+
+    private static ArrayList<Integer> changeFormat(int input) {
+        ArrayList<Integer> userAnswer = new ArrayList<>();
+
+        while (input != 0) {
+            userAnswer.add(0, input % 10);
+            input /= 10;
+        }
+
+        return userAnswer;
+    }
+
+    private static String judgeAnswer(ArrayList<Integer> userAnswer, ArrayList<Integer> correctAnswer) {
+
+        // 스트라이크 & 볼 판단
+        int ball = 0;
+        int strike = 0;
+        int index = 0;
+
+        for (int e : userAnswer) {
+            if (userAnswer.get(index).equals(correctAnswer.get(index))) {
+                strike++;
+            } else if (correctAnswer.contains(e)) {
+                ball ++;
+            }
+            index ++;
+        }
+
+        String result;
+
+        if (ball == 0 && strike == 0) {
+            result = "낫싱";
+        } else if (strike == 0) {
+            result = ball + "볼";
+        } else if (ball == 0) {
+            result = strike + "스트라이크";
+        } else {
+            result = strike + "스트라이크 " + ball + "볼";
+        }
+
+        System.out.println(result);
+
+        return result;
+    }
+
     public static void main(String[] args) {
         boolean keepGoing = true;
 
         while (keepGoing) {
-            HashSet<Integer> numbers = new HashSet<>();
-
-            // 1~9 서로 다른 세 숫자 선정
-            while (numbers.size() != 3) {
-                numbers.add((int) (Math.random() * 9 + 1));
-            }
-
-            ArrayList<Integer> correctAnswer = new ArrayList<>(numbers);
-            Collections.shuffle(correctAnswer);
-
-            System.out.println(correctAnswer);
+            ArrayList<Integer> correctAnswer = setAnswer();
 
             // 사용자가 유추한 정답 입력
             String result = "";
@@ -40,38 +90,9 @@ public class NumberBaseball {
                 System.out.print("숫자를 입력해주세요 : ");
                 int input = scanner.nextInt();
 
-                ArrayList<Integer> userAnswer = new ArrayList<>();
+                ArrayList<Integer> userAnswer = changeFormat(input);
 
-                while (input != 0) {
-                    userAnswer.add(0, input % 10);
-                    input /= 10;
-                }
-
-                // 스트라이크 & 볼 판단
-                int ball = 0;
-                int strike = 0;
-                int index = 0;
-
-                for (int e : userAnswer) {
-                    if (userAnswer.get(index).equals(correctAnswer.get(index))) {
-                        strike++;
-                    } else if (correctAnswer.contains(e)) {
-                        ball++;
-                    }
-                    index++;
-                }
-
-                if (ball == 0 && strike == 0) {
-                    result = "낫싱";
-                } else if (strike == 0) {
-                    result = ball + "볼";
-                } else if (ball == 0) {
-                    result = strike + "스트라이크";
-                } else {
-                    result = strike + "스트라이크 " + ball + "볼";
-                }
-
-                System.out.println(result);
+                result = judgeAnswer(userAnswer, correctAnswer);
             }
 
             System.out.println("3개의 숫자를 모두 맞히셨습니! 게임 종료");
@@ -84,6 +105,8 @@ public class NumberBaseball {
                 System.out.println("게임을 완전히 종료합니다.");
                 keepGoing = false;
             }
+
+            scanner.close();
         }
     }
 }
