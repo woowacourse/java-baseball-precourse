d59ec6dfe62602a74288365a16927e3edebd0a8f
deocksoo <dusol92@snu.ac.kr>
2019-03-28 14:05:05 +0900

    [ADD] gameMode 변수와 getGaameMode 함수를 추가합니다

--- src/main/java/numberBaseball/NumberBaseball.java
+++ src/main/java/numberBaseball/NumberBaseball.java
@@ -1,6 +1,13 @@
 package numberBaseball;
 
+
 public class NumberBaseball {
+    private String gameMode = "시작 모드";
+
+    public String getGameMode() {
+        return this.gameMode;
+    }
+
     public static void main(String args[]) {
 
     }
