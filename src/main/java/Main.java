6ec5ca6eb9e6cd4a2b36837b3f62d53d9ac7c3cd
hsycamp <hsycamp@gmail.com>
2019-03-28 17:24:09 +0900

    게임 실행 기능 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+
 	Scanner scanner = new Scanner(System.in);
 	Baseball bsb = new Baseball();
 
 	public void runPrompt() {
 
 		int[] com = bsb.makeComNumber();
+		int[] user = new int[3];
+		int[] hint = new int[2];
 
 		System.out.println("[[게임 시작]]\nComputer number:[ ? , ? , ? ]\n");
 
+		while (true) {
+			user = bsb.makeUserNumber();
+			hint = bsb.ballCount(com, user);
+			bsb.showHint(hint);
+			if (hint[0] == 3) {
+				break;
+			}
+		}
 		scanner.close();
 	}
 
