273af62d2ba9f992437deea1d23cfaa305552f37
deocksoo <dusol92@snu.ac.kr>
2019-03-28 15:12:36 +0900

    [ADD] PlayerBot Class의 setThreeNumber 함수를 구현합니다
    1. PlayerBot이 랜덤하게 정답 숫자를 설정할 수 있도록 randomPop 함수를
    추가하였습니다.
    2. 설정한 정답을 integer array변수에 기억해놓도록 하였습니다.

--- src/main/java/numberBaseball/player/PlayerBot.java
+++ src/main/java/numberBaseball/player/PlayerBot.java
@@ -1,8 +1,25 @@
 package numberBaseball.player;
 
-public class PlayerBot implements Player {
-    public void setThreeNumber() {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class PlayerBot implements Player {
+    private int[] answerNumber = new int[] {0, 0, 0};
+
+    public int randomPop(ArrayList<Integer> numberSet) {
+        int randomIdx = (new Random()).nextInt(numberSet.size());
+        return numberSet.remove(randomIdx);
+    }
+
+    public void setThreeNumber() {
+        ArrayList<Integer> numberSet = new ArrayList<Integer>();
+
+        for (int i = 1; i < 10; i++) {
+            numberSet.add(i);
+        }
+        for (int i = 0; i < this.answerNumber.length; i++) {
+            this.answerNumber[i] = randomPop(numberSet);
+        }
     }
 
     public int[] guessThreeNumber() {
