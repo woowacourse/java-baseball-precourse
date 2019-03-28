33d62c135a008e8b0dae90c934e71dafbcbeef91
deocksoo <dusol92@snu.ac.kr>
2019-03-28 14:31:46 +0900

    [ADD] gameMode를 고르는 기능을 추가하였습니다
    1. gameMode에 어떤 Mode들이 있는지 보여주기 위하여 showMenu함수를
    따로 구현하였습니다.
    2. setGameMode 함수의 이름을 기능을 고려하여 selectGameMode로 변경하였습니다.

--- src/main/java/numberBaseball/NumberBaseball.java
+++ src/main/java/numberBaseball/NumberBaseball.java
@@ -1,16 +1,40 @@
 package numberBaseball;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 import numberBaseball.player.*;
 
 public class NumberBaseball {
     private String gameMode = "시작 모드";
+    private ArrayList<String> menu = new ArrayList<String>();
+
+    public NumberBaseball() {
+        setMenu();
+    }
+
+    public void setMenu() {
+        this.menu.add("연습 모드");
+        this.menu.add("대전 모드");
+        this.menu.add("게임 종료");
+    }
 
     public String getGameMode() {
         return this.gameMode;
     }
 
-    public void setGameMode() {
+    public void showMenu() {
+        System.out.println("");
+        for (int i = 0; i < this.menu.size(); i++) {
+            System.out.println((i + 1) + ". " + this.menu.get(i));
+        }
+        System.out.println("");
+    }
 
+    public void selectGameMode() {
+        showMenu();
+        System.out.print("어떤 모드로 게임하시겠어요? ");
+        int selectedIdx = new Scanner(System.in).nextInt() - 1;
+        this.gameMode = this.menu.get(selectedIdx);
     }
 
     public void startGame(Player guesser, Player answerer) {
@@ -23,7 +47,7 @@ public class NumberBaseball {
         Player playerBot = new PlayerBot();
 
         while (numberBaseball.getGameMode() != "게임 종료") {
-            numberBaseball.setGameMode();
+            numberBaseball.selectGameMode();
             numberBaseball.startGame(player, playerBot);
         }
     }
