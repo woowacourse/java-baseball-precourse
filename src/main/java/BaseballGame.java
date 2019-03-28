9f3d7bdf1d2414f896a34252771f1c8421892215
DiceMono <46372339+DiceMono@users.noreply.github.com>
2019-03-28 19:51:31 +0900

    자바 코드 컨벤션에 맞추어 수정

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -10,24 +10,27 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Arrays;
+
 /**
  * 숫자 야구 게임!
  *
  * @version 1.00 2019.03.27
  * @author 김형선
  */
-
 public class BaseballGame {
 	/* 숫자 야구 게임 */
 	
 	static Random random = new Random();
 	static Scanner scan = new Scanner(System.in);
-	static Integer[] numsArray = {1,2,3,4,5,6,7,8,9};
+	/** createAnswer 메서드에서 Arraylist를 생성하기 위한 매개 변수 */
+	static Integer[] numsArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	/* 정답 생성  메서드
+	/** 
+	 * 정답 생성  메서드
 	 * numsArray를 ArrayList로 변환한다.
 	 * 로또 추첨기에서 공 뽑듯이 중복되지 않게 3개 수를 뽑는다.
-	 * ex) return {6, 3, 9} */
+	 * ex) return {6, 3, 9} 
+	 */
 	public static int[] createAnswer() {
 		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numsArray));
 		int[] answer = new int[3];
@@ -39,8 +42,10 @@ public class BaseballGame {
 		return answer;
 	}
 	
-	/* 사용자에게 3자리 수를 입력받아 int[]로 반환하는 메서드
-	 * ex) 입력값 : 315-> return {3, 1, 5} */
+	/**
+	 * 사용자에게 3자리 수를 입력받아 int[]로 반환하는 메서드
+	 * ex) 입력값 : 315-> return {3, 1, 5} 
+	 */
 	public static int[] inputNums() {
 		int[] playerNums = new int[3];
 		System.out.println("숫자를 입력해 주세요 :");
@@ -52,9 +57,11 @@ public class BaseballGame {
 		return playerNums;
 	}
 	
-	/* 해당 정수가 해당 int[]에 들어있는지 알려주는 메서드
+	/** 
+	 * 해당 정수가 해당 int[]에 들어있는지 알려주는 메서드
 	 * ex) isInArray(3, new int[] {4, 3, 2}) --> return true 
-	 *     isInArray(1, new int[] {2, 5, 6}) --> return false */
+	 *     isInArray(1, new int[] {2, 5, 6}) --> return false 
+	 */
 	public static boolean isInArray(int num, int[] numArray) {
 		for(int i : numArray) {
 			if(num == i) {
@@ -64,10 +71,12 @@ public class BaseballGame {
 		return false;
 	}
 	
-	/* 정답과 플레이어가 입력한 값을 비교하고
+	/**
+	 * 정답과 플레이어가 입력한 값을 비교하고
 	 * 스트라이크와 볼의 개수를 Array에 저장하는 메서드
 	 * ex) countStrikeBall(new int[] {4, 6, 2}, new int[] {4, 3, 2})
-	 *     --> return {2, 1} */
+	 *     --> return {2, 1} 
+	 */
 	public static int[] countStrikeBall(int[] answer, int[] playerNums) {
 		int strike = 0;
 		int ball = 0;
@@ -83,11 +92,13 @@ public class BaseballGame {
 		return strikeBallCount;
 	}
 	
-	/* countStrikeBall()에서 얻은 Array를 바탕으로 결과를 출력한다.
+	/** 
+	 * countStrikeBall()에서 얻은 Array를 바탕으로 결과를 출력한다.
 	 * 정답을 맞췄을 경우 true, 정답이 아닐경우 false을 리턴한다.
 	 * ex) printCount(new int[] {1, 2})
 	 *     출력문: 1 스트라이크 2볼
-	 *     --> return false */
+	 *     --> return false 
+	 */
 	public static boolean printCount(int[] strikeBallCount) {
 		int strike = strikeBallCount[0];
 		int ball = strikeBallCount[1];
@@ -104,7 +115,11 @@ public class BaseballGame {
 		return false;
 	}
 	
-	/* 게임을 실행하는 메서드 */
+	/** 
+	 * 게임을 실행하는 메서드 
+	 * 첫번째 while문은 입력값이 정답인지 확인합니다.
+	 * 두번째 while문은 사용자에게 게임을 종료할것인지, 재시작할 것인지 물어봅니다
+	 */
 	public static void playGame() {
 		int[] answer = createAnswer();
 		boolean checkAnswer = false;
@@ -127,7 +142,7 @@ public class BaseballGame {
 		}
 	}
 	
-	/* 게임이 실행되는 공간 */
+	/** 게임이 실행되는 공간 */
 	public static void main(String[] args) {
 		playGame();
 	}
