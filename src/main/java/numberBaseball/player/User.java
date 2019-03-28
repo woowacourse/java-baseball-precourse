82b0131fe6043c830eeeca342b2d51c4c253d2f7
deocksoo <dusol92@snu.ac.kr>
2019-03-28 15:34:37 +0900

    [ADD] User Class의 hasInvalid 함수를 구현합니다
    hasInvalid함수는 input을 입력받을 때 일어날 수 있는 네 가지 예외를 체크합니다.
    체크하는 예외는 다음과 같습니다.
    
    1. 입력에 숫자가 아닌 문자가 포함되어있을 때
    2. 입력받은 숫자의 길이가 3이 아닐 때
    3. 입력에 0이 포함되어 있을 때
    4. 입력에 중복된 숫자들이 포함되어 있을 때

--- src/main/java/numberBaseball/player/User.java
+++ src/main/java/numberBaseball/player/User.java
@@ -7,8 +7,52 @@ public class User implements Player {
 
     }
 
-    public boolean hasInvalid(String guessed) {
+    public boolean hasInvalidChar(String guessed) {
+        for (int i = 0; i < guessed.length(); i++) {
+            if (!Character.isDigit(guessed.charAt(i))) {
+                System.out.println("Error: 숫자만 입력할 수 있습니다.");
+                return true;
+            }
+        }
         return false;
+    }
+
+    public boolean hasInvalidLength(String guessed) {
+        final int validLength = 3;
+
+        if (guessed.length() != validLength) {
+            System.out.println("Error: 3개의 숫자를 입력해주셔야 합니다.");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasInvalidZeros(String guessed) {
+        if (guessed.contains("0")) {
+            System.out.println("Error: 숫자에는 0이 포함될 수 없습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasOverlappedNumber(String guessed) {
+        String checkingPart = new String();
+        char checkingChar;
+
+        for (int i = 1; i < guessed.length(); i++) {
+            checkingChar = guessed.charAt(i - 1);
+            checkingPart = guessed.substring(i);
+            if (checkingPart.contains(String.valueOf(checkingChar))) {
+                System.out.println("Error: 서로 중복되는 숫자는 포함될 수 없습니다.");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasInvalid(String guessed) {
+        return hasInvalidChar(guessed) || hasInvalidLength(guessed)
+                || hasInvalidZeros(guessed) || hasOverlappedNumber(guessed);
     }
 
     public int[] guessThreeNumber() {
