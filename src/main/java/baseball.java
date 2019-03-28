d61af2db228476b4526a4732d484aad1f6856aa5
giraffeJ <35956557+giraffeJ@users.noreply.github.com>
2019-03-28 22:42:49 +0900

    match_check

--- src/main/java/baseball.java
+++ src/main/java/baseball.java
@@ -3,6 +3,35 @@ import java.util.Random;
 
 public class baseball{
 
+    public static int match_check(int answer, int input){
+        int strike = 0;
+        int ball = 0;
+        int ans[] = new int[4];
+        int inp[] = new int[4];
+        for(int i=1; i<=3; i++){
+            ans[i] = answer%10;
+            inp[i] = input%10;
+            answer /= 10;
+            input /= 10;
+        }
+        for(int i=1; i<=3; i++){
+            int comp = baseball.find_pos(ans, inp[i]);
+            strike += (comp==i? 1:0);
+            ball += (comp!=i && comp!=-1? 1:0);
+        }
+        if(strike==3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return 0;
+        }
+        if(strike==0 && ball==0)
+            System.out.print("낫싱");
+        if(strike>0)
+            System.out.print(strike + " 스트라이크 ");
+        if(ball>0)
+            System.out.print(ball + " 볼");
+        System.out.println();
+        return 1;
+    }
     public static int makeanswer(){
         int check_dup[] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // 서로 다른 세 자리수를 고르기 위해 중복 체크
         int answer=0;
