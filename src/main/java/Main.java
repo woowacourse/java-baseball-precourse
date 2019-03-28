79776f58be3c7ea32104040a32db06946a649973
samsung <samsung@LAPTOP-859NPSQ7>
2019-03-28 18:23:19 +0900

    기능 8. main 함수 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/* 
+ * 클래스 이름 : Main
+ * 작성자 : Moonyoung Chae
+ * 날짜 : 2019/03/28
+ * Copyright (c) 2019 Moonyoung Chae
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		// 게임이 구현된 baseball 함수 호출
+		Baseball baseball = new Baseball();
+		baseball.run();
+	}
+}
