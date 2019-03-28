1220e3992612d0cf9b6e6458350ec9f351bdb319
hiwooj <hiwooj35@gmail.com>
2019-03-28 23:36:00 +0900

    컴퓨터 랜덤값 생성

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,3 +1,23 @@
 public class Baseball {
+    
+    public static int[] randomNumber(int[] computer) {
+        // 컴퓨터 숫자를 랜덤으로 부여
+        Random rand = new Random();
+        boolean[] sure = new boolean[10];
+        int count = 0;
+        int trial;
+
+        sure[0] = true; // 0제외
+
+        while (count < computer.length) {
+            trial = rand.nextInt(10);
+            if (!sure[trial]) {
+                computer[count] = trial;
+                sure[trial] = true; // 숫자 중복 방지
+                count++;
+            }
+        }
+        return computer;
+    }
 }
 
