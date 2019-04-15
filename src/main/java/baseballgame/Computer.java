21ac5edd358a31557cff55925713bf245c0f9bb7
Sunho Van <vsh123@naver.com>
2019-04-15 21:13:38 +0900

    refactor : computer 객체 수정 1, String 대신 List<Integer>를 사용하여 숫자를 저장

--- src/main/java/baseballgame/Computer.java
+++ src/main/java/baseballgame/Computer.java
@@ -6,6 +6,8 @@
 
 package baseballgame;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -15,18 +17,11 @@ import java.util.Random;
  * @author          반선호
  */
 class Computer {
-
-    /** 난수 생성 시 범위를 지정해주기 위한 상수 - (0~8)의 범위로 설정 */
     private static final int MAX_NUM = 9;
-
-   /** 위의 MAX_NUM을 이용해 생성된 난수에 1을 더하기 위한 상수 */
     private static final int ONE = 1;
-
-    /** 숫자의 최대 길이 상수 */
     private static final int BALL_LEN = 3;
 
-    /** 생성된 숫자를 담을 문자열 */
-    private String number = "";
+    private List<Integer> computer = new ArrayList<>(BALL_LEN);
 
     /**
      * Computer 생성자이며 서로 다른 수로 이루어진 3자리의 수를 만들어 낸다.
@@ -35,32 +30,12 @@ class Computer {
         Random random;
         int temp;
 
-        while(number.length() < BALL_LEN){
+        while(computer.size() < BALL_LEN){
             random = new Random();
             temp = random.nextInt(MAX_NUM) + ONE;
-
-            if(!checkDuplicate(temp)){
-
-                /* 만약 중복 되지 않는다면 생성된 난수를 합친다*/
-                number += String.valueOf(temp);
+            if(!computer.contains(temp)){
+                computer.add(temp);
             }
         }
-    }
-
-    /**
-     * 생성된 난수가 중복되는지 확인하는 메소드이다.
-     * @param temp = 1 ~ 9사이의 난수이다.
-     * @return 만약 난수가 중복된다면 true를 return한다.
-     */
-    private boolean checkDuplicate(int temp){
-        return number.contains(String.valueOf(temp));
-    }
-
-    /**
-     * 3자리 숫자를 return해주는 메소드이다.
-     * @return 3자리 숫자(문자열)을 return 한다.
-     */
-    public String getNumber(){
-        return number;
     }
 }
