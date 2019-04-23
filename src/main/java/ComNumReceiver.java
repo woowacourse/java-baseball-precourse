9f05e3ed5b1b4eaadea1a21123c7c81ccaec6023
wschoi8640 <cws911112@gmail.com>
2019-04-23 18:55:45 +0900

    ADD ComNumReceiver.java(Modulize ComNum)

---
+++ src/main/java/ComNumReceiver.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class ComNumReceiver {
+
+	/*
+	 * 임의의 값 3개를 정해 3자리 수로 만들어 주는 메소드
+	 * 
+	 * @return 임의의 세자리수
+	 */
+	public int receive() {
+
+		List<Integer> comNums = new Random().ints(1, 9).distinct().limit(3).boxed().collect(Collectors.toList());
+		return (comNums.get(0) * 100 + comNums.get(1) * 10 + comNums.get(2));
+	}
+}
