e8fc1b3648764148d825869664ab68e0a1a6dc5e
HeavyRyan92 <heavyryan92@gmail.com>
2019-11-28 05:37:23 +0900

    게임 상태를 선택 하는 문구를 출력하는 기능 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -19,9 +19,11 @@ public class BaseballGame {
         while(true) {
             if(calculateScore(user.getUserBat(),baseballs)){
                 System.out.println("3개 숫자를 모두 맞히셨습니다! 게임종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 break;
             }
         }
+
     }
 
 
