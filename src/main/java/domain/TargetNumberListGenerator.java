60f312a2a445522d9a3abcd35428058f784cbf8e
kouz <lastpuzly@gmail.com>
2019-11-27 23:20:32 +0900

    feat: 1부터 9까지의 숫자로 이루어진 난수 리스트 생성 구현

---
+++ src/main/java/domain/TargetNumberListGenerator.java
@@ -0,0 +1,45 @@
+/*
+ * TargetNumberListGenerator.java
+ * java-baseball-precourse
+ *
+ * Version 0.1
+ *
+ * Created by 김경준 on 27/11/2019.
+ *
+ * Copyright © 2019 kouz95. All rights reserved.
+ */
+
+package domain;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TargetNumberListGenerator {
+    private static final int MAX_NUM = 987;
+    private static final int MIN_NUM = 123;
+    private static final int BOUND_FROM_ZERO = MAX_NUM - MIN_NUM;
+    private static final int NUM_LEN = 3;
+
+    private ArrayList<Integer> targetNumberList = new ArrayList<>();
+
+    private void setTargetNumberList() {
+        String targetNumberString;
+        targetNumberString = generateRandomNumberString();
+        addToList(targetNumberString);
+    }
+
+    private String generateRandomNumberString() {
+        return Integer.toString(generateRandomNumber());
+    }
+
+    private int generateRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(BOUND_FROM_ZERO) + MIN_NUM + 1;
+    }
+
+    private void addToList(String string) {
+        for(int i = 0 ; i < string.length(); i++) {
+            targetNumberList.add((int)string.charAt(i));
+        }
+    }
+}
