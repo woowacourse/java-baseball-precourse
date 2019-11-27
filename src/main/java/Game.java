db4bdfe0ba7c40566364b5bd1bc7642c9025984a
DB2 <10100aaaaa@gmail.com>
2019-11-27 18:51:20 +0900

    사용자 입력 예외처리(중복, 범위) 메소드 추가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -28,26 +28,38 @@ public class Game {
         this.playerNumbers = playerNumbers;
     }
 
-    public void start(){
-        showInputMessage();
-        setPlayerNumbers(getInputsByScanner());
+    public void start() {
+        do {
+            showInputMessage();
+            setPlayerNumbers(getInputsByScanner());
+        } while (isOverlap(getPlayerNumbers()) || isOutOfScope(getPlayerNumbers()));
+
     }
 
-    public int[] randomGenerator(){
-        return ThreadLocalRandom.current()
-                .ints(1,10)
+    public int[] randomGenerator() {
+        return ThreadLocalRandom.current().ints(1,10)
                 .distinct()
                 .limit(3)
                 .toArray();
     }
 
-    public void showInputMessage(){
+    public void showInputMessage() {
+        if(playerNumbers != null){
+            System.out.println("잘못된 입력입니다.");
+        }
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public int[] getInputsByScanner(){
+    public int[] getInputsByScanner() {
         return new int[]{input.nextInt(), input.nextInt(), input.nextInt()};
     }
 
+    public boolean isOverlap(int[] numbers) {
+        return Arrays.stream(numbers).distinct().count() < 3;
+    }
+
+    public boolean isOutOfScope(int[] numbers) {
+        return Arrays.stream(numbers).anyMatch(x -> (x < 1 || x > 9));
+    }
 
 }
