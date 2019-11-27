d5c0b4f5416100465ebce9352ddb7bbcd3b04e8a
HeavyRyan92 <heavyryan92@gmail.com>
2019-11-28 04:44:48 +0900

    계산한 결과를 출력하는 기능 구현

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,6 +1,11 @@
 import java.util.List;
 
 public class BaseballGame {
+
+    private static final int STRIKE = 0;
+    private static final int BALL = 1;
+    private static final int SCORE_SIZE = 2;
+
     public static void main(String[] args) {
         Baseball baseball = new Baseball();
         User user = new User();
@@ -15,16 +20,22 @@ public class BaseballGame {
     }
 
     private static void calculateScore(List<Integer> userBats, List<Integer> baseballs) {
-        int[] gameScore = new int[2];
+        int[] gameScore = new int[SCORE_SIZE];
         for(int userBatLoc=0; userBatLoc<userBats.size(); userBatLoc++) {
             if(baseballs.contains(userBats.get(userBatLoc)) && baseballs.get(userBatLoc).equals(userBats.get(userBatLoc))) {
-                gameScore[0]++;
+                gameScore[STRIKE]++;
                 continue;
             }
             if(baseballs.contains(userBats.get(userBatLoc)) && !baseballs.get(userBatLoc).equals(userBats.get(userBatLoc))) {
-                gameScore[1]++;
+                gameScore[BALL]++;
             }
         }
+        displayGameScore(gameScore);
     }
+
+    private static void displayGameScore(int[] gameScore) {
+        System.out.println(gameScore[STRIKE] + "스트라이크" + gameScore[BALL] + "볼");
+    }
+
 
 }
