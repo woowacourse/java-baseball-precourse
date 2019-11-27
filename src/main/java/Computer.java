fdc37d4d5e21cce7579c70da53aa662ba3c02c33
sezikim <kdghj1002@gmail.com>
2019-11-28 04:23:11 +0900

    add makeBaseballGameAnswer

---
+++ src/main/java/Computer.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Computer {
+    private List<Integer> baseballGameAnswer;
+
+    private static final int MAX_NUMBER = 9;
+    private static final int ANSWER_NUMBERS_COUNT = 3;
+
+    Computer() {
+        this.baseballGameAnswer = makeBaseballGameAnswer();
+    }
+
+
+    private List<Integer> makeBaseballGameAnswer() {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < MAX_NUMBER; ++i) {
+            numbers.add(i + 1);
+        }
+
+        List<Integer> baseBallGameAnswer = new ArrayList<>();
+
+        for (int j = 0; j < ANSWER_NUMBERS_COUNT; ++j) {
+            int randomNumber = getRandomNumber() % numbers.size();
+            baseBallGameAnswer.add(randomNumber);
+            numbers.remove(randomNumber);
+        }
+
+        return baseBallGameAnswer;
+    }
+
+    private int getRandomNumber() {
+        return (int)(Math.random() * MAX_NUMBER);
+    }
+
+
+}
