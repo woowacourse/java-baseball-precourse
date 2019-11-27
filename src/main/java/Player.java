a26209107bac402e76ec0cbbc7b81eba889313d0
sezikim <kdghj1002@gmail.com>
2019-11-28 04:49:11 +0900

    add getPlayerInputNumber

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,15 @@
-public class Player {
+import java.util.Scanner;
+
+class Player {
+    private boolean wantToContinue;
+    private Scanner input;
+
+    Player() {
+        this.wantToContinue = true;
+        this.input = new Scanner(System.in);
+    }
+
+    public int getPlayerInputNumber() {
+        return input.nextInt();
+    }
 }
