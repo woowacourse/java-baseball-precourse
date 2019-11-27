b99df8d922e79583c4e4d84bf697339f6f4735c5
HeavyRyan92 <heavyryan92@gmail.com>
2019-11-28 03:29:18 +0900

    세자리 난수 생성(Baseball), 예외 처리

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Baseball {
+
+    private final int BASEBALL_LENGTH = 3;
+
+    public int[] getBaseball() {
+        int[] baseballArray = new int[BASEBALL_LENGTH];
+        List<Integer> baseballList = new ArrayList<>();
+
+        return createBaseball(baseballArray, baseballList);
+    }
+
+    private int[] createBaseball(int[] baseballArray, List<Integer> baseballList) {
+
+        while(baseballList.size() != 3) {
+
+            double createBaseball = Math.random();
+            int baseball = (int)(createBaseball*9)+1;
+            if(!baseballList.contains(baseball)) { baseballList.add(baseball); }
+        }
+
+        return inputBaseballListToArray(baseballArray, baseballList);
+    }
+
+    private int[] inputBaseballListToArray(int[] baseballArray, List<Integer> baseballList) {
+
+        for(int i=0; i<baseballList.size(); i++) {
+
+            baseballArray[i] = baseballList.get(i);
+        }
+
+        return baseballArray;
+    }
 }
