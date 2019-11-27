274c96c03577525f63643d2700ae589f15931356
Devhyein <jhn7121@naver.com>
2019-11-28 00:17:17 +0900

    README no.2 get 3 numbers from the user

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,9 +1,14 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Baseball {
     public static void main(String args[]) {
         int[] comNum = new int[3];
         boolean check;
+        int[] userNum = new int[3];
+        int num;
+        Scanner sc = new Scanner(System.in);
+
         for(int i = 0; i < 3; i++) {
             comNum[i] = (int) (Math.random() * 9) + 1; //1~10까지 난수 생성
             check = checkOverlap(comNum[i], comNum, i);
@@ -11,7 +16,13 @@ public class Baseball {
                 i -= 1;
             }
         }
-        System.out.print(Arrays.toString(comNum));
+
+        while (true) {
+            for(int i=0; i<3; i++) {
+                userNum[i] = sc.nextInt();
+            }
+            
+        }
     }
 
     public static boolean checkOverlap(int num, int[] comNum, int i) {
