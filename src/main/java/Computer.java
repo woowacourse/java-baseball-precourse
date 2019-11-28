9f475c96a9f8f8b85647881d4b4784acb37b6032
sujin cha <cktnwls1@knu.ac.kr>
2019-11-28 23:26:09 +0900

    control flow of total game

---
+++ src/main/java/Computer.java
@@ -0,0 +1,33 @@
+/*
+ * Class Name: main.java
+ * Version: 0.1
+ * Date: 2019.11.28
+ * Copyright(c) 2019 Sujin Cha(MIR013)
+ * All rights reserved.
+ *
+ * This software is made for precourse of woowacourse.
+ */
+
+/**
+ * 클래스 Computer의 역할
+ * 숫자야구 게임을 위한 난수를 생성한다.(단, 중복x)
+ * 그리고 사용자가 언급한 수와 난수와 비교하겨 결과를 출력한다.
+ */
+public class Computer {
+    /** 생성한 야구 게임 수 */
+    private String number = new String();
+
+    /** 초기화와 동시에 야구게임 난수 생성 */
+    public Computer()
+    {
+
+    }
+
+    /** 특정값에 따라 결과 반환 */
+    public boolean checkBaseball(String value)
+    {
+
+        return false;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@
 //package
 
 //import
+import java.util.Scanner;
 
 /**
  * 클래스 Main의 역할
@@ -20,9 +21,50 @@
  */
 public class Main {
 
+    private static final int BASEBALL_SIZE = 3;
+
     public static void main(String[] agrs)
     {
-        System.out.println("hello baseball");
+        System.out.println("hello! Wellcome to baseball game");
+        //변수 생성
+        Scanner scan = new Scanner(System.in);  // 입력용 스캐너
+        int isGoing;                // 게임 진행 여부
+
+        //게임 진행
+        do{
+            playBaseballGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            isGoing = scan.nextInt();
+        }while(isGoing == 1);
+
     }
+
+    /** 1번의 게임을 돌린다. */
+    private static void playBaseballGame()
+    {
+        //변수 생성
+        Computer computer = new Computer();     // 컴퓨터 객체 생성
+        String value = new String();         // 입력값 저장용 변수
+        Scanner scan = new Scanner(System.in);  // 입력용 스캐너
+
+        //게임 시작
+        do{
+            do{
+                value = generateValue();
+            }while(value == null);
+        }while(computer.checkBaseball(value));
+
+        //게임 종료
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        return;
+    }
+
+    /** 값을 생성해온다. */
+    private static String generateValue()
+    {
+       return null;
+    }
+
+
 
 }
