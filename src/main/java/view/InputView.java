e36792994f40385adccb08faa0ae1d66d3c35476
kouz <lastpuzly@gmail.com>
2019-11-28 18:30:15 +0900

    feat: 플레이어 Retry or Quit 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,7 +2,7 @@
  * Inputview.java
  * java-baseball-precourse
  *
- * Version 0.1
+ * Version 0.2
  *
  * Created by 김경준 on 27/11/2019.
  *
@@ -16,9 +16,15 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String NEW_GAME_OR_QUIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String getUserString() {
         System.out.println(INPUT_MESSAGE);
         return scanner.nextLine();
+    }
+
+    public static int getNewGameOrQuitNumber() {
+        System.out.println(NEW_GAME_OR_QUIT_MESSAGE);
+        return scanner.nextInt();
     }
 }
