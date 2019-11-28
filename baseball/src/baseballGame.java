068a25bd07dc53ad07481aecd0320e466f9b021f
Jinn_o <sstaw@naver.com>
2019-11-28 21:05:25 +0900

    initBaseballNumber 메소드 구현

--- baseball/src/baseballGame.java
+++ baseball/src/baseballGame.java
@@ -1,7 +1,32 @@
+import java.util.Random;
+
 /**
  * @author SMJin
  * baseballGame Class
  * */
 public class baseballGame {
 
+    public static void main(String[] args) {
+
+        int answerNumA = 0;
+        int answerNumB = 0;
+        int answerNumC = 0;
+
+        int[] answerNum = initBaseballNumber(answerNumA, answerNumB, answerNumC);
+
+    }
+
+    /*
+    정답 숫자들을 랜덤으로 생성하여 int형 배열로 반환하여 주는 메소드이다.
+     */
+    static private int[] initBaseballNumber (int answerNumA, int answerNumB, int answerNumC) {
+        Random random = new Random();
+        answerNumA = random.nextInt(10)+1;
+        answerNumB = random.nextInt(10)+1;
+        answerNumC = random.nextInt(10)+1;
+
+        int[] answerNum = {answerNumA, answerNumB, answerNumC};
+
+        return answerNum;
+    }
 }
