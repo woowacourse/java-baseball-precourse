d0f07763b51d189c236f40f287df81f8637ebf75
DB <rkdrbgks22@ks.ac.kr>
2019-11-28 11:15:43 +0900

    [ADD] 메뉴 추가
    - 1번 입력 시 게임이 시작
    - 2번 입력 시 게임이 종료
    - 등록되지 않은 입력의 경우 메뉴를 다시 실행

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
---
+++ src/main/java/Menu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Menu {
+    public void menu() {
+        Scanner inputNum = new Scanner(System.in);
+        char selected = '0';
+        while (!(selected == '2')) {
+            System.out.println("!! Number Baseball Game !!");
+            System.out.println();
+            System.out.println("1. Solution Mode");
+            System.out.println("2. Exit");
+            selected = inputNum.next().charAt(0);
+
+            if (selected == '1') {
+            }
+            else if (selected == '2') {
+                System.out.println("Quit game...");
+            }
+            else {
+                System.out.println("Wrong input!!");
+            }
+            System.out.println();
+        }
+    }
+}
