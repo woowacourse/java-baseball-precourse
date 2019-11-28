e781060fbfe8537331f2dfe265b64f04b8a32eba
sujin cha <cktnwls1@knu.ac.kr>
2019-11-28 23:28:49 +0900

    generate theh input value

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,6 +39,15 @@ public class Main {
 
     }
 
+    /** 예외처리 throws */
+    static void callBSSizeException() throws MyBaseballSizeException{
+        throw  new MyBaseballSizeException(BASEBALL_SIZE);
+    }
+
+    static void callBSSameValueException() throws MyBaseballSameValueException{
+        throw  new MyBaseballSameValueException();
+    }
+
     /** 1번의 게임을 돌린다. */
     private static void playBaseballGame()
     {
@@ -62,9 +71,55 @@ public class Main {
     /** 값을 생성해온다. */
     private static String generateValue()
     {
-       return null;
+        //변수 생성
+        String value = new String();            // 입력값 저장용 변수
+        Scanner scan = new Scanner(System.in);  // 입력용 스캐너
+
+        try{
+            System.out.print("숫자를 입력해주세요: ");
+            value = scan.nextLine();
+
+            //예외사항1. 입력 사이즈가 안맞을 경우
+            if (value.length() != BASEBALL_SIZE){
+                callBSSizeException();
+            }
+            //예외사항2. 서로 다른 수를 입력하지 않았을 경우
+            if (isSameValue(value)){
+                callBSSameValueException();
+            }
+        }catch (MyBaseballSizeException e){
+            System.out.println(e.getMessage());
+            System.out.println("다시 입력해 주세요");
+            value = null;
+            //e.printStackTrace();
+            //이 후에 에러로 종료시킬 수 없나?
+        }catch (MyBaseballSameValueException e2){
+            System.out.println(e2.getMessage());
+            System.out.println("다시 입력해 주세요");
+            value = null;
+        }
+
+        return value;
+    }
+
+    /** 같은 값을 입력으로 준 경우를 체크한다. */
+    public static boolean isSameValue(String value)
+    {
+       return false;
     }
 
 
 
+}
+
+/** 숫자 야구 게임의 입력값을 위한 예외처리를 담당 */
+class MyBaseballSizeException extends Exception{
+    public MyBaseballSizeException(int baseballSize){
+        super("[ERROR] 숫자 야구 게임의 입력 값은 "+baseballSize+"자리 입니다.");
+    }
+}
+class MyBaseballSameValueException extends Exception{
+    public MyBaseballSameValueException(){
+        super("[ERROR] 숫자 야구 게임의 입력 값은 중복이 되면 안됩니다.");
+    }
 }
