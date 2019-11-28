d1404074f0dbe4e9059c07d26f27f5120c854f86
JunSeong <ebseud6135@naver.com>
2019-11-28 23:17:00 +0900

    힌트 계산하기 기능 구현

--- src/main/java/NumberBaseBallGame.java
+++ src/main/java/NumberBaseBallGame.java
@@ -12,10 +12,40 @@ public class NumberBaseBallGame {
     public static final int DIGIT = 3;
     public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 9;
-    private String randomNumbers;
+    private String randomNumber;
+    private String playerNumber;
+    private GameResult gameResult;
 
+    public GameResult calculateResult() {
+        int strikeCount = 0;
+        int ballCount = 0;
+        for (int i=0; i<DIGIT; i++) {
+            if (randomNumber.charAt(i) == playerNumber.charAt(i)){
+                strikeCount++;
+            } else if (randomNumber.indexOf(playerNumber.charAt(i)) != -1) {
+                ballCount++;
+            }
+        }
 
-    public void setRandomNumbers(String randomNumbers) {
-        this.randomNumbers = randomNumbers;
+        GameResult aGameResult = new GameResult();
+        aGameResult.setStrike(strikeCount);
+        aGameResult.setBall(ballCount);
+
+        return aGameResult;
     }
+
+    public void setGameResult(GameResult gameResult) {
+        this.gameResult = gameResult;
+    }
+
+    public void setRandomNumbers(String randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    public void setPlayerNumber(String playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+
+
 }
