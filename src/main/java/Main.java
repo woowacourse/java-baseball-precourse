2f8e9b9d1d84f266a5da072d7ae010431923e6bb
sujin cha <cktnwls1@knu.ac.kr>
2019-11-28 21:53:52 +0900

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+/*
+* Class Name: main.java
+* Version: 0.1
+* Date: 2019.11.28
+* Copyright(c) 2019 Sujin Cha(MIR013)
+* All rights reserved.
+*
+* This software is made for precourse of woowacourse.
+*/
+
+//package
+
+//import
+
+/**
+ * 클래스 Main의 역할
+ * 전체 게임의 흐름을 담당한다.
+ * 객체들을 생성하고, 값을 입력받고, 게임시작흐름을 관리한다.
+ * 어떻게보면, 사용자와 유사하다.
+ */
+public class Main {
+
+    public static void main(String[] agrs)
+    {
+        System.out.println("hello baseball");
+    }
+
+}
