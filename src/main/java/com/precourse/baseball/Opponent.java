999be724eb537ae543af90f48c71d8fb94fcc65e
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-11-28 15:31:29 +0900

    [추가] 랜덤으로 서로 다른 세자리의 수를 만드는 기능 구현

--- src/main/java/com/precourse/baseball/Opponent.java
+++ src/main/java/com/precourse/baseball/Opponent.java
@@ -13,25 +13,62 @@ package com.precourse.baseball;
  * 플레이어가 예측에 틀릴 경우 힌트를 줍니다.
  *
  * @author HyungjuAn
- * @version 1.2         최댓값 제한 fianl 상수 구현,
- *                      threeDigitNumbers -> digitNumbers 수정
+ * @version 1.3        랜덤으로 서로 다른 세자리의 수를 만드는 기능 구현
  * @date 2019-11-28
  */
 public class Opponent {
-    private final int INF_MASK = 10;    // 마스크 크기 0 ~ 9
-    private final int INF_DIGIT = 3;    // 최대 자릿수
+    private final int ZERO = 0;
+    private final int TEN = 10;
+    private final int MAX_MASK = 10;    // 마스크 크기 0 ~ 9
+    private final int MAX_DIGIT = 3;    // 최대 자릿수
     private boolean[] digitMasks;       // 수 마스킹용 array
     private int[] digitNumbers;         // 생각한 수 array
 
     public Opponent() {
-        this.digitMasks = new boolean[INF_MASK];
-        this.digitNumbers = new int[INF_DIGIT];
+        this.digitMasks = new boolean[MAX_MASK];
+        this.digitNumbers = new int[MAX_DIGIT];
     }
 
     public void setRandomNumbers() {
-        for (int i = 0; i < INF_DIGIT; i++) {
-            digitNumbers[i] = (int) (Math.random() * 10);    // 0 ~ 9 까지
+        setDigitMasks(new boolean[MAX_MASK]);                 // digitMasks 초기화
+
+        for (int i = 0; i < MAX_DIGIT; i++) {
+            digitNumbers[i] = (int) (Math.random() * TEN);    // 0 ~ 9 까지
+            digitMasks[digitNumbers[i]] = true;
         }
+
+        if (!isValidNumbers() || isDuplicate()) {
+            setRandomNumbers();                               // 만든 세자리의 수가 유효하지 않는 경우
+        }
+    }
+
+    public boolean isValidNumbers() {
+        boolean result = true;
+
+        for (int i = 0; i < MAX_DIGIT; i++) {
+            if (digitNumbers[i] == ZERO) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isDuplicate() {
+        boolean result = false;
+        int count = 0;
+
+        for (int i = 0; i < MAX_MASK; i++) {
+            if (digitMasks[i]) {
+                count++;
+            }
+        }
+        if (count < MAX_DIGIT) {
+            result = true;
+        }
+
+        return result;
     }
 
     /* Getter Methods */
