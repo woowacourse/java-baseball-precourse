015870eec6e22c198ee28919d176d33358d156bb
chanin <pci2676@gmail.com>
2019-11-28 23:48:05 +0900

    java-baseball-precourse feat 입력 작성  - 숫자입력  - 경기 진행여부 입력

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.util.stream.Collectors.toList;
+
+public class InputView {
+    private static final String DELIMITER = "";
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static List<Integer> inputNumbers() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        String inputLine = SCANNER.nextLine();
+        return Arrays.stream(inputLine.split(DELIMITER))
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
+    public static Integer inputContinueFlag() {
+        System.out.println("게임을 새로 시작하려 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+}
