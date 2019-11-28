38cd34dda7162db0f02e27756f40e3ea4d9bd99d
begaonnuri <begaonnuri@gmail.com>
2019-11-28 11:31:36 +0900

    [Refactoring] 게임 종료 후 다음 동작 진행하는 방식을 변경  - nextStep 처리를 switch문 -> do-while문으로 변경  - startNewGame 메소드도 strikeCount 초기화할 필요 없게 while문 -> do-while문으로 변경

--- src/main/java/BaseballGameApplication.java
+++ src/main/java/BaseballGameApplication.java
@@ -2,34 +2,29 @@ public class BaseballGameApplication {
     public static void main(String[] args) {
         int nextStep;
 
-        startNewGame();
-        OutputBaseballGame.printFinishMsg();
-        nextStep = InputBaseballGame.inputNextStep();
-
-        switch (nextStep) {
-            case 1:
-                startNewGame();
-
-            case 2:
-                return;
-        }
+        do {
+            startNewGame();
+            OutputBaseballGame.printFinishMsg();
+            nextStep = InputBaseballGame.inputNextStep();
+        } while (nextStep == 1);
     }
 
     public static void startNewGame() {
         int userNumber;
-        int strikeCount = 0, ballCount;
+        int strikeCount, ballCount;
         BaseballGameSystem game = new BaseballGameSystem();
 
-        while (strikeCount != 3) {
+        do {
             userNumber = InputBaseballGame.inputUserNumber();
 
             game.checkAnswer(userNumber);
+
             strikeCount = game.getStrikeCount();
             ballCount = game.getBallCount();
             OutputBaseballGame.printPitchResult(strikeCount, ballCount);
 
             game.setStrikeCount(0);
             game.setBallCount(0);
-        }
+        } while (strikeCount != 3);
     }
 }
