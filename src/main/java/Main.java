595f4ad2a4cef0a07d81633f7b7e8d44b4262bc9
icyMojito <dev.icymojito@gmail.com>
2019-11-29 00:34:48 +0900

    2단계 완성 사용자에게 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력받아 유효한 값인지 검사한다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Main {
     private static final int NUMS_MAX_LENGTH = 3;
     private static final int MAX_PICK_NUM = 9;
     private static final int MIN_PICK_NUM = 1;
+    private static final int INVALID_VALUE = 0;
 
     private static ArrayList<Integer> pickComputerNums() {
         ArrayList<Integer> computerNums = new ArrayList<>();
@@ -24,6 +27,67 @@ public class Main {
             }
         }
         return false;
+    }
+
+    private static ArrayList<Integer> getUserNums() {
+        while (true) {
+            String userInput = getUserInput();
+            if (isThreeDigitNums(userInput) && doesNotContainZero(userInput) && isNotDuplicatedNums(userInput)) {
+                return getUserNumsList(userInput);
+            }
+            System.out.println("★1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해 주세요.");
+        }
+    }
+
+    private static String getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    private static ArrayList<Integer> getUserNumsList(String userInput) {
+        ArrayList<Integer> userInputList = new ArrayList<>();
+        for (int i=0; i<userInput.length(); i++) {
+            int num = (int)userInput.charAt(i);
+            userInputList.add(num);
+        }
+        return userInputList;
+    }
+
+    private static boolean isThreeDigitNums(String userInput) {
+        if (userInput.length() != NUMS_MAX_LENGTH) {
+            return false;
+        }
+
+        for (int i=0; i<userInput.length(); i++) {
+            char userInputCh = userInput.charAt(i);
+            if (userInputCh < toCharacter(MIN_PICK_NUM) || toCharacter(MAX_PICK_NUM) < userInputCh) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static char toCharacter(int num) {
+        return Character.forDigit(num, 10);
+    }
+
+    private static boolean doesNotContainZero(String userInput) {
+        for (int i=0; i<userInput.length(); i++) {
+            if (userInput.charAt(i) == toCharacter(INVALID_VALUE)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNotDuplicatedNums(String userInput) {
+        HashSet<Character> numSet = new HashSet<>();
+        for (int i=0; i<userInput.length(); i++) {
+            numSet.add(userInput.charAt(i));
+        }
+        return numSet.size() == NUMS_MAX_LENGTH;
     }
 
     public static void main(String[] args) {
