28adc54675e68a8c412a902fdfbc56ca28fffff4
KSKIM <kwangsoonkim@outlook.com>
2019-11-29 00:31:11 +0900

    feat: add ball count calculator

---
+++ src/main/java/baseballgame/BallCount.java
@@ -0,0 +1,50 @@
+package baseballgame;
+
+/**
+ * @author KSKIM
+ * @version 1.0.0
+ * @since 2019-11-28
+ */
+public class BallCount {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final int ZERO = 0;
+
+    private final int strike;
+    private final int ball;
+
+    public BallCount(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    @Override
+    public String toString() {
+        if (hasStrike() && hasBall()) {
+            return String.format("%d %s %d %s", strike, STRIKE, ball, BALL);
+        }
+
+        if (hasStrike()) {
+            return String.format("%d %s", strike, STRIKE);
+        }
+
+        if (hasBall()) {
+            return String.format("%d %s", ball, BALL);
+        }
+
+        return NOTHING;
+    }
+
+    public boolean hasStrike() {
+        return strike > ZERO;
+    }
+
+    public boolean hasBall() {
+        return ball > ZERO;
+    }
+
+    public boolean isStrikeOut() {
+        return strike == Number.TOTAL_NUMBER;
+    }
+}
---
+++ src/main/java/baseballgame/BallCountCalculator.java
@@ -0,0 +1,46 @@
+package baseballgame;
+
+import java.util.Objects;
+
+/**
+ * @author KSKIM
+ * @version 1.0.0
+ * @since 2019-11-28
+ */
+public class BallCountCalculator {
+    private final Number answer;
+
+    public BallCountCalculator(Number answer) {
+        this.answer = Objects.requireNonNull(answer);
+    }
+
+    public BallCount getBallCount(Number number) {
+        int strike = getStrike(number);
+        int ball = getBall(number);
+        return new BallCount(strike, ball);
+    }
+
+    public int getStrike(Number number) {
+        int strike = 0;
+        for (int index = 0; index < Number.TOTAL_NUMBER; ++index) {
+            if (isStrike(index, number)) {
+                ++strike;
+            }
+        }
+        return strike;
+    }
+
+    public int getBall(Number number) {
+        int ball = 0;
+        for (int index = 0; index < Number.TOTAL_NUMBER; ++index) {
+            if (!isStrike(index, number) && answer.contains(number.get(index))) {
+                ++ball;
+            }
+        }
+        return ball;
+    }
+
+    public boolean isStrike(int index, Number number) {
+        return answer.get(index) == number.get(index);
+    }
+}
