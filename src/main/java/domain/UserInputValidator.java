a1d36604761465a618157599318845c20d667e69
kouz <lastpuzly@gmail.com>
2019-11-28 10:51:32 +0900

    feat: 각 입력 문자 범위 유효성 검사 기능 구현

--- src/main/java/domain/UserInputValidator.java
+++ src/main/java/domain/UserInputValidator.java
@@ -2,7 +2,7 @@
  * UserInputValidator.java
  * java-baseball-precourse
  *
- * Version 0.1
+ * Version 0.2
  *
  * Created by 김경준 on 28/11/2019.
  *
@@ -21,11 +21,23 @@ public class UserInputValidator {
     }
 
     private boolean setIsValidString() {
-        return isValidLength();
+        if(!isValidLength()) {
+            return false;
+        }
+        if(isOutOfNumberRange()) {
+            return false;
+        }
+        return true;
     }
 
     private boolean isValidLength() {
         final int VALID_LENGTH = 3;
         return inputString.length() == VALID_LENGTH;
+    }
+
+    private boolean isOutOfNumberRange() {
+        final int MAX_NUM = 987;
+        final int MIN_NUM = 123;
+        return Integer.parseInt(inputString) > MAX_NUM || Integer.parseInt(inputString) < MIN_NUM;
     }
 }
