58de2cc4b8e64f0b91b29adadc17d7322e65c740
giantim <giantim132@gmail.com>
2019-11-28 13:02:00 +0900

    숫자 입력 받는 기능 구현

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,28 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class Player {
-    private List<Integer> playerNumberSet;
+    private ArrayList<Integer> playerNumberSet;
+
+    public Player() {
+        playerNumberSet = new ArrayList<Integer>();
+    }
+
+    public String inputNumber(String input) {
+        if (input.length() != 3)
+            return "3자리의 수를 입력해주세요.";
+        if (!checkValidation(input))
+            return "0을 제외하고 입력해주세요.";
+        for (char c : input.toCharArray()) {
+            playerNumberSet.add(c - '0');
+        }
+        return null;
+    }
+
+    private boolean checkValidation(String input) {
+        for (char c : input.toCharArray()) {
+            if (!(c >= 49 && c <= 57))
+                return false;
+        }
+        return true;
+    }
 }
