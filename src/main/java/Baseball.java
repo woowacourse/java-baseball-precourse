ee94a12f4f8aa30ce24fff71adb1daf6fa5c0317
HongBin kim <fusis1@naver.com>
2019-11-28 17:34:07 +0900

    function computeDataInit()

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -3,6 +3,21 @@ public class Baseball {
 
     static int randomdata[] = {0,0,0};
     static int user[] = {0,0,0};
+    // 컴퓨터의 랜덤값을 배열에 넣어주는 함수
+
+    static void computeDataInit(int i){
+        boolean visit[] = {true,false,false,false,false,false,false,false,false,false,false};
+        for(int k=0;k<i;k++) {
+            visit[randomdata[k]] = true;
+        }
+        while(true){
+            int temp = (int)(Math.random()*9)+1;
+            if(!visit[temp]){
+                randomdata[i] = temp;
+                break;
+            }
+        }
+    }
 
     // 실질적 게임을 작동하는 함수
     static int game(){
@@ -19,6 +34,9 @@ public class Baseball {
         int n = 1;
         while (n == 1){
             // depth 1
+            for(int i =0;i<3;i++) {
+                computeDataInit(i);
+            }
             n = game();
         }
     }
