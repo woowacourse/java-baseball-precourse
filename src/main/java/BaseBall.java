307535c89b99f9bda40f859da7e019715b9ee3d9
Mac Pro <sangbo@Macui-MacBook-Pro.local>
2019-11-28 16:24:57 +0900

    랜덤숫자 3개 구하기

---
+++ src/main/java/BaseBall.java
@@ -0,0 +1,13 @@
+
+public class BaseBall {
+
+	public static void main(String[] args) {
+		int f_num, s_num, t_num;
+		
+		f_num = (int)(Math.random()*9) + 1;
+		s_num = (int)(Math.random()*9) + 1;
+		t_num = (int)(Math.random()*9) + 1;
+		
+	}
+
+}
