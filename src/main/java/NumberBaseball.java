dc1ad6b600392739a1c6c29c74ee094536c0828b
ikswary <ikswary@naver.com>
2019-11-28 20:53:56 +0900

    기능 3 작성

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -2,20 +2,29 @@ import java.util.Scanner;
 
 class Num{
     int val=0;
+    int[] position = new int[3];
 
     public void setRnum(){
         while (this.val<100){
             this.val = (int)(Math.random()*1000);
         }
+        this.setPosition();
     }
+
     public void setVal(){
         System.out.println("type your answer : ");
         Scanner sc = new Scanner(System.in);
         this.val=sc.nextInt();
+        this.setPosition();
     }
+    private void setPosition(){
+        this.position[0] = this.val/100;
+        this.position[1] = this.val%100/10;
+        this.position[2] = this.val%10;
+    }
+
+
 }
-
-
 
 public class NumberBaseball {
 
@@ -24,8 +33,7 @@ public class NumberBaseball {
         Num player = new Num();
         answer.setRnum();
         player.setVal();
-        System.out.println(answer.val);
-        System.out.println(player.val);
     }
 
 }
+
