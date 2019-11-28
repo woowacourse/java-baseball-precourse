b2f41f1abcf5455e761b3bd8d93406fc86327934
Jinn_o <sstaw@naver.com>
2019-11-28 21:08:16 +0900

    isSame 메소드 구현

--- baseball/src/baseballGame.java
+++ baseball/src/baseballGame.java
@@ -29,4 +29,13 @@ public class baseballGame {
 
         return answerNum;
     }
+
+    /*
+    인자로 들어온 두 int 형 숫자가 같으면 TRUE 를 반환하는 메소드이다.
+     */
+    boolean isSame(int answerNum, int num) {
+        return (answerNum == num);
+    }
+
+
 }
