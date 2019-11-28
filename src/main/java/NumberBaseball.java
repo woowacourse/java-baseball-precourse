e481383582d1633326a73583290a5673b24c50e2
ikswary <ikswary@naver.com>
2019-11-28 20:18:25 +0900

    기능 2 작성

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -1,13 +1,17 @@
-import sun.security.util.ManifestDigester;
+import java.util.Scanner;
 
-class Rnum{
+class Num{
     int val=0;
 
     public void setRnum(){
         while (this.val<100){
             this.val = (int)(Math.random()*1000);
-            System.out.println(val);
         }
+    }
+    public void setVal(){
+        System.out.println("type your answer : ");
+        Scanner sc = new Scanner(System.in);
+        this.val=sc.nextInt();
     }
 }
 
@@ -16,9 +20,12 @@ class Rnum{
 public class NumberBaseball {
 
     public static void main(String[] args) {
-        Rnum answer = new Rnum();
+        Num answer = new Num();
+        Num player = new Num();
         answer.setRnum();
-        
+        player.setVal();
+        System.out.println(answer.val);
+        System.out.println(player.val);
     }
 
 }
