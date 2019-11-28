c2c0e695d2a35ed5162a0adf3c0943306f832a97
ParkDooWon <pass345@naver.com>
2019-11-28 18:15:32 +0900

    Main.java 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * baseballGame 실행을 위한 Main 클래스
+ * 
+ * @version 0.1 2019년 11월 28일
+ * @author 박두원
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
