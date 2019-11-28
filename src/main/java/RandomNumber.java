2a518d98fa7fc71eb3e1879245a950ccb83509f3
YebinK <yebin825@gmail.com>
2019-11-28 10:32:46 +0900

    기능1. 컴퓨터가 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 뽑기

---
+++ src/main/java/RandomNumber.java
@@ -0,0 +1,34 @@
+/**
+ * 클래스 이름       RandomNumber
+ * 버전 정보        1.0
+ * 날짜            2019.11.28
+ * 저작권          YebinK
+ */
+
+import java.util.Random;
+
+public class RandomNumber {
+
+    private int [] number = new int[3];
+    private int numberIndex = 0;
+
+    /* 1부터 9까지 서로 다른 3자리 수 뽑기 */
+    public void setNumber() {
+        Random r = new Random();
+
+        while (numberIndex < 3) {
+            int newNumber = r.nextInt(9) + 1;
+            if (!duplicated(newNumber)) {
+                number[numberIndex] = newNumber;
+                numberIndex++;
+            }
+        }
+    }
+
+    /* 새로 뽑은 수가 이전 수들과 중복되는지 확인 */
+    public boolean duplicated(int newNumber) {
+        for (int i = 0 ; i < numberIndex ; i++) {
+            if (number[i] == newNumber) return true;
+        } return false;
+    }
+}
