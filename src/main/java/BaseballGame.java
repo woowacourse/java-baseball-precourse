88ca4d501dc2bddf3d8d93bd7e5da9c9746a08d0
MOON JINJU <mjj920930@gmail.com>
2019-11-29 01:09:49 +0900

    fix: 컴퓨터의 숫자와 사용자의 숫자를 비교하는 기능을 BaseballGame 클래스로 이동, 불필요한 Computer 클래스 삭제

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,14 +1,34 @@
 public class BaseballGame {
-    private Computer computer;
+    private Numbers numbersOfComputer;
     private User user;
+    private boolean gameIsRunning;
 
     public BaseballGame() {
-        computer = new Computer();
+        numbersOfComputer = new Numbers();
         user = new User();
+        gameIsRunning = true;
     }
 
     public void run() {
-        Numbers numbersFromUser = user.inputNumbers();
-        computer.compareNumbersWith(numbersFromUser);
+        while(gameIsRunning) {
+            Numbers numbersFromUser = user.inputNumbers();
+            compareNumbersWith(numbersFromUser);
+        }
+    }
+
+    public void compareNumbersWith(Numbers numbersFromUser) {
+        int strike = numbersOfComputer.countStrike(numbersFromUser);
+        int ball = numbersOfComputer.countBall(numbersFromUser);
+
+        if((strike == 0) && (ball == 0)) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        System.out.println(strike + " 스트라이크 " + ball + " 볼");
+
+        if(strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 }
