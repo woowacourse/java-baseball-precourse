412e1a27c78bd2aa58bb61ab8adce78e2ba3d08b
HWooKim97 <rgd5454@naver.com>
2019-11-29 14:24:33 +0900

    add getInput

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,12 +2,13 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class User {
-    Scanner s = new Scanner(System.in);
     private int userInput;
 
     public void inputNumber() {
+        Scanner s = new Scanner(System.in);
+
         System.out.print("숫자를 입력해주세요 : ");
-        
+
         try {
             userInput = s.nextInt();
         } catch (InputMismatchException e) {
@@ -18,23 +19,29 @@ public class User {
             System.out.println("제대로 된 3자리 수를 입력해주세요!");
             inputNumber();
         }
+
+        s.close();
     }
 
-    public boolean checkInput(int n) {
+    public boolean checkInput(int input) {
         // input이 3자리 수가 아닌 경우
-        if(n < 100) {
+        if(input < 100) {
             return false;
-        } else if(n >= 1000) {
+        } else if(input >= 1000) {
             return false;
         }
 
         // input에 0이 들어가 있는 경우
-        for(; n > 0; n /= 10){
-            if(n % 10 == 0) {
+        for(; input > 0; input /= 10) {
+            if(input % 10 == 0) {
                 return false;
             }
         }
 
         return true;
+    }
+
+    public int getInput() {
+        return userInput;
     }
 }
