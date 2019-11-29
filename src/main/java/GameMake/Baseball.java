e685976a4ab1d8694973bbd24f30a1d6d0b8b789
Eun Seok <jeseok95@gmail.com>
2019-11-29 19:16:23 +0900

    사용자의 입력을 받는 makeInput 구현,빌드를 gradle로 했을때 print가 안나오는 버그 발견

--- src/main/java/GameMake/Baseball.java
+++ src/main/java/GameMake/Baseball.java
@@ -16,4 +16,21 @@ public class Baseball {
 //            System.out.print(ans[i]);
 //        }
     }
+    public void makeInput(){
+        System.out.flush();
+        System.out.print("숫자를 입력해주세요 : ");
+        Scanner sc = new Scanner(System.in);
+
+        int temp;
+        temp = sc.nextInt();
+        sc.nextLine();
+
+        for(int i=(input.length-1);i>=0;i--){
+            input[i] = temp%10;
+            temp/=10;
+        }
+//        for(int i=0;i<ans.length;i++) {
+//            System.out.print(input[i]);
+//        }
+    }
 }
--- src/main/java/baseballgame.java
+++ src/main/java/baseballgame.java
@@ -4,5 +4,6 @@ public class baseballgame {
     public static void main(String[] args) {
         Baseball game = new Baseball();
 
+        game.makeInput();
     }
 }
