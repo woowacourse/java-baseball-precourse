4a6be8b09c2fee9ab2ccded90ed98c65ff323981
lalize <printf@kakao.com>
2019-11-29 17:19:48 +0900

    feat: 게임 로직 처리

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Baseball {
     private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String INPUT_MENU_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String CORRECT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final int NEW_GAME = 1;
+    private static final int PROGRAM_EXIT = 2;
     private static final Scanner SCANNER = new Scanner(System.in);
+
+    private boolean isRunning = true;
 
     private static int[] inputNumbers() {
         try {
@@ -21,5 +26,40 @@ public class Baseball {
         } catch (Exception e) {
             return inputMenu();
         }
+    }
+
+    private void gameLoop() {
+        int[] computer = Computer.createNumbers();
+
+        while (true) {
+            int[] user = inputNumbers();
+            Result result = Computer.compute(user, computer);
+            System.out.println(result);
+            if (result.allMatch()) {
+                System.out.println(CORRECT_MESSAGE);
+                break;
+            }
+        }
+
+        switch (inputMenu()) {
+        case NEW_GAME:
+            break;
+        case PROGRAM_EXIT:
+            isRunning = false;
+            break;
+        default:
+            break;
+        }
+    }
+
+    public void run() {
+        while (isRunning) {
+            gameLoop();
+        }
+    }
+
+    public static void main(String[] args) {
+        Baseball baseball = new Baseball();
+        baseball.run();
     }
 }
