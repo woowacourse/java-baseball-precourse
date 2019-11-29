9f2f9a58d00604b5a4799154996bb0df445564f7
DESKTOP-64EH9A2\a301d <a301dks@naver.com>
2019-11-29 14:13:35 +0900

    [추가] 게임 재시작 기능 추가

--- src/main/java/com/precourse/baseball/Game.java
+++ src/main/java/com/precourse/baseball/Game.java
@@ -1,5 +1,5 @@
 /*
- * Game.java                       1.0.0   2019-11-29
+ * Game.java                       1.1.0   2019-11-29
  *
  * Copyright (c) 2019 Hyungju An.
  * All rights reserved.
@@ -8,18 +8,47 @@
 
 package com.precourse.baseball;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 게임 클래스입니다.
  * 숫자 야구 게임을 진행하는 메인 클래스입니다.
+ * 상대방(컴퓨터)가 생각한 수를 사용자가 예측하는 방식으로 진행,
+ * 게임이 끝나면 게임 재시작을 하거나, 게임을 완전히 종료한다.
  *
  * @author HyungjuAn
- * @version 1.0.0
+ * @version 1.1.0                        게임 재시작 기능 추가
  * @date 2019-11-29
  */
 public class Game {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Player computer = new Player();
+        Player user = new Player();
 
+
+    }
+
+    public static void startGame(Player computer, Player user) throws IOException{
+        do {
+
+        } while (isRestart());
+    }
+
+    public static boolean isRestart() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean result = false;
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int temp = Integer.parseInt(br.readLine());
+        if (temp == 1) {
+            result = true;
+        } else {
+            result = false;
+        }
+
+        return result;
     }
 }
