832b10118cd05242aa9fca6b9df8e54dde179c73
kimjunseok <jnunseok@gmail.com>
2019-11-29 12:50:48 +0900

    클래스 정리 및 기능 구현 완료

--- src/main/java/baseballGame.java
+++ src/main/java/baseballGame.java
@@ -8,17 +8,27 @@ public class baseballGame {
 
     public void baseballNumber() {
         numbers = new int[3];
-        this.setNumbers();
+        this.game();
     }
 
     public void game() {
         scan = new Scanner(System.in);
         boolean judge = false;
+        this.setNumbers();
         while (!judge) {
             System.out.print("숫자를 입력해주세요 : ");
             String meg = scan.next();
             int [] inputInt =  Arrays.stream(meg.split("")).mapToInt(Integer::parseInt).toArray();
             judge = this.numberCheck(this.numbers, inputInt);
+            if (judge) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int answer = scan.nextInt();
+                if (answer == 1) {
+                    judge = false;
+                    this.setNumbers();
+                }
+            }
         }
     }
 
@@ -76,6 +86,5 @@ public class baseballGame {
                 i--;
             }
         }
-        this.game();
     }
 }
