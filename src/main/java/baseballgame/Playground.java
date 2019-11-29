d19b7305a89dc6f5857a6413057a511f2d11613d
KongsonPark <sonypark0204+wootech@gmail.com>
2019-11-30 00:38:07 +0900

    [feat] Playground: 게임 재시작 또는 종료 선택 로직 구현
    - continueGameOrNot(): 재시작 또는 종료 선택
    - isGameOver(): 게임 종료 여부
    - printValidTypeOfNumber(): 올바른 선택 번호 알려주는 메시지 출력
    - printContinueGameOrNotMessage(): 재시작 또는 종료 선택 메시지 출력
    - printGameOverMessage(): 게임 종료 메시지 출력

--- src/main/java/baseballgame/Playground.java
+++ src/main/java/baseballgame/Playground.java
@@ -14,6 +14,8 @@ public class Playground {
             System.out.println(targetNumber);
         } while (isInProgress(scanner, targetNumber));
 
+        printGameOverMessage();
+
     }
 
     private boolean isInProgress(Scanner scanner, String targetNumber) {
@@ -30,6 +32,7 @@ public class Playground {
 
             if (isCorrectAnswer(targetNumber, inputNumber)) {
                 printCorrectAnswerMessage();
+                return continueGameOrNot();
             }
 
         }
@@ -37,6 +40,33 @@ public class Playground {
 
     private boolean isCorrectAnswer(String targetNumber, String inputNumber) {
         return targetNumber.equals(inputNumber);
+    }
+
+    private boolean continueGameOrNot() {
+        if (isGameOver(scanner)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isGameOver(Scanner scanner) {
+        final String STOP = "2";
+        final String CONTINUE = "1";
+        String inputNumber;
+
+        while (true) {
+            printContinueGameOrNotMessage();
+            inputNumber = scanner.nextLine(); // 예외 처리: 1) 1,2 이외의 숫자가 들어왔을 때, 2) 숫자가 아닌 경우
+
+            if (!inputNumber.equals(CONTINUE) && !inputNumber.equals(STOP)) {
+                printValidTypeOfNumber();
+                continue;
+
+            } else {
+                return inputNumber.equals(STOP);
+            }
+
+        }
     }
 
     private String getInputNumber(Scanner scanner) {
@@ -49,9 +79,21 @@ public class Playground {
         System.out.println("입력값이 잘못되었습니다.");
         System.out.println("1~9까지의 수 중 서로 다른 세자리 숫자를 입력해주세요.");
     }
-    
+
     private void printCorrectAnswerMessage() {
         System.out.println("정답! 3개의 숫자를 모두 맞혔습니다.");
+    }
+
+    private void printValidTypeOfNumber() {
+        System.out.println("1 또는 2만 입력 가능합니다.");
+    }
+
+    private void printContinueGameOrNotMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    }
+
+    private void printGameOverMessage() {
+        System.out.println("게임이 종료되었습니다.");
     }
 
 }
