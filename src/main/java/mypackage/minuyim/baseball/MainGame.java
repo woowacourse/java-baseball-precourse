cda0ddd95554a771d5fae69ff0b039d3fda1938a
minwoo_yim <minu_yim@naver.com>
2019-11-29 14:23:45 +0900

    Onegame 기능 추가

--- src/main/java/mypackage/minuyim/baseball/MainGame.java
+++ src/main/java/mypackage/minuyim/baseball/MainGame.java
@@ -1,20 +1,42 @@
 package mypackage.minuyim.baseball;
 
-import mypackage.minuyim.baseball.util.InputNumber;
-import mypackage.minuyim.baseball.util.MakeRandomNumber;
+import mypackage.minuyim.baseball.util.*;
 
 public class MainGame {
-
-	public static void main(String[] args) {
-		// int[] userNumber;
+	private static final int TARGET_NUMBER = 3;
+	
+	private static void Onegame(int[] answerNumbers) {
 		int userNumber;
-		int[] answerNumber;
+		int[] userNumbers;
 		
-		InputNumber inputNumberToArray = new InputNumber();
-		userNumber = inputNumberToArray.inputNumber();
+		while (true) {
+			InputNumber inputNumber = new InputNumber();
+			userNumber = inputNumber.inputNumber();
+			
+			ConvertToArray convertNumber = new ConvertToArray();
+			userNumbers = convertNumber.convertToArray(userNumber);
+			
+			MakeHint hint = new MakeHint(answerNumbers, userNumbers);
+			
+			PrintHint print = new PrintHint();
+			print.printHint(hint.strike, hint.ball);
+			
+			if (hint.strike == TARGET_NUMBER) {
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+				break;
+			}
+		}
+
+		return;
+	}
+	
+	public static void main(String[] args) {
+		int[] answerNumbers;
 		
 		MakeRandomNumber makeRandomNumber = new MakeRandomNumber();
-		answerNumber = makeRandomNumber.makeRandom();
+		answerNumbers = makeRandomNumber.makeRandom();
+		
+		Onegame(answerNumbers);
 
 		return;
 	}
