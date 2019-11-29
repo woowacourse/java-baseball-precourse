bbdd91555e356646602ac3a64f2cadc77d8030a0
Yeji Ahn <yeppilisa@naver.com>
2019-11-29 22:47:08 +0900

    [Feature]Change Integer(User input) into Integer Array

--- src/main/java/NumberBaseballGame.java
+++ src/main/java/NumberBaseballGame.java
@@ -1,8 +1,8 @@
 /*
  * 클래스 이름: NumberBaseballGame
  * 버전 정보: v1.0
- * 날짜: 11월 27일
- * 저작권 주
+ * 날짜: 11월 29일
+ * 저작권 주의
  */
 
 import java.util.Scanner;
@@ -14,11 +14,10 @@ public class NumberBaseballGame {
     }
 
     public static void Game() {
+        Scanner sc = new Scanner(System.in);
 
         /* 랜덤으로 정답이 되는 세 숫자 배정 */
         int answer[] = setAnswer();
-
-
     }
 
     public static int[] setAnswer() {
@@ -34,10 +33,22 @@ public class NumberBaseballGame {
         while (answer[0] == answer[2] || answer[1] == answer[2] || answer[2] == 0) {
             answer[2] = random.nextInt(10);
         }
+
         System.out.println("answer[0]: " + answer[0]);
         System.out.println("answer[1]: " + answer[1]);
         System.out.println("answer[2]: " + answer[2]);
 
         return answer;
     }
+
+    public static int[] numberIntoArray(int userInput) {
+        int[] userInputArray = new int[3];
+        userInputArray[2] = userInput % 10;
+        userInput /= 10;
+        userInputArray[1] = userInput % 10;
+        userInput /= 10;
+        userInputArray[0] = userInput % 10;
+        return userInputArray;
+    }
+
 }
