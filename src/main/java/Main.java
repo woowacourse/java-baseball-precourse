f7d879d624d28d4119d1960f0f866b7cc1beb51e
KongsonPark <sonypark0204+wootech@gmail.com>
2019-11-30 00:42:23 +0900

    [feat] Main 클래스 구현
    - Playground의 runBaseballGame 메서드 실행

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import baseballgame.Playground;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Playground pg = new Playground();
+        pg.runBaseballGame();
+    }
+}
