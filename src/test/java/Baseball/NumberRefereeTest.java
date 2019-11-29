c891d5ba4dd25418b7c5de65dac690ab7d76796a
junk-sound <wom2277@naver.com>
2019-11-29 19:27:20 +0900

    비기능: 기능4(심판이 타자와 투수의 경합 결과를 발표하는 기능)에 대한 단위 테스트.

--- src/test/java/Baseball/NumberRefereeTest.java
+++ src/test/java/Baseball/NumberRefereeTest.java
@@ -1,8 +1,11 @@
 package Baseball;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,9 +20,19 @@ class NumberRefereeTest {
     private List<Integer> ballsHit;
     private Result result;
 
+    private final ByteArrayOutputStream outView = new ByteArrayOutputStream();
+
+
+
     @BeforeEach
     void init() {
         referee = new NumberReferee();
+        System.setOut(new PrintStream(outView));
+    }
+
+    @AfterEach
+    void cleanUp() {
+        System.setOut(null);
     }
 
     @Test
@@ -167,5 +180,55 @@ class NumberRefereeTest {
         );
 
         assertThrows(InvalidParameterException.class, () -> referee.judge(tooManyBalls, tooShortBalls));
+    }
+
+    @Test
+    void announce_print_when_stirkes_exist_B_exist() {
+        //given
+        int strikes = 1;
+        int B = 2;
+        Result result = new Result(strikes,B);
+
+        //when
+        referee.announce(result);
+
+        //then
+        assertEquals(String.format("%d 스트라이크 %d볼\n", strikes, B), outView.toString());
+    }
+    @Test
+    void announce_print_when_only_strikes_exist() {
+        //given
+        int strikes = 1;
+        Result result = new Result(strikes,0);
+
+        //when
+        referee.announce(result);
+
+        //then
+        assertEquals(String.format("%d 스트라이크\n", strikes), outView.toString());
+    }
+    @Test
+    void announce_print_when_only_B_exist() {
+        //given
+        int B = 1;
+        Result result = new Result(0,B);
+
+        //when
+        referee.announce(result);
+
+        //then
+        assertEquals(String.format("%d볼\n", B), outView.toString());
+    }
+    @Test
+    void announce_print_when_StrikeOut() {
+        //given
+        int strikes = 3;
+        Result result = new Result(3, 0);
+
+        //when
+        referee.announce(result);
+
+        //then
+        assertEquals(String.format("%d 스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n", strikes), outView.toString());
     }
 }
