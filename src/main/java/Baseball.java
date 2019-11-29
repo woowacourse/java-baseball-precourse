c40bbf65af57611a709afe7736591cb4dc20b550
Kwon Taehyung <ktf1008@naver.com>
2019-11-29 22:36:02 +0900

    코드 컨벤션 맞춤

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,98 +1,150 @@
+/**
+ * Baseball class
+ *
+ * v1.0
+ *
+ * 2019-11-29
+ *
+ * MIT Licence
+ */
+import java.io.PrintStream;
 import java.util.Random;
 import  java.util.Scanner;
+
+
 public class Baseball {
+
+    /*게임 진행 클래스*/
     private String answer;
-    Baseball(){
-        Random rand = new Random();
-        answer = new String();
-        for(int i = 0, temp; i < 3;i++){
-            while(true) {
-                temp = rand.nextInt(8) + 1;
-                String currentNumber = Integer.toString(temp);
-                if (!answer.contains(currentNumber)) {
-                    answer += currentNumber;
-                    break;
-                }
-            }
+    private int stringMaxLength = 3;
+    private PrintStream printStream = System.out;
+    Baseball() {
+        answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < stringMaxLength; i++) {
+            sb.append(makeNumber());
         }
+        answer = sb.toString();
     }
-    public boolean doBaseball(){
-        while(true) {
-            String input = getInput();
-            int strike = getStrike(input);
-            if (strike == 3) {
-                System.out.println("3 strike");
-                System.out.println("You got an answer! end game");
-                break;
-            }
-            int ball = getBall(input);
-            if (strike + ball == 0) {
-                System.out.println("nothing");
-            }
-            else {
-                System.out.println(strike + " Strike, " + ball + " Ball");
-            }
-        }
-        System.out.println("To start new game press 1, Finish the game press 2");
+
+    public boolean doBaseball() {
         String option;
         Scanner scan = new Scanner(System.in);
-        while (true){
-            option = scan.nextLine();
-            if(option.equals("1")){
-                return true;
+
+        while (true) {
+            String input = getInput();
+            int strike = getStrike(input); //스트라이크 수
+            int ball = getBall(input); //볼 수
+
+            if (strike == stringMaxLength) {
+                printStream.println("3 strike"); //3스트라이크 시
+                printStream.println("You got an answer! end game");
+                break;
+            } else if (strike + ball == 0) {
+                printStream.println("nothing"); //모든 숫자가 빗나갔다면
+            } else {
+                printStream.println(strike + " Strike, " + ball + " Ball"); //하나라도 맞는다면
             }
-            if(option.equals("2")){
-                return false;
+        }
+        printStream.println("To start new game press 1, Finish the game press 2");
+
+        while (true) {
+            option = scan.nextLine(); //유효한 입력을 받을때까지 루프
+
+            if (option.equals("1")) {
+                return true; //계속 플레이
+            } else if (option.equals("2")) {
+                return false; //플레이 중지
             }
-            System.out.println("Input Correct option");
+            printStream.println("Input Correct option");
         }
     }
-    public String getInput(){
+
+    public String getInput() {
         String input;
         Scanner scan = new Scanner(System.in);
-        while (true){
-            System.out.println("Input the Number: ");
+
+        while (true) {
+
+            //유효한 입력을 받을때까지 실행
+            int input_number; //입력받은 숫자
+
+            printStream.println("Input the Number: ");
             input = scan.nextLine();
-            int input_number;
+
             try {
                 input_number = Integer.parseInt(input);
-            }
-            catch (Exception e){
-                System.out.println("Input only Number");
+            } catch (Exception e) {
+
+                //문자열에 숫자가 아닌게 있을때
+                printStream.println("Input only Number");
                 continue;
             }
             input = Integer.toString(input_number);
-            if(input.length() != 3){
-                System.out.println("Please input 3-length number");
+
+            if (input.length() != stringMaxLength) {
+
+                //문자열 길이가 3자리가 안될때
+                printStream.println("Please input 3-length number");
                 continue;
-            }
-            if(input.charAt(0) == input.charAt(1) ||input.charAt(0) == input.charAt(2) ||input.charAt(1) == input.charAt(2) ){
-                System.out.println("Please do not input duplicated number");
+            } else if ((input.charAt(0) == input.charAt(1)) || (input.charAt(0) == input.charAt(2))
+                    || (input.charAt(1) == input.charAt(2))) {
+
+                //중복된 숫자가 들어올때
+                printStream.println("Please do not input duplicated number");
                 continue;
             }
             break;
         }
         return input;
     }
-    public int getStrike(String input){
+
+    public int getStrike(String input) {
+
+        //스트라이크수 계산
         int strike = 0;
-        for(int i = 0; i < 3; i++){
+
+        for (int i = 0; i < stringMaxLength; i++){
+
             if(input.charAt(i) == answer.charAt(i)){
                 strike++;
             }
         }
         return strike;
     }
-    public int getBall(String input){
+
+    public int getBall(String input) {
+
+        //볼 수 계산
         int ball = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(i != j && input.charAt(i) == answer.charAt(j)){
+
+        for (int i = 0; i < stringMaxLength; i++) {
+
+            for (int j = 0; j < stringMaxLength; j++) {
+
+                if (i != j && input.charAt(i) == answer.charAt(j)) {
                     ball++;
                     break;
                 }
             }
         }
         return ball;
+    }
+
+    public String makeNumber() {
+
+        //한자리씩 랜덤한 숫자를 뽑는다
+        Random rand = new Random();
+        int temp;
+
+        while (true) {
+            temp = rand.nextInt(8) + 1;
+            String currentNumber = Integer.toString(temp);
+
+            if (!answer.contains(currentNumber)) {
+                return currentNumber;
+            }
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
+/**
+* Main class
+*
+* v1.0
+*
+* 2019-11-29
+*
+* MIT Licence
+*/
 public class Main {
+
+    /* 코드 진입점 */
     public static void main(String[] args) {
-        while(true){
+        while (true) {
+
+            /*무한히 진행한다*/
             Baseball b_ball = new Baseball();
             if(!b_ball.doBaseball()){
+
+                /*만약 플레이어가 종료하겠다면*/
                 break;
             }
         }
