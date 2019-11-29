29543171ac38901bb618ff8b7510b1e34e407ef0
ordinCode <ysh1346@naver.com>
2019-11-29 15:31:08 +0900

    gameMc.setUserNum()
    +gameMc.getUserNum()

--- src/main/java/baseballGame.java
+++ src/main/java/baseballGame.java
@@ -1,6 +1,7 @@
 public class baseballGame {
     public static void main(String[] args) {
         gameMc game = new gameMc();
-        game.getComNum();
+
+        game.setUserNum();
     }
 }
--- src/main/java/gameMc.java
+++ src/main/java/gameMc.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class gameMc {
     private ArrayList comNum;
-
+    private ArrayList userNum;
+    private Scanner inputNum;
+    private int round=1;
 
     public gameMc(){
         setComNum();
@@ -21,5 +24,27 @@ public class gameMc {
     }
     public void getComNum(){
         System.out.println("comNum : "+this.comNum);
+    }
+
+    public void setUserNum(){
+        try{
+            System.out.println("-------"+round+"round--------");
+            this.userNum = new ArrayList();
+            System.out.println("서로다른 3자리 숫자를 입력해주세요 : ");
+            inputNum = new Scanner(System.in);
+            String temp = inputNum.next();
+            String[] arrayTemp = temp.split("");
+            for(int i=0;i<3;i++){
+                this.userNum.add(Integer.valueOf(arrayTemp[i]));
+            }
+            round+=1;
+        }catch(Exception e){
+            System.out.println("3자리 숫자만 입력 해주세요");
+            setUserNum();
+        }
+
+    }
+    public void getUserNum(){
+        System.out.println("userNum : "+this.userNum);
     }
 }
