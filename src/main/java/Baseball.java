a283ffba9a059a3c357df67d739c4154915e58e5
Kwon Taehyung <ktf1008@naver.com>
2019-11-29 19:50:19 +0900

    숫자입력 구현

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import  java.util.Scanner;
 public class Baseball {
     private String answer;
     Baseball(){
@@ -16,7 +17,35 @@ public class Baseball {
         }
     }
     public boolean doBaseball(){
-        System.out.println(answer);
+        String input = getInput();
+        System.out.println(input);
         return true;
+    }
+    public String getInput(){
+        String input;
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            System.out.println("Input the Number: ");
+            input = scan.nextLine();
+            int input_number;
+            try {
+                input_number = Integer.parseInt(input);
+            }
+            catch (Exception e){
+                System.out.println("Input only Number");
+                continue;
+            }
+            input = Integer.toString(input_number);
+            if(input.length() != 3){
+                System.out.println("Please input 3-length number");
+                continue;
+            }
+            if(input.charAt(0) == input.charAt(1) ||input.charAt(0) == input.charAt(2) ||input.charAt(1) == input.charAt(2) ){
+                System.out.println("Please do not input duplicated number");
+                continue;
+            }
+            break;
+        }
+        return input;
     }
 }
