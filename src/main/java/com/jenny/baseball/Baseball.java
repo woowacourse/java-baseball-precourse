c8e617362f9cf7f078aeeda70cc693dfb7ba0937
jenny <jennylee4517@gmail.com>
2019-11-29 12:15:26 +0900

    createNumber 메서드 구현

--- src/main/java/com/jenny/baseball/Baseball.java
+++ src/main/java/com/jenny/baseball/Baseball.java
@@ -2,9 +2,32 @@ package com.jenny.baseball;
 
 public class Baseball implements Gamable {
 
+    /*
+     * 임의로 중복이 없는 1~9 숫자로 세자리 수를 만든다.
+     */
     @Override
     public int[] createNumber() {
-        return new int[0];
+
+        int[] num = new int[3];
+
+        makeDigits:
+        for(int i = 0 ; i < 3 ; i++){
+            while(true){
+
+                num[i] = (int)(Math.random()*10)/10;
+                if(num[i] == 0){
+                    continue makeDigits;
+                }
+                for(int prev = 0 ; prev < i ; prev++ ){
+                    if(num[prev] == num[i]){
+                        continue makeDigits;
+                    }
+                }
+                break;
+            }
+        }
+
+        return num;
     }
 
     @Override
