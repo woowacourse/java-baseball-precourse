508b56b13f3406dc413e841c2518b88322d2b9c7
SMJin <sstaw@naver.com>
2019-11-29 22:54:02 +0900

    Java Convention :: 숫자 상수를 변수로(숫자 자체 사용x)

--- baseball/src/Baseball.java
+++ baseball/src/Baseball.java
@@ -7,25 +7,29 @@ import java.util.Scanner;
  * */
 public class Baseball {
 
+    private static final int NUMBER_LENGTH = 3;         // 정답 숫자열, 사용자 입력 숫자열 모두 길이는 3개로 고정이다.
+    private static final int INIT_NUMBER = 0;           // 정답 숫자열의 초기화 값.
+
+    private static final int CONTINUE_GAME = 1;         // 사용자가 1을 입력하면, 게임 다시 시작.
+
+    private static final int RANDOM_MAX = 9;            // 컴퓨터가 숫자 초기화시, 랜덤으로 뽑을 숫자 중 가장 큰 값.
+    private static final int RAND0M_MIN = 1;            // 컴퓨터가 숫자 초기화시, 랜덤으로 뽑을 숫자 중 가장 작은 값.
+
     public static void main(String[] args) {
 
-        int answerNumA = 0;
-        int answerNumB = 0;
-        int answerNumC = 0;
+        int answerNum[] = new int[NUMBER_LENGTH];        // 정답 숫자들을 넣을 배열
+        int num[] = new int[NUMBER_LENGTH];             // 사용자가 입력한 숫자들을 넣을 배열
+        boolean finish = false;                         // 정답을 맞출 때까지 정답을 입력받을 수 있게 하는 while 문을 멈추게 할 boolean 형 변수
 
-        int answerNum[] = new int[3];        // 정답 숫자들을 넣을 배열
-        int num[] = new int[3];             // 사용자가 입력한 숫자들을 넣을 배열
-        boolean finish = false;             // 정답을 맞출 때까지 정답을 입력받을 수 있게 하는 while 문을 멈추게 할 boolean 형 변수
-
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         while (isOverlap(answerNum)) {      // 정답 숫자들이 서로 겹치지 않게 해주는 while 문. 겹치지 않을 때까지 돌린다.
-            answerNum = initBaseballNumber(answerNumA, answerNumB, answerNumC);
+            answerNum = initBaseballNumber(INIT_NUMBER, INIT_NUMBER, INIT_NUMBER);
         }
 
         while (!finish) {
             System.out.print("숫자를 입력해주세요 : ");
-            int UserNumber = scanner.nextInt();
+            int UserNumber = scan.nextInt();
             num[0] = UserNumber / 100;
             num[1] = (UserNumber % 100) / 10;
             num[2] = UserNumber % 10;
@@ -34,7 +38,7 @@ public class Baseball {
                     isBall(answerNum, num));
 
             if(finish) {
-                if (scanner.nextInt() == 1) {
+                if (scan.nextInt() == CONTINUE_GAME) {
                     finish = false;
                 }
             }
@@ -49,9 +53,9 @@ public class Baseball {
     static private int[] initBaseballNumber(int answerNumA,
                                             int answerNumB, int answerNumC) {
         Random random = new Random();
-        answerNumA = random.nextInt(9) + 1;
-        answerNumB = random.nextInt(9) + 1;
-        answerNumC = random.nextInt(9) + 1;
+        answerNumA = random.nextInt(RANDOM_MAX) + RAND0M_MIN;
+        answerNumB = random.nextInt(RANDOM_MAX) + RAND0M_MIN;
+        answerNumC = random.nextInt(RANDOM_MAX) + RAND0M_MIN;
 
         int answerNum[] = {answerNumA, answerNumB, answerNumC};
 
@@ -117,7 +121,7 @@ public class Baseball {
      */
     static private boolean printResult(int strikeNumber, int ballNumber) {
         boolean finish = false;
-        if (strikeNumber == 3) {
+        if (strikeNumber == NUMBER_LENGTH) {
             println("3개의 숫자를 모두 맞히셨습니다! 게임 종료" +
                     "\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             finish = true;
