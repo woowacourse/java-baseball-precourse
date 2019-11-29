24f910cd2159019be1392d6032b16f46dfddd024
lalize <printf@kakao.com>
2019-11-29 16:35:26 +0900

    feat: 게임 결과 내용을 만든다

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -16,4 +16,21 @@ public class Result {
     public boolean allMatch() {
         return strike == NUMBER_COUNT;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        if (strike > 0) {
+            output.append(strike);
+            output.append(" 스트라이크 ");
+        }
+        if (ball > 0) {
+            output.append(ball);
+            output.append("볼");
+        }
+        if (strike == 0 && ball == 0)  {
+            output.append("낫싱");
+        }
+        return output.toString().trim();
+    }
 }
