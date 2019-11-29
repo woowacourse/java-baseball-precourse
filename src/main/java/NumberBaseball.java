8de2c265dcb4a6a13245219c546bcdee679b7629
ikswary <ikswary@naver.com>
2019-11-29 16:23:28 +0900

    기능 4 작성

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -27,6 +27,47 @@ class Num{
 }
 
 public class NumberBaseball {
+    private static boolean rtnHint(Num answer, Num player){
+        int strike= 0;
+        int ball= 0;
+        String rtnMsg=""
+                ;
+        if(answer.position[0]==player.position[0]){
+            strike++;
+        }
+        if(answer.position[1]==player.position[1]){
+            strike++;
+        }
+        if(answer.position[2]==player.position[2]){
+            strike++;
+        }
+        if(answer.position[0]==player.position[1]||
+                answer.position[0]==player.position[2]){
+            ball++;
+        }
+        if(answer.position[1]==player.position[0]||
+                answer.position[1]==player.position[2]){
+            ball++;
+        }
+        if(answer.position[2]==player.position[0]||
+                answer.position[2]==player.position[1]){
+            ball++;
+        }
+
+        if(strike > 0){
+            rtnMsg=Integer.toString(strike)+" Strike ";
+        }
+        if(ball > 0){
+            rtnMsg+=Integer.toString(ball)+" Ball";
+        }
+        System.out.println(rtnMsg);
+        if (strike==3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
         Num answer = new Num();
