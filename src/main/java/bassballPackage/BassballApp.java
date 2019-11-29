0b1f7cd326a7f2fed0c5ebc7256945f7ef17a806
tiroring09 <tiroring09@gmail.com>
2019-11-29 17:22:27 +0900

    SBCount메서드 생성; 인덴트 뎁스제한 해결필요

--- src/main/java/bassballPackage/BassballApp.java
+++ src/main/java/bassballPackage/BassballApp.java
@@ -1,9 +1,26 @@
 package bassballPackage;
 
 public class BassballApp {
-
+	static int[] SBCount(int[] userArr, int[] cpuArr){
+		int[] result = {0, 0};
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(i == j && userArr[i] == cpuArr[j]) {
+					result[0] += 1;
+				}else if(userArr[i] == cpuArr[j]) {
+					result[1] += 1;
+				}
+			}
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("hi");
+		int[] user1 = {1,2,3};
+		int[] cpu1 = {1,2,3};
+		int[] result1 = SBCount(user1, cpu1);
+		System.out.println(result1[0]);
+		System.out.println(result1[1]);
 
 	}
 
