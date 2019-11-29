132ed8d4accc9978e7defdb6bdee67087e4d9997
Eun Seok <jeseok95@gmail.com>
2019-11-29 19:12:15 +0900

    Baseball 생성자 구현 ,README 수정(add 안하고 해서 recommit)

--- src/main/java/GameMake/Baseball.java
+++ src/main/java/GameMake/Baseball.java
@@ -1,14 +1,19 @@
 package GameMake;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Baseball {
     private static int[] ans = new int[3];
+    private static int[] input = new int[3];
 
-    public static void makeAns(){
+    public Baseball(){
         Random rand = new Random();
         for(int i=0;i<ans.length;i++) {
             ans[i] = rand.nextInt(9)+1; // 1 ~ 9 난수생성
         }
+//        for(int i=0;i<ans.length;i++) {
+//            System.out.print(ans[i]);
+//        }
     }
 }
--- src/main/java/baseballgame.java
+++ src/main/java/baseballgame.java
@@ -3,5 +3,6 @@ import GameMake.Baseball;
 public class baseballgame {
     public static void main(String[] args) {
         Baseball game = new Baseball();
+
     }
 }
