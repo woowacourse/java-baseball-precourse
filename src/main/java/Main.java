e5ac63408e966ae6653e0901414d6f23f1fc5ed9
RoenissMoon <roeniss2@gmail.com>
2019-11-29 21:21:29 +0900

    요구사항 2 (플레이어가 세 자리 수를 입력함)

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Computer computer = new Computer();
+    private static Scanner sc;
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+        boolean on_game = true; // 게임 추가 진행 플래그
+        while (on_game){
+            play();
+            // 탈출 플래그 세워야 함
+        }
+        sc.close();
+    }
+
+    private static void play() {
+        computer.make_answer();
+        int suggested_answer;
+        boolean success = false;
+        while (!success) {
+            System.out.print("숫자를 입력해주세요 :\n");
+            suggested_answer = sc.nextInt();
+            success = computer.compare_answer(suggested_answer);
+        }
+    }
+
+}
