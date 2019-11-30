5e1ac7aa95b15ad6382142675df6fca3f54fc99d
Jinhong Kim <deepred.kim@gmail.com>
2019-11-30 16:13:28 +0900

    Baseball 클래스 구현중

---
+++ src/main/java/com/woowahan/baseball/Baseball.java
@@ -0,0 +1,45 @@
+package com.woowahan.baseball;
+
+import java.util.ArrayList;
+
+public class Baseball extends AbstractBaseball{
+    @Override
+    protected Integer getRandomInteger(int min, int max) {
+        return null;
+    }
+
+    @Override
+    protected String getLine() {
+        return null;
+    }
+
+    @Override
+    protected ArrayList<Integer> initializeBallRange() {
+        return null;
+    }
+
+    @Override
+    protected Integer getRandomBall(ArrayList<Integer> range) {
+        return null;
+    }
+
+    @Override
+    protected ArrayList<Integer> getRandomBalls() {
+        return null;
+    }
+
+    @Override
+    protected EarlyResults.EarlyResult evaluateBall(Integer ball, ArrayList<Integer> balls) {
+        return null;
+    }
+
+    @Override
+    protected EarlyResults evaluateBalls(ArrayList<Integer> user, ArrayList<Integer> answer) {
+        return null;
+    }
+
+    @Override
+    public void Process() {
+
+    }
+}
