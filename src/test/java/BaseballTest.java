d182068687c16507ccfcd493d9ef97f144586fdc
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-30 14:19:30 +0900

    docs: Add functions in readme.md

---
+++ src/test/java/BaseballTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BaseballTest {
+
+    @Test
+    public void helloWorld(){
+        String hello = "Hello";
+        assertEquals(hello,"Hello");
+    }
+}
