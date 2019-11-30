4b89aa2db4fd3b7533106afda381cf7adbca9968
Jinhong Kim <deepred.kim@gmail.com>
2019-11-30 15:58:28 +0900

    EarlyResults 클래스 생성
    숫자야구 중간결과를 관리하는 클래스

---
+++ src/main/java/com/woowahan/baseball/EarlyResults.java
@@ -0,0 +1,60 @@
+/*
+ * EarlyResults
+ * 2019.11.30
+ * 숫자야구 중간결과
+ */
+
+package com.woowahan.baseball;
+
+public class EarlyResults {
+    private int ballScore;
+    private int strikeScore;
+
+    public enum EarlyResult {
+        NONE, BALL, STRIKE
+    };
+
+    private boolean isCorrect(){
+        return strikeScore == 3;
+    }
+
+    private boolean isNothing(){
+        return (strikeScore == 0) && (ballScore == 0);
+    }
+
+    private void printScore(){
+        if (strikeScore != 0) {
+            System.out.print(strikeScore + " 스트라이크 ");
+        }
+        if (ballScore != 0) {
+            System.out.print(ballScore + "볼");
+        }
+        System.out.println();
+    }
+
+    public void addEarlyResult(EarlyResult one){
+        if (one == EarlyResult.BALL){
+            ballScore++;
+        }
+        if (one == EarlyResult.STRIKE){
+            strikeScore++;
+        }
+    }
+
+    public boolean printResults(){
+        if (isCorrect()){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        if (isNothing()){
+            System.out.println("낫싱");
+        }
+        else {
+            printScore();
+        }
+
+        return false;
+    }
+
+}
