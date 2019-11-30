550eaeaa195f792f56ce94f5f6206b65584108cd
dkqyqyt <57524159+dkqyqyt@users.noreply.github.com>
2019-11-30 19:03:06 +0900

    숫자 입력받는 기능(리팩토링)

--- src/main/java/Baseball/InputNumber.java
+++ src/main/java/Baseball/InputNumber.java
@@ -4,7 +4,38 @@ import java.util.Scanner;
 
 public class InputNumber {
     private static int inputNumber;
+    private static Scanner sc = new Scanner(System.in);
+    private static boolean[] usedNumber;
 
+    public InputNumber(){}
 
+    public void input(){
+        System.out.println("숫자를 입력해주세요 : ");
+        inputNumber = sc.nextInt();
+        if(!inputCheck(inputNumber)){
+            input();
+        }
+    }
+
+    public boolean inputCheck(int inputNumber){
+        int temp = inputNumber;
+        if((temp < 100) || (temp > 999)){
+            return false;
+        }
+
+        while(temp != 0){
+            int num = temp%10;
+            if(usedNumber[num]) {
+                return false;
+            }
+            usedNumber[num] = true;
+            temp = temp/10;
+        }
+        return true;
+    }
+
+    public int getInputNumber(){
+        return inputNumber;
+    }
 
 }
