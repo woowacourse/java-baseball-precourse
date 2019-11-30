9710b6436849fda6ddfe90a50d610378475d9ba3
eatnug <wlq4568@gmail.com>
2019-11-30 19:35:09 +0900

    feat: Add 2-1. 사용자 입력받기
    - 게임 시작하는 메소드 start 추가
        - while 반복문을 돌면서 proceedTurn 메소드 실행
        - 추후 재시작 확인 함수를 활용해서 while의 조건문 부분을 수정할 것
    - 턴 진행하는 메소드 proceedTurn 추가
        - 내부에서 사용자 입력받고, 입력을 평가하고, 결과를 출력해줄 예정
        - 사용자 입력을 받는 getUserGuess 메소드까지만 작성
        - 잘못된 입력이 들어올 경우 에러를 발생시키는데 여기서 핸들링해줌
    - 사용자 입력 받는 메소드 getUserGuess 추가
        - 안내 텍스트 출력, 입력 받기, 입력값 확인, 입력값 반환 순서로 진행
        - 별도의 메소드로 입력값의 오류를 잡는다.
    - 입력값 오류 찾는 메소드 validateGuess 추가
        - 입력의 길이가 valid 한지, 입력이 숫자인지 확인하는 함수
        - 각각을 확인하는 메소드를 작성해서 사용하고 각각 에러(InputLength, InputNaN)를 발생시킨다
    - 에러핸들링하는 메소드 hanndleError 작성
        - 에러 타입마다 적당한 에러메시지 출력하는 메소드.
    
    - 위 사항들을 구현하기 위해 필요한 멤버 변수들 추가
        - 에러 클래스 InputLengthException
        - 에러 클래스 InputNaNException
        - 정답 문자열의 길이 LENGTH
    
    - Main 클래스 main 메소드에 game.start() 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 class Game{
 
+    private static class InputLengthException extends RuntimeException {};
+    private static class InputNaNException extends RuntimeException {};
     private static final String[] DIGITS = {"0","1","2","3","4","5","6","7","8","9"};
+    private static final int LENGTH = 3;
     private static Scanner scanner;
     private static Random random;
     private static String target;
@@ -12,6 +15,56 @@ class Game{
         scanner = new Scanner(System.in);
         random = new Random();
         init();
+    }
+
+
+    public void start(){
+        while(true){
+            proceedTurn();
+        }
+    }
+
+    private void proceedTurn(){
+        try{
+            String guess = getUserGuess();
+            // evaluate guess
+            // pring result
+        }
+        catch( RuntimeException e ){
+            hanndleError(e);
+        }
+    }
+
+    private String getUserGuess() throws RuntimeException {
+        System.out.print("숫자를 입력해주세요: ");
+        String input = scanner.nextLine();
+        validateGuess(input);
+        return input;
+    }
+
+    private void validateGuess(String guess){
+        if(notFit(guess)) throw new InputLengthException();
+        else if(NaN(guess)) throw new InputNaNException();
+    }
+
+    private Boolean notFit(String string){
+        if (string.length() != LENGTH) return true;
+        return false;
+    }
+
+    private Boolean NaN(String string){
+        try {
+            Integer.parseInt(string);
+            return false;
+        } catch(NumberFormatException e){
+            return true;
+        }
+    }
+
+    private void hanndleError(RuntimeException e){
+        if (e instanceof InputLengthException) System.err.println(String.format("입력의 길이가 %d이 아닙니다", LENGTH));
+        else if (e instanceof InputNaNException) System.err.println("입력이 숫자가 아닙니다.");
+        else System.err.println("예상치 못한 오류입니다.");
     }
 
     private void init(){
@@ -54,5 +107,6 @@ class Game{
 public class Main {
     public static void main(String[] args){
         Game game = new Game();
+        game.start();
     }
 }
