bde530f28820b6990d821a766038324800b8b5e8
eatnug <wlq4568@gmail.com>
2019-11-30 17:50:25 +0900

    feat: Add 1.난수 생성하기
    우선 기능 구현에 앞서 Game 클래스를 작성했다.클래스는 0~9 까지의 정수 배열인 DIGITS, Scanner, Random, String 객체를 각각  담기위한 변수 scanner, random, target을 가지고 있다.
    생성자가 호출되면 Scanner와 Random 객체를 생성해서 할당하고, init 메소드를 실행한다. init은 라운드 시작마다 호출된다. 서로 다른 3 정수로 이루어진 문자열을 target에 할당한다.

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Game{
+
+    private static final String[] DIGITS = {"0","1","2","3","4","5","6","7","8","9"};
+    private static Scanner scanner;
+    private static Random random;
+    private static String target;
+
+
+    public Game(){
+        scanner = new Scanner(System.in);
+        random = new Random();
+        init();
+    }
+
+    private void init(){
+        target = createAnswerString();
+    }
+
+    private String createAnswerString(){
+        String first = pickDigitExcept();
+        String second = pickDigitExcept(first);
+        String third = pickDigitExcept(first, second);
+        //pick 3 exclusive random numbers characters
+        return first + second + third;
+    }
+
+
+    private String pickDigitExcept(String ... except){
+
+        String[] pool;
+
+        if(except == null){
+            pool = newDigitPoolExcept(except);
+        }
+        else{
+            pool = DIGITS;
+        }
+
+        String pick = pool[random.nextInt(pool.length)];
+        return pick;
+    }
+
+
+    private String[] newDigitPoolExcept(String[] except){
+        List<String> newPool = new ArrayList<String>();
+        for(String digit: DIGITS){
+            if(!Arrays.asList(except).contains(digit)){
+                newPool.add(digit);
+            }
+        }
+        return newPool.toArray(new String[newPool.size()]);
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args){
+        Game game = new Game();
+    }
+}
