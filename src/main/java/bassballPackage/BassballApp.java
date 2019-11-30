854c3c1c08582198fa796d94b937214a088fd1eb
tiroring09 <tiroring09@gmail.com>
2019-11-30 20:27:35 +0900

    SBCount, SCountIf, BCountIf 메서드 수정: userInput쪽에서 출력하는 배열길이의 변경에 따른 인덱스 및 비교 값 보정

--- src/main/java/bassballPackage/BassballApp.java
+++ src/main/java/bassballPackage/BassballApp.java
@@ -3,7 +3,7 @@ package bassballPackage;
 public class BassballApp {
 	static int[] SBCount(int[] userArr, int[] cpuArr){
 		int[] result = {0, 0};
-		for(int i = 0; i < 3; i++) {
+		for(int i = 1; i < 4; i++) {
 			for(int j = 0; j < 3; j++) {
 				result[0] += SCountIf(userArr, cpuArr, i, j);
 				result[1] += BCountIf(userArr, cpuArr, i, j);
@@ -13,7 +13,7 @@ public class BassballApp {
 	}
 	
 	static int SCountIf(int[] userArr, int[] cpuArr, int i, int j) {
-		if(i == j && userArr[i] == cpuArr[j]) {
+		if(i-1 == j && userArr[i] == cpuArr[j]) {
 			return 1;
 		}else {
 			return 0;
@@ -21,7 +21,7 @@ public class BassballApp {
 	}
 	
 	static int BCountIf(int[] userArr, int[] cpuArr, int i, int j) {
-		if(i != j && userArr[i] == cpuArr[j]) {
+		if(i-1 != j && userArr[i] == cpuArr[j]) {
 			return 1;
 		}else {
 			return 0;
@@ -29,7 +29,7 @@ public class BassballApp {
 	}
 	
 	public static void main(String[] args) {
-		int[] user1 = {1,2,3};
+		int[] user1 = {123,1,2,3};
 		int[] cpu1 = {1,2,3};
 		int[] result1 = SBCount(user1, cpu1);
 		System.out.println(result1[0]);
