dfa653222b837ed939c1469b1a28e2f4566b76cb
chohanjoo <johanjoo@naver.com>
2019-11-30 13:57:36 +0900

    Add compare_number Method in Computer.java

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -19,6 +19,30 @@ public class Computer {
                 --i;
         }
     }
+    
+    public int[] compare_number(int[] user_numbers) {
+        int strike = 0;
+        int boll = 0;
+        int[] temp_computer_numbers = new int[3];
+        System.arraycopy(numbers, 0, temp_computer_numbers, 0, numbers.length);
+
+        for (int i = 0; i < user_numbers.length; ++i) {
+            int check = check_strike(user_numbers[i], i);
+            if (check == 1) {
+                temp_computer_numbers[i] = 0;
+                user_numbers[i] = 0;
+            }
+            strike += check;
+
+        }
+        for (int i = 0; i < user_numbers.length; ++i) {
+            if (user_numbers[i] != 0) {
+                boll += check_boll(user_numbers[i], temp_computer_numbers);
+            }
+        }
+        set_result(strike, boll);
+        return result;
+    }
 
     private int check_strike(int user_number, int index) {
         int check = 0;
