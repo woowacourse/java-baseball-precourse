9feaad60eb5a31de2804aca254c7df0059a5efca
tiroring09 <tiroring09@gmail.com>
2019-11-30 17:04:19 +0900

    cpuInput 메서드 생성: 컴퓨터 1~9사이 서로다른 3자리 정수 배열 출력.

--- src/main/java/bassballPackage/CpuInput.java
+++ src/main/java/bassballPackage/CpuInput.java
@@ -5,10 +5,34 @@ public class CpuInput {
 		return (int)(Math.random() * 9) + 1;
 	}
 	
+	static int[] cpuInput() {
+		int[] result = new int[3];
+		result[0] = randomGeneration();
+		
+		int tmpVal1 = randomGeneration();
+		while(tmpVal1 == result[0]) {
+			tmpVal1 = randomGeneration();
+		}
+		result[1] = tmpVal1;
+		
+		int tmpVal2 = randomGeneration();
+		while(tmpVal2 == result[0] || tmpVal2 == result[1] ) {
+			tmpVal2 = randomGeneration();
+		}
+		result[2] = tmpVal2;
+		
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		//randomGenereation test
-		System.out.println(randomGeneration());
+		//System.out.println(randomGeneration());
 
+		//cpuInput test
+		int[] testArray = cpuInput();
+		System.out.println(testArray[0]);
+		System.out.println(testArray[1]);
+		System.out.println(testArray[2]);
 	}
 
 }
