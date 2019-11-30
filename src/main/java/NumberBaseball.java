5e8f7babe80d6f2d7cbba05bb53a62af7c55e97d
hwangbo <taekwonod714@naver.com>
2019-12-01 00:09:02 +0900

    feat(...) : Compare between Answer and Input TODO : Game Loop, Divide Class by Functions

--- src/main/java/NumberBaseball.java
+++ src/main/java/NumberBaseball.java
@@ -5,7 +5,7 @@
  * 
  * November, 28th, 2019
  * 
- * Copyright(c)
+ * Copyright(c) Hwangbo Kyeong 2019
  */
 
 import java.util.*;
@@ -13,11 +13,74 @@ import java.util.*;
 public class NumberBaseball {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int question = scanner.nextInt();
 		ArrayList<Integer> answer = randomNumber(); 
+		boolean isAnswer = false;
 
-		scanner.close();
+		while (true) {
+			ArrayList<Integer> question = inputSlice();
+			isAnswer = checkAnswer(answer, question);
+			if(isAnswer == true) {				
+				break;
+			} else {}
+		}
+	}
+
+	public static ArrayList<Integer> inputSlice() {
+		ArrayList<Integer> question = new ArrayList<Integer>();
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력해주세요 : ");
+		int nums = scanner.nextInt();
+
+		if(nums < 100 || nums > 999) {
+			System.out.println("잘못된 입력입니다.");
+			inputSlice();
+		} else {
+			question.add(nums/100);
+			nums -= (nums/100) * 100;
+			question.add(nums/10);
+			nums -= (nums/10) * 10;
+			question.add(nums);
+		}
+
+		return question;
+	}
+
+	public static boolean checkAnswer(ArrayList<Integer> answer, ArrayList<Integer> input) {
+		int strike = 0;
+		int ball = 0;
+
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(answer.get(i) == input.get(j)) {
+					if(i == j) {
+						strike += 1;
+					} else {
+						ball += 1;
+					}
+				}
+			}
+		}
+		if(strike == 0 && ball == 0) {
+			System.out.println("낫싱");
+			return false;
+		}
+		else if(strike != 0 && ball == 0) {
+			System.out.printf("%d 스트라이크\n", strike);
+		}
+		else if(strike == 0 && ball != 0) {
+			System.out.printf("%d 볼\n", ball);
+			return false;
+		}
+		else {
+			System.out.printf("%d 스트라이크 %d 볼\n", strike, ball);
+			return false;
+		}
+		if(strike == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+			return true;
+		} else
+			return false;
 	}
 	public static boolean check(ArrayList<Integer> one, int two) {
 		for(int i = 0; i < one.size(); i++) {
