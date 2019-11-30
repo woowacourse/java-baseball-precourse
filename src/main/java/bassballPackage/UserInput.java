43eddd7caf3d894c97ec0f56f63a675a98deb30e
tiroring09 <tiroring09@gmail.com>
2019-11-30 15:00:31 +0900

    userInput 메서드 출력되는 배열에 원본3자리 숫자를 추가함

--- src/main/java/bassballPackage/UserInput.java
+++ src/main/java/bassballPackage/UserInput.java
@@ -5,16 +5,17 @@ import java.util.Scanner; //사용자 입력위해 필요.
 
 public class UserInput {
 	static int[] userInput(){
-		int[] result = new int[3];
+		int[] result = new int[4];
 		Scanner input = new Scanner(System.in);
 		String introTxt = "사용자 입력: 1 ~ 9 사이의 서로다른 정수로 이루어진 세자리 수를 입력하세요 :";
 		System.out.println(introTxt);
 		
 		int initial = input.nextInt();		//사용자입력
 		
-		result[0] = initial / 100;
-		result[1] = (initial % 100) / 10;
-		result[2] = initial % 10;
+		result[0] = initial;
+		result[1] = initial / 100;
+		result[2] = (initial % 100) / 10;
+		result[3] = initial % 10;
 		System.out.println("사용자 입력: " + initial + "을 입력하였습니다.");
 //		input.close();
 		return result;
@@ -34,15 +35,15 @@ public class UserInput {
 	public static void main(String[] args) {
 		//userInput메서드 테스트
 //		int[] result1 = userInput();
-//		System.out.println("결괏값 백의자리 수:" + result1[0]);
-//		System.out.println("결괏값 십의자리 수:" + result1[1]);
-//		System.out.println("결괏값 일의자리 수:" + result1[2]);
+//		System.out.println("결괏값 백의자리 수:" + result1[1]);
+//		System.out.println("결괏값 십의자리 수:" + result1[2]);
+//		System.out.println("결괏값 일의자리 수:" + result1[3]);
 		
 		//userInputValid메서드 테스트
 		int[] result1 = userInputValid();
-		System.out.println("결괏값 백의자리 수:" + result1[0]);
-		System.out.println("결괏값 십의자리 수:" + result1[1]);
-		System.out.println("결괏값 일의자리 수:" + result1[2]);
+		System.out.println("결괏값 백의자리 수:" + result1[1]);
+		System.out.println("결괏값 십의자리 수:" + result1[2]);
+		System.out.println("결괏값 일의자리 수:" + result1[3]);
 	}
 }
 
