d9b7c95946f139c741982f1046e558e19525df31
BEOMJUN KIM <skyblue300a@gmail.com>
2019-11-30 15:46:50 +0900

    '추측 결과 출력' 및 '게임 종료' 기능 추가
    1. 추측 결과 계산 및 출력 기능을 하고 게임 종료 기능을 하는 checkAnswerNumber 메소드 구현

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * Computer class - 컴퓨터의 기능을 정의
  *
- * @version         1.00 2019-11-30
+ * @version         1.01 2019-11-30
  * @author          김범준(ddaaac)
  */
 public class Computer {
@@ -29,5 +29,46 @@ public class Computer {
             answerNumber.add(candidate.get(randomIndex));                       // answerNumber 변수에 추가
             candidate.remove(randomIndex);                                      // 후보 숫자 제거
         }
+    }
+
+    /**
+     * 3자리 숫자를 입력받아 답인지 확인하고 결과를 출력하는 method
+     * @param inputNumber 입력받은 3자리 숫자
+     * @return Boolean 정답 여부
+     */
+    public Boolean checkAnswerNumber(int inputNumber) {
+        int strike = 0;             // strike 갯수
+        int ball = 0;               // ball 갯수
+
+        /*
+         * 1의 자리부터 한 자리씩 확인하며 같은 수가 같은 자리에 있으면 strike,
+         * 다른 자리에 있으면 ball 갯수를 1씩 증가
+         */
+        for (int i = 2; i >= 0; i--){
+            int place = inputNumber % 10;
+            if (answerNumber.get(i) == place) {
+                strike++;
+            } else if (answerNumber.contains(place)) {
+                ball++;
+            }
+            inputNumber /= 10;
+        }
+
+        /* 같은 수가 없으면 Nothing, 있다면 strike와 ball 갯수를 출력 */
+        if (strike == 0 && ball == 0) {
+            System.out.print("Nothing");
+        }
+        if (strike != 0) {
+            System.out.print(strike + " 스트라이크 ");
+        }
+        if (ball != 0) {
+            System.out.print(ball + "볼");
+        }
+        System.out.println();
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+
+        return (strike == 3);           // strike가 3개면 정답
     }
 }
