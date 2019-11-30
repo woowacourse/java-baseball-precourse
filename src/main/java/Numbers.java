f3eefd646719db505dfafe83588673e8042fdedf
MOON JINJU <mjj920930@gmail.com>
2019-11-30 20:52:00 +0900

    Numbers클래스를 구성하는 3자리의 숫자를 HashMap을 활용하여 저장하도록 변경

--- src/main/java/Numbers.java
+++ src/main/java/Numbers.java
@@ -1,131 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Numbers {
     private static Random random = new Random();
+    private static final int SIZE_OF_NUMBERS = 3;
+    private static final int MAX_NUMBER = 9;
 
-    private Number firstNumber;
-    private Number secondNumber;
-    private Number thirdNumber;
+    private HashMap<Integer, Integer> numbers = new HashMap<>();
 
     public Numbers() {
-        makeFirstNumber();
-        makeSecondNumber();
-        makeThirdNumber();
+        while (numbers.size() < SIZE_OF_NUMBERS) {
+            int index = numbers.size() + 1;
+            numbers.put(makeRandomNumber(), index);
+        }
     }
 
     public Numbers(int firstValue, int secondValue, int thirdValue) {
-        this.firstNumber = new Number(1, firstValue);
-        this.secondNumber = new Number(2, secondValue);
-        this.thirdNumber = new Number(3, thirdValue);
-    }
-
-    private void makeFirstNumber() {
-        firstNumber = new Number(1, makeRandomNumber());
-    }
-
-    private void makeSecondNumber() {
-        do {
-            secondNumber = new Number(2, makeRandomNumber());
-        } while (!secondNumber.isSameValue(firstNumber));
-    }
-
-    private void makeThirdNumber() {
-        do {
-            thirdNumber = new Number(3, makeRandomNumber());
-        } while ((!thirdNumber.isSameValue(firstNumber)) && (!thirdNumber.isSameValue(secondNumber)));
+        numbers.put(firstValue, 1);
+        numbers.put(secondValue, 2);
+        numbers.put(thirdValue, 3);
     }
 
     private int makeRandomNumber() {
-        return random.nextInt(9) + 1;
+        return random.nextInt(MAX_NUMBER) + 1;
     }
 
     public boolean hasNumber(int number) {
-        if ((firstNumber.isSameValue(number)) || (secondNumber.isSameValue(number)) || (thirdNumber.isSameValue(number))) {
-            return true;
-        }
-        return false;
+        return numbers.containsKey(number);
     }
 
     public boolean hasDuplicatedNumber() {
+        // HashMap 활용 후 불필요
         return true;
     }
 
-    public ResultOfComparison compareWith(Numbers numbersFromUser) {
-        // 비교하고 결과 출력하기
-        // 비교값을 담는 객체 별도 생성
+    public void compareWith(Numbers numbersFromUser) {
 
-        int strike = countStrike(numbersFromUser);
-        int ball = countBall(numbersFromUser);
-
-        if ((strike == 0) && (ball == 0)) {
-            System.out.println("낫싱");
-            return null;
-        }
-
-        System.out.println(strike + " 스트라이크 " + ball + " 볼");
-
-        if (strike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-        return null;
     }
-
-    public int countStrike(Numbers numbers) {
-        int strike = 0;
-        if (this.firstNumber == numbers.firstNumber) {
-            strike++;
-        }
-        if (this.secondNumber == numbers.secondNumber) {
-            strike++;
-        }
-        if (this.thirdNumber == numbers.thirdNumber) {
-            strike++;
-        }
-        return strike;
-    }
-
-    public int countBall(Numbers numbers) {
-        int ball = 0;
-        if ((this.firstNumber == numbers.secondNumber) || (this.firstNumber == numbers.thirdNumber)) {
-            ball++;
-        }
-        if ((this.secondNumber == numbers.firstNumber) || (this.secondNumber == numbers.thirdNumber)) {
-            ball++;
-        }
-        if ((this.thirdNumber == numbers.firstNumber) || (this.thirdNumber == numbers.secondNumber)) {
-            ball++;
-        }
-        return ball;
-    }
-}
-
-class Number {
-
-    private int index;
-    private int value;
-
-    Number(int index, int value) {
-        this.index = index;
-        this.value = value;
-    }
-
-    public boolean isSameIndex(int index) {
-        return this.index == index;
-    }
-
-    public boolean isSameIndex(Number number) {
-        return this.index == number.index;
-    }
-
-    public boolean isSameValue(int value) {
-        return this.value == value;
-    }
-
-    public boolean isSameValue(Number number) {
-        return this.value == number.value;
-    }
-
-
 }
--- src/main/java/ResultOfComparison.java
+++
@@ -1,6 +0,0 @@
-public class ResultOfComparison {
-
-    ResultOfComparison(Numbers numbersFromUser) {
-
-    }
-}
