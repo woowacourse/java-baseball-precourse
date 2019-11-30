16addffc3f3a280f898a816399c55640669358ca
KIMSIYOUNG <tldud2404@gmail.com>
2019-11-30 14:44:40 +0900

    feat: 1-9까지 서로 다른 난수 생성

---
+++ src/main/java/domain/Random.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Random {
+    private static final String DEFALUT_STRING_BLANK = "";
+    private static final int DIGIT_NUMBER = 3;
+    private static final int RANGE_OF_NUMBER = 9;
+    private static final int START_NUMBER = 1;
+
+    public int makeNumber(){
+        Set<Integer> setForRandomNumber = new HashSet<>();
+        String randomNumber = DEFALUT_STRING_BLANK;
+
+        while(setForRandomNumber.size()<DIGIT_NUMBER){
+            setForRandomNumber.add(
+                    (int)Math.random()*RANGE_OF_NUMBER + START_NUMBER
+            );
+        }
+        for(int s : setForRandomNumber)
+            randomNumber += String.valueOf(s);
+        return Integer.parseInt(randomNumber);
+    }
+
+}
