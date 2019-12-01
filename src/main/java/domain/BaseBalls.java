1c935a89c98fa1ab326df308e434a6880b90f894
mjung1798 <mor2222@naver.com>
2019-12-02 00:48:31 +0900

    2 : domain.BaseBalls.class 테스트코드 작성

--- src/main/java/domain/BaseBalls.java
+++ src/main/java/domain/BaseBalls.java
@@ -3,14 +3,28 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Getter;
+
+@Getter
 public class BaseBalls {
 	private List<OneBaseBall> baseBalls = new ArrayList<>();
 
-	public BaseBalls(List<Integer> numbers) {
+	private BaseBalls(List<Integer> numbers) {
 		for (int position = 0; position < numbers.size(); position++) {
 			Integer number = numbers.get(position);
 			this.baseBalls.add(OneBaseBall.ofInteger(number, position));
 		}
+	}
+
+	public static BaseBalls of(List<Integer> numbers) {
+		return new BaseBalls(numbers);
+	}
+
+	public OneBaseBall findBaseBallByPosition(Integer position) {
+		return this.baseBalls.stream()
+			.filter(ball -> ball.getBaseBallPosition().equals(position))
+			.findFirst()
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않은 position 입니다."));
 	}
 
 }
---
+++ src/test/java/domain/BaseBallsTest.java
@@ -0,0 +1,50 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("baseBall 집합에 대한 테스트")
+class BaseBallsTest {
+
+	@Test
+	@DisplayName("BaseBall을 List<Integer>에 따라서 생성한다. Number 같은지 테스트")
+	void number() {
+		List<Integer> numbers = Arrays.asList(9, 7, 3);
+		List<OneBaseBall> baseBalls = BaseBalls.of(numbers).getBaseBalls();
+
+		for (int i = 0; i < numbers.size(); i++) {
+			Integer baseBallValue = baseBalls.get(i).getBaseBallNumber().getValue();
+			assertThat(baseBallValue).isEqualTo(numbers.get(i));
+		}
+	}
+
+	@Test
+	@DisplayName("BaseBall을 List<Integer>에 따라서 생성한다. Position 같은지 테스트")
+	void position() {
+		List<Integer> numbers = Arrays.asList(9, 7, 3);
+		List<OneBaseBall> baseBalls = BaseBalls.of(numbers).getBaseBalls();
+
+		for (int i = 0; i < numbers.size(); i++) {
+			assertThat(baseBalls.get(i).getBaseBallPosition()).isEqualTo(i);
+		}
+	}
+
+	@Test
+	@DisplayName("position에 따라 OneBaseBall 리턴하기")
+	void findBaseBallByPosition() {
+		List<Integer> numbers = Arrays.asList(9, 7, 3);
+		BaseBalls baseBalls = BaseBalls.of(numbers);
+
+		for (int i = 0; i < numbers.size(); i++) {
+			OneBaseBall baseBallByPosition = baseBalls.findBaseBallByPosition(i);
+			assertThat(baseBallByPosition.getBaseBallPosition()).isEqualTo(i);
+			assertThat(baseBallByPosition.getBaseBallNumber().getValue()).isEqualTo(numbers.get(i));
+		}
+
+	}
+}
