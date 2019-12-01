66547658fe7d673a2f560ea065e451cb2795c369
jaeju.jang <jjj0611@gmail.com>
2019-12-02 06:57:27 +0900

    [ADD] 숫자야구게임의 전체적인 진행을 위한 Game 클래스를 추가했습니다.
    	- 게임을 초기화하는 메서드를 추가했습니다.
    	- 사용자에게 메세지를 출력하는 메서드를 추가했습니다.
    	- 숫자 야구게임 한 게임을 위한 메서드를 추가했습니다.
    	- 사용자에게 새로운 게임을 할지, 게임을 종료할지 응답받는 메서드를 추가했습니다.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.Scanner;
 
 public class Game {
+
+    private Host host;
+    private Client client;
+    private Scanner gameScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        boolean continueGame = true;
+        while(continueGame) {
+            game.startOneGame();
+            continueGame = game.checkContinueGame();
+        }
+    }
+
+    Game() {
+        host = new Host();
+        client = new Client();
+    }
+
+    private void startOneGame() {
+        initGame();
+        boolean isFinished = false;
+        while(!isFinished) {
+            try {
+                List<Integer> numbersFromClient = takeNumbersFromUser();
+                printWithNewline(host.makeResultStatement(numbersFromClient));
+                isFinished = host.matchAllNumbers(numbersFromClient);
+            } catch (InvalidInputException ie) {
+                printWithNewline(ie.getMessage());
+            }
+        }
+    }
+
+    private void initGame() {
+        host.generateNumbers();
+    }
+
+    private List<Integer> takeNumbersFromUser() {
+        printWithoutNewline("숫자를 입력하세요: ");
+        String inputFromUser = gameScanner.nextLine();
+        return client.makeNumbersFromString(inputFromUser);
+    }
+
+    private boolean checkContinueGame() {
+        String input;
+        while(true) {
+            input = gameScanner.nextLine().trim();
+            if (input.equals("1")) {
+                return true;
+            }
+            if (input.equals("2")) {
+                return false;
+            }
+            printWithNewline("잘못된 값을 입력하셨습니다. 다시 입력해주세요(새로운 게임: 1, 게임 종료: 2)");
+        }
+    }
+
+    private void printWithoutNewline(String message) {
+        System.out.print(message);
+    }
+
+    private void printWithNewline(String message) {
+        System.out.println(message);
+    }
+
 }
