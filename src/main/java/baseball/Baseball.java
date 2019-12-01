f724ead132516e436f7449929788b543694be2ac
junho <hwanghe159@naver.com>
2019-12-01 17:40:49 +0900

    두 int[] 배열을 비교해서 결과를 출력하는 compareAndPrintResult()메서드 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -7,21 +7,24 @@ public class Baseball {
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		while(true) {
-			int[] ans = GenerateThreeRandNum();
+			int[] ans = generateThreeRandNum();
+			boolean isEqual = false;
+			while(!isEqual) {
+				System.out.print("숫자를 입력해주세요 : ");
+				String str = scan.nextLine();
+				
+				//제대로 된 입력인지 판별. 아니면 예외처리
+				
+				int[] input = stringToIntArray(str);
 			
-			System.out.print("숫자를 입력해주세요 : ");
-			String str = scan.nextLine();
-			
-			//제대로 된 입력인지 판별. 아니면 예외처리
-			
-			int[] input = StringToIntArray(str);
-		
+				isEqual = compareAndPrintResult(ans,input);
+			}
 		}
 	}
 	
 	
 	
-	static int[] GenerateThreeRandNum() {
+	static int[] generateThreeRandNum() {
 		int ans[] = new int[3];
 		Random random = new Random();
 		int count = 0;
@@ -46,7 +49,7 @@ public class Baseball {
 		return false;	
 	}
 	
-	static int[] StringToIntArray(String str) {
+	static int[] stringToIntArray(String str) {
 		int ans[] = new int[3];
 		for(int i = 0; i<3; i++) {
 			ans[i] = str.charAt(i) - '0';
@@ -54,5 +57,44 @@ public class Baseball {
 		return ans;
 	}
 	
+	static boolean compareAndPrintResult(int[] arr1, int[] arr2) {
+		int strikeCount = 0;
+		int ballCount = 0;
+		if(arr2[0] == arr1[0]) {
+			strikeCount++;
+		}
+		if(arr2[0] == arr1[1]) {
+			ballCount++;
+		}
+		if(arr2[0] == arr1[2]) {
+			ballCount++;
+		}
+		if(arr2[1] == arr1[0]) {
+			ballCount++;
+		}
+		if(arr2[1] == arr1[1]) {
+			strikeCount++;
+		}
+		if(arr2[1] == arr1[2]) {
+			ballCount++;
+		}
+		if(arr2[2] == arr1[0]) {
+			ballCount++;
+		}
+		if(arr2[2] == arr1[1]) {
+			ballCount++;
+		}
+		if(arr2[2] == arr1[2]) {
+			strikeCount++;
+		}
+		
+		System.out.println(strikeCount+"스트라이크 "+ballCount+"볼");
+		
+		if(strikeCount == 3) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 }
