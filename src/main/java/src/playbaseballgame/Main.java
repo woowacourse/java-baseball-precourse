fc134357903adca28d6a4e013eafaf1c6104d650
YerinCho <forjustice9839@gmail.com>
2019-12-02 04:14:06 +0900

    게임 실행을 위한 메인 기능 구현

---
+++ src/main/java/src/playbaseballgame/Main.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)Main.java        2019/12/02
+ *
+ *
+ */
+
+package playbaseballgame;
+
+/**
+ * 숫자야구게임 실행을 위한 메인클래스
+ *
+ * @version		0.1 2019년 12월 2일
+ * @author		YerinCho          
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		PlayBaseballGame bgame = new PlayBaseballGame();
+		bgame.run();
+	}
+}
