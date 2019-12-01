c926b49b4a34e8ec47168eec219a70405cbbdeb4
ParkDooWon <pass345@naver.com>
2019-12-01 20:38:47 +0900

    refactor : 자바 코드 컨벤션 준수 및 클래스 수정

--- src/main/java/baseballgame/Computer.java
+++ src/main/java/baseballgame/Computer.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 public class Computer {
 	private static final int BALL_LEN = 3;
 	private static final int MAX_NUM = 9;
+	private ArrayList<Integer> randomNums = new ArrayList<Integer>(BALL_LEN);
 	Random random = new Random();
-	ArrayList<Integer> randomNums = new ArrayList<Integer>(BALL_LEN);
 	
 	public void makeRandomNums() {
 		randomNums.clear();
 		while (true) {
-			if(randomNums.size() == BALL_LEN) { break; }
+			if (randomNums.size() == BALL_LEN) { break; }
 			int randomNum = random.nextInt(MAX_NUM) + 1;
 			if (randomNums.contains(randomNum)) {
 				continue;
 			}
 			randomNums.add(randomNum);
 		}
+	}
+	ArrayList<Integer> getRandomNums(){
+		return randomNums;
 	}
 }
