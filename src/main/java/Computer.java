ebdc3002d3a8a2d0c149023b7f6f931aa2f78df4
seyun <ksy90101@naver.com>
2019-12-01 14:04:20 +0900

    컴퓨터(정답) 임의의 수 생성

---
+++ src/main/java/Computer.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Computer {
+    static int[] computerAnswer = new int[3];
+
+    public Computer(){
+        List<Integer> candidate = new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        Random random = new Random();
+        for (int i = 0; i < computerAnswer.length; i++){ // 정답확인
+            int index = random.nextInt(candidate.size());
+            computerAnswer[i] = candidate.get(index);
+            candidate.remove(index);
+        }
+    }
+}
