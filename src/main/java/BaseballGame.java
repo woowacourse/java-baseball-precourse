9d9c28d928b830572eef1aa32d8d0c0985a74dc4
ChooWon Sir <rebeccachooosir@gmail.com>
2019-12-01 14:47:35 +0900

    [FEATURE] 기능1 게임시작하기 method

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,11 @@
+public class BaseballGame {
+
+    public void startGame() {
+        //createAnswer();
+    }
+
+    public static void main(String[] args) {
+        BaseballGame game = new BaseballGame();
+        game.startGame();
+    }
+}
