cea03598b76f4de250a68874eccad877125fefc9
jb-oh <jb.oh@icloud.com>
2019-12-02 06:57:39 +0900

    Added random number generator feature

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,2 +1,22 @@
+import java.util.Random;
+
 public class BaseballGame {
+    public static String generateRandomNumber() {
+        Random rand = new Random();
+        int n1 = rand.nextInt(9) + 1;
+        int n2 = rand.nextInt(9) + 1;
+        int n3 = rand.nextInt(9) + 1;
+
+        String n1s = String.valueOf(n1);
+        String n2s = String.valueOf(n2);
+        String n3s = String.valueOf(n3);
+
+        String result = n1s + n2s + n3s;
+
+        return result;
+    }
+    public static void main(String[] args) {
+        String number = generateRandomNumber();
+        System.out.println(number);
+    }
 }
