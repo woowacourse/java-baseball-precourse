b9820d023cee62a8d2a42bb010ac464471b22a49
junho <hwanghe159@naver.com>
2019-12-01 16:56:57 +0900

    중복되지 않게 3자리 랜덤 수를 생성하는 GenerateThreeRandNum()메서드 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,7 +1,43 @@
 package baseball;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Baseball {
 	public static void main(String[] args){
-		System.out.println("My First Package");
+		while(true) {
+			int[] ans = GenerateThreeRandNum();
+			
+			//System.out.print("숫자를 입력해주세요 : ");
+			
+		
+		}
+	}
+	
+	
+	
+	static int[] GenerateThreeRandNum() {
+		int ans[] = new int[3];
+		Random random = new Random();
+		int count = 0;
+		while(count < 3) {
+			int randomNum = random.nextInt(9)+1;
+			if(duplicateCheck(ans, randomNum, count)) {
+				continue;
+			}else {
+				ans[count] = randomNum;
+				count++;
+			}
+		}		
+		return ans;
+	}
+	
+	static boolean duplicateCheck(int[] arr, int num, int count) {
+		for(int i = 0; i<count; i++) {
+			if(arr[i] == num) {
+				return true;
+			}
+		}
+		return false;	
 	}
 }
