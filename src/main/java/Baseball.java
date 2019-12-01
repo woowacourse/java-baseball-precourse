4ab8f7b05c981e3c884acc51f115abf804af64ff
gunhee lee <dlrjsgml42@naver.com>
2019-12-01 22:26:40 +0900

    Class Refactoring(in Baseball class)

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -3,12 +3,25 @@ import java.util.Random;
 public class Baseball {
     /*
      * Baseball은 숫자야구 수행을 위한 클래스이다.
-     *
+     * 큰 기능을 담당하는 함수들을 public 메소드로 분류하여 호출할 수 있게 하고,
+     * 작은 기능을 담당하는 세부함수들은 private로 정리해두고 내부에서만 호출하게 유도한다.
+     * 메서드 기능을 더 쪼갤 필요가 있다.
      */
-    public Baseball(){
-        //
+    private int[] answerData;
+    private int[] userData;
+    Scanner sc;
+    private void getData(){
+        int input;
+        Scanner sc = new Scanner(System.in);
+        input = sc.nextInt();
+        //예외처리부
+        //해야할일 : 숫자가 엉뚱한 값 들어오는 경우 예외처리문 짜리
+        userData[0] = input/100;
+        input %= 100;
+        userData[1] = input/10;
+        userData[2] = input%10;
     }
-    public int[] makeData(){
+    private int[] makeData(){
         int[] randomNumber = new int[3];
         Random random = new Random();
         randomNumber[0] = (random.nextInt(9) + 1);
@@ -16,30 +29,42 @@ public class Baseball {
             randomNumber[1] = (random.nextInt(9) + 1);
         }while(randomNumber[0] == randomNumber[1]);
         do{
-        randomNumber[2] = (random.nextInt(9) + 1);
+            randomNumber[2] = (random.nextInt(9) + 1);
         }while( (randomNumber[0] == randomNumber[2]) || (randomNumber[1] == randomNumber[2]) );
         //서로 다른 값이어야함!!
         return randomNumber;
     }
-    public int findBall(int[] answerData, int userNum, int pos) {
+
+    public Baseball(){
+        /*
+         * 생성자 메서드.
+         * 시작할 때 초기화해야 할 값들을 정리해준다.
+         * */
+        answerData = new int[3]; //answerData는 정답값, playMethod 호출시 값 생성/부여
+        userData = new int[3]; //userData는 사용자 입력값, playMethod에서 getData 호출로 값 입력/부여
+        sc = new Scanner(System.in); //input scanner class 생성
+    }
+    private int getBall(int pos) {
+        /*
+        * 입력된 위치의 숫자가 볼인지 아닌지 판정하여 1,0을 리턴
+        * */
         for(int i=0;i<3;i++) {
-            if (i != pos && answerData[i] == userNum){
+            if (i != pos && answerData[i] == userData[pos]){
                 return 1;
             }
         }
         return 0;
     }
-    public boolean checkMethod(int[] answerData, int[] userData){
-        int strike = 0;
-        int ball = 0;
-        //strike = getStrike(answerData, userData);
-        //ball = getBall(answerData, userData);
-        //test를 위해 노가다 소스로 짜두고 나중에 수정 예정
-        for(int i=0;i<3;i++) {
-            if (answerData[i] == userData[i]) strike++;
-            ball += findBall(answerData, userData[i], i);
+    private int getStrike(int pos) {
+        /*
+         * 입력된 위치의 숫자가 스트라이크인지 아닌지 판정하여 1,0을 리턴
+         * */
+        if(answerData[pos] == userData[pos]){
+            return 1;
         }
-        System.out.println("입력 데이터 : "+userData[0]+" "+userData[1]+" "+userData[2]+" \n");
+        return 0;
+    }
+    private void printState(int strike, int ball){
         if(strike != 0){
             System.out.println(strike+" 스트라이크 ");
         }
@@ -50,35 +75,33 @@ public class Baseball {
             System.out.println("낫싱");
         }
         System.out.println("\n");
-        return (strike == 3);
+    }
+    private boolean getResult(int[] answerData, int[] userData){
+        int strike = 0;
+        int ball = 0;
+
+        for(int i=0;i<3;i++) {
+            strike += getStrike(i);
+            ball += getBall(i);
+        }
+        System.out.println("입력 데이터 : "+userData[0]+" "+userData[1]+" "+userData[2]+" \n");//log
+        printState(strike, ball);
+        return (strike == 3); //game이 끝날 수 있는지 여부만 리턴해줌.
     }
     public void playMethod(){
-        int[] answerData = makeData();
-        int[] userData;
-        boolean isEnd=false;
-            System.out.println("만든 데이터 : "+answerData[0]+" "+answerData[1]+" "+answerData[2]+" \n");
-            while(isEnd == false){
+        answerData = makeData();
+        System.out.println("만든 데이터 : "+answerData[0]+" "+answerData[1]+" "+answerData[2]+" \n");//log
+        do{
             System.out.println("숫자를 입력해주세요 : ");
-            userData =  getData();
-            isEnd = checkMethod(answerData, userData);
-        }
-    }
-    public int[] getData(){
-        int input;
-        int[] data = new int[3];
-        Scanner sc = new Scanner(System.in);
-        input = sc.nextInt();
-        //예외처리부
-        //해야할일 : 숫자가 엉뚱한 값 들어오는 경우 예외처리문 짜리
-        data[0] = input/100;
-        input %= 100;
-        data[1] = input/10;
-        data[2] = input%10;
-        return data;
+            getData();
+        }while(!getResult(answerData, userData));
     }
     public boolean exitMethod() {
+        /*
+        * play 메서드가 종료된 후 실행되어 추가입력을 받는 메서드.
+        * 입력을 바탕으로, "끝낼 수 있는지" 여부를 판단하여 boolean으로 return.
+        * */
         int input;
-        Scanner sc = new Scanner(System.in);
         input = sc.nextInt();
         if (input == 1) {//이 부분의 로직이 마음에 들지는 않지만...더 직관적인 리턴법은?
             return false;
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,11 +1,9 @@
 public class MainClass {
     public static void main(String[] args){
         Baseball baseball = new Baseball();
-        boolean isEnd = false;
-        while(isEnd == false){
+        do{
             baseball.playMethod();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 눌러주세요.");
-            isEnd = baseball.exitMethod();
-        }
+        }while(!baseball.exitMethod());
     }
 }
