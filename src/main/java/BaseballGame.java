b89a51b26fd86a0b302e8ecf80b9687410a85bd8
chws <rebeccachoosir@gmail.com>
2019-12-01 17:30:22 +0900

    [FEATURE] 기능2 정답 생성
    - 세 자리 숫자인 정답 생성하는 method

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,11 +1,33 @@
+import java.util.Random;
+
 public class BaseballGame {
 
     public void startGame() {
-        //createAnswer();
+
+    }
+
+    public int[] createRandomAnswer() {
+        int arr[] = new int[3];
+        Random random = new Random();
+
+        for(int i = 0; i < 3; i++){
+            arr[i] = random.nextInt(8) + 1;
+            for(int j = 0; j < i; j++){
+                if(arr[i] == arr[j]){
+                    i--;
+                }
+            }
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
+        int[] answer;
         BaseballGame game = new BaseballGame();
+
         game.startGame();
+        answer = game.createRandomAnswer();
+        
     }
 }
