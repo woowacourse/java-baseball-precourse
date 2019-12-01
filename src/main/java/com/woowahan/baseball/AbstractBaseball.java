c87b79723ddc936fdc653e3a902dd1d7734d0f86
Jinhong Kim <deepred.kim@gmail.com>
2019-12-01 11:33:39 +0900

    evaluateBall() 매개변수 수정
    몇번째 공인지(order)를 추가적으로 받음
    primitive type으로 바꿈

--- src/main/java/com/woowahan/baseball/AbstractBaseball.java
+++ src/main/java/com/woowahan/baseball/AbstractBaseball.java
@@ -15,7 +15,7 @@ public abstract class AbstractBaseball {
     protected abstract ArrayList<Integer> initializeBallRange();
     protected abstract Integer getRandomBall(ArrayList<Integer> range);
     protected abstract ArrayList<Integer> getRandomBalls();
-    protected abstract EarlyResults.EarlyResult evaluateBall(Integer ball, ArrayList<Integer> balls);
+    protected abstract EarlyResults.EarlyResult evaluateBall(int ball, int order,  ArrayList<Integer> balls);
     protected abstract EarlyResults evaluateBalls(ArrayList<Integer> user, ArrayList<Integer> answer);
 
     public abstract void Process();
