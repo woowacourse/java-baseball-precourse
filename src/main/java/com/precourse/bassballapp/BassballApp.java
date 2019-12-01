40fc5993bf67ec311fee21f96f49da7f2c905d73
tiroring09 <tiroring09@gmail.com>
2019-12-01 23:02:10 +0900

    [리팩토링] 패키지, 클래스, 메서드, 클래스변수 리네이밍

--- src/main/java/com/precourse/bassballapp/BassballApp.java
+++ src/main/java/com/precourse/bassballapp/BassballApp.java
@@ -1,51 +1,24 @@
-package bassballPackage;
+package com.precourse.bassballapp;
 
-import bassballPackage.CpuInput;
-import bassballPackage.UserInput;
-import bassballPackage.PrintOut;
+import com.precourse.bassballapp.Cpu;
+import com.precourse.bassballapp.User;
+import com.precourse.bassballapp.Result;
+import com.precourse.bassballapp.StrikeBall;
 
 public class BassballApp {
-	static int[] SBCountArr;
-	
-	static int[] SBCount(int[] userArr, int[] cpuArr){
-		int[] result = {0, 0};
-		for(int i = 1; i < 4; i++) {
-			for(int j = 0; j < 3; j++) {
-				result[0] += SCountIf(userArr, cpuArr, i, j);
-				result[1] += BCountIf(userArr, cpuArr, i, j);
-			}
-		}
-		return result;
-	}
-	
-	static int SCountIf(int[] userArr, int[] cpuArr, int i, int j) {
-		if(i-1 == j && userArr[i] == cpuArr[j]) {
-			return 1;
-		}else {
-			return 0;
-		}
-	}
-	
-	static int BCountIf(int[] userArr, int[] cpuArr, int i, int j) {
-		if(i-1 != j && userArr[i] == cpuArr[j]) {
-			return 1;
-		}else {
-			return 0;
-		}
-	}
 	
 	public static void main(String[] args) {
 		/*프로그램 구조*/
 		boolean isLoopActive = true;
-		CpuInput.cpuNumArr = CpuInput.cpuInput();
+		Cpu.answerArr = Cpu.makeAnswer();
 		while (isLoopActive) {
 			System.out.println("[[디버깅용 메세지]]-컴퓨터 세자리 수 : " 
-					+ CpuInput.cpuNumArr[0]+","
-					+ CpuInput.cpuNumArr[1]+","
-					+ CpuInput.cpuNumArr[2]);
-			UserInput.userNumArr = UserInput.userInputValid2();
-			SBCountArr = SBCount(UserInput.userNumArr, CpuInput.cpuNumArr);
-			PrintOut.printOut(SBCountArr);
+					+ Cpu.answerArr[0]+","
+					+ Cpu.answerArr[1]+","
+					+ Cpu.answerArr[2]);
+			User.answerArr = User.getValidAnswer2();
+			StrikeBall.resultArr = StrikeBall.count(User.answerArr, Cpu.answerArr);
+			Result.print(StrikeBall.resultArr);
 		}
 	}
 }
