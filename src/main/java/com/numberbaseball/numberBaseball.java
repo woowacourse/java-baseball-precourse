8b1b88c7fb84723c32e5a761eb91ce147ccdbcd3
김효찬 <kom1230@hanyang.ac.kr>
2019-12-01 17:11:14 +0900

    numberBaseball initial commit

---
+++ src/main/java/com/numberbaseball/numberBaseball.java
@@ -0,0 +1,19 @@
+package com.numberbaseball;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * numberbasball
+ * @version 0.0.1
+ * @since 2019-07-11
+ * @author hyochan
+ */
+
+public class numberBaseball {
+    public static void main(String[] args) {
+        numberBaseball nb = new numberBaseball();
+        nb.game();
+    }
+}
