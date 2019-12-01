b1536a0322eaffedd21499b774b96d6d68ff04fe
junho <hwanghe159@naver.com>
2019-12-01 18:41:51 +0900

    잘못된 입력일 시 다시 입력받는 check()메서드 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -5,15 +5,16 @@ import java.util.Random;
 
 public class Baseball {
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
 		while(true) {
+			Scanner scan = new Scanner(System.in);
 			int[] ans = generateThreeRandNum();
 			boolean isEqual = false;
 			while(!isEqual) {
 				System.out.print("숫자를 입력해주세요 : ");
+				
 				String str = scan.nextLine();
 				
-				//제대로 된 입력인지 판별. 아니면 예외처리
+				str = check(str);
 				
 				int[] input = stringToIntArray(str);
 				isEqual = compareAndPrintResult(ans,input);
@@ -21,9 +22,11 @@ public class Baseball {
 			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
 			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 			int replay = scan.nextInt();
+			scan.nextLine();//nextInt()는 마지막의 개행문자 때문에 다음 입력에 영향을 줌.
 			if(replay == 2) {
 				break;
 			}
+			
 		}
 	}
 	
@@ -52,6 +55,20 @@ public class Baseball {
 			}
 		}
 		return false;	
+	}
+	
+	static String check(String str) {
+		Scanner scan = new Scanner(System.in);
+		if(str.length() == 3) {
+			if(str.charAt(0)-'0' >= 1 && str.charAt(0) - '0' <= 9
+					&&str.charAt(1)-'0' >= 1 && str.charAt(1) - '0' <= 9
+					&&str.charAt(2)-'0' >= 1 && str.charAt(2) - '0' <= 9) {
+				return str;
+			}
+		}
+		System.out.print("잘못된 숫자를 입력하셨습니다. 다시 입력해주세요 : ");
+		String anotherStr = scan.nextLine();
+		return check(anotherStr);
 	}
 	
 	static int[] stringToIntArray(String str) {
@@ -98,6 +115,9 @@ public class Baseball {
 		}
 		if(ballCount != 0) {
 			System.out.print(ballCount+"볼");
+		}
+		if(strikeCount == 0 && ballCount == 0) {
+			System.out.print("0스트라이크 0볼");
 		}
 		System.out.println();
 		
