e03d1d9959295ecb80be690bb349fa223f55de19
tiroring09 <tiroring09@gmail.com>
2019-12-01 16:39:19 +0900

    BassballApp.java 전체 프로그램 구조, 흐름도 정리

--- src/main/java/bassballPackage/BassballApp.java
+++ src/main/java/bassballPackage/BassballApp.java
@@ -29,12 +29,21 @@ public class BassballApp {
 	}
 	
 	public static void main(String[] args) {
-		int[] user1 = {123,1,2,3};
-		int[] cpu1 = {1,2,3};
-		int[] result1 = SBCount(user1, cpu1);
-		System.out.println(result1[0]);
-		System.out.println(result1[1]);
-
+		//SBCount test
+//		int[] user1 = {123,1,2,3};
+//		int[] cpu1 = {1,2,3};
+//		int[] result1 = SBCount(user1, cpu1);
+//		System.out.println(result1[0]);
+//		System.out.println(result1[1]);
+		
+		/**프로그램 구조**/
+		boolean isLoopActive = true;
+		//cpuInput메서드 호출할 자리.
+		while (isLoopActive) {
+			//userInputValid2메서드 호출할 자리.
+			//SBCount(유저, 컴퓨터); <<<변수이름 미정.
+			//printOut(S/B갯수 배열);
+		}
 	}
 
 }
