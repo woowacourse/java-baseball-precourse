1598ab66febb2a11c8847f306d214d64bf535772
Eun Seok <jeseok95@gmail.com>
2019-12-01 19:43:33 +0900

    중복숫자,0 입력받지않게 생성자와 check()수정, README수정

--- src/main/java/com/eunseok/BaseballGame/Baseball.java
+++ src/main/java/com/eunseok/BaseballGame/Baseball.java
@@ -22,19 +22,50 @@ import java.util.Scanner;
  * @version 1.0 2019년 11월 29일
  */
 public class Baseball {
-    /**Baseballgame의 답을 나타내는 배열*/
-    private static int[] ans = new int[3];
-    /**사용자의 입력을 담는 배열*/
-    private static int[] input = new int[3];
+    private static int[] ans = new int[3];      //Baseballgame의 답을 나타내는 배열
+    private static int[] input = new int[3];    //사용자의 입력을 담는 배열
 
     /**
      * 클래스의 생성자. 클래스를 생성하면 게임의 정답이 ans배열에 저장.
+     * 숫자가 중복되지 않게 받음
      */
-    public Baseball(){
+    public Baseball() {
         Random rand = new Random();
-        for(int i=0;i<ans.length;i++) {
-            ans[i] = rand.nextInt(9)+1;  // 1 ~ 9 난수생성
+        int temp = 0;
+        int i = 1;      //ans배열의 길이
+
+        /*
+        처음에는 비교할숫자가 없으므로 그냥 대입
+        */
+        temp = rand.nextInt(9) + 1;          // 1 ~ 9 난수생성
+        ans[0] = temp;
+
+        temp = rand.nextInt(9) + 1;
+
+        /*
+          i == 1 일때부터 하나씩 비교하며 중복숫자면 다시 값을받음
+          j가 배열안에 모든 숫자와 비교하고 나서만 저장
+        */
+        for (int j = 0; j < i; j++) {
+            if (ans[j] == temp) {
+                j = 0;
+                temp = rand.nextInt(9) + 1;
+                continue;
+            }
+
+            if(j==(i-1)) {
+                ans[i] = temp;
+                temp = rand.nextInt(9) + 1;
+                j = 0;
+                i++;
+            }
+
+            if (i == 3) {
+                break;
+            }
         }
+        for(int j=0; j<3;j++)
+            System.out.print(ans[j]);
     }
 
     /**사용자에게 입력을 받는 메소드.*/
@@ -43,13 +74,13 @@ public class Baseball {
         System.out.print("숫자를 입력해주세요 : ");
         Scanner sc = new Scanner(System.in);
 
-        /**한자리씩 배열에 저장하기 위해 임시저장하는 temp변수*/
+        /*한자리씩 배열에 저장하기 위해 임시저장하는 temp변수*/
         int temp;
         temp = sc.nextInt();
         sc.nextLine();
 
-        /**배열에 한자리씩 저장하기 위한 반복문*/
-        for(int i=(input.length-1);i>=0;i--){
+        /*배열에 한자리씩 저장하기 위한 반복문*/
+        for(int i=(input.length-1); i>=0; i--){
             input[i] = temp%10;
             temp/=10;
         }
@@ -59,29 +90,43 @@ public class Baseball {
      * strike 와 ball을 구하는 매소드.
      * 사용자의 input과 답을 저장하고있는 ans를 비교.
      * 각 자리 위치가 같으면 strike++, 다른위치이면 ball++
-     * @return strike 의 개수를 return. start()메소드의 무한루프 탈출조건.
+     * @return strike 의 개수를 return. start()메소드의 무한루프 탈출조건,중복숫자나 0 입력시 -1 return.
      */
     private int check(){
         int strike = 0;
         int ball = 0;
 
-        if (ans[0]==input[0]) {
+        /*중복숫자 또는 0 입력*/
+        if((input[0] == '0')
+                || (input[1] =='0')
+                || (input[2] =='0')) {
+            return -1;
+        }
+        if((input[0] ==input[1])
+                || (input[1] ==input[2])
+                || (input[2] ==input[0])) {
+            return -1;
+        }
+
+        /*strike counting*/
+        if (ans[0] == input[0]) {
             strike++;
         }
-        if (ans[1]==input[1]) {
+        if (ans[1] == input[1]) {
             strike++;
         }
-        if (ans[2]==input[2]) {
+        if (ans[2] == input[2]) {
             strike++;
         }
 
-        if (ans[0]==input[1]||ans[0]==input[2]) {
+        /*ball counting*/
+        if ((ans[0] == input[1])||(ans[0]==input[2])) {
             ball++;
         }
-        if (ans[1]==input[0]||ans[1]==input[2]) {
+        if ((ans[1]==input[0])||(ans[1]==input[2])) {
             ball++;
         }
-        if (ans[2]==input[0]||ans[2]==input[1]) {
+        if ((ans[2]==input[0])||(ans[2]==input[1])) {
             ball++;
         }
         System.out.println(strike + "스트라이크" + ball + "볼");
@@ -93,15 +138,24 @@ public class Baseball {
      * 게임을 시작하는 start() 메소드
      */
     public void start(){
-        /**strike개수를 리턴받는 변수.*/
+
+        /*strike개수를 리턴받는 변수.*/
         int checkStrike = 0;
 
         while (true) {
             makeInput();
             checkStrike = check();
+
+            if(checkStrike == -1){
+
+                /*사용자가 0이나 같은숫자입력할경우 재입력*/
+                System.out.println("0또는 중복숫자 입력불가");
+                continue;
+            }
+
             if(checkStrike == 3) {
 
-                /**check()에서 받은 strike개수가 3개일때 무한루프 탈출.*/
+                /*check()에서 받은 strike개수가 3개일때 무한루프 탈출.*/
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
