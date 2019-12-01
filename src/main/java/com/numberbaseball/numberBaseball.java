5a061f82dc55a8a698db65607e50eaf65e0e4896
김효찬 <kom1230@hanyang.ac.kr>
2019-12-01 17:23:33 +0900

    Update case "낫싱"

--- src/main/java/com/numberbaseball/numberBaseball.java
+++ src/main/java/com/numberbaseball/numberBaseball.java
@@ -124,8 +124,15 @@ public class numberBaseball {
             }
         }
         ball -= strike;
-
-        System.out.println(strike + "스트라이크 " + ball + "볼" + "\n");
+        if (strike>0) {
+            System.out.println(strike + "스트라이크 ");
+        }
+        if (ball>0) {
+            System.out.println(ball + "볼");
+        }
+        if (strike == 0 && ball ==0){
+            System.out.println("낫싱");
+        }
         return false;
     }
 
