37bed856cae61aa371041b8bd79801baac266e5c
YerinCho <forjustice9839@gmail.com>
2019-12-02 01:21:22 +0900

    플레이어에게 입력 요청하는 기능 추가

---
+++ src/main/java/src/playbaseballgame/Player.java
@@ -0,0 +1,47 @@
+
+/*
+ * @(#)Player.java		 2019/12/02
+ * 
+ * 
+ * Copyright (c) YerinCho
+ * 
+ * 
+ */
+
+package playbaseballgame;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @version 0.1 2019년 12월 2일
+ * @author YerinCho
+ */
+
+public class Player {
+	private int num = 0;
+	
+	/*플레이어에게 수 입력 요청하는 함수*/
+	public void askNumber() {
+		Scanner sc =new Scanner(System.in);
+		String tmpnum ="";		//사용자가 입력하는 값이 문자일 수도 있으므로, 스트링으로 tmpnum 선언
+		
+		while(true) {
+			System.out.print("1부터 9까지의 서로 다른 세자리 숫자를 입력해주세요 : ");
+			tmpnum=sc.next();
+			
+			/*플레이어가 입력한 값이 올바른지 처리하는 함수*/
+			if(checkInput(tmpnum)) {
+				break;
+			}
+			else {
+				System.out.println("입력한 값이 올바르지 않습니다.");
+			}
+		}
+		
+		/*문자였던 값을 정수로 변환*/
+		this.num=Integer.parseInt(tmpnum);
+	}
+	
+	
+}
