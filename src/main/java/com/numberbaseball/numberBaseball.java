818f02fd3aafd7d88bdd31ee93513ec979b7bcfb
김효찬 <kom1230@hanyang.ac.kr>
2019-12-01 17:11:54 +0900

    Add game method

--- src/main/java/com/numberbaseball/numberBaseball.java
+++ src/main/java/com/numberbaseball/numberBaseball.java
@@ -16,4 +16,34 @@ public class numberBaseball {
         numberBaseball nb = new numberBaseball();
         nb.game();
     }
+
+    public void game() {
+
+        Scanner scan = new Scanner(System.in);
+        boolean finish = false;
+
+        while(true){
+            if (!finish) {
+                gameLogic();
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+                finish = true;
+            }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+            String inputLine;
+            inputLine = scan.nextLine();
+
+            if (inputLine.equals("1")){
+                finish = false;
+                continue;
+            } else if (inputLine.equals("2")){
+                break;
+            } else {
+                System.out.println("잘못된 입력입니다.\n");
+                continue;
+            }
+        }
+        return;
+    }
+
 }
