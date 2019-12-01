8a5b2ef80fb09489f09ce9b119e89c4a8e9e69ed
In-Yeong <in0_2015@naver.com>
2019-12-01 16:23:50 +0900

    주석처리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,25 +7,25 @@ public class Main {
 		Matching matching = new Matching();
 		Result result = new Result();
 		
-		int[] target = computer.make_list();
-		int[] success = new int[2];
+		int[] target = computer.make_list();					// 랜덤 넘버(3개)를 array로 가져옴
+		int[] success = new int[2];								// [성공여부, 재시작여부]
 		
 		while (true) {
-			int[] answer = user.input();
+			int[] answer = user.input();						// 사용자가 답한 숫자
 			
-			int strike = matching.strike(target, answer);
-			int ball = matching.ball(target, answer);
+			int strike = matching.strike(target, answer);		// 스트라이크 갯수
+			int ball = matching.ball(target, answer);			// 볼 갯수
 			
-			success = result.release(strike, ball);
+			success = result.release(strike, ball);				// 결과 응답
 			
 			if (success[0] == 1) {
-				break;
+				break;											// 성공여부가 1일때 답을 맞춘 것이며, while 빠져나옴
 			}
 		}
 		
 		if (success[1] == 1) {
-			main(args);
-		}
+			main(args);											// 재시작여부가 1일때 재시작을 의미하며, main으로
+		}														// 돌아가서 게임을 재시작함
 	}
 	
 }
