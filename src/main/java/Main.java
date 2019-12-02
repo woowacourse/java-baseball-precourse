c300a4fa327e0ae32671eddd09f495b20ec3fd20
jaimemin <jaimemin@gmail.com>
2019-12-03 01:08:09 +0900

    숫자 비교 결과 출력하는 기능 구현 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     private static final int MAX_NUMBER = 9;
+    private static final int MAX_ARRAY_SIZE = 3;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         List<Integer> numbers = createNumbers();
-        
+
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            int guess = scanner.nextInt();
+
+            if (isGuessValid(guess, numbers)) {
+                break;
+            }
+        }
     }
 
     private static List<Integer> createNumbers() {
@@ -16,7 +25,7 @@ public class Main {
 
         List<Integer> numbers = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
             int temp = rand.nextInt(MAX_NUMBER) + 1;
 
             if (!isNumberUnique(temp, numbers)) {
@@ -43,5 +52,94 @@ public class Main {
         }
 
         return flag;
+    }
+
+    private static List<Integer> createGuesses(int guess) {
+        List<Integer> guesses = new ArrayList<>();
+        Stack<Integer> tempStack = new Stack<>();
+
+        while (guess != 0) {
+            tempStack.push(guess % 10);
+
+            guess /= 10;
+        }
+
+        while (!tempStack.empty()) {
+            guesses.add(tempStack.peek());
+
+            tempStack.pop();
+        }
+
+        return guesses;
+    }
+
+    private static boolean isGuessValid(int guess, List<Integer> numbers) {
+        List<Integer> guesses = createGuesses(guess);
+
+        if (guesses.size() != MAX_ARRAY_SIZE) {
+            System.out.println("3자리 숫자를 입력하셔야 합니다. 다시 입력해주세요");
+
+            return false;
+        }
+
+        int strikes = getStrikes(guesses, numbers);
+        int balls = getBalls(guesses, numbers);
+
+        if (strikes == MAX_ARRAY_SIZE) {
+            System.out.println("3 스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+            return true;
+        }
+
+        if (strikes == 0 && balls == 0) {
+            System.out.println("낫싱");
+
+            return false;
+        }
+
+        System.out.println(strikes + "스트라이크 " + balls + "볼");
+
+        return false;
+    }
+
+    private static int getStrikes(List<Integer> guesses, List<Integer> numbers) {
+        int strikes = 0;
+
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
+            if (guesses.get(i) == numbers.get(i)) {
+                strikes++;
+            }
+        }
+
+        return strikes;
+    }
+
+    private static boolean isGuessBall(int index, List<Integer> guesses, List<Integer> numbers) {
+        int guess = guesses.get(index);
+
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
+            if (i == index) {
+                continue;
+            }
+
+            if (numbers.get(i) == guess) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static int getBalls(List<Integer> guesses, List<Integer> numbers) {
+        int balls = 0;
+
+        for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
+            if (isGuessBall(i, guesses, numbers)) {
+                balls++;
+            }
+        }
+
+        return balls;
     }
 }
