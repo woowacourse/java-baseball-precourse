7a6825b172ea180447ddf867eeec3b02feb31fd7
chws <rebeccachoosir@gmail.com>
2019-12-02 23:42:52 +0900

    [FEATURE] 추가 기능-입력에 대한 예외처리
    - 범위를 벗어나는 정수의 입력일 경우 예외처리

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -3,47 +3,129 @@ import java.util.Scanner;
 
 public class BaseballGame {
 
+    public static void main(String[] args) {
+        int[] answer;
+        int[] userInput;
+        boolean finishGame;
+        int strike = 0;
+        int ball = 0;
+        BaseballGame game = new BaseballGame();
+
+        answer = game.createRandomAnswer();
+
+
+    }
+
     public void startGame() {
-        System.out.print("숫자를 입력해주세요:");
+        int [] userInput;
+        int [] answer;
+        boolean finishGame;
+        int strike = 0;
+        int ball = 0;
+
+        answer = createRandomAnswer();
+        System.out.print("숫자를 입력해주세요: ");
+        userInput = getUserInput();
+
+        finishGame = false;
+        while (!finishGame) {
+            game.startGame();
+            userInput = game.getUserInput();
+            strike = game.checkStrike(answer, userInput);
+            ball = game.checkBall(answer, userInput);
+
+            if (strike == 3) {
+                game.printResult(strike, 0);
+                finishGame = game.restart();
+                answer = game.createRandomAnswer();
+            } else if (strike > 0 || ball > 0) {
+                game.printResult(strike, ball);
+                finishGame = false;
+            } else {
+                game.printResult(0, 0);
+                finishGame = false;
+            }
+        }
     }
 
     public int[] createRandomAnswer() {
-        int arr[] = new int[3];
-        Random random = new Random();
+        boolean visited[] = new boolean[10];
+        int answer[] = new int[3];
+        int randomIndex;
 
-        for(int i = 0; i < 3; i++){
-            arr[i] = random.nextInt(8) + 1;
-            for(int j = 0; j < i; j++){
-                if(arr[i] == arr[j]){
-                    i--;
-                }
-            }
-            //System.out.print(arr[i] + " ");
+        visited[0] = true;
+        for(int i = 1; i < 10; i++){
+            visited[i] = false;
         }
 
-        return arr;
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            randomIndex = random.nextInt(8) + 1;
+            if(!visited[randomIndex]){
+                visited[randomIndex] = true;
+                answer[i] = randomIndex;
+            }else{
+                i--;
+            }
+        }
+
+        return answer;
     }
 
-    public int[] getUserInput(){
+    public int[] getUserInput() {
         int input;
-        int[] arr = new int[3];
+        int[] userAnswer = new int[3];
         Scanner sc = new Scanner(System.in);
         input = sc.nextInt();
 
-        for(int i = 2; i >= 0; i--){
-            arr[i] = input % 10;
+        while(!errorCheckRange(input)){
+            input = sc.nextInt();
+        }
+
+        while(!errorCheckDuplicate(input)){
+            input = sc.nextInt();
+        }
+
+        for (int i = 2; i >= 0; i--) {
+            userAnswer[i] = input % 10;
             input /= 10;
         }
 
-        return arr;
+        return userAnswer;
     }
 
-    public int checkStrike(int[] answer, int[] userInput){
+    public boolean errorCheckRange(int input) {
+        if(input < 100 || input > 1000){
+            System.out.println("3자리 숫자를 입력해 주세요!");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean errorCheckDuplicate(int input) {
+        int a, b, c;
+        a = input % 10;
+        input /= 10;
+        b = input % 10;
+        input /= 10;
+        c = input % 10;
+        if(a == b || a == c || b == c){
+            return false;
+        }else if(a == b && a == c){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public int checkStrike(int[] answer, int[] userInput) {
         int result;
 
         result = 0;
-        for(int i = 0; i < 3; i++){
-            if(answer[i] == userInput[i]){
+        for (int i = 0; i < 3; i++) {
+            if (answer[i] == userInput[i]) {
                 result++;
             }
         }
@@ -51,88 +133,54 @@ public class BaseballGame {
         return result;
     }
 
-    public int checkBall(int[] answer, int[] userInput){
+    public int checkBall(int[] answer, int[] userInput) {
         int result;
 
         result = 0;
-        if(userInput[0] == answer[1] || userInput[0] == answer[2]){
+        if (userInput[0] == answer[1] || userInput[0] == answer[2]) {
             result++;
         }
-        if(userInput[1] == answer[0] || userInput[1] == answer[2]){
+        if (userInput[1] == answer[0] || userInput[1] == answer[2]) {
             result++;
         }
-        if(userInput[2] == answer[0] || userInput[2] == answer[1]){
+        if (userInput[2] == answer[0] || userInput[2] == answer[1]) {
             result++;
         }
 
         return result;
     }
 
-    public void printResult(int strike, int ball){
-        if(strike == 0 && ball == 0) {
+    public void printResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
             System.out.println("낫싱 ");
             return;
         }
 
-        if(strike > 0){
+        if (strike > 0) {
             System.out.print(strike + "스트라이크 ");
         }
 
-        if(ball > 0){
+        if (ball > 0) {
             System.out.print(ball + "볼 ");
         }
 
         System.out.println();
 
-        if(strike == 3) {
+        if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         }
     }
 
-    public boolean restart(){
+    public boolean restart() {
         int option;
         Scanner sc = new Scanner(System.in);
         option = sc.nextInt();
 
-        if(option == 1){
+        if (option == 1) {
             return false;
-        }else{
+        } else {
             return true;
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] answer;
-        int[] userInput;
-        boolean finishGame;
-        int strike;
-        int ball;
-        BaseballGame game = new BaseballGame();
-
-        answer = game.createRandomAnswer();
-
-        finishGame = false;
-        ball = 0;
-        strike = 0;
-        while(!finishGame){
-            game.startGame();
-            userInput = game.getUserInput();
-            System.out.println();
-            strike = game.checkStrike(answer, userInput);
-            ball = game.checkBall(answer, userInput);
-
-            if(strike == 3){
-                game.printResult(strike, 0);
-                finishGame = game.restart();
-                answer = game.createRandomAnswer();
-            }else if(strike > 0 || ball > 0){
-                game.printResult(strike, ball);
-                finishGame = false;
-            }else {
-                game.printResult(0, 0);
-                finishGame = false;
-            }
         }
     }
 }
