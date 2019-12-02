f1fa18eb0c8e1bfe93f950999d4f3b460c82a9d0
jaeju.jang <jjj0611@gmail.com>
2019-12-02 23:48:43 +0900

    [ADD] Game 클래스에 게임 진행을 위한 안내 메시지 출력부분을 추가했습니다.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Game {
 
     private static final String RESTART_GAME = "1";
-    private static final  String EXIT_GAME = "2";
+    private static final String EXIT_GAME = "2";
+    private static final int COUNT_SHOULD_BE_MATCHED = 3;
 
     private Host host;
     private Client client;
@@ -13,27 +14,32 @@ public class Game {
     public static void main(String[] args) {
         Game game = new Game();
         boolean continueGame = true;
+
+        game.printIntroStatement();
         while (continueGame) {
             game.startOneGame();
+            game.printEndStatement();
             continueGame = game.checkContinueGame();
         }
     }
 
-    Game() {
+    private Game() {
         host = new Host();
         client = new Client();
     }
 
+
     private void startOneGame() {
-        initGame();
         boolean isFinished = false;
+
+        initGame();
         while (!isFinished) {
             try {
                 List<Integer> numbersFromClient = takeNumbersFromUser();
-                printWithNewline(host.makeResultStatement(numbersFromClient));
                 isFinished = host.matchAllNumbers(numbersFromClient);
+                System.out.println(host.makeResultStatement(numbersFromClient));
             } catch (InvalidInputException ie) {
-                printWithNewline(ie.getMessage());
+                System.out.println(ie.getMessage());
             }
         }
     }
@@ -43,7 +49,7 @@ public class Game {
     }
 
     private List<Integer> takeNumbersFromUser() {
-        printWithoutNewline("숫자를 입력하세요: ");
+        System.out.print("숫자를 입력하세요: ");
         String inputFromUser = gameScanner.nextLine();
         return client.makeNumbersFromString(inputFromUser);
     }
@@ -58,16 +64,21 @@ public class Game {
             if (input.equals(EXIT_GAME)) {
                 return false;
             }
-            printWithNewline("잘못된 값을 입력하셨습니다. 다시 입력해주세요(새로운 게임: 1, 게임 종료: 2)");
+            System.out.println("잘못된 값을 입력하셨습니다. 다시 입력해주세요(새로운 게임: 1, 게임 종료: 2)");
         }
     }
 
-    private void printWithoutNewline(String message) {
-        System.out.print(message);
+    private void printIntroStatement() {
+        String introStatement = "안녕하세요. 숫자야구게임에 오신 것을 환영합니다.\n" +
+                "숫자야구게임은 컴퓨터가 생성한 임의의 숫자 " + COUNT_SHOULD_BE_MATCHED + "개의 값과 자리를 맞추는 게임입니다.\n" +
+                "값과 자리가 모두 맞을 경우 스트라이크, 값만 맞을 경우 볼입니다.\n" +
+                COUNT_SHOULD_BE_MATCHED + "스트라이크가 될 경우 사용자의 승리로 게임이 끝납니다.\n" +
+                "그럼 게임을 시작하겠습니다.\n";
+        System.out.println(introStatement);
     }
 
-    private void printWithNewline(String message) {
-        System.out.println(message);
+    private void printEndStatement() {
+        System.out.println(COUNT_SHOULD_BE_MATCHED + "개의 숫자를 모두 맞추셨습니다. 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.");
     }
-
 }
