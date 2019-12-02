beef7be2da0e32c06fde41485f8302613eceb2d9
crystal_rim <remember2889@gmail.com>
2019-12-02 16:15:37 +0900

    [ADD] 정답숫자와 입력숫자 매칭하기

---
+++ src/main/java/game/domain/Game.java
@@ -0,0 +1,11 @@
+
+/*
+ * Game.java
+ * @author crystal_rim
+ * @version 1.0.0
+ * @date 2019-12-02
+ */
+package game.domain;
+
+public class Game {
+}
---
+++ src/main/java/game/domain/MatchResult.java
@@ -0,0 +1,11 @@
+
+/*
+ * MatchResult.java
+ * @author crystal_rim
+ * @version 1.0.0
+ * @date 2019-12-02
+ */
+package game.domain;
+
+public class MatchResult {
+}
