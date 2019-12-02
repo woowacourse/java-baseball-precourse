292d198443164bef0e39782d5bd5cdd4f2da9fac
koreaaerospaceU <zx811@naver.com>
2019-12-02 10:40:10 +0900

    run the program

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Main
+ *
+ * ver 1.0
+ *
+ * 2019.11.30
+ *
+ * CopyRight
+ *
+ */
+
+public class Main {
+    /*
+     * 매인 클래스 입니다.
+     * 게임을 시작합니다.
+     */
+    public static void main(String[] args){
+        Game.start();
+    }
+}
