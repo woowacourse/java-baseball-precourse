f76e5993f348d65151ad11a27e51ebcc928d336e
jungdahee <jungdahee010@naver.com>
2019-12-02 21:48:53 +0900

    feat: Add Main Class

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * 숫자 야구 게임을 시작하기 위한 클래스
+ *
+ * @version 1.0
+ *
+ * @date 2019.12.02
+ * 
+ * @author Jung dahee
+ */
+
+public class Main {
+
+    public static void main(String args[]){
+    	BaseballGame baseballGame = new BaseballGame();
+    	baseballGame.setGame();
+    	baseballGame.play();
+    }
+}
