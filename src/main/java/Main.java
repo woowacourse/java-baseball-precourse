45deb2f5caf240566e3819e6fcd6d6b86db08713
choiyeonseok <smr603@snu.ac.kr>
2019-12-02 14:31:08 +0900

    형식 : 주석 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		/**
+		 * 숫자야구게임 객체를 생성하고 play()메소드를 통해 게임을 실행합니다.
+		 */
 		BaseballGame game = new BaseballGame();
 		game.play();
 		
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -7,12 +7,25 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+/**
+ * 유저에게 세자리수 정수를 입력받고 배열에 저장한다. 게임 진행을 위해 playerNums을 만드는 클래스
+ * 
+ * @author smr60
+ *
+ */
 public class Player {
 
+	/** 게임을 위해 필요한 숫자의 자릿 수 */
 	private static final int DIGITS = 3;
 
+	/** 플레이어가 입력한 숫자를 저장하는 리스트  */
 	List<Integer> playerNums;
 
+	/**
+	 * 사용자로부터 입력받은 숫자를 배열로 저장하는 메소드 입니다. 
+	 * 입력받은 수가 세자리 수가 아니거나, 0을 포함하고 있거나, 중복되는 수가 포함되어 있으면 입력을 다시 받습니다.
+	 * 예외 사항에 해당하지 않으면 리스트를 playerNums에 복사합니다. 
+	 */
 	public void createNumbers() {
 		List<Integer> list = inputIntValue();
 
@@ -22,6 +35,11 @@ public class Player {
 		playerNums = new ArrayList<>(list);
 	}
 
+	/**
+	 * 사용자로부터 실질적으로 입력을 받는 메소드 입니다. 문자가 입력으로 들어왔을 경우, 예외처리가 발생하여 다시 입력을 받습니다.
+	 * 
+	 * @return 입력받은 정수를 한자리씩 List에 저장하여 반환합니다.
+	 */
 	private List<Integer> inputIntValue() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("숫자를 입력해주세요 : ");
