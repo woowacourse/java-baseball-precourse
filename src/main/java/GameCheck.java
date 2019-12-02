9898a17dfff498da1730b6cf9cd78c3b4e33f60f
seyun <ksy90101@naver.com>
2019-12-02 12:56:39 +0900

    게임 종료 및 게임 재시작 여부 설정 메인게임 생성

--- src/main/java/GameCheck.java
+++ src/main/java/GameCheck.java
@@ -1,42 +1,33 @@
-public class GameCheck {
+class GameCheck {
     static int strike = 0;
     static int ball = 0;
 
-    public GameCheck(int[] computer, int[] user){
+    public GameCheck(int[] computer, int[] user) {
         this.getStrike(computer, user);
         this.getBall(computer, user);
-        this.getNothting(strike, ball);
     }
 
-    public int getStrike(int[] computer, int[] user){
+    public int getStrike(int[] computer, int[] user) {
         strike = 0;
-        for (int i = 0; i < computer.length; i++){
-            if(computer[i] == user[i]){
+        for (int i = 0; i < computer.length; i++) {
+            if (computer[i] == user[i]) {
                 strike++;
             }
         }
         return strike;
     }
 
-    public int getBall(int[] computer, int[] user){
+    public int getBall(int[] computer, int[] user) {
         ball = 0;
-        if(user[0] == computer[1] || user[0] == computer[2]){
+        if (user[0] == computer[1] || user[0] == computer[2]) {
             ball++;
         }
-        if(user[1] == computer[0] || user[1] == computer[2]){
+        if (user[1] == computer[0] || user[1] == computer[2]) {
             ball++;
         }
-        if(user[2] == computer[0] || user[2] == computer[1]){
+        if (user[2] == computer[0] || user[2] == computer[1]) {
             ball++;
         }
         return ball;
-    }
-
-    public boolean getNothting(int strike, int ball){
-        if(strike == 0 && ball == 0){
-            return true;
-        }else{
-            return false;
-        }
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
-public class User {
+class User {
     static int[] userNumber = new int[3];
-    public User(){
+
+    public User() {
         Scanner sc = new Scanner(System.in);
         String input = "";
-        while(true) {
+        while (true) {
             System.out.println("세자리 임의의 다른 숫자를 입력해주세요.");
             input = sc.nextLine();
-            if(input.length() != 3 || input.contains("0")) {
+            if (input.length() != 3 || input.contains("0")) {
                 continue;
-            } else if(input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)){      // 중복되는 수가 있으면 입력 다시 받기
+            } else if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)) {      // 중복되는 수가 있으면 입력 다시 받기
                 continue;
             }
-            try{                                                                                                                            // 숫자말고 다른 수가 들어오면 다시 입력 받기
+            try {                                                                                                                            // 숫자말고 다른 수가 들어오면 다시 입력 받기
                 Integer.parseInt(input);
                 break;
-            }catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 continue;
             }
         }
-        for (int i = 0; i < userNumber.length; i++){
-            userNumber[i] = (int)input.charAt(i) - 48;
+        for (int i = 0; i < userNumber.length; i++) {
+            userNumber[i] = (int) input.charAt(i) - 48;
         }
     }
 }
