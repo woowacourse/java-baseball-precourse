18bd1653cc79f1bc6ee05f2651c96f12ad1585ad
jungdahee <jungdahee010@naver.com>
2019-12-02 22:06:32 +0900

    feat: Add Main class for BaseballGame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     public static void main(String args[]){
     	BaseballGame baseballGame = new BaseballGame();
+    	
     	baseballGame.setGame();
     	baseballGame.play();
     }
