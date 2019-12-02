063375a7cb7cf00a1ac0323e2df0f84269a34015
jaeju.jang <jjj0611@gmail.com>
2019-12-02 22:44:49 +0900

    [EDIT] final 변수의 변수 명을 컨벤션을 따라 변경하였고, 메서드명을 변경하였습니다.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,21 +3,22 @@ import java.util.List;
 
 public class Client {
 
-    static final int baseballGameSize = 3;
-    static final int notAllowedInteger = 0;
+    private static final int COUNT_SHOULD_BE_MATCHED = 3;
+    private static final int NOT_ALLOWED_INTEGER = 0;
 
     public List<Integer> makeNumbersFromString(String inputFromUser) {
-        checkValidation(inputFromUser);
+        validates(inputFromUser);
         return convertToNumbers(inputFromUser);
     }
 
-    private boolean checkValidation(String inputFromUser) {
-        return checkLength(inputFromUser) && checkAllNumbers(inputFromUser) && checkNoZeroNumber(inputFromUser) && checkNoDuplication(inputFromUser);
+    private boolean validates(String inputFromUser) {
+        return checkLength(inputFromUser) && checkAllNumbers(inputFromUser) &&
+                checkNoZeroNumber(inputFromUser) && checkNoDuplication(inputFromUser);
     }
 
     private boolean checkLength(String inputFromUser) {
-        if (inputFromUser.length() != baseballGameSize) {
-            throw new InvalidInputException("입력하신 문자의 길이가 " + baseballGameSize + "이 아닙니다.");
+        if (inputFromUser.length() != COUNT_SHOULD_BE_MATCHED) {
+            throw new InvalidInputException("입력하신 문자의 길이가 " + COUNT_SHOULD_BE_MATCHED + "이 아닙니다.");
         }
         return true;
     }
@@ -33,8 +34,8 @@ public class Client {
 
     private boolean checkNoZeroNumber(String inputFromUser) {
         for (int i = 0; i < inputFromUser.length(); i++) {
-            if (Integer.parseInt(String.valueOf(inputFromUser.charAt(i))) == notAllowedInteger) {
-                throw new InvalidInputException("입력하신 문자 중 " + notAllowedInteger + "(이)가 포함되어 있습니다.");
+            if (convertFromCharToInt(inputFromUser.charAt(i)) == NOT_ALLOWED_INTEGER) {
+                throw new InvalidInputException("입력하신 문자 중 " + NOT_ALLOWED_INTEGER + "(이)가 포함되어 있습니다.");
             }
         }
         return true;
@@ -50,9 +51,12 @@ public class Client {
     private List<Integer> convertToNumbers(String inputFromUser) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < inputFromUser.length(); i++) {
-            numbers.add(Integer.parseInt(String.valueOf(inputFromUser.charAt(i))));
+            numbers.add(convertFromCharToInt(inputFromUser.charAt(i)));
         }
         return numbers;
     }
 
+    private int convertFromCharToInt(char number) {
+        return Character.getNumericValue(number);
+    }
 }
