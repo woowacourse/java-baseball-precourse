27f421e58a812f3a01e24422abc0cd5ad11fd2d7
aegis1920 <aegis1920@gmail.com>
2019-12-02 23:59:40 +0900

    add : 플레이어의 입력을 받는 메소드 추가

--- src/main/java/com/woowacourse/one/NumberBaseballGame.java
+++ src/main/java/com/woowacourse/one/NumberBaseballGame.java
@@ -1,5 +1,9 @@
 package com.woowacourse.one;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.HashSet;
+
 /**
  * 기능 구현 목록
  * 1. 랜덤변수를 만드는 메소드
@@ -41,6 +45,89 @@ public class NumberBaseballGame {
 
             randNumList[i] = randNum;
         }
+
+    }
+
+    /**
+     * 플레이어로부터 숫자를 입력받아 배열로 리턴시켜주는 메소드입니다. 3자리가 아니거나 같은 숫자가 있다면 while문으로 다시 입력해달라고
+     * 재요청합니다.
+     *
+     * @param sc
+     * @return playerNumList
+     */
+    public static int[] inputPlayerNum(Scanner sc) {
+
+        System.out.println("숫자를 입력해주세요 : ");
+
+        int playerNum = sc.nextInt();
+
+        while (true) {
+
+            if (!checkNumDigit(playerNum)) {
+                System.out.println("숫자를 3자리로 다시 입력해주세요!");
+                playerNum = sc.nextInt();
+                continue;
+            }
+
+            if (!checkNumDuplicate(playerNum)) {
+                System.out.println("서로 다른 숫자로 다시 입력해주세요!");
+                playerNum = sc.nextInt();
+                continue;
+            }
+
+            break;
+
+        }
+
+        int[] playerNumList = new int[DIGIT];
+
+        String temp = playerNum + "";
+
+        for (int i = 0; i < DIGIT; i++) {
+            playerNumList[i] = temp.charAt(i) - '0';
+        }
+
+        return playerNumList;
+    }
+
+    /**
+     * 3자리(digit)인지 체크하는 메소드입니다.
+     *
+     * @param playerNum
+     * @return boolean
+     */
+    public static boolean checkNumDigit(int playerNum) {
+
+        int playerNumLength = (playerNum + "").length();
+
+        if (playerNumLength == DIGIT) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * 중복되어 있는 숫자가 있는지 체크하는 메소드입니다. HashSet으로 중복체크를 해줍니다.
+     *
+     * @param playerNum
+     * @return boolean
+     */
+    public static boolean checkNumDuplicate(int playerNum) {
+
+        HashSet<Integer> hs = new HashSet<Integer>();
+
+        while (playerNum != 0) {
+            if (hs.contains(playerNum % 10)) {
+                return false;
+            } else {
+                hs.add(playerNum % 10);
+                playerNum /= 10;
+            }
+        }
+
+        return true;
 
     }
 
