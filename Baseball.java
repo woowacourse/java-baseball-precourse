8e5ee2cc3cc75c8485b295b3df7625a2ae804a99
unknown <woncoh1@gmail.com>
2019-12-02 12:26:52 +0900

    [추가] 정답 생성 기능 refactoring"

--- Baseball.java
+++ Baseball.java
@@ -9,19 +9,28 @@ public class Baseball {
 
     private static String threeDigitInteger() {
         // 1부터 9까지의 정수 리스트 만들기
-        List<Integer> tenIntegers =
+        List<Integer> digits = allowedDigits();
+        // 9개의 정수를 임의로 순서 바꾸기
+        Collections.shuffle(digits);
+        // 9개 정수 중 앞의 3개만 가져오기
+        List<Integer> threeDigits = digits.subList(0, 3);
+        // 3개의 정수로 이루어진 리스트를 하나의 스트링으로 바꾸기
+        return toDigitString(threeDigits);
+    }
+
+    private static List<Integer> allowedDigits() {
+        List<Integer> digits =
                 IntStream.range(1, 10)
                         .boxed()
                         .collect(Collectors.toList());
-        // 9개의 정수를 임의로 순서 바꾸기
-        Collections.shuffle(tenIntegers);
-        // 9개 정수 중 앞의 3개만 가져오기
-        List<Integer> threeIntegers = tenIntegers.subList(0, 3);
-        // 3개의 정수로 이루어진 리스트를 하나의 스트링으로 바꾸기
-        String answer = ""
-                + threeIntegers.get(0)
-                + threeIntegers.get(1)
-                + threeIntegers.get(2);
-        return answer;
+        return digits;
+    }
+
+    private static String toDigitString(List<Integer> digits) {
+        String digitString = "";
+        for (Integer digit : digits) {
+            digitString += digit;
+        }
+        return digitString;
     }
 }
