45c7ae5c9649be7012b3ad82f21958595d9a680c
sezikim <kdghj1002@gmail.com>
2019-12-03 02:25:40 +0900

    add getPlayerWillingToContinue, printContinueMessage

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -15,6 +15,7 @@ class Player {
 
     public List<Integer> getPlayerInputNumbers() {
         List<Integer> inputNumbers = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
         String inputString = input.next();
 
         for (int i = 0; i < BaseballGame.ANSWER_NUMBERS_COUNT; ++i) {
@@ -22,6 +23,16 @@ class Player {
         }
 
         return inputNumbers;
+    }
+
+    public int getPlayerWillingToContinue() {
+        printContinueMessage();
+        return input.nextInt();
+    }
+
+    private void printContinueMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     public boolean toBeContinued() {
