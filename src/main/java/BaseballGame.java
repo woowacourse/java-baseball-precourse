4d7eed8abcd57193b0c24941e9db1edd554814b4
choiyeonseok <smr603@snu.ac.kr>
2019-12-02 14:28:58 +0900

    형식 : 주석 추가

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -3,16 +3,31 @@ package src.main.java;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * 게임진행의 역할을 담당하는 클래스
+ * 
+ * @author smr60
+ *
+ */
 public class BaseballGame {
 
+	/** 게임을 위해 필요한 숫자의 자릿 수 */
 	private static final int DIGITS = 3;
+	
+	/** 정답일 경우, 게임을 종료하기 위한 상수 */
 	private static final int EXIT = 2;
+	
+	/** 정답일 경우, 게임을 재시작하기 위한 상수 */
 	private static final int RESTART = 1;
 
 
+	/** strike의 갯수 */
 	private int strike;
+	
+	/** ball의 갯수 */
 	private int ball;
 
+	/** 컴퓨터와 플레이어의 객체 선언 */
 	Computer com;
 	Player player;
 
@@ -21,7 +36,9 @@ public class BaseballGame {
 		player = new Player();
 	}
 
-
+	/**
+	 * 전체 게임 로직을 실행하는 메소드입니다. 각 단계에서의 필요한 메소드들을 호출합니다.
+	 */
 	void play() {
 		while(true) {
 			com.createNumbers();
@@ -33,9 +50,12 @@ public class BaseballGame {
 			restartOrExit();
 		}
 		
-		
 	}
 
+	/**
+	 * 컴퓨터가 생성한 수와 플레이어가 생성한 수를 비교하여 strike와 ball의 갯수를 계산합니다. 
+	 * 계산이 필요할 때마다 strike와 ball을 0으로 초기화한 후 계산값을 새로 저장합니다.
+	 */
 	private void calculateScore() {
 		strike = 0;
 		ball = 0;
@@ -48,6 +68,9 @@ public class BaseballGame {
 		}
 	}
 	
+	/**
+	 * 플레이어의 입력에 따른 strike와 ball의 결과를 출력해 줍니다.
+	 */
 	private void showResult() {
 		String result = "낫싱";
 		if (strike > 0 && ball > 0) {
@@ -60,6 +83,11 @@ public class BaseballGame {
 		System.out.println(result);
 	}
 	
+	/**
+	 * 정답을 확인하는 메소드 입니다. strike 갯수가 3이면 정답으로 간주하고 축하 메시지와 함께 게임의 재시작/종료 여부를 묻습니다.
+	 * 
+	 * @return 정답일 경우 true를 반환, 그렇지 않으면 false를 반환합니다.
+	 */
 	private boolean checkAnswer() {
 		if (strike == DIGITS) {
 			System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
@@ -69,6 +97,11 @@ public class BaseballGame {
 		return false;
 	}
 	
+	/**
+	 * 플레이어에게 1 또는 2를 입력 받고, 2일 경우에는 프로그램이 종료됩니다. 
+	 * 문자를 입력받거나, 1 또는 2이외의 수를 입력받았을 경우는 예외처리가 발생하여 다시 입력을 받습니다. 
+	 * 1을 입력했을 경우 strike와 ball이 0으로 초기화되고 게임을 재시작합니다.
+	 */
 	private void restartOrExit() {
 		Scanner input = new Scanner(System.in);
 		int inputValue = 0;
@@ -92,6 +125,11 @@ public class BaseballGame {
 
 	}
 	
+	/**
+	 * 1 또는 2이외의 정수를 입력으로 받았을 때, 임의로 예외처리를 발생시키는 메소드 입니다. 예외가 발생하면 입력을 다시 받습니다.
+	 * 
+	 * @param inputValue
+	 */
 	private void createException(int inputValue) {
 		if (inputValue != RESTART && inputValue != EXIT) {
 			throw new InputMismatchException();
--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -4,14 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * 1에서 9까지 서로 다른 임의의 세 수를 저장하는 클래스
+ * 
+ * @author smr60
+ *
+ */
 public class Computer {
 
+	/** 게임을 위해 필요한 숫자의 자릿 수 */
 	private static final int DIGITS = 3;
+	
+	/** 게임에 사용되는 숫자의 범위 1 ~ 9 를 만들기 위한 상수 */
 	private static final int MAX = 9;
 	private static final int MIN = 1;
 
+	/** 컴퓨터가 랜덤으로 지정한 숫자를 저장하는 리스트 */
 	List<Integer> comNums;
 
+	/**
+	 * 컴퓨터가 1에서 9까지의 서로다른 임의의 수를 리스트에 저장하는 메소드 입니다.
+	 */
 	void createNumbers() {
 		comNums = new ArrayList<>();
 		while (comNums.size() < DIGITS) {
