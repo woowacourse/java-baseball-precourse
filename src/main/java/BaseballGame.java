170df190ab98c75a135d8181479495cb88b86ba0
sezikim <kdghj1002@gmail.com>
2019-12-03 02:45:33 +0900

    add playBaseballGame

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,8 +1,12 @@
+import java.util.List;
+
 class BaseballGame {
 
 
     static final int MAX_NUMBER = 9;
     static final int ANSWER_NUMBERS_COUNT = 3;
+    static final int PLAYER_CONTINUE = 1;
+    static final int PLAYER_QUIT = 2;
 
     public static void main(String[] args) {
         Player player = new Player();
@@ -16,7 +20,24 @@ class BaseballGame {
     }
 
     private static void playBaseballGame(Player player, Computer computer) {
+        List<Integer> playerInput;
+
         computer.makeBaseballGameAnswer();
+
+        do {
+            playerInput = player.getPlayerInputNumbers();
+            computer.printHintMessage(playerInput);
+        }
+        while (!computer.isNumberCorrect(playerInput));
+
+        switch (player.getPlayerWillingToContinue()) {
+            case PLAYER_CONTINUE:
+                return;
+
+            case PLAYER_QUIT:
+                player.playerQuitGame();
+                break;
+        }
     }
 
     private static void printEndMessage() {
