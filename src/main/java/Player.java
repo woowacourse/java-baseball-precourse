dc06537e303791f3ae7a6edf0c79d711b31eae5c
choiyeonseok <smr603@snu.ac.kr>
2019-12-02 14:10:20 +0900

    기능 : 문자입력시 예외처리 구현

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,7 @@
 package src.main.java;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,7 +19,18 @@ public class Player {
 	private List<Integer> inputIntValue() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("숫자를 입력해주세요 : ");
-		int inputIntValue = input.nextInt();
+		int inputIntValue;
+
+		while (true) {
+			try {
+				inputIntValue = input.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+				System.out.print("숫자를 입력해주세요 : ");
+				input = new Scanner(System.in);
+			} catch (Exception e) {
+			}
+		}
 
 		List<Integer> list = new ArrayList<>();
 		while (inputIntValue > 0) {
