23e8512c0a4937fe54b098b467e68cf2d5e50228
jnsorn <jnsorn@gmail.com>
2019-12-03 16:38:42 +0900

    add : get a predictive value from a player

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,19 @@
 public class Game {
     Computer computer;
     Player player;
+
     private void init(){
         computer = new Computer();
         player = new Player();
     }
 
+    private void play(){
+        computer.generateRandomNumber();
+        player.getInput();
+    }
+
     public void run(){
         init();
-        computer.generateRandomNumber();
+        play();
     }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,57 @@
+/*
+ * Copyright (c) 2019 by SorinJin
+ * All rights reserved.
+ *
+ * Player.java
+ *
+ * @author      Sorin Jin
+ * @version      1.0
+ * @date      03 Dec 2019
+ *
+ */
+
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Player {
+
+    String input;
+
+    private boolean checkInput(){
+        // 숫자 판단
+        try{
+            Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            System.out.println(Message.INPUT_ERROR_NOT_A_NUMBER);
+            return false;
+        }
+        // 자리수 판단
+        if(input.length()!=Computer.RANDOM_NUMBER_SIZE){
+            System.out.println(Message.INPUT_ERROR_WRONG_DIGIT);
+            return false;
+        }
+        // 범위 판단
+        if(input.contains("0")){
+            System.out.println(Message.INPUT_ERROR_WRONG_RANGE);
+            return false;
+        }
+        // 중복 값 판단
+        HashSet<Integer> answerSet = new HashSet<>();
+        for(int i=0; i<input.length(); i++){
+            answerSet.add(Character.getNumericValue(input.charAt(i)));
+        }
+        if(answerSet.size()!=Computer.RANDOM_NUMBER_SIZE){
+            System.out.println(Message.INPUT_ERROR_DUPLICATION);
+            return false;
+        }
+        return true;
+    }
+
+    public void getInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(Message.GET_ANSWER);
+        do{
+            input = scanner.next();
+        }while(!checkInput());
+    }
 }
