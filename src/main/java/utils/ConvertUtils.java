62b00d101ee1528ee27e94b70c0f7de88c23253f
MinKyu Song <ohh93mk@gmail.com>
2019-12-03 21:50:34 +0900

    [Features] Added a method that converts a number to ArrayList for strike and ball comparison.

--- src/main/java/utils/ConvertUtils.java
+++ src/main/java/utils/ConvertUtils.java
@@ -1,4 +1,17 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ConvertUtils {
+    public static ArrayList<Integer> toDigitArrayList(Integer number){
+        int temp=number;
+        ArrayList<Integer> digitArrayList=new ArrayList<>();
+        while(temp>0){
+            digitArrayList.add(temp%10);
+            temp/=10;
+        }
+        Collections.reverse(digitArrayList);
+        return digitArrayList;
+    }
 }
