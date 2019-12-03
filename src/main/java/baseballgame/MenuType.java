c1e9e46e03f742d84f98f3c072592b0e1e2fcfc6
KSKIM <kwangsoonkim@outlook.com>
2019-12-03 14:34:44 +0900

    refactor: rename exception message

--- src/main/java/baseballgame/MenuType.java
+++ src/main/java/baseballgame/MenuType.java
@@ -9,7 +9,7 @@ public enum MenuType {
 
     RESTART("1"), EXIT("2");
 
-    private static final String ERROR_NOT_MENU = "# 잘못된 메뉴를 선택하셨습니다.";
+    private static final String EXCEPTION_NOT_MENU = "# 잘못된 메뉴를 선택하셨습니다.";
 
     private final String code;
 
@@ -23,6 +23,6 @@ public enum MenuType {
                 return menuValue;
             }
         }
-        throw new IllegalArgumentException(ERROR_NOT_MENU);
+        throw new IllegalArgumentException(EXCEPTION_NOT_MENU);
     }
 }
