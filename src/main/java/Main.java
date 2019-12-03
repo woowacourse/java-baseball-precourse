ada5a362838c6e305cabca4a914b0611cf453a8a
sssssseungmi <tmdal9516@naver.com>
2019-12-03 10:20:54 +0900

    Refactor code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	private static Scanner sc = new Scanner(System.in);
+	private static String computerInput;
+	private static int strike;
+	private static int ball;
+	private static int nothing;
+	private static String command;
 
 	public static void main(String[] args) {
 		init();
@@ -10,12 +17,10 @@ public class Main {
 
 	private static void init() {
 		while (true) {
-			String computerInput = randomize();
+			randomize();
+			startGame();
+			inputCommand();
 
-			startGame(computerInput);
-
-			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			String command = command();
 			if (command.equals("1")) {
 				continue;
 			} else {
@@ -24,29 +29,32 @@ public class Main {
 		}
 	}
 
-	private static String randomize() {
-		String number = "";
-		boolean[] check = new boolean[10];
+	private static void randomize() {
+		computerInput = "";
+		Set<Integer> checkNumberSet = new HashSet<>();
 		Random generator = new Random();
 		for (int i = 0; i < 3; i++) {
-			int randomNumber = generator.nextInt(9) + 1;
-			if (check[randomNumber]) {
+			int randomDigit = generator.nextInt(9) + 1;
+			if (checkNumberSet.contains(randomDigit)) {
 				i--;
 			} else {
-				check[randomNumber] = true;
-				number += randomNumber;
+				checkNumberSet.add(randomDigit);
+				computerInput += randomDigit;
 			}
 		}
-		System.out.println(number);
-		return number;
+		System.out.println(computerInput);
 	}
 
-	private static void startGame(String computerInput) {
+	private static void startGame() {
 		while (true) {
 			System.out.print("숫자를 입력해주세요: ");
 			String userInput = sc.nextLine();
-
-			int strike = compare(userInput, computerInput);
+			if (userInput.length() != 3) {
+				System.out.println("3자리 숫자를 입력해주세요!!");
+				continue;
+			}
+			compare(userInput, computerInput);
+			printResult();
 			if (strike == 3) {
 				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 				break;
@@ -55,11 +63,10 @@ public class Main {
 
 	}
 
-	private static int compare(String userInput, String computerInput) {
-		int strike = 0;
-		int ball = 0;
-		int nothing = 0;
-
+	private static void compare(String userInput, String computerInput) {
+		strike = 0;
+		ball = 0;
+		nothing = 0;
 		for (int i = 0; i < 3; i++) {
 			char digit = userInput.charAt(i);
 			if (computerInput.charAt(i) == digit) {
@@ -70,6 +77,9 @@ public class Main {
 				nothing++;
 			}
 		}
+	}
+
+	private static void printResult() {
 		if (strike > 0) {
 			System.out.print(strike + "스트라이크 ");
 		}
@@ -80,11 +90,10 @@ public class Main {
 			System.out.print(nothing + "낫싱 ");
 		}
 		System.out.println();
-		return strike;
 	}
 
-	private static String command() {
-		String command = "";
+	private static void inputCommand() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 		while (true) {
 			command = sc.nextLine();
 			if (command.equals("1")) {
@@ -95,6 +104,5 @@ public class Main {
 				System.out.println("잘못된 숫자를 입력하셨습니다. 다시 입력해주세요!(새로 시작: 1, 종료: 2)");
 			}
 		}
-		return command;
 	}
 }
