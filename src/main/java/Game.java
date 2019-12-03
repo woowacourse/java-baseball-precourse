d5ba92699154c69c66af57698b882c755aae992c
MinKyu Song <ohh93mk@gmail.com>
2019-12-03 21:57:27 +0900

    [Features] Ask whether play Game again or not.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,4 +9,17 @@ public class Game {
         }while(askRetry());
         InputUtils.closeInput();
     }
+    static boolean askRetry(){
+        boolean result=false;
+        while (true) {
+            int input = InputUtils.inputInteger(String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.\n", RETRY,EXIT));
+            if (input==RETRY || input==EXIT) {
+                result = input==RETRY;
+                break;
+            } else {
+                System.out.println("유효하지 않은 입력입니다.");
+            }
+        }
+        return result;
+    }
 }
