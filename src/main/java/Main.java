473bdab7f8f798c3238b3823de5ea011a82d860c
Tak Hyungmin <milkho11@naver.com>
2019-12-03 16:16:19 +0900

    10. 메인 함수 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/*
+ * 클래스 이름 : Main
+ *
+ * 버전 정보 : 1.0
+ *
+ * 날짜 : 2019.12.3
+ *
+ * 저작권 주의 : https://github.com/takhyeongmin
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        new BaseBallGameStarter().startBaseBallGame();
+    }
+}
