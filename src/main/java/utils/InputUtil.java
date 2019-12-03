bc66e29a8e60a839cf24a4465cebb013b9fc702b
Tak Hyungmin <milkho11@naver.com>
2019-12-03 16:10:35 +0900

    7. 사용자 입력을 저장하는 기능
    InputUtil에 사용자 입력을 저장하는 기능 추가

---
+++ src/main/java/utils/InputUtil.java
@@ -0,0 +1,22 @@
+/*
+ * 클래스 이름 : InputUtil
+ *
+ * 버전 정보 : 1.0
+ *
+ * 날짜 : 2019.12.3
+ *
+ * 저작권 주의 : https://github.com/takhyeongmin
+ */
+
+package utils;
+
+import java.util.Scanner;
+
+public class InputUtil {
+
+    public Integer inputInteger() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+}
