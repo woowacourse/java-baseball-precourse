4eb1babe81fa1884683ad74b428eff186e513869
KSKIM <kwangsoonkim@outlook.com>
2019-12-03 14:28:48 +0900

    refactor: rename user input scanner

--- src/main/java/baseballgame/InputView.java
+++ src/main/java/baseballgame/InputView.java
@@ -5,7 +5,7 @@ package baseballgame;
  * @version 1.0.0
  * @since 2019-11-28
  */
-public class Scanner {
+public class InputView {
     private static final String GUIDE_INPUT_NUMBER = "# 서로 다른 세 자리 숫자를 입력하세요.";
     private static final String GUIDE_INPUT_CONTINUE_OR_EXIT = "# 다시 하시려면 1번, 종료 하시려면 2번을 입력하세요.";
     private static final String ERROR_INVALID_INPUT = "# 잘못 입력하셨습니다.";
