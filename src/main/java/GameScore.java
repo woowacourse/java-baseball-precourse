706e65000093e468a4caf5823ede7ed8c2d27464
joi0104 <joi0104@naver.com>
2019-12-03 22:19:07 +0900

    스트라이크 개수 세기

---
+++ src/main/java/GameScore.java
@@ -0,0 +1,36 @@
+/**
+ * 클래스 이름       GameScore
+ * 버전 정보        1.0
+ * 날짜            2019.12.03
+ * 저작권          joi0104
+ */
+
+public class GameScore {
+	int[] score;
+	int[] answer;
+	int[] input;
+	
+	GameScore() {
+		score = new int[3];
+		answer = new int[3];
+		input = new int[3];
+	}
+	
+	/*스크라이크 수를 세는 함수*/
+	public void checkStrike() {
+		score[0]=0;                               //스크라이크 수 초기화
+		if(answer[0]==input[0]) score[0]++;
+		if(answer[1]==input[1]) score[0]++;
+		if(answer[2]==input[2]) score[0]++;
+	}
+	
+
+	/*스트라이크,볼,낫싱값이 들어간 score를 함수*/
+	public int[] getScore(int[] answer, int[] input) {
+		this.answer = answer;
+		this.input = input;
+		checkStrike();                           //스크라이크 확인
+		return score;
+	}
+	
+}
