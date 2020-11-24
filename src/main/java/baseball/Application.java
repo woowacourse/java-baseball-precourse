6a9b0026a0cc0404a6cacf5a289956ef1f27a3f5
woowahan-pjs <jason@woowahan.com>
2020-11-24 17:53:31 +0900

    feat(baseball): add the skeleton code

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        // TODO 구현 진행
+    }
+}
