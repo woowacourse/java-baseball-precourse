35ad1a0eb9ecffa0c28b8688387bb372b5b6be56
Seonho Kim <zxzx235@naver.com>
2020-11-25 22:26:59 +0900

    feat: add getInputNumber
    3자리 수를 입력받는 기능 추가.
    예외 입력시 IllegalArgumentException 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,23 +3,51 @@ package baseball;
 import java.util.Scanner;
 
 public class Application {
+    final static int TARGET_LENGTH = 3;
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        final int TARGET_LENGTH = 3;
-        int[] target = setTarget(TARGET_LENGTH);
 
-        System.out.print(target[0]);
-        System.out.print(target[1]);
-        System.out.println(target[2]);
+        int[] target = setTarget();
+        int[] inputNumber = new int[TARGET_LENGTH];
+
+        try {
+            inputNumber = getInputNumber(scanner);
+        } catch (IllegalArgumentException e) {
+            System.out.println("오류 : 0을 제외한 3자리 숫자만 입력 가능!");
+        }
+
+        System.out.println(inputNumber[2]);
+
 
     }
 
-    public static int[] setTarget(int targetLength) {
-        int[] target = new int[targetLength];
+    public static int[] setTarget() {
+        int[] target = new int[TARGET_LENGTH];
         for (int i = 0; i < 3; i++) {
             target[i] = utils.RandomUtils.nextInt(1, 9);
         }
         return target;
+    }
+
+    public static int[] getInputNumber(Scanner scanner) throws IllegalArgumentException {
+        int[] inputNumber = new int[TARGET_LENGTH];
+        System.out.println("숫자를 입력하세요: ");
+        String input = scanner.next();
+
+        if (input.length() != TARGET_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < TARGET_LENGTH; i++) {
+            String temp = input.substring(i, i + 1);
+            inputNumber[i] = Integer.parseInt(temp);
+            if (inputNumber[i] == 0) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return inputNumber;
     }
 }
