87c79bf3cd0c4eaf172d1874cffea46cd2088eb0
sor <bosl95@naver.com>
2020-11-26 00:18:37 +0900

    feat(application) : Add makeAnswer function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,21 @@
 package baseball;
 
+import utils.RandomUtils;
+
 import java.util.Scanner;
 
 public class Application {
+    private static int[] answer = new int[3];
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        makeAnswer(answer);
+    }
+
+    private static void makeAnswer(int[] answer){
+        for(int i=0;i<3;i++){
+            answer[i]=RandomUtils.nextInt(1, 9);
+        }
     }
 }
