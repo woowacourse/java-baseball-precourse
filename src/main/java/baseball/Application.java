1b22495cee22c947354e40443e7dcdadf305b6f9
lns13301 <rkdrbgks22@ks.ac.kr>
2020-11-25 18:41:28 +0900

    refactor: 입력 값의 타당성 여부를 검사하는 부분을 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,9 @@ package baseball;
 
 import view.OutputView;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    private static final String START_VALUE = "1";
-    private static final String END_VALUE = "2";
-
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
@@ -21,35 +16,14 @@ public class Application {
         try {
             int value = Integer.parseInt(scanner);
 
-
-
-            if (isContainZero(value)) {
-                throw new IllegalArgumentException();
-            }
-
-            if (isAnyMatchNumbers(value)) {
-                throw new IllegalArgumentException();
-            }
+            Validator.isValidRangeNumber(value);
+            Validator.isContainZero(value);
+            Validator.isAnyMatchNumbers(value);
 
             return value;
         } catch (NumberFormatException e) {
             return inputPlayerNumber(scanner);
         }
-    }
-
-    public static boolean isAnyMatchNumbers(int number) {
-        char[] chars = Integer.toString(number).toCharArray();
-        List<Character> values = new ArrayList<>();
-
-        for (char c : chars) {
-            if (values.contains(c)) {
-                return true;
-            }
-
-            values.add(c);
-        }
-
-        return false;
     }
 
     public static void play(Scanner scanner) {
@@ -69,27 +43,9 @@ public class Application {
         }
     }
 
-    public static boolean isContainZero(int value) {
-        char[] chars = Integer.toString(value).toCharArray();
-
-        for (char c : chars) {
-            if (c == '0') {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     public static boolean doOneMoreGame(String scanner) {
         try {
-            if (scanner.equals(START_VALUE)) {
-                return true;
-            }
-
-            if (scanner.equals(END_VALUE)) {
-                return false;
-            }
+            return Validator.isEqualRestartValue(scanner);
         } catch (NumberFormatException e) {
             doOneMoreGame(scanner);
         }
--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,5 +1,62 @@
 package baseball;
 
-public class Validator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Validator {
+    private static final String START_VALUE = "1";
+    private static final String END_VALUE = "2";
+
+    public static void isValidRangeNumber(int value) {
+        if (value < 100 || value > 999) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isContainZero(int value) {
+        if (checkZero(value)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isAnyMatchNumbers(int value) {
+        if (checkAnyMatchNumbers(value)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isEqualRestartValue(String value) {
+        return value.equals(START_VALUE);
+    }
+
+    public static boolean isEqualEndValue(String value) {
+        return value.equals(END_VALUE);
+    }
+
+    private static boolean checkZero(int value) {
+        char[] chars = Integer.toString(value).toCharArray();
+
+        for (char c : chars) {
+            if (c == '0') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean checkAnyMatchNumbers(int number) {
+        char[] chars = Integer.toString(number).toCharArray();
+        List<Character> values = new ArrayList<>();
+
+        for (char c : chars) {
+            if (values.contains(c)) {
+                return true;
+            }
+
+            values.add(c);
+        }
+
+        return false;
+    }
 }
