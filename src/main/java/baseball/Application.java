80dcad645081dfdb90d9a423e1511eae3deb9805
taelee <taelee@student.42seoul.kr>
2020-11-26 01:07:15 +0900

    feature(Game): add exception handler for input data

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,17 +5,11 @@ public class Application {
     public static void main(String[] args) {
         boolean playing = true;
 
-        Integer number;
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        Game newGame = new Game();
+
         while (playing) {
-            System.out.print("숫자를 입력해주세요 : ");
-            if (scanner.hasNextInt()) {
-                number = scanner.nextInt();
-                System.out.println("입력숫자: " + number);
-            } else {
-                System.out.println("숫자를 입력해주세요 : ");
-            }
+            newGame.start(scanner);
         }
     }
 
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,34 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Game {
+    public void start(Scanner scanner) {
+        Integer number;
+
+        System.out.print("서로 다른 3자리 숫자를 입력해주세요 : ");
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+            if (validate(number)) {
+                System.out.println("입력숫자: " + number);
+            }
+        }
+        else if (!scanner.hasNextInt()){
+            scanner.next();
+        }
+    }
+
+    public boolean validate(int number) {
+        int first = number / 100;
+        int second = (number % 100) / 10;
+        int third = number % 10;
+
+        if (number >= 1000 || number < 100) {
+            return false;
+        }
+        else if (first == second || first == third || second == third) {
+            return false;
+        }
+        return true;
+    }
 }
