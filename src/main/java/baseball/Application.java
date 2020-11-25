711a8b0f85482883a89422eba0e6f7269328fa27
soongjamm <kimstz0303@gmail.com>
2020-11-26 04:10:49 +0900

    feat(baseball): add coode to count strikes & balls and to determine nothing

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
 import utils.RandomUtils;
 
 public class Application {
@@ -42,24 +44,59 @@ public class Application {
         }
     }
 
+    private static int[] countStrikesAndBalls(int[] answer, int[] inputNumbers) {
+        int strikes = 0;
+        int balls = 0;
+        int[] result = new int[2];
+        for (int i = 0; i < NUMBERS_LENGTH; i++) {
+            int num = inputNumbers[i];
+            if (answer[i] == inputNumbers[i]) {
+                strikes ++;
+            } else if (Arrays.stream(answer).anyMatch(x -> x == num)) {
+                balls ++;
+            }
+        }
+        result[0] = strikes;
+        result[1] = balls;
+
+        return result;
+    }
+
+    private static boolean isNothing(int strikes, int balls) {
+        if (strikes == 0 && balls == 0) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
         boolean gameStart = true;
+
+        // start game
         while (gameStart) {
 
             int[] answer = generateRandomNumbers();
             int[] inputNumbers = null;
             String inputString = "";
+            int strikes = -1;
+            int balls = -1;
+            boolean nothing = false;
 
             do {
                 System.out.println("숫자를 입력해주세요 : ");
                 inputString = scanner.nextLine();
             } while (!isInputAcceptable(inputString));
+
             inputNumbers = parseInputNumbers(inputString);
-            
-        }
+            int[] numberOfStrikesAndBalls = countStrikesAndBalls(answer, inputNumbers);
+            strikes = numberOfStrikesAndBalls[0];
+            balls = numberOfStrikesAndBalls[1];
+            nothing = isNothing(strikes, balls);
+
+        } // end of game
 
     }
 }
