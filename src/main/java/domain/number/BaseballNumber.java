dcc1bd4c1593c93a394fc8d4d6dd7bf694ed7cf9
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-25 22:46:15 +0900

    feat : add exception code about invalid baseballNumber range
    Throw IllegalArgumentException when baseballNumber is not
    ranged 1 to 9.

--- src/main/java/domain/number/BaseballNumber.java
+++ src/main/java/domain/number/BaseballNumber.java
@@ -5,6 +5,9 @@ public class BaseballNumber {
     private final int baseballNumber;
 
     public BaseballNumber(int baseballNumber) {
+        if (baseballNumber < 1 || baseballNumber > 9) {
+            throw new IllegalArgumentException();
+        }
         this.baseballNumber = baseballNumber;
     }
 }
