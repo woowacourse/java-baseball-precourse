2f31db97ebbe652eadc0a68d543a600e6b8d8957
lns13301 <rkdrbgks22@ks.ac.kr>
2020-11-25 20:35:42 +0900

    refactor: 상수 값 치환

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -9,13 +9,15 @@ public class Validator {
     private static final String START_VALUE = "1";
     private static final String END_VALUE = "2";
     private static final char ZERO = '0';
+    private static final int MIN_NUMBER_VALUE = 100;
+    private static final int MAX_NUMBER_VALUE = 999;
     private static final String invalidRangeNumberMessage = "3자리의 수를 입력하세요.";
     private static final String doNotContainZeroMessage = "'0'은 사용할 수 없습니다.";
     private static final String doNotContainMatchNumberMessage = "중복된 숫자는 사용수 없습니다.";
     private static final String invalidValueMessage = "'1' 혹은 '2'의 값을 입력하세요.";
 
     public static void isValidRangeNumber(int value) {
-        if (value < 100 || value > 999) {
+        if (value < MIN_NUMBER_VALUE || value > MAX_NUMBER_VALUE) {
             throw new IllegalArgumentException(invalidRangeNumberMessage);
         }
     }
