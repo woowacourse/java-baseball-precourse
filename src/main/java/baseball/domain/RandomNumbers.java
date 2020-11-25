2615ee8f25e794359eeca862a6c8b468841bdd03
dusdn1702 <dusdn1702@naver.com>
2020-11-25 22:34:33 +0900

    refactor: 서로 다른 세 숫자 골라내는 함수 추가
    이미 있는 숫자 골라내면 다시 골라내도록 구현

--- src/main/java/baseball/domain/RandomNumbers.java
+++ src/main/java/baseball/domain/RandomNumbers.java
@@ -10,11 +10,16 @@ public class RandomNumbers {
     private static final int LOWER_BOUNDARY = 0;
     private static final int UPPER_BOUNDARY = 9;
 
-    private final List<Character> intToCharacterArray = new ArrayList<>();
+    private final List<Integer> intToCharacterArray = new ArrayList<>();
 
-    public List<Character> getRandomNumbers() {
+    public List<Integer> getRandomNumbers() {
         for (int i = 0; i < LIST_SIZE; i++) {
-            intToCharacterArray.add((char) RandomUtils.nextInt(LOWER_BOUNDARY, UPPER_BOUNDARY));
+            int newNumber = RandomUtils.nextInt(LOWER_BOUNDARY, UPPER_BOUNDARY);
+            if(intToCharacterArray.stream().anyMatch(integer -> integer.equals(newNumber))){
+                i--;
+            } else{
+                intToCharacterArray.add(newNumber);
+            }
         }
         return intToCharacterArray;
     }
