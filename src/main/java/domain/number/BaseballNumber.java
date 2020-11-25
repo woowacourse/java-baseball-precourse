96aed3af5bae1fd84d7be53c50bbde35463253cd
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-25 22:55:34 +0900

    refactor : add cache map and static factory function
    Decide to use cache and return proper value from map.
      - baseballValue is always ranged from 1 to 9.
      - Don't need to construct object everytime call happens.
      - Had better use stored object.

--- src/main/java/domain/number/BaseballNumber.java
+++ src/main/java/domain/number/BaseballNumber.java
@@ -1,6 +1,16 @@
 package domain.number;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BaseballNumber {
+    private static final Map<Integer, BaseballNumber> CACHE = new HashMap<>();
+
+    static {
+        for (int i = 1; i <= 9; i++) {
+            CACHE.put(i, new BaseballNumber(i));
+        }
+    }
 
     private final int baseballNumber;
 
@@ -9,5 +19,11 @@ public class BaseballNumber {
             throw new IllegalArgumentException();
         }
         this.baseballNumber = baseballNumber;
+    }
+
+    public static BaseballNumber of(int baseballNumber) {
+        return CACHE.computeIfAbsent(baseballNumber, key -> {
+            throw new IllegalArgumentException();
+        });
     }
 }
