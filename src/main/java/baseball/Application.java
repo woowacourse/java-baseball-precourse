718896ac8087e0ab1f7c47ccb309af4bb586b74c
taelee <taelee@student.42seoul.kr>
2020-11-25 23:06:41 +0900

    feature(Application): add the feature that can enter input data from user

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,22 @@
 package baseball;
-
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        boolean playing = true;
+
+        Integer number;
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        while (playing) {
+            System.out.print("숫자를 입력해주세요 : ");
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                System.out.println("입력숫자: " + number);
+            } else {
+                System.out.println("숫자를 입력해주세요 : ");
+            }
+        }
     }
+
 }
