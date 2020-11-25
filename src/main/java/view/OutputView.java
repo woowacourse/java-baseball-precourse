0044f3cd1f8d93459b648bc0f118de7167d0ab5d
lns13301 <rkdrbgks22@ks.ac.kr>
2020-11-25 18:03:31 +0900

    Refactor: OutputView 출력 문구 조정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,24 +1,26 @@
 package view;
 
 public class OutputView {
+    private static final String NEW_LINE = "\n";
+
     public static void printInputNumber() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print(NEW_LINE + "숫자를 입력해주세요 : ");
     }
 
     public static void printResultBallCount(int ball) {
-        System.out.print(ball + "볼");
+        System.out.print(ball + "볼 ");
     }
 
     public static void printResultStrikeCount(int strike) {
-        System.out.println(strike + "스트라이크");
+        System.out.print(strike + "스트라이크");
     }
 
     public static void printResultNothing() {
-        System.out.println("낫싱");
+        System.out.print("낫싱");
     }
     
     public static void printGameEnd() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(NEW_LINE + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
