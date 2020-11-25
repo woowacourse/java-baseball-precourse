83e1a1548e11a776beebca793fe2e5d042556201
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-25 22:44:23 +0900

    test : add BaseballNumber constructor fail test
    BaseballNumber object can't be generated if paramter is not ranged
    1 to 9.

--- src/test/java/domain/number/BaseballNumberTest.java
+++ src/test/java/domain/number/BaseballNumberTest.java
@@ -5,15 +5,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BaseballNumberTest {
 
-    @DisplayName("1 - 9 범위의 값에서만 BaseballNumber 객체가 정상 생성된다")
+    @DisplayName("1 - 9 범위의 값에서만 BaseballNumber 객체가 정상 생성")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9})
     public void BaseballNumber_객체_정상_생성(int baseballNumber) {
         assertThatCode(() -> {
             new BaseballNumber(baseballNumber);
         }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("1 - 9 범위가 아닌 경우, BaseballNumber 객체 생성 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 10})
+    public void BaseballNumber_객체_생성_예외_발생(int baseballNumber) {
+        assertThatThrownBy(() -> {
+            new BaseballNumber(baseballNumber);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
