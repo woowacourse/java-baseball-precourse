b8528d5d36076dd5fe47995c1092f20f2314cceb
김주원 <kjw11077@naver.com>
2020-11-25 17:32:43 +0900

    feat: update Ball
    equals 메소드로 공 숫자 비교하도록 함

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -13,4 +13,15 @@ public class Ball {
             throw new IllegalArgumentException("공의 숫자는 1~9의 자연수만 가능합니다.");
         }
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        Ball ball;
+        if (obj instanceof Ball) {
+            ball = (Ball) obj;
+        } else {
+            throw new ClassCastException("Ball 객체 끼리만 비교가 가능합니다.");
+        }
+        return this.number == ball.number;
+    }
 }
