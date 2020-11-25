7d3c9bc1e1ceac365e68e04985c39d04c5339df6
sakjung <58401309+sakjung@users.noreply.github.com>
2020-11-25 18:40:36 +0900

    feat: 랜덤 3자리 숫자 생성기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,18 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+        StringBuilder randomNum = new StringBuilder();
+        for (int i = 0; i<3; i++) {
+            randomNum.append(RandomUtils.nextInt(1, 9));
+        }
+        int answer = Integer.parseInt(randomNum.toString());
+        System.out.println(answer);
     }
 }
