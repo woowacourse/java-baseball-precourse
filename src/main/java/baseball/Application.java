ee8ed5b2aae83a36b4392b8a37deca81ea6cc268
soongjamm <kimstz0303@gmail.com>
2020-11-26 02:16:17 +0900

    feat(baseball): add exceptions for abnormal inputs

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,16 +25,41 @@ public class Application {
         return inputNumbers;
     }
 
+    private static boolean isInputAcceptable(String inputString) {
+
+        try {
+            Integer.parseInt(inputString);
+            if (inputString.length() != NUMBERS_LENGTH) {
+                throw new IllegalArgumentException();
+            }
+            if (inputString.contains("0")) {
+                throw new IllegalArgumentException();
+            }
+            return true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("비정상적인 입력입니다.");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
-        // 3자리 랜덤 숫자 생성
-        int[] answer = generateRandomNumbers();
-        // 3자리 숫자 입력 받기
-        System.out.println("숫자를 입력해주세요 : ");
-        String inputString = scanner.nextLine();
-        int[] inputNumbers = parseInputNumbers(inputString);
+        boolean gameStart = true;
+        while (gameStart) {
+
+            int[] answer = generateRandomNumbers();
+            int[] inputNumbers = null;
+            String inputString = "";
+
+            do {
+                System.out.println("숫자를 입력해주세요 : ");
+                inputString = scanner.nextLine();
+            } while (!isInputAcceptable(inputString));
+            inputNumbers = parseInputNumbers(inputString);
+            
+        }
 
     }
 }
