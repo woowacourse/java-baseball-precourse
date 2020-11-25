a0bfc1d1d10fe8728c67e8d5752e1628bb7ffda0
김주원 <kjw11077@naver.com>
2020-11-26 00:03:00 +0900

    feat(Balls): add class Balls
    Ball을 담고 있는 일급컬렉션

---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,55 @@
+package baseball;
+
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Balls {
+    private final List<Ball> balls;
+    public static final int COUNT_OF_BALLS = 3;
+
+    private Balls(List<Ball> balls) {
+        validateSize(balls);
+        validateDuplicate(balls);
+        this.balls = balls;
+    }
+
+    private void validateSize(List<Ball> balls) {
+        if (balls.size() != COUNT_OF_BALLS) {
+            String msg = String.format("공의 개수는 %d개 여야 합니다.", COUNT_OF_BALLS);
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+    private void validateDuplicate(List<Ball> balls) {
+        Set<Ball> ballSet = new HashSet<>(balls);
+        if (ballSet.size() != COUNT_OF_BALLS) {
+            String msg = "공들의 숫자는 중복되지 않아야 합니다.";
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+    public static Balls generateRandomBalls() {
+        List<Ball> balls = new ArrayList<>();
+        Ball ball;
+        int randomNumber;
+
+        while (balls.size() != COUNT_OF_BALLS) {
+            randomNumber = RandomUtils.nextInt(1, 9);
+            ball = new Ball(randomNumber);
+
+            if (!balls.contains(ball)) {
+                balls.add(ball);
+            }
+        }
+        return new Balls(balls);
+    }
+
+    public Boolean compare() {
+        // TODO Balls 끼리 비교
+        return true;
+    }
+}
