8f816169a154eaea9c43dd6bf50c76df1507d40b
SB <woltaryksb1@gmail.com>
2020-11-25 20:26:54 +0900

    feat: 랜덤 난수 생성 기능 추가

---
+++ src/main/java/controller/RandomVendor.java
@@ -0,0 +1,53 @@
+package controller;
+
+import utils.RandomUtils;
+
+import java.util.Arrays;
+
+public class RandomVendor {
+
+    private static final int START_NUMBER = 100;
+    private static final int END_NUMBER = 999;
+
+    private static boolean[] visited;
+
+    public RandomVendor() {
+        this.visited = new boolean['9' + 1];
+    }
+
+    public int roll() {
+
+        int num = getNumber();
+
+        while(!checkVaild(num)) {
+            num = getNumber();
+        }
+
+        return num;
+    }
+
+    private int getNumber() {
+        return RandomUtils.nextInt(START_NUMBER, END_NUMBER);
+    }
+
+    private boolean checkVaild(int num) {
+
+        initVisited();
+
+        while(num != 0) {
+            if(visited[num % 10]) {
+                return false;
+            }
+
+            visited[num % 10] = true;
+            num /= 10;
+        }
+
+        return true;
+    }
+
+    private void initVisited() {
+        Arrays.fill(visited, false);
+    }
+
+}
