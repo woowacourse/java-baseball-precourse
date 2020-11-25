0094e570a072a62dae90bbac8880ad8f0d5b9184
doorisopen <audauddl2@gmail.com>
2020-11-26 01:21:20 +0900

    feat: 메인 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,17 @@ package baseball;
 
 import java.util.Scanner;
 
+import static baseball.domain.Number.GAME_START;
+
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
         // TODO 구현 진행
+        BaseBallGame baseBallGame = new BaseBallGame();
+        int gameStatus = GAME_START;
+        while(gameStatus == GAME_START) {
+            gameStatus = baseBallGame.play(scanner);
+        }
     }
 }
