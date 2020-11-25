3472afb66deb65788fce8d520d659349527d7dbe
lns13301 <rkdrbgks22@ks.ac.kr>
2020-11-25 16:43:11 +0900

    feat: 플레이어에게 세 자리 수를 입력 받아 값을 검사하고 값을 대입하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,38 @@
 package baseball;
 
+import view.OutputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        int playerValue = 0;
 
         // TODO 구현 진행
+        OutputView.printInputNumber();
+        playerValue = inputPlayerNumber(scanner.next());
+    }
+
+    public static int inputPlayerNumber(String scanner) {
+        try {
+            int value = Integer.parseInt(scanner);
+            System.out.println(value);
+            if (value < 100 || value > 999) {
+                throw new IllegalArgumentException();
+            }
+
+            if (isAnyMatchNumbers(value)) {
+                throw new IllegalArgumentException();
+            }
+
+            return value;
+        } catch (NumberFormatException e) {
+            return inputPlayerNumber(scanner);
+        }
+    }
+
+    public static boolean isAnyMatchNumbers(int number) {
+        return false;
     }
 }
