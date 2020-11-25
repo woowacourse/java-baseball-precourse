6d9c9f8ff39db168c41ffaf46fa3c89a320645af
Shion <ehdrhelr@gmail.com>
2020-11-25 17:47:34 +0900

    feat : add func to input player number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,14 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        String playerNumber = inputPlayerNumber(scanner);
+
+    }
+
+    public static String inputPlayerNumber(Scanner scanner) {
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerNumber = scanner.nextLine();
+
+        return playerNumber;
     }
 }
