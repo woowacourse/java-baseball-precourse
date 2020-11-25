9c056687e5c2d6bd63888fd5ee38c8a77fab468b
sakjung <58401309+sakjung@users.noreply.github.com>
2020-11-25 22:42:46 +0900

    feat: 랜덤 3자리 숫자 제너레이터 (정답 생성) 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,16 +2,18 @@ package baseball;
 
 import java.util.Scanner;
 import utils.RandomUtils;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Application {
 
-    public static int guessNum (Scanner scanner) {
-        System.out.print("숫자를 입력해 주세요: ");
+    public static int guessNum(Scanner scanner) {
+        System.out.print("숫자를 입력해 주세요 : ");
         if (!scanner.hasNextInt()) {
             throw new IllegalArgumentException("정수가 아닙니다");
         }
-        int input = scanner.nextInt();
 
+        int input = scanner.nextInt();
         if (input < 0) {
             throw new IllegalArgumentException("음수는 입력할 수 없습니다");
         }
@@ -19,20 +21,42 @@ public class Application {
         if (input < 100 || 1000 <= input) {
             throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다");
         }
-
         return input;
     }
+    public static int[] intToArray(int num) {
+        int[] this_array = new int[3];
+        for (int i = 2; i > -1; i--) {
+            this_array[i] = num % 10;
+            num %= 10;
+        }
+        return this_array;
+    }
+
+    public static boolean isValidateRandomNum(int candidate) {
+        Set<Integer> store = new HashSet<>();
+
+        int[] candidate_array = intToArray(candidate);
+        for (int i : candidate_array) {
+            if (store.contains(i)) return false;
+            store.add(i);
+        }
+        return true;
+    }
+
+//    public static boolean compareGuessAndAnswer(int num) {
+//        int[] guessArray = intToArray(num);
+//
+//    }
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
         // 3자리 숫자 생성
-        StringBuilder randomNum = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
-            randomNum.append(RandomUtils.nextInt(1, 9));
-        }
-        int answer = Integer.parseInt(randomNum.toString());
+        int answer;
+        do {
+            answer = RandomUtils.nextInt(100, 999);
+        } while (isValidateRandomNum(answer));
         System.out.println(answer);
 
         int number;
