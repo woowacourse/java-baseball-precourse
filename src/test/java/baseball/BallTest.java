94c61167c8d7451fdf14354f8a5232eaea9f6f2e
김주원 <kjw11077@naver.com>
2020-11-25 17:33:26 +0900

    feat: update BallTest
    Ball 생성자 단위 테스트

---
+++ src/test/java/baseball/BallTest.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallTest {
+
+    @Test
+    public void 정상범위보다_작은_경우() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                ()-> new Ball(-1));
+    }
+
+    @Test
+    public void 정상범위보다_큰_경우() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                ()-> new Ball(10));
+    }
+
+    @Test
+    public void 정상적인_입력인_경우() {
+        assertDoesNotThrow(()-> new Ball(9));
+        assertDoesNotThrow(()-> new Ball(1));
+        assertDoesNotThrow(()-> new Ball(5));
+    }
+}
