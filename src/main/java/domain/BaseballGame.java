1b2f72eeb3bfad32e450e160cee3f798bef37116
ecsimsw <ecsimsw@gmail.com>
2020-11-26 02:04:45 +0900

    feat : 사용자 추측 숫자 입력 구현

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -1,4 +1,17 @@
 package domain;
 
+import view.InputView;
+
+import java.util.Scanner;
+
 public class BaseballGame {
+    private final Scanner SCANNER;
+
+    public BaseballGame(Scanner SCANNER){
+        this.SCANNER = SCANNER;
+    }
+
+    public void play(){
+        InputView.askGuessNum(SCANNER);
+    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,20 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final String ASK_NUMBER_MSG = "숫자를 입력해주세요 : ";
+
+    public static String askGuessNum(Scanner SCANNER){
+        printAskNumberMsg();
+        return readInputLine(SCANNER);
+    }
+
+    private static void printAskNumberMsg(){
+        System.out.println(ASK_NUMBER_MSG);
+    }
+
+    private static String readInputLine(Scanner SCANNER){
+        return SCANNER.nextLine();
+    }
 }
