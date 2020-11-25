e1fac3028a90609ad9154129cac6b37d191d71d5
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-25 22:40:56 +0900

    feat : add BaseballNumber constructor
    The constructor has only one int value parameter.

---
+++ src/main/java/domain/number/BaseballNumber.java
@@ -0,0 +1,10 @@
+package domain.number;
+
+public class BaseballNumber {
+
+    private final int baseballNumber;
+
+    public BaseballNumber(int baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+}
