f47210d3957eb74172b000382a50473d3b2d8025
Seonho Kim <zxzx235@naver.com>
2020-11-25 19:00:39 +0900

    feat: add setTarget
    임의의 3자리 수를 설정하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,20 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        System.out.println("Hello Woowacourse!");
+        final int TARGET_LENGTH = 3;
+        int[] target = setTarget(TARGET_LENGTH);
+
+        System.out.print(target[0]);
+        System.out.print(target[1]);
+        System.out.println(target[2]);
+
+    }
+
+    public static int[] setTarget(int targetLength) {
+        int[] target = new int[targetLength];
+        for (int i = 0; i < 3; i++) {
+            target[i] = utils.RandomUtils.nextInt(1, 9);
+        }
+        return target;
     }
 }
