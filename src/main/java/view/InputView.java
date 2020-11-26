d5a45b312cc899ede2086cc26e6c05db7403f397
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-26 19:14:53 +0900

    refactor: InputView에 있던 출력 코드 OutputView로 이동

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,7 @@ import utils.InputValidator;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
 
     private static Scanner scanner = new Scanner(System.in);
 
@@ -14,14 +13,14 @@ public class InputView {
         this.scanner = scanner;
     }
 
-    public static String inputUserNumbers() {
+    public static String inputPlayerNumbers() {
         try {
-            System.out.print(INPUT_NUMBER_MESSAGE);
+            OutputView.printInputMessage();
             String userNumbers = scanner.nextLine();
             InputValidator.validateNumbers(userNumbers);
             return userNumbers;
         }catch (IllegalArgumentException e) {
-            return inputUserNumbers();
+            return inputPlayerNumbers();
         }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,29 +8,34 @@ public class OutputView {
     private static final String NOTHING_MESSAGE = "낫싱";
     private static final String CORRECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
-    public void printStrike(int strikeCount) {
+    public static void printStrike(int strikeCount) {
         System.out.printf(STRIKE_MESSAGE, strikeCount);
     }
 
-    public void printBall(int ballCount) {
+    public static void printBall(int ballCount) {
         System.out.printf(BALL_MESSAGE, ballCount);
     }
 
-    public void printStrikeBall(int strikeCount, int ballCount) {
+    public static void printStrikeBall(int strikeCount, int ballCount) {
         System.out.printf(STRIKE_BALL_MESSAGE, strikeCount, ballCount);
     }
 
-    public void printNothing() {
+    public static void printNothing() {
         System.out.println(NOTHING_MESSAGE);
     }
 
-    public void printCorrectAnswer() {
+    public static void printCorrectAnswer() {
         System.out.println(CORRECT_ANSWER_MESSAGE);
     }
 
-    public void printRestartEnd() {
+    public static void printRestartEnd() {
         System.out.println(RESTART_OR_END_MESSAGE);
+    }
+
+    public static void printInputMessage() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
 }
