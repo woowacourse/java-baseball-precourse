7cd9122fefabd544aae3e024dc1b2bc9d161844a
Minjeong <mathmjseo@naver.com>
2020-11-27 00:21:44 +0900

    feat: 플레이어에게 세 자리 수를 입력받는 메서드 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,9 +1,28 @@
 package baseball;
 
 
+import java.util.Scanner;
+
 public class Player {
+    boolean[] checkNumberDuplicated = new boolean[Constants.MAX_RANGE];
+    int[] numbers;
 
     public void Player(){
 
+    }
+
+    public void inputThreeNumbers(){
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("숫자를 입력해주세요 : ");
+            String input = sc.nextLine();
+            if(true){ //true 대신 유효성 검증 부분 추가
+                throw new IllegalArgumentException("1~9 사이의 수로 이루어진 서로 다른 세 자리 수만 입력 가능합니다.");
+            } else {
+                // input을 Integer Array로 변경하는 로직 추가
+                break;
+            }
+        }
     }
 }
