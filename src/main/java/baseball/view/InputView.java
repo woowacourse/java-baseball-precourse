767d54ca0891290dd7fdb6e9a95dec5fa37966e2
operasystem6 <operasystem6@naver.com>
2020-11-26 20:55:29 +0900

    feat: 플레이어 입력 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String PLAY_OR_NOT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ";
+
+    public static String inputPlayerNumber(Scanner scanner) {
+        System.out.print(INPUT_MESSAGE);
+        return scanner.next();
+    }
+
+    public static int inputPlayOrNotNumber(Scanner scanner) {
+        System.out.println(PLAY_OR_NOT_MESSAGE);
+        return scanner.nextInt();
+    }
+}
