134194131d020fae9bdcf176e2f5ad2eb1d3f488
김경록 <goodboy3421@gmail.com>
2020-11-27 06:38:59 +0900

    feat: main 함수에 BaseballGame 인스턴스 생성
    Controller 역할을 하는 Application의 main함수에 BaseballGame을 실행하기 위해 인스턴스들을 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,28 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+import baseball.domain.BaseballNumbers;
+import baseball.domain.DefaultBaseballNumbersGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        BaseballGame baseballGame = BaseballGame.play(new DefaultBaseballNumbersGenerator());
+        while (baseballGame.isPlaying()) {
+            BaseballNumbers baseballNumbers = InputView.inputBaseballNumbers(scanner);
+            int strikeNumber = baseballGame.countStrike(baseballNumbers);
+            int ballNumber = baseballGame.countBall(baseballNumbers);
+            OutputView.printHint(strikeNumber, ballNumber);
+
+            if (baseballGame.isCorrectTo(baseballNumbers)) {
+                OutputView.printEndGame();
+                baseballGame.restart(InputView.inputRestartOrStopRequest(scanner), new DefaultBaseballNumbersGenerator());
+            }
+        }
     }
 }
