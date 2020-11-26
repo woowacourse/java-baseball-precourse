0ce83ef25757445a0461f07674f3312238a4c0f7
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-26 22:56:29 +0900

    feat: 재시작 또는 종료 번호 입력하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 
 public class InputView {
 
-
+    private static final String INCORRECT_NUMBER_ERROR = "올바른 숫자를 입력해주세요.";
+    private static final String INCORRECT_MENU_NUM_ERROR = "1번 또는 2번을 입력해주세요.";
+    private static final String RESTART = "1";
+    private static final String END = "2";
     private static Scanner scanner = new Scanner(System.in);
 
     public InputView(Scanner scanner) {
@@ -16,11 +19,26 @@ public class InputView {
     public static String inputPlayerNumbers() {
         try {
             OutputView.printInputMessage();
-            String userNumbers = scanner.nextLine();
-            InputValidator.validateNumbers(userNumbers);
-            return userNumbers;
+            String playerNumbers = scanner.nextLine();
+            int tmpNumbers = Integer.parseInt(playerNumbers);
+            InputValidator.validateNumbers(playerNumbers);
+            return String.valueOf(playerNumbers);
         }catch (IllegalArgumentException e) {
+            e = new IllegalArgumentException(INCORRECT_NUMBER_ERROR);
             return inputPlayerNumbers();
+        }
+    }
+
+    public static String inputRestartOrEnd() {
+        try {
+            OutputView.printRestartEnd();
+            String selectMenuNum = scanner.nextLine();
+            if(selectMenuNum.equals(RESTART) || selectMenuNum.equals(END)) {
+                return selectMenuNum;
+            }
+            throw new IllegalArgumentException(INCORRECT_MENU_NUM_ERROR);
+        } catch (IllegalArgumentException e) {
+            return inputRestartOrEnd();
         }
     }
 }
