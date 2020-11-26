bef7250b6339590810e2090d8ac814fcf1f37d9e
platanus <platanus@canxan.com>
2020-11-26 23:47:27 +0900

    feat(baseball): Add method to compare values between two arrays

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
@@ -22,13 +23,17 @@ public class Application {
         // 입출력 검증 테스트용
         System.out.println(Arrays.toString(inputNumber));
         System.out.println(Arrays.toString(randomNumber));
+
+        diffArray(inputNumber, randomNumber);
     }
-    public static void checkInputNumber(int numbers){
-        if(numbers < 100 || numbers > 1000) {
+
+    public static void checkInputNumber(int numbers) {
+        if (numbers < 100 || numbers > 1000) {
             throw new IllegalArgumentException("3자리 수가 아닙니다.");
         }
     }
-    public static int[] createInputNumber(int numbers){
+
+    public static int[] createInputNumber(int numbers) {
         String temp = Integer.toString(numbers);
         int[] inputNumberArray = new int[temp.length()];
         for (int i = 0; i < temp.length(); i++) {
@@ -36,11 +41,47 @@ public class Application {
         }
         return inputNumberArray;
     }
+
     public static int[] createRandomNumber() {
         int[] randomNumberArray = new int[3];
         for (int i = 0; i < 3; i++) {
             randomNumberArray[i] = RandomUtils.nextInt(1, 9);
         }
         return randomNumberArray;
+    }
+
+    public static boolean diffArray(int[] user, int[] random) {
+        boolean gameResult = false;
+        int strikeCount = 0;
+        int ballCount = 0;
+        for (int i = 0; i < user.length; i++) {
+            for (int j = 0; j < random.length; j++) {
+                if (user[i] == random[j] && i == j) {
+                    strikeCount++;
+                }
+                if (user[i] == random[j]) {
+                    ballCount++;
+                }
+            }
+        }
+        if (strikeCount >= 3) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        if (strikeCount > 0 && ballCount > 0 && strikeCount != ballCount) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        if (strikeCount > 0 && ballCount <= 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        if (ballCount > 0 && strikeCount <= 0) {
+            System.out.println(ballCount + "볼");
+        }
+        if (ballCount <= 0 && strikeCount <= 0) {
+            System.out.println("낫싱");
+        }
+        if (strikeCount >= 3) {
+            gameResult = true;
+        }
+        return gameResult;
     }
 }
