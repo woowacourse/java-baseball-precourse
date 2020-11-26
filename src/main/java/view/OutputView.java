d84d4662be911a5fad80237dec53fc2866901781
opdshe <opdshe@naver.com>
2020-11-26 22:58:26 +0900

    feat: 점수(볼카운트) 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,39 @@
 package view;
 
+import domain.Score;
+
 public class OutputView {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String SPACE = " ";
+    private static final int ZERO = 0;
+
     private OutputView() {
+    }
+
+    public static void printScore(Score score) {
+        if (score.getStrike() == ZERO && score.getBall() == ZERO) {
+            printMessageAndNewLine(NOTHING);
+        }
+        String message = getBallMessage(score) + getStrikeMessage(score);
+        printMessageAndNewLine(message);
+    }
+
+    private static String getBallMessage(Score score) {
+        String message = "";
+        if (score.getBall() > 0) {
+            message += score.getBall() + BALL + SPACE;
+        }
+        return message;
+    }
+
+    private static String getStrikeMessage(Score score) {
+        String message = "";
+        if (score.getStrike() > 0) {
+            message += score.getStrike() + STRIKE;
+        }
+        return message;
     }
 
     public static void printMessage(String message) {
