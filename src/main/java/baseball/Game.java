45fc27562459ca08d969679bf3c9b4a086c705fc
bepoz <kangsy763@naver.com>
2020-11-26 15:42:07 +0900

    refactor: gameStart() method refactoring process
    baseball game should be able to continue if client want.
    So, add do-while and game steps method has been designed.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,11 +4,19 @@ import java.util.Scanner;
 
 public class Game {
     public static void gameStart(int tartgetValue, Scanner scanner){
-        int input = valueInsert(scanner);
+        boolean flag = false;
+        do{
+            int input = valueInsert(scanner);
+            if (checking(tartgetValue)) {
+                flag = gameContinue();
+            } else {
+                hint(tartgetValue);
+            }
+        } while(!flag);
     }
 
     private static int valueInsert(Scanner scanner) {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
         return scanner.nextInt();
     }
 }
