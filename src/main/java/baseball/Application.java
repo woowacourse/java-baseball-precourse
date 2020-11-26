429b18f87bbd36660e1ab8f99e0e8d3f8a396000
Wannte <T.wannte@gmail.com>
2020-11-26 22:25:10 +0900

    feat(Application): add scanner exception handling
    scanner.nextInt() vs scanner.nextLine()
    -> scanner.nextLine()
    multiple input with space have potential risk like below example.
    ex) 12 32 aa
    
    String.strip() method for handling unexpected space perfix or suffix.
    
    Two case for exception handling implemented.
    1. Not with one Integer.
    2. Not with Three Distinct Digits Integer.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import utils.ThreeDigitsUtils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
@@ -9,5 +10,24 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         int targetNumber = ThreeDigitsUtils.generateBaseballNumber();
         System.out.println(targetNumber);
+
+        while (true){
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputString = scanner.nextLine();
+            try {
+                inputString = inputString.strip();
+                int inputNumber = Integer.parseInt(inputString);
+                if (ThreeDigitsUtils.isDistinctThreeDigits(inputNumber)){
+                    System.out.println(inputNumber);
+                } else {
+                    throw new IllegalArgumentException("Input should be Distinct Three Digits Integer");
+//                    System.out.println("Not Distinct Three Digits Number: " + inputNumber);
+                }
+            }
+            catch (NumberFormatException e){
+                throw new IllegalArgumentException("Input should be Integer");
+//                System.out.println(e + " " + inputString);
+            }
+        }
     }
 }
--- src/main/java/utils/ThreeDigitsUtils.java
+++ src/main/java/utils/ThreeDigitsUtils.java
@@ -4,7 +4,14 @@ public class ThreeDigitsUtils {
     private ThreeDigitsUtils() {
     }
 
-    private static boolean isDistinctDigit(int number) {
+    private static boolean isThreeDigits(int number) {
+        return (100 <= number && number < 1000);
+    }
+
+    public static boolean isDistinctThreeDigits(int number) {
+        if (!isThreeDigits(number)){
+            return false;
+        }
         int a = number / 100;
         int b = (number % 100) / 10;
         int c = number % 10;
@@ -17,7 +24,7 @@ public class ThreeDigitsUtils {
     public static int generateBaseballNumber() {
         while (true){
             int number = RandomUtils.nextInt(100, 999);
-            if (isDistinctDigit(number)){
+            if (isDistinctThreeDigits(number)){
                 return number;
             }
         }
