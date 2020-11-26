8201fac1ea578c44b165e5fd9b8df0c7a6a7a028
RinSabbit <48675973+RinSabbit@users.noreply.github.com>
2020-11-26 22:22:30 +0900

    refactor: 상수를 전역변수로 변경

--- src/main/java/utils/CheckDuplicate.java
+++ src/main/java/utils/CheckDuplicate.java
@@ -1,22 +1,24 @@
-package playBaseBall;
-import java.util.HashSet;
+package utils;
 
+import java.util.HashSet;
 
 /**
  * 생성된 난수에 중복된 숫자가 있는지 검사하는 클래스
  */
 public class CheckDuplicate {
 	
+	final int TRUE_SIZE = 3;
+	
 	/**
 	 * 중복된 숫자가 없으면 생성된 난수를 반환한다
 	 * @return int
 	 */
-	public static int makeRandomNumber() {
+	public String makeRandomNumber() {
 		int randomNumber = RandomUtils.nextInt(100, 999);
 		if(isDuplicate(randomNumber)) {
 			return makeRandomNumber();
 		}
-		return randomNumber;
+		return String.valueOf(randomNumber);
 	}
 
 	/**
@@ -24,15 +26,14 @@ public class CheckDuplicate {
 	 * @param randomNum
 	 * @return boolean
 	 */
-	private static boolean isDuplicate(int randomNum) {
+	private boolean isDuplicate(int randomNum) {
 		HashSet<Integer> set = new HashSet<Integer>();
 		
 		for(int i=0; i<3; i++) {
 			set.add(randomNum % 10);
 			randomNum /= 10;
 		}
-		
-		if(set.size()==3) {
+		if(set.size()==TRUE_SIZE) {
 			return false;
 		}
 		return true;
