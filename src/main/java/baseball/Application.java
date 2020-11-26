f275c7f2610ec9061cf3f77aa53a2e0557cb1429
Seonho Kim <zxzx235@naver.com>
2020-11-26 11:34:00 +0900

    feat: add getInputNumber
    3자리 수를 입력받는 기능 추가.
    예외 입력시 IllegalArgumentException 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,16 +11,20 @@ public class Application {
 
         int[] target = setTarget();
         int[] inputNumber = new int[TARGET_LENGTH];
+        int[] result = new int[2];
+
+        System.out.println(target[0]);
 
         try {
             inputNumber = getInputNumber(scanner);
         } catch (IllegalArgumentException e) {
-            System.out.println("오류 : 0을 제외한 3자리 숫자만 입력 가능!");
+            System.out.println("오류 : 0을 제외한 서로 다른 3자리 숫자만 입력 가능!");
         }
 
-        System.out.println(inputNumber[2]);
+        result = compare(target, inputNumber);
 
-
+        System.out.println(result[0]);
+        System.out.println(result[1]);
     }
 
     public static int[] setTarget() {
@@ -33,7 +37,7 @@ public class Application {
 
     public static int[] getInputNumber(Scanner scanner) throws IllegalArgumentException {
         int[] inputNumber = new int[TARGET_LENGTH];
-        System.out.println("숫자를 입력하세요: ");
+        System.out.println("숫자를 입력하세요(0을 제외한 서로 다른 3자리 숫자): ");
         String input = scanner.next();
 
         if (input.length() != TARGET_LENGTH) {
@@ -48,6 +52,44 @@ public class Application {
             }
         }
 
+        for (int i = 0; i < TARGET_LENGTH; i++) {
+            int temp = isThereNumber(inputNumber, inputNumber[i]);
+            if (temp > 1) {
+                throw new IllegalArgumentException();
+            }
+        }
+
         return inputNumber;
+    }
+
+    public static int[] compare(int[] target, int[] inputNumber) {
+        int[] compareResult = new int[2];
+        int ballAndStrike = 0;
+        int strike = 0;
+        int ball = ballAndStrike - strike;
+        for (int i = 0; i < TARGET_LENGTH; i++) {
+            int temp = isThereNumber(target, inputNumber[i]);
+            ballAndStrike = ballAndStrike + temp;
+        }
+        for (int i = 0; i < TARGET_LENGTH; i++) {
+            if (target[i] == inputNumber[i]) {
+                strike++;
+            }
+        }
+
+        compareResult[0] = ball;
+        compareResult[1] = strike;
+
+        return compareResult;
+    }
+
+    public static int isThereNumber(int[] target, int anInputNumber) {
+        int result = 0;
+        for (int i = 0; i < TARGET_LENGTH; i++) {
+            if (target[i] == anInputNumber) {
+                result++;
+            }
+        }
+        return result;
     }
 }
