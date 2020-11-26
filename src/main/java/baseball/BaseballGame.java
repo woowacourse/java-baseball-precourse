a7025dc986edb6fe10055c39e0fe581738b1aefb
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-11-26 20:52:33 +0900

    feat 플레이어에게 숫자를 입력 받는다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -17,8 +17,14 @@ public class BaseballGame {
         }
     }
 
-    public void game(){
+    public void game() {
         List<String> ballNumberList = ballNumbers();
+        int ballCounter = 0;
+        int strikeCounter = 0;
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerBallNumber = scanner.nextLine();
+
+        List<String> playerBallSplitNumberList = playerBallNumber(playerBallNumber);
     }
 
     public List<String> ballNumbers() {
@@ -27,5 +33,14 @@ public class BaseballGame {
             set.add(String.valueOf(RandomUtils.nextInt(1, 9)));
         }
         return new ArrayList<>(set);
+    }
+
+    public List<String> playerBallNumber(String playerBallNumber) {
+        String[] playerBallSplitNumber = playerBallNumber.split("");
+        List<String> playerBallSplitNumberList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            playerBallSplitNumberList.add(playerBallSplitNumber[i]);
+        }
+        return playerBallSplitNumberList;
     }
 }
