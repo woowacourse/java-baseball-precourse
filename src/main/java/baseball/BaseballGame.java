069b7d3fa0e0c02e521b5fa131d4714ebcb50629
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-11-26 20:55:37 +0900

    feat 두 번호를 맞추어 볼, 스트라이크 카운트를 매긴다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -25,6 +25,9 @@ public class BaseballGame {
         String playerBallNumber = scanner.nextLine();
 
         List<String> playerBallSplitNumberList = playerBallNumber(playerBallNumber);
+
+        strikeCounter = strikeCompare(ballNumberList, playerBallSplitNumberList);
+        ballCounter = ballCompare(ballNumberList, playerBallSplitNumberList);
     }
 
     public List<String> ballNumbers() {
@@ -42,5 +45,30 @@ public class BaseballGame {
             playerBallSplitNumberList.add(playerBallSplitNumber[i]);
         }
         return playerBallSplitNumberList;
+    }
+
+    public int strikeCompare(List<String> ballNumberList, List<String> playerBallSplitNumberList) {
+        int strikeCounter = 0;
+
+        for (int i = 0; i < ballNumberList.size(); i++) {
+            if (ballNumberList.get(i).equals(playerBallSplitNumberList.get(i)))
+                strikeCounter += 1;
+        }
+        return strikeCounter;
+    }
+
+    public int ballCompare(List<String> ballNumberList, List<String> playerBallSplitNumberList) {
+        int ballCounter = 0;
+        for (int answerIndex = 0; answerIndex < 3; answerIndex++) {
+            for (int playerIndex = 0; playerIndex < 3; playerIndex++) {
+                if (answerIndex == playerIndex)
+                    continue;
+                else {
+                    if (ballNumberList.get(answerIndex).equals(playerBallSplitNumberList.get(playerIndex)))
+                        ballCounter += 1;
+                }
+            }
+        }
+        return ballCounter;
     }
 }
