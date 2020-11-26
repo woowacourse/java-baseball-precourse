bb2c5eeb36dee257d405e6f64da98c3320595d02
bluewood <bluewood.truth@gmail.com>
2020-11-26 22:38:28 +0900

    feat: 세 자리 숫자를 생성하는 generateNumber() 메서드 작성

--- src/main/java/modules/NumberGenerator.java
+++ src/main/java/modules/NumberGenerator.java
@@ -1,11 +1,15 @@
 package modules;
 
 import java.util.ArrayList;
+import utils.RandomUtils;
 
 public class NumberGenerator {
-    private ArrayList<Integer> numberRange;
     static final int NUMBER_RANGE_MIN = 1;
     static final int NUMBER_RANGE_MAX = 9;
+    static final int NUMBER_SIZE = 3;
+
+    private ArrayList<Integer> numberRange;
+    private int[] generatedNumber;
     
     private void initializeNumberRange(){
         numberRange = new ArrayList<Integer>();
@@ -13,5 +17,23 @@ public class NumberGenerator {
         for(int i = NUMBER_RANGE_MIN; i <= NUMBER_RANGE_MAX; i++){
             numberRange.add(i);
         }
+    }
+
+    private void generateNumber(){
+        generatedNumber = new int[NUMBER_SIZE];
+        for(int i = 0; i < generatedNumber.length; i++){
+            generatedNumber[i] = popFromNumberRange();
+        }
+    }
+
+    private int popFromNumberRange(){
+        int index = getRandomIndexOfNumberRange();
+        int popValue = numberRange.get(index);
+        numberRange.remove(index);
+        return popValue;
+    }
+
+    private int getRandomIndexOfNumberRange(){
+        return RandomUtils.nextInt(0, numberRange.size() - 1);
     }
 }
