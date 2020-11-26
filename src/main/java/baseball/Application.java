e233e7339c67d3683ebdd67607ff59c073f86de5
483759 <483759@naver.com>
2020-11-26 16:13:04 +0900

    숫자를 입력하라는 안내 메시지를 출력하는 printNumberInput() 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,20 @@ package baseball;
 
 import java.util.Scanner;
 
+/*
+*       숫자 야구 게임을 진행하는 클래스
+*       author: 483759/윤이진
+* */
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        printNumberInput();
         // TODO 구현 진행
+    }
+
+    /* 게임 진행을 위해 숫자를 입력하라는 안내를 출력하는 함수 */
+    public static void printNumberInput(){
+        System.out.print("숫자를 입력해 주세요 : ");
     }
 }
