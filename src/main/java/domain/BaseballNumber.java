1a1c1bf951013f7e9f9df820963cc6f02ee45961
opdshe <opdshe@naver.com>
2020-11-26 17:11:31 +0900

    test: BaseballNumber 생성자 검증 테스트 코드 작성

--- src/main/java/domain/BaseballNumber.java
+++ src/main/java/domain/BaseballNumber.java
@@ -9,7 +9,7 @@ public class BaseballNumber {
     public static final int MIN_OF_NUMBER = 1;
     public static final int MAX_OF_NUMBER = 9;
 
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public BaseballNumber(List<Integer> numbers) {
         BaseballNumberValidator.validateBaseballNumber(numbers);
---
+++ src/test/java/domain/BaseballNumberTest.java
@@ -0,0 +1,40 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class BaseballNumberTest {
+    @Test
+    public void 입력값이_세개가_아니면_객체_만들지_않고_예외_발생() {
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2);
+
+        //when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> BaseballNumberCreator.createBaseballNumber(numbers));
+    }
+
+    @Test
+    public void 입력값이_0을_포함하면_객체_만들지_않고_예외_발생() {
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2, 0);
+
+        //when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> BaseballNumberCreator.createBaseballNumber(numbers));
+    }
+
+    @Test
+    public void 입력값이_중복된_값을_포함하면_객체_만들지_않고_예외_발생() {
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2, 2);
+
+        //when & then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> BaseballNumberCreator.createBaseballNumber(numbers));
+    }
+}
