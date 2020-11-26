aa8d234541edb5adf23121e7028e7f4725bb9eb8
naru <hsik0225@gmail.com>
2020-11-26 17:18:49 +0900

    test(score-board-test): 스트라이크가 1개일 경우 hasZeroStrike()가 false를 반환하는지 테스트

--- src/test/java/baseball/ScoreBoardTest.java
+++ src/test/java/baseball/ScoreBoardTest.java
@@ -3,6 +3,7 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import baseball.domain.ScoreBoard;
@@ -10,9 +11,16 @@ import baseball.domain.ScoreBoard;
 public class ScoreBoardTest {
 
     @Test
-    @DisplayName("스트라이크가 0개 일경우 hasZeroStrike() true 반환 테스트")
+    @DisplayName("스트라이크가 0개일 경우 hasZeroStrike() true 반환 테스트")
     public void hasZeroStrikeTest() {
         ScoreBoard scoreBoard = new ScoreBoard();
         assertTrue(scoreBoard.hasZeroStrike());
+    }
+
+    @Test
+    @DisplayName("스트라이크가 1개일 경우 hasZeroStrike() false 반환 테스트")
+    public void hasOneStrikeTest() {
+        ScoreBoard scoreBoard = new ScoreBoard(1, 0);
+        assertFalse(scoreBoard.hasZeroStrike());
     }
 }
