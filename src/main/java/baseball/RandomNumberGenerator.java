b790c73d873a0c7fc1ddd11e05c2f9f5a045de68
JungYoonSung <ybook2012@gmail.com>
2020-11-27 02:46:57 +0900

    feat : 난수 생성기 클래스 구현
    * requiring refactoring
      * GameProgress에서 접근할때 명시적이지 않음.
      * GameSetting 에서 굳이 또 객체를 만들어서 작업했어야 했는지에 대한 의문

---
+++ src/main/java/baseball/RandomNumberGenerator.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import utils.RandomUtils;
+
+public class RandomNumberGenerator {
+    public int[] answerNumbers =  new int[Constants.NUMBER_COUNT];
+    public boolean[] isUsedNumber = new boolean[Constants.NUMBER_LIMIT + 1];
+
+    public void makeRandomNumber() {
+        int numberCount = 0;
+        while(numberCount != Constants.NUMBER_COUNT) {
+            int candidateNumber = RandomUtils.nextInt(1, Constants.NUMBER_LIMIT);
+            if (isUsedNumber[candidateNumber]) continue;
+            isUsedNumber[candidateNumber] = true;
+            answerNumbers[numberCount] = candidateNumber;
+            numberCount++;
+        }
+
+        System.out.print("생성된 번호 : ");
+        for(int numbers : answerNumbers){
+            System.out.print(numbers);
+        }
+        System.out.println();
+    }
+}
