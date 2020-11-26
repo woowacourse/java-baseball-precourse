0429822db1c1e1e2602743c504d1933aada4e324
pjy1368 <basejin0810@naver.com>
2020-11-26 22:17:20 +0900

    feat: 유저가 입력할 숫자와 세 자리 난수의 초기값을 세팅함.

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -6,6 +6,19 @@ import utils.RandomUtils;
 
 public class BaseBall {
     private static final int RADIX = 10;
+    private static final int DIGIT_NUM = 3;
+    private String userNum;
+    private String randNum;
+
+    public BaseBall() {
+        userNum = "";
+        randNum = "";
+    }
+
+    public void init() {
+        randNum = makeRandNum(DIGIT_NUM);
+    }
+
     private String makeRandNum(final int digit) {
         Set<Character> randNumSet = new HashSet<>();
         int cnt = 0;
