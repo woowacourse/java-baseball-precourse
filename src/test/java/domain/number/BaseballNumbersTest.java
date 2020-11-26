06a42c717868e12272647347e79931ac09983484
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-26 19:19:39 +0900

    test : add test for calculating strike counts

--- src/test/java/domain/number/BaseballNumbersTest.java
+++ src/test/java/domain/number/BaseballNumbersTest.java
@@ -20,6 +20,12 @@ class BaseballNumbersTest {
                 Arguments.of(Arrays.asList(6, 7, 3)));
     }
 
+    private static Stream<Arguments> getBaseballNumbersForStrike() {
+        return Stream.of(Arguments.of(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3), 3),
+                Arguments.of(Arrays.asList(3, 7, 9), Arrays.asList(1, 2, 3), 0),
+                Arguments.of(Arrays.asList(9, 1, 6), Arrays.asList(3, 7, 6), 1));
+    }
+
     @DisplayName("BaseballNumbers를 자동 생성하면, 중복되지 않은 3개의 랜덤 숫자가 뽑힘")
     @Test
     public void BaseballNumbers_자동_생성_중복_없는_랜덤_3개_숫자_반환() {
@@ -39,5 +45,15 @@ class BaseballNumbersTest {
         BaseballNumbers baseballNumbers = BaseballNumbers.generateInputBaseballNumbers(inputBaseballNumbers);
         List<Integer> generatedBaseballNumbers = baseballNumbers.getBaseballNumbers();
         assertThat(generatedBaseballNumbers).hasSameElementsAs(inputBaseballNumbers);
+    }
+
+    @DisplayName("두 개의 BaseballNumbers를 비교하고, 스트라이크 개수를 정상 계산")
+    @ParameterizedTest
+    @MethodSource("getBaseballNumbersForStrike")
+    public void 스트라이크_개수_파악(List<Integer> firstNumbers, List<Integer> secondNumbers, int strikeCounts) {
+        BaseballNumbers firstBaseballNumbers = BaseballNumbers.generateInputBaseballNumbers(firstNumbers);
+        BaseballNumbers secondBaseballNumbers = BaseballNumbers.generateInputBaseballNumbers(secondNumbers);
+        int testStrikeCounts = firstBaseballNumbers.calculateStrikeCounts(secondBaseballNumbers);
+        assertThat(testStrikeCounts).isEqualTo(strikeCounts);
     }
 }
