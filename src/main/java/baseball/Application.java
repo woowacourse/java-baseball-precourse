7312d9686073fddf0d16700e7a6580135f4ee2fa
awesomeo184 <awesomeo184@gmail.com>
2020-11-26 12:35:18 +0900

    feat: 세자리 난수 리스트 생성 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,26 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+    }
+
+    public static ArrayList<Integer> createAnswer() {
+        final int START_INT = 123;
+        final int END_INT = 987;
+        ArrayList<Integer> answerNumberList = new ArrayList<>();
+
+        int answer = RandomUtils.nextInt(START_INT, END_INT);
+        String answerString = Integer.toString(answer);
+        for (int i = 0; i < answerString.length(); i++) {
+            answerNumberList.add(Character.getNumericValue(answerString.charAt(i)));
+        }
+        // TODO 유효성 검사
+        return answerNumberList;
     }
 }
