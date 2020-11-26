b2ecc70606a19fb642c132b91528734f5e30fe64
Minjeong <mathmjseo@naver.com>
2020-11-27 00:47:21 +0900

    feat: 게임 시작 및 종료를 입력받는 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,17 @@ import utils.RandomUtils;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        String command = null;
+        while(true){
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            command = scanner.next();
+            if(command.equals(Constants.EXIT_GAME)){
+                System.out.println("게임 종료");
+                scanner.close();
+                break;
+            }
+
+            // Run
+        }
     }
 }
