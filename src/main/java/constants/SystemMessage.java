7ef80325b5b6557f443360aef647a0ac60aa354c
pjy1368 <basejin0810@naver.com>
2020-11-26 19:02:45 +0900

    feat: 시스템 메시지를 상수화.

---
+++ src/main/java/constants/SystemMessage.java
@@ -0,0 +1,10 @@
+package constants;
+
+public final class SystemMessage {
+    public static final String INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    public static final String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
