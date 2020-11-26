be9952922f5fb984e5d219fa5248f8103f8ca0d6
JUNGHOJONG <hojong1351@naver.com>
2020-11-26 17:09:45 +0900

    feat(baseball): 1 - 9 숫자 중 서로 다른 3개의 숫자 랜덤으로 생성하는 기능 구현

---
+++ src/main/java/baseball/Competitor.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import utils.RandomUtils;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Competitor {
+
+    private static final int FIRST_BOUNDARY_VALUE = 1;
+    private static final int LAST_BOUNDARY_VALUE = 9;
+    private static final int RANDOM_NUMBER_COUNT = 3;
+
+    public static String getRandomNumbers(){
+        Set<Integer> randomNumbers = new LinkedHashSet<>();
+        while(randomNumbers.size() != RANDOM_NUMBER_COUNT){
+            randomNumbers.add(RandomUtils.nextInt(FIRST_BOUNDARY_VALUE, LAST_BOUNDARY_VALUE));
+        }
+        return randomNumbers.toString();
+    }
+
+}
