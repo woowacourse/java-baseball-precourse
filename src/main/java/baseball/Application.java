3c296705fe3664d39ced23b35543af4b2bd520fd
Junhyeong Jo <118jjh@naver.com>
2020-11-26 18:24:33 +0900

    feat: 게임에서 사용될 초기 전역 변수 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,9 @@ package baseball;
 import java.util.Scanner;
 
 public class Application {
+    static public final Scanner scanner = new Scanner(System.in);
+    static public int[] targetNumbers = {0,0,0};
+    static public int[] numberChecker = {0,0,0,0,0,0,0,0,0,0};
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
