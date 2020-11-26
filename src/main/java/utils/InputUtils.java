fa7db18b1248ba7d266b78f547dc208245e8f048
Hyeon9mak <jinha3507@gmail.com>
2020-11-26 19:29:45 +0900

    refactor(InputUtils): modify methods paramter data type

--- src/main/java/utils/InputUtils.java
+++ src/main/java/utils/InputUtils.java
@@ -5,27 +5,33 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class InputUtils {
-    public static void checkUserInput(Scanner scanner) {
-        isNumber(scanner);
-        isRightLength(scanner);
-        isWithoutDuplicate(scanner);
+    public static String checkUserInput(Scanner scanner) {
+        String userInput = scanner.nextLine();
+
+        isNumber(userInput);
+        isRightLength(userInput);
+        isWithoutDuplicate(userInput);
+
+        return userInput;
     }
 
-    private static void isNumber(Scanner scanner) {
-        if (!scanner.hasNextInt()) {
+    private static void isNumber(String userInput) {
+        for(int i = 0; i < userInput.length(); i++){
+            if(!Character.isDigit(userInput.charAt(i))){
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static void isRightLength(String userInput) {
+        if (userInput.length() != 3) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void isRightLength(Scanner scanner) {
-        if (scanner.nextLine().length() != 3) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void isWithoutDuplicate(Scanner scanner) {
+    private static void isWithoutDuplicate(String userInput) {
         Set<Integer> numberSet = new HashSet<Integer>();
-        String[] strList = scanner.nextLine().split("");
+        String[] strList = userInput.split("");
 
         for (int i = 0; i < strList.length; i++) {
             int number = Integer.parseInt(strList[i]);
