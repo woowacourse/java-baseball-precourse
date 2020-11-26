22705060718c4f8962e7e2c4a2e85cda1a0b2329
JuGyeong Lim <zoov0716@gmail.com>
2020-11-26 21:28:48 +0900

    test(GameSystemTest): 2번 기능 구현 테스트
    - GameSystem 클래스의 setAnswer 메소드 수정 필요 (랜덤 범위 오류)
    - 2번 기능 구현에서 입력을 -12와 같은 입력 통과됨, 수정 필요

--- src/test/java/baseballtest/GameSystemTest.java
+++ src/test/java/baseballtest/GameSystemTest.java
@@ -1,11 +1,14 @@
 package baseballtest;
 
+import java.util.Scanner;
+
 import baseball.GameSystem;
 
 public class GameSystemTest {
     public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
         GameSystem gameSystem = new GameSystem();
-        gameSystem.playGame();
+        gameSystem.playGame(scanner);
         gameSystem.printAnswerForTest();
     }
 }
