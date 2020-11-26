1403b12be30fd955c06d96d3d345a3c5f2419f53
Minjeong <mathmjseo@naver.com>
2020-11-27 00:13:45 +0900

    feat: Constants 클래스 생성 및 상수 추가

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Constants {
+    public final static int MIN_RANGE = 0;
+    public final static int MAX_RANGE = 10;
+    public final static int MAX_LEN = 3;
+    public final static String RUN_GAME = "1";
+    public final static String EXIT_GAME = "2";
+    public final static int INITIALIZE = -1;
+}
