6aa68722b9a0744458bb9a8de73f6bb1ca9f7dab
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-11-26 22:02:10 +0900

    fix: main에 BaseBallGame인스턴스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,16 @@
 package baseball;
 
+import utils.RandomUtils;
+
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        BaseBallGame baseBallGame=new BaseBallGame();
+        baseBallGame.run();
     }
 }
