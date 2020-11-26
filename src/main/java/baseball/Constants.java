b0664fee58e593bd5baa065ab2c0f4f688168fc9
JungYoonSung <ybook2012@gmail.com>
2020-11-27 02:42:43 +0900

    feat : 상수를 관리하는 클래스 분리

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public final class Constants {
+    public static final int NUMBER_COUNT = 3;
+    public static final int NUMBER_LIMIT = 9;
+    public static final String BALL_MESSAGE = "볼";
+    public static final String STRIKE_MESSAGE = "스크라이크";
+    public static final String GAME_FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    public static final int GAME_RESTART = 1;
+    public static final int GAME_STOP = 2;
+}
