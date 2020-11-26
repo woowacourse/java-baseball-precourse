f5630f58062742ba91839936e15b72a5a69d8299
bepoz <kangsy763@naver.com>
2020-11-26 15:54:49 +0900

    feat: gameContinue method added
    gameContinue() is a method that asks whether or not to play more games.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class Game {
     public static void gameStart(int tartgetValue, Scanner scanner){
-        boolean flag = false;
+        boolean flag = true;
         do{
             int input = valueInsert(scanner);
             if (checking(tartgetValue, input)) {
-                flag = gameContinue();
+                flag = gameContinue(scanner);
             } else {
                 hint(tartgetValue);
             }
-        } while(!flag);
+        } while(flag);
+    }
+
+    private static boolean gameContinue(Scanner scanner) {
+        int input;
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        input = scanner.nextInt();
+        if (input == 1) {
+            return true;
+        } else if (input == 2) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static boolean checking(int tartgetValue, int input) {
