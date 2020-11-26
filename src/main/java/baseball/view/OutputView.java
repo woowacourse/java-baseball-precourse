c74300c26331098ef05b43dfef592ab7bca193b0
김경록 <goodboy3421@gmail.com>
2020-11-27 06:10:36 +0900

    feat: 정답 문구를 출력하는 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     private static final String STRIKE = "스트라이크";
     private static final String SPACE = " ";
     private static final String NOTHING = "낫싱";
+    private static final String ENDING_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printHint(int strikeNumber, int ballNumber) {
         if (strikeNumber == 0 && ballNumber == 0) {
@@ -20,5 +21,9 @@ public class OutputView {
             System.out.print(strikeNumber + STRIKE);
         }
         System.out.println();
+    }
+
+    public static void printEndGame() {
+        System.out.println(ENDING_BASEBALL_GAME_MESSAGE);
     }
 }
