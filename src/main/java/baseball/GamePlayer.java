053d09a19014b7b4e5caf9752efba867ab2ffe4a
JUNGHOJONG <hojong1351@naver.com>
2020-11-26 18:14:25 +0900

    feat(baseball): 플레이어가 3자리 숫자를 입력
    1 - 9 숫자 중 서로 다른 3개의 숫자를 입력해야 하므로 입력한 숫자를 검사하여 모두 숫자가 아닐 경우
    또는 3자리가 아닌 경우 또는 중복되는 숫자가 있으면 재입력("다시 입력해주세요") 요구

---
+++ src/main/java/baseball/GamePlayer.java
@@ -0,0 +1,47 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GamePlayer {
+
+    private static final char FIRST_BOUNDARY_VALUE = '1';
+    private static final char LAST_BOUNDARY_VALUE = '9';
+    private static final int INPUT_NUMBER_COUNT = 3;
+
+    public static String getInputNumbers(String inputNumbers)
+                                            throws IllegalArgumentException{
+        if(inputNumbers.length() != INPUT_NUMBER_COUNT){
+            throw new IllegalArgumentException("다시 입력해주세요");
+        }
+
+        if(isNotAllNumbers(inputNumbers)){
+            throw new IllegalArgumentException("다시 입력해주세요");
+        }
+
+        if(haveDuplicateNumbers(inputNumbers)){
+            throw new IllegalArgumentException("다시 입력해주세요");
+        }
+
+        return inputNumbers;
+    }
+
+    private static boolean isNotAllNumbers(String inputNumbers){
+        for(int i=0; i<inputNumbers.length(); i++){
+            if(inputNumbers.charAt(i) < FIRST_BOUNDARY_VALUE
+                    || inputNumbers.charAt(i) > LAST_BOUNDARY_VALUE){
+               return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean haveDuplicateNumbers(String inputNumbers){
+        Set<Character> numbers = new HashSet<>();
+        for(int i=0; i<inputNumbers.length(); i++){
+            numbers.add(inputNumbers.charAt(i));
+        }
+        return numbers.size() != INPUT_NUMBER_COUNT;
+    }
+
+}
