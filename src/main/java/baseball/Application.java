a01820baa3850c7f57e5c94f77f4a28261d67488
Wannte <T.wannte@gmail.com>
2020-11-26 23:54:56 +0900

    feat(Application): complete restart, exit logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,30 +8,48 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        int targetNumber = ThreeDigitsUtils.generateBaseballNumber();
-        System.out.println(targetNumber);
-
-        Game game = new Game(targetNumber);
-        boolean isCorrect = false;
-        while (!isCorrect){
-            System.out.print("숫자를 입력해주세요 : ");
+        boolean onGame = true;
+        while (onGame) {
+            int targetNumber = ThreeDigitsUtils.generateBaseballNumber();
+            System.out.println(targetNumber);
+            Game game = new Game(targetNumber);
+            boolean isCorrect = false;
+            while (!isCorrect) {
+                System.out.print("숫자를 입력해주세요 : ");
+                String inputString = scanner.nextLine();
+                try {
+                    inputString = inputString.strip();
+                    int inputNumber = Integer.parseInt(inputString);
+                    if (ThreeDigitsUtils.isDistinctThreeDigits(inputNumber)) {
+                        System.out.println(inputNumber);
+                        String hint = game.getHint(inputNumber);
+                        System.out.println(hint);
+                        isCorrect = game.isAnswer(inputNumber);
+                    } else {
+                        throw new IllegalArgumentException("Input should be Distinct Three Digits Integer");
+                        //                    System.out.println("Not Distinct Three Digits Number: " + inputNumber);
+                    }
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Input should be Integer");
+                    //                System.out.println(e + " " + inputString);
+                }
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String inputString = scanner.nextLine();
             try {
                 inputString = inputString.strip();
                 int inputNumber = Integer.parseInt(inputString);
-                if (ThreeDigitsUtils.isDistinctThreeDigits(inputNumber)){
-                    System.out.println(inputNumber);
-                    String hint = game.getHint(inputNumber);
-                    System.out.println(hint);
-                    isCorrect = game.isAnswer(inputNumber);
+                if (inputNumber == 1) {
+                    continue;
+                } else if (inputNumber == 2) {
+                    onGame = false;
                 } else {
-                    throw new IllegalArgumentException("Input should be Distinct Three Digits Integer");
-//                    System.out.println("Not Distinct Three Digits Number: " + inputNumber);
+                    throw new IllegalArgumentException("Input should be 1 ro 2");
                 }
-            }
-            catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("Input should be Integer");
-//                System.out.println(e + " " + inputString);
+                //                System.out.println(e + " " + inputString);
             }
         }
     }
