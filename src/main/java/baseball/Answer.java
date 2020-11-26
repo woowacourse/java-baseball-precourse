fd21cd99c5a0137634b606ff838375bbe69e5c80
Minjeong <mathmjseo@naver.com>
2020-11-27 00:15:43 +0900

    feat: 1-9 사이의 서로 다 임의의 수 3개를 생성하는 메소드 구현

--- src/main/java/baseball/Answer.java
+++ src/main/java/baseball/Answer.java
@@ -3,8 +3,21 @@ package baseball;
 import utils.RandomUtils;
 
 public class Answer {
+    int[] answers = new int[Constants.MAX_LEN];
+    boolean[] checkNumberDuplicated = new boolean[Constants.MAX_RANGE];
+    int index = 0;
 
-    public void Answer(){
+    public Answer(){
+        while(index < Constants.MAX_LEN){
+            int number = RandomUtils.nextInt(Constants.MIN_RANGE, Constants.MAX_RANGE-1);
+            if(!checkNumberDuplicated[number]){
+                checkNumberDuplicated[number] = true;
+                answers[index++] = number;
+            }
+        }
+    }
 
+    public int[] getAnswers() {
+        return answers;
     }
 }
