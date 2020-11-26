277eea5548f93c9a7e1cd5fe83bada0d46242ebe
Minjeong <mathmjseo@naver.com>
2020-11-27 00:31:08 +0900

    feat: 입력값 유효성 검증 메소드 추가
    - 모두 숫자로 이루어져있는가 검증하는 메소드
    - 길이가 3인지 검증하는 메소드
    - 중복이 있는지 검증하는 메소드
    - 범위(1~9)에 속하는지 검증하는 메소드

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -17,7 +17,10 @@ public class Player {
         while(true){
             System.out.println("숫자를 입력해주세요 : ");
             String input = sc.nextLine();
-            if(true){ //true 대신 유효성 검증 부분 추가
+            if(!isDigit(input)
+                    || !isEqualsToMaxLength(input)
+                    || !isDuplicated(input)
+                    || !isInRange(input)){
                 throw new IllegalArgumentException("1~9 사이의 수로 이루어진 서로 다른 세 자리 수만 입력 가능합니다.");
             } else {
                 parseStringToIntegerArray(input);
@@ -26,6 +29,8 @@ public class Player {
         }
     }
 
+    public int[] getNumbers() { return numbers; }
+
     private void parseStringToIntegerArray(String string){
         numbers = new int[Constants.MAX_LEN];
         for(int i = 0; i < string.length(); i++){
@@ -33,5 +38,44 @@ public class Player {
         }
     }
 
-    public int[] getNumbers() { return numbers; }
+    private boolean isEqualsToMaxLength(String string){
+        return (string.length() == Constants.MAX_LEN);
+    }
+
+    private boolean isDuplicated(String string){
+        boolean result = true;
+        String temp;
+
+        for(int i = 0; i < string.length(); i++){
+            temp = string.replaceFirst(String.valueOf(string.charAt(i)),"");
+            if(temp.contains(String.valueOf(string.charAt(i)))){
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    private boolean isInRange(String string){
+        boolean result = true;
+        for(int i = 0; i < string.length(); i++){
+            if(string.charAt(i) == '0'){
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isDigit(String string){
+        try{
+            Integer.parseInt(string);
+        } catch (NumberFormatException e){
+            return false;
+        }
+        return true;
+    }
+
 }
