039becb26ea24e4ff45ea89533aea5f97d7cf9df
yhh1056 <yhh1056@naver.com>
2020-11-26 19:29:28 +0900

    test: RandomNumbers valueOf() duplicate test

--- src/test/java/utils/RandomNumbersTest.java
+++ src/test/java/utils/RandomNumbersTest.java
@@ -1,8 +1,11 @@
 package utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +22,21 @@ class RandomNumbersTest {
         List<Integer> result = RandomNumbers.valueOf(size);
 
         assertEquals(3, result.size());
+    }
+
+    @Test
+    @DisplayName("중복 여부")
+    void duplicate() {
+        int testLength = 1000;
+        List<Integer> numbers = RandomNumbers.valueOf(size);
+
+        // 중복되는 숫자가 있다면 사이즈가 감소
+        for (int i = 0; i < testLength; i++) {
+            Set<Integer> result = new HashSet<>(numbers);
+
+            if (result.size() != size) {
+                fail();
+            }
+        }
     }
 }
