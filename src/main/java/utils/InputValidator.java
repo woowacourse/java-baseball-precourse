7eb9bc208ac6f0a348b5bb933714614937a33284
ecsimsw <ecsimsw@gmail.com>
2020-11-27 02:56:35 +0900

    feat : 사용자 입력 유효 여부 조사 확인 기능

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -1,4 +1,57 @@
 package utils;
+import java.util.*;
 
 public class InputValidator {
+
+    private InputValidator(){}
+
+    public static boolean validGuessNumber(String inputNumber, int len) throws Exception{
+        if(!isValidLength(inputNumber, len)){
+            return false;
+        }
+
+        if(!isNumericString(inputNumber)){
+            return false;
+        }
+
+        if(!isAllDifferent(inputNumber)){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validPlayAgainAnswer(String input) throws Exception{
+        if(!isNumericString(input)){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidLength(String input, int len){
+        if(input.length() == len){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isNumericString(String input){
+        try{
+            Integer.parseInt(input);
+        }catch (IllegalArgumentException IAE){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isAllDifferent(String input){
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+        for(int i =0; i<chars.length-1; i++){
+            if(chars[i] == chars[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
