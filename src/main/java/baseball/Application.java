8f448066651fcf00af956b9c2956156098c42930
platanus <platanus@canxan.com>
2020-11-26 21:52:36 +0900

    feat(baseball): Add method to validate input value

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,15 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        int userInput;
+
+        System.out.println("숫자를 입력해주세요 : ");
+        userInput = scanner.nextInt();
+        checkInputNumber(userInput);
+    }
+    public static void checkInputNumber(int numbers){
+        if(numbers < 100 || numbers > 1000){
+            throw new IllegalArgumentException("3자리 수가 아닙니다.");
+        }
     }
 }
