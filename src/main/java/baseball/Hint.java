c6701b72b8384d8eda6f238aa63ad773c83955f7
Minjeong <mathmjseo@naver.com>
2020-11-27 00:34:06 +0900

    feat: Hint 클래스 추가

---
+++ src/main/java/baseball/Hint.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Hint {
+    private int strike, ball;
+
+    public Hint(int strike, int ball){
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
