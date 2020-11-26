3b0f3f9bc0cc9441108ad2840864e39492ffc2db
Minjeong <mathmjseo@naver.com>
2020-11-27 00:38:23 +0900

    feat: 플레이어의 답안과 정답을 비교하는 메소드 추가
    - 플레이어의 답안과 정답을 비교하는 메소드
    - 스트라이크의 수를 세는 메소드
    - 볼의 수를 세는 메소드

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -7,4 +7,25 @@ public class Hint {
         this.strike = strike;
         this.ball = ball;
     }
+
+    public void compareAnswerAndPlayerNumber(int[] answer, int[] playerAnswer){
+        for(int i = 0; i < Constants.MAX_LEN; i++){
+            countStrike(answer[i], playerAnswer[i]);
+            countBall(answer, playerAnswer[i], i);
+        }
+    }
+
+    private void countStrike(int answer, int playerAnswer){
+        if(answer == playerAnswer){
+            this.strike++;
+        }
+    }
+
+    private void countBall(int[] answer, int playerAnswer, int digits){
+        for(int i = 0; i < Constants.MAX_LEN; i++){
+            if(i != digits && answer[i] == playerAnswer){
+                this.ball++;
+            }
+        }
+    }
 }
