38536317df0ee3c70f9b17a6730a1e0ac68a010c
kihwankim <clack2933@gmail.com>
2020-11-26 21:30:01 +0900

    refactor: 상수 변수로 변경

--- src/test/java/baseball/game/BaseballResultTest.java
+++ src/test/java/baseball/game/BaseballResultTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BaseballResultTest {
-    final int maxBaseballSize = 3;
-    final int zero = 0;
+    private static final int MAX_BASEBALL_SIZE = 3;
+    private static final int ZERO = 0;
 
     @Test
     void isNothingTest() {
         // given
-        BaseballResult baseballResult = new BaseballResult(maxBaseballSize);
+        BaseballResult baseballResult = new BaseballResult(MAX_BASEBALL_SIZE);
 
         // when
         int ballCount = baseballResult.getBaseballTypeFrequency(BaseballType.BALL);
         int strikeCount = baseballResult.getBaseballTypeFrequency(BaseballType.STRIKE);
 
         // then
-        assertThat(ballCount == zero && strikeCount == zero).isTrue();
+        assertThat(ballCount == ZERO && strikeCount == ZERO).isTrue();
         assertThat(baseballResult.isNothing()).isTrue();
     }
 
     @Test
     void isAllStrikeTest() {
         // given
-        BaseballResult baseballResult = new BaseballResult(maxBaseballSize);
-        for (int index = 0; index < maxBaseballSize; index += 1) {
+        BaseballResult baseballResult = new BaseballResult(MAX_BASEBALL_SIZE);
+        for (int index = 0; index < MAX_BASEBALL_SIZE; index += 1) {
             baseballResult.addBaseballTypeFrequency(BaseballType.STRIKE);
         }
 
@@ -36,14 +36,14 @@ class BaseballResultTest {
         int strikeCount = baseballResult.getBaseballTypeFrequency(BaseballType.STRIKE);
 
         // then
-        assertThat(ballCount == zero && strikeCount == maxBaseballSize).isTrue();
+        assertThat(ballCount == ZERO && strikeCount == MAX_BASEBALL_SIZE).isTrue();
         assertThat(baseballResult.isAllStrike()).isTrue();
     }
 
     @Test
     void addBaseballTypeFrequency() {
         // given
-        BaseballResult baseballResult = new BaseballResult(maxBaseballSize);
+        BaseballResult baseballResult = new BaseballResult(MAX_BASEBALL_SIZE);
         int increaseNumber = 1;
 
         // when
