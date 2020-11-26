319e4a33b4d7942645625d0d0042c331455f966d
Minjeong <mathmjseo@naver.com>
2020-11-27 00:41:56 +0900

    feat: Hint를 출력하는 메소드 및 getter 메소드 추가

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -8,6 +8,23 @@ public class Hint {
         this.ball = ball;
     }
 
+    public void printHint(){
+        if(strike == 0 && ball == 0){
+            System.out.println("낫싱");
+        }
+        if(strike > 0){
+            System.out.println(strike + "스트라이크 ");
+        }
+        if(ball > 0){
+            System.out.println(ball + "볼 ");
+        }
+        System.out.println();
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
     public void compareAnswerAndPlayerNumber(int[] answer, int[] playerAnswer){
         for(int i = 0; i < Constants.MAX_LEN; i++){
             countStrike(answer[i], playerAnswer[i]);
