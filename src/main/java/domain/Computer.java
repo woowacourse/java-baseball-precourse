1f4bf288585b581e20b0a1dd9fc63ca9b7031431
park seok chae <tjrco56@naver.com>
2020-11-26 22:40:18 +0900

    feat: provide private constructor in domain

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -2,7 +2,10 @@ package domain;
 
 public class Computer extends Player {
 	private static Computer computer = new Computer();
-	
+
+	private Computer() {
+	}
+
 	public static Computer getInstance() {
 		return computer;
 	}
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -2,7 +2,10 @@ package domain;
 
 public class User extends Player {
 	private static User user = new User();
-	
+
+	private User() {
+	}
+
 	public static User getInstance() {
 		return user;
 	}
