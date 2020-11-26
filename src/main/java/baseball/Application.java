949543aee266f75c332487e2c77889d9a2129866
daeun <leede0418@likelion.org>
2020-11-27 00:52:30 +0900

    feat(application): scan a number
    - [플레이어] 3자리 수를 입력하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,15 @@
 package baseball;
 
+import number.NumberHandler;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        // 플레이어가 3자리 수를 입력한다.
+        System.out.print("숫자를 입력해주세요 : ");
+        String scannerNumber = scanner.nextLine();
     }
 }
