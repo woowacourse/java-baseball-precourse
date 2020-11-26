4ae3048aeb807357a2ca737a80872cc5454b5994
김경록 <goodboy3421@gmail.com>
2020-11-27 06:40:28 +0900

    refactor: 출력단의 하드코딩부분 상수로 리팩토링
    0 --> ZERO

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,18 +6,19 @@ public class OutputView {
     private static final String SPACE = " ";
     private static final String NOTHING = "낫싱";
     private static final String ENDING_BASEBALL_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final int ZERO = 0;
 
     public static void printHint(int strikeNumber, int ballNumber) {
-        if (strikeNumber == 0 && ballNumber == 0) {
+        if (strikeNumber == ZERO && ballNumber == ZERO) {
             System.out.println(NOTHING);
             return;
         }
 
-        if (ballNumber > 0) {
+        if (ballNumber > ZERO) {
             System.out.print(ballNumber + BALL + SPACE);
         }
 
-        if (strikeNumber > 0) {
+        if (strikeNumber > ZERO) {
             System.out.print(strikeNumber + STRIKE);
         }
         System.out.println();
