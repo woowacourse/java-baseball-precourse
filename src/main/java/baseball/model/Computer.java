6a84db1cac014ac2f5ec99ca83bda9965889de12
Hyeon9mak <jinha3507@gmail.com>
2020-11-26 19:28:30 +0900

    feat(domain): Computer class and User class

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,24 @@
+package baseball.model;
+
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    public static List<Integer> balls = new ArrayList<>();
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int MAX_LENGTH = 3;
+
+    public static void makeBalls() {
+        balls.clear();
+
+        while(balls.size() < MAX_LENGTH){
+            int number = RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
+            if(!balls.contains(number)){
+                balls.add(number);
+            }
+        }
+    }
+}
---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,19 @@
+package baseball.model;
+
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    public static List<Integer> balls = new ArrayList<>();
+
+    public static void makeBalls(String userInput) {
+        balls.clear();
+
+        String[] stringArray = userInput.split("");
+        for(int i = 0; i < stringArray.length; i++){
+            balls.add(Integer.parseInt(stringArray[i]));
+        }
+    }
+}
