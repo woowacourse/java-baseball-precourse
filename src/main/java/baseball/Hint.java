ee97a32d6f7d225a83705f76441bd99b3f85437a
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2020-11-26 12:20:03 +0900

    feat(hint): add hint class
    - add hint class
    - add lombok dependency

---
+++ src/main/java/baseball/Hint.java
@@ -0,0 +1,14 @@
+package baseball;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Hint {
+
+    private final int strike;
+
+    private final int ball;
+}
