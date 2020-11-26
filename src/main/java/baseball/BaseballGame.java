a0516b7933c670d8ed528e010c6b064fca33277d
operasystem6 <operasystem6@naver.com>
2020-11-27 01:04:00 +0900

    feat: 숫자 야구 게임 플레이 클래스 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,68 @@
+/*
+ * BaseballGame.java
+ *
+ * version 1.0
+ *
+ * 2020/11/27
+ *
+ * Copyright (c) 2020 Junhyoung Park
+ */
+
+package baseball;
+
+import baseball.domain.GenerateRandomNumber;
+import baseball.domain.Hint;
+import baseball.domain.InputValidation;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.Scanner;
+
+public class BaseballGame {
+    private Hint hint;
+
+    private String setPlayNumber(Scanner scanner) {
+        String playerNumber = InputView.inputPlayerNumber(scanner);
+        InputValidation inputValidation = new InputValidation(playerNumber);
+        if (!inputValidation.checkValidNumber()) {
+            throw new IllegalArgumentException("잘못된 데이터 입니다.");
+        }
+        return playerNumber;
+    }
+
+    private boolean endGame() {
+        if (hint.isWinning()) {
+            OutputView.printWinning();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean newGame(Scanner scanner) {
+        final int NEW_GAME = 1;
+        final int EXIT = 2;
+        int selectNumber = InputView.inputPlayOrNotNumber(scanner);
+        if ((selectNumber != NEW_GAME) && (selectNumber != EXIT)) {
+            throw new IllegalArgumentException("잘못된 데이터 입니다.");
+        }
+        return selectNumber == NEW_GAME;
+    }
+
+    private boolean playOrExit(Scanner scanner) {
+        if (endGame()) {
+            return !newGame(scanner);
+        }
+        return false;
+    }
+
+    public void play(Scanner scanner) {
+        GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
+        String programNumber = generateRandomNumber.getRandomNumber();
+        System.out.println(programNumber);          // 랜덤 값 테스트
+        do {
+            hint = new Hint(setPlayNumber(scanner), programNumber);
+            hint.startGame();
+            OutputView.printBallCount(hint.getBall(), hint.getStrike());
+        } while(!playOrExit(scanner));
+    }
+}
