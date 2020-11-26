9bbd87c9b720ee5cc82a1c57a4c4c16fa34dc8ef
naru <hsik0225@gmail.com>
2020-11-26 20:18:05 +0900

    refactor(baseball-numbers): 중복된 값 입력 예외 처리

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -5,11 +5,17 @@ import java.util.Set;
 
 public class BaseballNumbers {
 
+    public static final String DUPLICATE_NUMBER_MESSAGE = "중복된 값이 입력되었습니다!";
+
     private final Set<Integer> numbers;
 
     public BaseballNumbers(String inputNumbers) {
         this.numbers = new HashSet<>();
         addNumbers(inputNumbers);
+
+        if (hasDuplicateNumber()) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_MESSAGE);
+        }
     }
 
     public boolean hasDuplicateNumber() {
