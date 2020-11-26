a8c8025f071db335edbf073929e3aff0f5da6a20
ecsimsw <ecsimsw@gmail.com>
2020-11-27 03:08:27 +0900

    fix : 누락된 입력 예외 사항

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -2,29 +2,48 @@ package utils;
 import java.util.*;
 
 public class InputValidator {
+    private static final String INVALID_LENGTH_MSG = "잘못된 입력 길이입니다.";
+    private static final String INVALID_RANGE_MSG = "범위 외 입력입니다.";
+    private static final String INVALID_DUPLICATE_MSG = "중복이 포함된 입력입니다.";
+
+    private static final int BUTTON_PLAY_AGAIN_MAX = 2;
+    private static final int BUTTON_PLAY_AGAIN_MIN = 1;
 
     private InputValidator(){}
 
-    public static boolean validGuessNumber(String inputNumber, int len) throws Exception{
-        if(!isValidLength(inputNumber, len)){
-            return false;
+    public static boolean validPlayAgainAnswer(String input) throws Exception{
+        if(!isInRange(input, BUTTON_PLAY_AGAIN_MIN, BUTTON_PLAY_AGAIN_MAX)){
+            throw new InvalidInputHandler(INVALID_RANGE_MSG);
         }
-
-        if(!isNumericString(inputNumber)){
-            return false;
-        }
-
-        if(!isAllDifferent(inputNumber)){
-            return false;
-        }
-
         return true;
     }
 
-    public static boolean validPlayAgainAnswer(String input) throws Exception{
+    private static boolean isInRange(String input, int min, int max){
         if(!isNumericString(input)){
             return false;
         }
+
+        for(char num : input.toCharArray()){
+            if(Character.getNumericValue(num)<min || Character.getNumericValue(num)>max){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean validGuessNumber(String inputNumber, int len) throws Exception{
+        if(!isValidLength(inputNumber, len)){
+            throw new InvalidInputHandler(INVALID_LENGTH_MSG);
+        }
+
+        if(!isNumericString(inputNumber)){
+            throw new InvalidInputHandler(INVALID_RANGE_MSG);
+        }
+
+        if(!isAllDifferent(inputNumber)){
+            throw new InvalidInputHandler(INVALID_DUPLICATE_MSG);
+        }
+
         return true;
     }
 
@@ -47,6 +66,7 @@ public class InputValidator {
     private static boolean isAllDifferent(String input){
         char[] chars = input.toCharArray();
         Arrays.sort(chars);
+
         for(int i =0; i<chars.length-1; i++){
             if(chars[i] == chars[i+1]){
                 return false;
