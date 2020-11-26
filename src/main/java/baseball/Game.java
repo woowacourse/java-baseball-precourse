c0dbf921f9b2ddfe649054cc9af4355aa9ff41f9
bepoz <kangsy763@naver.com>
2020-11-26 15:46:59 +0900

    feat: checking method added
    checking() returns whether or not the value is matched.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,12 +7,16 @@ public class Game {
         boolean flag = false;
         do{
             int input = valueInsert(scanner);
-            if (checking(tartgetValue)) {
+            if (checking(tartgetValue, input)) {
                 flag = gameContinue();
             } else {
                 hint(tartgetValue);
             }
         } while(!flag);
+    }
+
+    private static boolean checking(int tartgetValue, int input) {
+        return tartgetValue == input;
     }
 
     private static int valueInsert(Scanner scanner) {
