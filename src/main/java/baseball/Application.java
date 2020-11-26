cdac6257d76c8753f784e3c32f1e946f513c2529
483759 <483759@naver.com>
2020-11-26 16:46:21 +0900

    개별 character가 숫자 1 이상 9 이하인지 검사하는 isValidCharacter() 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -29,11 +29,20 @@ public class Application {
 
     /* 유저가 입력한 숫자가 올바른 형식인지 검사하는 함수 */
     public static boolean isValidInput(String inputValue){
-        boolean isValid=false;
+        boolean isValid=true;
         for(int i=0;i<inputValue.length();i++){
             // 개별 문자에 대해 형식이 맞는지 검사
+            boolean isNumber=isValidCharacter(inputValue.charAt(i));
+            if(isNumber==false)
+                isValid=false;
         }
         return  isValid;
+    }
+
+    /* 해당 문자가 입력 요구사항에 일치하는지 검사하는 함수 */
+    public static boolean isValidCharacter(char inputCharacter){
+        boolean isValid=false;
+        return inputCharacter>='1'&&inputCharacter<='9';
     }
 
 }
