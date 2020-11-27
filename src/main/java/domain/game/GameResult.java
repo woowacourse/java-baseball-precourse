35d0659a34f61e061f84f7b5fa4e41a5df681b9e
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-27 14:32:46 +0900

    feat : add method verifying three strikes

--- src/main/java/domain/game/GameResult.java
+++ src/main/java/domain/game/GameResult.java
@@ -7,6 +7,7 @@ public class GameResult {
     private static final String STRIKE_KEY = "스트라이크";
     private static final String BALL_KEY = "볼";
     private static final int VALID_KEY_COUNTS = 2;
+    private static final int FULL_SCORE_STRIKE_COUNTS = 3;
     private static final int ZERO = 0;
 
     private final Map<String, Integer> gameResult;
@@ -24,6 +25,10 @@ public class GameResult {
         if (!gameResultKeys.contains(STRIKE_KEY) || !gameResultKeys.contains(BALL_KEY)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public boolean isFullScore() {
+        return gameResult.get(STRIKE_KEY) == FULL_SCORE_STRIKE_COUNTS;
     }
 
     public boolean isNothing() {
