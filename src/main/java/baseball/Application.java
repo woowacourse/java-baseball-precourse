0592acefd5d74f0f3084899872378d58d99168ab
DWL <DWL>
2020-11-27 17:44:25 +0900

    feat: add start game logig in Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,8 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        Player player = new Player(scanner);
+        BaseBallGameController baseBallGameController = new BaseBallGameController(player);
+        baseBallGameController.startGame();
     }
 }
