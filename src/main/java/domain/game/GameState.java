8c0deda89efa42984ce8d7137e6a183a25a8452b
Jinhong <56240505+xlffm3@users.noreply.github.com>
2020-11-27 14:04:57 +0900

    feat : add enum GameState and static factory method

--- src/main/java/domain/game/GameState.java
+++ src/main/java/domain/game/GameState.java
@@ -1,4 +1,22 @@
 package domain.game;
 
+import java.util.Arrays;
+
 public enum GameState {
+    KEEP_PLAYING(0),
+    RESTART(1),
+    EXIT(2);
+
+    private final int signatureValue;
+
+    private GameState(int signatureValue) {
+        this.signatureValue = signatureValue;
+    }
+
+    public static GameState findGameState(int signatureValue) {
+        return Arrays.stream(values())
+                .filter(gameState -> gameState.signatureValue == signatureValue)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
