bfaff877f95e1f3f910e8d1f789b62ca2be16337
bluewood <bluewood.truth@gmail.com>
2020-11-27 13:45:15 +0900

    feat: 사용자 입력과 출력을 담당하는 InputOutputManager 클래스 작성

---
+++ src/main/java/modules/InputOutputManager.java
@@ -0,0 +1,11 @@
+package modules;
+
+import java.util.Scanner;
+
+public class InputOutputManager {
+    Scanner scanner;
+    
+    public InputOutputManager(Scanner scanner){
+        this.scanner = scanner;
+    }
+}
