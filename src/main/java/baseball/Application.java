9c3f0f8203a7480f64fafed56aba0b7a36a7094d
seunghyun <ggyool@naver.com>
2020-11-27 19:11:54 +0900

    feat: Application 실행 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        // 외부에서 플레이어가 게임에 참여한다고 가정하였음
+        Player player = new Player();
+
+        GameController gameController = new GameController(scanner, player);
+        gameController.run();
     }
 }
