e4c2e7b00235b2a3bd21d2493429c9951ec3d4fa
seunghyun <ggyool@naver.com>
2020-11-27 21:21:57 +0900

    feat: 게임의 대략적인 진행 흐름을 정함, 로직 작성

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -18,6 +18,33 @@ public class GameController {
     }
 
     public void run() {
+        do {
+            startRound();
+        } while (checkRestart());
+    }
 
+    public void startRound() {
+        Number playerNumber;
+
+        computer.pickNumber();
+        do {
+            playerNumber = requestPlayerNumber(scanner);
+
+        } while (checkAnswer());
+    }
+
+    private boolean checkRestart() {
+        // TODO: 플레이어의 입력에 따른 재시작 여부 결정
+        return false;
+    }
+
+    private Number requestPlayerNumber(Scanner scanner) {
+        // TODO: 플레이어의 입력을 받아와 Number로 변환
+        return null;
+    }
+
+    private boolean checkAnswer() {
+        // TODO: 컴퓨터와 플레이어의 숫자를 비교하여 정답 여부 결정
+        return false;
     }
 }
