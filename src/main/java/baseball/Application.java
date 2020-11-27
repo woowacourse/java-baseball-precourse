e3cc45e625695bc7a7c695690403585ceaf763de
mungto <daum7766@naver.com>
2020-11-27 18:37:27 +0900

    feat(baseball): create function prototype

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,35 @@ package baseball;
 import java.util.Scanner;
 
 public class Application {
+
+    private static int[] getRandomNumber() {
+        return new int[0];
+    }
+
+    private static int[] getUserNumber(Scanner scanner) {
+        return new int[0];
+    }
+
+    private static boolean getIsRestart(Scanner scanner) {
+        return true;
+    }
+
+    private static int findStrike(int[] randomNumber, int[] userNumber) {
+        return 0;
+    }
+
+    private static int findBall(int[] randomNumber, int[] userNumber) {
+        return 0;
+    }
+
+    private static void printStr(int strike, int ball) {
+
+    }
+
+    private static void playGame(Scanner scanner, int[] randomNumber) {
+
+    }
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
