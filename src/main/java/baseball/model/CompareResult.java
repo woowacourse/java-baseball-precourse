fe39975c0a9f588d52d31bb96cb13aea5a5b0004
Hyeon9mak <jinha3507@gmail.com>
2020-11-27 12:54:25 +0900

    feat(CompareResult): add compare baseball rule class compare ComputerBalls and UserBalls save strike and ball value

--- src/main/java/baseball/model/CompareResult.java
+++ src/main/java/baseball/model/CompareResult.java
@@ -1,2 +1,21 @@
-package baseball.model;public class CompareResult {
+package baseball.model;
+
+public class CompareResult {
+    public static int ball;
+    public static int strike;
+
+    private static final int RESET = 0;
+    private static final int THREE_STRIKE = 3;
+
+    public static void resetResult() {
+        ball = RESET;
+        strike = RESET;
+    }
+
+    public static boolean isThreeStrike(){
+        if (strike == THREE_STRIKE) {
+            return true;
+        }
+        return false;
+    }
 }
