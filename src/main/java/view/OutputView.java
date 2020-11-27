8e077669c075691940ca80a19b0edbb4dbb0d490
Junyoung Choi <cjy1705@naver.com>
2020-11-27 15:42:14 +0900

    refactor: strike, ball 화면 표시 함수 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -15,25 +15,37 @@ public class OutputView {
     public static int showHint(int strike, int ball) {
 
         if(strike == 0 && ball == 0){
-            System.out.println("낫싱");
+            threeBall();
             return 0;
         }else if(strike == 3){
-            System.out.println("3 스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            threeStrike();
             Scanner scanner = new Scanner(System.in);
             return scanner.nextInt();
         }else{
-
-            if(strike == 0){
-                System.out.printf("%d 볼\n", ball);
-            }else if(ball == 0){
-                System.out.printf("%d 스트라이크\n", strike);
-            }else{
-                System.out.printf("%d 볼 %d 스트라이크\n", ball, strike);
-            }
+            makeHint(strike, ball);
 
             return 0;
         }
+    }
+
+    public static void makeHint(int strike, int ball){
+        if(strike == 0){
+            System.out.printf("%d 볼\n", ball);
+        }else if(ball == 0){
+            System.out.printf("%d 스트라이크\n", strike);
+        }else{
+            System.out.printf("%d 볼 %d 스트라이크\n", ball, strike);
+        }
+
+    }
+
+    public static void threeBall(){
+        System.out.println("낫싱");
+    }
+
+    public static void threeStrike(){
+        System.out.println("3 스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
