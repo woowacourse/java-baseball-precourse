7d2c5d9c56280af8b81a0800290d89846a7d903a
Sehwan_Jang <whilter08@gmail.com>
2020-11-27 20:19:23 +0900

    랜덤한 정답생성 기능 구현
    * generateAnswer() 메서드 추가
    * main() : answer 배열 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,11 +4,27 @@ import utils.RandomUtils;
 
 import java.util.Scanner;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
+        int[] answer = generateAnswer();
+    }
+
+    public static int[] generateAnswer() {
+        int[] answer = new int[3];
+
+        for (int i = 0; i < answer.length; i++) {
+            int tmpAnswer = RandomUtils.nextInt(1, 9);
+
+            if (IntStream.of(answer).anyMatch(x -> x == tmpAnswer)) {
+                i--;
+                continue;
+            }
+
+            answer[i] = tmpAnswer;
+        }
+        return answer;
     }
 }
