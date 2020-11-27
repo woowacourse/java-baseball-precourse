dcd16bd512b7d0bbb8e45ad6ed7daa6465bea48a
bluewood <bluewood.truth@gmail.com>
2020-11-27 18:22:18 +0900

    feat: 게임 흐름의 기본적인 뼈대 작성

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -9,8 +9,19 @@ public class GameManager {
     InputOutputManager inputOutputManager;
     NumberGenerator numberGenerator;
 
+    int strike = 0;
+    int ball = 0;
+
     public GameManager(Scanner scanner){
         inputOutputManager = new InputOutputManager(scanner);
         numberGenerator = new NumberGenerator();
     }
+
+    public void run(){
+        numberGenerator.generate();
+
+        playOneRound();
+    }
+
+    private void playOneRound(){}
 }
