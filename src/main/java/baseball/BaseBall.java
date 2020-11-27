bb67b77bbc2f565abb861c252e08daef77d67bf9
pjy1368 <basejin0810@naver.com>
2020-11-27 10:21:40 +0900

    feat: 유저가 입력한 숫자와 난수를 비교하여 스트라이크와 볼 여부 확인.

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -1,23 +1,67 @@
 package baseball;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import constants.SystemMessage;
 import utils.RandomUtils;
 
 public class BaseBall {
     private static final int RADIX = 10;
     private static final int DIGIT_NUM = 3;
+    private final Scanner scanner;
     private String userNum;
     private String randNum;
 
-    public BaseBall() {
+    public BaseBall(Scanner scanner) {
+        this.scanner = scanner;
         userNum = "";
         randNum = "";
+    }
+
+    public String getRandNum() {
+        return randNum;
     }
 
     public void init() {
         randNum = makeRandNum(DIGIT_NUM);
     }
+
+    private String umpire() {
+		int ballNum = 0;
+		int strikeNum = 0;
+
+		for (int i = 0; i < DIGIT_NUM; i++) {
+			char digit = userNum.charAt(i);
+
+			if (isStrike(digit, i)) {
+				strikeNum++;
+			} else if (isBall(digit)) {
+				ballNum++;
+			}
+        }
+
+        return "";
+		// return getHint(ballNum, strikeNum);
+    }
+
+	private boolean isStrike(final char digit, final int pos) {
+		for (int i = 0; i < DIGIT_NUM; i++) {
+			if (digit == randNum.charAt(i) && pos == i) {
+			 return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean isBall(final char digit) {
+		for (int i = 0; i < DIGIT_NUM; i++) {
+			if (digit == randNum.charAt(i)) {
+			 return true;
+			}
+		}
+		return false;
+	}
 
     private String makeRandNum(final int digit) {
         Set<Character> randNumSet = new HashSet<>();
