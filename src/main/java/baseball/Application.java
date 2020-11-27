da7085e9f61fb182afb334d365005eb0ef198551
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-27 18:29:48 +0900

    feat: main 메소드 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,20 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+import baseball.view.InputView;
+
 import java.util.Scanner;
 
 public class Application {
+    private static final String END = "2";
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        BaseballGame baseballGame = new BaseballGame();
+        do {
+            baseballGame.play(scanner);
+        } while (!InputView.inputRestartOrEnd(scanner).equals(END));
     }
+
 }
