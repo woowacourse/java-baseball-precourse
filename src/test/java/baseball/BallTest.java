aee4735efa88ea2d804651f06c84b9491219c48b
김주원 <kjw11077@naver.com>
2020-11-27 18:03:33 +0900

    test(BallTest): update test

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -7,18 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class BallTest {
 
     @Test
-    public void 정상범위보다_작은_경우() {
+    public void validateRange_fail() {
         assertThrows(IllegalArgumentException.class, () -> new Ball(-1));
         assertThrows(IllegalArgumentException.class, () -> new Ball(0));
-    }
-
-    @Test
-    public void 정상범위보다_큰_경우() {
         assertThrows(IllegalArgumentException.class, () -> new Ball(10));
-    }
 
-    @Test
-    public void 정상적인_입력인_경우() {
         assertDoesNotThrow(() -> new Ball(9));
         assertDoesNotThrow(() -> new Ball(1));
     }
