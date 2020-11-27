4c27017e4bf38c8f10e9ff5a830af882db2c9168
DWL <DWL>
2020-11-27 16:14:53 +0900

    feat: add TextResource
    야구 게임에서 사용되는 text들을 모아놓은 class 입니다.

---
+++ src/main/java/utils/TextResource.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class TextResource {
+    public static final String STATEMENT_NUMBER_INPUT = "숫자를 입력해주세요 :";
+    public static final String STATEMENT_INPUT_ANSWER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String STATEMENT_ASK_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String HINT_NOTHING = "낫싱";
+    public static final String HINT_BALL = "볼";
+    public static final String HINT_STRIKE = "스트라이크";
+
+}
