8e12d8d0f649a6eac72a822e1892a98843f197a6
Hyeon9mak <jinha3507@gmail.com>
2020-11-27 09:56:06 +0900

    feat: baseballgame init baseball/Application.java baseball/controller/BaseBallGame.java
    baseballgame start and init User, Computer balls

--- src/main/java/baseball/controller/BaseBallGame.java
+++ src/main/java/baseball/controller/BaseBallGame.java
@@ -1,2 +1,14 @@
-package baseball.controller;public class BaseBallGame {
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.model.User;
+import utils.InputUtils;
+
+import java.util.Scanner;
+
+public class BaseBallGame {
+    public static void start(Scanner scanner) {
+        Computer.makeBalls();
+        User.makeBalls(InputUtils.checkUserInput(scanner.nextLine()));
+    }
 }
