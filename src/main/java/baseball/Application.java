247b499d4236cf90f2ac3a32a79ef4157aacc2e5
Gayoung <gyyoon4u@naver.com>
2020-11-28 01:56:11 +0900

    [FEAT] NumberCompare 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,13 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
+        System.out.println(NumberGenerator.getRandomNumberNotRepeat());
         System.out.println(UserInputNumber.userInputNumber());
 
     }
---
+++ src/main/java/baseball/NumberCompare.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import java.util.ArrayList;
+
+public class NumberCompare {
+
+    private static final int NUMBER_LENGTH = 3;
+    private static ArrayList<Integer> randomNumberList = new ArrayList<>();
+    private static ArrayList<Integer> userNumberList = new ArrayList<>();
+    private static ArrayList<Integer> ballStrikeCountList = new ArrayList<>();
+
+    public NumberCompare(ArrayList<Integer> randomNumberList, ArrayList<Integer> userNumberList) {
+        this.randomNumberList = randomNumberList;
+        this.userNumberList = userNumberList;
+    }
+
+    public static ArrayList<Integer> NumberCompare() {
+        ballStrikeCountList.add(getFinalBallCount());
+        ballStrikeCountList.add(compareStrikeCount());
+
+        return ballStrikeCountList;
+    }
+
+    public static int compareBallCount() {
+        int count = 0;
+        for (int singleUserNumber : userNumberList) {
+            if (randomNumberList.contains(singleUserNumber)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int compareStrikeCount() {
+        int count = 0;
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (userNumberList.get(i) == randomNumberList.get(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int getFinalBallCount() {
+        int count = 0;
+        count = compareBallCount() - compareStrikeCount();
+        return count;
+    }
+}
