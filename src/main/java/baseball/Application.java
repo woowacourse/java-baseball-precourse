5db4ffcf6cebf9f5612acd4419d25ae624ca8c79
amy choi <cgc8016@ewhain.net>
2020-11-27 22:29:37 +0900

    fix(Application): bug fix for random number
    range setting error fix
    all different setting error fix
    +code formatting

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,12 +14,11 @@ public class Application {
         while (!gameOver) {
             int[] choice = new int[3];
             // for all different
-            while (choice[0] == choice[1] || choice[1] == choice[2] || choice[0] == choice[2]) {
-                choice[0] = RandomUtils.nextInt(1, 10);
-                choice[1] = RandomUtils.nextInt(1, 10);
-                choice[2] = RandomUtils.nextInt(1, 10);
+            while (choice[0] == choice[1] | choice[1] == choice[2] | choice[0] == choice[2]) {
+                choice[0] = RandomUtils.nextInt(1, 9);
+                choice[1] = RandomUtils.nextInt(1, 9);
+                choice[2] = RandomUtils.nextInt(1, 9);
             }
-
             gameStart(choice);
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
                 + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
@@ -70,12 +69,12 @@ public class Application {
         int strike = 0;
         int ball = 0;
 
-        for (int i=0; i<3; i++) {
+        for (int i = 0; i < 3; i++) {
             if (guess[i] == choice[i]) {
                 strike++;
                 continue;
             }
-            if (guess[i] == choice[(i+1)%3] | guess[i] == choice[(i-1+3)%3]) {
+            if (guess[i] == choice[(i + 1) % 3] | guess[i] == choice[(i - 1 + 3) % 3]) {
                 ball++;
             }
         }
