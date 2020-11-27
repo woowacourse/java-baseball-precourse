b55164f25840ec49238e67aef44dc51bb7512b52
DWL <DWL>
2020-11-27 17:21:28 +0900

    feat: add maekRandomNumber function in BaseBallGameController

--- src/main/java/baseball/BaseBallGameController.java
+++ src/main/java/baseball/BaseBallGameController.java
@@ -1,11 +1,28 @@
 package baseball;
 
+import utils.RandomUtils;
+import static baseball.Config.*;
+
 public class BaseBallGameController {
     private Player player = null;
     private BaseBallGame game = null;
 
     public BaseBallGameController(Player player) {
         this.player = player;
+    }
+
+    private String makeRandomNumber() {
+        StringBuilder randomNumber = new StringBuilder();
+        while (true) {
+            String num = Integer.toString(RandomUtils.nextInt(START_INCLUSIVE_NUM, END_INCLUSIVE_NUM));
+            if (randomNumber.toString().contains(num)) {
+                continue;
+            }
+            randomNumber.append(num);
+            if (randomNumber.length() == NUMBER_OF_DIGITS) {
+                return randomNumber.toString();
+            }
+        }
     }
 
 }
---
+++ src/main/java/baseball/Config.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Config {
+    public static final int NUMBER_OF_DIGITS = 3;
+    public static final int START_INCLUSIVE_NUM = 1;
+    public static final int END_INCLUSIVE_NUM = 9;
+}
