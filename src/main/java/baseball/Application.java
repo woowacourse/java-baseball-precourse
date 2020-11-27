afb9b1cc77f7d55c42c0d9c09bc8fab895a51890
daeun <leede0418@likelion.org>
2020-11-28 05:01:09 +0900

    refactor(application): scan a number
    - [플레이어] 3자리 수를 입력하는 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,12 +8,11 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        boolean gameStatus = true;
+
         // 플레이어가 3자리 수를 입력한다.
-        do {
-            System.out.println("숫자를 입력해주세요 : ");
-            String scannerNumber = scanner.nextLine();
-            String result = GameController.scanPlayerNumber(scannerNumber);
-            System.out.println(result);
-        } while (GameController.continueGame());
+        while (gameStatus) {
+            GameController.scanPlayerNumber(scanner);
+        }
     }
 }
--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,21 +4,25 @@ import baseball.generator.HintGenerator;
 import utils.InputUtils;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * 게임을 컨트롤하는 클래스
  */
 public class GameController {
+    private static final String PLAYER_NUMBER_INFO = "숫자를 입력해주세요 : ";
     private static final int NUMBER_SIZE = 3;
     private static final int MINIMUM_INDEX = 0;
     private static final int MAXIMUM_INDEX = 2;
-    private static final String ANSWER = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String INFORMATION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private static String result = new String();
+    private static String scannerNumber = new String();
 
     // 플레이어로부터 3자리 수를 입력받는 함수
-    public static String scanPlayerNumber(String scannerNumber) {
+    public static void scanPlayerNumber(Scanner scanner) {
+        System.out.print(PLAYER_NUMBER_INFO);
+        scannerNumber = scanner.nextLine();
+
         // 정상적인 입력인 경우
         if (InputUtils.checkValidation(scannerNumber)) {
             // String 형태를 ArrayList<Integer> 형태로 변환한다.
@@ -26,9 +30,8 @@ public class GameController {
 
             // 플레이어에게 힌트를 제공한다.
             result = HintGenerator.giveHint(playerNumber);
+            System.out.println(result);
         }
-
-        return result;
     }
 
     // String 형태를 ArrayList<Integer> 형태로 변환하는 함수
@@ -41,14 +44,5 @@ public class GameController {
         }
 
         return playerNumber;
-    }
-
-    // 프로그램의 3자리 숫자와 플레이어의 3자리 숫자가 같은 경우 게임을 종료하는 함수
-    public static boolean continueGame() {
-        if (result.equals(NUMBER_SIZE+ANSWER)) {
-            System.out.println(INFORMATION);
-            return false;
-        }
-        return true;
     }
 }
