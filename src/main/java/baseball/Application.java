b406b0ee6eb67ea66c1710407cf101f7ade152cb
sakjung <58401309+sakjung@users.noreply.github.com>
2020-11-27 17:15:21 +0900

    refactor: 가독성을 위해 Constants 클래스 없애고 Application 클래스 밑으로 상수 이동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,32 +1,9 @@
 package baseball;
 
 import java.util.Scanner;
-
-import com.sun.org.apache.bcel.internal.Const;
 import utils.RandomUtils;
 import java.util.HashSet;
 import java.util.Set;
-
-final class Constants {
-    // RANDOM NUMBER
-    public static final int NUMBER_START_INCLUSIVE = 100;
-    public static final int NUMBER_END_INCLUSIVE = 999;
-
-    // GAME PLAY
-    public static final String GUESS_INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
-    public static final String GUESS_INPUT_ERROR_MESSAGE = "1 에서 9 사이의 서로 다른 숫자로 이루어진 3자리 정수만 입력 가능합니다";
-
-    // HINT
-    public static final String BALL = "볼";
-    public static final String STRIKE = "스트라이크";
-    public static final String NOTHING = "낫싱";
-
-    // AFTER GAME
-    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String RESTART_OR_QUIT_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    public static final String RESTART_OR_QUIT_INPUT_ERROR_MESSAGE = "1 혹은 2만 입력 가능합니다";
-    public static final String QUIT_MESSAGE = "GG";
-}
 
 // Strike, Ball 카운트를 위한 클래스
 class StrikeAndBall {
@@ -48,6 +25,25 @@ class StrikeAndBall {
 }
 
 public class Application {
+    // RANDOM NUMBER
+    public static final int NUMBER_START_INCLUSIVE = 100;
+    public static final int NUMBER_END_INCLUSIVE = 999;
+
+    // GAME PLAY
+    public static final String GUESS_INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    public static final String GUESS_INPUT_ERROR_MESSAGE = "1 에서 9 사이의 서로 다른 숫자로 이루어진 3자리 정수만 입력 가능합니다";
+
+    // HINT
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    // AFTER GAME
+    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_QUIT_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART_OR_QUIT_INPUT_ERROR_MESSAGE = "1 혹은 2만 입력 가능합니다";
+    public static final String QUIT_MESSAGE = "GG";
+
     public static int[] intToArray(int num) {
         int[] this_array = new int[3];
         for (int i = 2; i > -1; i--) {
@@ -74,13 +70,13 @@ public class Application {
     public static int getRandomNum() {
         int randomNum;
         do {
-            randomNum = RandomUtils.nextInt(Constants.NUMBER_START_INCLUSIVE, Constants.NUMBER_END_INCLUSIVE);
+            randomNum = RandomUtils.nextInt(NUMBER_START_INCLUSIVE, NUMBER_END_INCLUSIVE);
         } while (!isValidNum(randomNum));
         return randomNum;
     }
 
     public static int guessNum(Scanner scanner) throws IllegalArgumentException {
-        System.out.print(Constants.GUESS_INPUT_MESSAGE);
+        System.out.print(GUESS_INPUT_MESSAGE);
         // 정수가 아닌 경우
         if (!scanner.hasNextInt()) {
             throw new IllegalArgumentException();
@@ -104,7 +100,7 @@ public class Application {
                 number = guessNum(scanner);
                 isValidGuess = true;
             } catch (IllegalArgumentException e) {
-                System.out.println(Constants.GUESS_INPUT_ERROR_MESSAGE);
+                System.out.println(GUESS_INPUT_ERROR_MESSAGE);
                 scanner.nextLine();
             }
         }
@@ -154,13 +150,13 @@ public class Application {
         int ballCount = thisTurnResult.getBall();
 
         if (0 < strikeCount && 0 < ballCount) {
-            System.out.println(ballCount + Constants.BALL + " " + strikeCount + Constants.STRIKE);
+            System.out.println(ballCount + BALL + " " + strikeCount + STRIKE);
         } else if (0 < strikeCount && 0 == ballCount) {
-            System.out.println(strikeCount + Constants.STRIKE);
+            System.out.println(strikeCount + STRIKE);
         } else if (0 == strikeCount && 0 < ballCount) {
-            System.out.println(ballCount + Constants.BALL);
+            System.out.println(ballCount + BALL);
         } else if (0 == strikeCount && 0 == ballCount) {
-            System.out.println(Constants.NOTHING);
+            System.out.println(NOTHING);
         }
         return strikeCount;
     }
@@ -175,7 +171,7 @@ public class Application {
     }
 
     public static void decideGoOrStop(Scanner scanner) throws IllegalArgumentException {
-        System.out.println(Constants.RESTART_OR_QUIT_INPUT_MESSAGE);
+        System.out.println(RESTART_OR_QUIT_INPUT_MESSAGE);
         if (!scanner.hasNextInt()) {
             throw new IllegalArgumentException();
         }
@@ -185,14 +181,14 @@ public class Application {
         if (input == 1) {
             main(null);
         } else if (input == 2) {
-            System.out.println(Constants.QUIT_MESSAGE);
+            System.out.println(QUIT_MESSAGE);
         } else {
             throw new IllegalArgumentException();
         }
     }
 
     public static void goOrStop (Scanner scanner) {
-        System.out.println(Constants.WIN_MESSAGE);
+        System.out.println(WIN_MESSAGE);
         scanner.nextLine();
 
         boolean isValidGoOrStop = false;
@@ -201,7 +197,7 @@ public class Application {
                 decideGoOrStop(scanner);
                 isValidGoOrStop = true;
             } catch (IllegalArgumentException e) {
-                System.out.println(Constants.RESTART_OR_QUIT_INPUT_ERROR_MESSAGE);
+                System.out.println(RESTART_OR_QUIT_INPUT_ERROR_MESSAGE);
                 scanner.nextLine();
             }
         }
