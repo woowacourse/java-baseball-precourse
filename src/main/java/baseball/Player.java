67b8a58f70a6213dc79355e54bad7950b4070220
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-11-27 21:57:32 +0900

    feat: 사용자 입력 기능 구현 및 분리
    기존 프로토타입에 있었던 것들을 분리 하는 작업
    사용자 3자리 숫자 입력
    사용자 메뉴 선택 구현
    입력값에 따른 유효성검사

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Player {
+    private Validator validator = new Validator();
+
+    protected String playerInputNumber(Scanner scanner) {
+        try {
+            System.out.println("숫자를 입력해 주세요 : ");
+            String playerInput = scanner.next();
+            validator.isValidInputNumber(playerInput);
+            System.out.println("inputData: " + playerInput);//확인
+            return playerInput;
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+            return playerInputMenu(scanner);
+        }
+    }
+
+    protected String playerInputMenu(Scanner scanner) {
+        try {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String menu = scanner.next();
+            validator.isValidMenu(menu);
+            System.out.println("menu: " + menu);//확인
+            return menu;
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+            return playerInputMenu(scanner);
+        }
+    }
+}
