2082f44779121af9c20a3dd1258f54b6b2544834
Junhyeong Jo <118jjh@naver.com>
2020-11-27 16:23:13 +0900

    feat: 숫자 중복 체크 배열 초기화 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,12 @@ public class Application {
     static public final Scanner scanner = new Scanner(System.in);
     static public int[] targetNumbers = {0,0,0};
     static public int[] numberChecker = {0,0,0,0,0,0,0,0,0,0};
+
+    public static void initNumberChecker(){
+        for(int i = 0; i < 3; i++){
+            numberChecker[targetNumbers[i]] = 0;
+        }
+    }
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
