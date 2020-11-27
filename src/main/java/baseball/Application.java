d03f025ef7b4fa101f40246307d714cf406b4864
김주원 <kjw11077@naver.com>
2020-11-27 19:56:29 +0900

    feat(Application): add main logic
     게임시작 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        final GameManager gameManager = new GameManager(scanner);
+        gameManager.game();
     }
 }
