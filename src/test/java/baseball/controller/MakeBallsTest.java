0368ca0b137271b72a53fc0467d7d585139213bf
Hyeon9mak <jinha3507@gmail.com>
2020-11-27 09:43:20 +0900

    test(MakeBallTest): add test code for make domains Computer.balls User.balls

--- src/test/java/baseball/controller/MakeBallsTest.java
+++ src/test/java/baseball/controller/MakeBallsTest.java
@@ -1,2 +1,24 @@
-package baseball.controller;public class MakeBallsTest {
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.model.User;
+import org.junit.jupiter.api.Test;
+import utils.InputUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MakeBallsTest {
+    @Test
+    public void computerMakeBallsTest() {
+        Computer.makeBalls();
+
+        assertThat(Computer.balls).isNotEmpty();
+    }
+
+    @Test
+    public void userMakeBallsTest() {
+        User.makeBalls(InputUtils.checkUserInput("123"));
+
+        assertThat(User.balls).isNotEmpty();
+    }
 }
