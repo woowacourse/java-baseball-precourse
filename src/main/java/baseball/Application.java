3578910ecbcecf5ffd700bed7c31cb4776c6877c
sjpark-dev <sjpark.dev@gmail.com>
2020-11-27 13:00:33 +0900

    feat(application): add get player input and check feature
    include check length of player input feature
    include check if number from A to B feature
    include string to number array feature
    reuse checking if all different numbers feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,53 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         int[] computerNumber = generateRandomNumber();
+        int[] playerNumber = getPlayerNumber(scanner);
+    }
+
+    public static int[] getPlayerNumber(Scanner scanner) {
+        int[] playerNumber = new int[3];
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerInput = scanner.nextLine();
+
+        // 3자리 입력인지 체크
+        checkLength(playerInput, 3);
+
+        // 각 자리수가 1에서 9사이의 숫자인지 체크
+        checkIfNumberfromAtoB(playerInput, 1, 9);
+
+        // 숫자 배열화 기능
+        for (int i=0; i<playerInput.length(); i ++) {
+            playerNumber[i] = Integer.parseInt(playerInput.substring(i, i + 1));
+        }
+
+        // 서로 다른 숫자인지 체크
+        if (!checkAllDifferent(playerNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+        return playerNumber;
+    }
+
+    public static void checkLength(String playerInput, int requiredLength) {
+        if (playerInput.length() != requiredLength) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 각 자리수가 A에서 B사이의 숫자인지 체크하는 기능
+    public static void checkIfNumberfromAtoB(String playerInput, int fromA, int toB) {
+        char fromAChar = Integer.toString(fromA).charAt(0);
+        char toBChar = Integer.toString(toB).charAt(0);
+
+        for (int i=0; i<playerInput.length(); i++) {
+            char temp = playerInput.charAt(i);
+
+            if (fromAChar <= temp && temp <= toBChar) {
+                continue;
+            }
+
+            throw new IllegalArgumentException();
+            }
     }
 
     // 컴퓨터의 랜덤 숫자 생성 기능
