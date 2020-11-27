a1673dd48cc3ee43eb018f4a22d63d3295f55460
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-28 01:24:12 +0900

    refactor: 상수에 누락된 static 코드 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -6,9 +6,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Computer {
-    private final int NUMBERS_LENGTH = 3;
-    private final int MIN_NUMBER = 1;
-    private final int MAX_NUMBER = 9;
+    private static final int NUMBERS_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
     private StringBuilder stringBuilder;
     private Set<Integer> numbersSet;
 
