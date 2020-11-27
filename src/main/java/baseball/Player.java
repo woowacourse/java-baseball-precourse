6d50cf1f9c40361d000d468d66d5aa4487662de5
DWL <DWL>
2020-11-27 16:58:59 +0900

    feat: add inputNumber function in Player

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,10 +2,20 @@ package baseball;
 
 import java.util.Scanner;
 
+import static utils.TextResource.STATEMENT_NUMBER_INPUT;
+
 public class Player {
     private Scanner scanner = null;
 
     public Player(Scanner scanner) {
         this.scanner = scanner;
+    }
+
+    public String inputNumber() {
+        scanner.reset();
+        System.out.print(STATEMENT_NUMBER_INPUT);
+        System.out.print("");
+        String inputNumber = scanner.next();
+        return inputNumber;
     }
 }
