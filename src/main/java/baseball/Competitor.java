89de3a5f129001d3dacf047f86346bdbfded459b
JUNGHOJONG <hojong1351@naver.com>
2020-11-27 12:05:58 +0900

    fix(baseball): 경쟁자(컴퓨터)가 서로 다른 3개의 숫자 랜덤으로 생성하는 기능 수정
    LinkedHashSet의 toString함수는 [1, 2, 3] 형식으로 출력되기 때문에 플레이어가
    입력하는 123 형식과 일치하지 않는다. 경쟁자의 랜덤 문자열 길이와 플레이어의
    입력 문자열 길이가 일치하지 않아 Comparator클래스의 getBallCount함수에서
    StringIndexOutOfBoundsException오류 발생. 이를 해결하기 위해 LinkedHashSet을
    순회하며 StringBuilder에 랜덤 숫자를 모두 저장 후 출력

--- src/main/java/baseball/Competitor.java
+++ src/main/java/baseball/Competitor.java
@@ -6,7 +6,6 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Competitor {
-
     private static final int FIRST_BOUNDARY_VALUE = 1;
     private static final int LAST_BOUNDARY_VALUE = 9;
     private static final int RANDOM_NUMBER_COUNT = 3;
@@ -16,7 +15,14 @@ public class Competitor {
         while(randomNumbers.size() != RANDOM_NUMBER_COUNT){
             randomNumbers.add(RandomUtils.nextInt(FIRST_BOUNDARY_VALUE, LAST_BOUNDARY_VALUE));
         }
-        return randomNumbers.toString();
+        return getRandomNumbersConvertedToString(randomNumbers);
     }
 
+    private static String getRandomNumbersConvertedToString(Set<Integer> randomNumbers){
+        StringBuilder sb = new StringBuilder();
+        for(Integer randomNumber : randomNumbers){
+            sb.append(randomNumber);
+        }
+        return sb.toString();
+    }
 }
