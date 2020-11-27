107d9e77bc03e85fd0cabe986c39791a9e68ce87
483759 <483759@naver.com>
2020-11-27 20:44:43 +0900

    nextInt 함수를 호출하여 난수 생성

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -5,10 +5,18 @@ import java.util.Random;
 public class RandomUtils {
 
     private static final Random RANDOM = new Random();
+    private static final int INPUT_NUM = 3;
+    public int[] comNumbers;
 
     private RandomUtils() {
+        comNumbers=new int[INPUT_NUM];
+        for(int i=0;i<INPUT_NUM;i++){
+            //comNumbers에 들어갈 숫자를 nextInt 함수의 호출로 지정
+            comNumbers[i]=nextInt(0,9);
+        }
     }
 
+    /* Random 생성한 배열에 들어갈 특정 범위의 숫자를 정하는 함수 */
     public static int nextInt(final int startInclusive, final int endInclusive) {
         try {
             if (startInclusive > endInclusive) {
