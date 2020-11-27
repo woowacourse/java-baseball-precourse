03b55d24a2e02e4afe2b4a744a10aa12c3122129
Sehwan_Jang <whilter08@gmail.com>
2020-11-27 20:58:23 +0900

    feat: Add hint printing code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -57,7 +57,25 @@ public class Application {
                 ballCount++;
             }
         }
+        printHint(ballCount, strikeCount);
 
         return strikeCount == 3;
+    }
+
+    public static void printHint(int ballCount, int strikeCount) {
+        if (strikeCount == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n" +
+                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        } else if (strikeCount + ballCount == 0) {
+            System.out.println("낫싱");
+        } else {
+            if (ballCount != 0) {
+                System.out.print(ballCount + "볼 ");
+            }
+            if (strikeCount != 0) {
+                System.out.print(strikeCount + "스트라이크");
+            }
+            System.out.println();
+        }
     }
 }
