c28d969f20d736a977d647a24fda696007f96735
Hyeon9mak <jinha3507@gmail.com>
2020-11-27 12:04:34 +0900

    feat(ResultView): create print baseballgame result code ResultView.java
    printPleaseInput()
    printGameResult()
    appendBall()
    appendStrike()
    appendSpace()
    printGameEnd()

---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,50 @@
+package baseball.view;
+
+public class ResultView {
+    private static StringBuilder printBallStrike;
+    private static final int ZERO = 0;
+
+    public static void printPleaseInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printGameResult(int ball, int strike) {
+        printBallStrike.setLength(ZERO);    // reset
+
+        appendBall(ball);
+        appendSpace();
+        appendStrike(strike);
+
+        if (printBallStrike.length() == ZERO) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        System.out.println(printBallStrike);
+    }
+
+    private static void appendBall(int ball) {
+        if (ball > ZERO) {
+            printBallStrike.append(ball);
+            printBallStrike.append("볼");
+        }
+    }
+
+    private static void appendSpace() {
+        if (printBallStrike.length() > ZERO) {
+            printBallStrike.append(" ");
+        }
+    }
+
+    private static void appendStrike(int strike) {
+        if (strike > ZERO) {
+            printBallStrike.append(strike);
+            printBallStrike.append("스트라이크");
+        }
+    }
+
+    public static void printGameEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
