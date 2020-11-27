8c87bb936eff471699622d394da0683bd0d69089
김주원 <kjw11077@naver.com>
2020-11-27 17:42:51 +0900

    refactor(Balls): optimize import

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -2,10 +2,7 @@ package baseball;
 
 import utils.RandomUtils;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Balls {
     private final List<Ball> balls;
