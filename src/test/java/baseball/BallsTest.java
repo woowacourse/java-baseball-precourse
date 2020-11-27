720c1006db1c2cb6b2527ca77ce32d2ddbb424e7
김주원 <kjw11077@naver.com>
2020-11-27 18:05:58 +0900

    test(BallTest): add unittest

---
+++ src/test/java/baseball/BallsTest.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class BallsTest {
+
+    @Test
+    void validateSize() {
+        assertThrows(IllegalArgumentException.class, () -> Balls.stringToBalls("1234"));
+        assertThrows(IllegalArgumentException.class, () -> Balls.stringToBalls("12"));
+        assertThrows(IllegalArgumentException.class, () -> Balls.stringToBalls(""));
+        assertDoesNotThrow(() -> Balls.stringToBalls("123"));
+    }
+
+    @Test
+    void validateDuplicate() {
+        assertThrows(IllegalArgumentException.class, () -> Balls.stringToBalls("111"));
+        assertThrows(IllegalArgumentException.class, () -> Balls.stringToBalls("999"));
+        assertDoesNotThrow(() -> Balls.stringToBalls("123"));
+        assertDoesNotThrow(() -> Balls.stringToBalls("159"));
+    }
+}
