1f9c740bf068b486374a092f2fa4e4477de90f6a
yhh1056 <yhh1056@naver.com>
2020-11-27 15:46:41 +0900

    feat(exception): invalid size baseball number

--- src/main/java/domain/BaseballNumber.java
+++ src/main/java/domain/BaseballNumber.java
@@ -7,9 +7,18 @@ import java.util.List;
  * @since 2020/11/26
  */
 public class BaseballNumber {
+    public static final int BASEBALL_LIST_SIZE = 3;
+
     private final List<Integer> baseballNumbers;
 
     public BaseballNumber(final List<Integer> baseballNumbers) {
+        validateSize(baseballNumbers);
         this.baseballNumbers = baseballNumbers;
+    }
+
+    private void validateSize(List<Integer> baseballNumbers) {
+        if (baseballNumbers.size() != BASEBALL_LIST_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 }
