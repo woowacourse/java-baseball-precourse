2b512768d17bdba500de6d36ede124ea29435a9a
DWL <DWL>
2020-11-27 16:55:22 +0900

    feat: add Player class

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Player {
+    private Scanner scanner = null;
+
+    public Player(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
