44852e5dbd131e618213745416f3d84e59f8645f
DWL <DWL>
2020-11-27 17:20:14 +0900

    feat: add BaseBallGameController Class

---
+++ src/main/java/baseball/BaseBallGameController.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class BaseBallGameController {
+    private Player player = null;
+    private BaseBallGame game = null;
+
+    public BaseBallGameController(Player player) {
+        this.player = player;
+    }
+
+}
