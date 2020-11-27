f63e38395b4e9003241f066312d83f20aadb5cf9
SiHyung92 <fjzjqhdl@gmail.com>
2020-11-27 14:38:34 +0900

    feat : 숫자를 입력받으면 적절한 값인지 검증하는 기능 추가
    조건1. 3글자인지
    조건2. 숫자인지
    조건3. 0이 없는지
    조건4. 각 자릿수가 서로 중복되지 않는지

--- src/main/java/baseball/ConsoleView.java
+++ src/main/java/baseball/ConsoleView.java
@@ -1,19 +1,65 @@
 package baseball;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class ConsoleView {
-    private final Scanner scanner;
+    private final Scanner SCANNER;
+    private final int DIGITS = 3;
+    private final char ZERO = '0';
+    private final String REQUEST_INPUT_NUMBER_MSG = "숫자를 입력해주세요 : ";
+    private final String WRONG_INPUT_MSG = "입력값 형식을 확인해주세요!";
+
     public ConsoleView(Scanner scanner) {
-        this.scanner = scanner;
+        this.SCANNER = scanner;
     }
 
-    public String requestNumber(){
-        println("숫자를 입력해주세요 : ");
-        return scanner.nextLine();
+    public String requestNumber() {
+        println(REQUEST_INPUT_NUMBER_MSG);
+        String inputNumber = SCANNER.nextLine();
+        if (isRightFormatted(inputNumber)) {
+            return inputNumber;
+        }
+        throw new IllegalArgumentException(WRONG_INPUT_MSG);
     }
 
-    public void println(String message){
+    private boolean isRightFormatted(String inputNumber) {
+        char[] numbers = inputNumber.toCharArray();
+        if (inputNumber.length() != DIGITS) {
+            return false;
+        }
+        if (!isDigitAndNotZero(numbers)) {
+            return false;
+        }
+        if (!isEachDigitDifferent(numbers)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isEachDigitDifferent(char[] numbers) {
+        HashSet<Character> duplicateChecker = new HashSet<>();
+        for (char number : numbers) {
+            duplicateChecker.add(number);
+        }
+
+        return duplicateChecker.size() == DIGITS;
+    }
+
+    private boolean isDigitAndNotZero(char[] numbers) {
+        for (char number : numbers) {
+            if (!Character.isDigit(number)) {
+                return false;
+            }
+
+            if (number == ZERO) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void println(String message) {
         System.out.println(message);
     }
 }
