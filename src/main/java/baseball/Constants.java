df21c8c1f21aaa2faf6173055b7f633b5ef2c7f0
Younghoon Kwon <younghoon.kwon91@gmail.com>
2020-11-27 14:17:24 +0900

    feat: add constant string message which checks duplicate

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static final char MAX_VALID_INPUT_DIGIT = '9';
     public static final String PROMPT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String ERR_MSG_VALID_LENGTH = "Digits should be length of 3";
+    public static final String ERR_MSG_DUPLICATE_DIGITS = "Digits should be without duplication";
     public static final String ERR_MSG_VALID_INPUT_PLAYMORE = "Input should be 1 or 2";
     public static final String ERR_MSG_VALID_DIGITS = "Each digits should be 0<value<=9";
     public static final String ASK_PLAY_MORE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n게임을 새로 시작하려면 1, " +
