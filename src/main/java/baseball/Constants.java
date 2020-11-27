e216bade1217fd8ee3723bc783e466e8f3c991c4
pika96 <52442949+pika96@users.noreply.github.com>
2020-11-28 05:03:43 +0900

    상수 선언

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Constants {
+    public final static int MIN_NUMBER = 1;
+    public final static int MAX_NUMBER = 9;
+    public final static int NUMBER_LEN = 3;
+    public final static int RESTART = 1;
+    public final static int EXIT = 2;
+}
