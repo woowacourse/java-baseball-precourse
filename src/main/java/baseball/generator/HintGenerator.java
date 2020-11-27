376466a5cfad4df0ca6deec0f7bc1d944cb96da2
daeun <leede0418@likelion.org>
2020-11-28 02:26:48 +0900

    fix(hint-generator): initialize ball and strike
    - [힌트] 플레이어가 입력한 수에 대한 힌트를 제공하는 기능 수정
    - `볼`, `스트라이크`를 초기화하는 함수 추가

--- src/main/java/baseball/generator/HintGenerator.java
+++ src/main/java/baseball/generator/HintGenerator.java
@@ -9,26 +9,21 @@ public class HintGenerator {
     private static final int INITIAL_VALUE = 0;
     private static final int MINIMUM_INDEX = 0;
     private static final int MAXIMUM_INDEX = 2;
-    private static final String TRUE = "true";
-    private static final String FALSE = "false";
     private static final String ANSWER = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String NOTHING = "낫싱";
     private static final String BALL = "볼";
     private static final String STRIKE = "스트라이크";
 
-    static int ball;
-    static int strike;
-
-    private HintGenerator() {
-        ball = INITIAL_VALUE;
-        strike = INITIAL_VALUE;
-    }
+    private static int ball;
+    private static int strike;
 
     // 플레이어가 입력한 수에 대한 힌트를 제공하는 함수
     public static String giveHint(ArrayList<Integer> playerNumber) {
         // 프로그램이 정한 임의의 3자리 수
         ArrayList<Integer> programNumber = NumberGenerator.generateProgramNumber();
 
+        // `볼`, `스트라이크`를 초기화한다.
+        initBallStrike();
         // 같은 숫자가 있는지 확인한다. (`볼` 개수 확인)
         checkSameNumber(programNumber, playerNumber);
         // 같은 자리에 있는지 확인한다. (`스트라이크` 개수 확인)
@@ -38,6 +33,12 @@ public class HintGenerator {
         String result = checkResult();
 
         return result;
+    }
+
+    // `볼`, `스트라이크`를 초기화하는 함수
+    public static void initBallStrike() {
+        ball = INITIAL_VALUE;
+        strike = INITIAL_VALUE;
     }
 
     // 같은 숫자가 있는지 확인하는 함수 (`볼` 개수 확인)
