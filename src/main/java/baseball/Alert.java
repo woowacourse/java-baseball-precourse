c02c823bf819a8530b265b3ed19d06fd1b4a76be
bangjom <kbj7227@gmail.com>
2020-11-28 05:23:14 +0900

    feat(alert): create messages

---
+++ src/main/java/baseball/Alert.java
@@ -0,0 +1,49 @@
+package baseball;
+
+public class Alert {
+    private static final String NUMBER_INPUT = "숫자를 입력해주세요 : ";
+    private static final String ALL_CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final int ZERO = 0;
+
+    public static void enterNumber(){
+        System.out.println(NUMBER_INPUT);
+    }
+
+    public static void finishGame(){
+        System.out.println(ALL_CORRECT);
+    }
+
+    public static void askToRestart(){
+        System.out.println(RESTART);
+    }
+
+    public static void markScore(int ball, int strike){
+        if(ball == ZERO && strike == ZERO){
+            sayNothing();
+        }
+        if(ball == ZERO){
+            sayOnlyStrike(strike);
+        }
+        if(strike == ZERO){
+            sayOnlyBall(ball);
+        }
+        System.out.println(ball+BALL+" "+strike+STRIKE);
+    }
+
+
+    private static void sayOnlyBall(int ball) {
+        System.out.println(ball+BALL);
+    }
+
+    private static void sayOnlyStrike(int strike) {
+        System.out.println(strike+STRIKE);
+    }
+
+    private static void sayNothing() {
+        System.out.println(NOTHING);
+    }
+}
