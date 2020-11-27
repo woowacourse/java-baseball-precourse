fc0db6f57f0e871bb7d14912feaf7bfb408e12ba
Jun0922 <tjrwns126@gmail.com>
2020-11-27 16:31:05 +0900

    feat: 컴퓨터가 선택할 값을 결정할 메소드 구현 -게임이 시작되면 컴퓨터는 1~9의 숫자를 세개 중복되지 않게 선택하여 저장한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,6 +10,8 @@ public class Application {
         // TODO 구현 진행
         boolean gameCoin = true;
         while(gameCoin){
+            String computerChoice = chooseComputerChoice();
+            System.out.println(computerChoice);
             gameCoin = checkGameCoin();
         }
     }
@@ -17,5 +21,20 @@ public class Application {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int gameCoin = sc.nextInt();
         return gameCoin == 1;
+    }
+
+    static String chooseComputerChoice(){
+        int[] answer = new int[3];
+        String computerChoice;
+        answer[0] = RandomUtils.nextInt(1,9);
+        do{
+            answer[1] = RandomUtils.nextInt(1,9);
+        }while(answer[0]==answer[1]);
+        do{
+            answer[2] = RandomUtils.nextInt(1,9);
+        }while(answer[0]==answer[2] || answer[1]==answer[2]);
+        
+        computerChoice = Arrays.toString(answer).replaceAll("[^0-9]","");
+        return computerChoice;
     }
 }
