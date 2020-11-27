9f3c85111cc94dcd7aeb1d982400ade0cdace74f
bluewood <bluewood.truth@gmail.com>
2020-11-27 12:18:29 +0900

    test: NumberGenerator에 대한 단위 테스트 작성
    다음의 경우에 대한 테스트 작성:
    - 생성된 숫자는 각 자리수가 서로 다른가
    - 각 자리수는 1에서 9까지 무작위로 선택되었는가 (골고루 선택되었는가)

---
+++ src/test/java/modules/NumberGeneratorTest.java
@@ -0,0 +1,71 @@
+package modules;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class NumberGeneratorTest {
+    static final int TEST_ITERATION = 10000;
+
+    @Test
+    public void checkGeneratedNumberNotDuplicated(){
+        NumberGenerator numberGenerator = new NumberGenerator();
+        boolean isNotDuplicated = true;
+
+        for(int i = 0; i < TEST_ITERATION; i++){
+            numberGenerator.generate();
+            int[] generatedNumber = numberGenerator.getNumber();
+            if(!isUniqueNumber(generatedNumber)){
+                isNotDuplicated = false;
+                break;
+            }
+        }
+
+        assertEquals(true, isNotDuplicated);
+    }
+
+    @Test
+    public void checkGeneratedNumberActuallyRandom(){
+        NumberGenerator numberGenerator = new NumberGenerator();
+        HashMap<Integer, Integer> numberCounter = new HashMap<>();
+
+        for(int i = 0; i < TEST_ITERATION; i++){
+            numberGenerator.generate();
+            int[] generatedNumber = numberGenerator.getNumber();
+            count(numberCounter, generatedNumber);
+        }
+
+        // 모든 숫자가 골고루 나왔는지 눈으로 확인한다
+        for(int key : numberCounter.keySet()){
+            System.out.printf("%d : %d%n", key, numberCounter.get(key));
+        }
+    }
+    
+    private boolean isUniqueNumber(int[] number){
+        // TODO 인수값에 대한 예외처리
+
+        HashMap<Integer, Integer> known = new HashMap<>();
+
+        for(int i = 0; i < number.length; i++){
+            int currentNumber = number[i];
+            if(known.containsKey(currentNumber)){
+                return false;
+            }
+            known.put(currentNumber, currentNumber);
+        }
+        return true;
+    }
+
+    private void count(HashMap<Integer, Integer> counter, int[] number){
+        // TODO 인수값에 대한 예외처리
+
+        for(int i = 0; i < number.length; i++){
+            int currentNumber = number[i];
+            if(!counter.containsKey(currentNumber)){
+                counter.put(currentNumber, 0);
+            }
+            counter.put(currentNumber, counter.get(currentNumber) + 1);
+        }
+    }
+}
