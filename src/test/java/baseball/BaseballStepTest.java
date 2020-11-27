0081a7f72af337f98d8a9fe7e034d795d1df599b
Jinho <jh8579@gmail.com>
2020-11-27 16:47:22 +0900

    test(baseball): add test code for BaseballStep class
    add 5 test case
    - check calculating strike when more than one same number and order
    - check calculating ball when more than one only same number
    - check calculating strike and ball when both strike and ball case
    - check calculating nothing when any same number or order
    - check ending game when all same number and order

---
+++ src/test/java/baseball/BaseballStepTest.java
@@ -0,0 +1,64 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class BaseballStepTest {
+
+    @Test
+    public void Should_ReturnStrike_When_SameNumberAndOrderMoreThanOne() {
+        int[] randomNumbers = new int[]{1, 2, 3};
+        int[] plauerNumbers = new int[]{1, 4, 5};
+
+        BaseballStep baseballStep = new BaseballStep(randomNumbers, plauerNumbers);
+        baseballStep.compare();
+
+        assertThat(baseballStep.toString()).isEqualTo("1스트라이크");
+    }
+
+    @Test
+    public void Should_ReturnBall_When_OnlySameNumberMoreThanOne() {
+        int[] randomNumbers = new int[]{1, 2, 3};
+        int[] plauerNumbers = new int[]{2, 1, 5};
+
+        BaseballStep baseballStep = new BaseballStep(randomNumbers, plauerNumbers);
+        baseballStep.compare();
+
+        assertThat(baseballStep.toString()).isEqualTo("2볼");
+    }
+
+    @Test
+    public void Should_ReturnStrikeAndBall_When_BothSameNumberAndSameNumberOrder() {
+        int[] randomNumbers = new int[]{1, 2, 3};
+        int[] plauerNumbers = new int[]{2, 1, 3};
+
+        BaseballStep baseballStep = new BaseballStep(randomNumbers, plauerNumbers);
+        baseballStep.compare();
+
+        assertThat(baseballStep.toString()).isEqualTo("2볼 1스트라이크");
+    }
+
+    @Test
+    public void Should_ReturnNothing_When_AnySameNumberAndOrder() {
+        int[] randomNumbers = new int[]{1, 2, 3};
+        int[] plauerNumbers = new int[]{4, 5, 6};
+
+        BaseballStep baseballStep = new BaseballStep(randomNumbers, plauerNumbers);
+        baseballStep.compare();
+
+        assertThat(baseballStep.toString()).isEqualTo("낫싱");
+    }
+
+    @Test
+    public void Should_EndGame_When_AllSameNumberAndOrder() {
+        int[] randomNumbers = new int[]{1, 2, 3};
+        int[] plauerNumbers = new int[]{1, 2, 3};
+
+        BaseballStep baseballStep = new BaseballStep(randomNumbers, plauerNumbers);
+        baseballStep.compare();
+
+        assertThat(baseballStep.isGameEnded()).isEqualTo(true);
+    }
+
+}
