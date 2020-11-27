b6d9616186899c7abd900f26f2a51391c1967a36
JungYoonSung <ybook2012@gmail.com>
2020-11-27 17:12:32 +0900

    refactor : delete GameSetting
    * 객체가 아닌 BaseBallGame의 메소드로 분리

--- src/main/java/baseball/GameSetting.java
+++
@@ -1,9 +0,0 @@
-package baseball;
-
-public class GameSetting {
-    public RandomNumberGenerator randomNumberGenerator;
-    public void makeRandomNumber(){
-        randomNumberGenerator = new RandomNumberGenerator();
-        randomNumberGenerator.makeRandomNumber();
-    }
-}
