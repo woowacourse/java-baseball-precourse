193b2143299244d45fe5b1d574d249c2ff0237e2
sounmind <sounmind777@gmail.com>
2020-11-27 23:19:41 +0900

    feat(inputException): 프로그램과 사용자가 중복 없이 숫자를 입력하도록 예외처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,47 +6,57 @@ import java.util.Scanner;
 
 
 public class Application {
+    static final int NUMBER_LENGTH = 3;
+    static final int MIN_NUMBER = 1;
+    static final int MAX_NUMBER = 9;
+    static int[] NumbersPickedByComputer = new int[NUMBER_LENGTH];
+    static int[] NumbersPickedByPlayer = new int[NUMBER_LENGTH];
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        final int NUMBER_LENGTH = 3;
-        final int MIN_NUMBER = 1;
-        final int MAX_NUMBER = 9;
-        int[] computerPickedNumbers = new int[NUMBER_LENGTH];
-        int[] playerPickedNumbers = new int[NUMBER_LENGTH];
-
-        // 프로그램이 임의(1~9)의 수 3개를 정하도록 하기
-        for(int i = 0; i < NUMBER_LENGTH; i++) {
-            computerPickedNumbers[i] = RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
-        }
-
+        inputRandomAndNoDuplicationNumbers(NumbersPickedByComputer);
+        printArray(NumbersPickedByComputer);
         System.out.print("숫자를 입력해주세요 : ");
         String playerInputString[] = scanner.next().split("");
-        if(!isRightLength(playerInputString, NUMBER_LENGTH))
-            throw new IllegalArgumentException();
-        if(!AreNumbers(playerInputString))
-            throw new IllegalArgumentException();
-        for(int i = 0; i < NUMBER_LENGTH; i++) {
-            playerPickedNumbers[i] = Integer.parseInt(playerInputString[i]);
-        }
+        inputPlayerNumbersToArray(playerInputString);
 
         // 비교하기 -> 정답 맞출 경우 종료
         for(int i = 0; i < NUMBER_LENGTH; i++) {
-            if(computerPickedNumbers[i] == playerPickedNumbers[i]){
+            if(NumbersPickedByComputer[i] == NumbersPickedByPlayer[i]){
                 System.out.println("정답!");
             } else {
                 System.out.println("오답!");
             }
         }
-
         // 결과 출력
         // 다시 할지 묻기
         // 비정상적 입력에 대해서는 IllegalArgumentException을 발생시킨다.
 
     }
-    public static boolean isRightLength(String[] string, int length_limit) {
-        if(string.length != length_limit) {
-            System.out.println(length_limit+" 자리의 숫자를 입력해주세요!");
+
+    public static boolean isValidInput(String[] string) {
+        if(!isRightLength(string, NUMBER_LENGTH) || !AreNumbers(string))
+            return false;
+        return true;
+    }
+
+    public static void inputPlayerNumbersToArray(String[] playerInputString) {
+        if(!isValidInput(playerInputString))
+            throw new IllegalArgumentException();
+        for(int i = 0; i < NUMBER_LENGTH; i++) {
+            int numberPlayerInput = Integer.parseInt(playerInputString[i]);
+            if(isSameDigitInArray(NumbersPickedByPlayer, numberPlayerInput)) {
+                System.out.println("중복된 숫자가 있어요!");
+                throw new IllegalArgumentException();
+            }
+            NumbersPickedByPlayer[i] = numberPlayerInput;
+        }
+    }
+
+    public static boolean isRightLength(String[] string, int lengthLimit) {
+        if(string.length != lengthLimit) {
+            System.out.println(lengthLimit+" 자리의 숫자를 입력해주세요!");
             return false;
         }
         return true;
@@ -56,11 +66,35 @@ public class Application {
             try {
                 Integer.parseInt(string[i]);
             } catch (Exception e) {
-                System.out.println("숫자를 입력해주세요!");
+                System.out.println("숫자(1~9)를 입력해주세요!");
                 return false;
             }
         }
         return true;
+    }
+    public static boolean isSameDigitInArray(int[] array, int digit) {
+        for(int i = 0; i < array.length; i++) {
+            if(digit == array[i])
+                return true;
+        }
+        return false;
+    }
+
+    public static void inputRandomAndNoDuplicationNumbers(int[] array){
+        for(int i = 0; i < NUMBER_LENGTH; i++) {
+            int newDigit = RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
+            if(isSameDigitInArray(array, newDigit)) {
+                i -= 1;
+                continue;
+            }
+            array[i] = newDigit;
+        }
+    }
+
+    public static void printArray(int[] array){
+        for(int i:array)
+            System.out.printf("%d ", i);
+        System.out.println();
     }
 
 
