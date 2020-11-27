38cbc6a5ffb0a6804746d9ad8c0a1fb99a8ba6f3
Jun0922 <tjrwns126@gmail.com>
2020-11-27 16:24:36 +0900

    feat: 게임 실행 구현 - 처음에 게임을 실행한다. - 게임이 끝난 후, 1이 입력되면 게임이 계속 다시 실행된다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,16 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        boolean gameCoin = true;
+        while(gameCoin){
+            gameCoin = checkGameCoin();
+        }
+    }
+
+    static boolean checkGameCoin(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int gameCoin = sc.nextInt();
+        return gameCoin == 1;
     }
 }
