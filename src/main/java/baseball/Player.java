35e7e4cca64f1affffb5751f46f1af23f426b79f
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-11-28 00:25:34 +0900

    feat(Player): 플레이어에게 입력받은 숫자 저장

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,13 @@
 package baseball;
 
+import java.util.List;
+
 public class Player {
+    private List<Integer> numbers;
+
+    public Player() {}
+
+    public void changeNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
 }
