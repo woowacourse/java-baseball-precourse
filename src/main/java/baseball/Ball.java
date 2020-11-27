29ba6f6536cfff1dd45487c1819c874d79afa28c
김주원 <kjw11077@naver.com>
2020-11-27 17:38:30 +0900

    feat(Ball): override hashcode()
    HashSet으로 중복 검사를 위해 hashcode() 재정의

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -24,4 +24,15 @@ public class Ball {
         }
         return this.number.equals(ball.number);
     }
+
+    @Override
+    public int hashCode() {
+
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
+
+        return result;
+    }
 }
