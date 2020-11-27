25ce2ac4a10f3144f4d7cf3a91da5a25c3a36460
Jun0922 <tjrwns126@gmail.com>
2020-11-27 16:48:30 +0900

    feat: 힌트를 제공하는 메서드 구현 - strike와 ball의 정보를 출력해준다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -73,6 +73,7 @@ public class Application {
             }
         }
         ball -= strike;
+        callUmpire(strike,ball);
         return strike;
     }
 
@@ -84,6 +85,18 @@ public class Application {
             }
         }
         return ball;
+    }
+    
+    static void callUmpire(int strike, int ball){
+        if(strike == 0 && ball == 0){
+            System.out.println("낫싱");
+        }else if(strike == 0 && ball != 0){
+            System.out.println(ball+"볼");
+        }else if(strike !=0 && ball == 0){
+            System.out.println(strike+"스트라이크");
+        }else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
     }
 
 }
