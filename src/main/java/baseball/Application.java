210a6208e6885c9322c703f70166af43a2b28aa0
sakjung <58401309+sakjung@users.noreply.github.com>
2020-11-27 16:26:16 +0900

    feat: 상수 클래스를 따로 추가
    style: 상수 클래스 추가됨에 따라 그에 맞게 내용 스타일 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,35 @@
 package baseball;
 
 import java.util.Scanner;
+
+import com.sun.org.apache.bcel.internal.Const;
 import utils.RandomUtils;
 import java.util.HashSet;
 import java.util.Set;
 
+final class Constants {
+    // RANDOM NUMBER
+    public static final int NUMBER_START_INCLUSIVE = 100;
+    public static final int NUMBER_END_INCLUSIVE = 999;
+
+    // GAME PLAY
+    public static final String GUESS_INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    public static final String GUESS_INPUT_ERROR_MESSAGE = "1 에서 9 사이의 서로 다른 숫자로 이루어진 3자리 정수만 입력 가능합니다";
+
+    // HINT
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    // AFTER GAME
+    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_QUIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART_OR_QUIT_INPUT_ERROR_MESSAGE = "1 혹은 2만 입력 가능합니다";
+    public static final String QUIT_MESSAGE = "GG";
+}
+
 // Strike, Ball 카운트를 위한 클래스
-final class StrikeAndBall {
+class StrikeAndBall {
     int strike;
     int ball;
 
@@ -25,7 +48,6 @@ final class StrikeAndBall {
 }
 
 public class Application {
-
     public static int[] intToArray(int num) {
         int[] this_array = new int[3];
         for (int i = 2; i > -1; i--) {
@@ -49,8 +71,16 @@ public class Application {
         return true;
     }
 
+    public static int getRandomNum() {
+        int randomNum;
+        do {
+            randomNum = RandomUtils.nextInt(Constants.NUMBER_START_INCLUSIVE, Constants.NUMBER_END_INCLUSIVE);
+        } while (!isValidNum(randomNum));
+        return randomNum;
+    }
+
     public static int guessNum(Scanner scanner) throws IllegalArgumentException {
-        System.out.print("숫자를 입력해 주세요 : ");
+        System.out.print(Constants.GUESS_INPUT_MESSAGE);
         // 정수가 아닌 경우
         if (!scanner.hasNextInt()) {
             throw new IllegalArgumentException();
@@ -74,26 +104,26 @@ public class Application {
                 number = guessNum(scanner);
                 isValidGuess = true;
             } catch (IllegalArgumentException e) {
-                System.out.println("1 에서 9 사이의 서로 다른 숫자로 이루어진 3자리 정수만 입력 가능합니다");
+                System.out.println(Constants.GUESS_INPUT_ERROR_MESSAGE);
                 scanner.nextLine();
             }
         }
         return number;
     }
 
-    public static StrikeAndBall countStrikeAndBall(int guessNum, int answer) {
+    public static StrikeAndBall getStrikeAndBall(int guessNum, int answer) {
         int[] guessArray = intToArray(guessNum);
         int[] answerArray = intToArray(answer);
-        int strike = 0;
-        int ball = 0;
+        int strikeCount = 0;
+        int ballCount = 0;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                strike += isStrike(guessArray[i], i, answerArray[j], j);
-                ball += isBall(guessArray[i], i, answerArray[j], j);
+                strikeCount += isStrike(guessArray[i], i, answerArray[j], j);
+                ballCount += isBall(guessArray[i], i, answerArray[j], j);
             }
         }
-        return new StrikeAndBall(strike, ball);
+        return new StrikeAndBall(strikeCount, ballCount);
     }
 
     public static int isStrike(int num1, int index1, int num2, int index2) {
@@ -115,29 +145,28 @@ public class Application {
         int ballCount = thisTurnResult.getBall();
 
         if (0 < strikeCount && 0 < ballCount) {
-            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            System.out.println(ballCount + Constants.BALL + " " + strikeCount + Constants.STRIKE);
         } else if (0 < strikeCount && 0 == ballCount) {
-            System.out.println(strikeCount + "스트라이크");
+            System.out.println(strikeCount + Constants.STRIKE);
         } else if (0 == strikeCount && 0 < ballCount) {
-            System.out.println(ballCount + "볼");
+            System.out.println(ballCount + Constants.BALL);
         } else if (0 == strikeCount && 0 == ballCount) {
-            System.out.println("낫싱");
+            System.out.println(Constants.NOTHING);
         }
         return strikeCount;
     }
 
     public static void playGame(Scanner scanner, int answer) {
-        int number = getGuessNum(scanner);
-        StrikeAndBall thisTurnResult = countStrikeAndBall(number, answer);
-
         int strikeCount = 0;
         while (strikeCount != 3) {
+            int number = getGuessNum(scanner);
+            StrikeAndBall thisTurnResult = getStrikeAndBall(number, answer);
             strikeCount = getResultOfGuess(thisTurnResult);
         }
     }
 
     public static void decideGoOrStop(Scanner scanner) throws IllegalArgumentException {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(Constants.RESTART_OR_QUIT_MESSAGE);
         if (!scanner.hasNextInt()) {
             throw new IllegalArgumentException();
         }
@@ -147,14 +176,14 @@ public class Application {
         if (input == 1) {
             main(null);
         } else if (input == 2) {
-            System.out.println("GG");
+            System.out.println(Constants.QUIT_MESSAGE);
         } else {
             throw new IllegalArgumentException();
         }
     }
 
     public static void goOrStop (Scanner scanner) {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(Constants.WIN_MESSAGE);
         scanner.nextLine();
 
         boolean isValidGoOrStop = false;
@@ -163,7 +192,7 @@ public class Application {
                 decideGoOrStop(scanner);
                 isValidGoOrStop = true;
             } catch (IllegalArgumentException e) {
-                System.out.println("1 혹은 2만 입력 가능합니다");
+                System.out.println(Constants.RESTART_OR_QUIT_INPUT_ERROR_MESSAGE);
                 scanner.nextLine();
             }
         }
@@ -174,10 +203,7 @@ public class Application {
         // TODO 구현 진행
 
         // 3자리 숫자 생성
-        int answer;
-        do {
-            answer = RandomUtils.nextInt(100, 999);
-        } while (!isValidNum(answer));
+        int answer = getRandomNum();
 
         // 게임 플레이
         playGame(scanner, answer);
