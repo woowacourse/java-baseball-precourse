baf7b375c0fd52e7af798acea3f70a08708ddb51
bluewood <bluewood.truth@gmail.com>
2020-11-27 14:17:46 +0900

    feat: 사용자로부터 입력받은 숫자를 int 배열로 반환하는 askUserInputNumber() 메서드 작성

--- src/main/java/modules/InputOutputManager.java
+++ src/main/java/modules/InputOutputManager.java
@@ -8,4 +8,21 @@ public class InputOutputManager {
     public InputOutputManager(Scanner scanner){
         this.scanner = scanner;
     }
+
+    public int[] askUserInputNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = scanner.nextLine();
+        return parseToIntArray(inputNumber);
+    }
+
+    private int[] parseToIntArray(String input){
+        // TODO 예외처리
+
+        int[] intArray = new int[input.length()];
+        for(int i = 0; i < input.length(); i++){
+            intArray[i] = Character.getNumericValue(input.charAt(i));
+        }
+
+        return intArray;
+    }
 }
