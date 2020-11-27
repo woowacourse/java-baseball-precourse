9878351a37191dbc6bb41ca7a99d19089b34a254
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-11-27 15:06:58 +0900

    feat: 사용자 입력과 랜덤숫자 세팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,34 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+        // 사용자 입력
+        String playerInput;
+        System.out.println("숫자를 입력해 주세요 : ");
+        playerInput = scanner.next();
+        System.out.println("inputData: " + playerInput);
+
+        // 랜덤숫자 세팅
+        String randomNumbers = makeRandomNumber();
+        System.out.println("randomNumbers: " + randomNumbers);
     }
+
+    private static String makeRandomNumber() {
+        String randomNumbers = "";
+        for (int randomIndex = 0; randomIndex < 3; randomIndex++) {
+            randomNumbers += RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
+        }
+        return randomNumbers;
+    }
+
+
 }
