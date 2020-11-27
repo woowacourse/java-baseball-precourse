09983c41472c3bff1da44fdf555d2ff2b9be5f2c
Junhyeong Jo <118jjh@naver.com>
2020-11-27 18:37:45 +0900

    feat: 랜덤 숫자 3개 구하는 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
     static public final Scanner scanner = new Scanner(System.in);
@@ -10,6 +12,18 @@ public class Application {
     public static void initNumberChecker(){
         for(int i = 0; i < 3; i++){
             numberChecker[targetNumbers[i]] = 0;
+        }
+    }
+
+    public static void getRandomNumbers(){
+        int i = 0;
+        while(i < 3){
+            final int newNumber = RandomUtils.nextInt(1,9);
+            if(numberChecker[newNumber] == 0){
+                numberChecker[newNumber] = 1;
+                targetNumbers[i] = newNumber;
+                i++;
+            }
         }
     }
     public static void main(String[] args) {
