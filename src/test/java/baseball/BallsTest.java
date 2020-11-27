f9619a61ac6204992114be4545fc47042f87a62c
김주원 <kjw11077@naver.com>
2020-11-27 19:51:46 +0900

    refactor(BallsTest): optimize import

--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
