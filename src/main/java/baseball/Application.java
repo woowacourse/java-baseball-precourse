0b215b7ff65632bad4131a87a6cd2eaa793ad831
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-11-27 18:42:41 +0900

    feat: 예외사항 발생시 처리기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,15 +11,37 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
-        // 사용자 입력
-        String playerInput;
-        System.out.println("숫자를 입력해 주세요 : ");
-        playerInput = scanner.next();
-        System.out.println("inputData: " + playerInput);
+        Validate validate = new Validate();
+        // 게임 상태, 초기 0, 재시작 1, 완전종료 2, 게임종료 3
+        int flag = 1;
+        String randomNumbers ="";
+        while (flag != 2)
+        {
+            if (flag == 1) {
+                // 랜덤숫자 세팅
+                flag = 0;
+                randomNumbers = makeRandomNumber();
+                System.out.println("randomNumbers: " + randomNumbers);
+            }
+            // 사용자 입력
+            try{
+                System.out.println("숫자를 입력해 주세요 : ");
+                String playerInput = scanner.next();
+                if (isValidInput(playerInput, validate))
+                    System.out.println("inputData: " + playerInput);
+            } catch (IllegalArgumentException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
-        // 랜덤숫자 세팅
-        String randomNumbers = makeRandomNumber();
-        System.out.println("randomNumbers: " + randomNumbers);
+    private static boolean isValidInput(String playerInput, Validate validate)
+    {
+        if (validate.isValidInputSize(playerInput) &&
+                validate.isNumberInteger(playerInput))
+            return true;
+        return false;
     }
 
     private static String makeRandomNumber() {
@@ -29,6 +51,4 @@ public class Application {
         }
         return randomNumbers;
     }
-
-
 }
