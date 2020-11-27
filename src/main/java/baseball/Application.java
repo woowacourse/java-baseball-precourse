4a7da17d7607ee269e64c15e4ccb97770e75e8f1
483759 <483759@naver.com>
2020-11-27 22:39:26 +0900

    isValidInput()이 잘못된 Input 길이에 대해 IllegalArgument를 발생하도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,11 @@ package baseball;
 import java.util.Scanner;
 
 /*
-*       숫자 야구 게임을 진행하는 클래스
-*       author: 483759/윤이진
-* */
+ *       숫자 야구 게임을 진행하는 클래스
+ *       author: 483759/윤이진
+ * */
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         String inputValue;
@@ -23,36 +24,42 @@ public class Application {
     }
 
     /* 게임 진행을 위해 숫자를 입력하라는 안내를 출력하는 함수 */
-    public static void printNumberInput(){
+    public static void printNumberInput() {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
     /* 유저에게 숫자를 입력받아 반환하는 함수 */
-    public static String getUserInput(Scanner scanner){
-        String inputValue=scanner.next();
+    public static String getUserInput(Scanner scanner) {
+        String inputValue = scanner.next();
         return inputValue;
     }
 
     /* 유저가 입력한 숫자가 올바른 형식인지 검사하는 함수 */
-    public static boolean isValidInput(String inputValue){
-        boolean isValid=true;
+    public static boolean isValidInput(String inputValue) {
+        boolean isValid = true;
 
-        for(int i=0;i<inputValue.length();i++){
-            // 개별 문자에 대해 형식이 맞는지 검사
-            boolean isNumber=isValidCharacter(inputValue.charAt(i));
-            if(isNumber==false)
-                isValid=false;
+        try {
+            if(inputValue.length()!=3){
+                throw new IllegalArgumentException();
+            }
+            for (int i = 0; i < inputValue.length(); i++) {
+                // 개별 문자에 대해 형식이 맞는지 검사
+                isValid &= isValidCharacter(inputValue.charAt(i));
+            }
+        } catch (IllegalArgumentException e){
+            System.out.println("3자리 숫자를 입력해 주세요!");
+            return false;
         }
-        return  isValid;
+        return isValid;
     }
 
     /* 해당 문자가 입력 요구사항에 일치하는지 검사하는 함수 */
-    public static boolean isValidCharacter(char inputCharacter){
-        try{
-            if(inputCharacter<'1'||inputCharacter>'9'){
+    public static boolean isValidCharacter(char inputCharacter) {
+        try {
+            if (inputCharacter < '1' || inputCharacter > '9') {
                 throw new IllegalArgumentException();
             }
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("1과 9 사이의 숫자를 입력해 주세요!");
             return false;
         }
