5507dca909d8530c0462cab7271df90ff26bbda6
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-27 23:45:01 +0900

    fix: 숫자 길이가 3보다 작은 값을 입력했을 때 발생하는 에러 수정

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -14,7 +14,6 @@ public class InputValidator {
     private static final String NUMBER_RANGE_ERROR = "1~9의 숫자를 입력해주세요.";
     private static final String NUMBER_DUPLICATE_ERROR = "서로 다른 숫자를 입력해주세요.";
 
-
     private static Set<Character> duplicateCheckSet;
 
     private InputValidator() {
@@ -38,7 +37,7 @@ public class InputValidator {
 
 
     private static void validateLength(String numbers) {
-        if (numbers.length() > NUMBERS_LENGTH) {
+        if (numbers.length() != NUMBERS_LENGTH) {
             throw new IllegalArgumentException(NUMBER_LENGTH_ERROR);
         }
 
