ee6083ae04d0683cdc8c270ab7ec9777f18b9fa0
taehee-kim-dev <taehee.kim.dev@gmail.com>
2020-11-27 11:42:12 +0900

    test(computer generating numbers)

---
+++ src/test/java/players/computer/ComputerNumberTest.java
@@ -0,0 +1,24 @@
+package players.computer;
+
+import static baseball.checker.CheckerType.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import baseball.checker.NumberChecker;
+import baseball.players.numbers.ComputerNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ComputerNumberTest {
+
+    @DisplayName("컴퓨터가 생성한 번호가 유효한지")
+    @Test
+    void computerNumberTest() {
+        ComputerNumbers numbers = new ComputerNumbers();
+        numbers.generateNumber();
+
+        assertDoesNotThrow(() -> {
+            assertTrue(NumberChecker.isValidNumbers(numbers.getNumbers(), COMPUTER));
+        });
+    }
+}
