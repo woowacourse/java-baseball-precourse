25e2204acd1a1f51252d8999a84bffe77493edb5
awesomeo184 <awesomeo184@gmail.com>
2020-11-27 15:45:15 +0900

    feat: 사용자 입력에 맞는 결과를 반환하는 GameRule 구현
    볼, 스트라이크를 카운트한다
    스트라이크가 3개일 경우 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,19 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        ArrayList<Integer> answer = answerGenerator.answer;
+        startGame(answer, scanner);
+    }
+
+    public static void startGame(ArrayList<Integer> answer, Scanner scanner) {
+        Input input = new Input();
+        System.out.println(answer);
+        ArrayList<Integer> userInput = input.generateUserInputList(input.getUserInput(scanner));
+        GameRule gameRule = new GameRule(answer, userInput);
+        if (!gameRule.isWin()){
+            startGame(answer, scanner);
+        }
     }
 
 }
---
+++ src/main/java/baseball/GameRule.java
@@ -0,0 +1,77 @@
+package baseball;
+
+import java.util.ArrayList;
+
+public class GameRule {
+
+    private final ArrayList<Integer> answer;
+    private final ArrayList<Integer> userInput;
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final int NUM_LEN = 3;
+    private int strike = 0;
+    private int ball = 0;
+
+    public GameRule(ArrayList<Integer> answer, ArrayList<Integer> userInput) {
+        this.answer = answer;
+        this.userInput = userInput;
+        gameStatCounter();
+        getResult();
+    }
+
+    private void gameStatCounter() {
+        for (int index = 0; index < NUM_LEN; index++) {
+            if (isStrike(index)) {
+                strike++;
+                continue;
+            }
+            if (isBall(index)) {
+                ball++;
+            }
+        }
+    }
+
+    private boolean isStrike(int index) {
+        return answer.get(index).equals(userInput.get(index));
+    }
+
+    private boolean isBall(int index) {
+        return answer.contains(userInput.get(index));
+    }
+
+    private boolean isNothing() {
+        return strike == 0 && ball == 0;
+    }
+
+    public boolean isWin() {
+        return strike == NUM_LEN;
+    }
+
+    public void printStatCount() {
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball + BALL_MESSAGE + " " + strike + STRIKE_MESSAGE);
+            return;
+        }
+        if (strike != 0) {
+            System.out.println(strike + STRIKE_MESSAGE);
+            return;
+        }
+        if (ball != 0) {
+            System.out.println(ball + BALL_MESSAGE);
+        }
+    }
+
+
+    public void getResult() {
+        if (isWin()) {
+            System.out.println(WIN_MESSAGE);
+        } else if (isNothing()) {
+            System.out.println(NOTHING_MESSAGE);
+        } else {
+            printStatCount();
+        }
+    }
+
+}
