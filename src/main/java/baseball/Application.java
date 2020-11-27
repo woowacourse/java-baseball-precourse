efa9b79ed5c39687c0107599c86459991ada2c9c
platanus <platanus@canxan.com>
2020-11-27 23:34:20 +0900

    fix(application): Change variable name according to context

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,20 +8,20 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         String userInput;
-        int[] inputNumber;
         int validatedNumber;
+        int[] inputNumber;
         int[] randomNumber;
-        boolean gameResult = false;
-        while (!gameResult) {
+        boolean gameEndFlag = false;
+        while (!gameEndFlag) {
             System.out.println("숫자를 입력해주세요 : ");
             userInput = scanner.next();
             validatedNumber = NumberUtils.validateInput(userInput);
             inputNumber = NumberUtils.createInputNumber(validatedNumber);
             randomNumber = NumberUtils.createRandomNumber();
-            gameResult = Baseball.playGame(inputNumber, randomNumber);
-            if (gameResult) {
+            gameEndFlag = Baseball.playGame(inputNumber, randomNumber);
+            if (gameEndFlag) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                gameResult = restartGame();
+                gameEndFlag = restartGame();
             }
         }
     }
@@ -29,12 +29,12 @@ public class Application {
     private static boolean restartGame() {
         Scanner scanner = new Scanner(System.in);
         int userRestartInput;
-        boolean restart = true;
+        boolean restartFlag = true;
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         userRestartInput = scanner.nextInt();
         if (userRestartInput <= 1) {
-            restart = false;
+            restartFlag = false;
         }
-        return restart;
+        return restartFlag;
     }
 }
