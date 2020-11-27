56743a08ad3a37469a64249e0a7ac9c169aed0e0
kyle456 <kyle2123@naver.com>
2020-11-27 18:04:57 +0900

    feat: 사용자의 입력 기능 추가 및 예외 처리
    Application에 getInput() 메소드로 입력 기능 추가
    InputValidator로 입력 예외 처리 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,26 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        
+        do {
+            getInput(scanner);
+        } while (true);
+    }
+
+    public static String getInput(Scanner scanner) {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = scanner.nextLine();
+
+        try {
+            InputValidator.validateInputIsNumber(input);
+            InputValidator.validateInputLengthEqualsDigit(input);
+            InputValidator.validateZero(input);
+            InputValidator.validateDuplication(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            getInput(scanner);
+        }
+
+        return input;
     }
 }
---
+++ src/main/java/baseball/InputValidator.java
@@ -0,0 +1,56 @@
+package baseball;
+
+public class InputValidator {
+    private static final String ZERO = "0";
+    private static final String DIGIT_REGEX = "\\d+";
+    private static final String NUMBER_EXCEPTION_MESSAGE = "반드시 숫자만 입력해야 합니다.";
+    private static final String WRONG_LENGTH_MESSAGE = "세 자리를 입력해야 합니다.";
+    private static final String NO_ZERO_MESSAGE = "0이 포함되지 않은 숫자를 입력해야 합니다.";
+    private static final String NO_DUPLICATION = "중복된 숫자가 없어야 합니다.";
+    private static final int DIGIT = 3;
+    private static final int NEXT = 1;
+    private static final int NOT_EXIST = -1;
+
+    public static void validateInputIsNumber(String input) {
+        if (!input.matches(DIGIT_REGEX)) {
+            throw new IllegalArgumentException(NUMBER_EXCEPTION_MESSAGE);
+        }
+    }
+
+    public static void validateInputLengthEqualsDigit(String input) {
+        if (input.length() != DIGIT) {
+            throw new IllegalArgumentException(WRONG_LENGTH_MESSAGE);
+        }
+    }
+
+    public static void validateZero(String input) {
+        if (input.contains(ZERO)) {
+            throw new IllegalArgumentException(NO_ZERO_MESSAGE);
+        }
+    }
+
+    public static void validateDuplication(String input) {
+        char inputCharacterElement;
+
+        for (int i = 0; i < input.length(); i++) {
+            inputCharacterElement = input.charAt(i);
+            checkSameElement(isSameElement(input, Character.toString(inputCharacterElement), i));
+        }
+    }
+
+    public static boolean isSameElement(String input, String inputCharacterElement, int nowIndex) {
+        int nextIndex = nowIndex + NEXT;
+
+        if (input.indexOf(inputCharacterElement, nextIndex) == NOT_EXIST) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void checkSameElement(boolean isSameElement) {
+        if (isSameElement) {
+            throw new IllegalArgumentException(NO_DUPLICATION);
+        }
+    }
+
+}
