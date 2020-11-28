f9ab8486e7c9c35e238ca1e78cc197f68e91cc59
Jun0922 <tjrwns126@gmail.com>
2020-11-28 17:45:21 +0900

    feat: 유저가 값을 입력하는 기능 추가

--- src/main/java/baseball/GamePlayer.java
+++ src/main/java/baseball/GamePlayer.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class GamePlayer {
     String userChoice;
 
-    
+    static String chooseUserChoice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        String userChoice = scanner.nextLine();
+        return userChoice;
+    }
 }
