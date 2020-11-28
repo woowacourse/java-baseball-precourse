1d2cb1f14ec553345cdc04ae492bd3b3940e2563
byungwook <bbwwpark@naver.com>
2020-11-28 15:39:55 +0900

    feat: 사용자로부터 3자리수를 입력받는 기능 구현 - 입력받은 숫자가 비정상적인 입력인지 검사하도록 구현 - 숫자가 0 이하인지 체크 - 숫자가 3자리수인지 체크 - 숫자의 각 자리수가 중복되지 않는지 체크 - 숫자의 각 자리수의 범위가 1에서 9인지 체크

--- src/main/java/baseball/NumberBaseBallGame.java
+++ src/main/java/baseball/NumberBaseBallGame.java
@@ -1,14 +1,22 @@
 package baseball;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import utils.RandomUtils;
 
 public class NumberBaseBallGame {
 
     private static List<Integer> answerNumber = new LinkedList<>();
+    private static List<Integer> questionNumber = new LinkedList<>();
+    private static Scanner scanner;
+
+    public NumberBaseBallGame(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
     public static void setAnswerNumber() {
         Set<Integer> set = new HashSet<>();
@@ -20,6 +28,62 @@ public class NumberBaseBallGame {
                 set.add(num);
                 answerNumber.add(num);
             }
+        }
+    }
+
+    public static void setQuestionNumber(int num) {
+        while (num > 0) {
+            questionNumber.add(num % 10);
+            num /= 10;
+        }
+        Collections.reverse(questionNumber);
+    }
+
+    public static int getQuestionNumber() {
+        try {
+            System.out.print("숫자를 입력해주세요 : ");
+            int input = scanner.nextInt();
+            checkIsLessZero(input);
+            checkDigitLength(input);
+            checkRangeEachOfDigit(input, 1, 9);
+            checkIsDuplicateDigit(input);
+            return input;
+        } catch (Exception e) {
+            return getQuestionNumber();
+        }
+    }
+
+    private static void checkIsLessZero(int num) {
+        if (num <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkDigitLength(int num) {
+        if ((int) (Math.log10(num) + 1) != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkRangeEachOfDigit(int num, int min, int max) {
+        while (num > 0) {
+            int digit = num % 10;
+            if (!(digit >= min && digit <= max)) {
+                throw new IllegalArgumentException();
+            }
+            num /= 10;
+        }
+    }
+
+    private static void checkIsDuplicateDigit(int num) {
+        Set<Integer> set = new HashSet<>();
+
+        while (num > 0) {
+            if (set.contains(num % 10)) {
+                throw new IllegalArgumentException();
+            }
+            set.add(num % 10);
+            num /= 10;
         }
     }
 
