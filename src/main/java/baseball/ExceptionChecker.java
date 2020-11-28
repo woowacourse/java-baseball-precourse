b2dec7290d1a737b011c963b3dfc008fd2443059
Jun0922 <tjrwns126@gmail.com>
2020-11-28 19:23:48 +0900

    feat: 유효하지 않은 입력시 예외를 발생시키는 기능 구현

--- src/main/java/baseball/ExceptionChecker.java
+++ src/main/java/baseball/ExceptionChecker.java
@@ -3,11 +3,19 @@ package baseball;
 public class ExceptionChecker {
     
     static void checkException(String userChoice) {
-        int a = 0;
+        if (userChoice.length() != 3) {
+            throw new IllegalArgumentException();
+        } else if (userChoice.charAt(0) == userChoice.charAt(1) || userChoice.charAt(1) == userChoice.charAt(2) || userChoice.charAt(0) == userChoice.charAt(1)) {
+            throw new IllegalArgumentException();
+        } else if(userChoice.charAt(0) == '0' || userChoice.charAt(1) == '0' || userChoice.charAt(2) == '0') {
+            throw new IllegalArgumentException();
+        }
     }
 
     static void checkException(int gameCoin) {
-        int b = 0;
+        if (gameCoin !=1 && gameCoin != 2) {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
