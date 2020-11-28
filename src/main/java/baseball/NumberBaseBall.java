279956ce9d746df15081bc4604a38d35e8a47654
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-11-28 21:11:46 +0900

    feat: 힌트생성기능 수정
    기존에 contains 메소드를 사용해서 '볼'을 찾는과정을
    Arrays클래스에 binarySearch메소드로 변경

--- src/main/java/baseball/NumberBaseBall.java
+++ src/main/java/baseball/NumberBaseBall.java
@@ -2,8 +2,10 @@ package baseball;
 
 import utils.RandomUtils;
 
+import java.util.Arrays;
+
 public class NumberBaseBall {
-    public String randomNumber;
+    private String randomNumber;
     private static int INITIAL_VALUE = 0;
     private static int NUMBER = 3;
 
@@ -12,48 +14,57 @@ public class NumberBaseBall {
         char[] random = randomNums.toCharArray();
         int ball = INITIAL_VALUE;
         int strike = INITIAL_VALUE;
-        //스트라이크 찾기
+
         for (int i = 0; i < NUMBER; i++) {
             if (player[i] == random[i])
                 strike++;
         }
-        //볼 찾기
+        Arrays.sort(random);
         for (int i = 0; i < NUMBER; i++) {
-            if (randomNums.contains("" + player[i]))
+            if (Arrays.binarySearch(random, player[i]) >= 0)
                 ball++;
         }
-        if (ball > INITIAL_VALUE)
+       if (ball > INITIAL_VALUE)
             ball = ball - strike;
-        return makeResult(ball, strike);//결과 만들기
+        return makeResult(ball, strike);
     }
 
     private String makeResult(int ball, int strike)
     {
         StringBuilder result = new StringBuilder();
-
-        if (ball > INITIAL_VALUE && strike > INITIAL_VALUE) {
+        if (strike == 3) {
+            result.append(strike + "스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        else if (ball > INITIAL_VALUE && strike > INITIAL_VALUE) {
             result.append(ball + "볼 " + strike + "스트라이크");
-        } else if (ball > INITIAL_VALUE) {
-            result.append(ball + "볼");
-        } else if (strike > INITIAL_VALUE) {
+        }
+        else if (strike > INITIAL_VALUE) {
             result.append(strike + "스트라이크");
-            if (strike == 3) {
-                result.append("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            }
-        }else
+        }
+        else if (ball > INITIAL_VALUE) {
+            result.append(ball + "볼");
+        }
+        else
             result.append("낫싱");
         return result.toString();
     }
 
-    protected String makeRandomNumber(int minNumber, int maxNumber) {
-        String randomNumbers = "";
-        for (int randomIndex = 0; randomIndex < NUMBER; randomIndex++) {
-            randomNumbers += RandomUtils.nextInt(minNumber, maxNumber);
+    protected String makeRandomNumbers(int minNumber, int maxNumber) {
+        StringBuilder randomNumbers = new StringBuilder();
+        while (randomNumbers.length() != NUMBER)
+        {
+            String randomNum = String.valueOf(RandomUtils.nextInt(minNumber, maxNumber));
+            if (randomNumbers.indexOf(randomNum) < 0)
+                randomNumbers.append(randomNum);
         }
-        return randomNumbers;
+        return randomNumbers.toString();
     }
 
-    public void setRandomNumber(String randomNumber) {
+    public void setRandomNumbers(String randomNumber) {
         this.randomNumber = randomNumber;
+    }
+
+    public String getRandomNumbers() {
+        return randomNumber;
     }
 }
