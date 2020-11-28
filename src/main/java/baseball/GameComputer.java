45a98abf328d31d4870d494bf10d3abb58e7a2cf
Jun0922 <tjrwns126@gmail.com>
2020-11-28 17:14:21 +0900

    feat: 야구 게임(컴퓨터) 객체 구현

---
+++ src/main/java/baseball/GameComputer.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public class GameComputer {
+    String computerChoice;
+    int strike;
+    int ball;
+
+    String chooseComputerChoice(){
+
+        return computerChoice;
+    }
+
+    int umpireStrike(String computerChoice){
+        return strike;
+    }
+
+    int umpireball(String computerChoice){
+        return ball;
+    }
+}
