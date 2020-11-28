929ed04b8c006d9e6ad1a5e683bdb9256e5c3bb3
naru <hsik0225@gmail.com>
2020-11-28 18:51:09 +0900

    refactor(pitcher): set이 아닌 생성자에서 ball을 생성하도록 수정
    - setter를 사용하지 않고 생성자를 사용함으로써 객체의 불변성을 유지

--- src/main/java/baseball/domain/Pitcher.java
+++ src/main/java/baseball/domain/Pitcher.java
@@ -2,8 +2,8 @@ package baseball.domain;
 
 public class Pitcher extends Player {
 
-    public Pitcher(NumbersGenerator generator) {
-        super(generator);
+    public Pitcher(String balls) {
+        this.numbers = new BaseballNumbers(balls);
     }
 
     public int pitch(int numberIndex) {
