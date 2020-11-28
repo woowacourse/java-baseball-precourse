40a67041bac26e361df82083a85b7483448d62c5
김주원 <kjw11077@naver.com>
2020-11-28 23:13:18 +0900

    refactor(GameResultTest): divide testcases

--- src/test/java/baseball/GameResultTest.java
+++ src/test/java/baseball/GameResultTest.java
@@ -7,24 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GameResultTest {
 
     @Test
-    void getResult() {
+    void getResult_s_b() {
+        int strike = 1, ball = 1;
+        assertEquals(
+                new GameResult(strike, ball).getResult(),
+                strike + "스트라이크 " + ball + "볼");
+    }
 
-        int strike = 1;
-        int ball = 1;
-        GameResult gameResult = new GameResult(strike, ball);
-        String result = gameResult.getResult();
-        assertEquals(result, strike + "스트라이크 " + ball + "볼");
+    @Test
+    void getResult_s() {
+        int strike = 1, ball = 0;
+        assertEquals(
+                new GameResult(strike, ball).getResult(),
+                strike + "스트라이크");
+    }
 
-        strike = 1;
-        ball = 0;
-        gameResult = new GameResult(strike, ball);
-        result = gameResult.getResult();
-        assertEquals(result, strike + "스트라이크");
-
-        strike = 0;
-        ball = 1;
-        gameResult = new GameResult(strike, ball);
-        result = gameResult.getResult();
-        assertEquals(result, ball + "볼");
+    @Test
+    void getResult_b() {
+        int strike = 0, ball = 1;
+        assertEquals(
+                new GameResult(strike, ball).getResult(),
+                ball + "볼");
     }
 }
