652c639009f1bb76aeac82272c210e733098a986
박우림 <woorimprog@gmail.com>
2020-11-28 17:20:20 +0900

    feat(player) : add player class skeleton code

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Player {
+	private String number;
+
+	private void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String getNumber() {
+		return this.number;
+	}
+	
+	public void input() {
+		final Scanner scanner = new Scanner(System.in);
+		String number;
+		
+		System.out.print("숫자를 입력해주세요 : ");
+		number = scanner.next();
+		
+		if (number.length() != 3) {
+			throw new IllegalArgumentException("\n\t세자리 수를 입력하시오.\n");
+		}
+		setNumber(number);
+	}
+}
