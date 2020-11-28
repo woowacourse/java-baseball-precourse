4b4d60faef0d5f7b79d9857240a072abb7fc2fac
Jun0922 <tjrwns126@gmail.com>
2020-11-28 17:50:04 +0900

    feat: 정답 값과 추측 값을 비교하는 기능(strike&ball 계산) 구현

--- src/main/java/baseball/GameComputer.java
+++ src/main/java/baseball/GameComputer.java
@@ -24,11 +24,34 @@ public class GameComputer {
         return computerChoice;
     }
 
-    int umpireStrike(String computerChoice) {
+    static int umpirePitch(String computerChoice, String userChoice) {
+        int strike = 0;
+        int ball = 0;
+        
+        for (int i = 0; i < computerChoice.length(); i++) {
+            if (computerChoice.charAt(i) == userChoice.charAt(i)) {
+                strike += 1;
+            }
+        }
+
+        for (int i = 0; i < userChoice.length(); i++) {
+            if (calculateBall(computerChoice, userChoice.charAt(i))) {
+                ball += 1;
+            }
+        }
+
+        ball -= strike;
         return strike;
     }
 
-    int umpireball(String computerChoice) {
+    static boolean calculateBall(String computerChoice, int pitch) {
+        boolean ball = false;
+
+        for (int i = 0; i<computerChoice.length(); i++) {
+            if (computerChoice.charAt(i) == pitch) {
+                ball = true;
+            }
+        }
         return ball;
     }
 }
