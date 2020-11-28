86583b7e4e77d9d4f54c717497903a716271a957
박우림 <woorimprog@gmail.com>
2020-11-28 18:10:15 +0900

    feat(game-manager) : add query method at game-manager class

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,15 +1,30 @@
 package baseball;
 
+import java.util.Scanner;
+
 import utils.RandomUtils;
 
 public class GameManager {
 	private final static int START_INCLUSIVE = 100;
 	private final static int END_INVLUSIVE = 999;
 	
+	private int answer;
+	
 	public String getNumber() {
 		int random = RandomUtils.nextInt(START_INCLUSIVE, END_INVLUSIVE);
 		
 		
 		return Integer.toString(random);
+	}
+	
+	public void query() {
+		final Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		this.answer = scanner.nextInt();
+	}
+	
+	public int getQueryAnswer() {
+		return this.answer;
 	}
 }
