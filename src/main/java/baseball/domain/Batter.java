2e37f40df74091b911393ad8310a2e7f80665f2d
naru <hsik0225@gmail.com>
2020-11-28 18:53:59 +0900

    refactor(batter): generator를 생성할 넘버를 생성할 때만 사용하므로 속성에서 삭제

--- src/main/java/baseball/domain/Batter.java
+++ src/main/java/baseball/domain/Batter.java
@@ -2,10 +2,7 @@ package baseball.domain;
 
 public class Batter extends Player {
 
-    private final NumbersGenerator generator;
-
     public Batter(NumbersGenerator generator) {
-        this.generator = generator;
         this.numbers = new BaseballNumbers(generator.generateNumbers());
     }
 
