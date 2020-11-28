5fed8ea72797d08d6d296bd54a2318429352c952
Junhyeong Jo <118jjh@naver.com>
2020-11-28 14:05:55 +0900

    feat: 입력 값 검증 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,6 +26,17 @@ public class Application {
             }
         }
     }
+
+    public static boolean isValidInput(String input){
+        if(input.length() != 3) return false;
+        for(int i = 0; i < 3; i++){
+            if(input.charAt(i) < '1' || input.charAt(i) > '9') return false;
+        }
+        if(input.charAt(0) == input.charAt(1)) return false;
+        if(input.charAt(0) == input.charAt(2)) return false;
+        if(input.charAt(1) == input.charAt(2)) return false;
+        return true;
+    }
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
