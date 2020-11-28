17c9a6103687b37e87e30ffb055cf2a4917a1862
최문경 <munkyung0324@gmail.com>
2020-11-28 16:29:52 +0900

    feat(Application): add 3 distinct number logic
    Added logic to pick 3 number:
    - set 3 distinct number from RandomUtils

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,37 @@
 package baseball;
 
+import utils.RandomUtils;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
+    static boolean[] duplCheck;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+        duplCheck = new boolean[10];
+        int[] systemNum = new int[3];
+        int systemNumCnt = 0;
+        while(systemNumCnt < 3) {
+            int tmp = RandomUtils.nextInt(1, 9);
+            if(!isDupl(tmp)) {
+                systemNum[systemNumCnt] = tmp;
+                systemNumCnt++;
+            }
+        }
+
+        System.out.println(Arrays.toString(systemNum));
+
+    }
+
+    private static boolean isDupl(int checkNum) {
+        if(duplCheck[checkNum]) {
+            return true;
+        } else {
+            duplCheck[checkNum] = true;
+            return false;
+        }
     }
 }
