9c5982c1ab46f69610d2d9289296b8ad8be74481
naru <hsik0225@gmail.com>
2020-11-28 18:45:32 +0900

    refactor(player): pitcher에서 사용하지 않는 속성과 메소드 삭제

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,26 +1,7 @@
 package baseball.domain;
 
-import java.util.List;
-import java.util.Scanner;
-
 public abstract class Player {
-
-    protected final NumbersGenerator generator;
 
     protected BaseballNumbers numbers;
 
-    protected final Validator validator;
-
-    public Player(NumbersGenerator generator) {
-        this.generator = generator;
-        this.validator = new Validator();
-    }
-
-    public List<Integer> getNumbers() {
-        return numbers.getNumbers();
-    }
-
-    public boolean wantsToStop(Scanner scanner) {
-
-    }
 }
