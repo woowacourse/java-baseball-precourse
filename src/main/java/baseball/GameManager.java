c8ec0192f845f9f825f86879e3ca210e3a8f21de
teo.kkim <taehongkim496@gmail.com>
2020-11-28 17:21:38 +0900

    feat : gameStart 메서드 구현
    - 게임 진행 순서를 gameStart 메서드로 구현했음

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -12,6 +12,31 @@ public class GameManager {
         baseBallNumber = new BaseBallNumber();
     }
 
-    public void gameStart() {}
+    public void gameStart() {
+        // 무작위 숫자 생성
+        baseBallNumber.resetNumber();
 
+        do {
+            // 입력 받기
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = inputHandler.inputWrapper(Request.THREENUMS);
+
+            // 정답 여부와 볼 스트라이크 계산
+            baseBallNumber.calculateMatching(input);
+
+            // 상태에 맞는 결과 출력
+            if (baseBallNumber.isCorrectAnswer()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                baseBallNumber.resetNumber();
+            } else
+                System.out.println(baseBallNumber.getHint());
+
+            // 종료 여부 결정
+        } while (!receiveEndCode());
+    }
+
+    public boolean receiveEndCode() {
+        return false;
+    }
 }
