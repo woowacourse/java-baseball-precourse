12248d2d2e3cdaf898492141d46c3cf72dbaf9e6
Beenie93 <seongbeen93@naver.com>
2020-11-28 19:08:52 +0900

    feat(service): 정답으로 서로 다른 임의의 수 3개를 생성하는 기능 구현
    AnswerGenerator 클래스

---
+++ src/main/java/baseball/service/AnswerGenerator.java
@@ -0,0 +1,31 @@
+package baseball.service;
+
+import baseball.constant.Constants;
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnswerGenerator {
+
+    private AnswerGenerator() {}
+
+    public static List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<Integer>();
+
+        while (answer.size() != Constants.ANSWER_LENGTH) {
+            int number = RandomUtils.nextInt(Constants.VALUE_ONE,Constants.VALUE_NINE);
+            addNumber(answer, number);
+        }
+
+        return answer;
+    }
+
+    private static void addNumber(List<Integer> answer, int number) {
+
+        if (!answer.contains(number)) {
+            answer.add(number);
+        }
+
+    }
+}
