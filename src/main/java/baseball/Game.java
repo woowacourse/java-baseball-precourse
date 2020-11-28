4a9c739a1a5b8bea5b633416ee0eee7e46603fac
Eugine Park <pkeugine@gmail.com>
2020-11-28 23:13:43 +0900

    feat(Game): include Game class to start the game

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Game {
+    private Scanner userInput;
+
+    public Game(Scanner userInput) {
+        this.userInput = userInput;
+    }
+
+    public void play() {
+        initialize();
+        proceedRound();
+    }
+
+    private void initialize() {
+
+    }
+
+    private void proceedRound() {
+    }
+}
