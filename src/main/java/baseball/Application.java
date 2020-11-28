c578a329c6df08d2df1968bdb07c22c9aabc692a
sounmind <sounmind777@gmail.com>
2020-11-28 19:22:38 +0900

    feat(Application): 한 파일 내에서 모든 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,38 +1,92 @@
 package baseball;
 
 import utils.RandomUtils;
-
 import java.util.Scanner;
-
 
 public class Application {
     static final int NUMBER_LENGTH = 3;
     static final int MIN_NUMBER = 1;
     static final int MAX_NUMBER = 9;
+    static final int MAX_STRIKE = 3;
     static int[] NumbersPickedByComputer = new int[NUMBER_LENGTH];
     static int[] NumbersPickedByPlayer = new int[NUMBER_LENGTH];
+    static String[] playerInputString;
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        inputRandomAndNoDuplicationNumbers(NumbersPickedByComputer);
-        printArray(NumbersPickedByComputer);
-        System.out.print("숫자를 입력해주세요 : ");
-        String playerInputString[] = scanner.next().split("");
-        inputPlayerNumbersToArray(playerInputString);
-
-        // 비교하기 -> 정답 맞출 경우 종료
-        for(int i = 0; i < NUMBER_LENGTH; i++) {
-            if(NumbersPickedByComputer[i] == NumbersPickedByPlayer[i]){
-                System.out.println("정답!");
-            } else {
-                System.out.println("오답!");
-            }
+        while(true) {
+            inputRandomAndNoDuplicationNumbers(NumbersPickedByComputer);
+            printArray(NumbersPickedByComputer);
+            playerInputAndCompareProcess(scanner);
+            int answer = askPlayerGameRestart(scanner);
+            if(answer == 1)
+                continue;
+            if(answer == 2)
+                break;
         }
-        // 결과 출력
-        // 다시 할지 묻기
-        // 비정상적 입력에 대해서는 IllegalArgumentException을 발생시킨다.
+    }
 
+    public static int askPlayerGameRestart(Scanner scanner) {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int answerPlayerInput;
+        try {
+            answerPlayerInput = scanner.nextInt();
+        } catch(Exception e) {
+            throw new IllegalArgumentException();
+        }
+        if(answerPlayerInput != 1 && answerPlayerInput != 2)
+            throw new IllegalArgumentException("1과 2 중에 하나를 입력해주세요!");
+        return answerPlayerInput;
+    }
+
+    public static void playerInputAndCompareProcess(Scanner scanner) {
+        while(true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            playerInputString = scanner.next().split("");
+            inputPlayerNumbersToArray(playerInputString);
+            if (compareComputerAndPlayer())
+                break;
+        }
+    }
+
+    public static boolean compareComputerAndPlayer() {
+        int strike = countStrike();
+        int ball = countBall(strike);
+        if(strike != MAX_STRIKE) {
+            printHint(strike, ball);
+            return false; // 다시 입력
+        }
+        System.out.printf("%d스트라이크 \n%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n", MAX_STRIKE, MAX_STRIKE);
+        return true; // 게임 종료, 게임 다시 시작할지 완전히 종료할지 묻기
+
+    }
+
+    public static void printHint(int strike, int ball) {
+        if(ball != 0)
+            System.out.printf("%d볼 ", ball);
+        if(strike != 0)
+            System.out.printf("%d스트라이크", strike);
+        if(ball == 0 && strike == 0)
+            System.out.printf("낫싱");
+        System.out.println();
+    }
+
+    public static int countStrike() {
+        int strikeCounter = 0;
+        for(int i = 0; i < NUMBER_LENGTH; i++) {
+            if (NumbersPickedByComputer[i] == NumbersPickedByPlayer[i])
+                strikeCounter++;
+        }
+        return strikeCounter;
+    }
+    public static int countBall(int strikeCounter) {
+        int ballCounter = 0;
+        for(int i = 0; i < NUMBER_LENGTH; i++) {
+            if (isSameDigitInArray(NumbersPickedByComputer, NumbersPickedByPlayer[i]))
+                ballCounter++;
+        }
+        return ballCounter - strikeCounter;
     }
 
     public static boolean isValidInput(String[] string) {
@@ -42,13 +96,13 @@ public class Application {
     }
 
     public static void inputPlayerNumbersToArray(String[] playerInputString) {
+        initializeArray(NumbersPickedByPlayer);
         if(!isValidInput(playerInputString))
             throw new IllegalArgumentException();
         for(int i = 0; i < NUMBER_LENGTH; i++) {
             int numberPlayerInput = Integer.parseInt(playerInputString[i]);
             if(isSameDigitInArray(NumbersPickedByPlayer, numberPlayerInput)) {
-                System.out.println("중복된 숫자가 있어요!");
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("중복된 숫자가 있어요!");
             }
             NumbersPickedByPlayer[i] = numberPlayerInput;
         }
@@ -80,7 +134,13 @@ public class Application {
         return false;
     }
 
+    public static void initializeArray(int[] array) {
+        for(int i = 0; i < array.length; i++)
+            array[i] = -1;
+    }
+
     public static void inputRandomAndNoDuplicationNumbers(int[] array){
+        initializeArray(NumbersPickedByComputer);
         for(int i = 0; i < NUMBER_LENGTH; i++) {
             int newDigit = RandomUtils.nextInt(MIN_NUMBER, MAX_NUMBER);
             if(isSameDigitInArray(array, newDigit)) {
