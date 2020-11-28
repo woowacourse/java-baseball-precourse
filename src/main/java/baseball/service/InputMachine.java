069442660f5c50f425ab795d3fa506fa0ba67638
Beenie93 <seongbeen93@naver.com>
2020-11-28 21:31:12 +0900

    feat(service): 사용자 입력과 입력받은 값을 검증하는 기능 구현
    service.InputMachine: 사용자 입력받는 클래스
    service.InputValidator: 사용자 입력값을 검증하는 클래스

---
+++ src/main/java/baseball/service/InputMachine.java
@@ -0,0 +1,19 @@
+package baseball.service;
+
+import baseball.view.InputView;
+
+import java.util.Scanner;
+
+public class InputMachine {
+    private InputMachine() {}
+
+    public static String inputUserNumber(Scanner scanner) {
+        InputView.setUserNumber();
+        return scanner.nextLine();
+    }
+
+    public static String inputRetryNumber(Scanner scanner) {
+        InputView.setRetryNumber();
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/baseball/service/InputValidator.java
@@ -0,0 +1,78 @@
+package baseball.service;
+
+import baseball.constant.Constants;
+import baseball.view.OutputView;
+import com.sun.tools.classfile.ConstantPool;
+import utils.StringToListIntegerutils;
+
+import java.util.List;
+
+public class InputValidator {
+
+    private InputValidator() {}
+
+    public static boolean validateUserNumber(String userNumber) {
+        return isNaturalNumber(userNumber)
+                && isThreeDigitNumber(userNumber)
+                && isNonZero(userNumber)
+                && isAllDifferentNumber(userNumber);
+    }
+
+    public static boolean validateRetryNumber(String retryNumber) {
+        return isNaturalNumber(retryNumber)
+                && isNumberOneOrTwo(retryNumber);
+    }
+
+    private static boolean isNaturalNumber(String number) {
+
+        try {
+            Integer.parseInt(number);
+        } catch (IllegalArgumentException expected) {
+            Constants.ERROR_MESSAGE = Constants.PLEASE_INPUT_THREE_DIGIT_NUMBER;
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isThreeDigitNumber(String number) {
+
+        if (number.length() != Constants.ANSWER_LENGTH) {
+            Constants.ERROR_MESSAGE = Constants.PLEASE_INPUT_THREE_DIGIT_NUMBER;
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isNonZero(String number) {
+
+        if (number.contains(Constants.CONTAIN_ZERO)) {
+            Constants.ERROR_MESSAGE = Constants.PLEASE_INPUT_NON_ZERO_NUMBER;
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isAllDifferentNumber(String number) {
+        List<Integer> numberList = StringToListIntegerutils.convertToList(number);
+
+        if (numberList.stream().distinct().count() != Constants.ANSWER_LENGTH) {
+            Constants.ERROR_MESSAGE = Constants.PELASE_INPUT_ALL_DIFFERENT_NUMBER;
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isNumberOneOrTwo(String number) {
+
+        if (!(number.contains(Constants.RESTART) || number.contains(Constants.EXIT))) {
+            Constants.ERROR_MESSAGE = Constants.PLEASE_INPUT_RETRY_NUMBER;
+            return false;
+        }
+
+        return true;
+    }
+}
