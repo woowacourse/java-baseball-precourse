9e31a05bc8f8214fbb491a47598b62e87cb25554
Beenie93 <seongbeen93@naver.com>
2020-11-29 03:28:04 +0900

    feat(service): 사용자 입력값과 정답을 비교한 결과를 출력하는 기능 구현

---
+++ src/main/java/baseball/service/OutputMachine.java
@@ -0,0 +1,42 @@
+package baseball.service;
+
+import baseball.constant.Constants;
+import baseball.model.Record;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.Scanner;
+
+public class OutputMachine {
+
+    private OutputMachine() {}
+
+    public static void print(Record record) {
+
+        if (CountValidator.isAllStrike(record.getStrike())) {
+            OutputView.showResult(Constants.ALL_STRIKE);
+            return;
+        }
+
+        if (CountValidator.isNothing(record.getBall(), record.getStrike())) {
+            OutputView.showResult(Constants.NOTHING);
+            return;
+        }
+
+        OutputView.showResult(generateOutput(record.getBall(), record.getStrike()));
+    }
+
+    public static String generateOutput(int ballCount, int strikeCount) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (ballCount != Constants.VALUE_ZERO) {
+            stringBuilder.append(ballCount + "볼 ");
+        }
+
+        if (strikeCount != Constants.VALUE_ZERO) {
+            stringBuilder.append(strikeCount + "스트라이크");
+        }
+
+        return stringBuilder.toString();
+    }
+}
