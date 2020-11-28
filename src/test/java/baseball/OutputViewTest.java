4093c5fc5608b7a3539546dcf1d49773a8a00b65
naru <hsik0225@gmail.com>
2020-11-28 22:13:58 +0900

    test(output-view): 스트라이크와 볼이 0이 아닐 경우 테스트 코드 작성

--- src/test/java/baseball/OutputViewTest.java
+++ src/test/java/baseball/OutputViewTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,17 @@ import baseball.domain.ScoreBoard;
 
 public class OutputViewTest {
 
+    private OutputView outputView;
+
+    @BeforeEach
+    public void initOutputView() {
+        outputView = new OutputView();
+    }
+
     @Test
     @DisplayName("낫싱 반환 테스트")
     public void nothingTest() {
         ScoreBoard scoreBoard = new ScoreBoard(0, 0);
-        OutputView outputView = new OutputView();
         assertThat(outputView.getResult(scoreBoard)).isEqualTo(Judgment.NOTHING.getMessage());
     }
 
@@ -22,7 +29,6 @@ public class OutputViewTest {
     @DisplayName("스트라이크가 0이고 볼이 0이 아닐 경우 반환 테스트")
     public void hasZeroStrikeTest() {
         ScoreBoard scoreBoard = new ScoreBoard(0, 1);
-        OutputView outputView = new OutputView();
         assertThat(outputView.getResult(scoreBoard)).isEqualTo(1 + Judgment.BALL.getMessage());
     }
 
@@ -30,16 +36,23 @@ public class OutputViewTest {
     @DisplayName("스트라이크가 0이 아니고 볼이 0일 경우 반환 테스트")
     public void hasZeroBallTest() {
         ScoreBoard scoreBoard = new ScoreBoard(1, 0);
-        OutputView outputView = new OutputView();
         assertThat(outputView.getResult(scoreBoard)).isEqualTo(1 + Judgment.STRIKE.getMessage());
+    }
+
+    @Test
+    @DisplayName("스트라이크와 볼이 0이 아닐 경우 반환 테스트")
+    public void hasStrikeAndBallTest() {
+        ScoreBoard scoreBoard = new ScoreBoard(1, 2);
+        assertThat(outputView.getResult(scoreBoard))
+                .isEqualTo(2 + Judgment.BALL.getMessage() + 1 + Judgment.STRIKE.getMessage());
     }
 
     @Test
     @DisplayName("정답일 경우 반환 테스트")
     public void answerTest() {
         ScoreBoard scoreBoard = new ScoreBoard(3, 0);
-        OutputView outputView = new OutputView();
         assertThat(outputView.getResult(scoreBoard))
-                .isEqualTo(3 + Judgment.STRIKE.getMessage() + OutputView.GAME_END);
+                .isEqualTo(BaseballGame.BALLS_LENGTH + Judgment.STRIKE.getMessage() +
+                        OutputView.GAME_END);
     }
 }
