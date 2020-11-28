c5989d8465e67f65db425b7de70533e58bfecbc4
Jun0922 <tjrwns126@gmail.com>
2020-11-28 17:43:13 +0900

    feat: 컴퓨터가 정답 값을 선택하는 기능 추가

--- src/main/java/baseball/GameComputer.java
+++ src/main/java/baseball/GameComputer.java
@@ -1,20 +1,34 @@
 package baseball;
 
+import java.util.Scanner;
+import java.util.Arrays;
+import utils.RandomUtils;
+
 public class GameComputer {
-    String computerChoice;
+    final Scanner scanner = new Scanner(System.in);
+    static String computerChoice;
     int strike;
     int ball;
 
-    String chooseComputerChoice(){
+    static String chooseComputerChoice(){
+        int[] answer = new int[3];
 
+        answer[0] = RandomUtils.nextInt(1,9);
+        do {
+            answer[1] = RandomUtils.nextInt(1,9);
+        } while (answer[0] == answer[1]);
+        do {
+            answer[2] = RandomUtils.nextInt(1,9);
+        } while (answer[0] == answer[2] || answer[1] == answer[2]);
+        computerChoice = Arrays.toString(answer).replaceAll("[^0-9]","");
         return computerChoice;
     }
 
-    int umpireStrike(String computerChoice){
+    int umpireStrike(String computerChoice) {
         return strike;
     }
 
-    int umpireball(String computerChoice){
+    int umpireball(String computerChoice) {
         return ball;
     }
 }
