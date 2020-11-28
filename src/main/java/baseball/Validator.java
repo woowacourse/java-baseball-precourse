dcaabf23336373c598d0f41d51fbf169e4616c6c
박우림 <woorimprog@gmail.com>
2020-11-29 00:06:23 +0900

    feat(validator) : add a method to check if the value is available

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,10 +1,26 @@
 package baseball;
 
 public class Validator {
-	public static boolean isSame(String random, String playerNumber) {
-		if (random.equals(playerNumber)) {
+	public static boolean isSame(String origin, String compare) {
+		if (origin.equals(compare)) {
 			return true;
 		}
 		return false;
+	}
+	
+	public static boolean isUseable(String random) {
+		if (random.contains("0")) {
+			return false;
+		}
+		
+		for (int i = 0; i < GameManager.MAX_NUM_LENGTH; i++) {
+			for (int j = i+1; j < GameManager.MAX_NUM_LENGTH; j++) {
+				if (isSame(random.substring(i, i+1), random.substring(j, j+1))) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
