81b51d33c5ca4aad2b88cafba422f1d5498e3a6d
baekchan1024 <dlqhs1024@naver.com>
2020-11-28 21:17:31 +0900

    feat : Player 클래스 기능 구현
        문자 타입으로 된 숫자를 입력 받아서 문자열 배열로 나눔

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,9 +1,28 @@
 package baseball;
 
 import java.util.Scanner;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Player {
-	
+	final static int MAX_LENGTH = 3;
+	final static int START_GAME = 1;
+	final static int END_GAME = 2;
+	final static int MAX_RANGE = 9;
+	final static int MIN_RANGE = 1;
+
+	public void inputThreeNumbers() {
+		Scanner scanner = new Scanner(System.in);
+		String inputNumbers = scanner.nextLine();
+	}
+
+	public void convertInputNumbersTOArray(String input) {
+		Set<Character> numbersSet = new HashSet();
+		char[] arrayNumbers = input.toCharArray();
+
+		for (int i = 0; i < arrayNumbers.length; i++) {
+			numbersSet.add(arrayNumbers[i]);
+		}
+	}
 
 }
