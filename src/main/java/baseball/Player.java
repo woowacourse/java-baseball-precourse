efe98fcae032d22de8eb5a9b53b0b71c2d03e9f9
baekchan1024 <dlqhs1024@naver.com>
2020-11-28 16:00:57 +0900

    style : 코드 포맷팅

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,29 +6,28 @@ import java.util.HashMap;
 public class Player {
 	Scanner scanner = new Scanner(System.in);
 	StringGameGuide guide = new StringGameGuide();
-	HashMap<Integer,Integer> playerNumbersMap = new HashMap<Integer,Integer>();
-	final int MAX_PLAYER_KEY_NUMBERS = 3; 
-	
+	HashMap<Integer, Integer> playerNumbersMap = new HashMap<Integer, Integer>();
+	final int MAX_PLAYER_KEY_NUMBERS = 3;
+
 	private int[] inputThreeNumbers() {
 		int inputNumbers = scanner.nextInt();
 		int[] arrayNumbers = new int[MAX_PLAYER_KEY_NUMBERS];
-		for(int num =0; num <MAX_PLAYER_KEY_NUMBERS; num++) {
-			while(num >0){
-				arrayNumbers[num] += inputNumbers%10;
+		for (int num = 0; num < MAX_PLAYER_KEY_NUMBERS; num++) {
+			while (num > 0) {
+				arrayNumbers[num] += inputNumbers % 10;
 				inputNumbers /= 10;
 			}
 		}
 		return arrayNumbers;
 	}
-	
-	public HashMap<Integer,Integer> duplicateThreeNumbers() {
+
+	public HashMap<Integer, Integer> duplicateThreeNumbers() {
 		System.out.println(guide.InputNumbers());
-		final int[] array = inputThreeNumbers();
-		for (int num =0; num <= MAX_PLAYER_KEY_NUMBERS; num++) {
-			playerNumbersMap.put(num,array[num]);
+		final int[] playerThreeNumbersArray = inputThreeNumbers();
+		for (int num = 0; num <= MAX_PLAYER_KEY_NUMBERS; num++) {
+			playerNumbersMap.put(num, playerThreeNumbersArray[num]);
 		}
 		return playerNumbersMap;
 	}
-	
-	
+
 }
