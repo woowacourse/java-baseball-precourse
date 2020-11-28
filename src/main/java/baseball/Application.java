e77ae05cd06f9f8b4d61c8f3c2f30f0ddcdc61dd
483759 <483759@naver.com>
2020-11-28 10:53:22 +0900

    숫자 야구 게임의 진행을 담당하는 메소드 startGame() 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 /*
  *       숫자 야구 게임을 진행하는 클래스
@@ -10,31 +11,33 @@ public class Application {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        String inputValue;
 
-        /*
+        // TODO 구현 진행
+    }
+
+    /* 전반적인 게임 진행을 담당하는 메소드 */
+    public static void startGame(Scanner scanner){
+        String inputValue;
         boolean correctValue=false;
         while(!correctValue){
             printNumberInput();
             inputValue=getUserInput(scanner);
             correctValue=isValidInput(inputValue);
         }
-        */
-        // TODO 구현 진행
     }
 
-    /* 게임 진행을 위해 숫자를 입력하라는 안내를 출력하는 함수 */
+    /* 게임 진행을 위해 숫자를 입력하라는 안내를 출력하는 메소드 */
     public static void printNumberInput() {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
-    /* 유저에게 숫자를 입력받아 반환하는 함수 */
+    /* 유저에게 숫자를 입력받아 반환하는 메소드 */
     public static String getUserInput(Scanner scanner) {
         String inputValue = scanner.next();
         return inputValue;
     }
 
-    /* 유저가 입력한 숫자가 올바른 형식인지 검사하는 함수 */
+    /* 유저가 입력한 숫자가 올바른 형식인지 검사하는 메소드 */
     public static boolean isValidInput(String inputValue) {
         boolean isValid = true;
 
@@ -53,7 +56,7 @@ public class Application {
         return isValid;
     }
 
-    /* 해당 문자가 입력 요구사항에 일치하는지 검사하는 함수 */
+    /* 해당 문자가 입력 요구사항에 일치하는지 검사하는 메소드 */
     public static boolean isValidCharacter(char inputCharacter) {
         try {
             if (inputCharacter < '1' || inputCharacter > '9') {
