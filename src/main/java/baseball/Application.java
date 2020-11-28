dbe390ccdfbbb48bf33e7022672a63e0ad7dd5b4
Naeun <knae11@naver.com>
2020-11-28 15:07:49 +0900

    feat(RandomUtils): 중복되지 않는 1~9 정수 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,13 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        System.out.println(Arrays.toString(RandomUtils.getComputerNumbers()));
     }
 }
--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -1,11 +1,39 @@
 package utils;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class RandomUtils {
+
     private static final Random RANDOM = new Random();
+    private static final int COUNT = 3;
+    private static final int RANGE_START = 1;
+    private static final int RANGE_END = 9;
+
+    private static int[] computerNumbers = new int[COUNT];
 
     private RandomUtils() {
+
+    }
+
+    public static int[] getComputerNumbers() {
+        makeRamdomNumbers();
+        return computerNumbers;
+    }
+
+    private static void makeRamdomNumbers(){
+        for (int i = 0; i < COUNT; i++) {
+            computerNumbers[i] = nextInt(RANGE_START, RANGE_END);
+            i = checkSame(i);
+        }
+    }
+    private static int checkSame(int i){
+        for (int j = 0; j < i; j++) {
+            if (computerNumbers[i] == computerNumbers[j]) {
+                return i-1;
+            }
+        }
+        return i;
     }
 
     public static int nextInt(final int startInclusive, final int endInclusive) {
