781264d6195ccba26ad20c0a99076e35856a7a8c
Jun0922 <tjrwns126@gmail.com>
2020-11-28 17:22:13 +0900

    비정상적 입력을 판별하는 객체 생성

---
+++ src/main/java/baseball/ExceptionChecker.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class ExceptionChecker {
+    
+    static void checkException(String userChoice) {
+        int a = 0;
+    }
+
+    static void checkException(int gameCoin) {
+        int b = 0;
+    }
+
+}
