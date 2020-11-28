4aeef670d12c1686ce10348b71128d8e6f8630f7
Beenie93 <seongbeen93@naver.com>
2020-11-28 21:33:27 +0900

    feat(view): 사용자에게 보여줄 View 구현
    view.InputView: 사용자 입력받을 시 보여주는 View
    view.OutputView: 사용자에게 에러와 결과를 보여주는 View

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+import baseball.constant.Constants;
+
+public class InputView {
+
+    private InputView() {}
+
+    public static void setUserNumber() {
+        System.out.print(Constants.PLEASE_INPUT_NUMBER);
+    }
+
+    public static void setRetryNumber() {
+        System.out.println(Constants.PLEASE_INPUT_RETRY_NUMBER);
+    }
+}
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,14 @@
+package baseball.view;
+
+public class OutputView {
+
+    private OutputView() {}
+
+    public static void showError(String error) {
+        System.out.println(error);
+    }
+
+    public static void showResult(String result) {
+        System.out.println(result);
+    }
+}
