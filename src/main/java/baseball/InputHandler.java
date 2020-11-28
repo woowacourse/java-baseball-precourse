ad761aa591af0fd1ee174b10799a59e739c99388
teo.kkim <taehongkim496@gmail.com>
2020-11-28 16:54:28 +0900

    fix : InputHandler 의 메서드 구조 수정
    - 기존에 public 으로 공개한 getValidInput 은 에러 핸들링을 할 수 없다.
    - 또한 에러를 catch 해서 올바른 입력을 받기까지 대기하는 것도 불가능했다.
    - 이를 해결하고자 래퍼함수를 추가하고 이 메서드만 public 으로 공개했다.

--- src/main/java/baseball/InputHandler.java
+++ src/main/java/baseball/InputHandler.java
@@ -10,7 +10,7 @@ public class InputHandler {
         this.scanner = scanner;
     }
 
-    public String getValidInput(Request reqType) {
+    private String getValidInput(Request reqType) {
         String input = scanner.next();
         if (reqType == Request.THREENUMS)
             return threeNumValidCheck(input);
@@ -34,4 +34,25 @@ public class InputHandler {
         return input;
     }
 
+    /**
+     * inputWrapper 로 감싼 이유는?
+     * 입력을 받아오는 getValidInput 은 입력이 유효한지만 검사해서 반환한다.
+     * 이 동작을 에러 핸들링까지 추가해서, 유효하게 입력할때까지 무한 반복 시키려하는데,
+     * 인덴트 제한이 있으므로 Wrapper 로 뽑아낸 것.
+     *
+     * @param req
+     * @return
+     */
+    public String inputWrapper(Request req) {
+        String userInput;
+        while (true) {
+            try {
+                userInput = getValidInput(req);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+            }
+        }
+        return userInput;
+    }
 }
