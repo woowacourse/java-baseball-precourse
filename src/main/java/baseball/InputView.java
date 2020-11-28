cafacaa9e87e385ede79918b864b5ff7d1faac06
naru <hsik0225@gmail.com>
2020-11-28 17:08:24 +0900

    refactor(input-view): 숫자 입력 유효성 검사 기능을 InputView 클래스 안으로 이동

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -2,18 +2,25 @@ package baseball;
 
 import java.util.Scanner;
 
+import baseball.domain.Validator;
+
 public class InputView {
 
     public static final String ASK_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
 
+    private final Validator validator;
+
     private final Scanner scanner;
 
-    public InputView(Scanner scanner) {
+    public InputView(Validator validator, Scanner scanner) {
+        this.validator = validator;
         this.scanner = scanner;
     }
 
     public String askNumbers() {
         System.out.print(ASK_NUMBERS_MESSAGE);
-        return scanner.nextLine();
+        String numbers = scanner.nextLine();
+        validator.validateNumbers(Validator.NUMBERS_PATTERN, numbers);
+        return numbers;
     }
 }
