8e56cf11c7fc300f83a0ecf25ca82c28f6e31bac
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-11-28 21:14:10 +0900

    feat: 게임 실행 기능 수정
    게임이 진행되는과정을 메소드로 분리
    gameStatus에 따라 게임 종료, 시작수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,35 +5,33 @@ import java.util.Scanner;
 public class Application {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    private static final int RESTART = 1;
+    private static final int GAME_START = 1;
     private static final int SHUTDOWN = 2;
-    private static final int GAME_OVER = 3;
-    private static final int START_GAME = 0;
+    private static int gameStatus = GAME_START;
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         Player player = new Player();
         NumberBaseBall numberBaseBall = new NumberBaseBall();
-        // 게임 상태: 재시작 1, 완전종료 2, 게임종료 3
-        int menu = RESTART;
-        while (true) {
-            // 랜덤숫자 세팅
-            if (menu == RESTART) {
-                numberBaseBall.setRandomNumber(numberBaseBall.makeRandomNumber(MIN_NUMBER, MAX_NUMBER));
-                menu = START_GAME;
-                System.out.println("randomNumbers: " + numberBaseBall.randomNumber);
-            }
-            // 사용자 입력
-            String playerInput = player.playerInputNumber(scanner);
-            // 게임 결과
-            String roundResult = numberBaseBall.makeHint(playerInput, numberBaseBall.randomNumber);
-            System.out.println(roundResult);
-            if (roundResult.charAt(0) - '0' == GAME_OVER) {
-                menu = player.playerInputMenu(scanner);
-            }
-            if (menu == SHUTDOWN)
+
+        while (gameStatus == GAME_START) {
+            numberBaseBall.setRandomNumbers(numberBaseBall.makeRandomNumbers(MIN_NUMBER, MAX_NUMBER));// 랜덤숫자 세팅
+            System.out.println("randomNumbers: " + numberBaseBall.getRandomNumbers());
+            gameStatus = playGame(player, numberBaseBall, scanner);
+            if (gameStatus == SHUTDOWN)
                 break;
         }
     }
+
+    private static int playGame(Player player, NumberBaseBall numberBaseBall, Scanner scanner) {
+        String playerInput = player.playerInputNumbers(scanner); // 사용자 입력
+        String roundResult = numberBaseBall.makeHint(playerInput, numberBaseBall.getRandomNumbers()); // 게임 결과
+        System.out.println(roundResult);
+        if (roundResult.contains("3스트라이크")) {
+            return player.playerInputMenu(scanner);
+        }
+        return playGame(player, numberBaseBall, scanner);
+    }
+
 }
