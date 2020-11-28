8f91c89d1cb9d4fed5928ff37a386153f815c147
baekchan1024 <dlqhs1024@naver.com>
2020-11-28 23:49:16 +0900

    feat : Constants 클래스 추가
    리터럴 값들 변수에 넣을 클래스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,20 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-        
-    }
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		// TODO 구현 진행
+		String[] randomNumbers = new String[3];
+
+		for (int i = 0; i < 3; i++) {
+			int random = RandomUtils.nextInt(1, 9);
+			randomNumbers[i] = String.valueOf(random);
+		}
+		for (int i = 0; i < 3; i++) {
+		System.out.println(randomNumbers[i]);
+		}
+	}
 }
