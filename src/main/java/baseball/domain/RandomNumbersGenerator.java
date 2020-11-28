b33e44f32b18f5c93becc054caee40331baa25f9
naru <hsik0225@gmail.com>
2020-11-28 19:12:47 +0900

    refactor(random-numbers-generator): 랜던값 boundery 상수화

--- src/main/java/baseball/domain/RandomNumbersGenerator.java
+++ src/main/java/baseball/domain/RandomNumbersGenerator.java
@@ -5,12 +5,16 @@ import utils.RandomUtils;
 
 public class RandomNumbersGenerator implements NumbersGenerator {
 
+    public static final int LOWER_BOUND = 0;
+
+    public static final int UPPER_BOUND = 10;
+
     @Override
     public String generateNumbers() {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < BaseballGame.BALLS_LENGTH; i++) {
-            stringBuilder.append(RandomUtils.nextInt(0, 10));
+            stringBuilder.append(RandomUtils.nextInt(LOWER_BOUND, UPPER_BOUND));
         }
 
         return stringBuilder.toString();
