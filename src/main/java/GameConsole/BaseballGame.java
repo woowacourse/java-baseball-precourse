1e33c6552ce1d3a05cc63081bab51196f2bb0ba8
tomas <cmg14111@gmail.com>
2020-11-29 02:21:40 +0900

    feat: main함수 구현, 게임 동작 클래스 구현
    main함수에서 게임이 돌아가는 전체적인 반복을 구현하였음.
    한 게임마다의 동작인 BaseballGame클래스를 구현하였음.

---
+++ src/main/java/GameConsole/BaseballGame.java
@@ -0,0 +1,20 @@
+package GameConsole;
+
+import java.util.ArrayList;
+
+public class BaseballGame {
+
+    public static BaseballGame createBaseballGame() {
+        return new BaseballGame();
+    }
+
+    public int playGame(ArrayList<Integer> answerNumber) {
+
+        return 0;
+    }
+
+    public boolean continueGame(int result) {
+
+        return true;
+    }
+}
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,25 @@
 package baseball;
 
+import GameConsole.BaseballGame;
+import numbers.AnswerNumber;
 import numbers.UserSelectNumber;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        AnswerNumber answerNumber = AnswerNumber.creatAnswerNumber();
+        BaseballGame baseballGame = BaseballGame.createBaseballGame();
+        int gameResult;
+        ArrayList<Integer> answer;
+        do {
+            answer = answerNumber.getAnswerNumber();
+            gameResult = baseballGame.playGame(answer);
+        } while(baseballGame.continueGame(gameResult));
+
+        scanner.close();
     }
 }
