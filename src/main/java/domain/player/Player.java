befaed9178c5f59f5d461cbc682e6b4eae414ffe
jaeseongDev <qkrwotjd1445@naver.com>
2020-11-28 13:15:15 +0900

    refactor(Player) : 게임 시작, 게임 재시작 기능에서 메서드 분리를 통한 리팩토링
    - 게임 재시작 여부를 물어보는 기능을 메서드(checkIfRestartGame)로 분리
    - 게임 한 판을 하는 기능을 메서드(playOneGame)로 분리
    - 사용되지 않는 메서드(isStartingGame) 삭제
    - 잘못된 접근 제어가 설정되어 있는 메서드(enterNumberWith) 수정

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -5,38 +5,25 @@ import domain.computer.Computer;
 import java.util.*;
 
 public class Player {
-    private boolean isStartingGame = false;
+    boolean isStartingGame = true;
     private String userInput;
     String[] seperatedUserInputByLetter;
     Computer computer = new Computer();
 
     public void startGame(Scanner scanner) {
-        int status;
-        isStartingGame = true;
         while (isStartingGame) {
-            enterNumberWith(scanner);
-            if (computer.isCorrectAnswer(seperatedUserInputByLetter)) {
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                status = scanner.nextInt();
-                if (status != 1 && status != 2) {
-                    throw new IllegalArgumentException("숫자 1 또는 2만 입력하셔야 합니다");
-                }
-                if (status == 1) {
-                    isStartingGame = true;
-                    computer.resetAnswer();
-                }
-                if (status == 2) {
-                    isStartingGame = false;
-                }
-            }
+            playOneGame(scanner);
         }
     }
 
-    public boolean isStartingGame() {
-        return isStartingGame;
+    private void playOneGame(Scanner scanner) {
+        enterNumberWith(scanner);
+        if (computer.isCorrectAnswer(seperatedUserInputByLetter)) {
+            isStartingGame = checkIfRestartGame(scanner);
+        }
     }
 
-    public void enterNumberWith(Scanner scanner) {
+    private void enterNumberWith(Scanner scanner) {
         System.out.print("숫자를 입력해주세요 : ");
         userInput = scanner.next();
         seperatedUserInputByLetter = userInput.split("");
@@ -103,5 +90,18 @@ public class Player {
             throw new IllegalArgumentException("세 자리의 숫자에서 각 자리의 숫자에 중복된 숫자가 포함되면 안 됩니다");
         }
         return false;
+    }
+
+    private boolean checkIfRestartGame(Scanner scanner) {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int status = scanner.nextInt();
+        if (status == 1) {
+            computer.resetAnswer();
+            return true;
+        }
+        if (status == 2) {
+            return false;
+        }
+        throw new IllegalArgumentException("숫자 1 또는 2만 입력하셔야 합니다");
     }
 }
