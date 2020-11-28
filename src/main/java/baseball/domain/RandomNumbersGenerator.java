cf0ecec1714d28dea6d6b8503a1019caef131e24
naru <hsik0225@gmail.com>
2020-11-28 20:06:43 +0900

    refactor(random-numbers-generator): 중복값이 있을 경우 랜덤 숫자 재생성

--- src/main/java/baseball/domain/RandomNumbersGenerator.java
+++ src/main/java/baseball/domain/RandomNumbersGenerator.java
@@ -11,6 +11,15 @@ public class RandomNumbersGenerator implements NumbersGenerator {
 
     @Override
     public String generateNumbers() {
+        String numbers  = getRandomValues();
+        while (hasDuplicateNumber(numbers)) {
+            numbers = getRandomValues();
+        }
+
+        return numbers;
+    }
+
+    private String getRandomValues() {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < BaseballGame.BALLS_LENGTH; i++) {
@@ -18,5 +27,10 @@ public class RandomNumbersGenerator implements NumbersGenerator {
         }
 
         return stringBuilder.toString();
+    }
+
+    private boolean hasDuplicateNumber(String input) {
+        int deduplicatedNumbersLength = (int) input.chars().distinct().count();
+        return deduplicatedNumbersLength != BaseballGame.BALLS_LENGTH;
     }
 }
