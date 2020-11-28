4edfd75780009e66cdfd91bcb6f9caba04c8dbca
김주원 <kjw11077@naver.com>
2020-11-28 13:45:28 +0900

    refactor(GameManager): update gaming()
    출력문 추가, 반복문 수정

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -20,12 +20,17 @@ public class GameManager {
         Balls myBalls;
         GameResult gameResult;
 
-        do {
+        while (true) {
             botBalls = Balls.generateRandomBalls();             // 랜덤 생성 balls
             myBalls = getMyBalls();                             // 사용자 balls
             gameResult = myBalls.judgeResult(botBalls);         // 결과 계산
             System.out.println(gameResult.getResult());         // 결과 출력
-        } while (!gameResult.isThreeStrikes());                 // 3스트라이크: 루프탈출
+
+            if (gameResult.isThreeStrikes()) {                  // 3스트라이크: 루프탈출
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
     }
 
     private Balls getMyBalls() {
