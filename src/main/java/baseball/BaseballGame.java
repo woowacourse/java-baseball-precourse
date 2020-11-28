ab359f646b07afab4180c61f7bd4bf3235bd1cbf
Naeun <knae11@naver.com>
2020-11-28 18:30:58 +0900

    feat: 힌트 보여주기

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -11,6 +11,10 @@ public class BaseballGame {
     private final int THREE_DIGIT = 3;
     private int[] computerNumbers;
     private int[] userNumbers = new int[THREE_DIGIT];
+    private boolean isPlaying = true;
+    private final String STRIKE = "스트라이크";
+    private final String BALL = "볼";
+    private final String NOTHING = "낫싱";
 
 
     public BaseballGame(Scanner scanner) {
@@ -20,7 +24,8 @@ public class BaseballGame {
     }
 
     private void gameStart() {
-        while (true) {
+        while (isPlaying) {
+            System.out.println("답 : " + Arrays.toString(computerNumbers) );
             getUserNumbers();
             showHints();
         }
@@ -36,6 +41,45 @@ public class BaseballGame {
     }
 
     private void showHints() {
+        int strike = 0;
+        int ball = 0;
+        int[] resultState = checkState(strike, ball);
+        strike = resultState[0];
+        ball = resultState[1];
+
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball + BALL + " " + strike + STRIKE);
+        }
+        if (strike != 0 && ball == 0) {
+            System.out.println(strike + STRIKE);
+        }
+        if (strike == 0 && ball != 0) {
+            System.out.println(ball + BALL);
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.println(NOTHING);
+        }
+    }
+
+    private int[] checkState(int strike, int ball) {
+        for (int i = 0; i < THREE_DIGIT; i++) {
+            if (computerNumbers[i] == userNumbers[i]) {
+                strike += 1;
+                continue;
+            }
+            ball = checkBallState(i, ball);
+        }
+        int[] resultState = {strike, ball};
+        return resultState;
+    }
+
+    private int checkBallState(int i, int ball) {
+        for (int j = 0; j < THREE_DIGIT; j++) {
+            if (computerNumbers[j] == userNumbers[i]) {
+                return ball +1;
+            }
+        }
+        return ball;
     }
 
     private void checkValidNumbers(String numbers) {
