d97fc8a9bdb2b818187114298b6bca336f2598cf
operasystem6 <operasystem6@naver.com>
2020-11-28 23:23:47 +0900

    refactor: 프로그램 랜덤 숫자 생성 알고리즘 변경

--- src/main/java/baseball/domain/RandomNumberGenerator.java
+++ src/main/java/baseball/domain/RandomNumberGenerator.java
@@ -1,9 +1,9 @@
 /*
  * GenerateRandomNumber.java
  *
- * version 1.0
+ * version 1.1
  *
- * 2020/11/27
+ * 2020/11/28
  *
  * Copyright (c) 2020 Junhyoung Park
  */
@@ -12,17 +12,38 @@ package baseball.domain;
 
 import utils.RandomUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomNumberGenerator {
+    private static int NUM_LEN = 3;
+
+    private List<Integer> numberList = new ArrayList<>();
 
     public String getRandomNumber() {
-        String randomNumber = "";
-        InputValidation inputValidation;
+        return generateRandomNumber(initNumberList());
+    }
 
-        do {
-            randomNumber = Integer.toString(RandomUtils.nextInt(123, 987));
-            inputValidation = new InputValidation(randomNumber);
-        } while(!inputValidation.checkValidNumber());
+    private List<Integer> initNumberList() {
+        for (int i = 1; i < 10; i ++) {
+            numberList.add(i);
+        }
+
+        return numberList;
+    }
+
+    private String generateRandomNumber(List<Integer> numberList) {
+        String randomNumber = "";
+        int index = 0;
+        int listSize = 0;
+
+        for (int i = 0; i < NUM_LEN; i++) {
+            listSize = numberList.size();
+            index = RandomUtils.nextInt(0, listSize - 1);
+            randomNumber += Integer.toString(numberList.remove(index));
+        }
 
         return randomNumber;
     }
+
 }
