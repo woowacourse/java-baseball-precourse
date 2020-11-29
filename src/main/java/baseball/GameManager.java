4609f8485f6e478d049514a3b0a6a3633472e487
박우림 <woorimprog@gmail.com>
2020-11-29 23:37:47 +0900

    refactor(game-manager) : update a typing error

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -33,7 +33,7 @@ public class GameManager {
 	public void query() {
 		final Scanner scanner = new Scanner(System.in);
 
-		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 		answer = scanner.nextInt();
 	}
 
