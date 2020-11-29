583b33ebb9b13ed1d5f113187f5f49dba9f86ffd
Jaeyoung Shim <sjy4566@naver.com>
2020-11-29 19:02:59 +0900

    feat(application): check whether correct answer or not

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,6 +25,11 @@ public class Application {
         }
 
         application.printResult();
+
+        if (application.isCorrectAnswer()) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
     }
 
     private void validateInput(int input) {
@@ -111,5 +116,9 @@ public class Application {
         }
 
         System.out.println(sb.toString());
+    }
+
+    private boolean isCorrectAnswer() {
+        return getNumOfStrikes() == 3;
     }
 }
