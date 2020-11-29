41d98f29ca200fdbde27ce6b9821ba2b16c3d24e
Jack2ee-dev <jack2ee.dev@gmail.com>
2020-11-29 15:06:33 +0900

    feat(Game): 게임 진행 로직 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,50 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Game {
+    private RandomNumbers randomNumbers;
+    private Player player;
+    private InputHandler inputHandler;
+    private Result result;
+
+    public Game(Scanner scanner) {
+        this.randomNumbers = new RandomNumbers();
+        this.player = new Player(scanner);
+        this.inputHandler = new InputHandler(scanner);
+    }
+
+    public void start() {
+        while (true) {
+            init();
+            if (this.inputHandler.restart()) {
+                start();
+            }
+            break;
+        }
+    }
+
+    private void init() {
+        this.randomNumbers.setNumbers();
+        System.out.println(this.randomNumbers.getNumbers());
+        turn();
+    }
+
+    private void turn() {
+        while (true) {
+            this.player.setNumbers();
+            this.result = new Result(this.randomNumbers, this.player);
+            System.out.println(this.result);
+            if (stop()) {
+                break;
+            };
+        }
+    }
+
+    private boolean stop() {
+        if (this.result.terminated()) {
+            return true;
+        }
+        return false;
+    }
 }
