204644a13d54e7585b4808bac3e142dbb2054e6e
박우림 <woorimprog@gmail.com>
2020-11-29 23:58:30 +0900

    feat(player) : add player exception handling

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -22,6 +22,10 @@ public class Player {
 		
 		if (number.length() != 3) {
 			throw new IllegalArgumentException("\n\t세자리 수를 입력하시오.\n");
+		} else if (!Validator.isStrinNumber(number)) {
+			throw new IllegalArgumentException("\n\t숫자를 입력해 주십시오.\n");
+		} else if (!Validator.isUseable(number) ) {
+			throw new IllegalArgumentException("\n\t1~9까지 서로 다른 값을 입력해 주십시오.\n");
 		}
 		setNumber(number);
 	}
--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -5,17 +5,26 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Validator {	
-	public static boolean isUseable(String random) {
+public class Validator {
+	public static boolean isUseable(String number) {
 		Set<String> set;
-		List<String> randomList = Arrays.asList(random.split(""));
-		
+		List<String> randomList = Arrays.asList(number.split(""));
+
 		set = new HashSet<String>(randomList);
-		
-		if (random.contains("0")) {
+
+		if (number.contains("0")) {
 			return false;
 		}
-		
+
 		return set.size() == GameManager.MAX_NUM_LENGTH;
+	}
+
+	public static boolean isStrinNumber(String s) {
+		try {
+			Double.parseDouble(s);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
