7fa9c06e94ffe979ab1d96cd35c0b3933942f6c0
최문경 <munkyung0324@gmail.com>
2020-11-29 22:35:46 +0900

    feat(Application): add ongoing select logic
    Added ongoing select logic:
    - add PLAY(0), ING(1), FINISH(2) variable
    - change distinct logic to function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,71 +2,87 @@ package baseball;
 
 import utils.RandomUtils;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Application {
-    static boolean[] duplCheck;
+    static boolean[] checkDistinct;
+    static final int PLAY = 0;
+    static final int ING = 1;
+    static final int FINISH = 2;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
-        // 프로그램이 1에서 9까지 서로 다른 임의의 수 3개를 정한다
-        duplCheck = new boolean[10];
-        int[] systemNum = new int[3];
-        int numCnt = 0;
-        while(numCnt < 3) {
-            int tmp = RandomUtils.nextInt(1, 9);
-            if(!isDupl(tmp)) {
-                systemNum[numCnt] = tmp;
-                numCnt++;
+        int ongoingInput = PLAY;
+        checkDistinct = new boolean[10];
+        int[] systemNum = getDistinctNumber();
+
+        while(ongoingInput < FINISH) {
+            // 플레이어가 3자리 수를 입력한다.
+            System.out.print("숫자를 입력해주세요 : ");
+            char[] charUserNum = scanner.next().toCharArray();
+
+            int[] userNum = new int[3];
+            for (int i = 0; i < 3; i++) {
+                userNum[i] = charUserNum[i] - '0';
             }
-        }
-        System.out.println(Arrays.toString(systemNum));
 
-        // 플레이어가 3자리 수를 입력한다.
-        System.out.print("숫자를 입력해주세요 : ");
-        char[] charUserNum = scanner.next().toCharArray();
+            // 스트라이크 체크하기
+            int strikeCnt = checkStrike(userNum, systemNum);
 
-        int[] userNum = new int[3];
-        for (int i = 0; i < 3; i++) {
-            userNum[i] = charUserNum[i] - '0';
-        }
+            // 정답 체크하기
+            if (strikeCnt == 3) {
+                System.out.println(strikeCnt + "스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                ongoingInput = scanner.nextInt();
+            }
 
-        System.out.println(Arrays.toString(userNum));
+            // 게임을 완전히 종료할 지 체크하기
+            if(ongoingInput == FINISH) {
+                break;
+            } else if(ongoingInput == ING) {
+                checkDistinct = new boolean[10];
+                systemNum = getDistinctNumber();
+                ongoingInput = PLAY;
+                continue;
+            }
 
-        // 스트라이크 체크하기
-        int strikeCnt = checkStrike(userNum, systemNum);
-
-        // 정답 체크하기
-        if(strikeCnt == 3) {
-            System.out.println(strikeCnt + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-
-        // 볼 체크하기
-        int ballCnt = checkBall(userNum, systemNum);
-        if(ballCnt == 0 && strikeCnt == 0) {
-            System.out.println("낫싱");
-        } else if(ballCnt == 0){
-            System.out.println(strikeCnt + "스트라이크");
-        } else if(strikeCnt == 0) {
-            System.out.println(ballCnt + "볼");
-        } else {
-            System.out.println(ballCnt + "볼" + " " + strikeCnt + "스트라이크");
+            // 볼 체크하기
+            int ballCnt = checkBall(userNum, systemNum);
+            if (ballCnt == 0 && strikeCnt == 0) {
+                System.out.println("낫싱");
+            } else if (ballCnt == 0) {
+                System.out.println(strikeCnt + "스트라이크");
+            } else if (strikeCnt == 0) {
+                System.out.println(ballCnt + "볼");
+            } else {
+                System.out.println(ballCnt + "볼" + " " + strikeCnt + "스트라이크");
+            }
         }
 
     }
 
-    private static int checkBall(int[] userNum, int[] systemNum) {
-        int ballCnt = 0;
-        for (int i = 0; i < 3; i++) {
-            int tmp = userNum[i];
-            if(tmp != systemNum[i] && duplCheck[tmp]) {
-                ballCnt++;
+    private static int[] getDistinctNumber() {
+        int[] systemNum = new int[3];
+        int numCnt = 0;
+        while (numCnt < 3) {
+            int tmp = RandomUtils.nextInt(1, 9);
+            if (!isDistinct(tmp)) {
+                systemNum[numCnt] = tmp;
+                numCnt++;
             }
         }
-        return ballCnt;
+        return systemNum;
+    }
+
+    private static boolean isDistinct(int checkNum) {
+        if(checkDistinct[checkNum]) {
+            return true;
+        } else {
+            checkDistinct[checkNum] = true;
+            return false;
+        }
     }
 
     private static int checkStrike(int[] userNum, int[] systemNum) {
@@ -79,12 +95,15 @@ public class Application {
         return strikeCnt;
     }
 
-    private static boolean isDupl(int checkNum) {
-        if(duplCheck[checkNum]) {
-            return true;
-        } else {
-            duplCheck[checkNum] = true;
-            return false;
+    private static int checkBall(int[] userNum, int[] systemNum) {
+        int ballCnt = 0;
+        for (int i = 0; i < 3; i++) {
+            int tmp = userNum[i];
+            if(tmp != systemNum[i] && checkDistinct[tmp]) {
+                ballCnt++;
+            }
         }
+        return ballCnt;
     }
+
 }
