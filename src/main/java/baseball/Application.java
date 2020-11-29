1f518603db74693922866cbacca3e876db48373b
Gomding <backchida@gmail.com>
2020-11-29 19:15:25 +0900

    fix : 사용자의 입력을 받는 기능 수정
    3자리 숫자입력받는 기능 수정
    게임 재시작 기능 좀 더 명확하게 구현
    무엇을 입력해야하는지 알려주는 출력 추가 ex) 숫자를 입력해주세요 :, 1에서 9 사이의 서로 다른 숫자 3자리를 입력해주세요.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,11 +6,22 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        while (true) {
-            BaseballGame.startGame(scanner);
-            int inputNum = scanner.nextInt();
 
-            if (inputNum == 2); {
+        while (true) {
+            BaseballGame.startGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int inputNum = 0;
+
+            while (inputNum != 1 && inputNum != 2) {
+                inputNum = scanner.nextInt();
+
+                if (inputNum != 1 && inputNum != 2) {
+                    System.out.println("[잘못된 입력]게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                }
+            }
+
+            if (inputNum == 2) {
+                System.out.println("프로그램을 종료합니다.");
                 break;
             }
         }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 
 public class BaseballGame {
     public static Computer computer;
-    public static User user;
+    public static User user = new User();
+    public static Scanner scanner = new Scanner(System.in);
 
-    public static void startGame(Scanner scanner) {
+    public static void startGame() {
         computer = new Computer();
 
         while (true) {
             try {
-                user.makeNumber(scanner.nextLine());
+                System.out.print("숫자를 입력해주세요 : ");
+                String input = scanner.nextLine();
+                user.makeNumber(input);
             } catch (IllegalArgumentException e) {
-                System.out.println("1에서 9사이의 서로 다른 숫자 3자리를 입력해주세요.");
+                System.out.println("[잘못된 입력] 1에서 9사이의 서로 다른 숫자 3자리를 입력해주세요.");
                 continue;
             }
 
             int strike = 0;
             int ball = 0;
-
+            break;
         }
     }
 }
