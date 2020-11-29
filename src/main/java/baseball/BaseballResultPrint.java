d6aaaecbaa5fc70159fd2e9d71ecef89c115055c
sjparkk <66293784+sjparkk@users.noreply.github.com>
2020-11-29 18:40:48 +0900

    fix 게임 결과 출력 방법 수정

--- src/main/java/baseball/BaseballResultPrint.java
+++ src/main/java/baseball/BaseballResultPrint.java
@@ -3,18 +3,18 @@ package baseball;
 public class BaseballResultPrint {
     public static void resultPrint(BaseballCounterResult baseballCounterResult) {
 
-        if (1 <= baseballCounterResult.getBall() && 1 <= baseballCounterResult.getStrike()) {
-            System.out.println(String.format("%d볼 %d스트라이크 입니다.", baseballCounterResult.getBall(), baseballCounterResult.getStrike()));
-        } else if (1 <= baseballCounterResult.getBall() && baseballCounterResult.getStrike() == 0) {
-            System.out.println(baseballCounterResult.getBall() + "볼 입니다.");
-        } else if (0 == baseballCounterResult.getBall() && baseballCounterResult.getStrike() == 1) {
-            System.out.println(baseballCounterResult.getStrike() + "스트라이크 입니다.");
-        } else if (0 == baseballCounterResult.getBall() && baseballCounterResult.getStrike() == 2) {
-            System.out.println(baseballCounterResult.getStrike() + "스트라이크 입니다.");
-        } else if (baseballCounterResult.getStrike() == 3) {
+        if (baseballCounterResult.getBall() > 0) {
+            System.out.printf("%d볼 ", baseballCounterResult.getBall());
+        }
+        if (baseballCounterResult.getStrike() > 0) {
+            System.out.printf("%d스트라이크", baseballCounterResult.getStrike());
+        }
+        if (baseballCounterResult.getBall() == 0 && baseballCounterResult.getStrike() == 0) {
+            System.out.printf("낫싱");
+        }
+        System.out.println();
+        if (baseballCounterResult.getStrike() == 3) {
             System.out.println(baseballCounterResult.getStrike() + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } else {
-            System.out.println("낫싱");
         }
     }
 }
