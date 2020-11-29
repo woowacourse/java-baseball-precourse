e6e0e43d2fcf90787eefb8820a3b4690cda362ff
Injun <ison@sfu.ca>
2020-11-29 16:10:40 +0900

    refactor(baseball): 임의의 수 생성하는 기능 RandomNumber 클래스로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,20 +11,7 @@ public class Application {
         // TODO 구현 진행
         final int NUMBER_LENGTH = 3;
 
-        String randomNumber = generateNumbers(NUMBER_LENGTH);
-
-    }
-
-    //1에서 9까지 서로 다른 임의의 수 3개를 생성하는 함수
-    public static String generateNumbers(int numberLength){
-        String returnNumber = "";
-        while(returnNumber.length() < numberLength){
-            //숫자를 생성해서 String으로 형변환 한다
-            String tempNumber = Integer.toString(RandomUtils.nextInt(0,9));
-            if(!returnNumber.contains(tempNumber)){
-                returnNumber += tempNumber;
-            }
-        }
-        return returnNumber;
+        String randomNumber = RandomNumber.generateNumbers(NUMBER_LENGTH);
+        System.out.print(randomNumber);
     }
 }
---
+++ src/main/java/baseball/RandomNumber.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import utils.RandomUtils;
+
+public class RandomNumber {
+    static final int MIN_NUMBER = 1;
+    static final int MAX_NUMBER = 9;
+
+    //1에서 9까지 서로 다른 임의의 수 3개를 생성하는 함수
+    public static String generateNumbers(int numberLength){
+        String returnNumber = "";
+        while(returnNumber.length() < numberLength){
+            //숫자를 생성해서 String으로 형변환 한다
+            String tempNumber = Integer.toString(RandomUtils.nextInt(MIN_NUMBER,MAX_NUMBER));
+            if(!returnNumber.contains(tempNumber)){
+                returnNumber += tempNumber;
+            }
+        }
+        return returnNumber;
+    }
+}
