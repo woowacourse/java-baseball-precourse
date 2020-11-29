98a3b206e00ae8851cf2ed4d7814050e54a61e29
sor <bosl95@naver.com>
2020-11-30 06:40:12 +0900

    style(baseball) : Appplication 불필요한 변수 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,8 +5,6 @@ import baseball.service.BaseballGame;
 import java.util.Scanner;
 
 public class Application {
-    private static final String GAME_START = "1";
-
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
