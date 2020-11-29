c3cb88b2198c4e98859d692fa5be33747a4673e2
(학생) 이서윤 (전기전자컴퓨터공학부) <lynn312@unist.ac.kr>
2020-11-29 22:44:41 +0900

    user_guess_input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,39 @@
 package baseball;
 
 import java.util.Scanner;
+import javax.security.sasl.SaslServer;
+import utils.RandomUtils;
+
+class Game{
+    private int guess;
+    private int answer;
+
+
+    Game(){
+
+    }
+
+};
 
 public class Application {
+
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        boolean isEnd = false;
+        int guess;
+        int userChoice;
+        int answer;
+
+        while(!isEnd){
+            System.out.print("숫자를 입력해주세요 : ");
+            final Scanner scanner = new Scanner(System.in);
+            guess = scanner.nextInt();
+
+            Game g = new Game();
+
+
+
+        }
+
+
     }
 }
