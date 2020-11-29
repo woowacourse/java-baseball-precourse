44a76de6d2343fb1ac25b2cee59f57402cd659da
eoc940 <hgk0000@naver.com>
2020-11-29 15:25:49 +0900

    feat: add result method in Game

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -39,4 +39,22 @@ public class Game {
         return ball;
     }
 
+    //스트라이크, 볼 결과를 출력하는 메서드
+    public void result(int strike, int ball) {
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        if ((ball == 0) && (strike == 0)) {
+            System.out.print("낫싱");
+        }
+        System.out.println();
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
+
 }
