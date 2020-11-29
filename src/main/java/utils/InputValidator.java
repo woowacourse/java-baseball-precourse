a8fd642797b89e0c804f6d63f2f04df925123978
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2020-11-29 19:34:11 +0900

    refactor: 반복문 초기 index 상수 처리한 것 수정

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -4,11 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class InputValidator {
-    private static final int FIRST_INDEX = 0;
     private static final int NUMBERS_LENGTH = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-
     private static final String INCORRECT_NUMBER_ERROR = "올바른 숫자를 입력해주세요.";
     private static final String NUMBER_LENGTH_ERROR = "3자리 숫자를 입력해주세요.";
     private static final String NUMBER_RANGE_ERROR = "1~9의 숫자를 입력해주세요.";
@@ -44,7 +42,7 @@ public class InputValidator {
     }
 
     private static void validateRange(String numbers) {
-        for (int i = FIRST_INDEX; i < NUMBERS_LENGTH; i++) {
+        for (int i = 0; i < NUMBERS_LENGTH; i++) {
             int number = Character.getNumericValue(numbers.charAt(i));
             if (number < MIN_NUMBER || number > MAX_NUMBER) {
                 throw new IllegalArgumentException(NUMBER_RANGE_ERROR);
@@ -54,10 +52,10 @@ public class InputValidator {
 
     private static void validateDuplicate(String numbers) {
         duplicateCheckSet = new HashSet<>();
-        for(int i=FIRST_INDEX; i < NUMBERS_LENGTH; i++) {
+        for(int i = 0; i < NUMBERS_LENGTH; i++) {
             duplicateCheckSet.add(numbers.charAt(i));
         }
-        if(duplicateCheckSet.size() != NUMBERS_LENGTH) {
+        if (duplicateCheckSet.size() != NUMBERS_LENGTH) {
             throw new IllegalArgumentException(NUMBER_DUPLICATE_ERROR);
         }
     }
