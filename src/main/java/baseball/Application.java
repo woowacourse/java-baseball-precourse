832f89804f82969183358a36431037a5ddb0a2a0
Gomding <backchida@gmail.com>
2020-11-29 21:20:33 +0900

    refactor : Application 클래스, BaseballGame 클래스 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,20 +10,56 @@ public class Application {
         while (true) {
             BaseballGame.startGame();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int inputNum = 0;
 
-            while (inputNum != 1 && inputNum != 2) {
-                inputNum = scanner.nextInt();
 
-                if (inputNum != 1 && inputNum != 2) {
-                    System.out.println("[잘못된 입력]게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                }
-            }
+            int inputNum = inputNum(scanner);
 
             if (inputNum == 2) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }
         }
+    }
+
+    /**
+     * 사용자에게 1또는 2를 입력받는 메서드,
+     * 1또는 2 이외의 값을 입력 받으면  IllegalArgumentException 발생
+     *
+     * @param scanner 입력 받기위한 Scanner 타입의 매개변수
+     * @return 1또는 2의 값을 반환합니다.
+     */
+    private static int inputNum(Scanner scanner) {
+        int inputNum = 0;
+        while (!isOneOrTwo(inputNum)) {
+            inputNum = scanner.nextInt();
+            try {
+                checkInputNum(inputNum);
+            } catch (IllegalArgumentException e) {
+                System.out.println("[잘못된 입력]게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }
+        }
+        return inputNum;
+    }
+
+    /**
+     * 사용자가 입력한 값이 1 또는 2인지 확인하는 메서드.
+     *
+     * @param inputNum 사용자가 입력한 값을 매개변수로 받습니다.
+     * @throws IllegalArgumentException 1또는 2 이외의 값을 입력받으면 해당 예외를 던집니다.
+     */
+    private static void checkInputNum(int inputNum) throws IllegalArgumentException{
+        if (!isOneOrTwo(inputNum)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * 매개변수 inputNum이 1또는 2인지 확인하는 메서드
+     *
+     * @param inputNum 사용자가 입력한 값
+     * @return 1또는 2 라면 true, 아니라면 false
+     */
+    private static boolean isOneOrTwo(int inputNum) {
+        return inputNum == 1 || inputNum == 2;
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,13 +2,21 @@ package baseball;
 
 import java.util.Scanner;
 
+/**
+ * 숫자 야구 게임을 진행하는 클래스
+ *
+ * @version 1.0 2020-11-29
+ */
 public class BaseballGame {
     public static Computer computer;
     public static User user = new User();
-    public static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * 숫자 야구 게임을 시작하는 메서드
+     */
     public static void startGame() {
         computer = new Computer();
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             try {
@@ -20,37 +28,89 @@ public class BaseballGame {
                 continue;
             }
 
-            String userNum = user.getNumber();
-            String comNum = computer.getNumber();
-            int strike = 0;
-            int ball = 0;
-
-            for (int i = 0; i < 3; i++) {
-
-                if (comNum.charAt(i) == userNum.charAt(i)) {
-                    strike++;
-                } else if (comNum.contains(String.valueOf(userNum.charAt(i)))) {
-                    ball++;
-                }
-            }
-
-            StringBuilder sb = new StringBuilder();
-
-            if (ball > 0) {
-                sb.append(ball + "볼 ");
-            }
-            if (strike > 0) {
-                sb.append(strike + "스트라이크");
-            }
-            if (strike == 0 && ball == 0) {
-                sb.append("낫싱");
-            }
-            System.out.println(sb);
-
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            if (isCorrectAnswer()) {
                 break;
             }
+            checkNumber();
         }
+    }
+
+    /**
+     * 사용자가 입력한 숫자가 정답인지 확인하고 정답이면 true, 오답이면 false를 반환
+     *
+     * @return 정답이면 true, 오답이면 false를 반환
+     */
+    private static boolean isCorrectAnswer() {
+        if (computer.getNumber().equals(user.getNumber())) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 힌트를 제공하기 위해 사용자의 입력값을 검사하는 메서드
+     *
+     * strike : 같은 자리에 같은 숫자가 있는 경우
+     * ball : 다른 자리에 같은 숫자가 있는 경우
+     */
+    private static void checkNumber() {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+
+            if (isStrike(i)) {
+                strike++;
+            } else if (isBall(i)) {
+                ball++;
+            }
+        }
+
+        printHint(strike, ball);
+
+    }
+
+    /**
+     * 사용자와 컴퓨터의 숫자 하나가 같은 자리에 같은 숫자가 있는 경우인지 판단하는 메서드
+     *
+     * @param index 검사해야하는 위치값
+     * @return 같은 자리에 같은 숫자가 있으면 true, 아니면 false
+     */
+    private static boolean isStrike(int index) {
+        return computer.getNumber().charAt(index) == user.getNumber().charAt(index);
+    }
+
+    /**
+     * 사용자의 숫자 하나가 같은 자리는 아니지만 컴퓨터의 숫자안에 존재하는지 판단하는 메서드
+     *
+     * @param index 사용자의 숫자에 검사해야하는 위치값
+     * @return 사용자의 숫자 하나가 같은 자리는 아니지만 컴퓨터의 숫자안에 존재한다면 true, 아니면 false
+     */
+    private static boolean isBall(int index) {
+        return computer.getNumber().contains(String.valueOf(user.getNumber().charAt(index)));
+    }
+
+    /**
+     * 힌트를 출력해주는 메서드 strike도 ball도 없다면 "낫싱"을 출력합니다.
+     *
+     * @param strike 같은 자리에 같은 숫자가 있는 경우를 나타낸 int 타입의 값
+     * @param ball 다른 자리에 같은 숫자가 있는 경우를 나타낸 int 타입의 값
+     */
+    private static void printHint(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+
+        if (ball > 0) {
+            sb.append(ball + "볼 ");
+        }
+        if (strike > 0) {
+            sb.append(strike + "스트라이크");
+        }
+        if (strike == 0 && ball == 0) {
+            sb.append("낫싱");
+        }
+        System.out.println(sb);
+
     }
 }
