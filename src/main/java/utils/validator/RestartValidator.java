c480c911ce5ed325b6c485acc1263548582371e2
sor <bosl95@naver.com>
2020-11-30 03:43:41 +0900

    feat(validator) : RestartValidator의 excute() 메소드 구현

--- src/main/java/utils/validator/RestartValidator.java
+++ src/main/java/utils/validator/RestartValidator.java
@@ -1,15 +1,14 @@
 package utils.validator;
 
-import static baseball.domain.Game.GAME_END;
-import static baseball.domain.Game.GAME_START;
+import baseball.domain.Game;
 
 public class RestartValidator implements Validator {
     @Override
     public boolean execute(String input) {
-        if (input.equals(GAME_START) || input.equals(GAME_END)) {
+        if (input.equals(Game.GAME_START) || input.equals(Game.GAME_END)) {
             return true;
         }
-        
+
         return false;
     }
 }
