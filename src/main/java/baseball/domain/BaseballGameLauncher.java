f0e8498179580e91633b36b1c0131d56a3c6363f
LeeJuHyuk <dlwngur1535@gmail.com>
2020-11-29 20:05:49 +0900

    feat: 게임 종료 또는 재시작 기능 추가

--- src/main/java/baseball/domain/BaseballGameLauncher.java
+++ src/main/java/baseball/domain/BaseballGameLauncher.java
@@ -4,13 +4,35 @@ import java.util.Scanner;
 
 public class BaseballGameLauncher {
 
-    public static void play(Scanner scanner){
+    public static void play(Scanner scanner) {
+        playNewBaseballGame(scanner);
+        try {
+            confirmGameRestart(scanner);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            confirmGameRestart(scanner);
+        }
+    }
+
+    private static void playNewBaseballGame(Scanner scanner) {
         BaseballGame baseballGame = new BaseballGame();
         Player player = new Player();
 
-        while(baseballGame.isGameOver()) {
+        while (baseballGame.isGameOver()) {
             player.inputBaseballNumbers(scanner);
             baseballGame.compareBaseBallNumbers(player.getBaseballNumbers());
+        }
+    }
+
+    private static void confirmGameRestart(Scanner scanner) {
+        System.out.println("게임을 새로 시작하시려면 1, 종료하시려면 2를 입력하세요.");
+        String confirm = scanner.nextLine();
+        if (confirm.equals("1")) {
+            play(scanner);
+        } else if (confirm.equals("2")) {
+            return;
+        } else {
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
         }
     }
 }
