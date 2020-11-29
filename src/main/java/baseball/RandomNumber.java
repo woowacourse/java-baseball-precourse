ca23f98911f51b9d047914aa669309c6bef450e9
eoc940 <hgk0000@naver.com>
2020-11-29 14:42:06 +0900

    feat: add makeThreeRandom method in RandomNumber

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -3,6 +3,16 @@ package baseball;
 import utils.RandomUtils;
 
 public class RandomNumber {
+    //랜덤 숫자를 3개 뽑을 것이기 때문에 상수로 3 설정
+    static final int RAND_NUM_LENGTH = 3;
 
+    //세 개의 한자리 랜덤 숫자를 배열로 반환. 중복검사는 하지 않음
+    private int[] makeThreeRandNum() {
+        int[] threeNum = new int[RAND_NUM_LENGTH];
+        for (int i = 0; i < threeNum.length; i++) {
+            threeNum[i] = RandomUtils.nextInt(1, 9);
+        }
+        return threeNum;
+    }
 
 }
