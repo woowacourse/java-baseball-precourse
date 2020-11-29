e29ad89f750586749d3efd30f77f0190c7866646
483759 <483759@naver.com>
2020-11-29 11:05:26 +0900

    중복된 숫자를 검사하는 isUsedNumber 메서드 구현

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -5,12 +5,23 @@ import java.util.ArrayList;
 
 public class Number {
     private int[] numArray;
+    private boolean[] isUsed;
 
     public Number(int size){
         numArray= new int[size];
+        isUsed=new boolean[10];
+        for(int i=0;i<10;i++)
+            isUsed[i]=false;
     }
 
-    public void setArrayNumberAt(int index, int number){this.numArray[index]=number;}
+    public void setArrayNumberAt(int index, int number){
+        this.numArray[index]=number;
+        isUsed[number]=true;
+    }
 
     public int getArrayNumberAt(int index){return this.numArray[index];}
+
+    public boolean isUsedNumber(int number){
+        return isUsed[number];
+    }
 }
