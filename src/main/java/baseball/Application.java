7561732f9386471a10ce3b04d9df16cddde13eaf
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-11-30 00:50:01 +0900

    feat(Application) : 메인 함수 및 게임플레이 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,37 @@
 package baseball;
 
+import utils.PlayBall;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+        while(true) {
+
+            PlayBall playBall = new PlayBall();
+            playBall.play(scanner);
+
+            if(checkReplay(playBall, scanner)) {
+                break;
+            }
+            /* 게임을 새로 시작하기 위해 1 입력 후 enter 입력 시 IllegalArgumentException 오류가 throw 됨.
+             * 따라서 이를 막기 위해 nextLine()으로 다음 줄을 무시.
+             */
+            //scanner.nextLine();
+        }
+    }
+
+    public static boolean checkReplay(PlayBall playBall, Scanner scanner) {
+        while(true) {
+            try {
+                boolean check_replay = playBall.isReplay(scanner);
+                return check_replay;
+            } catch(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+        }
     }
 }
+
