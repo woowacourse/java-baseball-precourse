638ab4a596da6fd17963fd30420e34a154ebae12
minsugn Son <sns2831@gmail.com>
2020-11-29 16:36:38 +0900

    feat(baseball): BaseballNumbersValidator 클래스 추가
    BaseballNumbers 클래스의 예외사항을 확인할 수 있는 클래스 추가.

---
+++ src/main/java/baseball/BaseballNumbersValidator.java
@@ -0,0 +1,32 @@
+package baseball;
+
+public class BaseballNumbersValidator {
+    private final static int BASEBALL_NUMBER_LENGTH = 3;
+    private final static int BASEBALL_NUMBER_MINIMUM = 1;
+    private final static int BASEBALL_NUMBER_MAXIMUM = 9;
+
+    public static boolean isValid(BaseballNumbers baseballNumbers) {
+        return isRightLength(baseballNumbers)
+                && isDistinct(baseballNumbers)
+                && isRightRangeAndIsNumeric(baseballNumbers);
+    }
+
+    private static boolean isRightLength(BaseballNumbers baseballNumbers) {
+        return baseballNumbers.getBaseballNumbers().size() == BASEBALL_NUMBER_LENGTH;
+    }
+
+    private static boolean isDistinct(BaseballNumbers baseballNumbers) {
+        return BASEBALL_NUMBER_LENGTH == baseballNumbers.getBaseballNumbers()
+                .stream()
+                .distinct()
+                .count();
+    }
+
+    private static boolean isRightRangeAndIsNumeric(BaseballNumbers baseballNumbers) {
+        return BASEBALL_NUMBER_LENGTH == baseballNumbers.getBaseballNumbers()
+                .stream()
+                .filter(baseballNumber -> BASEBALL_NUMBER_MINIMUM <= baseballNumber
+                        && baseballNumber <= BASEBALL_NUMBER_MAXIMUM)
+                .count();
+    }
+}
