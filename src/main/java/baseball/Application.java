eb5822e4ae46f52d1a575d1fb16c962884e29cb5
Jaeyoung Shim <sjy4566@naver.com>
2020-11-29 15:55:50 +0900

    feat(application): input player number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,12 +8,20 @@ public class Application {
     private static final int MAX_NUMBER_RANGE = 9;
 
     private int[] computerNums = new int[3];
+    private int[] playerNums = new int[3];
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         Application application = new Application();
         application.initComputerNums();
+        application.inputPlayerNums(scanner);
+    }
+
+    private void inputPlayerNums(Scanner scanner) {
+        for (int i = 0; i < playerNums.length; i++) {
+            playerNums[i] = scanner.nextInt();
+        }
     }
 
     private void initComputerNums() {
