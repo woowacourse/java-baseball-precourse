38c609e737e0460e90330b64c5efdfbcc292e518
이동건 <tnpdlzi@naver.com>
2020-11-29 13:15:13 +0900

    feat(BaseBallGameTest): 베이스볼 게임 테스트 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -36,11 +36,9 @@ public class BaseBallGame {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         code = BaseBallUtils.isCodeRight(scanner);
-
         if (code == CONTINUE_CODE) {
             return true;
         }
-
         return false;
     }
 
---
+++ src/test/java/BaseBallGameTest.java
@@ -0,0 +1,54 @@
+/*
+ * Class:   BaseBallUtils.java
+ *
+ * Version: 1.0.0
+ *
+ * Date:    2020-11-29
+ *
+ * Author:  Dong Gun Lee
+ *
+ */
+
+import baseball.AnswerBalls;
+import baseball.BaseBallGame;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BaseBallGameTest {
+    private static AnswerBalls answerBalls;
+
+    @BeforeAll
+    public static void setUp() {
+        answerBalls = new AnswerBalls();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+    }
+
+    @Test
+    public void playGameTest() {
+        Scanner scanner;
+        BaseBallGame baseBallGame;
+        ArrayList<Integer> ballsNum = answerBalls.getAnswerBalls();
+
+        String ballStr = "";
+
+        for (int num : ballsNum) {
+            ballStr += Integer.toString(num);
+        }
+
+        scanner = new Scanner(ballStr);
+        baseBallGame = new BaseBallGame(scanner, answerBalls);
+
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> {
+                    baseBallGame.play();})
+                .withMessage("한 자리의 숫자만 입력해주세요.");
+    }
+}
