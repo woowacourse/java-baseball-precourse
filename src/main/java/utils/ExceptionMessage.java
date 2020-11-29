e947828a6676403e237f815144d801ea1400aa56
meeansub <welcltm@gmail.com>
2020-11-29 23:05:06 +0900

    refactor: 입력값에 대한 유효성검사 수정
    메뉴 선택이 유효한지를 검사하는 부분 수정(isValidMenu)
    에러메시지 내용 수정

--- src/main/java/utils/ExceptionMessage.java
+++ src/main/java/utils/ExceptionMessage.java
@@ -1,9 +1,9 @@
 package utils;
 
 public class ExceptionMessage {
-    public static final String UNDER_INPUT_SIZE = "입력한 숫자의 수가 3개 보다 적습니다.";
-    public static final String OVER_INPUT_SIZE = "입력한 숫자의 수가 3개 보다 많습니다.";
-    public static final String NOT_DIGIT = "입력한 값 중에 1 ~ 9사이가 아닌 문자 있습니다.";
+    public static final String UNDER_INPUT_SIZE = "입력한 숫자의 수가 3보다 적습니다.";
+    public static final String OVER_INPUT_SIZE = "입력한 숫자의 수가 3보다 많습니다.";
+    public static final String NOT_INTEGER = "입력한 값 중에 1 ~ 9사이가 아닌 문자 있습니다.";
     public static final String NOT_DIFFERENT_DIGIT = "입력한 값에 중복된 값이 있습니다";
-    public static final String NOT_VALID_MENU = "입력한 값은 유효한 메뉴가 아닙니다.";
+    public static final String NOT_VALID_MENU = "유효하지 않은 입력입니다.";
 }
--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -1,48 +1,45 @@
 package utils;
 
 public class Validator {
-    private static final int GAME_START = 1;
-    private static final int SHUTDOWN = 2;
     private static final int NUMBER_SIZE = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
 
-    public boolean isValidInputNumber(String playerInput) {
-        if (isValidInputSize(playerInput) && isNumberInteger(playerInput) && isDifferentNumbers(playerInput))
-            return true;
-        return false;
+    public void isValidInputNumber(String playerInput) {
+         isValidInputSize(playerInput);
+         isNumberInteger(playerInput);
+         isDifferentNumbers(playerInput);
     }
 
-    public boolean isValidMenu(String playerInput) {
-        int gameStatus = Integer.parseInt(playerInput);
-        if (gameStatus < GAME_START || gameStatus > SHUTDOWN)
+    public void isValidMenu(String playerInput) {
+        if (!playerInput.matches("[1-2]")){
             throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_MENU);
-        return true;
+        }
     }
 
-    private boolean isValidInputSize(String playerInput) {
-        if (playerInput.length() > NUMBER_SIZE)
+    private void isValidInputSize(String playerInput) {
+        if (playerInput.length() > NUMBER_SIZE) {
             throw new IllegalArgumentException(ExceptionMessage.OVER_INPUT_SIZE);
-        if (playerInput.length() < NUMBER_SIZE)
+        }
+        if (playerInput.length() < NUMBER_SIZE) {
             throw new IllegalArgumentException(ExceptionMessage.UNDER_INPUT_SIZE);
-        return true;
+        }
     }
 
-    private boolean isNumberInteger(String playerInput) {
+    private void isNumberInteger(String playerInput) {
         char[] inputArr = playerInput.toCharArray();
         for (char input : inputArr) {
             int compareValue = input - '0';
-            if (compareValue < MIN_NUMBER || compareValue > MAX_NUMBER)
-                throw new IllegalArgumentException(ExceptionMessage.NOT_DIGIT);
+            if (compareValue < MIN_NUMBER || compareValue > MAX_NUMBER) {
+                throw new IllegalArgumentException(ExceptionMessage.NOT_INTEGER);
+            }
         }
-        return true;
     }
 
-    private boolean isDifferentNumbers(String playerInput) {
+    private void isDifferentNumbers(String playerInput) {
         char[] inputArr = playerInput.toCharArray();
         if (inputArr[0] == inputArr[1] || inputArr[0] == inputArr[2] || inputArr[1] == inputArr[2]) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_DIFFERENT_DIGIT);
         }
-        return true;
     }
 }
