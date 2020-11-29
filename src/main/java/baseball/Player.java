f4c53e74f9a047dc75665f6c4d148f8e70f7948e
baekchan1024 <dlqhs1024@naver.com>
2020-11-29 23:27:58 +0900

    feat (Computer) : 랜덤 숫자를 중복되지 않게 char 배열화

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,13 +7,14 @@ public class Player {
 
 	public void inputNumbers() {
 		System.out.println("1~9까지의 정수를 입력하세요.");
-		String userInputNumbers = scanner.next();
-		userNumbersArray(userInputNumbers);	
+		String userInputNumbers = scanner.next();	
 	}
 	
 	public static char[] userNumbersArray(String userInputNumbers) {
 		char[] userInputNumbersArray = userInputNumbers.toCharArray();
 		return userInputNumbersArray;
 	}
+	
+	
 		
 }
