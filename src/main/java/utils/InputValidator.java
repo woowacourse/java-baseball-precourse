42cfc28d23f74cd010482978180f715be6709ef1
soongjamm <kimstz0303@gmail.com>
2020-11-29 21:12:26 +0900

    refactor: add InputValidator class

---
+++ src/main/java/utils/InputValidator.java
@@ -0,0 +1,30 @@
+package utils;
+
+public class InputValidator {
+
+
+    public static void inGameNumber(String inputString, int requiredLength) {
+
+        if (inputString.length() != requiredLength) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < requiredLength; i++) {
+            if (!Character.isDigit(inputString.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        if (inputString.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void gameStartOrStop(String inputString, String RESTART_GAME, String FINISH_GAME) {
+
+        if (!inputString.equals(RESTART_GAME) && !inputString.equals(FINISH_GAME)) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+}
