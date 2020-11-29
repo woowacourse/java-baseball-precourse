271bcc9515b24f532eba65a4e1023f672f1d596d
DH Lim <dlarkfl2@gamil.com>
2020-11-29 21:09:46 +0900

    Feat(Service) : validateDigit() 메소드 추가
    게임종료 여부 입력 데이터를 유효성 검사하는 메소드 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,11 +5,12 @@ import java.util.Set;
 
 public class Player {
 
+
     /*
     player 3자리수 입력 유효성 검사 메소드
     - 숫자, 3자리수, 1~9범위, 중복 체크
     */
-    public void digitsValidate(String playerNumber) {
+    public void validate3Digits(String playerNumber) {
         try{
             Integer.parseInt(playerNumber);
         } catch(NumberFormatException e) {
@@ -27,7 +28,21 @@ public class Player {
         if(set.size()!=3) {
             throw new IllegalArgumentException();
         }
-
     }
 
+    /* player 종료여부 입력 유효성 검사 메소드 */
+    public static void validateDigit(String playerInput) {
+        try{
+            Integer.parseInt(playerInput);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        Set<String> validInputSet = new HashSet<>();
+        validInputSet.add("1");
+        validInputSet.add("2");
+
+        if(!validInputSet.contains(playerInput)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
