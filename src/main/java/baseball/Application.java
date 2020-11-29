f72fbbb8776c4c642f6ef1416c80b3de57364db7
baekchan1024 <dlqhs1024@naver.com>
2020-11-29 21:27:57 +0900

    style : 이클립스 포멧팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,14 +13,13 @@ public class Application {
 		final int END_INCLUSIVE = 9;
 		int strike;
 		int ball;
-		
-		for (int i =0; i < already.length; i ++) {
+
+		for (int i = 0; i < already.length; i++) {
 			already[i] = false;
 		}
-		
-		
+
 		int count = 0;
-		
+
 		while (count < 3) {
 			int randomNumbers = utils.RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE);
 			if (!already[randomNumbers]) {
@@ -29,32 +28,34 @@ public class Application {
 				count++;
 			}
 		}
-		
+
 		boolean finish = false;
-		
+
 		while (!finish) {
 			strike = 0;
 			ball = 0;
 			System.out.println("1~9까지의 정수를 입력하세요.");
 			String UserInputNumbers = scanner.next();
 			char[] userInputNumbersArray = UserInputNumbers.toCharArray();
-			
+
 			for (int i = 0; i < 3; ++i) {
 				for (int j = 0; j < 3; ++j) {
 					if (userInputNumbersArray[i] == randomNumbersArray[j]) {
-						if(i == j) {strike++;}
-						else ball++;
+						if (i == j) {
+							strike++;
+						} else
+							ball++;
 					}
 				}
 			}
-			
+
 			System.out.println(strike + " 스트라이크" + ball + "볼");
 			if (strike == 3) {
 				System.out.println("축하합니다!");
-				finish =true;
+				finish = true;
 			}
 		}
 		scanner.close();
-		
+
 	}
 }
