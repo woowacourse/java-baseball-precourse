5db7236f25abb9fe153df02bcf190d74d026d949
Jaeyoung Shim <sjy4566@naver.com>
2020-11-29 15:42:01 +0900

    feat(application): make random number and set computer number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,24 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
+    private static final int MIN_NUMBER_RANGE = 1;
+    private static final int MAX_NUMBER_RANGE = 9;
+
+    private int[] computerNums = new int[3];
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        Application application = new Application();
+        application.initComputerNums();
+    }
+
+    private void initComputerNums() {
+        for (int i = 0; i < computerNums.length; i++) {
+            computerNums[i] = RandomUtils.nextInt(MIN_NUMBER_RANGE, MAX_NUMBER_RANGE);
+        }
     }
 }
