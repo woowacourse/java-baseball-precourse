fbb0fa61ee46fb22249496cd58b68c1c8f60e4c7
kmj990929 <kmj990929@dgist.ac.kr>
2020-11-29 16:47:21 +0900

    feat: 플레이어의 예측값 입력 기능 추가 구현

--- src/main/java/baseball/UserInputScanner.java
+++ src/main/java/baseball/UserInputScanner.java
@@ -3,10 +3,21 @@ package baseball;
 import java.util.Scanner;
 
 public class UserInputScanner {
+    private static final String GUESS_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
     private UserInputScanner() {
     }
 
     public static String scanUserInput(Scanner scanner) {
         return scanner.nextLine();
+    }
+
+    public static void printGuessInputMessage() {
+        System.out.print(GUESS_INPUT_MESSAGE);
+    }
+
+    public static String scanGuess(Scanner scanner) {
+        printGuessInputMessage();
+        return scanUserInput(scanner);
     }
 }
