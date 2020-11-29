97f2663bbe556d422c1ca778cb51bccc14c9a367
kmj990929 <kmj990929@dgist.ac.kr>
2020-11-29 16:29:31 +0900

    feat: 플레이어의 예측값 입력 기능 구현

---
+++ src/main/java/baseball/UserInputScanner.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class UserInputScanner {
+    private UserInputScanner() {
+    }
+
+    public static String scanUserInput(Scanner scanner) {
+        return scanner.nextLine();
+    }
+}
