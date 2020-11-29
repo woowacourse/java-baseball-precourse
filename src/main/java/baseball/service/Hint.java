62d3d3dc1c5f0afc5d10ad2a588b2efb1e61d18d
sor <bosl95@naver.com>
2020-11-30 06:18:24 +0900

    feat(service) : 게임의 힌트를 제공하는 Hint 클래스 구현

---
+++ src/main/java/baseball/service/Hint.java
@@ -0,0 +1,79 @@
+package baseball.service;
+
+import baseball.domain.Number;
+import baseball.domain.NumberGenerator;
+
+public class Hint {
+    Number correctNumber;
+    NumberGenerator numberGenerator;
+    boolean correct;
+
+    public Hint() {
+        numberGenerator = new NumberGenerator();
+    }
+
+    public String ask(int[] userInput) {
+        String hintAnswer = "";
+
+        hintAnswer = hintAnswer + countBall(userInput);
+        if (!hintAnswer.equals("")) {
+            hintAnswer += " ";
+        }
+        hintAnswer = hintAnswer + countStrike(userInput);
+
+        return isNothing(hintAnswer);
+    }
+
+    private String isNothing(String hintAnswer) {
+        if (hintAnswer.equals("")){
+            return "낫싱";
+        }
+
+        return hintAnswer;
+    }
+
+    private String countStrike(int[] userInput) {
+        int strike = 0;
+
+        for (int i = 0; i < Number.NUM_LENGTH; i++) {
+            if (correctNumber.isEquals(i, userInput[i])) {
+                strike++;
+            }
+        }
+
+        String strikeAnswer = "";
+
+        if (strike > 0) {
+            if (strike == Number.NUM_LENGTH) {
+                correct = true;
+            }
+            strikeAnswer = strike + "스트라이크";
+        }
+
+        return strikeAnswer;
+    }
+
+    private String countBall(int[] userInput) {
+        int ball = 0;
+
+        for (int i=0; i<Number.NUM_LENGTH; i++) {
+            if (correctNumber.containNotEquals(i, userInput[i])) {
+                ball++;
+            }
+        }
+
+        String ballAnswer = "";
+
+        if (ball > 0) {
+            ballAnswer = ball + "볼";
+        }
+
+        return ballAnswer;
+    }
+
+    public void createAnswer() {
+        correctNumber = numberGenerator.execute();
+        System.out.println(correctNumber.toString());
+        correct = false;
+    }
+}
