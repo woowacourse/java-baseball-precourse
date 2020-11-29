9772623c3d63f533e0baf1b1b2015b5826042d02
sor <bosl95@naver.com>
2020-11-30 06:19:04 +0900

    feat(service) : 출력값 기능을 담당하는 Output 구현

---
+++ src/main/java/baseball/service/Output.java
@@ -0,0 +1,21 @@
+package baseball.service;
+
+import baseball.domain.Number;
+
+public class Output {
+    public void correctAnswer() {
+        System.out.println(Number.NUM_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void showResult(String answer) {
+        System.out.println(answer);
+    }
+
+    public void inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void askRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
