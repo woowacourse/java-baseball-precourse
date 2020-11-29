54ef93f37162f96b1cb60b80c93042a75450e00b
sor <bosl95@naver.com>
2020-11-30 06:20:13 +0900

    feat(domain) : 야구 숫자 정보가 담겨있는 Number 구현

---
+++ src/main/java/baseball/domain/Number.java
@@ -0,0 +1,52 @@
+package baseball.domain;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Number {
+    public static int NUM_LENGTH = 3;
+    public static int MIN = 1;
+    public static int MAX = 9;
+    private int[] number;
+
+    public Number(int[] number){
+        this.number = number;
+    }
+
+    public boolean isEquals(String number){
+        return this.number.equals(number);
+    }
+
+    public boolean contains(int number) {
+        return IntStream.of(this.number).anyMatch(i->i==number);
+    }
+
+    public void create(int index, int value) {
+        this.number[index] = value;
+    }
+
+    public boolean isEquals(int index, int number) {
+        if (this.number[index]==number) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean containNotEquals(int index, int number) {
+        for (int i = 0; i < Number.NUM_LENGTH; i++) {
+            if (index != i && this.number[i] == number) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Number{" +
+                "number=" + Arrays.toString(number) +
+                '}';
+    }
+}
