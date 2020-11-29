65b967e658a821679b0ea41690dd6749a88cb4e7
thisisyoungbin <thisisyoungbin@gmail.com>
2020-11-29 17:58:24 +0900

    feat: 인스턴스 생성 및 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,11 +6,7 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        int[] comArr = int[3];
-        int[] userArr = int[3];
-
-
-
-        System.out.println(a);
+        Baseball baseball = new Baseball();
+        baseball.gameRun();
     }
 }
