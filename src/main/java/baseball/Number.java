154990239a569be6b6846e4ae6099243521f3016
483759 <483759@naver.com>
2020-11-29 13:59:55 +0900

    특정 값을 가진 원소가 있는지 찾는 메서드 findNumber() 구현

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -24,4 +24,15 @@ public class Number {
     public boolean isUsedNumber(int number){
         return isUsed[number];
     }
+
+    /* 해당 객체에 number값을 가진 원소의 위치를 반환하는 메서드 */
+    public int findNumber(int number){
+        for(int i=0;i<numArray.length;i++)
+            if(numArray[i]==number){
+                //number값의 원소를 반환
+                return i;
+            }
+        //number와 일치하는 원소가 없다면 -1을 반환
+        return -1;
+    }
 }
