87fc07781430dfab4505691038d97bff0a028dea
meeansub <welcltm@gmail.com>
2020-11-29 14:48:50 +0900

    feat: 예외사항 메시지를 따로 관리하도록 수정
    ExceptionMessage class로 발생하는 예외사항에 대한 메시지를 따로 담아 관리한다

---
+++ src/main/java/baseball/ExceptionMessage.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class ExceptionMessage {
+    public static final String UNDER_INPUT_SIZE = "입력한 숫자의 수가 3개 보다 적습니다.";
+    public static final String OVER_INPUT_SIZE = "입력한 숫자의 수가 3개 보다 많습니다.";
+    public static final String NOT_DIGIT = "입력한 값 중에 1 ~ 9사이가 아닌 문자 있습니다.";
+    public static final String NOT_DIFFERENT_DIGIT = "입력한 값에 중복된 값이 있습니다";
+    public static final String NOT_VALID_MENU = "입력한 값은 유효한 값이 아닙니다.";
+}
--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -11,15 +11,15 @@ public class Validator {
     protected boolean isValidMenu(String menuId) {
         int menu = Integer.parseInt(menuId);
         if (menu > 2 || menu < 0)
-            throw new IllegalArgumentException("입력한 값은 유효한 값이 아닙니다.");
+            throw new IllegalArgumentException(ExceptionMessage.NOT_VALID_MENU);
         return true;
     }
 
     private boolean isValidInputSize(String playerInput) {
         if (playerInput.length() > 3)
-            throw new IllegalArgumentException("입력한 숫자의 수가 3개 보다 많습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.OVER_INPUT_SIZE);
         if (playerInput.length() < 3)
-            throw new IllegalArgumentException("입력한 숫자의 수가 3개 보다 적습니다.");
+            throw new IllegalArgumentException(ExceptionMessage.UNDER_INPUT_SIZE);
         return true;
     }
 
@@ -27,7 +27,7 @@ public class Validator {
         char[] inputArr = playerInput.toCharArray();
         for (char input : inputArr) {
             if (input < '1' || input > '9')
-                throw new IllegalArgumentException("입력한 값 중에 1 ~ 9사이의 문자가 아닌것이 있습니다.");
+                throw new IllegalArgumentException(ExceptionMessage.NOT_DIGIT);
         }
         return true;
     }
@@ -35,7 +35,7 @@ public class Validator {
     private boolean isDifferentNumbers(String playerInput) {
         char[] inputArr = playerInput.toCharArray();
         if (inputArr[0] == inputArr[1] || inputArr[0] == inputArr[2] || inputArr[1] == inputArr[2]) {
-            throw new IllegalArgumentException("입력한 값에 중복된 값이 있습니다");
+            throw new IllegalArgumentException(ExceptionMessage.NOT_DIFFERENT_DIGIT);
         }
         return true;
     }
