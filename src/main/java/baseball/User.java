2935b6234de7bfff6a054f279e92a0e67444ac40
Gomding <backchida@gmail.com>
2020-11-29 16:56:24 +0900

    refactor : separate method User class
    1. checkInputSize(String input) : 유저가 입력한 숫자가 3자리가 아니라면 IllegalArgumentException 발생
    2. checkNumber(String input) : 유저가 입력한 숫자의 각 자리가 1에서 9사이의 숫자가 아니라면 IllegalArgumentException 발생
    3. isDuplicate(char aChar, char nextChar) : 유저가 입력한 숫자 3자리에 중복된 숫자가 있다면
    IllegalArgumentException 발생

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -8,26 +8,44 @@ public class User {
     private static final int SIZE = 3;
 
     public void makeNumber(String input) throws IllegalArgumentException {
+        checkInputSize(input);
+        checkNumber(input);
+        checkDuplicate(input);
 
+        number = input;
+    }
+
+    private void checkInputSize(String input) {
         if (input.length() != SIZE) {
             throw new IllegalArgumentException();
         }
+    }
 
+    private void checkNumber(String input) {
         for (int i = 0; i < SIZE; i++) {
             char aChar = input.charAt(i);
-            if (aChar < START_CAHR_VAL || aChar > END_CHAR_VAL) {
-                throw new IllegalArgumentException();
-            }
+            isRightNumber(aChar);
         }
+    }
 
+    private void isRightNumber(char aChar) {
+        if (aChar < START_CAHR_VAL || aChar > END_CHAR_VAL) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDuplicate(String input) {
         for (int i = 0; i < SIZE-1; i++) {
             char aChar = input.charAt(i);
-            if (aChar == input.charAt(i + 1)) {
-                throw new IllegalArgumentException();
-            }
+            char nextChar = input.charAt(i + 1);
+            isDuplicate(aChar, nextChar);
         }
+    }
 
-        number = input;
+    private void isDuplicate(char aChar, char nextChar) {
+        if (aChar == nextChar) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getNumber() {
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -3,22 +3,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class UserTest {
 
     User user;
 
     @BeforeEach
-    public void user_초기화한다() {
+    void user_초기화한다() {
         user = new User();
     }
 
     @Test
-    public void 임의의수3자리_저장된다() {
+    void 임의의수3자리_저장된다() {
         String number = "123";
         user.makeNumber(number);
 
         assertThat(user.getNumber().length()).isEqualTo(3);
         assertThat(user.getNumber()).isEqualTo("123");
+    }
+
+    @Test
+    public void IllegalArgumentException_예외가발생한다() {
+        String number = "222";
+        assertThatIllegalArgumentException().isThrownBy(() -> user.makeNumber(number));
     }
 }
