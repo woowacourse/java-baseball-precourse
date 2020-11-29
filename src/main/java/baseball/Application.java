8340abe696755a17ed29db7b20837cb681fa05d6
thisisyoungbin <thisisyoungbin@gmail.com>
2020-11-29 15:46:26 +0900

    기능 구현 README 파일 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,11 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        int[] comArr = int[3];
+        int[] userArr = int[3];
+
+
+
+        System.out.println(a);
     }
 }
