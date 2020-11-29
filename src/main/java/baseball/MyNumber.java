160fa3cf129c5733c1b32b0254d6990a8810d7dd
eoc940 <hgk0000@naver.com>
2020-11-29 14:59:34 +0900

    feat: add notInputThreeDigit method in MyNumber

--- src/main/java/baseball/MyNumber.java
+++ src/main/java/baseball/MyNumber.java
@@ -1,4 +1,13 @@
 package baseball;
 
 public class MyNumber {
+
+    //세자리 숫자를 입력받을 것이기 때문에 상수로 3 설정
+    static final int MY_NUM_LENGTH = 3;
+
+    //입력받은 값이 세자리인지 아닌지 판단하는 메서드
+    private boolean notInputThreeDigit(int inputNum) {
+        return (inputNum < 100) || (inputNum > 999);
+    }
+
 }
