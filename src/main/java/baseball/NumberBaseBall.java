60a7977ecab4db8a42394b2ce6ce053c2feaece3
meeansub <welcltm@gmail.com>
2020-11-29 23:03:07 +0900

    feat: 숫자야구게임 '볼' 처리 수정
    문자열의 contains로 검색하는 것으로 변경
    매직 넘버 개선

--- src/main/java/baseball/NumberBaseBall.java
+++ src/main/java/baseball/NumberBaseBall.java
@@ -8,6 +8,7 @@ public class NumberBaseBall {
     private String randomNumber;
     private static int INITIAL_VALUE = 0;
     private static int NUMBER_SIZE = 3;
+    private static int GAME_OVER = 3;
 
     protected String makeHint(String playerNums, String randomNums) {
         char[] player = playerNums.toCharArray();
@@ -16,52 +17,53 @@ public class NumberBaseBall {
         int strike = INITIAL_VALUE;
 
         for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (player[i] == random[i])
+            if (player[i] == random[i]) {
                 strike++;
+            }
         }
-        Arrays.sort(random);
         for (int i = 0; i < NUMBER_SIZE; i++) {
-            if (Arrays.binarySearch(random, player[i]) >= 0)
+            String compareValue = String.valueOf(player[i]);
+            if (randomNums.contains(compareValue)) {
                 ball++;
+            }
         }
-        if (ball > INITIAL_VALUE)
+        if (ball > 0) {
             ball = ball - strike;
+        }
         return makeResult(ball, strike);
     }
 
     private String makeResult(int ball, int strike)
     {
         StringBuilder result = new StringBuilder();
-        if (strike == 3) {
+        if (strike == GAME_OVER) {
             result.append(strike + "스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
-        else if (ball > INITIAL_VALUE && strike > INITIAL_VALUE) {
+        else if (ball > 0 && strike > 0) {
             result.append(ball + "볼 " + strike + "스트라이크");
         }
-        else if (strike > INITIAL_VALUE) {
+        else if (strike > 0) {
             result.append(strike + "스트라이크");
         }
-        else if (ball > INITIAL_VALUE) {
+        else if (ball > 0) {
             result.append(ball + "볼");
         }
-        else
+        else {
             result.append("낫싱");
+        }
         return result.toString();
     }
 
-    protected String makeRandomNumber(int minNumber, int maxNumber) {
-        StringBuilder randomNumber = new StringBuilder();
-        while (randomNumber.length() != NUMBER_SIZE)
+    protected void createRandomNumber(int minNumber, int maxNumber) {
+        StringBuilder makeRandomNum = new StringBuilder();
+        while (makeRandomNum.length() != NUMBER_SIZE)
         {
             String creatNum = String.valueOf(RandomUtils.nextInt(minNumber, maxNumber));
-            if (randomNumber.indexOf(creatNum) < 0)
-                randomNumber.append(creatNum);
+            if (makeRandomNum.indexOf(creatNum) < 0) {
+                makeRandomNum.append(creatNum);
+            }
         }
-        return randomNumber.toString();
-    }
-
-    public void setRandomNumber(String randomNumber) {
-        this.randomNumber = randomNumber;
+        this.randomNumber = makeRandomNum.toString();
     }
 
     public String getRandomNumbers() {
