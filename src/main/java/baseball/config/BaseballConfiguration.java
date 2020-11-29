f58af0f831d27963eea2796cd49ab20fc73ecce5
LeeJuHyuk <dlwngur1535@gmail.com>
2020-11-29 22:03:53 +0900

    fix: END_INCLUSIVE 값 수정

--- src/main/java/baseball/config/BaseballConfiguration.java
+++ src/main/java/baseball/config/BaseballConfiguration.java
@@ -3,5 +3,5 @@ package baseball.config;
 public class BaseballConfiguration {
     public final static int BALL_LENGTH = 3;
     public final static int START_INCLUSIVE = 1;
-    public final static int END_INCLUSIVE = 1;
+    public final static int END_INCLUSIVE = 9;
 }
