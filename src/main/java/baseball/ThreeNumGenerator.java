55e0e076715b2af701c153554cc83d1186858004
thisisyoungbin <thisisyoungbin@gmail.com>
2020-11-29 16:32:07 +0900

    feat: 컴퓨터가 가질 서로 다른 수 3개(정답) 생성

---
+++ src/main/java/baseball/ThreeNumGenerator.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import utils.RandomUtils;
+
+public class ThreeNumGenerator {
+    private static int[] comNum = new int[3];
+
+    public static int[] generate() {
+        /* 길이 3의 배열 생성 후 각 인덱스에 임의의 수 할당 */
+        comNum[0] = RandomUtils.nextInt(1, 9);
+        comNum[1] = RandomUtils.nextInt(1, 9);
+        comNum[2] = RandomUtils.nextInt(1, 9);
+
+        /* 각 인덱스에 겹치는 수가 있다면 새로운 값을 할당해 겹치는 수가 없게 만듦 */
+        while (comNum[0] == comNum[1] || comNum[0] == comNum[2] || comNum[1] == comNum[2]) {
+            comNum[1] = RandomUtils.nextInt(1, 9);
+            comNum[2] = RandomUtils.nextInt(1, 9);
+        }
+
+        return comNum;
+    }
+}
