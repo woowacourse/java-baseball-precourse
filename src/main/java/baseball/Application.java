cf58a7d9502ffc0cb8e4553ac8354534d0902056
zzudy <wngus1100@gmail.com>
2020-11-29 18:11:24 +0900

    refacor ; datalen을 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import utils.RandomUtils;
 import java.util.*;
 
 public class Application {
+    final static int dataLen =3;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
@@ -13,13 +14,15 @@ public class Application {
             final int[] userInput= userInput(scanner);
             //랜덤으로 정답 생성
             final int[] answer = makeRandom();
+
         }
     }
+
     public static int[] makeRandom(){
-        int[] userInput = new int[3];
+        int[] userInput = new int[dataLen];
         Set<Integer> randNum = new LinkedHashSet<Integer>();
         while(true){
-            if(randNum.size() ==3) break;
+            if(randNum.size() == dataLen) break;
             randNum.add(RandomUtils.nextInt(1,9));
         }
         Iterator<Integer> it = randNum.iterator();
@@ -34,7 +37,7 @@ public class Application {
         final String INPUT_MESSAGE = "숫자를 입력해주세요: ";
         System.out.print(INPUT_MESSAGE);
         String userNumberString = scanner.nextLine();
-        int[] userInput = new int[3];
+        int[] userInput = new int[dataLen];
         try{
             //숫자의 개수가 3개가 아닐 때
             isThree(userNumberString);
@@ -50,20 +53,20 @@ public class Application {
 
     public static int[] toIntArray(String inputString) {
         int[] userInput = new int[3];
-        for(int i=0; i<3; i++){
+        for(int i=0; i<dataLen; i++){
             userInput[i] = inputString.charAt(i) -'0';
         }
         return userInput;
     }
 
     public static void isThree(String inputString){
-        if(inputString.length() !=3 ){
+        if(inputString.length() != dataLen ){
             throw new IllegalArgumentException("잘못된 입력입니다");
         }
     }
 
     public static void isNumber(String inputString){
-        for(int i=0; i<3; i++){
+        for(int i=0; i<dataLen; i++){
             if(inputString.charAt(i) <'1' || inputString.charAt(i) >'9')
                 throw new IllegalArgumentException("잘못된 입력입니다");
         }
