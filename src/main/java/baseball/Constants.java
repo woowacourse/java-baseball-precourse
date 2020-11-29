036941c2f8abe6d64fc05f776be4d8ad22f2a8af
JungYoonSung <ybook2012@gmail.com>
2020-11-29 21:50:52 +0900

    fix : typo edit

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -5,10 +5,12 @@ public final class Constants {
     public static final int NUMBER_LIMIT = 9;
     public static final int ANSWER_COUNT = 3;
 
+    public static final String INPUT_REQ_MESSAGE = "숫자를 입력해주세요 : ";
+
     public static final String BALL_MESSAGE = "볼";
-    public static final String STRIKE_MESSAGE = "스크라이크";
+    public static final String STRIKE_MESSAGE = "스트라이크";
     public static final String ALL_MISS_MESSAGE = "낫싱";
-    public static final String GAME_FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    public static final String GAME_FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static final String INPUT_ERROR_MESSAGE = "잘못된 입력입니다. ";
     public static final String RESTART_REQ_MESSAGE = "다시 입력해 주세요";
