0befda09461c3f3f8eafc2739568a87349a9693c
sor <bosl95@naver.com>
2020-11-30 05:26:41 +0900

    feat(utils) : string을 int[]로 변환하는 InputConverter 기능 추가

---
+++ src/main/java/utils/InputConverter.java
@@ -0,0 +1,9 @@
+package utils;
+
+import java.util.Arrays;
+
+public class InputConverter {
+    public static int[] stringToArray(String input){
+        return Arrays.asList(input.split("")).stream().mapToInt(Integer::parseInt).toArray();
+    }
+}
