91fa61e502fc1eeb0722790eb7de48d991c01f15
최문경 <munkyung0324@gmail.com>
2020-11-30 02:39:00 +0900

    refactor(Application): refactor by Java Coding Convention
    Refactor:
    - add comment
    - extract function from main method
    - fix grammatical error
    
    Added exception logic:
    - throw IllegalArgumentException if player input duplicated number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,28 +6,27 @@ import java.util.Scanner;
 
 public class Application {
     static boolean[] checkDistinct;
-    static final int PLAY = 0;
     static final int ING = 1;
-    static final int FINISH = 2;
+    static final int END = 2;
+    static int ongoingInput;
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
-        int ongoingInput = PLAY;
+        // 세 개의 중복되지 않은 수 정하기
         checkDistinct = new boolean[10];
         int[] systemNum = getDistinctNumber();
 
-        while(ongoingInput < FINISH) {
-            // 플레이어가 3자리 수를 입력한다.
+        while(true) {
+            // 플레이어가 입력
             System.out.print("숫자를 입력해주세요 : ");
             char[] charUserNum = scanner.next().toCharArray();
 
-            // 정상적인 입력인지 체크
+            // 정상적인 입력인지 체크 (같은 숫자 중복 입력, 3자리가 넘는 수 입력, 숫자가 아닌것 입력)
             try {
-                validateInput(charUserNum);
+                validateNumberInput(charUserNum);
             } catch (IllegalArgumentException e) {
-                String message = e.getMessage();
-                System.out.println(message);
+                System.out.println(e.getMessage());
                 continue;
             }
 
@@ -38,20 +37,16 @@ public class Application {
             int strikeCnt = checkStrike(userNum, systemNum);
 
             // 정답 체크하기
-            if (strikeCnt == 3) {
-                System.out.println(strikeCnt + "스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            if(checkAnswer(strikeCnt)) {
                 ongoingInput = scanner.nextInt();
             }
 
-            // 게임을 완전히 종료할 지 체크하기
-            if(ongoingInput == FINISH) {
+            // 게임을 완전히 종료할 지 체크
+            if(ongoingInput == END) {
                 break;
             } else if(ongoingInput == ING) {
                 checkDistinct = new boolean[10];
                 systemNum = getDistinctNumber();
-                ongoingInput = PLAY;
                 continue;
             }
 
@@ -59,28 +54,24 @@ public class Application {
             int ballCnt = checkBall(userNum, systemNum);
 
             // 결과 출력하기
-            if (ballCnt == 0 && strikeCnt == 0) {
-                System.out.println("낫싱");
-            } else if (ballCnt == 0) {
-                System.out.println(strikeCnt + "스트라이크");
-            } else if (strikeCnt == 0) {
-                System.out.println(ballCnt + "볼");
-            } else {
-                System.out.println(ballCnt + "볼" + " " + strikeCnt + "스트라이크");
-            }
+            printResult(ballCnt, strikeCnt);
         }
-
     }
 
-    private static void validateInput(char[] charUserNum){
+    private static void validateNumberInput(char[] charUserNum) {
         if(charUserNum.length > 3) {
-            throw new IllegalArgumentException("3자리수를 입력해야합니다.");
+            throw new IllegalArgumentException("잘못 입력하셨습니다. 3자리수를 입력해야합니다. 다시 입력해주세요.");
         }
 
+        boolean[] checkDuplication = new boolean[10];
         for (int i = 0; i < 3; i++) {
             int tmp = charUserNum[i] - '0';
             if(tmp > 9 || tmp < 1) {
-                throw new IllegalArgumentException("숫자가 아닙니다.");
+                throw new IllegalArgumentException("잘못 입력하셨습니다. 숫자가 아닙니다. 다시 입력해주세요.");
+            } else if(!checkDuplication[tmp]) {
+                checkDuplication[tmp] = true;
+            } else {
+                throw new IllegalArgumentException("잘못 입력하셨습니다. 각 자리수 숫자는 달라야 합니다. 다시 입력해주세요.");
             }
         }
     }
@@ -99,7 +90,7 @@ public class Application {
         int numCnt = 0;
         while (numCnt < 3) {
             int tmp = RandomUtils.nextInt(1, 9);
-            if (!isDistinct(tmp)) {
+            if (isDistinct(tmp)) {
                 systemNum[numCnt] = tmp;
                 numCnt++;
             }
@@ -108,10 +99,10 @@ public class Application {
     }
 
     private static boolean isDistinct(int checkNum) {
-        if(checkDistinct[checkNum]) {
+        if(!checkDistinct[checkNum]) {
+            checkDistinct[checkNum] = true;
             return true;
         } else {
-            checkDistinct[checkNum] = true;
             return false;
         }
     }
@@ -126,6 +117,17 @@ public class Application {
         return strikeCnt;
     }
 
+    private static boolean checkAnswer(int strikeCnt) {
+        if (strikeCnt == 3) {
+            System.out.println(strikeCnt + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     private static int checkBall(int[] userNum, int[] systemNum) {
         int ballCnt = 0;
         for (int i = 0; i < 3; i++) {
@@ -135,6 +137,18 @@ public class Application {
             }
         }
         return ballCnt;
+    }
+
+    private static void printResult(int ballCnt, int strikeCnt) {
+        if (ballCnt == 0 && strikeCnt == 0) {
+            System.out.println("낫싱");
+        } else if (ballCnt == 0) {
+            System.out.println(strikeCnt + "스트라이크");
+        } else if (strikeCnt == 0) {
+            System.out.println(ballCnt + "볼");
+        } else {
+            System.out.println(ballCnt + "볼" + " " + strikeCnt + "스트라이크");
+        }
     }
 
 }
