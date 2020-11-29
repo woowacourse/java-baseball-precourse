d1772701a93668b023fb53bed6b84e6bfd1d2268
zzudy <wngus1100@gmail.com>
2020-11-29 19:50:42 +0900

    feat: 스트라이크 개수를 구하는 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,12 @@
-package baseball;public class Computer {
+package baseball;
+
+public class Computer {
+    final static int dataLen =3;
+    public static int getStrikeNum(int input[], int answer[]) {
+        int strike =0;
+        for(int i=0; i<dataLen; i++){
+            if(input[i] == answer[i]) strike++;
+        }
+        return strike;
+    }
 }
