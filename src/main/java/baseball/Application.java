f3a041e13d02db8da5399577d280da7f095711d1
zzudy <wngus1100@gmail.com>
2020-11-29 16:23:22 +0900

    feat : 사용자 입력받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,54 @@
 package baseball;
 
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        boolean flag =true;
+        while(flag){
+            //사용자 입력 받기
+            final int[] userInput= userInput(scanner);
+        }
+    }
+    public static int[] userInput(Scanner scanner){
+        final String INPUT_MESSAGE = "숫자를 입력해주세요: ";
+        System.out.print(INPUT_MESSAGE);
+        String userNumberString = scanner.nextLine();
+        int[] userInput = new int[3];
+        try{
+            //숫자의 개수가 3개가 아닐 때
+            isThree(userNumberString);
+            //문자를 입력 받았을 때 && 0도 처리
+            isNumber(userNumberString);
+            userInput = toIntArray(userNumberString);
+            return userInput;
+        }catch(IllegalArgumentException e){
+            System.out.println("잘못된 입력입니다");
+            return userInput;
+        }
+    }
+
+    public static int[] toIntArray(String inputString) {
+        int[] userInput = new int[3];
+        for(int i=0; i<3; i++){
+            userInput[i] = inputString.charAt(i) -'0';
+        }
+        return userInput;
+    }
+
+    public static void isThree(String inputString){
+        if(inputString.length() !=3 ){
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+    }
+
+    public static void isNumber(String inputString){
+        for(int i=0; i<3; i++){
+            if(inputString.charAt(i) <'1' || inputString.charAt(i) >'9')
+                throw new IllegalArgumentException("잘못된 입력입니다");
+        }
     }
 }
