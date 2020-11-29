4a175c7b7858dbc61e0ec7cf75cb89d57d16116f
sor <bosl95@naver.com>
2020-11-30 06:16:56 +0900

    refactor(validator) : 예외시 IllegalArgumentException() 발생하도록 수정

--- src/main/java/utils/validator/DuplicateValidator.java
+++ src/main/java/utils/validator/DuplicateValidator.java
@@ -2,17 +2,15 @@ package utils.validator;
 
 public class DuplicateValidator implements Validator {
     @Override
-    public boolean execute(String userInput) {
+    public void execute(String userInput) {
         int[] countNumber = new int[10];
 
         for (String input : userInput.split("")){
             int intInput = Integer.parseInt(input);
 
             if (++countNumber[intInput] > 1) {
-                return false;
+                throw new IllegalArgumentException();
             }
         }
-
-        return true;
     }
 }
