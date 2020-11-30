e760671c60f624b0a20b33c8e14ea661fe261bca
DH Lim <dlarkfl2@gamil.com>
2020-12-01 00:56:29 +0900

    Refactor(Service) : 접근제어자 수정, 상수 추가

--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -1,59 +1,67 @@
 package baseball;
 
 
-
 public class Service {
+    private int ballCount;
 
-    public int ballCount;
-    public int strikeCount;
+    private int strikeCount;
+
+    private static final int BASEBALL_NUMBER_LENGTH = 3;
 
     /* 볼, 스트라이크 카운트 메소드 */
     public void counter(String opponentNumber, String playerNum) {
         int totalCount = 0;
-        for(int i=0; i<3; i++) {
+        for (int i = 0; i < BASEBALL_NUMBER_LENGTH; i++) {
             String opponentDigit = opponentNumber.split("")[i];
             String playerDigit = playerNum.split("")[i];
-
-            if(opponentNumber.contains(playerDigit) ) {
+            if (opponentNumber.contains(playerDigit)) {
                 totalCount++;
             }
-            if(opponentDigit.equals(playerDigit)) {
+            if (opponentDigit.equals(playerDigit)) {
                 strikeCount++;
             }
         }
-
         ballCount = totalCount - strikeCount;
     }
 
     /* 힌트 출력 메소드 */
-    public void printHint(int ballCount, int strikeCount) {
+    public void printHint() {
         if (ballCount != 0 && strikeCount == 0) {
             System.out.println(ballCount + "볼");
             return;
         }
-
         if (ballCount == 0 && strikeCount != 0) {
             System.out.println(strikeCount + "스트라이크");
             return;
         }
-
         if (ballCount != 0 && strikeCount != 0) {
             System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
             return;
         }
-
         if (ballCount == 0 && strikeCount == 0) {
             System.out.println("낫싱");
         }
     }
 
     /* 승리 확인 메소드 */
-    public boolean isWin(int ballCount, int strikeCount) {
-        if(strikeCount==3 && ballCount==0) {
+    public boolean isWin() {
+        if (strikeCount == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
         return false;
     }
 
+    /* 게임 초기화 여부를 결정하는 메소드 */
+    public static boolean isRestartBaseball(String playerInput) {
+        if (playerInput.equals("1")) {
+            return true;
+        }
+        if (playerInput.equals("2")) {
+            return false;
+        }
+        return true;
+    }
 }
+
+
