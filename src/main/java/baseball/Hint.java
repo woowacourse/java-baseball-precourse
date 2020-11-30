29dc4ac445c4fe468fcf00cab2f16810d68089f3
KJunseo <max9106@naver.com>
2020-12-01 02:28:53 +0900

    feat: Hint 클래스 생성

---
+++ src/main/java/baseball/Hint.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Hint {
+    int strike;
+    int ball;
+
+    public Hint(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
