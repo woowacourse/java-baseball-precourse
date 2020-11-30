c0de2e8331242baa890feb068719261e12ccf08d
JUNGHOJONG <hojong1351@naver.com>
2020-11-30 15:53:21 +0900

    refactor(baseball): GamePlayer클래스 변수명 변경

--- src/main/java/baseball/GamePlayer.java
+++ src/main/java/baseball/GamePlayer.java
@@ -7,10 +7,10 @@ import java.util.Set;
 public class GamePlayer {
     private static final char FIRST_BOUNDARY_VALUE = '1';
     private static final char LAST_BOUNDARY_VALUE = '9';
-    private static final int INPUT_NUMBER_COUNT = 3;
-    private static final String REINPUT = "다시 입력해주세요";
-    private static final int RESTART = 1;
-    private static final int END = 2;
+    private static final int COUNT_LIMIT = 3;
+    private static final int GAME_RESTART = 1;
+    private static final int GAME_END = 2;
+    private static final String RE_ENTER = "다시 입력해주세요";
 
     private Scanner scanner;
 
@@ -19,57 +19,60 @@ public class GamePlayer {
     }
 
     public String generateNumbersOfPlayerEntered()
-                                            throws IllegalArgumentException{
+                                            throws IllegalArgumentException {
         String generatedNumbers = scanner.next();
-        if(generatedNumbers.length() != INPUT_NUMBER_COUNT){
-            throw new IllegalArgumentException(REINPUT);
+
+        if(generatedNumbers.length() != COUNT_LIMIT){
+            throw new IllegalArgumentException(RE_ENTER);
         }
 
         if(isNotAllNumbers(generatedNumbers)){
-            throw new IllegalArgumentException(REINPUT);
+            throw new IllegalArgumentException(RE_ENTER);
         }
 
         if(haveDuplicateNumbers(generatedNumbers)){
-            throw new IllegalArgumentException(REINPUT);
+            throw new IllegalArgumentException(RE_ENTER);
         }
 
         return generatedNumbers;
     }
 
-    private boolean isNotAllNumbers(String generatedNumbers){
+    private boolean isNotAllNumbers(String generatedNumbers) {
         for(int i=0; i<generatedNumbers.length(); i++){
             if(generatedNumbers.charAt(i) < FIRST_BOUNDARY_VALUE
-                    || generatedNumbers.charAt(i) > LAST_BOUNDARY_VALUE){
-               return true;
+                    || generatedNumbers.charAt(i) > LAST_BOUNDARY_VALUE) {
+                return true;
             }
         }
         return false;
     }
 
-    private boolean haveDuplicateNumbers(String generatedNumbers){
+    private boolean haveDuplicateNumbers(String generatedNumbers) {
         Set<Character> numbers = new HashSet<>();
-        for(int i=0; i<generatedNumbers.length(); i++){
+
+        for (int i=0; i<generatedNumbers.length(); i++) {
             numbers.add(generatedNumbers.charAt(i));
         }
-        return numbers.size() != INPUT_NUMBER_COUNT;
+
+        return numbers.size() != COUNT_LIMIT;
     }
 
     public int selectRestartOrEnd() {
-        String inputNumber = scanner.next();
+        String selectedMode = scanner.next();
 
-        while (isInvalidInputNumber(inputNumber)) {
-            System.out.println(REINPUT);
-            inputNumber = scanner.next();
+        while (isInvalidMode(selectedMode)) {
+            System.out.println(RE_ENTER);
+            selectedMode = scanner.next();
         }
 
-        return inputNumber.charAt(0) - '0';
+        return selectedMode.charAt(0) - '0';
     }
 
-    private boolean isInvalidInputNumber(String inputNumber){
-        int singleDigitInputNumber = inputNumber.charAt(0) - '0';
+    private boolean isInvalidMode(String selectedMode) {
+        int firstDigitOfSelectedMode = selectedMode.charAt(0) - '0';
 
-        return inputNumber.length() != 1
-                || (singleDigitInputNumber != RESTART && singleDigitInputNumber != END);
+        return selectedMode.length() != 1
+                || (firstDigitOfSelectedMode != GAME_RESTART && firstDigitOfSelectedMode != GAME_END);
     }
 
 }
