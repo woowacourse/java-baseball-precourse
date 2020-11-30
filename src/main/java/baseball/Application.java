a1e8736c5da1d4cebfe3d3f677acdbee2780b30a
yejin <jinee0717@daum.net>
2020-11-30 21:33:06 +0900

    feat : Application 클래스 작업
    사용자 입력을 받는 틀 구성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+    	final Scanner scanner = new Scanner(System.in);
+        int flag;
+        
+        while(true) {
+        	System.out.println("숫자야구 게임을 시작하려면 1, 종료하려면 2를 입력하세요!");
+        	flag = scanner.nextInt();
+        	
+        	if(flag==2) {
+        		System.out.println("안녕히가세요!");
+        		scanner.close();
+        		break;
+        	}else{
+
+        	}
+        }
     }
 }
