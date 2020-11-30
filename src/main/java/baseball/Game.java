18f591b032610390b395f26a9ffd3f502e9bfb66
kmj990929 <kmj990929@dgist.ac.kr>
2020-11-30 22:10:38 +0900

    feat: 3 strike 확인 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,7 +3,7 @@ package baseball;
 import java.util.Scanner;
 
 public class Game {
-    private int answer;
+    private static int answer;
 
     public Game() {
         this.answer = AnswerGenerator.generateAnswer();
@@ -14,6 +14,21 @@ public class Game {
     }
 
     public static void playRound(Scanner scanner) {
-        int guess = UserInputScanner.getGuess(scanner);
+        boolean ongoing = true;
+        do {
+            int guess = UserInputScanner.getGuess(scanner);
+            boolean correct = roundResult(answer, guess);
+        } while (ongoing);
+    }
+
+    public static boolean roundResult(int answer, int guess) {
+        int[] hint = HintGenerator.getHint(answer, guess);
+        int ball = hint[0];
+        int strike = hint[1];
+        HintPrinter.printHint(ball, strike);
+        if (strike == 3) {
+            return true;
+        }
+        return false;
     }
 }
