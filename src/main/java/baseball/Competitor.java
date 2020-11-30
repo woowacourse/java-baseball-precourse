5b50fe8eb100c22c0a37cb62acfcc11761889e67
JUNGHOJONG <hojong1351@naver.com>
2020-11-30 15:29:43 +0900

    refactor(baseball): Competitor클래스 변수명 변경

--- src/main/java/baseball/Competitor.java
+++ src/main/java/baseball/Competitor.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Competitor {
     private static final int FIRST_BOUNDARY_VALUE = 1;
     private static final int LAST_BOUNDARY_VALUE = 9;
-    private static final int RANDOM_NUMBER_COUNT = 3;
+    private static final int COUNT_LIMIT = 3;
 
     private final String generatedRandomNumbers;
 
@@ -22,13 +22,13 @@ public class Competitor {
 
     private String generateRandomNumbers(){
         Set<Integer> randomNumbers = new LinkedHashSet<>();
-        while(randomNumbers.size() != RANDOM_NUMBER_COUNT){
+        while(randomNumbers.size() < COUNT_LIMIT){
             randomNumbers.add(RandomUtils.nextInt(FIRST_BOUNDARY_VALUE, LAST_BOUNDARY_VALUE));
         }
-        return getRandomNumbersConvertedToString(randomNumbers);
+        return convertRandomNumbersIntoString(randomNumbers);
     }
 
-    private static String getRandomNumbersConvertedToString(Set<Integer> randomNumbers){
+    private static String convertRandomNumbersIntoString(Set<Integer> randomNumbers){
         StringBuilder sb = new StringBuilder();
         for(Integer randomNumber : randomNumbers){
             sb.append(randomNumber);
