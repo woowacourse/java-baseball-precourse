fba3509baeb79d2c28680d7fdc3c0e7251cd086b
Minsung <sns2831@gmail.com>
2020-12-01 07:46:02 +0900

    refactor(baseball): RestartOrExitValidator 클래스 static 으로 변경
    메소드를 static으로 변경 및 생성자, 맴버 변수의 접근제어자 private로 변경

--- src/main/java/baseball/RestartOrExitValidator.java
+++ src/main/java/baseball/RestartOrExitValidator.java
@@ -2,15 +2,17 @@ package baseball;
 
 public class RestartOrExitValidator {
 
-    final static int RIGHT_LENGTH = 1;
-    final static int RESTART = 1;
-    final static int EXIT = 2;
+    final private static int RIGHT_LENGTH = 1;
+    final private static int RESTART = 1;
+    final private static int EXIT = 2;
 
-    public boolean isValid(String input) {
+    private RestartOrExitValidator(){}
+
+    public static boolean isValid(String input) {
         return isNumeric(input) && isRightLength(input) && isRightInput(input);
     }
 
-    private boolean isNumeric(String input) {
+    private static boolean isNumeric(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -19,11 +21,11 @@ public class RestartOrExitValidator {
         return true;
     }
 
-    private boolean isRightLength(String input) {
+    private static boolean isRightLength(String input) {
         return input.length() == RIGHT_LENGTH;
     }
 
-    private boolean isRightInput(String input) {
+    private static boolean isRightInput(String input) {
         int inputIntValue = Integer.parseInt(input);
 
         return inputIntValue == RESTART || inputIntValue == EXIT;
