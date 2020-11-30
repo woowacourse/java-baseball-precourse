9d6466957ea5f0310de7797efec4f142c6fe020f
yejin <jinee0717@daum.net>
2020-12-01 00:28:20 +0900

    fix : 출력문 삭제, 주석 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,5 @@
 package baseball;
 
-import java.util.Arrays;
-
 public class Game {
 
 	Player player = new Player();
@@ -10,13 +8,17 @@ public class Game {
 
 	// 실제 게임을 실행하는 함수
 	public void startGame() {
+		// 타깃 넘버 생성 
 		answer.makeAnswer();
-    	System.out.println("-> target "+ Arrays.toString(answer.answerNum));
+
+		// 게임 한 세트 실행
     	while(!hint.flag) {
     		player.getPlayerNum();
     		hint.makeHint(answer.answerNum, player.playerNum);
     		hint.clearStrikeAndBall();
     	}
+    	
+    	// 다음 게임을 위한 초기화 실행
     	hint.flag = false;
     	hint.clearStrikeAndBall();
 	}
