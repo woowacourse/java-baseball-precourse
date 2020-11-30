eb32ad6c70129274ff5c44b1c3278d7deae3ee6c
sounmind <sounmind777@gmail.com>
2020-12-01 06:04:21 +0900

    feat(PlayerHuman): 플레이어 입력부와 비정상입력 예외 처리 구현

--- src/main/java/baseball/PlayerHuman.java
+++ src/main/java/baseball/PlayerHuman.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class PlayerHuman {
     private static final String MESSAGE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
     private static final String MESSAGE_ASK_GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String MESSAGE_INPUT_ERROR = "1과 2 중에 하나를 입력해주세요!";
+    private static final String MESSAGE_INPUT_ERROR_RESTART = "1과 2 중에 하나를 입력해주세요!";
+    private static final String MESSAGE_INPUT_ERROR_DUPLICATION = "중복된 숫자가 입력되었습니다!";
+    private static final String MESSAGE_INPUT_ERROR_NUMBER = "1에서 9까지 숫자를 입력해주세요!";
+    private static final String MESSAGE_INPUT_ERROR_NUMBER_LENGTH = "알맞은 길이의 숫자를 입력해주세요!";
     private static final int GAME_RESTART = 1;
+    private static final String DELIMITER = "";
     private static int[] records = new int[Judge.RECORD_LENGTH];
 
     static {
@@ -15,8 +19,50 @@ public class PlayerHuman {
     }
 
     public int[] getRecords(Scanner scanner) {
-        int[] records = new int[Judge.RECORD_LENGTH];
+        System.out.println(MESSAGE_INPUT_NUMBER);
+        String[] stringArrayRecords = scanner.next().split(DELIMITER);
+        return convertStringToInteger(stringArrayRecords);
+    }
+
+    private int[] convertStringToInteger(String[] stringArrayRecords) {
+        isValidInput(stringArrayRecords);
+        for(int i = 0; i < Judge.RECORD_LENGTH; i++) {
+            int numberPlayerInput = Integer.parseInt(stringArrayRecords[i]);
+            if (isSameDigitInRecords(records, numberPlayerInput)) {
+                throw new IllegalArgumentException(MESSAGE_INPUT_ERROR_DUPLICATION);
+            }
+            records[i] = numberPlayerInput;
+        }
         return records;
+    }
+
+    private void isValidInput(String[] stringArrayRecords) {
+        AreNumbers(stringArrayRecords);
+        isRightLength(stringArrayRecords, Judge.RECORD_LENGTH);
+    }
+
+    private void AreNumbers(String[] stringArrayRecords) {
+        for(int i = 0; i<stringArrayRecords.length; i++) {
+            try {
+                Integer.parseInt(stringArrayRecords[i]);
+            } catch (Exception e) {
+                throw new IllegalArgumentException(MESSAGE_INPUT_ERROR_NUMBER);
+            }
+        }
+    }
+
+    private void isRightLength(String[] stringArrayRecords, int recordLength) {
+        if(stringArrayRecords.length != recordLength) {
+            throw new IllegalArgumentException(MESSAGE_INPUT_ERROR_NUMBER_LENGTH);
+        }
+    }
+
+    private boolean isSameDigitInRecords(int[] records, int newDigit) {
+        for(int i = 0; i < records.length; i++) {
+            if(newDigit == records[i])
+                return true;
+        }
+        return false;
     }
 
     public boolean wantToRestart(Scanner scanner) {
@@ -25,10 +71,10 @@ public class PlayerHuman {
         try {
             answerPlayerInput = scanner.nextInt();
         } catch(Exception e) {
-            throw new IllegalArgumentException(MESSAGE_INPUT_ERROR);
+            throw new IllegalArgumentException(MESSAGE_INPUT_ERROR_RESTART);
         }
         if(answerPlayerInput != 1 && answerPlayerInput != 2)
-            throw new IllegalArgumentException(MESSAGE_INPUT_ERROR);
+            throw new IllegalArgumentException(MESSAGE_INPUT_ERROR_RESTART);
         if(answerPlayerInput == GAME_RESTART)
             return true;
         return false;
