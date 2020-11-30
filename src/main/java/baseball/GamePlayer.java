6777d2fdfe3e4d407a04efe342b3fc2cc6f5736b
JUNGHOJONG <hojong1351@naver.com>
2020-11-30 12:04:10 +0900

    refactor(baseball): scanner객체를 주입한 GamePlayer클래스로 객체를 만들어 사용
    기존 입력하는 기능이 Application클래스와 GamePlayer클래스에 모두 존재했다. 입력기능이
    플레이어에 의해 행해지는 것이기 때문에 Application클래스의 입력기능을 GamePlayer에게
    할당했다. GamePlayer클래스는 게임 시작부터 종료까지 입력기능을 유지해야 하므로 객체를
    생성하지 않고 클래스로 사용했던 기존의 방식에서 생성자의 매개변수로 scanner객체를
    주입받아 객체로 생성해 사용하는 방식으로 변경했다.

--- src/main/java/baseball/GamePlayer.java
+++ src/main/java/baseball/GamePlayer.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class GamePlayer {
@@ -8,40 +9,67 @@ public class GamePlayer {
     private static final char LAST_BOUNDARY_VALUE = '9';
     private static final int INPUT_NUMBER_COUNT = 3;
     private static final String REINPUT = "다시 입력해주세요";
+    private static final int RESTART = 1;
+    private static final int END = 2;
 
-    public static String getInputNumbers(String inputNumbers)
-                                            throws IllegalArgumentException{
-        if(inputNumbers.length() != INPUT_NUMBER_COUNT){
-            throw new IllegalArgumentException(REINPUT);
-        }
+    private Scanner scanner;
 
-        if(isNotAllNumbers(inputNumbers)){
-            throw new IllegalArgumentException(REINPUT);
-        }
-
-        if(haveDuplicateNumbers(inputNumbers)){
-            throw new IllegalArgumentException(REINPUT);
-        }
-
-        return inputNumbers;
+    public GamePlayer(Scanner scanner){
+        this.scanner = scanner;
     }
 
-    private static boolean isNotAllNumbers(String inputNumbers){
-        for(int i=0; i<inputNumbers.length(); i++){
-            if(inputNumbers.charAt(i) < FIRST_BOUNDARY_VALUE
-                    || inputNumbers.charAt(i) > LAST_BOUNDARY_VALUE){
+    public String generateNumbersOfPlayerEntered()
+                                            throws IllegalArgumentException{
+        String generatedNumbers = scanner.next();
+        if(generatedNumbers.length() != INPUT_NUMBER_COUNT){
+            throw new IllegalArgumentException(REINPUT);
+        }
+
+        if(isNotAllNumbers(generatedNumbers)){
+            throw new IllegalArgumentException(REINPUT);
+        }
+
+        if(haveDuplicateNumbers(generatedNumbers)){
+            throw new IllegalArgumentException(REINPUT);
+        }
+
+        return generatedNumbers;
+    }
+
+    private boolean isNotAllNumbers(String generatedNumbers){
+        for(int i=0; i<generatedNumbers.length(); i++){
+            if(generatedNumbers.charAt(i) < FIRST_BOUNDARY_VALUE
+                    || generatedNumbers.charAt(i) > LAST_BOUNDARY_VALUE){
                return true;
             }
         }
         return false;
     }
 
-    private static boolean haveDuplicateNumbers(String inputNumbers){
+    private boolean haveDuplicateNumbers(String generatedNumbers){
         Set<Character> numbers = new HashSet<>();
-        for(int i=0; i<inputNumbers.length(); i++){
-            numbers.add(inputNumbers.charAt(i));
+        for(int i=0; i<generatedNumbers.length(); i++){
+            numbers.add(generatedNumbers.charAt(i));
         }
         return numbers.size() != INPUT_NUMBER_COUNT;
+    }
+
+    public int selectRestartOrEnd() {
+        String inputNumber = scanner.next();
+
+        while (isInvalidInputNumber(inputNumber)) {
+            System.out.println(REINPUT);
+            inputNumber = scanner.next();
+        }
+
+        return inputNumber.charAt(0) - '0';
+    }
+
+    private boolean isInvalidInputNumber(String inputNumber){
+        int singleDigitInputNumber = inputNumber.charAt(0) - '0';
+
+        return inputNumber.length() != 1
+                || (singleDigitInputNumber != RESTART && singleDigitInputNumber != END);
     }
 
 }
