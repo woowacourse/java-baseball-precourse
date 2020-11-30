d10dda5cea2974732a35c713004db99c3fbdbe1b
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-01 06:12:27 +0900

    feat(BaseballNumbers): add methods for comparing two class
    Added following methods:
    - BaseballNumbers.isStrikeAtIndex
    - BaseballNumbers.isBallOutsideIndex
    - BaseballNumbers.getCountOfStrike
    - BaseballNumbers.getCountOfBall
    Edited TODO section of README.md to reflect changes

--- src/main/java/baseball/BaseballNumbers.java
+++ src/main/java/baseball/BaseballNumbers.java
@@ -16,4 +16,45 @@ public class BaseballNumbers {
         validateItemRangeInArray(numberArray);
         this.numberArray = numberArray;
     }
+
+    // 현재 객체가 index 위치에 지닌 값이 value와 같은지 판별하는 메소드
+    private boolean isStrikeAtIndex(final int index, final int value) {
+        return this.numberArray[index] == value;
+    }
+
+    // 현재 객체가 index를 제외한 위치에 지닌 값이 value와 같은지 판별하는 메소드
+    private boolean isBallOutsideIndex(final int index, final int value) {
+        for (int i = 0; i < this.numberArray.length; i++) {
+            // i와 index가 같은 경우는 스트라이크를 판별할 때만 사용하므로 제외
+            if (i == index) {
+                continue;
+            }
+            if (this.numberArray[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 자신과 friend 객체의 numberList를 비교하여 스트라이크 수를 계산하는 객체
+    public int getCountOfStrike(final BaseballNumbers friend) {
+        int countOfStrike = 0;
+        for (int index = 0; index < this.numberArray.length; index++) {
+            if (isStrikeAtIndex(index, friend.numberArray[index])) {
+                countOfStrike++;
+            }
+        }
+        return countOfStrike;
+    }
+
+    // 자신과 friend 객체의 numberList를 비교하여 볼 수를 계산하는 객체
+    public int getCountOfBall(final BaseballNumbers friend) {
+        int countOfBall = 0;
+        for (int index = 0; index < this.numberArray.length; index++) {
+            if (isBallOutsideIndex(index, friend.numberArray[index])) {
+                countOfBall++;
+            }
+        }
+        return countOfBall;
+    }
 }
