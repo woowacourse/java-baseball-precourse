823403b9d3effa0acb99a81fe118c1354486d40d
DH Lim <dlarkfl2@gamil.com>
2020-12-01 00:10:42 +0900

    Refactor(Opponent) : 접근제어자 수정, getter 생성, 상수 추가

--- src/main/java/baseball/Opponent.java
+++ src/main/java/baseball/Opponent.java
@@ -3,20 +3,28 @@ package baseball;
 import utils.RandomUtils;
 
 public class Opponent {
-    public String opponentNumber;
+    private String opponentNumber;
+
+    private static final int START_INCLUSIVE = 1;
+
+    private static final int END_INCLUISIVE = 9;
+
+    private static final int BASEBALL_NUMBER_LENGTH = 3;
 
     /* opponent 난수 생성 메소드 */
-    public String generateNumber() {
+    public void generateNumber() {
         String opponentNumber = "";
-        while(opponentNumber.length()<3) {
-            int randomNumber = RandomUtils.nextInt(1, 9);
-            if(!opponentNumber.contains(String.valueOf(randomNumber))) {
+        while (opponentNumber.length() < BASEBALL_NUMBER_LENGTH) {
+            String randomNumber = String.valueOf(RandomUtils.nextInt(START_INCLUSIVE, END_INCLUISIVE));
+            if (!opponentNumber.contains(randomNumber)) {
                 opponentNumber += randomNumber;
             }
         }
         this.opponentNumber = opponentNumber;
-        return opponentNumber;
     }
 
+    public String getOpponentNumber() {
+        return opponentNumber;
+    }
 
 }
