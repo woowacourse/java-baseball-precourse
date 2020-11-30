bc9e4058c2d6ab15d1bcda2158bcb8cda42a5b7c
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-30 19:50:43 +0900

    feat(baseball): 게임 정답 확인 및 재시작 최상위 기능 추가
    랜덤한 값을 생성하여 해당 값이 맞는지 확인한다.
    맞출 경우 재시작, 종료 값을 입력받아 시행한다.
    
    BREAKING CHANGE: makeRandomThreeNumber를 사용하여 랜덤한
    값을
    생성하며
    baseBallGameController를 사용하여 해당 값이 정답인지, 정답이 아니면 반복을 수행한다
    정답 입력
    후
    재시작 혹은 종료를 수행한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,15 +4,50 @@ package baseball;
 
 import java.util.Scanner;
 import utils.RandomUtils;
-import static baseball.MakeThreeRandomNumber.makeRandomThreeNumber;
+
+import static baseball.AnswerCheck.answerCheck;
+import static baseball.BaseBallGameController.baseBallGameController;
+import static baseball.MakeRandomThreeNumber.makeRandomThreeNumber;
+import static baseball.UserInput.errorPrint;
+import static baseball.UserInput.userInput;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        int validCountOfNumber = 3;
+        int userChoiceRestartOption;
+        while(true) {
+            int [] computerNumber = makeRandomThreeNumber();
+            /*
+            테스트용 컴퓨터 숫자 출력 코드
+            System.out.println("computerNumber");
+            System.out.println(computerNumber[0]);
+            System.out.println(computerNumber[1]);
+            System.out.println(computerNumber[2]);
+            */
+            System.out.println("computerNumber");
+            System.out.println(computerNumber[0]);
+            System.out.println(computerNumber[1]);
+            System.out.println(computerNumber[2]);
+            baseBallGameController(computerNumber, scanner, validCountOfNumber);
 
-        int [] computerNumber = makeRandomThreeNumber();
-        for(int i:computerNumber){
-            System.out.println(i);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            try {
+                userChoiceRestartOption = userInput(scanner.nextLine(), 1)[0];
+            } catch(IllegalArgumentException e){
+                errorPrint();
+                break;
+            }
+            if (userChoiceRestartOption == 1){
+                continue;
+            }
+            else if (userChoiceRestartOption == 2){
+                break;
+            }
+            else{
+                errorPrint();
+                break;
+            }
         }
     }
 }
