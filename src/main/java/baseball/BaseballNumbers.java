eee9e8dc434a6a0117d13627dd73b1b06fde1fc2
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-01 05:17:33 +0900

    refactor(BaseballNumbers): change numberList to numberArray
    Changed container's type from List<Integer> to int[]
    Fixed functions to fit changing in type:
    - validateSize
    - validateDuplicate
    
    Because the length of List does not change during entire game, I changed its type into Array.

--- src/main/java/baseball/BaseballNumbers.java
+++ src/main/java/baseball/BaseballNumbers.java
@@ -3,34 +3,38 @@ package baseball;
 import static baseball.Constants.GAME_MAX_BASEBALL_PITCH;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 // 야구 게임에서 각 자리의 숫자를 도맡는 클래스
 public class BaseballNumbers {
 
     // 각 투구에 해당하는 숫자를 저장해 둘 배열
-    private final List<Integer> numberList;
+    private final int[] numberArray;
 
-    public BaseballNumbers(List<Integer> numberList) {
-        validateSize(numberList);
-        validateDuplicate(numberList);
-        this.numberList = numberList;
+    public BaseballNumbers(int[] numberArray) {
+        validateSize(numberArray);
+        validateDuplicate(numberArray);
+        this.numberArray = numberArray;
     }
 
     // 입력받은 배열의 크기가 MAX_PITCH 값과 같은지 확인하는 메소드
-    private void validateSize(List<Integer> baseballNumbers) {
-        if (baseballNumbers.size() != GAME_MAX_BASEBALL_PITCH) {
+    private void validateSize(int[] numberArray) {
+        if (numberArray.length != GAME_MAX_BASEBALL_PITCH) {
             String exceptionMessage = "숫자는 " + GAME_MAX_BASEBALL_PITCH + "개만 입력해야 합니다";
             throw new IllegalArgumentException(exceptionMessage);
         }
     }
 
     // 입력받은 배열 내에 중복된 값이 있는지 확인하는 메소드
-    private void validateDuplicate(List<Integer> baseballNumbers) {
-        Set<Integer> baseballNumberSet = new HashSet<>(baseballNumbers);
+    private void validateDuplicate(int[] numberArray) {
+        final Set<Integer> baseballNumberSet = new HashSet<>();
+
+        for (int number : numberArray) {
+            baseballNumberSet.add(number);
+        }
+
         // 중복된 수가 입력받은 배열에 있을 경우, 배열과 셋의 길이가 다른 것을 이용
-        if (baseballNumbers.size() != baseballNumberSet.size()) {
+        if (baseballNumberSet.size() != numberArray.length) {
             throw new IllegalArgumentException("배열 내에 중복된 숫자가 있으면 안 됩니다");
         }
     }
