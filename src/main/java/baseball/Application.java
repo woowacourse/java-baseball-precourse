da8151034d080224db7008a9356af1684b6b2414
yejin <jinee0717@daum.net>
2020-12-01 00:10:19 +0900

    fix : Application 클래스에서 1 또는 2만 허용되도록 수정
    시작 1, 종료 2 이외의 값은 받지 않도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,13 +11,15 @@ public class Application {
         while(true) {
         	System.out.println("숫자야구 게임을 시작하려면 1, 종료하려면 2를 입력하세요!");
         	flag = scanner.nextInt();
-        	
         	if(flag==2) {
         		System.out.println("안녕히가세요!");
         		scanner.close();
         		break;
-        	}else{
+        	}else if(flag==1){
         		game.startGame();
+        	}else {
+        		System.out.println("잘못된 값을 입력하셨습니다.");
+        		continue;
         	}
         }
     }
