f1196f5fbb306afd09e253ddf9e29306e038b54e
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-30 17:36:20 +0900

    feat(baseball): 사용자 입력 정보 중복 처리
    사용자의 입력중 중복이 있을 경우 IllegalArgumentException()를 출력하는 기능을 추가
    
    BREAKING CHANGE: 본 구현 문제에 중복 처리에 대한 내용이 없어서 임의 판단으로 중복은 허용하지 않겠다고
    판단.
    
    중복의 경우 예외 발생
    예외처리 : IllegalArgumentException()

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -1,16 +1,28 @@
 package baseball;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class UserInput {
+    private static boolean duplicateCheck(int[] arr, int validCounteOfNumber){
+        Set<Integer> duplicateCheckHash = new HashSet<>();
+        for(int number:arr){
+            duplicateCheckHash.add(number);
+        }
+        return duplicateCheckHash.size() == validCounteOfNumber;
+    }
+
     public static int[] userInput(String userInput,int validCountOfNumber){
+        int [] retValidNumber = new int[validCountOfNumber];
         String pattern = "[1-9]*";
         if (Pattern.matches(pattern,userInput) && userInput.length() == validCountOfNumber){
-            int [] retValidNumber = new int[validCountOfNumber];
             for(int i=0;i<validCountOfNumber;i++){
                 retValidNumber[i] =  userInput.charAt(i) - '0';
             }
+        }
+        if (duplicateCheck(retValidNumber,validCountOfNumber)){
             return retValidNumber;
         }
         throw new IllegalArgumentException();
