5928769a989bcc4e7652fb4d31318021e25f5056
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-01 03:30:04 +0900

    feat: 게임 재실행 여부 입력이 정상적인지 확인하는 기능 구현

--- src/main/java/baseball/GameResetter.java
+++ src/main/java/baseball/GameResetter.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GameResetter {
     private static final String FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
+    private static final String RESTART_FACTOR_ERROR_MESSAGE = "1 또는 2를 입력하세요.";
     private GameResetter() {
     }
 
@@ -19,8 +19,19 @@ public class GameResetter {
         return UserInputScanner.scanUserInput(scanner);
     }
 
+    public static boolean checkRestartFactorCondition(String restartFactor) {
+        if (restartFactor.equals("1") || restartFactor.equals("2")) {
+            return true;
+        }
+        return false;
+    }
+
     public static boolean getRestartFactor(Scanner scanner) {
         String restartFactor = scanRestartFactor(scanner);
+        boolean suitability = checkRestartFactorCondition(restartFactor);
+        if (!suitability) {
+            throw new IllegalArgumentException(RESTART_FACTOR_ERROR_MESSAGE);
+        }
         if (restartFactor.equals("1")) {
             return true;
         }
