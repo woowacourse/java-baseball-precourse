ca418342c14073451b58acdd0badc4625f40cdc4
KJunseo <max9106@naver.com>
2020-11-30 22:35:29 +0900

    feat: 사용자에게 3자리 숫자 입력받기

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class User {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final int NUMBER_LEN = 3;
+
+    int[] userNumber = new int[NUMBER_LEN];
+
+    public void enterNumber(Scanner scanner) {
+        System.out.println(INPUT_MESSAGE);
+        String number = scanner.nextLine();
+
+        if (isValidInput(number)) {
+            setUserNumber(number);
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    private void setUserNumber(String number) {
+
+    }
+
+    private boolean isValidInput(String number) {
+        return true;
+    }
+}
