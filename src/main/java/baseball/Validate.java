3647a1457eff62bada3af2dd63b62867db844bf7
taelee <taelee@student.42seoul.kr>
2020-11-30 22:56:03 +0900

    refactor(Validate): change all message to constant

--- src/main/java/baseball/Validate.java
+++ src/main/java/baseball/Validate.java
@@ -1,6 +1,13 @@
 package baseball;
 
 public class Validate {
+
+    public static final String ERR_MSG_NOT_INTEGER = "입력 오류: 숫자가 아닙니다.";
+    public static final String ERR_MSG_HAS_ZERO = "입력 오류: 숫자에 0포함";
+    public static final String ERR_MSG_NOT_3_DIGITS = "입력 오류: 세자리 숫자가 아님";
+    public static final String ERR_MSG_DUPLICATE_NUMBER = "입력 오류: 중복된 숫자 존재";
+    public static final String MSG_SUCCESS = "3개의 숫자를 모두 맞히셨습니다.";
+
     public static boolean isValidate(String number) {
         try {
             if (isInteger(number) && !hasZero(number) &&!exceedNumberSize(number) && !hasDuplicateNumber(number)) {
@@ -12,34 +19,34 @@ public class Validate {
         }
         return true;
     }
-    private static boolean isInteger(String guess) {
+    public static boolean isInteger(String guess) {
         try {
             Integer.parseInt(guess);
             return true;
         } catch (NumberFormatException exception){
-            throw new IllegalArgumentException("입력 오류: 숫자가 아닙니다.");
+            throw new IllegalArgumentException(ERR_MSG_NOT_INTEGER);
         }
     }
-    private static boolean hasZero(String guess) {
+    public static boolean hasZero(String guess) {
         for (int i = 0; i < guess.length(); i++) {
             if (guess.charAt(i) < '1' || guess.charAt(i) > '9') {
-                throw new IllegalArgumentException("입력 오류: 숫자에 0포함");
+                throw new IllegalArgumentException(ERR_MSG_HAS_ZERO);
             }
         }
         return false;
     }
-    private static boolean exceedNumberSize(String guess) {
+    public static boolean exceedNumberSize(String guess) {
         if (guess.length() != 3) {
-            throw new IllegalArgumentException("입력 오류: 세자리 숫자가 아님");
+            throw new IllegalArgumentException(ERR_MSG_NOT_3_DIGITS);
         }
         return false;
     }
-    private static boolean hasDuplicateNumber(String guess) {
+    public static boolean hasDuplicateNumber(String guess) {
         char first = guess.charAt(0);
         char second = guess.charAt(1);
         char third = guess.charAt(2);
         if (first == second || first == third || second == third) {
-            throw new IllegalArgumentException("입력 오류: 중복된 숫자 존재");
+            throw new IllegalArgumentException(ERR_MSG_DUPLICATE_NUMBER);
         }
         return true;
     }
@@ -57,7 +64,7 @@ public class Validate {
             }
         }
         if (strike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다.");
+            System.out.println(MSG_SUCCESS);
             return true;
         } else if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
