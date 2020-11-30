b29f3a8cf83b531337bb4688e82443e2186f2721
TaewanKimmmm <ktww88@gmail.com>
2020-12-01 00:43:14 +0900

    feat: Application에서의 게임 실행기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,7 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        NumberBaseBallGame baseBallGame = new NumberBaseBallGame(scanner);
+        baseBallGame.start();
     }
 }
