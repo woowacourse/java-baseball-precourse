88cef8eb7cfe750a221ee9e852b152665d85e2db
surfing2003 <surfing2003@naver.com>
2020-12-01 00:20:16 +0900

    feat : 3자리 랜덤 숫자 생성

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -23,4 +23,19 @@ public class RandomUtils {
 
         return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
     }
+
+    public static String makeGameNumber(){
+        String gameNumber = "";
+        int countLength = 0;
+
+        while (countLength < 3) {
+            int randomNumber = RandomUtils.nextInt(1, 9);
+
+            if (!gameNumber.contains(String.valueOf(randomNumber))) {
+                gameNumber += randomNumber;
+                countLength++;
+            }
+        }
+        return gameNumber;
+    }
 }
