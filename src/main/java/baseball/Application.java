634f2082a894fdd0036729905218d61dc59e7fa6
yangfriendship <yangfriendship@naver.com>
2020-11-30 21:47:00 +0900

    feat (Application): GameController을 이용해서 게임시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,18 @@
 package baseball;
 
+import baseball.controller.GameController;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+        GameController gameController = new GameController(scanner);
+
+        gameController.run();
+
     }
+
 }
