7b4039cebc34f7f8589f5cd0594a6f94e4e6cc5c
RinSabbit <celena1004@naver.com>
2020-11-30 20:27:19 +0900

    refactor: String 상수화

--- src/main/java/utils/CheckResult.java
+++ src/main/java/utils/CheckResult.java
@@ -7,14 +7,21 @@ public class CheckResult {
 
 	private static final int TRUE_SIZE = 3;
 
-	private final String randomNum;
-	private final String inputNum;
+	private final String NOTHING = "낫싱";
+	private final String ONLY_BALL = "%d볼\n";
+	private final String ONLY_STRIKE = "%d스트라이크\n";
+	private final String BALL_AND_STRIKE = "%d볼 %d스트라이크\n";
+	private final String CORRECT_ANSWER = "3개의 숫자를 모두 맞추셨습니다! 게임 종료.\n" +
+			"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+	private final String random;
+	private final String input;
 	private int strike;
 	private int ball;
 
-	public CheckResult(String inputNum, String randomNum) {
-		this.inputNum = inputNum;
-		this.randomNum = randomNum;
+	public CheckResult(String input, String random) {
+		this.input = input;
+		this.random = random;
 		strike = 0;
 		ball = 0;
 	}
@@ -26,32 +33,32 @@ public class CheckResult {
 		return showResult();
 	}
 
-	private void countStrike(int i) {
-		if (inputNum.charAt(i) == randomNum.charAt(i)) {
+	private void countStrike(int index) {
+		if (input.charAt(index) == random.charAt(index)) {
 			strike++;
 		} else {
-			countBall(i);
+			countBall(index);
 		}
 	}
 
-	private void countBall(int i) {
-		if (randomNum.contains(String.valueOf(inputNum.charAt(i)))) {
+	private void countBall(int index) {
+		if (random.contains(String.valueOf(input.charAt(index)))) {
 			ball++;
 		}
 	}
 
 	private boolean showResult() {
 		if (strike == 0 && ball == 0) {
-			System.out.println("낫싱");
+			System.out.println(NOTHING);
 		} else if (strike == 0) {
-			System.out.printf("%d볼\n", ball);
+			System.out.printf(ONLY_BALL, ball);
 		} else if (ball == 0) {
-			System.out.printf("%d스트라이크\n", strike);
+			System.out.printf(ONLY_STRIKE, strike);
 		} else {
-			System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+			System.out.printf(BALL_AND_STRIKE, ball, strike);
 		}
 		if (strike == TRUE_SIZE) {
-			System.out.println("3개의 숫자를 모두 맞추셨습니다! 게임 종료");
+			System.out.println(CORRECT_ANSWER);
 			return true;
 		}
 		return false;
