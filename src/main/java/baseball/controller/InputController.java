ec20a04aa87b37a88089244f4593d85f6dd6ab29
yangfriendship <yangfriendship@naver.com>
2020-11-30 17:08:51 +0900

    feat (InputController): 사용자 입력값을 처리할 InputController

---
+++ src/main/java/baseball/controller/InputController.java
@@ -0,0 +1,46 @@
+package baseball.controller;
+
+import utils.Validator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputController {
+
+    private static Scanner scanner;
+
+    private static String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public InputController(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<Integer> input() {
+
+        printInputMessage();
+
+        String input = scanner.nextLine();
+
+        try {
+            Validator.validateInput(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return input();
+        }
+
+        return toIntegerList(input);
+    }
+
+    private void printInputMessage() {
+        System.out.print(INPUT_MESSAGE);
+    }
+
+    private List<Integer> toIntegerList(String input) {
+        return Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+}
