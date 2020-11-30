a5f7ba852e109d841f4f10ba8cbb8204a84da4e4
PaperCrafter <leejonghee342@gmail.com>
2020-12-01 00:36:14 +0900

    feat(OutputView): add view showing results for given input
    added output view class for displaying strike, ball, nothing
    - print 3 strikes and display END_MSG when scoreboard includes three strike
    - print strike and ball when both value is not 0
    - print strikes when scoreboard.strike is not 0
    - print balls when scoreboard.ball is not zero
    - print nothing if both ball and stike are zero

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,24 @@
+package baseball.view;
+
+import baseball.domain.ScoreBoard;
+import baseball.domain.value.Result;
+
+public class OutputView {
+    public static final String GAME_END_MSG = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static void printResult(ScoreBoard scoreBoard){
+        if(scoreBoard.getStrike() == scoreBoard.getNumbersToGuess()){
+            System.out.println(scoreBoard.getStrike() + Result.STRIKE.getKorean());
+            System.out.println(scoreBoard.getStrike() + GAME_END_MSG);
+        }else if(scoreBoard.getBall() != 0 && scoreBoard.getStrike() != 0){
+            System.out.println(scoreBoard.getBall() + Result.BALL.getKorean() + " "
+                    + scoreBoard.getBall() + Result.STRIKE.getKorean());
+        } else if(scoreBoard.getBall() != 0){
+            System.out.println(scoreBoard.getBall() + Result.BALL.getKorean());
+        } else if(scoreBoard.getStrike() != 0){
+            System.out.println(scoreBoard.getStrike() + Result.STRIKE.getKorean());
+        }else{
+            System.out.println(Result.NOTHING.getKorean());
+        }
+    }
+}
