6e335f79997e57910bcea3f25aa14d5d3a0f2ffd
JUNGHOJONG <hojong1351@naver.com>
2020-11-30 15:16:04 +0900

    refactor(baseball): Application클래스 변수명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,9 @@ package baseball;
 import java.util.Scanner;
 
 public class Application {
-    private static final int CONTINUE = 0;
-    private static final int RESTART = 1;
-    private static final int END = 2;
+    private static final int GAME_CONTINUE = 0;
+    private static final int GAME_RESTART = 1;
+    private static final int GAME_END = 2;
     private static final String GAME_EXIT_CONDITIONS = "3스트라이크";
     private static final String GAME_START_STATEMENT = "숫자를 입력해주세요 : ";
     private static final String GAME_END_STATEMENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료" +
@@ -15,41 +15,43 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         Competitor competitor = new Competitor();
         GamePlayer gamePlayer = new GamePlayer(scanner);
+
         while (true) {
             System.out.print(GAME_START_STATEMENT);
-            int kindOfGame = getIndexOfGameProgress(competitor, gamePlayer);
+            int gameState = playGame(competitor, gamePlayer);
 
-            if (kindOfGame == RESTART) {
+            if (gameState == GAME_RESTART) {
                 competitor = new Competitor();
             }
 
-            if (kindOfGame == END) {
+            if (gameState == GAME_END) {
                 break;
             }
         }
     }
 
-    private static int getIndexOfGameProgress(Competitor competitor, GamePlayer gamePlayer){
+    private static int playGame(Competitor competitor, GamePlayer gamePlayer){
         try {
-            String inputRandomNumbers = gamePlayer.generateNumbersOfPlayerEntered();
+            String generatedNumbersOfPlayer = gamePlayer.generateNumbersOfPlayerEntered();
             String comparativeResult = Comparator.getComparativeResult(
-                                            competitor.getGeneratedRandomNumbers(), inputRandomNumbers);
+                                            competitor.getGeneratedRandomNumbers(),
+                                            generatedNumbersOfPlayer);
 
             System.out.println(comparativeResult);
             if(!comparativeResult.equals(GAME_EXIT_CONDITIONS)){
-                return CONTINUE;
+                return GAME_CONTINUE;
             }
 
             System.out.println(GAME_END_STATEMENT);
-            int selectMenu = gamePlayer.selectRestartOrEnd();
-            if(selectMenu == RESTART){
-                return RESTART;
+            int selectedMode = gamePlayer.selectRestartOrEnd();
+            if(selectedMode == GAME_RESTART){
+                return GAME_RESTART;
             }
 
-            return END;
+            return GAME_END;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return CONTINUE;
+            return GAME_CONTINUE;
         }
     }
 
