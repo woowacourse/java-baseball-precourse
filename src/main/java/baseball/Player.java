4d93ecebc41a2d3dc702ee72b43cd80b68bc03df
yejin <jinee0717@daum.net>
2020-11-30 21:01:56 +0900

    feat : Player 클래스 작업
    사용자에게 숫자를 입력받는 로직 구현
    숫자의 유효성을 검증하는 함수 작업

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Player {
+
+	int[] playerNum = new int[3];
+
+	public void getPlayerNum() {
+		final Scanner scanner = new Scanner(System.in);
+
+		System.out.print("숫자를 입력해주세요 : ");
+		String input = scanner.next();
+		if (input.length() == 3 && isNum(input) && isContainZero(input)) {
+			stringToIntArray(input);
+		} else {
+			System.out.println("입력하신 수의 형식이 올바르지 않습니다.");
+			throw new IllegalArgumentException();
+		}
+
+	}
+
+	// 입력값을 int형 배열로 변환
+	public void stringToIntArray(String input) {
+		String[] temp = input.split("");
+		for (int i = 0; i < 3; i++) {
+			playerNum[i] = Integer.parseInt(temp[i]);
+		}
+	}
+
+	// 입력값이 정수값인지, 1-9 범위 내에 있는지 검증
+	public boolean isNum(String input) {
+		String pattern = "^[1-9]+$";
+		if (input.matches(pattern)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// 입력값에 0이 포함되어 있는지 검증
+	public boolean isContainZero(String input) {
+		for (int i = 0; i < 3; i++) {
+			if (input.charAt(i) == '0')
+				return false;
+		}
+		return true;
+	}
+}
