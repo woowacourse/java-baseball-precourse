aba125096201940e04684369777df08dff1d58ea
Solji Choi <bssj9307@gmail.com>
2020-12-01 04:25:15 +0900

    feat(Answer): static 키워드 추가

--- src/main/java/baseball/Answer.java
+++ src/main/java/baseball/Answer.java
@@ -5,12 +5,12 @@ import java.util.List;
 import utils.RandomUtils;
 
 public class Answer {
-    private int min = 1;
-    private int max = 1;
+    private static final int min = 1;
+    private static final int max = 1;
 
-    private List<Integer> ans = new ArrayList<>();
+    private static final List<Integer> ans = new ArrayList<>();
 
-    public List<Integer> makeAnswer() {
+    public static List<Integer> makeAnswer() {
         int randomNumber = RandomUtils.nextInt(min, max);
         if (!ans.contains(randomNumber)) {
             ans.add(randomNumber);
