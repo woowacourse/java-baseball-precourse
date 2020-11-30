63386e11560cd307de0ad1358376eb2172b7699b
Minsung <sns2831@gmail.com>
2020-12-01 08:08:53 +0900

    feat(baseball): Application 클래스, 게임 시작 로직 추가.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,8 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        BaseballGameInput baseballGameInput = new BaseballGameInput(scanner);
+        BaseballGame baseballGame = new BaseballGame(baseballGameInput);
+        baseballGame.run();
     }
 }
