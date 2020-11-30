7d5995a9275dd21c335ca39286ab302745e5af03
KJunseo <max9106@naver.com>
2020-12-01 06:52:37 +0900

    feat: 게임 종료시 재시작 여부를 처리하는 메소드

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class Game {
     private static final int NUMBER_LEN = 3;
+    private static final int RESTART = 1;
+    private static final int EXIT = 2;
     private static final String VICTORY_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     private Computer computer;
     private User user;
@@ -40,6 +43,18 @@ public class Game {
     }
 
     private boolean isRestart() {
-        return true;
+        System.out.println(RESTART_MESSAGE);
+
+        int restart = scanner.nextInt();
+
+        if (restart == RESTART) {
+            computer = new Computer();
+            running = true;
+            return true;
+        } else if (restart == EXIT) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
     }
 }
