7eba13ff463e1d1ee68f9fe24d116bbdd6781cb0
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-01 04:47:32 +0900

    feat(Constants): Add class for Constants
    Added class for constants
    Moved constants in other files into this class

--- src/main/java/baseball/BaseballNumbers.java
+++ src/main/java/baseball/BaseballNumbers.java
@@ -1,15 +1,13 @@
 package baseball;
 
+import static baseball.Constants.GAME_MAX_BASEBALL_PITCH;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 // 야구 게임에서 각 자리의 숫자를 도맡는 클래스
 public class BaseballNumbers {
-
-    // 최대 투구 횟수를 저장할 변수
-    // 이번 숫자 야구게임에서는 세 개의 투구만을 하므로, 이 값은 3이다
-    public static final int MAX_PITCH = 3;
 
     // 각 투구에 해당하는 숫자를 저장해 둘 배열
     private final List<Integer> numberList;
@@ -22,8 +20,8 @@ public class BaseballNumbers {
 
     // 입력받은 배열의 크기가 MAX_PITCH 값과 같은지 확인하는 메소드
     private void validateSize(List<Integer> baseballNumbers) {
-        if (baseballNumbers.size() != MAX_PITCH) {
-            String exceptionMessage = "숫자는 " + MAX_PITCH + "개만 입력해야 합니다";
+        if (baseballNumbers.size() != GAME_MAX_BASEBALL_PITCH) {
+            String exceptionMessage = "숫자는 " + GAME_MAX_BASEBALL_PITCH + "개만 입력해야 합니다";
             throw new IllegalArgumentException(exceptionMessage);
         }
     }
---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,34 @@
+package baseball;
+
+public final class Constants {
+
+    // 숫자 야구 게임의 최대 투구 횟수를 저장할 변수
+    public static final int GAME_MAX_BASEBALL_PITCH = 3;
+    // 숫자 야구 게임의 최소 숫자를 저장햘 변수
+    public static final int GAME_MIN_BASEBALL_NUMBER = 1;
+    // 숫자 야구 게임의 최대 숫자를 저장할 변수
+    public static final int GAME_MAX_BASEBALL_NUMBER = 9;
+
+    // 사용자가 프로그램 종료를 원하지 않을 경우 입력할 것으로 예상될 문자열
+    public static final String STRING_USER_NOT_EXIT = "1";
+    // 사용자가 프로그램 종료를 원할 경우 입력할 것으로 예상될 문자열
+    public static final String STRING_USER_EXIT = "2";
+
+    // 사용자에게 입력을 요구할 때 콘솔에 출력할 문자열
+    public static final String MESSAGE_GAME_INPUT = "숫자를 입력해 주세요 : ";
+    // 게임이 종료되었음을 알릴 때 콘솔에 출력할 문자열
+    public static final String MESSAGE_GAME_FINISHED =
+        GAME_MAX_BASEBALL_PITCH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    // 게임의 재시작 여부를 사용자에게 물을 때 콘솔에 출력할 문자열
+    public static final String MESSAGE_GAME_REPLAY =
+        "게임을 새로 시작하려면 " + STRING_USER_NOT_EXIT + ", 종료하려면 " + STRING_USER_NOT_EXIT + "를 입력하세요.";
+    // 스트라이크와 볼이 모두 존재하지 않을 경우 콘솔에 출력할 문자열
+    public static final String MESSAGE_RESULT_NOTHING = "낫싱";
+    // 볼의 갯수 뒤에 붙여 콘솔에 출력할 문자열
+    public static final String MESSAGE_RESULT_BALL = "볼";
+    // 스트라이크의 갯수 뒤에 붙여 콘솔에 출력할 문자열
+    public static final String MESSAGE_RESULT_STRIKE = "스트라이크";
+
+    private Constants() {
+    }
+}
