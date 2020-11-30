f21d7839a7506ee191d706b4d1fc8544b7ce88f3
KJunseo <max9106@naver.com>
2020-12-01 02:33:10 +0900

    feat: 정답과 유저의 입력값을 비교

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -1,11 +1,28 @@
 package baseball;
 
 public class Hint {
+    private static final int NUMBER_LEN = 3;
+
     int strike;
     int ball;
 
     public Hint(int strike, int ball) {
         this.strike = strike;
         this.ball = ball;
+    }
+
+    public void countHint(int[] computerAnswer, int[] userAnswer) {
+        for (int i = 0; i < NUMBER_LEN; i++) {
+            countStrike(computerAnswer[i], userAnswer[i]);
+            countBall(computerAnswer, userAnswer[i], i);
+        }
+    }
+
+    private void countStrike(int computerNum, int userNum) {
+
+    }
+
+    private void countBall(int[] computerAnswer, int userNum, int userNumIdx) {
+
     }
 }
