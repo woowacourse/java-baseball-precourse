383388680f28c20d876b1b7ecf45bf007688a86f
xrabcde <joara124@ewhain.net>
2020-12-01 00:11:06 +0900

    feat(Application): 3자리 난수 생성하는 generateNumber 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,24 @@
 package baseball;
 
+import utils.RandomUtils;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        int[] answerList = generateNumber();
+        scanner.close();
+    }
+
+    private static int[] generateNumber() {
+        /* 1~9의 서로 다른 수로 이루어진 3자리 난수 생성*/
+        int[] answer = new int[3];
+        while(answer[0] == answer[1] || answer[0] == answer[2] || answer[1] == answer[2]) {
+        /* 중복된 숫자가 있을 경우 다시 생성*/
+            answer[0] = RandomUtils.nextInt(1,9);
+            answer[1] = RandomUtils.nextInt(1,9);
+            answer[2] = RandomUtils.nextInt(1,9);
+        }
+        return answer;
     }
 }
