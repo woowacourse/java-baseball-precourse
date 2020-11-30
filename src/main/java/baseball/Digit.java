8ad62a0d05a14c3d873be0959c827f6c669c24e8
TaewanKimmmm <ktww88@gmail.com>
2020-12-01 01:38:36 +0900

    refactor: 숫자 하드코딩 상수형 변수로 변경

--- src/main/java/baseball/Digit.java
+++ src/main/java/baseball/Digit.java
@@ -7,7 +7,7 @@ public class Digit {
 
     Digit(int input){
         if (input < MIN || MAX < input) {
-            throw new IllegalArgumentException("각 자리수의 숫자는 1 이상 9 이하여야 합니다.");
+            throw new IllegalArgumentException("각 자리수의 숫자는" + MIN + " 이상 " + MAX + " 이하여야 합니다.");
         }
         this.digit = input;
     }
