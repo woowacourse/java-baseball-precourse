ec056fb6addd0ea4cbce7b7562d21b554fd04646
LeeJuHyuk <dlwngur1535@gmail.com>
2020-11-30 22:12:02 +0900

    fix: 중복제거 기능 수정

--- src/main/java/utils/Convertor.java
+++ src/main/java/utils/Convertor.java
@@ -1,17 +1,19 @@
 package utils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Convertor {
 
     public Convertor() {}
 
     public static List<Integer> stringToIntList(String target) {
-        List<Integer> result = new ArrayList<>();
+        Set<Integer> result = new HashSet<>();
         for (int i = 0; i < target.length(); i++) {
             result.add(Character.getNumericValue(target.charAt(i)));
         }
-        return result;
+        return new ArrayList<>(result);
     }
 }
