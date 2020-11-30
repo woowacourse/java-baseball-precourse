6f2df5ba2ec2a0c7b078da99ca734ef7c81977fb
MyaGya <38939015+MyaGya@users.noreply.github.com>
2020-11-30 19:35:53 +0900

    feat(baseball): 정답을 맞출 때 까지 반복하는 기능 추가
    정답을 정확히 맞출 떄 까지 반복하며, 맞춘 경우 종료하는 클래스 작성
    
    BREAKING CHANGE: 해당 기능은 볼, 스트라이크 판단과 사용자 입력만을 반복해서 재생하며 정답 입력시 완전히
    종료한다.
    재시작 기능은 함수 호출 위치에서 처리해주어야 한다.

---
+++ src/main/java/baseball/BaseBallGameController.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import static baseball.AnswerCheck.answerCheck;
+import static baseball.UserInput.errorPrint;
+import static baseball.UserInput.userInput;
+
+import java.util.Scanner;
+
+public class BaseBallGameController {
+    public static void baseBallGameController(int[] computerNumber, Scanner scanner, int validCountOfNumber){
+        int [] score;
+        int [] userNumber;
+        while (true) {
+            System.out.print("숫자를 입력해 주세요 : ");
+            try {
+                userNumber = userInput(scanner.nextLine(), validCountOfNumber);
+            } catch (IllegalArgumentException e){
+                errorPrint();
+                continue;
+            }
+            score = answerCheck(computerNumber, userNumber,
+                validCountOfNumber); // {strike, ball}
+            if (score[0] == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            } else if (score[0] == 0 && score[1] == 0) {
+                System.out.println("낫싱");
+            } else if (score[0] == 0) {
+                System.out.println(score[1] + "스트라이크");
+            } else if (score[1] == 0) {
+                System.out.println(score[0] + "볼");
+            } else {
+                System.out.println(score[1] + " 볼 " + score[0] + "스트라이크");
+            }
+        }
+    }
+}
