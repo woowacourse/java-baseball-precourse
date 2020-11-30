0a7ac241d7fc713ab6c6f99de4679790794e4858
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-11-30 11:57:50 +0900

    test(ValidationUtilsTest): add unit tests for validateInputInProgress() method

---
+++ src/test/java/utils/ValidationUtilsTest.java
@@ -0,0 +1,62 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ValidationUtilsTest {
+    private static final int NUMBER_OF_DIGITS = 3;
+    private static final int MIN_DIGIT = 1;
+    private static final int MAX_DIGIT = 9;
+
+    @Test
+    public void validateInput() {
+        String userInput = "123";
+
+        String validInput =
+                ValidationUtils.validateInputInProgress(
+                        userInput,
+                        NUMBER_OF_DIGITS,
+                        MIN_DIGIT,
+                        MAX_DIGIT);
+
+        assertThat(userInput).isEqualTo(validInput);
+    }
+
+    @Test
+    public void validateInput_hasDifferentNumberOfDigits() {
+        String userInput = "1234";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> ValidationUtils.validateInputInProgress(
+                        userInput,
+                        NUMBER_OF_DIGITS,
+                        MIN_DIGIT,
+                        MAX_DIGIT));
+    }
+
+    @Test
+    public void validateInput_isNotDigits() {
+        String userInput = "a12";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> ValidationUtils.validateInputInProgress(
+                        userInput,
+                        NUMBER_OF_DIGITS,
+                        MIN_DIGIT,
+                        MAX_DIGIT));
+    }
+
+    @Test
+    public void validateInput_hasDuplicateDigits() {
+        String userInput = "111";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> ValidationUtils.validateInputInProgress(
+                        userInput,
+                        NUMBER_OF_DIGITS,
+                        MIN_DIGIT,
+                        MAX_DIGIT));
+    }
+}
