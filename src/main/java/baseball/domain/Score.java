53f116df87e8cd29753351bdaa37d931ffd5b392
LeeJuHyuk <dlwngur1535@gmail.com>
2020-11-30 23:05:31 +0900

    feat: 게임 상태 확인 기능 추가

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import static baseball.config.BaseballConfiguration.BALL_LENGTH;
+
 public class Score {
     private int ball;
     private int strike;
@@ -10,5 +12,25 @@ public class Score {
 
     public void increaseStrikeCount() {
         strike++;
+    }
+
+    public boolean isZeroBall() {
+        return ball == 0;
+    }
+
+    public boolean isZeroStrike() {
+        return strike == 0;
+    }
+
+    public boolean isFullStrike() {
+        return strike == BALL_LENGTH;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
     }
 }
