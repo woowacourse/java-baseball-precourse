8ea7cc6da47a40f8bd6fbea9b2961a187e6aa2ff
platanus <platanus@canxan.com>
2020-11-30 11:00:00 +0900

    fix(NumberUtils): Add '0' character exception throw

--- src/main/java/baseball/NumberUtils.java
+++ src/main/java/baseball/NumberUtils.java
@@ -4,12 +4,15 @@ import java.util.HashSet;
 import utils.RandomUtils;
 
 public class NumberUtils {
-
+    
     public static int validateInput(String inputString) {
         int numbers;
         for (int i = 0; i < inputString.length(); i++) {
             if (!Character.isDigit(inputString.charAt(i))) {
                 throw new IllegalArgumentException("3자리 수가 아닙니다.");
+            }
+            if (inputString.charAt(i) == '0') {
+                throw new IllegalArgumentException("1 부터 9까지의 수를 입력해 주세요.");
             }
         }
         numbers = Integer.parseInt(inputString);
