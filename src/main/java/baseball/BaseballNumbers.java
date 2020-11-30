497f665c6b14c64e516366da3539bc8647742e2d
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-01 05:23:09 +0900

    feat(BaseballNumbers): add function validateNumberRange
    Added function for validating array whether its all item is within permitted range
    Changed constructor of BaseballNumbers to use this function

--- src/main/java/baseball/BaseballNumbers.java
+++ src/main/java/baseball/BaseballNumbers.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import static baseball.Constants.GAME_MAX_BASEBALL_NUMBER;
 import static baseball.Constants.GAME_MAX_BASEBALL_PITCH;
+import static baseball.Constants.GAME_MIN_BASEBALL_NUMBER;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,6 +16,7 @@ public class BaseballNumbers {
     public BaseballNumbers(int[] numberArray) {
         validateSize(numberArray);
         validateDuplicate(numberArray);
+        validateNumberRange(numberArray);
         this.numberArray = numberArray;
     }
 
@@ -36,6 +39,15 @@ public class BaseballNumbers {
         // 중복된 수가 입력받은 배열에 있을 경우, 배열과 셋의 길이가 다른 것을 이용
         if (baseballNumberSet.size() != numberArray.length) {
             throw new IllegalArgumentException("배열 내에 중복된 숫자가 있으면 안 됩니다");
+        }
+    }
+
+    // 입력받은 배열 내에 허용된 범위를 넘어가는 값이 있는지 확인하는 메소드
+    private void validateNumberRange(int[] numberArray) {
+        for (int number : numberArray) {
+            if (number > GAME_MAX_BASEBALL_NUMBER || number < GAME_MIN_BASEBALL_NUMBER) {
+                throw new IllegalArgumentException("배열 내에 범위를 벗어난 숫자가 존재합니다");
+            }
         }
     }
 }
