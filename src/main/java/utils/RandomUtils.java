8828d8fbb2bdeacff5fee318e4cdd95867bb96f5
RinSabbit <celena1004@naver.com>
2020-11-30 20:02:58 +0900

    refactor: RandomUtils.java 주석 삭제

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -3,9 +3,6 @@ package utils;
 
 import java.util.Random;
 
-/**
- * 랜덤한 숫자를 생성하는 클래스
- */
 public class RandomUtils {
     private static final Random RANDOM = new Random();
 
@@ -24,7 +21,6 @@ public class RandomUtils {
         if (startInclusive == endInclusive) {
             return startInclusive;
         }
-
         return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
     }
 }
