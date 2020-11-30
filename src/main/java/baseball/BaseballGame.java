b78496004a4d37aa7cc481c42de1bdd631fd1c74
Hansaem Woo <feliscatus@hanyang.ac.kr>
2020-12-01 06:29:01 +0900

    feat(BaseballGame): add class for calculating match result
    Added BaseballGame class which calculates result of current match
    Edited TODO section of README.md to reflect changes

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,14 +1,37 @@
 package baseball;
 
-import java.util.LinkedHashSet;
+import static baseball.Constants.GAME_MAX_BASEBALL_PITCH;
+import static baseball.UtilityFunctions.generateAnswerArray;
 
 public class BaseballGame {
 
-    // 스트라이크와 볼을 계산해주는 메소드
-    static int checkStrikeAndBall(LinkedHashSet<Integer> answerSet, Integer[] answerArr,
-        int[] guessArr) {
-        int strikeAndBall = 0;
-        // TODO 구현 진행
-        return strikeAndBall;
+    // 현재 게임의 정답을 저장해둘 곳
+    private final BaseballNumbers answerNumbers;
+    // 유저가 입력한 추측에 맞는 게임 결과를 저장해두는 곳
+    private BaseballResult baseballResult;
+
+    public BaseballGame() {
+        this.answerNumbers = new BaseballNumbers(generateAnswerArray());
+        this.baseballResult = new BaseballResult(0, 0);
+    }
+
+    // 입력받은 userGuessArray를 이용하여 경기를 진행하여 스트라이크/볼을 계산하는 메소드
+    public void playBall(int[] userGuessArray) {
+        BaseballNumbers userGuessNumbers = new BaseballNumbers(userGuessArray);
+
+        final int countOfStrike = this.answerNumbers.getCountOfStrike(userGuessNumbers);
+        final int countOfBall = this.answerNumbers.getCountOfBall(userGuessNumbers);
+
+        this.baseballResult = new BaseballResult(countOfStrike, countOfBall);
+    }
+
+    // 저장중인 BaseballResult 를 반환하는 메소드
+    public BaseballResult getBaseballResult() {
+        return this.baseballResult;
+    }
+
+    // 경기가 끝나있는지를 boolean 으로 알려주는 메소드
+    public boolean isFinished() {
+        return this.baseballResult.getStrike() == GAME_MAX_BASEBALL_PITCH;
     }
 }
