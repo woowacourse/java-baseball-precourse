76221576a31271f73152f1e3838d57ea2bb8c5d3
LeeJuHyuk <dlwngur1535@gmail.com>
2020-11-30 23:45:32 +0900

    feat: Player의 IllegalArgumentException 관련 문구 상수화

---
+++ src/main/java/baseball/view/IllegalArgumentExceptionView.java
@@ -0,0 +1,6 @@
+package baseball.view;
+
+public class IllegalArgumentExceptionView {
+    public static final String INPUT_NOT_DUPLICATED_THREE_DIGIT_NUMBERS = "중복 되지 않는 3자리 수를 입력해주세요.";
+    public static final String INPUT_OUT_OF_RANGE_NUMBER = "입력 숫자는 1~9 사이의 숫자만 허용됩니다.";
+}
