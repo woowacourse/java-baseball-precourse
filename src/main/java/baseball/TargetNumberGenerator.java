0c01fe99af43ce90a59a07c89ce6db3eb3a6b8bf
yunjung <cairofaith@gmail.com>
2020-12-01 00:33:30 +0900

    feat: target number generating function
    - Target number is generated by the game system

---
+++ src/main/java/baseball/TargetNumberGenerator.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import utils.RandomUtils;
+
+import java.util.ArrayList;
+
+public class TargetNumberGenerator {
+
+    public ArrayList<Integer> targetNumber;
+
+    public TargetNumberGenerator() {
+        targetNumber = generateNumber();
+    }
+
+    public ArrayList<Integer> getTargetNumber() {
+        return targetNumber;
+    }
+
+    private ArrayList<Integer> generateNumber() {
+        ArrayList<Integer> numberList = new ArrayList<>();
+
+        for(int digit = 0; digit < 3; digit++) {
+            int generatedNumber = avoidRepeat(numberList);
+            numberList.add(generatedNumber);
+        }
+        return numberList;
+    }
+
+    private int avoidRepeat(ArrayList<Integer> finalNumbers) {
+        int randomNumber = RandomUtils.nextInt(1, 9);
+        while(checkRepeat(finalNumbers, randomNumber)) {
+            randomNumber = RandomUtils.nextInt(1, 9);
+        }
+        return randomNumber;
+    }
+
+    private boolean checkRepeat(ArrayList<Integer> finalNumbers, int candidateNumber) {
+        return finalNumbers.contains(candidateNumber);
+    }
+}
