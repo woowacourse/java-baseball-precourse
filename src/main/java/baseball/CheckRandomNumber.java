95b4b985a9ea7acb7cd99031bfcfc95b0bddb2b8
RinSabbit <celena1004@naver.com>
2020-11-30 21:25:56 +0900

    refactor: 클래스명 변경(CheckDuplicate->CheckRandomNumber)

--- src/main/java/baseball/CheckRandomNumber.java
+++ src/main/java/baseball/CheckRandomNumber.java
@@ -7,26 +7,26 @@ import java.util.Set;
 /**
  * 생성된 난수에 중복된 숫자가 있는지 검사하는 클래스
  */
-public class CheckDuplicate {
+public class CheckRandomNumber {
 
 	final static int TRUE_SIZE = 3;
 	final static int START_NUMBER = 100;
 	final static int END_NUMBER = 999;
 
 	public String makeRandomNumber() {
-		int randomNumber = RandomUtils.nextInt(START_NUMBER, END_NUMBER);
-		if(isDuplicate(randomNumber)) {
+		int random = RandomUtils.nextInt(START_NUMBER, END_NUMBER);
+		if(isDuplicate(random)) {
 			return makeRandomNumber();
 		}
-		return String.valueOf(randomNumber);
+		return String.valueOf(random);
 	}
 
-	private boolean isDuplicate(int randomNum) {
+	private boolean isDuplicate(int random) {
 		Set<Integer> set = new HashSet<>();
 
 		for(int i=0; i<TRUE_SIZE; i++) {
-			set.add(randomNum % 10);
-			randomNum /= 10;
+			set.add(random % 10);
+			random /= 10;
 		}
 		return !(isLengthThree(set));
 	}
