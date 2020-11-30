32d2556fd7e6b4b4465b77fb855d778cdcfb7810
kmj990929 <kmj990929@dgist.ac.kr>
2020-11-30 15:17:21 +0900

    feat: 비정상적인 예측값일 때 에러 출력 기능 구현

--- src/main/java/baseball/UserInputScanner.java
+++ src/main/java/baseball/UserInputScanner.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class UserInputScanner {
     private static final String GUESS_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GUESS_ERROR_MESSAGE = "중복되지 않는 1~9까지로 이루어진 세 자리 수를 입력해주세요.";
 
     private UserInputScanner() {
     }
@@ -19,5 +20,14 @@ public class UserInputScanner {
     public static String scanGuess(Scanner scanner) {
         printGuessInputMessage();
         return scanUserInput(scanner);
+    }
+
+    public static int getGuess(Scanner scanner) {
+        String guessCandidate = scanGuess(scanner);
+        boolean suitability = GuessConditionChecker.checkGuessCondition(guessCandidate);
+        if (!suitability) {
+            throw new IllegalArgumentException(GUESS_ERROR_MESSAGE);
+        }
+        return Integer.parseInt(guessCandidate);
     }
 }
