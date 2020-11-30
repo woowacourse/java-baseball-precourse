04b2175619813846487f672a92f0dbd9f0626c31
yangfriendship <yangfriendship@naver.com>
2020-11-30 15:19:12 +0900

    feat (Computer): 3가지 숫자를 생성하는 Computer클래스 생성

---
+++ src/main/java/baseball/controller/Computer.java
@@ -0,0 +1,54 @@
+package baseball.controller;
+
+import utils.RandomUtils;
+import utils.Validator;
+
+import java.util.*;
+
+import static utils.Validator.INPUT_MAX_NUMBER;
+import static utils.Validator.INPUT_MIN_NUMBER;
+
+public class Computer {
+
+    private static List<Integer> numbers;
+
+    private Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static Computer create() {
+
+        List<Integer> numbers = createNumbers();
+
+        return new Computer(numbers);
+    }
+
+    private static List<Integer> createNumbers() {
+
+        Set<Integer> numbers = new HashSet<>();
+
+        while (numbers.size() < Validator.REQUIRE_NUMBER_COUNT) {
+            numbers.add(getRandom());
+        }
+
+        return new ArrayList<>(numbers);
+    }
+
+    private static int getRandom() {
+        return RandomUtils.nextInt(INPUT_MIN_NUMBER, INPUT_MAX_NUMBER);
+    }
+
+
+    public boolean hasNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    public boolean hasNumberOfIndex(int number, int index) {
+        return numbers.indexOf(number) == index;
+    }
+
+    public List<Integer> cloneNumbers() {       //TODO 테스트용
+        return Collections.unmodifiableList(this.numbers);
+    }
+
+}
---
+++ src/main/java/utils/Validator.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Validator {
+
+    public static final int REQUIRE_NUMBER_COUNT = 3;
+    public static final int INPUT_MAX_NUMBER = 9;
+    public static final int INPUT_MIN_NUMBER = 1;
+
+
+}
