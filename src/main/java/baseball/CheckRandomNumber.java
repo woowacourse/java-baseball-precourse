b676e2949c106a5b4a43a760b045f2404e1ac6e7
RinSabbit <celena1004@naver.com>
2020-11-30 21:45:52 +0900

    style: 사용하지 않는 모듈 삭제

--- src/main/java/baseball/CheckRandomNumber.java
+++ src/main/java/baseball/CheckRandomNumber.java
@@ -1,9 +1,7 @@
 package baseball;
 
 import utils.RandomUtils;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * 조건에 맞는 난수를 생성하는 클래스
@@ -12,7 +10,7 @@ public class CheckRandomNumber {
 
 	private final static int START_NUMBER = 100;
 	private final static int END_NUMBER = 999;
-	private Scanner scanner;
+	private final Scanner scanner;
 
 	CheckRandomNumber(Scanner scanner){
 		this.scanner = scanner;
