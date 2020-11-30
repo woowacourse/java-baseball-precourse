ed6610ae3bf9dd26f4cf1c17701efabef153357e
NewWisdom <wlgp2500@gmail.com>
2020-11-30 17:28:15 +0900

    feat(referee): add compare-answer-and-guess method

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -13,12 +13,25 @@ public class Referee {
     }
 
     public void compareNumber(String answer, String guess) {
+        initCnt();
+        for (int i = 0; i < 3; i++) {
+            char crtAnswer = answer.charAt(i);
+            char crtGuess = guess.charAt(i);
+            isBall(i, answer, crtGuess);
+            isStrike(crtAnswer, crtGuess);
+        }
     }
 
     public void isBall(int i, String answer, char crtGuess) {
+        if (answer.indexOf(crtGuess) != -1 && answer.charAt(i) != crtGuess) {
+            ballCnt++;
+        }
     }
 
     public void isStrike(char crtAnswer, char crtGuess) {
+        if (crtAnswer == crtGuess) {
+            strikeCnt++;
+        }
     }
 
     public int getBallCnt() {
