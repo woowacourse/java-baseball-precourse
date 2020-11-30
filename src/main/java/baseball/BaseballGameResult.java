b96aa33f1d53ddefce4be070207ffbce98c80d09
Minsung <sns2831@gmail.com>
2020-12-01 06:55:57 +0900

    feat(baseball): BaseballGameResult 클래스 추가
    게임의 결과 계산 로직 구현 및 결과 확인 구현

---
+++ src/main/java/baseball/BaseballGameResult.java
@@ -0,0 +1,74 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BaseballGameResult {
+    private final int BALL = 0;
+    private final int STRIKE = 1;
+    private final int FINISH_THRESHOLD = 3;
+
+    private BaseballNumbers computerBaseballNumbers;
+    private BaseballNumbers userBaseballNumbers;
+
+    private Map<Integer, Integer> compareResult = new HashMap<>();
+
+    private BaseballGameResult(BaseballNumbers computerBaseballNumbers, BaseballNumbers userBaseballNumbers) {
+        this.computerBaseballNumbers = computerBaseballNumbers;
+        this.userBaseballNumbers = userBaseballNumbers;
+        this.compareResult.put(BALL, 0);
+        this.compareResult.put(STRIKE, 0);
+    }
+
+    public static BaseballGameResult getBaseballGameResult(BaseballNumbers computerBaseballNumbers, BaseballNumbers userBaseballNumbers) {
+        BaseballGameResult baseballGameResult = new BaseballGameResult(computerBaseballNumbers, userBaseballNumbers);
+        baseballGameResult.calculateResult();
+
+        return baseballGameResult;
+    }
+
+    private void calculateResult() {
+        int[] result = compareBaseballNumbers();
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] == i) {
+                compareResult.put(STRIKE, compareResult.get(STRIKE) + 1);
+            } else if (result[i] != -1) {
+                compareResult.put(BALL, compareResult.get(BALL) + 1);
+            }
+        }
+    }
+
+    private int[] compareBaseballNumbers() {
+        return userBaseballNumbers
+                .getBaseballNumbers()
+                .stream()
+                .mapToInt(baseballNumber ->
+                        computerBaseballNumbers.getBaseballNumbers().indexOf(baseballNumber))
+                .toArray();
+    }
+
+    public boolean isFinished() {
+        return compareResult.get(STRIKE) == FINISH_THRESHOLD;
+    }
+
+    @Override
+    public String toString() {
+        List<String> results = new ArrayList<>();
+
+        if (compareResult.get(BALL) != 0) {
+            results.add(compareResult.get(BALL) + "볼");
+        }
+
+        if (compareResult.get(STRIKE) != 0) {
+            results.add(compareResult.get(STRIKE) + "스트라이크");
+        }
+
+        if (results.size() == 0) {
+            results.add("낫싱");
+        }
+
+        return String.join(" ", results);
+    }
+}
