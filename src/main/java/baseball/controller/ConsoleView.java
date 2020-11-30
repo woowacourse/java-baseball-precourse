bbd6e3bf569f86910ef550d9b45b42849d9b32ef
yangfriendship <yangfriendship@naver.com>
2020-11-30 18:52:06 +0900

    feat (ConsoleView): 힌트와 결과를 사용자에게 출력하는 ConsoleView 생성

---
+++ src/main/java/baseball/controller/ConsoleView.java
@@ -0,0 +1,62 @@
+package baseball.controller;
+
+public class ConsoleView {
+
+    private static final StringBuilder stringBuilder = new StringBuilder();
+
+    private static final String PRINT_BALL_MESSAGE = "볼 ";
+    private static final String PRINT_STRIKE_MESSAGE = "스트라이크";
+    private static final String PRINT_NOTING_MESSAGE = "낫싱";
+    private static final String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    private static final int NONE_USEFUL_NUMBER = 0;
+    private static final int SUCCESS_REQUIRE_COUNT = 3;
+
+
+    private static void resetStringBuilder() {
+        stringBuilder.setLength(0);
+    }
+
+    public void printResult() {
+        if (stringBuilder.length() == NONE_USEFUL_NUMBER) {
+            printNothing();
+            return;
+        }
+
+        System.out.println(stringBuilder.toString());
+        resetStringBuilder();
+    }
+
+    public void setBallCount(int ballCount) {
+        if (ballCount == NONE_USEFUL_NUMBER) {
+            return;
+        }
+        stringBuilder.append(ballCount + PRINT_BALL_MESSAGE);
+
+    }
+
+    public void setStrikeCount(int strikeCount) {
+        if (strikeCount == NONE_USEFUL_NUMBER) {
+            return;
+        }
+
+        if (strikeCount == SUCCESS_REQUIRE_COUNT) {
+            printGameSuccess();
+            return;
+        }
+        stringBuilder.append(strikeCount + PRINT_STRIKE_MESSAGE);
+    }
+
+
+    private void printNothing() {
+        System.out.println(PRINT_NOTING_MESSAGE);
+    }
+
+    private void printGameSuccess() {
+        if (stringBuilder.length() != NONE_USEFUL_NUMBER) {
+            resetStringBuilder();
+        }
+        stringBuilder.append(GAME_CLEAR_MESSAGE);
+    }
+
+}
