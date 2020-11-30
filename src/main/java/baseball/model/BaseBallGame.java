a7774e977d2a21aa21bb8f74b249937714f2f4ea
DESKTOP-B3EGD8V\HANULPARK <hanulparkk@gmail.com>
2020-11-30 17:49:30 +0900

    feat(BaseBallGame): compare user input to random digits

--- src/main/java/baseball/model/BaseBallGame.java
+++ src/main/java/baseball/model/BaseBallGame.java
@@ -13,4 +13,26 @@ public class BaseBallGame {
     public void generateRandomDigits() {
         randomDigitList = DataGenerator.generateRandomDigitList(parameterSet);
     }
+
+    public ResultSet compareDigits(String digits) {
+        int numberOfStrikes = 0;
+        int numberOfBalls = 0;
+        int aDigit;
+
+        for (int i = 0; i < digits.length(); i++) {
+            aDigit = Character.getNumericValue(digits.charAt(i));
+
+            if (this.randomDigitList.get(i) == aDigit) {
+                numberOfStrikes++;
+                continue;
+            }
+
+            if (this.randomDigitList.contains(aDigit)) {
+                numberOfBalls++;
+            }
+        }
+
+        ResultSet resultSet = new ResultSet(numberOfStrikes, numberOfBalls);
+        return resultSet;
+    }
 }
