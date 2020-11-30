983fdc417f2726320daf81edc95b6a01cf9ca594
PaperCrafter <leejonghee342@gmail.com>
2020-11-30 16:46:58 +0900

    feat(NumberFormatValidator): add class for validate input
    Added NumberFormatValidaton class for valide
    - check if number format could be converted to integer
    - check if length of number is specific size (in this case 3)
    - check if input has duplicated number

---
+++ src/main/java/baseball/validator/NumberFormatValidator.java
@@ -0,0 +1,22 @@
+package baseball.validator;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class NumberFormatValidator {
+    private static final int NUMBER_OF_NUMBERS = 3;
+
+    public static boolean validate(String number){
+        try {
+            Set<Integer> numberSet = new HashSet<>(Arrays.stream(number.split(""))
+                    .map(Integer::new)
+                    .collect(Collectors.toSet()));
+            if (number.length() != numberSet.size() || numberSet.size() != NUMBER_OF_NUMBERS) {
+                return false;
+            }
+        }catch (NumberFormatException ne){
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/baseball/validator/NumberFormatValidatorTest.java
@@ -0,0 +1,34 @@
+package baseball.vaildator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberFormatValidatorTest {
+    @Test
+    public void testValidatorValueWithValid(){
+        //given
+        String givenNum = "124";
+        //when
+        //then
+        assertEquals(NumberFormatValidator.validate(givenNum), true);
+    }
+
+    @Test
+    public void testValidatorValueWithInvalidNumber(){
+        //given
+        String givenNum = "12464";
+        //when
+        //then
+        assertEquals(NumberFormatValidator.validate(givenNum), false);
+    }
+
+    @Test
+    public void testValidatorValueWithInvalidCharacters(){
+        //given
+        String givenNum = "sdagbjklew";
+        //when
+        //then
+        assertEquals(NumberFormatValidator.validate(givenNum), false);
+    }
+}
