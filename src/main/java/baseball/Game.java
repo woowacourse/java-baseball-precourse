2fedad2f999e6966ededcd742dcc64b33b791d35
taelee <taelee@student.42seoul.kr>
2020-12-01 13:48:54 +0900

    fix(Game): delete unused variable

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,8 +6,7 @@ public class Game {
 
     public static final String MSG_TYPE_DIGITS = "숫자를 입력해주세요 : ";
     public static final String MSG_GAME_EXIT = "게임종료";
-    public static final String MSG_INPUT_ERROR = "입력 오류";
-    public static final int MODE_VALIDATE = 1;
+ static final int MODE_VALIDATE = 1;
 
     public boolean startGame(Scanner scanner) {
         String answer = RandomAnswer.randomAnswer();
