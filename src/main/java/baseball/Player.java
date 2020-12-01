fb94b5f2451e76da74fad56a071f728d7919a45e
Solji Choi <bssj9307@gmail.com>
2020-12-01 16:09:58 +0900

    feat(Player): 플레이어 입력 예외 처리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,10 +5,29 @@ import java.util.Scanner;
 import java.util.List;
 
 public class Player {
+	
+	private static int playerInput;
+	private static final String errorMessage = "3자리의 수만 입력할 수 있습니다.";
 
     public static int getPlayerInput() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	if (scanner.hasNextInt()) {
+    		playerInput = scanner.nextInt();
+    	} else {
+    		System.out.println(errorMessage);
+    		getPlayerInput();
+    	}
+    	
+    	
+    	try {
+    		validateInput(playerInput);
+    	} catch (IllegalArgumentException e) {
+    		System.out.println(e.toString());
+    		getPlayerInput();
+    	}
+    	
+        return playerInput;
     }
 
     public static List<Integer> numberToArray(int num) {
@@ -20,6 +39,15 @@ public class Player {
         }
 
         return userNumber;
+    }
+    
+    public static boolean validateInput(int input) throws IllegalArgumentException {
+
+        if ((int)(Math.log10(input))+1 != 3) {
+        	throw new IllegalArgumentException(errorMessage);
+        }
+        
+    	return true;
     }
 
 }
