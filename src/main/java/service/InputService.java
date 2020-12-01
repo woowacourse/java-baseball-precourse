1dc28fef41ce1141889b7d52e7289c5b0cd163f8
yoon-chaejin <kevin3st@naver.com>
2020-12-01 11:36:40 +0900

    feat : check the validity of the guess input
    Invalid Guess Input Type
    - Input length not 3
    - Input including characters other than 1~9

--- src/main/java/service/InputService.java
+++ src/main/java/service/InputService.java
@@ -10,6 +10,9 @@ public class InputService {
             try {
                 System.out.print("숫자를 입력해주세요 : ");
                 input = scanner.nextLine();
+
+                checkInputValidity(input);
+
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e);
@@ -19,5 +22,17 @@ public class InputService {
         }
 
         return input;
+    }
+
+    private static void checkInputValidity(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력값의 길이가 맞지 않습니다.(요구: 1에서 9까지의 수로 이루어진 3자리의 수)");
+        }
+
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) < '1' || input.charAt(i) > '9') {
+                throw new IllegalArgumentException("유효하지 않은 입력값입니다.(요구: 1에서 9까지의 수로 이루어진 3자리의 수)");
+            }
+        }
     }
 }
