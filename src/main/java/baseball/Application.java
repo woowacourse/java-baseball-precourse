eea99c3b3938c2e41616b1a3d33a60f0767afbd3
100-yeeun <100yeeun@gmail.com>
2020-12-01 19:21:35 +0900

    feature(baseball): add compareNumber, printHint
    두 숫자를 비교해 볼과 스트라이크 개수를 세는 함수 compareNumber 생성
    얻은 볼과 스트라이크 수를 출력하는 함수 compareNumber 생성
    
    Breaks checkValidation에서 idx 오류 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,11 +5,18 @@ import baseball.service.Number;
 import java.util.Scanner;
 
 public class Application {
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
         int[] randomNumber = Number.makeNumber();
+        int[] inputNumber = new int[Number.NUMBER_LENGTH];
         int input = 0;
+
+        for (int i = 0 ; i<Number.NUMBER_LENGTH; i++) {
+            System.out.printf(String.valueOf(randomNumber[i]));
+        }
 
         try {
             System.out.printf("숫자를 입력해주세요 : ");
@@ -19,9 +26,11 @@ public class Application {
         }
 
         try {
-            Number.checkValidation(input);
+            inputNumber = Number.checkValidation(input);
         } catch (IllegalArgumentException e) {
             System.out.println("1에서 9까지 서로 다른 임의의 수 3개의 숫자를 공백없이 입력해 주세요.");
         }
+
+        Number.printHint(randomNumber, inputNumber);
     }
 }
--- src/main/java/baseball/service/Number.java
+++ src/main/java/baseball/service/Number.java
@@ -15,11 +15,12 @@ public class Number {
         int numLength = 0;
 
         while (number > 0) {
-            int num = (number % 10);
-            if (usedNum[num] != 0) {
+            int idx = NUMBER_LENGTH - numLength - 1;
+            numArr[idx] = (number % 10);
+            if (usedNum[numArr[idx]] != 0) {
                 throw new IllegalArgumentException();
             }
-            usedNum[num] = 1;
+            usedNum[numArr[idx]] = 1;
             number /= 10;
             numLength++;
         }
@@ -45,5 +46,43 @@ public class Number {
         }
 
         return num;
+    }
+
+    public static int[] compareNumber(int answer, int[] number, int idx) {
+        int ballCnt = 0;
+        int strikeCnt = 0;
+
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (answer == number[i] && i == idx) {
+                strikeCnt++;
+            } else if (answer == number[i] && i != idx) {
+                ballCnt++;
+            }
+        }
+
+        return new int[]{strikeCnt, ballCnt};
+    }
+
+    public static int printHint(int[] answer, int[] number) {
+        int strikeCnt = 0;
+        int ballCnt = 0;
+
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            int[] score = compareNumber(answer[i], number, i);
+            strikeCnt += score[0];
+            ballCnt += score[1];
+        }
+
+        if (strikeCnt == 0 && ballCnt == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCnt == 0 && ballCnt != 0) {
+            System.out.println(ballCnt + "볼");
+        } else if (strikeCnt != 0 && ballCnt == 0) {
+            System.out.println(strikeCnt + "스트라이크");
+        } else {
+            System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
+        }
+
+        return strikeCnt;
     }
 }
