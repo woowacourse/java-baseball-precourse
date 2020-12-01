82f1d4cdefe4ddfa2a9188f810263c9a98c3307b
park seok chae <tjrco56@naver.com>
2020-12-01 17:39:40 +0900

    docs(utils): add comments to InputCheckUtils

--- src/main/java/utils/InputCheckUtils.java
+++ src/main/java/utils/InputCheckUtils.java
@@ -2,6 +2,13 @@ package utils;
 
 import java.util.HashSet;
 
+/**
+ * 입력값을 검사하는 util
+ * 기능 1. 입력값 중 중복된 값이 없는지 여부를 boolean으로 리턴
+ * 기능 2. 입력값이 숫자로만 이루어졌는지 여부를 boolean으로 리턴
+ * 기능 3. 입력값의 숫자들이 정해진 범위 내에 존재하는지 여부를 boolean으로 리턴
+ * 기능 4. 위의 조건들을 모두 충족하는지 여부를 boolean으로 리턴
+ */
 public class InputCheckUtils {
 	private static final Integer MAX_NUMBER_RANGE = 9;
 	private static final Integer MIN_NUMBER_RANGE = 1;
@@ -9,6 +16,7 @@ public class InputCheckUtils {
 	private InputCheckUtils() {
 	}
 	
+	/** 입력값이 모든 조건들을 충족하는지 여부를 boolean으로 리턴 */
 	public static boolean isVaild(String input) {
 		if (!isNumber(input)) {
 			return false;
@@ -22,6 +30,7 @@ public class InputCheckUtils {
 		return true;
 	}
 	
+	/** 입력값 중 중복된 값이 없는지 여부를 boolean으로 리턴 */
 	public static boolean isAllUniqueElement(String input) {
 		HashSet<Character> inputChracterSet = new HashSet<Character>();
 		for (char ch : input.toCharArray()) {
@@ -33,6 +42,7 @@ public class InputCheckUtils {
 		return true;
 	}
 
+	/** 입력값이 숫자로만 이루어졌는지 여부를 boolean으로 리턴 */
 	public static boolean isNumber(String input) {
 		if (isNull(input)) {
 			return false;
@@ -45,6 +55,7 @@ public class InputCheckUtils {
 		return true;
 	}
 
+	/** 입력값의 숫자들이 정해진 범위 내에 존재하는지 여부를 boolean으로 리턴 */
 	public static boolean isRightRange(String input) {
 		for (char ch : input.toCharArray()) {
 			int i = Character.getNumericValue(ch);
@@ -55,6 +66,7 @@ public class InputCheckUtils {
 		return true;
 	}
 
+	/** 입력값이 null인지 여부 boolean으로 리턴 */
 	public static boolean isNull(String input) {
 		if (input == null || input.isEmpty()) {
 			return true;
