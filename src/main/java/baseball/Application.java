0ec2aa17168f6e8aa3a747719b1aff4923ac4033
yunjung <cairofaith@gmail.com>
2020-12-01 13:35:41 +0900

    feat: start game and run when played
    - Create GamePlay object
    - Run game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+        GamePlay game = new GamePlay(scanner);
+        game.run();
     }
 }
