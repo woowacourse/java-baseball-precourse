42f555b3aa200bf16038760da89bbe675d5e1004
yunjung <cairofaith@gmail.com>
2020-12-01 14:37:14 +0900

    chore: erase unimportant comment

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
         GamePlay game = new GamePlay(scanner);
         game.run();
     }
