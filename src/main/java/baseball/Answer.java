548f5ae4bfa3d757f3bd9be0dd3f5ca9eec699d6
Solji Choi <bssj9307@gmail.com>
2020-12-01 14:04:04 +0900

    fix(Answer): 랜덤유틸 파라미터 잘못된 설정 변경

--- src/main/java/baseball/Answer.java
+++ src/main/java/baseball/Answer.java
@@ -6,7 +6,7 @@ import utils.RandomUtils;
 
 public class Answer {
     private static final int min = 1;
-    private static final int max = 1;
+    private static final int max = 9;
 
     private static final List<Integer> ans = new ArrayList<>();
 
