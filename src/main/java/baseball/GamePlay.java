9a4ebf6bd884227a065421a624a891118e228aba
yunjung <cairofaith@gmail.com>
2020-12-01 13:34:12 +0900

    feat: add Gameplay interface
    - Responsible of overall game flow

---
+++ src/main/java/baseball/GamePlay.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GamePlay {
+
+    private final Scanner scanner;
+    ArrayList<Integer> targetNumber;
+    TargetNumberGenerator targetNumberGenerator;
+    Player player;
+    Hint hint;
+
+    public GamePlay(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void run() {
+        do {
+            setTargetNumber();
+            setPlayer();
+            setHint(targetNumber);
+            playGame();
+        }
+        while(player.getIfContinue());
+    }
+
+    private void setTargetNumber() {
+        targetNumberGenerator = new TargetNumberGenerator();
+        this.targetNumber = targetNumberGenerator.getTargetNumber();
+        System.out.println(targetNumber);
+    }
+
+    private void setPlayer() {
+        this.player = new Player(scanner);
+    }
+
+    private void setHint(ArrayList<Integer> targetNum) {
+        this.hint = new Hint(targetNum);
+    }
+
+    private void playGame() {
+        boolean success = false;
+        ArrayList<Integer> playerNumber;
+        while(!success) {
+            playerNumber = player.getInputNumber();
+            String givenHint = hint.getHint(playerNumber);
+            System.out.println(givenHint);
+            success = hint.getResult();
+        }
+    }
+}
