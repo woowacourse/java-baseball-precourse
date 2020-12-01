4ff4c70440711a3bed3d6bf3df6a77a10e4ef9fc
park <msmn1729@gmail.com>
2020-12-01 23:00:25 +0900

    feat: 스크라이크, 볼, 낫싱 체크구현

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -2,27 +2,67 @@ package controller;
 
 import ganerator.NumberGenerator;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GameController {
     private static String scannerNumber = new String();
-
+    private static int[] targetNumber = new int[3];
+    private static int[] inputNumber = new int[3];
+    private static int strike, ball;
 
     /**
      * 게임을 시작하는 메소드
      */
     public static void start(Scanner scanner) {
-        System.out.print("숫자를 입력주세요 : ");
-        scanNumber(scanner);
+
         //타겟넘버 생성
         NumberGenerator numberGenerator = new NumberGenerator();
-        int[] targetNumber = numberGenerator.generatorNewNumber();
+        targetNumber = numberGenerator.generatorNewNumber();
+
+        System.out.print("정답: ");
         for (int i = 0; i < 3; i++) {
-            System.out.println(targetNumber[i]);
+            System.out.print(targetNumber[i]); //정답 테스트용
+        }
+        System.out.println();
+
+        boolean finish = false;
+        while (!finish) {
+            System.out.print("숫자를 입력주세요 : ");
+            scanNumber(scanner);
+            strike = ball = 0;
+            countStrike();
+            countBall();
+            if (ball > 0) {
+                System.out.println(ball + "볼 ");
+            }
+            if (strike > 0) {
+                System.out.println(strike + "스트라이크");
+            }
+            if (strike == 3) {
+                System.out.println("3 개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                finish = true;
+            }
         }
 
         System.out.println("결과"); //테스트용
+    }
+
+    /**
+     * 스트라이크 개수를 카운팅하는 메소드
+     */
+    public static void countStrike() {
+        for (int i = 0; i < 3; i++) {
+            if(targetNumber[i] == inputNumber[i]) strike++;
+        }
+    }
+
+    /**
+     * 볼의 개수를 카운팅하는 메소드
+     */
+    public static void countBall() {
+        if(targetNumber[0] == inputNumber[1] || targetNumber[0] == inputNumber[2]) ball++;
+        if(targetNumber[1] == inputNumber[2] || targetNumber[1] == inputNumber[0]) ball++;
+        if(targetNumber[2] == inputNumber[0] || targetNumber[2] == inputNumber[1]) ball++;
     }
 
     /**
@@ -72,15 +112,13 @@ public class GameController {
 
     /**
      * 정상입력된 문자열을 정수형 배열로 변환하는 메소드
+     *
      * @param scannerNumber
-     * @return
      */
-    public static int[] convertNumber(String scannerNumber) {
-        int[] inputNumber = new int[3];
+    public static void convertNumber(String scannerNumber) {
         for (int i = 0; i < 3; i++) {
             //각 숫자를 배열에 대입
             inputNumber[i] = Integer.valueOf(scannerNumber.charAt(i)) - '0';
         }
-        return inputNumber;
     }
 }
