159c867cdd4e307d58177de354213558d504192d
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-01 12:56:52 +0900

    refactor: InputView내의 공통 로직 분리
    - 서로 다른 메소드 간의 공통 로직을 분리해 getInputWithMessage(String) 메소드를 만들었습니다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,14 +3,14 @@ package baseball.view;
 import java.util.Scanner;
 
 /**
- * OutputView.java
+ * InputView.java
  * 입력과 출력이 섞인 로직을 담당하는 View 클래스
  *
  * @author Kimun Kim / kkm97351@gmail.com
  */
 
 public class InputView {
-    private static final String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
     private static final String GET_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String RETYPE_AGAIN = "값을 입력해주세요";
 
@@ -22,24 +22,24 @@ public class InputView {
     }
 
     public static String getNumbers() {
-        System.out.print(GET_NUMBERS_MESSAGE);
-        String userInput = deleteWhiteSpaces(scanner.nextLine());
-        if (!userInput.equals("")) {
-            return userInput;
-        }
-        System.out.println(RETYPE_AGAIN);
-        return getNumbers();
+        return getInputWithMessage(GET_NUMBERS_MESSAGE);
     }
 
     /* 재시작을 묻고 값을 받아 리턴하는 메소드 */
     public static String getRestartChoice() {
-        System.out.println(ASK_RESTART_MESSAGE);
+        return getInputWithMessage(ASK_RESTART_MESSAGE);
+    }
+
+    /* 파라미터로 받은 메세지와 함께 입력을 요구하며, 공백 입력에 대해 재입력을 요구합니다. */
+    public static String getInputWithMessage(String msg){
+        System.out.print(msg);
         String userInput = deleteWhiteSpaces(scanner.nextLine());
         if (!userInput.equals("")) {
             return userInput;
         }
         System.out.println(RETYPE_AGAIN);
-        return getRestartChoice();
+        return getInputWithMessage(msg);
+
     }
 
     /* 입력값에 존재하는 모든 공백을 제거 */
