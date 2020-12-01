ed202b0faa9c93474685afdb32d03df35eafb7a7
100-yeeun <100yeeun@gmail.com>
2020-12-01 15:35:20 +0900

    feat(baseball): add service.RandomNumber
    baseball 아래 service.RandomNumber 추가
    utils.RandomUtils를 이용해, 서로 다른 수로 이뤄진 랜덤한 3자리 숫자 생성
    만든 숫자를 배열로 저장해 반환
    
    Breaks service 패키지를 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import baseball.service.RandomNumber;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        int[] randomNumber = RandomNumber.makeNumber();
+
     }
 }
