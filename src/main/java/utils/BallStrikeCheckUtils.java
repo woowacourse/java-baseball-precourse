75e7b2f3e3f407bf70ebbb5b9c60d226b750ab74
park seok chae <tjrco56@naver.com>
2020-12-01 17:34:13 +0900

    docs(utils): add comment to BallStrikeCheckUtils

--- src/main/java/utils/BallStrikeCheckUtils.java
+++ src/main/java/utils/BallStrikeCheckUtils.java
@@ -3,13 +3,23 @@ package utils;
 import domain.Computer;
 import domain.User;
 
+/**
+ * 현재 저장된 숫자 리스트를 비교해 볼과 스트라이크 수를 리턴해주는 util
+ * 기능 1. 볼 갯수를 리턴
+ * 기능 2. 스트라이크 갯수를 리턴
+ */
 public class BallStrikeCheckUtils {
+	/** 최대로 생성할 수 있는 숫자 갯수를 한정하는 상수 */
 	private static final Integer MAX_BALL_CNT = 3;
+	
+	/** 초기값을 위한 상수 */
 	private static final Integer ZERO = 0;
 
+	/** 볼, 스트라이크 갯수를 체크할 두 객체 불러오기 */
 	private static Computer computer = Computer.getInstance();
 	private static User user = User.getInstance();
 
+	/** 볼 갯수를 리턴하는 메소드 */
 	public static int getBall() {
 		int ball = ZERO;
 		
@@ -25,6 +35,7 @@ public class BallStrikeCheckUtils {
 		return ball;
 	}
 
+	/** 스트라이크 갯수를 리턴하는 메소드 */
 	public static int getStrike() {
 		int strike = ZERO;
 		
