24101a23ef7a2e20b183b3870d869b158e636255
park seok chae <tjrco56@naver.com>
2020-12-01 17:59:49 +0900

    docs(domain): add comments to Player, User class

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -1,11 +1,16 @@
 package domain;
 
+/**
+ * 컴퓨터(상대방) 클래스
+ * 기능 1. 컴퓨터가 가진 컴퓨터 객체를 싱글톤 형태로 리턴
+ */
 public class Computer extends Player {
 	private static Computer computer = new Computer();
 
 	private Computer() {
 	}
 
+	/** 컴퓨터가 가진 컴퓨터 객체를 싱글톤 형태로 리턴 */
 	public static Computer getInstance() {
 		return computer;
 	}
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,11 +1,16 @@
 package domain;
 
+/**
+ * 유저(사용자) 클래스
+ * 기능 1. 유저가 가진 유저 객체를 싱글톤 형태로 리턴
+ */
 public class User extends Player {
 	private static User user = new User();
 
 	private User() {
 	}
-
+	
+	/** 유저가 가진 유저 객체를 싱글톤 형태로 리턴 */
 	public static User getInstance() {
 		return user;
 	}
