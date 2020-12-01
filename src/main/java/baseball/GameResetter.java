822225f320a57622f0d2f770df7a541d76d76eca
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-01 12:25:34 +0900

    docs: 기능 목록 수정

--- src/main/java/baseball/GameResetter.java
+++ src/main/java/baseball/GameResetter.java
@@ -6,6 +6,7 @@ public class GameResetter {
     private static final String FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String RESTART_FACTOR_ERROR_MESSAGE = "1 또는 2를 입력하세요.";
+
     private GameResetter() {
     }
 
