d2dccfe33241b819be3ec457229fcb04ad7e84f0
xorud2592 <xorud2592@gmail.com>
2020-12-01 18:12:42 +0900

    게임 종료시점 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,26 +5,44 @@ import java.util.Scanner;
 public class Application {
 
 	private static final String INPUT_GUIDE = "숫자를 입력해주세요 : ";
+	private static final String NOT_THING = "낫싱 ";
+	private static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String RESTART_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private static final String RESTART_GAME = "1";
+	private static final String END_GAME = "2";
+
 	private static int ball = 0;
 	private static int strike = 0;
-	
+
 	public static void main(String[] args) {
 		final Scanner scanner = new Scanner(System.in);
 		// TODO 援ы쁽 吏꾪뻾
-		threeStrike = false;
 
-		while (!threeStrike) {
-			System.out.print(INPUT_GUIDE);
-			input = scanner.nextLine();
+		boolean gameEnd = false;
+		boolean threeStrike;
+		String input;
 
-			int answer = Integer.parseInt(input);
+		while (!gameEnd) {
+			int question = makeQuestion();
 
-			threeStrike = checkNumber(answer, question);
+			threeStrike = false;
 
-			ball = 0;
-			strike = 0;
+			while (!threeStrike) {
+				System.out.print(INPUT_GUIDE);
+				input = scanner.nextLine();
+
+				int answer = Integer.parseInt(input);
+
+				threeStrike = checkNumber(answer, question);
+
+				ball = 0;
+				strike = 0;
+			}
+
+			System.out.println(RESTART_GUIDE);
+			String restartAnswer = scanner.nextLine();
+
 		}
-
 	}
 
 	static int makeQuestion() {
@@ -40,7 +58,7 @@ public class Application {
 
 		return question;
 	}
-	
+
 	static boolean makeCheck(int question, int number) {
 		while (question != 0) {
 			if (question % 10 == number) {
@@ -50,9 +68,8 @@ public class Application {
 		}
 		return true;
 	}
-	
-	static boolean checkNumber(int answer, int question)
-	{
+
+	static boolean checkNumber(int answer, int question) {
 		answerPickNumber(answer, question);
 
 		if (strike == 3) {
@@ -64,21 +81,19 @@ public class Application {
 			System.out.println(NOT_THING);
 			return false;
 		}
-		
+
 		System.out.println("볼 : " + ball + " 스타라이크 : " + strike);
 
 		return false;
 	}
-	
-	static void answerPickNumber(int answer, int question)
-	{
+
+	static void answerPickNumber(int answer, int question) {
 		for (int answerCount = 0; answerCount < 3; answerCount++) {
 			checkBS(answer % 10, answerCount, question);
 			answer /= 10;
 		}
 	}
-	
-	
+
 	static void checkBS(int answerTemp, int answerCount, int question) {
 		for (int questionCount = 0; questionCount < 3; questionCount++) {
 			int questionTemp = question % 10;
@@ -94,6 +109,5 @@ public class Application {
 			}
 		}
 	}
-	
-}
 
+}
