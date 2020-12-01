a74a65975ec60fbfca27509f5b361700f4786fb0
park <msmn1729@gmail.com>
2020-12-01 20:08:33 +0900

    test: 난수 발생 정상작동여부 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,18 @@
 package baseball;
 
+import ganerator.NumberGenerator;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        NumberGenerator numberGenerator = new NumberGenerator();
+        ArrayList<Integer> newNumber = new ArrayList<>(3); //세자리 수
+        newNumber = numberGenerator.generatorNewNumber();
+        for (Integer i : newNumber) {
+            System.out.println(i);
+        }
     }
 }
