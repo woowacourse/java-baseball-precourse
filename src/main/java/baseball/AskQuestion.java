ec3ee7d175c4193091af0ff2b8869ba0942d4a6d
JinseongHwang <eddy5360@naver.com>
2020-12-01 21:33:32 +0900

    feat: add exception handling for invalid inputs

--- src/main/java/baseball/AskQuestion.java
+++ src/main/java/baseball/AskQuestion.java
@@ -2,18 +2,27 @@ package baseball;
 
 public class AskQuestion {
 
-    public static final int GO = 1;
-    public static final int STOP = 2;
+    public static final String GO = "1";
+    public static final String STOP = "2";
 
     private boolean keepGoing = true;
+    private boolean isValidInput = false;
 
     public boolean getKeepGoing() {
         return keepGoing;
     }
 
+    public boolean getIsVaildInput() {
+        return isValidInput;
+    }
+
     // if keepGoing has false value, stop playing game
-    public void setFalseKeepGoing() {
-        keepGoing = false;
+    public void setKeepGoing(boolean input) {
+        keepGoing = input;
+    }
+
+    public void setIsValidInput(boolean input) {
+        isValidInput = input;
     }
 
     public void ask() {
@@ -21,14 +30,17 @@ public class AskQuestion {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public void askResponse(GameData gameData, int input) {
-        if (input == GO) {
+    public void askResponse(GameData gameData, String input) {
+        if (input.equals(GO)) {
+            setIsValidInput(true);
             // if start a game again, reset random values
             gameData.selectRandomNumber();
-        } else {
-            setFalseKeepGoing();
+        } else if (input.equals(STOP)) {
+            setIsValidInput(true);
+            setKeepGoing(false);
             System.out.println("게임을 종료하겠습니다.");
+        } else {
+            System.out.println("유효하지 않은 입력입니다. 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         }
     }
-
 }
