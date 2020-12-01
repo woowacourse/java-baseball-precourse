98f56a8d281588d02948285f66cc63cdf3b0fbe8
Solji Choi <bssj9307@gmail.com>
2020-12-01 14:02:55 +0900

    feat(main): 게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        new Game(scanner);
+		// TODO 구현 진행
+        Game.startGame();
     }
 }
