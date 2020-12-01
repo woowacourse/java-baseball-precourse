8ab33b76c38ab2974b8d228c98c85612e1fcca57
yoon-chaejin <kevin3st@naver.com>
2020-12-01 22:55:37 +0900

    style : remove unnecessary comment

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,7 @@ import service.InputService;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
         do {
             GameService.start(scanner);
         } while (InputService.resume(scanner));
