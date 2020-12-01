1e9e295cf9b1359213e8da23b0129c95283b6bea
park seok chae <tjrco56@naver.com>
2020-12-01 17:46:23 +0900

    feat(utils): add isRightLength() method in InputCheckUtils and change order of methods

--- src/main/java/utils/InputCheckUtils.java
+++ src/main/java/utils/InputCheckUtils.java
@@ -10,8 +10,12 @@ import java.util.HashSet;
  * 기능 4. 위의 조건들을 모두 충족하는지 여부를 boolean으로 리턴
  */
 public class InputCheckUtils {
+	/** 입력값의 숫자의 범위를 한정하는 상수 */
 	private static final Integer MAX_NUMBER_RANGE = 9;
 	private static final Integer MIN_NUMBER_RANGE = 1;
+	
+	/** 입력값의 길이 체크를 위한 상수 */
+	private static final Integer INPUT_LENGTH = 3;
 	
 	private InputCheckUtils() {
 	}
@@ -26,6 +30,33 @@ public class InputCheckUtils {
 		}
 		if (!isAllUniqueElement(input)) {
 			return false;
+		}
+		if (!isRightLength(input)) {
+			return false;
+		}
+		return true;
+	}
+	
+	/** 입력값이 숫자로만 이루어졌는지 여부를 boolean으로 리턴 */
+	public static boolean isNumber(String input) {
+		if (isNull(input)) {
+			return false;
+		}
+		try {
+			Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	/** 입력값의 숫자들이 정해진 범위 내에 존재하는지 여부를 boolean으로 리턴 */
+	public static boolean isRightRange(String input) {
+		for (char ch : input.toCharArray()) {
+			int i = Character.getNumericValue(ch);
+			if (i > MAX_NUMBER_RANGE || i < MIN_NUMBER_RANGE) {
+				return false;
+			}
 		}
 		return true;
 	}
@@ -42,26 +73,10 @@ public class InputCheckUtils {
 		return true;
 	}
 
-	/** 입력값이 숫자로만 이루어졌는지 여부를 boolean으로 리턴 */
-	public static boolean isNumber(String input) {
-		if (isNull(input)) {
+	/** 입력값이 정해진 길이로 입력되었는지 여부를 boolean으로 리턴 */
+	public static boolean isRightLength(String input) {
+		if(input.length() != INPUT_LENGTH) {
 			return false;
-		}
-		try {
-			Integer.parseInt(input);
-		} catch (NumberFormatException e) {
-			return false;
-		}
-		return true;
-	}
-
-	/** 입력값의 숫자들이 정해진 범위 내에 존재하는지 여부를 boolean으로 리턴 */
-	public static boolean isRightRange(String input) {
-		for (char ch : input.toCharArray()) {
-			int i = Character.getNumericValue(ch);
-			if (i > MAX_NUMBER_RANGE || i < MIN_NUMBER_RANGE) {
-				return false;
-			}
 		}
 		return true;
 	}
