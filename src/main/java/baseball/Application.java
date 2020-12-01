2455497a41c404140e17ee4be1491f6532cc99c8
xorud2592 <xorud2592@gmail.com>
2020-12-01 17:59:31 +0900

    1~9 숫자 3개 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,22 @@ package baseball;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-    }
+
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		// TODO 구현 진행
+		
+		System.out.println(makeQuestion());
+	}
+
+	static int makeQuestion() {
+		int max = 0;
+		int question = 0;
+		while (max != 3) {
+			int number = RandomUtils.nextInt(1, 9);
+			question = question * 10 + number;
+			max++;
+		}
+		return question;
+	}
 }
