9a56d613abb894751d5271a49c52672d124eb6d3
LeeJuHyuk <dlwngur1535@gmail.com>
2020-12-01 19:45:08 +0900

    refactor: 기본 생성자 추가

--- src/main/java/baseball/view/IllegalArgumentExceptionView.java
+++ src/main/java/baseball/view/IllegalArgumentExceptionView.java
@@ -4,4 +4,7 @@ public class IllegalArgumentExceptionView {
     public static final String INPUT_NOT_DUPLICATED_THREE_DIGIT_NUMBERS = "중복 되지 않는 3자리 수를 입력해주세요.";
     public static final String INPUT_OUT_OF_RANGE_NUMBER = "입력 숫자는 1~9 사이의 숫자만 허용됩니다.";
     public static final String PLEASE_INPUT_ONE_OR_TOW = "1 또는 2를 입력해주세요.";
+
+    private IllegalArgumentExceptionView() {
+    }
 }
