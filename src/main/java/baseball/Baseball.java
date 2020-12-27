8ea7075bc341c92ad76b82555cf3d4ba65cfdb3d
outstandingboy <outstanding1301@gmail.com>
2020-12-27 22:59:51 +0900

    add(Baseball) - 정답을 담는 객체

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,4 +1,38 @@
 package baseball;
 
+import utils.RandomUtils;
+
 public class Baseball {
+    private int[] numbers = {0, 0, 0}; // 정답
+
+    public Baseball() {
+        rand();
+    }
+
+    public int numberAt(int index) {
+        return numbers[index];
+    }
+
+    private boolean contains(int num) {
+        for (int number : numbers) {
+            if (number == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void setNumber(int index) {
+        int num = RandomUtils.nextInt(1, 9);
+        while (contains(num)) {
+            num = RandomUtils.nextInt(1, 9);
+        }
+        numbers[index] = num;
+    }
+
+    public void rand() {
+        for (int i=0; i<3; i++) {
+            setNumber(i);
+        }
+    }
 }
