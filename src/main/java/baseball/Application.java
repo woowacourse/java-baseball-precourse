d2287257b697d215ccafb1f02fb997680a63a5e1
outstandingboy <outstanding1301@gmail.com>
2020-12-27 23:02:57 +0900

    fix(Application) - 게임 시작 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,12 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        BaseballIO io = new BaseballIO(scanner);
+        Baseball baseball = new Baseball();
+        BaseballController controller = new BaseballController(baseball, io);
+
+        while (controller.isPlaying()) {
+            controller.doGame();
+        }
     }
 }
