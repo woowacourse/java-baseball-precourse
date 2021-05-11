6b3987a745a2e6a43ab452ec638dcd33f07e6b18
Kim TaeWon <kimtae9217@gmail.com>
2021-05-11 17:14:58 +0900

    java-baseball fork후 커밋

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+
     }
 }
