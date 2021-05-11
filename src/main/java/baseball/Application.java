d7e9aab5c0533deac1b219a767b57294edf482c1
Kim TaeWon <kimtae9217@gmail.com>
2021-05-11 22:26:14 +0900

    feat(baseball): 컴퓨터 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,16 +6,8 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-//        NumberGenerator generator = new NumberGenerator();
-//        List<Integer> numbers = generator.createRandomNumbers();
-//        System.out.println(numbers);
-
-        Judgement judgement = new Judgement();
-//        final int count = judgement.correctCount(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
-//        System.out.println(count);
-
-        final boolean place = judgement.hasPlace(Arrays.asList(7, 8, 9), 0, 7); // 첫 번째(0)가 7이냐?
-        System.out.println(place); // true
+        final Referee referee = new Referee();
+        final String result = referee.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        System.out.println(result); // 3 스트라이크
     }
 }
--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class Referee {
     public String compare(List<Integer> computer, List<Integer> player) {
-        return "3 스트라이크";
+        //  몇 개의 숫자가 같은지 알아낸 뒤
+        // 스트라이크의 개수를 구해 뺀다.
+        // 남은 개수는 볼의 개수이다.
+
+        final Judgement judgement = new Judgement();
+        int correctCount = judgement.correctCount(computer, player);
+
+        int strike = 0;
+        for(int placeIndex = 0; placeIndex < player.size(); placeIndex++){
+            if (judgement.hasPlace(computer, placeIndex, player.get(placeIndex))){
+                strike++;
+            }
+        }
+        int ball = correctCount - strike;
+        return ball + " 볼 " + strike + " 스트라이크 ";
     }
 }
