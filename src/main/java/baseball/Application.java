3f4defc2357ff180488fb80819465ad0cb113348
Consome1 <78946348+Consome1@users.noreply.github.com>
2021-11-15 15:22:27 +0900

    input function added

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,21 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		int answer = RandomUtils.nextInt(100, 1000);
+		boolean isFinish = false;
+		String input;
+
+		while (isFinish == false) {
+			System.out.println("숫자를 입력해주세요 : ");
+			input = scanner.nextLine();
+			System.out.println(input);
+
+		}
+
+	}
 }
--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -3,24 +3,25 @@ package utils;
 import java.util.Random;
 
 public class RandomUtils {
-    private static final Random RANDOM = new Random();
+	private static final Random RANDOM = new Random(); // static이 붙었으므로 클래스 변수
 
-    private RandomUtils() {
-    }
+	private RandomUtils() {
+	}
 
-    public static int nextInt(final int startInclusive, final int endInclusive) {
-        if (startInclusive > endInclusive) {
-            throw new IllegalArgumentException();
-        }
+	public static int nextInt(final int startInclusive, final int endInclusive) {
+		if (startInclusive > endInclusive) {
+			throw new IllegalArgumentException();
+		}
 
-        if (startInclusive < 0) {
-            throw new IllegalArgumentException();
-        }
+		if (startInclusive < 0) {
+			throw new IllegalArgumentException();
+		}
 
-        if (startInclusive == endInclusive) {
-            return startInclusive;
-        }
+		if (startInclusive == endInclusive) {
+			return startInclusive;
+		}
 
-        return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
-    }
+		return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1);
+	}
+
 }
