73a020f031e163aa83c5e1bae0ae8f38ba520283
LeeDongGyu <woowahanCU@woowahan.com>
2021-11-23 16:15:16 +0900

    chore: remove unnecessary code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,7 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Application {
     public static void main(String[] args) {
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
-        System.out.println(Randoms.pickNumberInRange(1, 9));
+        //TODO: 숫자 야구 게임 구현
     }
 }
