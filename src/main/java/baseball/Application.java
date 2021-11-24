27b406dc89836b3278eadc5d4a5d75670ac391dc
DK <ldk980130@gmail.com>
2021-11-25 00:08:06 +0900

    feat : 정답이 나올 때까지 반복 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,21 +1,52 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import random.Random;
 
 public class Application {
-    public static void main(String[] args) {
 
+	public static void main(String[] args) {
 
-    }
+	}
 
-    static void verifyInput(String input) {
+	private static void proceed(String answer) {
+		
+		while (true) {
 
-        if (!input.matches("-?\\d+")) {
-            throw new IllegalArgumentException("정수를 입력해야 합니다.");
-        }
+			System.out.print(Message.QUESTION);
 
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("세 자리 수를 입력해야 합니다.");
-        }
-    }
+			String input = Console.readLine();
+
+			verifyInput(input);
+
+			String resultMessage = Game.play(answer, input);
+
+			System.out.println(resultMessage);
+
+			if (resultMessage.equals(3+Message.STRIKE)) {
+
+				System.out.println(Message.CLEAR);
+
+				break;
+			}
+		}
+	}
+
+	static void verifyInput(int input) {
+
+		if (input != 1 && input != 2) {
+			throw new IllegalArgumentException("1과 2 중에서 선택해야 합니다.");
+		}
+	}
+
+	static void verifyInput(String input) {
+
+		if (!input.matches("-?\\d+")) {
+			throw new IllegalArgumentException("정수를 입력해야 합니다.");
+		}
+
+		if (input.length() != 3) {
+			throw new IllegalArgumentException("세 자리 수를 입력해야 합니다.");
+		}
+	}
 }
