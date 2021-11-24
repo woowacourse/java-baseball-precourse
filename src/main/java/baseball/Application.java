8116448816da53a78e36567b169607e071efbd43
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-24 20:59:55 +0900

    feat : Add Application main method to run the program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,25 @@
 package baseball;
 
+import static util.GameConstant.*;
+
 public class Application {
     public static void main(String[] args) {
-
-
+        GamePlayer gamePlayer = new GamePlayer();
+        GameProvider gameProvider = new GameProvider();
+        while (gameProvider.getGameStatus().equals(ON_GOING)){
+            gameProvider.generateAnswer();
+            gamePlayer.writeAnswer();
+            GameScore gameScore = gameProvider.checkAnswer(gamePlayer.getPlayerAnswer());
+            gameScore.printResult();
+            if (gameProvider.getGameStatus().equals(CORRECT)){
+                printGuideMessage(THREE_STRIKE);
+                printGuideMessage(ASK_RESTART_OR_STOP);
+                gameProvider.askRestartOrStop();
+            }
+        }
+        printGuideMessage(FINISH_GAME);
+    }
+    private static void printGuideMessage(String message){
+        System.out.println(message);
     }
 }
