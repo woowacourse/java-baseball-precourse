79e5cb2ec41da6946c9b06795177690a186d6fab
0chil <0@chll.it>
2021-11-24 23:04:34 +0900

    feat: 사용자 숫자입력 힌트 출력 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+import static baseball.constants.GameConstants.Hint.*;
+
+public class OutputView {
+	public static void printInputHint() {
+		System.out.print(HINT_ASK_INPUT.getString());
+	}
+}
