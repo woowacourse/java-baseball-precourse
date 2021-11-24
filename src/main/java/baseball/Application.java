f7ac0d1c814b2840c3d2ee49998f57a93d1da784
wilgur513 <wilgur513@naver.com>
2021-11-24 23:48:34 +0900

    feat: implement Application.main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,35 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		GameManager gameManager = createGameManager();
+		run(gameManager);
+	}
+
+	private static GameManager createGameManager() {
+		Display display = new Display();
+		BaseballNumberValidator validator = new BaseballNumberValidator();
+		BaseballNumberGenerator generator = new BaseballNumberGenerator(validator);
+		BaseballNumberReferee referee = new BaseballNumberReferee(validator);
+		JudgeResultFormatter formatter = new JudgeResultFormatter();
+		return new GameManager(display, generator, referee, formatter);
+	}
+
+	private static void run(GameManager gameManager) {
+		gameManager.startGame();
+
+		while(!gameManager.isGameFinished()) {
+			handleInput(gameManager);
+		}
+	}
+
+	private static void handleInput(GameManager gameManager) {
+		gameManager.handleGuessNumber(Console.readLine());
+
+		if(gameManager.isGameFinished()) {
+			gameManager.handleOptionNumber(Console.readLine());
+		}
+	}
 }
