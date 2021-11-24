4325c5fbe61394ff4829bde274845418b1eab872
Hyunta <mohai2618@naver.com>
2021-11-24 20:15:17 +0900

    feat : 숫자 입력 요청 문구 출력

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballGame {
+    private RandomNumberGenerator randomNumberGenerator;
+
+
+    public void start() {
+        do {
+            int[] answer = randomNumberGenerator.makeRandomNumber();
+            playGame(answer);
+        } while (true);//TODO 재시작 여부
+
+
+    }
+
+    private void playGame(int[] answer) {
+        while (true) {
+            System.out.print(Message.ENTER_NUMBER);
+
+        }
+
+    }
+
+}
---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,5 @@
+package baseball;
+
+public class Message {
+    static final String ENTER_NUMBER = "숫자를 입력해주세요 : ";
+}
