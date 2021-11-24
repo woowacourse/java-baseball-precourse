699d5f716874c052960361cccf017569ebc43bd7
sun <syoun602@gmail.com>
2021-11-24 17:08:16 +0900

    feat(startgame): add startGame method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,8 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.startGame();
     }
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class BaseballGame {
+
+    private int computerNumber;
+
+    public void startGame() {
+        setComputerNumber();
+        // getPlayerNumber();
+        // verifyNumber();
+        // getResult();
+        // continueGame() using do..while
+    }
+
+    public void setComputerNumber() {
+
+    }
+}
