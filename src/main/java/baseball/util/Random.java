d334ea70f0040bf8739f9749e9c4a0142a7adc69
jaeyoungjang2 <lion85488@gmail.com>
2021-11-24 17:17:11 +0900

    feat: 컴퓨터에서 3자리 랜덤 숫자 추출

--- src/main/java/baseball/util/Random.java
+++ src/main/java/baseball/util/Random.java
@@ -1,2 +1,14 @@
-package baseball.util;public class Random {
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class Random {
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
+    private static final int COUNT = 3;
+
+    public static List<Integer> extractNumbers() {
+        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+    }
 }
