e1f3e453306c25028ea392393677c478b8ca2687
jaeyoungjang2 <lion85488@gmail.com>
2021-11-24 16:57:19 +0900

    test: 랜덤 숫자 뽑기 테스트 한자리 숫자를 뽑아본다.

---
+++ src/test/java/baseball/RandomTest.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RandomTest {
+    @DisplayName("랜덤 사용법 테스트, 한자리 숫자를 뽑아본다.")
+    @Test
+    void randomNumberTest() {
+        List<Integer> numberList = new ArrayList<>(Arrays.asList(1));
+        int randomNumber = Randoms.pickNumberInList(numberList);
+        assertThat(randomNumber).isEqualTo(1);
+    }
+
+
+}
