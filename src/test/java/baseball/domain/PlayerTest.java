030881d7bcad15074ad26f03e7fc6e1f9f459265
0chil <0@chll.it>
2021-11-24 20:58:07 +0900

    feat: 사용자 입력 삽입 구현
    검증 구현

---
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import baseball.utils.ConversionUtils;
+
+class PlayerTest {
+	@Test
+	void 사용자_수_입력(){
+		// given, when
+		Player player = new Player();
+		String userInputNumber = "123";
+		List<Integer> userInputNumberList = ConversionUtils.userInputStringToIntegerList(userInputNumber);
+		player.setNumberList(userInputNumberList);
+
+		// then
+		Assertions.assertThat(player.getNumberList()).isEqualTo(Arrays.asList(1,2,3));
+	}
+
+}
---
+++ src/test/java/baseball/utils/ValidationUtilsTest.java
@@ -0,0 +1,73 @@
+package baseball.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ValidationUtilsTest {
+
+	@Test
+	void 난수_중복() {
+		// given, when
+		List<Integer> randomNumberList = Arrays.asList(1, 1, 2);
+
+		// then
+		Assertions.assertThat(ValidationUtils.validateRandomDigits(randomNumberList)).isFalse();
+	}
+
+	@Test
+	void 난수_길이_4() {
+		// given, when
+		List<Integer> randomNumberList = Arrays.asList(1, 2, 3, 4);
+
+		// then
+		Assertions.assertThat(ValidationUtils.validateRandomDigitsLength(randomNumberList)).isFalse();
+	}
+
+	@Test
+	void 유저_숫자_길이_4() {
+		// given, when
+		String userInputNumberString = "1234";
+
+		// then
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+			ValidationUtils.validateUserInputDigitsLength(userInputNumberString);
+		});
+	}
+
+	@Test
+	void 유저_입력_0() {
+		// given, when
+		String userInputNumberString = "120";
+
+		// then
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+			ValidationUtils.validateUserInputDigitsInRange(userInputNumberString);
+		});
+	}
+
+	@Test
+	void 유저_입력_문자() {
+		// given, when
+		String userInputNumberString = "a12";
+
+		// then
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+			ValidationUtils.validateUserInputDigitsInRange(userInputNumberString);
+		});
+	}
+
+	@Test
+	void 유저_입력_중복() {
+		// given, when
+		String userInputNumberString = "112";
+		List<Integer> userInputNumberList = ConversionUtils.userInputStringToIntegerList(userInputNumberString);
+
+		// then
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+			ValidationUtils.validateUserInputDigitListDuplicate(userInputNumberList);
+		});
+	}
+}
