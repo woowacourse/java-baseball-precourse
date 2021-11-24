2a35e0371d10ba547d6e35713f615769976d8318
최창준 <aowert6518@gmail.com>
2021-11-24 17:04:26 +0900

    [feat] 사용자에게 숫자 입력받기 - domain/Controller class 안에 내장함수 getUserNumber를 만듬 - 요구사항의 Console.readLine으로 사용자 입력값을 받아옴 - 우선 숫자라고 가정하고 parseInt를 통해 숫자로 변환

--- src/main/java/baseball/domain/Controller.java
+++ src/main/java/baseball/domain/Controller.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Controller {
@@ -8,4 +9,9 @@ public class Controller {
     private Integer createRandomNumber() {
         return Randoms.pickNumberInRange(100, 1000);
     }
+
+    private Integer getUserNumber() {
+        return Integer.parseInt(Console.readLine());
+    }
+
 }
