c9d2c46a86fc2879d91ccd9520217877d4493aa9
Hyunta <mohai2618@naver.com>
2021-11-24 19:43:33 +0900

    initial commit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
