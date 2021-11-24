e6f8c7f7569cc38718ad07751053be965339f3dd
jaeyoungjang2 <lion85488@gmail.com>
2021-11-24 17:16:48 +0900

    feat: 컨트롤러 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.baseballGameController;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        baseballGameController.run();
     }
 }
