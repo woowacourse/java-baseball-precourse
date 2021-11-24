3a24c78e7c521c94e9ad6e437063cf45b0507d2f
nailseong <nnis9857@gmail.com>
2021-11-25 07:37:28 +0900

    feat: 사용자에게 입력받은 값을 검증

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,11 +2,13 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 
 public class Game {
 
     private static final int NUMBER_LENGTH = 3;
+    private static final String INPUT_CHECK_REGEX = "[1-9][1-9][1-9]";
     private int randomNumber;
 
     public void init() {
@@ -23,6 +25,23 @@ public class Game {
         }
 
         randomNumber = Integer.parseInt(stringBuilder.toString());
+    }
+
+    private boolean isValid(String input) {
+        if (!input.matches(INPUT_CHECK_REGEX)) {
+            return false;
+        }
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            hashSet.add(input.charAt(i) - '0');
+        }
+
+        if (hashSet.size() != NUMBER_LENGTH) {
+            return false;
+        }
+
+        return true;
     }
 
 }
