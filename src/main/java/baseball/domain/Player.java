eaf3ee74231eb4a0af86ed6c57d4c15a83c9673b
0chil <0@chll.it>
2021-11-24 20:58:20 +0900

    feat: 사용자 입력 삽입 구현
    검증 구현

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Player {
+	private List<Integer> numberList;
+
+	public void setNumberList(List<Integer> numberList) {
+		this.numberList = numberList;
+	}
+
+	public List<Integer> getNumberList() {
+		return numberList;
+	}
+}
--- src/main/java/baseball/utils/ValidationUtils.java
+++ src/main/java/baseball/utils/ValidationUtils.java
@@ -6,8 +6,6 @@ import static baseball.constants.GameConstants.Message.*;
 
 import java.util.List;
 
-import baseball.constants.GameConstants;
-
 public class ValidationUtils {
 	public static boolean validateRandomDigits(List<Integer> numberList) {
 		boolean[] duplicateCheck = new boolean[END_INCLUSIVE.getNumber() + 1];
@@ -22,6 +20,30 @@ public class ValidationUtils {
 	}
 
 	public static boolean validateRandomDigitsLength(List<Integer> numberList) {
-		return numberList.size() == GameConstants.RandomDigit.LENGTH.getNumber();
+		return numberList.size() == LENGTH.getNumber();
+	}
+
+	public static void validateUserInputDigitsLength(String userInputString) {
+		if (userInputString.length() != LENGTH.getNumber())
+			throw new IllegalArgumentException(ERR_IMPROPER_LENGTH.getMessage());
+	}
+
+	public static void validateUserInputDigitsInRange(String userInputString) {
+		for (int i = 0; i < userInputString.length(); i++) {
+			int userInputNumber = Character.getNumericValue(userInputString.charAt(i));
+			if (START_INCLUSIVE.getNumber() > userInputNumber || END_INCLUSIVE.getNumber() < userInputNumber) {
+				throw new IllegalArgumentException(ERR_NOT_IN_RANGE.getMessage());
+			}
+		}
+	}
+
+	public static void validateUserInputDigitListDuplicate(List<Integer> numberList) {
+		boolean[] duplicateCheck = new boolean[END_INCLUSIVE.getNumber() + 1];
+
+		for (Integer number : numberList) {
+			if (duplicateCheck[number])
+				throw new IllegalArgumentException(ERR_DUPLICATE_EXISTS.getMessage());
+			duplicateCheck[number] = true;
+		}
 	}
 }
