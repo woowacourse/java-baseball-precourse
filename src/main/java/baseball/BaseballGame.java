8f22c6a2de9591de454f82cce39c7909b40f2f67
sun <syoun602@gmail.com>
2021-11-24 20:30:50 +0900

    feat(scannumber): scan player number

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,21 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseballGame {
 
     private RandomNumberGenerator randomNumberGenerator;
+    private Player player;
+    private String playerNumber;
+    private String computerNumber;
 
     public void startGame() {
         setComputer();
-        // getPlayer();
+        getPlayer();
         // verifyNumber();
         // getResult();
-        // continueGame() using do..while
+    }
+
+    private void getPlayer() {
+        Player player = new Player();
+        playerNumber = player.scanNumber();
     }
 
     public void setComputer() {
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-        String computer = randomNumberGenerator.generateNumber();
+        computerNumber = randomNumberGenerator.generateNumber();
 
-        System.out.println(computer);
+        System.out.println("COMPNUM = " + computerNumber);
     }
 }
---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+    public String scanNumber() {
+        String number = Console.readLine();
+
+        return number;
+    }
+}
