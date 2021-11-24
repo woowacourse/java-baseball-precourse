3490e26b84d375ea29be1046cd1abb1811394db3
0chil <0@chll.it>
2021-11-24 23:37:09 +0900

    feat: 게임 반복 진행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import static baseball.constants.GameConstants.GameOverInput.*;
+
+import baseball.domain.Computer;
+import baseball.domain.Game;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        runGameLoop();
+    }
+
+    private static void runGameLoop(){
+        Game game = new Game();
+        while(true){
+            int userGameSelection = game.runGameOnce();
+            if(userGameSelection == RESTART.getNumber())
+                game.setComputer(new Computer());
+            if(userGameSelection == STOP.getNumber())
+                break;
+        }
     }
 }
