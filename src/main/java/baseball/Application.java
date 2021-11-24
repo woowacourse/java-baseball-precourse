0d8c5e60e5ecb910294dda561db16a0dffdb69cd
KIM <km0192@naver.com>
2021-11-24 19:56:41 +0900

    [ADD] README.md, main method 1. README.md에 기능 구현사항 추가 2. main method에 기능 구현(숫자 생성 직후 입력받기 실행) 3. 숫자를 만드는 Make, 입력받는 Input, 출력하는 PrintOut, 확인하는 Check 클래스 추가(빈상태) 	수정함:        README.md 	수정함:        src/main/java/baseball/Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,33 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        Make.init3DiffDigits();
+        Input.run();
     }
+}
+
+class Make{
+    static String ThreeDifferentDigits;
+
+    static void init3DiffDigits(){
+        int[] NumberList = {1,2,3,4,5,6,7,8,9};
+    }
+
+    String get3DiffStr(){
+        return ThreeDifferentDigits;
+    }
+}
+
+class Input{
+    static void run(){
+
+    }
+}
+
+class PrintOut{
+
+}
+
+class Check{
+
 }
