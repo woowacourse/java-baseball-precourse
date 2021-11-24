1f66f52b8b7fe364447d9499d6c8424e23c8a2fa
유영우 <fbduddn97@naver.com>
2021-11-25 01:26:22 +0900

    [refactor]: java 중괄호 관련 convention 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,18 +7,22 @@ public class Application {
 
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-
+        //TODO: BaseBall class 로 빼기
         boolean start = true;
         // 재실행 여부
         while(start){
             // 1. 타겟 넘버 생성
             randomAns = createTargetNum();
+
+            //TODO: 최종 제출 때 빼기
             System.out.println(randomAns[0]+""+randomAns[1]+""+randomAns[2]);
 
             // 2. 유저에게 입력 받기
             // 3. 유저에게 결과 알려주기
             boolean ansCheck = false;
-            while(!ansCheck) ansCheck = compareToRandomAns(userInputNum());
+            while(!ansCheck){
+                ansCheck = compareToRandomAns(userInputNum());
+            }
             start = checkRestart();
         }
     }
@@ -59,8 +63,11 @@ public class Application {
         if(inputStr.length()<3 || inputStr.length()>3) throw new IllegalArgumentException();
 
         // 숫자로 이루어지지 않았을 경우, 0이 포함 되어 있을 경우
+        // TODO: 같은 숫자가 있는 경우
         for(int i=0; i<3; i++){
-            if(inputStr.charAt(i)=='0' || !Character.isDigit(inputStr.charAt(i))) throw new IllegalArgumentException();
+            if(inputStr.charAt(i)=='0' || !Character.isDigit(inputStr.charAt(i))){
+                throw new IllegalArgumentException();
+            }
             userInputNumArr[i] = inputStr.charAt(i)-'0';
         }
 
@@ -76,6 +83,8 @@ public class Application {
         // 타겟넘버 와 유저인풋넘버 비교
         for(int i=0; i<3; i++){
             int num = userInputNumArr[i];
+
+            //TODO: depth 넘어간 것 수정
             for(int j=0; j<3; j++){
                if(num==randomAns[j]){
                    if(i==j){
@@ -99,11 +108,19 @@ public class Application {
             return true;
         }
 
-        if(strike>0 && ball>0) System.out.println(ball+"볼"+" "+strike+"스트라이크");
+        if(strike>0 && ball>0){
+            System.out.println(ball+"볼"+" "+strike+"스트라이크");
+        }
         else{
-            if(strike==0 && ball==0) System.out.println("낫싱");
-            else if(ball==0) System.out.println(strike+"스트라이크");
-            else System.out.println(ball+"볼");
+            if(strike==0 && ball==0){
+                System.out.println("낫싱");
+            }
+            else if(ball==0){
+                System.out.println(strike+"스트라이크");
+            }
+            else{
+                System.out.println(ball+"볼");
+            }
         }
 
         return false;
@@ -113,8 +130,14 @@ public class Application {
     static boolean checkRestart(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userRestartInput = Console.readLine();
-        if(userRestartInput.equals("1")) return true;
-        else if(userRestartInput.equals("2")) return false;
-        else throw new IllegalArgumentException();
+        if(userRestartInput.equals("1")){
+            return true;
+        }
+        else if(userRestartInput.equals("2")){
+            return false;
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
     }
 }
