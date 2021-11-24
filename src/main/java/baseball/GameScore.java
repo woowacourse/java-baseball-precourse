79288c68565a2dd2ef19e2237e61a367dc6974d3
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-24 18:01:38 +0900

    feat(GameScore) : Add GameScore

---
+++ src/main/java/baseball/GameScore.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import static util.GameConstant.*;
+
+class GameScore {
+
+    private int strike;
+    private int ball;
+
+    public GameScore() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    public void addStrike(){
+        strike++;
+    }
+
+    public void addBall(){
+        ball++;
+    }
+
+    public void printResult(){
+        if(strike > 0 && ball > 0){
+            System.out.println(ball + BALL + " " + strike + STRIKE);
+        }else if(strike > 0){
+            System.out.println(strike + STRIKE);
+        }else if(ball > 0){
+            System.out.println(ball + BALL);
+        }else{
+            System.out.println(NOTHING);
+        }
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
