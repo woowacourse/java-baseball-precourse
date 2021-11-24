f959324b9973b534b327a29a7d9a0b1771b07fc7
Yunkeun <yunx2@naver.com>
2021-11-24 20:27:01 +0900

    feat: 유저가 세 자리 수를 입력하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        GameController.controlGame();
     }
 }
--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,5 +1,25 @@
 package baseball.controller;
 
+import baseball.view.InputView;
+
 public class GameController {
+
+	/**
+	 * 게임을 제어하는 함수
+	 */
+	public static void controlGame() {
+		startGame();
+	}
+
+	/**
+	 * 게임을 시작하는 함수
+	 */
+	public static void startGame() {
+		String playerNumber;
+
+		InputView.askNumber();
+		playerNumber = InputView.writeInput();
+		System.out.println(playerNumber);
+	}
 
 }
