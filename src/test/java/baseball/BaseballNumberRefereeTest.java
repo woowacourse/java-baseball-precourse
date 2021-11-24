44bf4825bba42df4419d11ada22fd8a1d1357f2a
wilgur513 <wilgur513@naver.com>
2021-11-24 21:05:06 +0900

    test: extract setUp method

--- src/test/java/baseball/BaseballNumberRefereeTest.java
+++ src/test/java/baseball/BaseballNumberRefereeTest.java
@@ -3,6 +3,7 @@ package baseball;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,18 +11,25 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class BaseballNumberRefereeTest {
+	BaseballNumberValidator validator;
+	BaseballNumberReferee referee;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		validator = new BaseballNumberValidator();
+		referee = new BaseballNumberReferee(validator);
+	}
+
 	@ParameterizedTest(name = "입력값 : {0}")
 	@ValueSource(strings = {"012", "invalid", "1234", "112"})
 	@DisplayName("잘못된 입력값의 경우 예외 발생")
 	void judge_wrong_input(String invalidInput) throws Exception {
-		BaseballNumberReferee referee = new BaseballNumberReferee(new BaseballNumberValidator());
 		assertThrows(IllegalArgumentException.class, () -> referee.judge(invalidInput, "123"));
 	}
 
 	@Test
 	@DisplayName("낫싱 판단")
 	void judge_nothing() throws Exception {
-		BaseballNumberReferee referee = new BaseballNumberReferee(new BaseballNumberValidator());
 		JudgeResult result = referee.judge("123", "456");
 		assertThat(result.getStrike()).isEqualTo(0);
 		assertThat(result.getBall()).isEqualTo(0);
