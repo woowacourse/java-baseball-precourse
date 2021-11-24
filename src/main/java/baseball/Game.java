ac0d35cd97172d9097d0e82ac621aa5754f50608
meatsby <meatsby@gmail.com>
2021-11-24 20:30:29 +0900

    feat: 라운드 실행 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -11,6 +11,21 @@ public class Game {
     private int ball;
     private int strike;
 
+    private void startRound() {
+        randNum = makeRandNum();
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            userNum = Console.readLine();
+            isValidInput();
+            String result = compareNumbs();
+            if (result.equals("3스트라이크")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
+            }
+        }
+    }
+
     private String makeRandNum () {
         StringBuilder randNum = new StringBuilder();
 
