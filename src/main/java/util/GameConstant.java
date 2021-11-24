ad5c775191c408dcfb6a0b71f31ba340da5858c1
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-24 17:03:08 +0900

    feat : Add GameConstant

---
+++ src/main/java/util/GameConstant.java
@@ -0,0 +1,19 @@
+package util;
+
+/**
+ * Game 진행에 사용될 상수값
+ */
+public class GameConstant {
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 9;
+    public static final int NUMBER_LENGTH = 3;
+
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    public static final String REQUEST_INPUT = "숫자를 입력해주세요 : ";
+    public static final String THREE_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String ASK_RESTART_OR_STOP = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+    public static final String FINISH_GAME = "게임을 종료합니다.";
+}
