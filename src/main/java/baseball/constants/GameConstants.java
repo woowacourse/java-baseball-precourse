7aa6991a6bab84ca87b2cb94a5cb0fffdef2a6a8
0chil <0@chll.it>
2021-11-24 23:05:51 +0900

    feat: 게임 판정 출력 구현

--- src/main/java/baseball/constants/GameConstants.java
+++ src/main/java/baseball/constants/GameConstants.java
@@ -63,7 +63,10 @@ public class GameConstants {
 	}
 
 	public enum Hint {
-		HINT_ASK_INPUT("숫자를 입력해주세요 : ");
+		HINT_ASK_INPUT("숫자를 입력해주세요 : "),
+		HINT_GAME_OVER(
+			Digit.LENGTH.getNumber() + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 " + GameOverInput.RESTART.getNumber()
+				+ ", 종료하려면 " + GameOverInput.RESTART.getNumber() + "를 입력하세요");
 
 		private final String string;
 
