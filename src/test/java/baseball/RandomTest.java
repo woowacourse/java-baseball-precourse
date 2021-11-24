5b4429fd68eeb3db86ba77df0bb61e6f0b8a36f7
jaeyoungjang2 <lion85488@gmail.com>
2021-11-24 16:59:22 +0900

    test: 랜덤 숫자 뽑기 테스트 세자리 숫자를 뽑아본다.

--- src/test/java/baseball/RandomTest.java
+++ src/test/java/baseball/RandomTest.java
@@ -19,5 +19,11 @@ public class RandomTest {
         assertThat(randomNumber).isEqualTo(1);
     }
 
-
+    @DisplayName("랜덤 사용법 테스트, 세자리 숫자를 뽑아본다.")
+    @Test
+    void randomNumbersTest() {
+        List<Integer> numberList = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 3, 3);
+        assertThat(randomNumbers.size()).isEqualTo(3);
+    }
 }
