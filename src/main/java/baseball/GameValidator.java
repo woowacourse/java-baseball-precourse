3651cf4d969877837bfdf9f5a8ef146a24eddada
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-24 18:32:17 +0900

    feat : Add GameValidator to validate GamePlayer input value

---
+++ src/main/java/baseball/GameValidator.java
@@ -0,0 +1,51 @@
+package baseball;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static util.GameConstant.*;
+
+public class GameValidator {
+
+    public static boolean isValidInput(String input){
+        return isNumber(input) && isRightLength(input)
+                && isNotDuplicate(input) && isRangeBetweenOneToTen(input);
+    }
+
+    private static boolean isNumber(String input){
+        for(int i = 0 ; i < NUMBER_LENGTH; i++){
+            char digit = input.charAt(i);
+            if(!Character.isDigit(digit)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isRightLength(String input){
+        return input.length() == NUMBER_LENGTH;
+    }
+
+    private static boolean isNotDuplicate(String input){
+        Set<Character> tempSet = new HashSet<>();
+        for(int i = 0 ; i < NUMBER_LENGTH; i++){
+            tempSet.add(input.charAt(i));
+        }
+        if(tempSet.size() == NUMBER_LENGTH){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isRangeBetweenOneToTen(String input){
+        for(int i = 0 ; i < NUMBER_LENGTH; i++){
+            int digit = Character.getNumericValue(input.charAt(i));
+
+            if(digit < MIN_NUMBER || digit > MAX_NUMBER){
+                return false;
+            }
+        }
+        return true;
+    }
+}
