f075ab4717ed994c20983c613b16a0f0eb2b80a3
Hyunta <mohai2618@naver.com>
2021-11-24 20:01:44 +0900

    test: 각 자리수가 서로 다른지 확인하는 테스트

---
+++ src/test/java/baseball/RandomNumberGeneratorTest.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    void makeRandomNumber() {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        int[] numbers = randomNumberGenerator.makeRandomNumber();
+        Assertions.assertThat(Arrays.stream(numbers).distinct().count()).isEqualTo(Rules.NUMBER_SIZE);
+    }
+}
