2c89a687f6ba082f0707ece8705b1fe514d2c128
DK <ldk980130@gmail.com>
2021-11-25 00:10:06 +0900

    feat : 난수 생성부터 게임 종료 및 계속까지 흐름 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,19 @@ public class Application {
 
 	public static void main(String[] args) {
 
+		while (true) {
+
+			String answer = Random.createAnswer();
+
+			proceed(answer);
+
+			System.out.println(Message.RESTART_OR_EXIT);
+
+			if (selectRestartOrExit()) {
+				break;
+			}
+		}
+
 	}
 
 	private static void proceed(String answer) {
