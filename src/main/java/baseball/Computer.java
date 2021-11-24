7011ce9f3f28d9bfe1a94745af9a4c9c71559721
OzRagwort <ozragwort@gmail.com>
2021-11-24 17:28:24 +0900

    feat(Computer): generate random number

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
+	private List<Integer> randomNumbers;
+
 	public Computer() {}
+
+	public void setNewRandomNumbers() {
+		randomNumbers = new ArrayList<>();
+		while (randomNumbers.size() < 3) {
+			int number = Randoms.pickNumberInRange(1, 9);
+			if (!randomNumbers.contains(number)) {
+				randomNumbers.add(number);
+			}
+		}
+	}
 }
