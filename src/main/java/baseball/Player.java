895afe50e0d858dc9ab9b3e3d36ad9c8e013b4c5
seongwoo <seongwoo0513@uos.ac.kr>
2021-11-25 03:00:15 +0900

    fix: 플레이어로부터 수 입력받을 때 예외 처리 작업 삭제

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -11,28 +11,23 @@ import java.util.stream.Collectors;
 public class Player {
 
     private static final int DIGIT = 3;
+    private static final String PLAYER_NUMBER_REGEX = "^[1-9]{3}$";
 
-    public List<Integer> playerNumber() {
+    public List<Integer> playerNumber() throws IllegalArgumentException {
         System.out.print("숫자를 입력해주세요 : ");
-        try {
-            String inputNumber = readLine();
-            return validateInputNumber(inputNumber);
-        } catch (IllegalArgumentException exception) {
-            exception.getMessage();
-            return playerNumber();
-        }
+        String inputNumber = readLine();
+        return validateInputNumber(inputNumber);
     }
 
     private List<Integer> validateInputNumber(String inputNumber) throws IllegalArgumentException {
-        validateNumberRange(inputNumber);
+        validateNumberRange(inputNumber, PLAYER_NUMBER_REGEX);
         validateNumberDuplication(inputNumber);
         return stringToIntegerList(inputNumber);
     }
 
-    private void validateNumberRange(String inputNumber) {
-        String pattern = "^[1-9]{3}$";
+    private void validateNumberRange(String inputNumber, String pattern) {
         if (!Pattern.matches(pattern, inputNumber)) {
-            throw new IllegalArgumentException("1부터 9사이의 숫자로 이루어진 3자리 수를 입력해주세요.");
+            throw new IllegalArgumentException("정확한 값이 입력되지 않았습니다.");
         }
     }
 
