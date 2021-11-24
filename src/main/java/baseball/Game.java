02c5833bf828d80744ed2bd04daec3411972e92a
meatsby <meatsby@gmail.com>
2021-11-24 20:27:36 +0900

    feat: 대조결과에 따른 힌트 반환 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -57,13 +57,17 @@ public class Game {
         }
     }
 
-    private void compareNumbs() {
+    private String compareNumbs() {
         ball = 0;
         strike = 0;
 
         for (int i = 0; i < DIGIT; i++) {
             parseNum(i, randNum.charAt(i));
         }
+
+        String hint = getHint();
+        System.out.println(hint);
+        return hint;
     }
 
     private void parseNum(int i, char curNum) {
@@ -74,5 +78,21 @@ public class Game {
             }
             ball++;
         }
+    }
+
+    private String getHint() {
+        if (strike == 3) {
+            return "3스트라이크";
+        }
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        if (strike == 0) {
+            return ball + "볼";
+        }
+        if (ball == 0) {
+            return strike + "스트라이크";
+        }
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
