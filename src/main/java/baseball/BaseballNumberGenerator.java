34bd4654455647a17057a723c6e99f6467921299
wilgur513 <wilgur513@naver.com>
2021-11-25 00:06:04 +0900

    fix: change generate logic
    ApplicationTest의 "게임종료_후_재시작" 테스트에서 일의 자리수 숫자를 여러번 생성해 전체 숫자를
    생성하도록 만들어짐. 따라서, 기존에 3자리 숫자를 랜덤으로 한번에 생성하던 것을 숫자를 하나씩 3번 생
    성 후 합쳐서 만듬

--- src/main/java/baseball/BaseballNumberGenerator.java
+++ src/main/java/baseball/BaseballNumberGenerator.java
@@ -1,6 +1,9 @@
 package baseball;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static java.util.stream.Collectors.*;
+
+import java.util.stream.IntStream;
 
 public class BaseballNumberGenerator {
 	private final BaseballNumberValidator validator;
@@ -10,12 +13,20 @@ public class BaseballNumberGenerator {
 	}
 
 	public String generate() {
-		int number = pickNumberInRange(123, 987);
+		String number = createRandomNumber();
 
-		if (validator.validate(String.valueOf(number))) {
-			return String.valueOf(number);
+		if (validator.validate(number)) {
+			return number;
 		}
 
 		return generate();
+	}
+
+	private String createRandomNumber() {
+		return IntStream
+				.range(0, 3)
+				.map(i -> pickNumberInRange(1, 9))
+				.mapToObj(String::valueOf)
+				.collect(joining());
 	}
 }
