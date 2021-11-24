10af4904ae17815f6f01da7ea4f12b5758d77d12
sure-why-not <pkse1213@gmail.com>
2021-11-25 00:37:41 +0900

    feat: 사용자에게 3자리 수 입력 받기 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,16 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseBallGame {
     private int answerNum;
+    private int inputNum;
+
+    public BaseBallGame() {
+        answerNum = 0;
+        inputNum = 0;
+    }
 
     public void start() {
         makeAnswerNum();
+        getInputNumByConsole();
     }
 
     private void makeAnswerNum() {
         answerNum = Randoms.pickNumberInRange(111, 999);
     }
 
+    private void getInputNumByConsole() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String strNum = Console.readLine();
+        inputNum = Integer.parseInt(strNum);
+    }
 }
