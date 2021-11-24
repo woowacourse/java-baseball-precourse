b2d7c7728e8b59f64e65c0883cfc86717ca2298b
DK <ldk980130@gmail.com>
2021-11-24 22:59:27 +0900

    feat : 출력 메세지 상수에 "낫싱" 추가

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -5,6 +5,7 @@ public class Message {
     static final String QUESTION = "숫자를 입력해주세요 : ";
     static final String BALL = "볼";
     static final String STRIKE = "스트라이크";
+    static final String NOTHING = "낫싱";
     static final String CLEAR = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     static final String RESTART_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 }
