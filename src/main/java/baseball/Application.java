f2f5b9d3b29287e7ac1779080e7e0056bd0fbf3e
DK <ldk980130@gmail.com>
2021-11-24 23:34:48 +0900

    feat : 잘못된 입력값이 들어오면 예외 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
 
+
+    }
+
+    static void verifyInput(String input) {
+
+        if (!input.matches("-?\\d+")) {
+            throw new IllegalArgumentException("정수를 입력해야 합니다.");
+        }
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세 자리 수를 입력해야 합니다.");
+        }
     }
 }
