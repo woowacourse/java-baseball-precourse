507a10a458eaab0d8677cc2de6cdaacffc043994
inhyeokjo <alzpqm@naver.com>
2021-11-24 21:40:03 +0900

    feat: BaseballGame클래스와 생성자 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class BaseballGame {
+    private int[] enemyNum;
+    private int[] playerNum;
+
+    public BaseballGame() {
+        enemyNum = new int[3];
+        playerNum = new int[3];
+    }
+}
