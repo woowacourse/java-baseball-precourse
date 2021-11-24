faf16cdc354c5979c659438b41563a0521c3729d
OzRagwort <ozragwort@gmail.com>
2021-11-24 20:57:40 +0900

    feat(Game): read user number

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,11 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
 	private Computer computer;
 	private int strike;
@@ -9,5 +15,20 @@ public class Game {
 		this.computer = computer;
 		this.strike = 0;
 		this.ball = 0;
+	}
+
+	public void play() {
+		System.out.print("숫자를 입력해주세요 : ");
+		String userInputMessage = readLine();
+		List<Integer> userNumbers = toIntegerList(userInputMessage);
+	}
+
+	public List<Integer> toIntegerList(String input) {
+		String[] splitInput = input.split("");
+		List<Integer> integerList = Arrays
+			.stream(splitInput)
+			.map(Integer::parseInt)
+			.collect(Collectors.toList());
+		return integerList;
 	}
 }
