d42b2c87fb82931376c29bd07b9297e29dd5fd66
최창준 <aowert6518@gmail.com>
2021-11-24 17:47:52 +0900

    [feat] 입력 문구 및 결과 출력 및 다시하기 문구 출력하기 - inputNumber() : 숫자를 입력해주세요 출력 - printStrikeBall() : ball과 strike를 출력하는 함수로 0이 아닐때 문자열에 추가되며 strike가 3이면 정답 문구 출력 - printEndMessage() : 게임 재시작/종료문구 출력

---
+++ src/main/java/baseball/domain/Viewer.java
@@ -0,0 +1,30 @@
+package baseball.domain;
+
+public class Viewer {
+    public static void inputNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void printStrikeBall(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+        if (ball != 0) {
+            sb.append(ball);
+            sb.append("볼");
+            sb.append(" ");
+        }
+        if (strike != 0) {
+            sb.append(strike);
+            sb.append("스트라이크");
+        }
+        if (strike == 3) {
+            sb.append("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void printEndMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    }
+
+
+}
