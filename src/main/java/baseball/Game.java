6686b679868cd8f7dc682fc41e0085e47a62a1b9
meatsby <meatsby@gmail.com>
2021-11-24 20:19:49 +0900

    feat: 사용자입력 예외처리 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Game {
 
     private final int DIGIT = 3;
+    private String userNum;
 
     private String makeRandNum () {
         StringBuilder randNum = new StringBuilder();
@@ -19,5 +20,37 @@ public class Game {
         }
 
         return String.valueOf(randNum);
+    }
+
+    private void isValidInput() {
+        if (userNum.length() != DIGIT) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < DIGIT; i++) {
+            isNumber(i);
+            noZeros(i);
+            noOverlap(i);
+        }
+    }
+
+    private void isNumber(int i) {
+        if (!Character.isDigit(userNum.charAt(i))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void noZeros(int i) {
+        if (Character.toString(userNum.charAt(i)).equals("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void noOverlap(int i) {
+        for (int j = i+1; j < DIGIT; j++) {
+            if (userNum.charAt(i) == userNum.charAt(j)) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
