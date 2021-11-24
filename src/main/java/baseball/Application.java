6a07c828d1fe26f7564344f66dc510a0e4c72c73
EastHShin <xldk78@gmail.com>
2021-11-25 02:00:48 +0900

    feat: 1부터 9까지 서로 다른 임의의 수 3개 뽑는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,12 @@
 package baseball;
 
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
---
+++ src/main/java/baseball/RandomNumbers.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumbers {
+	private static final int START_INCLUSIVE = 1;
+	private static final int END_INCLUSIVE = 9;
+	private static final int NUM_OF_DIGIT = 3;
+
+	public RandomNumbers(){};
+
+	public static List<Integer> getRandomNumbers() {
+		List<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i < NUM_OF_DIGIT; i++) {
+			numbers.add(getNotDuplicateNum(numbers));
+		}
+		return numbers;
+	}
+
+	public static Integer getNotDuplicateNum(List<Integer> numbers) {
+		Integer number;
+		do {
+			number = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+		}while(numbers.contains(number));
+
+		return number;
+	}
+}
