bf1f258b1a02ecacee2cf8cef23a33367fe77697
유영우 <fbduddn97@naver.com>
2021-11-24 19:19:58 +0900

    [feat]: 타겟 넘버 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+        // 1. 타겟 넘버 생성
+        int[] randomAns = createTargetNum();
+
+        // 2. 유저에게 입력 받기
+
+
+        // 3. 유저에게 결과 알려주기
+
+
+        // 4. 재실행 여부
     }
+
+    static public int[] createTargetNum(){
+        int[] randomAns = new int[3];
+        boolean[] check = new boolean[10];
+        for(int i=0; i<3; i++){
+            int num = Randoms.pickNumberInRange(1,4);
+
+            while(check[num]){
+                num = Randoms.pickNumberInRange(1,4);
+            }
+
+            check[num] = true;
+            randomAns[i] = num;
+        }
+        return randomAns;
+    }
+
 }
