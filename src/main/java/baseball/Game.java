1ab3f70a6751aa705dedb4f064f7f5b88adf8585
DK <ldk980130@gmail.com>
2021-11-24 23:20:05 +0900

    feat : 정답 난수와 입력값을 비교해 게임 결과 생성

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,59 @@
+package baseball;
+
+public class Game {
+
+    public static String play(String answer, String input) {
+
+        String[] answerArray = answer.split("");
+        String[] inputArray = input.split("");
+
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+
+            String result = checkOneNumber(answerArray, inputArray[i], i);
+
+            if (result.equals(Message.STRIKE)) {
+                strike++;
+            } else if (result.equals(Message.BALL)) {
+                ball++;
+            }
+        }
+
+        String message = makeMessage(strike, ball, "");
+
+        return message;
+    }
+
+    private static String checkOneNumber(String[] answerArray, String number, int index) {
+
+        for (int i = 0; i < 3; i++) {
+
+            if (answerArray[i].equals(number) && index == i) {
+                return Message.STRIKE;
+            } else if (answerArray[i].equals(number) && index != i) {
+                return Message.BALL;
+            }
+        }
+
+        return Message.NOTHING;
+    }
+
+    private static String makeMessage(int strike, int ball, String message) {
+
+        if (ball == 0 && strike == 0) {
+            return Message.NOTHING;
+        }
+
+        if (ball > 0) {
+            message += ball + Message.BALL + " ";
+        }
+
+        if (strike > 0) {
+            message += strike + Message.STRIKE;
+        }
+
+        return message;
+    }
+}
