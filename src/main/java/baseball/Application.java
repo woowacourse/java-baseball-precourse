43b3b7a5b081be54ffcbe60ee69255105c97cb06
유영우 <fbduddn97@naver.com>
2021-11-25 01:06:47 +0900

    [feat]: 타겟넘버와 유저인풋넘버 비교해서  유저에게 결과 알려주기 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,6 +13,7 @@ public class Application {
         while(start){
             // 1. 타겟 넘버 생성
             randomAns = createTargetNum();
+            System.out.println(randomAns[0]+""+randomAns[1]+""+randomAns[2]);
 
             // 2. 유저에게 입력 받기
             // 3. 유저에게 결과 알려주기
@@ -22,14 +23,15 @@ public class Application {
         }
     }
 
+
     static int[] createTargetNum(){
         int[] randomAns = new int[3];
         boolean[] check = new boolean[10];
         for(int i=0; i<3; i++){
-            int num = Randoms.pickNumberInRange(1,4);
+            int num = Randoms.pickNumberInRange(1,9);
 
             while(check[num]){
-                num = Randoms.pickNumberInRange(1,4);
+                num = Randoms.pickNumberInRange(1,9);
             }
 
             check[num] = true;
@@ -37,6 +39,7 @@ public class Application {
         }
         return randomAns;
     }
+
 
     static int[] userInputNum(){
         System.out.print("숫자를 입력해주세요 : ");
@@ -47,6 +50,7 @@ public class Application {
 
         return userInputNumArr;
     }
+
 
     static int[] checkUserInputNum(String inputStr){
         int[] userInputNumArr = new int[3];
@@ -63,12 +67,48 @@ public class Application {
         return userInputNumArr;
     }
 
+
     static boolean compareToRandomAns(int[] userInputNumArr){
         // 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 포볼 또는 낫싱
+        int strike=0;
+        int ball=0;
 
+        // 타겟넘버 와 유저인풋넘버 비교
+        for(int i=0; i<3; i++){
+            int num = userInputNumArr[i];
+            for(int j=0; j<3; j++){
+               if(num==randomAns[j]){
+                   if(i==j){
+                       strike++;
+                       break;
+                   }
+                   ball++;
+                   break;
+               }
+            }
+        }
 
-        return true;
+        return notifyBallAndStrike(strike,ball);
     }
+
+
+    static boolean notifyBallAndStrike(int strike, int ball){
+        if(strike==3){
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        if(strike>0 && ball>0) System.out.println(ball+"볼"+" "+strike+"스트라이크");
+        else{
+            if(strike==0 && ball==0) System.out.println("낫싱");
+            else if(ball==0) System.out.println(strike+"스트라이크");
+            else System.out.println(ball+"볼");
+        }
+
+        return false;
+    }
+
 
     static boolean checkRestart(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
