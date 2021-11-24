e92b0352f270b5861cf81f79993679ffdd226525
0chil <0@chll.it>
2021-11-24 23:05:51 +0900

    feat: 게임 판정 출력 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,8 +2,14 @@ package baseball.view;
 
 import static baseball.constants.GameConstants.Hint.*;
 
+import baseball.domain.Game;
+
 public class OutputView {
 	public static void printInputHint() {
 		System.out.print(HINT_ASK_INPUT.getString());
+	}
+
+	public static void printGameResult(Game game) {
+		System.out.println(game);
 	}
 }
