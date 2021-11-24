20c5093cf556098056ed75a12fc1ce429925285e
pup-paw <wldusdl0310@gmail.com>
2021-11-24 23:31:52 +0900

    서로 다른 수로 이루어진 3자리 수 생성

---
+++ src/main/java/baseball/gameController/NumberGenerator.java
@@ -0,0 +1,24 @@
+package baseball.gameController;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class NumberGenerator {
+
+	public HashSet<Integer> generateNumSet() {
+		HashSet<Integer> selectedNum = new HashSet<>();
+
+		while (selectedNum.size() < 3) {
+			int randomNum = (int)(Math.random() * 10) % 9 + 1;
+			selectedNum.add(randomNum);
+		}
+
+		return selectedNum;
+	}
+
+	public int generateNumber() {
+		Iterator<Integer> iterator = generateNumSet().iterator();
+
+		return iterator.next() * 100 + iterator.next() * 10 + iterator.next();
+	}
+}
