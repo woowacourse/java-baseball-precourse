8b4bbd361bbfef41ced29644d60b1944fe5adb47
jongwon <54282927+loosie@users.noreply.github.com>
2021-11-24 19:37:12 +0900

    feat : `judge` attacker.number입력 후 defender.number의 값 비교하기
    - attacker.number 입력 받기, camp.nextstep.edu.missionutils.Console 사용
    - attacker.number의 크기가 3이 아닌 경우, 0이 포함된 경우 예외 처리(IllegalArgumentException)
    - 자리와 숫자 일치하면 strike, 숫자만 일치하면 ball 카운트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,19 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         int coin = 0;
-        Defender defender =null;
+        Defender defender = Defender.getInstance();
         Judge judge = null;
         for(int test=0; test<3; test++){
-            if (defender == null) {
-                defender = Defender.getInstance();
-            } else {
-                defender.changeNumber();
-            }
-
             judge = judge.passNumberToJudge(defender.getNumber() + "");
+            System.out.print("숫자를 입력해주세요 : ");
+            System.out.println(judge.judging(readLine()));
         }
 
-        //TODO(Attacker): 3자리 숫자 입력하기
         //TODO(Attacker): 게임 시작 혹은 종료하기 (1/2)
-        // TODO(Judge): Attacker(Player)의 숫자와 Defender 숫자 비교하기
 
 
     }
--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -6,12 +6,43 @@ public class Judge {
     private Judge(){}
     public static Judge passNumberToJudge(String number) {
         defendNumber = number.toCharArray();
-        System.out.println("passToJudge");
-        for (char c : defendNumber) {
-            System.out.print(c);
-        }
-        System.out.println();
+//        System.out.println("passToJudge");
+//        for (char c : defendNumber) {
+//            System.out.print(c);
+//        }
+//        System.out.println();
         return new Judge();
+    }
+
+    public String judging(String attackNumber){
+        if(attackNumber.length()!=3 || attackNumber.contains("0")) {
+            System.out.println("입력 오류 ");
+            throw new IllegalStateException();
+        }
+        char[] a =attackNumber.toCharArray();
+        int strike=0;
+        int ball=0;
+        for(int i=0; i<3; i++){
+            if(defendNumber[i]==a[i])strike++;
+            else {
+                if (attackNumber.contains(defendNumber[i] + "")) {
+                    ball++;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if (ball == 0 && strike == 0) {
+            sb.append("낫싱");
+        } else {
+            if (ball> 0) {
+                sb.append(ball + "볼 ");
+            }
+            if (strike> 0) {
+                sb.append(strike + "스트라이크");
+            }
+        }
+
+        return sb.toString();
     }
 
 }
