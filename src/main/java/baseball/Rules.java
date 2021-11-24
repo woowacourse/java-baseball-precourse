ea2e310c6a038eb720c2050e587adf82f419c73c
Hyunta <mohai2618@naver.com>
2021-11-24 20:46:50 +0900

    style : NUMBER_SIZE를 NUMBER_LENGTH로 변경

--- src/main/java/baseball/Rules.java
+++ src/main/java/baseball/Rules.java
@@ -3,6 +3,5 @@ package baseball;
 public class Rules {
     static final int START_NUMBER = 1;
     static final int END_NUMBER = 9;
-    static final int NUMBER_SIZE = 3;
-
+    static final int NUMBER_LENGTH = 3;
 }
