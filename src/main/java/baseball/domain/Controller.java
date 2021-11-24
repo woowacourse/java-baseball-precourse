6892cf9bcbbb041525f629d75cc8904bc4b767db
최창준 <aowert6518@gmail.com>
2021-11-24 17:34:19 +0900

    [feat] 랜덤 숫자와 사용자 숫자 비교하기 - initGame() : 랜덤숫자 생성하고 user에게 숫자를 받기 - setBallNumbers() : ball을 카운트하기 위해 ballNumbers 변수를 만들고 randomNumber 하나씩 넣기 - compareNumbers() : strike와 ball 변수로 카운트함  숫자 하나씩 비교하면서 맞으면 strike를 +하고 ballNumbers에 포함되어있으면 ball을 +함

--- src/main/java/baseball/domain/Controller.java
+++ src/main/java/baseball/domain/Controller.java
@@ -3,15 +3,45 @@ package baseball.domain;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+
 public class Controller {
+    private String randomNumber;
+    private String userNumber;
+    private ArrayList<Character> ballNumbers;
 
-
-    private Integer createRandomNumber() {
-        return Randoms.pickNumberInRange(100, 1000);
+    private String createRandomNumber() {
+        return String.valueOf(Randoms.pickNumberInRange(100, 1000));
     }
 
-    private Integer getUserNumber() {
-        return Integer.parseInt(Console.readLine());
+    private String getUserNumber() {
+        return Console.readLine();
+    }
+
+    private void setBallNumbers() {
+        for (char number : randomNumber.toCharArray()) {
+            ballNumbers.add(number);
+        }
+    }
+
+    public void initGame() {
+        randomNumber = createRandomNumber();
+        userNumber = getUserNumber();
+        setBallNumbers();
+    }
+
+    public void compareNumbers() {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            char randomNum = randomNumber.charAt(i);
+            char userNum = userNumber.charAt(i);
+            if (randomNum == userNum) {
+                strike++;
+            } else if (ballNumbers.contains(userNum)) {
+                ball++;
+            }
+        }
     }
 
 }
