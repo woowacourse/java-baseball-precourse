e551502b1b4a32344c76db88fab56cad2aae2d53
Hyunta <mohai2618@naver.com>
2021-11-24 20:44:19 +0900

    test : 입력값의 길이와 정수확인, 중복검증 오류

---
+++ src/test/java/baseball/ValidationTest.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationTest {
+
+    @Test
+    void checkLength() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.checkInput("12"));
+        String message = exception.getMessage();
+        assertEquals(Message.ERROR_INPUT_LENGTH, message);
+    }
+
+    @Test
+    void checkInteger() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.checkInput("12a"));
+        String message = exception.getMessage();
+        assertEquals(Message.ERROR_INPUT_TYPE, message);
+    }
+
+    @Test
+    void checkDuplicate() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Validation.checkInput("111"));
+        String message = exception.getMessage();
+        assertEquals(Message.ERROR_INPUT_DUPLICATE, message);
+    }
+
+}
