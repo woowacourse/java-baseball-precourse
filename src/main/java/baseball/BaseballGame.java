51fe8dc37e079861dfa85fe04e3130c54a1329b1
inhyeokjo <alzpqm@naver.com>
2021-11-24 21:42:23 +0900

    feat: 컴퓨터의 숫자를 난수로 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class BaseballGame {
     private int[] enemyNum;
     private int[] playerNum;
@@ -7,5 +9,28 @@ public class BaseballGame {
     public BaseballGame() {
         enemyNum = new int[3];
         playerNum = new int[3];
+        setEnemyNum();
+    }
+
+    private void setEnemyNum() {
+        int smallNumberIndex = 1;
+        for (int i = 0; i < 3; i++) {
+            enemyNum[i] = Randoms.pickNumberInRange(1, 9 - i);
+        }
+        if (enemyNum[0] <= enemyNum[1]) {
+            enemyNum[1] += 1;
+            smallNumberIndex = 0;
+        }
+        if (enemyNum[smallNumberIndex] <= enemyNum[2]) {
+            enemyNum[2] += 1;
+            if (smallNumberIndex == 1) {
+                smallNumberIndex = 0;
+            } else {
+                smallNumberIndex = 1;
+            }
+        }
+        if (enemyNum[smallNumberIndex] <= enemyNum[2]) {
+            enemyNum[2] += 1;
+        }
     }
 }
