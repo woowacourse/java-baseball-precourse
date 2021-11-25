579915a722842828a19d054f10a304869f468604
jaeyoungjang2 <lion85488@gmail.com>
2021-11-25 16:22:48 +0900

    feat: 야구 게임에 사용되는 수가 1-9사의 값이 아닐 경우 발생하는 예외

--- src/main/java/baseball/exception/InvalidBaseBallNumberRangeException.java
+++ src/main/java/baseball/exception/InvalidBaseBallNumberRangeException.java
@@ -1,2 +1,10 @@
-package baseball.exception;public class InvalidBaseBallNumberRangeException {
+package baseball.exception;
+
+public class InvalidBaseBallNumberRangeException extends IllegalArgumentException {
+
+    private static final String INVALID_BASEBALL_NUMBER_RANGE = "1 ~ 9 사이의 값을 입력해야 합니다.";
+
+    public InvalidBaseBallNumberRangeException() {
+        super(INVALID_BASEBALL_NUMBER_RANGE);
+    }
 }
