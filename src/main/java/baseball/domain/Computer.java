f57f14e306a432ebb8909cc0a0510380dfde2597
jaeyoungjang2 <lion85488@gmail.com>
2021-11-25 16:20:42 +0900

    feat: 세자리 난수 생성 클래스

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,2 +1,36 @@
-package baseball.controller;public class Computer {
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Computer {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int COUNT = 3;
+
+    private static List<BaseBallNumber> randomNumbers;
+
+    public Computer() {
+        this.randomNumbers = extractRandomNumbers();
+    }
+
+    private List<BaseBallNumber> extractRandomNumbers() {
+        List<BaseBallNumber> randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() != COUNT) {
+            BaseBallNumber randomNumber = new BaseBallNumber(Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE));
+            if (randomNumbers.contains(randomNumber)) {
+                continue;
+            }
+            randomNumbers.add(randomNumber);
+
+        }
+        return randomNumbers;
+    }
+
+    public List<BaseBallNumber> randomNumbers() {
+        return Collections.unmodifiableList(randomNumbers);
+    }
 }
