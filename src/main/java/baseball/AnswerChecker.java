dfa99840d4e7c4efe6138fee4ad27d73a23964ef
her0807 <her0807@naver.com>
2021-11-25 20:02:04 +0900

    feat(answerChecker): 정답 여부를 계산하는 로직 구현
    - ball 과 strike 를 계산하는 메소드
    - ball 검증 시간을 단축하기 위한 메서드 visit

---
+++ src/main/java/baseball/AnswerChecker.java
@@ -0,0 +1,54 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AnswerChecker {
+    private int strikeCount;
+    private int ballCount;
+    private static boolean[] visit;
+    private static ArrayList<Integer> computer;
+
+    public AnswerChecker(ArrayList<Integer> computer) {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+        this.visit = new boolean[10];
+        this.computer = new ArrayList<>(computer);
+        setVisit();
+    }
+
+    public void calculateBallOrStrike(ArrayList<Integer> user) {
+        for (int i = 0; i < computer.size(); i++) {
+            checkStrike(computer.get(i), user.get(i));
+        }
+    }
+
+    public void checkStrike(Integer computerNumber, Integer userNumber) {
+        if (computerNumber == userNumber) {
+            strikeCount++;
+        } else {
+            checkBall(userNumber);
+        }
+    }
+
+    public void checkBall(Integer userNumber) {
+        if (visit[userNumber.intValue()]) {
+            ballCount++;
+        }
+    }
+
+    public void setVisit() {
+        Iterator<Integer> iterator = computer.iterator();
+        while (iterator.hasNext()) {
+            visit[iterator.next()] = true;
+        }
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+}
