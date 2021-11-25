246477b6488784aefe20682d5e24fbf7baf3bb90
Seungmin Lim <seungmin9066@gmail.com>
2021-11-25 20:44:05 +0900

    [수정] 숫자 야구 게임 객체 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,77 +1,124 @@
 package baseball;
 import java.util.*;
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
+import static camp.nextstep.edu.missionutils.Randoms.*;
 
 
-public class BaseballGameApplication {
 
-    public static final String GAME_START_MESSAGE = "숫자를 입력해주세요 : ";
-    public static final String MESSAGE_ABOUT_WRONG_INPUT = "잘못된 입력입니다.";
-    public static final String HINT_BALL_MESSAGE = "%d볼 ";
-    public static final String HINT_STRIKE_MESSAGE = "%d스트라이크 ";
-    public static final String HINT_NOTHING_MESSAGE = "낫싱";
-    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String CHECK_KEEP_GOING_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    public static final int LENGTH_OF_HINT = 3;
-    public static final int LENGTH_OF_END_MESSAGE = 1;
-    public static List<Integer> answerNumber = Randoms.pickUniqueNumbersInRange(1, 10, 3);
-    public static boolean keep_playing_game = true;
+public class BaseballGame {
 
-    public static void main(String[] args) {
-        while (keep_playing_game){
-            String userInput;
-            System.out.print(GAME_START_MESSAGE);
-            userInput = Console.readLine();
-            checkWrongInput(userInput, LENGTH_OF_HINT);
-            giveResultMessage(userInput);
+    private static List<Integer> answerNumber;
+    private final Player player;
+    private static boolean not_three_strike = true;
+
+    private static final String NEW_GAME = "1";
+    private static final String QUIT = "2";
+
+    private static final String QUESTION_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String MESSAGE_ABOUT_WRONG_INPUT = "잘못된 입력입니다.";
+    private static final String HINT_BALL_MESSAGE = "%d볼 ";
+    private static final String HINT_STRIKE_MESSAGE = "%d스트라이크";
+    private static final String HINT_NOTHING_MESSAGE = "낫싱";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String CHECK_KEEP_GOING_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int LENGTH_OF_ANSWER = 3;
+
+
+    public BaseballGame() {
+        player = new Player();
+        startGame();
+        endGame();
+    }
+
+    private void startGame(){
+        String playerNumber;
+        answerNumber = setAnswerNumber();
+        while(not_three_strike){
+            System.out.print(QUESTION_MESSAGE);
+            playerNumber = player.enterTheAnswer();
+            checkWrongInput(playerNumber);
+            checkDuplicatedInput(playerNumber);
+            checkAnswer(playerNumber);
         }
     }
 
-    public static void setAnswerNumber(){
-        answerNumber = Randoms.pickUniqueNumbersInRange(1, 10, 3);
+    private void endGame(){
+        String playerAnswer;
+        System.out.println(GAME_END_MESSAGE);
+        System.out.println(CHECK_KEEP_GOING_MESSAGE);
+        playerAnswer = player.enterTheAnswer();
+        if (checkKeepGoingGame(playerAnswer)){
+            startGame();
+        }
     }
 
-    public static void checkWrongInput(String number, int limitLength){
-        boolean isNumeric = number.matches("[+-]?\\d*(\\.\\d+)?");
-        if (!isNumeric || number.length() != limitLength){
+    private boolean checkKeepGoingGame(String playerNumber){
+        if(!playerNumber.equals(NEW_GAME) && !playerNumber.equals(QUIT)) {
+            throw new IllegalArgumentException(MESSAGE_ABOUT_WRONG_INPUT);
+        }
+        return playerNumber.equals(NEW_GAME);
+    }
+
+    private List<Integer> setAnswerNumber(){
+        HashSet<Integer> createAnswerNumber = new HashSet<Integer>();
+        while(createAnswerNumber.size() != LENGTH_OF_ANSWER){
+            createAnswerNumber.add(pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE));
+        }
+        return new ArrayList<>(createAnswerNumber);
+    }
+
+    private void checkWrongInput(String playerNumber){
+        boolean isNumeric = playerNumber.matches("[+-]?\\d*(\\.\\d+)?");
+        if (!isNumeric || playerNumber.length() != LENGTH_OF_ANSWER){
             throw new IllegalArgumentException(MESSAGE_ABOUT_WRONG_INPUT);
         }
     }
 
-    public static void giveResultMessage(String number) {
-        int[] info_ball_and_strike = compareNumber(number);
-        if (info_ball_and_strike[0] > 0 && info_ball_and_strike[1] > 0){
-            System.out.printf(HINT_BALL_MESSAGE, info_ball_and_strike[0]);
-            System.out.printf(HINT_STRIKE_MESSAGE + '\n', info_ball_and_strike[1]);
-        }
-        else if (info_ball_and_strike[0] > 0){
-            System.out.printf(HINT_BALL_MESSAGE + '\n', info_ball_and_strike[0]);
-        }
-        else if (info_ball_and_strike[1] > 0){
-            System.out.printf(HINT_STRIKE_MESSAGE + '\n', info_ball_and_strike[1]);
-        }
-        else{
-            System.out.println(HINT_NOTHING_MESSAGE);
-        }
-        if (checkGameEnd(info_ball_and_strike[1])){
-            System.out.println(GAME_END_MESSAGE);
-            checkKeepGoingGame();
+    private void checkDuplicatedInput(String playerNumber){
+        HashSet<String> numbersWithNoDuplicates = new HashSet<>(Arrays.asList(playerNumber.split("")));
+        if (numbersWithNoDuplicates.size() != LENGTH_OF_ANSWER){
+            throw new IllegalArgumentException(MESSAGE_ABOUT_WRONG_INPUT);
         }
     }
 
-    public static int[] compareNumber(String number){
+    private void checkAnswer(String playerNumber) {
+        int[] BallAndStrike = compareNumber(playerNumber);
+        if (BallAndStrike[1] == LENGTH_OF_ANSWER){
+            System.out.printf(HINT_STRIKE_MESSAGE + '\n', BallAndStrike[1]);
+            not_three_strike = false;
+            return;
+        }
+        System.out.println(printHintMessage(BallAndStrike[0], BallAndStrike[1]));
+    }
+
+    private String printHintMessage(int ballNumber, int strikeNumber){
+        String hintMessage = "";
+        if (ballNumber > 0){
+            hintMessage += String.format(HINT_BALL_MESSAGE, ballNumber);
+        }
+        if (strikeNumber > 0){
+            hintMessage += String.format(HINT_STRIKE_MESSAGE, strikeNumber);
+        }
+        if (hintMessage.equals("")){
+            hintMessage += HINT_NOTHING_MESSAGE;
+        }
+        return hintMessage;
+    }
+
+    public static int[] compareNumber(String playerNumber){
         int ballNumber = 0;
         int strikeNumber = 0;
         for (int i=0; i<3; i++){
-            strikeNumber += countStrikeNumber(answerNumber.get(i), Character.getNumericValue(number.charAt(i)));
-            ballNumber += countBallNumber(i, Character.getNumericValue(number.charAt(i)));
+            strikeNumber += countStrikeNumber(answerNumber.get(i), Character.getNumericValue(playerNumber.charAt(i)));
+            ballNumber += countBallNumber(i, Character.getNumericValue(playerNumber.charAt(i)));
         }
         return new int[] {ballNumber, strikeNumber};
     }
 
-    public static int countStrikeNumber(int answer, int userInput){
-        if (answer == userInput){
+    public static int countStrikeNumber(int answerNumber, int playerNumber){
+        if (answerNumber == playerNumber){
             return 1;
         }
         return 0;
@@ -83,25 +130,5 @@ public class BaseballGameApplication {
             return 1;
         }
         return 0;
-    }
-
-    public static boolean checkGameEnd(int strike_number){
-        return strike_number==3;
-    }
-
-    public static void checkKeepGoingGame(){
-        String user_answer;
-        System.out.println(CHECK_KEEP_GOING_MESSAGE);
-        user_answer = Console.readLine();
-        checkWrongInput(user_answer, LENGTH_OF_END_MESSAGE);
-        if (user_answer.equals("2")) {
-            keep_playing_game = false;
-            return;
-        }
-        if (user_answer.equals("1")){
-            setAnswerNumber();
-            return;
-        }
-        throw new IllegalArgumentException(MESSAGE_ABOUT_WRONG_INPUT);
     }
 }
