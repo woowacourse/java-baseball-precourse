4a095985fe12085407afde168eb22bd16c193433
SoulBriz <adoredia@naver.com>
2021-11-25 21:27:36 +0900

    style: 전체적인 출력문 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -99,13 +99,13 @@ public class Application {
             }
         }
         if (strike + ball == 0) {
-            System.out.println("out!");
+            System.out.println("낫싱");
         } else if (strike == 0) {
-            System.out.println(ball + " Ball");
+            System.out.println(ball + "볼");
         } else if (ball == 0) {
-            System.out.println(strike + " Strike");
+            System.out.println(strike + "스트라이크");
         } else {
-            System.out.println(strike + " Strike" + " " + ball + " Ball");
+            System.out.println(ball + "볼" + " " + strike + "스트라이크");
         }
 
         int[] result = {strike, ball};
@@ -120,12 +120,14 @@ public class Application {
             String randomNumber = getRandomNumber();
             System.out.println(randomNumber);
             while (compareValues[0] != 3) {
+                System.out.print("숫자를 입력해주세요 : ");
                 String inputNumber = getInputNumber();
 //                System.out.println(inputNumber);
                 compareValues = compareValues(randomNumber, inputNumber);
 //                System.out.println(compareValues[0] + "" + compareValues[1]);
             }
-            System.out.println("restart = 1, exit = 2");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String gameStart = Console.readLine();
 //            System.out.println(gameStart);
             if (gameStart.equals("1")) {
