5819ae9f58968af9613b73e24cf7fb883e09179d
HyeonbinSa <shb1833@gmail.com>
2021-11-25 23:41:07 +0900

    feat: Game Rule 기능구현
    Class : GameReferee
    기능 1. 스트라이크, 볼 판단
    기능 2. 승리, 낫싱 판단
    기능 3. 판단 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer();
-        ArrayList<Integer> computerNumber = computer.createComputerNumber();
-        System.out.println(computerNumber);
+        ArrayList<Integer> computerNumberList = computer.createComputerNumber();
+        System.out.println(computerNumberList);
 
         User user = new User();
-        ArrayList<Integer> userNumber = user.inputUserNumber();
-        System.out.println(userNumber);
+        ArrayList<Integer> userNumberList = user.inputUserNumber();
+        System.out.println(userNumberList);
 
+        GameReferee gameReferee = new GameReferee();
+        gameReferee.decideResult(computerNumberList, userNumberList);
     }
 }
---
+++ src/main/java/baseball/GameReferee.java
@@ -0,0 +1,78 @@
+package baseball;
+
+import java.util.ArrayList;
+
+public class GameReferee {
+    private static final int LENGTH = 3;
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private int ballCount;
+    private int strikeCount;
+    private ArrayList<Integer> computerNumberList;
+    private ArrayList<Integer> userNumberList;
+    private String resultMessage;
+
+    public boolean decideResult(ArrayList<Integer> computerNumberList, ArrayList<Integer> userNumberList) {
+        this.initializeReferee(computerNumberList, userNumberList);
+        this.countStrikeAndBall();
+        this.createResultMessage();
+        System.out.println(resultMessage);
+        if (strikeCount == LENGTH) {
+            System.out.println(WIN);
+            return true;
+        }
+        return false;
+    }
+
+    private void initializeReferee(ArrayList<Integer> computerNumberList, ArrayList<Integer> userNumberList) {
+        this.computerNumberList = computerNumberList;
+        this.userNumberList = userNumberList;
+        this.ballCount = 0;
+        this.strikeCount = 0;
+        this.resultMessage = "";
+    }
+
+    private boolean isStrike(int index) {
+        if (computerNumberList.get(index) == userNumberList.get(index)) {
+            strikeCount++;
+            return true;
+        }
+        return false;
+    }
+
+    private void isBall(int index) {
+        if (computerNumberList.contains(userNumberList.get(index))) {
+            ballCount++;
+        }
+    }
+
+    private void countStrikeAndBall() {
+        for (int index = 0; index < LENGTH; index++) {
+            if (!this.isStrike(index)) {
+                this.isBall(index);
+            }
+        }
+    }
+
+    private void createResultMessage() {
+        if (ballCount == 0 && strikeCount == 0) {
+            resultMessage += NOTHING;
+            return;
+        }
+        if (ballCount != 0 && strikeCount != 0) {
+            resultMessage += ballCount + BALL + " " + strikeCount + STRIKE;
+            return;
+        }
+        if (ballCount != 0 && strikeCount == 0) {
+            resultMessage += ballCount + BALL;
+            return;
+        }
+        if (ballCount == 0 && strikeCount != 0) {
+            resultMessage += strikeCount + STRIKE;
+            return;
+        }
+    }
+
+}
