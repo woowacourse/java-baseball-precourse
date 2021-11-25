a5b9e6d04df07f2693873f541dd2e5e902bd320e
SoulBriz <adoredia@naver.com>
2021-11-25 12:39:04 +0900

    feat: 서로 겹치지 않는 3자리 수를 입력받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class Application {
 
@@ -33,8 +36,39 @@ public class Application {
         return randomNumber;
     }
 
+    public static String getInputNumber() {
+        String input = Console.readLine();
+        if (input.length() != 3) {
+//            System.out.println("input error");
+            throw new IllegalArgumentException();
+        }
+        if (input.contains("0")) {
+//            System.out.println("contains zero");
+            throw new IllegalArgumentException();
+        }
+
+        List<String> inputNumber = new ArrayList<String>(Arrays.asList(input.split("")));
+//        System.out.println(inputNumber);
+//        System.out.println(inputNumber.getClass());
+//        System.out.println(inputNumber.size());
+
+        HashSet<String> checkDuplicate = new HashSet<String>(inputNumber);
+//        System.out.println(checkDuplicate);
+//        System.out.println(checkDuplicate.getClass());
+//        System.out.println(checkDuplicate.size());
+
+        if (checkDuplicate.size() != inputNumber.size()) {
+//            System.out.println("duplicate detected");
+            throw new IllegalArgumentException();
+        }
+
+        return input;
+    }
+
     public static void main(String[] args) {
         String randomNumber = getRandomNumber();
         System.out.println(randomNumber);
+        String inputNumber = getInputNumber();
+        System.out.println(inputNumber);
     }
 }
