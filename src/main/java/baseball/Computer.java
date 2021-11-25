eb564943da1eadcc37e1d80b57100e3cbcdf859d
Hongdonggeon <ghd700@naver.com>
2021-11-25 16:03:39 +0900

    feat : 컴퓨터 야구공 리스트 초기화하기

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Ball> balls;
+
+    public Computer() {
+        balls = new ArrayList<>();
+        int position = 1;
+        for (int generateNumber : RandomNumberGenerate()) {
+            Ball ball = new Ball(position++, generateNumber);
+            balls.add(ball);
+        }
+    }
+
+    public List<Integer> RandomNumberGenerate() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < Constant.MAX_SIZE) {
+            int number = Randoms.pickNumberInRange(Constant.MIN_VALUE, Constant.MAX_VALUE);
+            if (!randomNumbers.contains(number))
+                randomNumbers.add(number);
+        }
+        return randomNumbers;
+    }
+}
