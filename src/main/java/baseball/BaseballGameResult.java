614663b753803e2f838d981b8f792d1efec33e70
SeungCheol <ssc6839@gmail.com>
2021-11-26 00:11:44 +0900

    feat: 게임 승리 검증 기능 구현

---
+++ src/main/java/baseball/BaseballGameResult.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class BaseballGameResult {
+
+    private static final String BASEBALL_RIGHT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public boolean checkRightAnswer(String hint) {
+        if (hint.equals("3스트라이크")) {
+            System.out.println(BASEBALL_RIGHT_ANSWER_MESSAGE);
+            return true;
+        }
+        return false;
+    }
+}
