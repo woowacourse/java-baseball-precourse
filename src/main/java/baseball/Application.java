02024a7f03721b5a81f5a9961d2df8155eff5306
TaeHyeon Kim <kth990303@naver.com>
2021-11-25 19:53:41 +0900

    feat: 컴퓨터의 수 생성 클래스 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,6 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class Application {
     public static void main(String[] args) {
-
+        Game game=new Game();
     }
 }
--- src/main/java/baseball/ComputerNumber.java
+++ src/main/java/baseball/ComputerNumber.java
@@ -1,9 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+
 /**
  * 컴퓨터의 정답 수를 만드는 클래스.
  * 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 랜덤으로 만든다.
  * pickNumberInRange() 메소드를 활용한다.
  */
 public class ComputerNumber {
+    private List<Integer> computerNumber;
+
+    public ComputerNumber() {
+        computerNumber = generateNumber();
+    }
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    public List<Integer> generateNumber() {
+        HashSet<Integer> numbers = new LinkedHashSet<>();
+        while (numbers.size() < 3) {
+            int number = Randoms.pickNumberInRange(BallInputNumber.RANGE_START, BallInputNumber.RANGE_END);
+            numbers.add(number);
+        }
+        return new ArrayList<>(numbers);
+    }
 }
