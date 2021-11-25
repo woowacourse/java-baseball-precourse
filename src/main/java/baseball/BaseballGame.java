84fdd844642b5f64c4d92366f68f5a9ed0a9d3bd
chawani <60432062+chawani@users.noreply.github.com>
2021-11-25 15:48:41 +0900

    feat: 컴퓨터와 플레이어의 3자리의 수를 비교
    컴퓨터와 플레이어의 숫자를 속성 값으로 가지게 함
    스트라이크, 볼, 낫싱을 계산하여 비교

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,6 +3,13 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGame {
+    private String computerNumber;
+    private String playerNumber;
+
+    public BaseballGame(String computerNumber, String playerNumber) {
+        this.computerNumber = computerNumber;
+        this.playerNumber = playerNumber;
+    }
 
     public String inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
@@ -22,4 +29,34 @@ public class BaseballGame {
             System.exit(1);
         }
     }
+
+    public int calcStrike() {
+        int count=0;
+        for(int i=0; i<computerNumber.length(); i++) {
+            if(computerNumber.charAt(i)==playerNumber.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int calcBall() {
+        int count=0;
+        for(int i=0; i<playerNumber.length(); i++) {
+            String s = playerNumber.substring(i,i+1);
+            int sameIndex = computerNumber.indexOf(s);
+            if(sameIndex != -1 && sameIndex != i) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean calcNothing() {
+        if(calcStrike() == 0 && calcBall() == 0) {
+            return true;
+        }
+        return false;
+    }
+
 }
