10f76e9f118d46507dc9ecbdbbb8593fff322174
YJGwon <yj970125@gmail.com>
2021-11-25 18:46:33 +0900

    feat(Application.java) : [시작]3자리 난수 생성 실행 구문 추가
    - BaseballUtil.generateAnswer() 이용 3자리 정답수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        LinkedHashSet<Integer> answer = BaseballUtil.generateAnswer(3);
+        // System.out.println(Arrays.toString(answerSet.toArray()));
     }
 }
