d1f3bd4b6bac9b251447852b81d9db14fb0b1f8b
minsulee <alkwen0996@naver.com>
2021-11-25 18:22:21 +0900

    refactor: 입력값 정답확인 기능 수정 user 클래스 - checkAnswer메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-        GameProgress gameProgress = new GameProgress();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.startGame();
     }
 }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,21 +4,35 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class User {
+
+    private static final int RANDOM_START_INCLUSIVE = 1;
+    private static final int RANDOM_END_INCLUSIVE = 9;
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해 주세요 : ";
+
+    public boolean checkAnswer(final String answer, final String guessNumber) {
+        return answer.equals(guessNumber);
+    }
+
     public void printInputMessage() {
-        System.out.print("숫자를 입력해 주세요 : ");
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
     public String inputPlayerNumber() {
         return Console.readLine();
     }
 
-    public String inputAnswerNumber() {
+    public String connectEachAnswerNumbers() {
         final StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < 3; i++) {
-            stringBuilder.append(Randoms.pickNumberInRange(1, 9));
+            stringBuilder.append(inputAnswerNumber());
         }
 
         return stringBuilder.toString();
     }
+
+    private int inputAnswerNumber() {
+        return Randoms.pickNumberInRange(RANDOM_START_INCLUSIVE, RANDOM_END_INCLUSIVE);
+    }
+
 }
