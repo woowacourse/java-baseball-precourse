476d96dc1aab868cab81f535882b18bb0317e5a8
minsulee <alkwen0996@naver.com>
2021-11-25 20:26:30 +0900

    feat: 게임 재시작 / 종료 메시지 출력 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -7,9 +7,21 @@ public class BaseballGame {
     public static final String STRIKE_ENGLISH = "strike";
     public static final String BALL_ENGLISH = "ball";
     public static final String COLLECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_NOT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void startGame() {
-        progressGame();
+
+            progressGame();
+            printRestartOrNotMessage();
+
+    }
+
+    private void printRestartOrNotMessage() {
+        System.out.println(RESTART_OR_NOT_MESSAGE);
+    }
+
+    private void printCorrectAnswer() {
+        System.out.println(COLLECT_ANSWER_MESSAGE);
     }
 
     public void progressGame() {
@@ -33,10 +45,6 @@ public class BaseballGame {
         }
 
         printCorrectAnswer();
-    }
-
-    private void printCorrectAnswer() {
-        System.out.println(COLLECT_ANSWER_MESSAGE);
     }
 
 }
