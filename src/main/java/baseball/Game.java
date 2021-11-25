2bb82fc6a046f35bfaa731b245bcd47ccfbd4220
DK <ldk980130@gmail.com>
2021-11-25 19:45:20 +0900

    refactor : 필요 없는 반복 제거

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,58 +2,58 @@ package baseball;
 
 public class Game {
 
-    public static String play(String answer, String input) {
+	public static String play(String answer, String input) {
 
-        String[] answerArray = answer.split("");
-        String[] inputArray = input.split("");
+		String[] answerArray = answer.split("");
+		String[] inputArray = input.split("");
 
-        int strike = 0;
-        int ball = 0;
+		int strike = 0;
+		int ball = 0;
 
-        for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 
-            String result = checkOneNumber(answerArray, inputArray[i], i);
+			String result = checkOneNumber(answerArray, inputArray[i], i);
 
-            if (result.equals(Message.STRIKE)) {
-                strike++;
-            } else if (result.equals(Message.BALL)) {
-                ball++;
-            }
-        }
+			if (result.equals(Message.STRIKE)) {
+				strike++;
+			} else if (result.equals(Message.BALL)) {
+				ball++;
+			}
+		}
 
-        String message = makeMessage(strike, ball, "");
+		String message = makeMessage(strike, ball, "");
 
-        return message;
-    }
+		return message;
+	}
 
-    private static String checkOneNumber(String[] answerArray, String number, int index) {
+	private static String checkOneNumber(String[] answerArray, String number, int index) {
 
-        for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 
-            if (answerArray[i].equals(number) && index == i) {
-                return Message.STRIKE;
-            } else if (answerArray[i].equals(number) && index != i) {
-                return Message.BALL;
-            }
-        }
+			if (answerArray[i].equals(number) && index == i) {
+				return Message.STRIKE;
+			} else if (answerArray[i].equals(number)) {
+				return Message.BALL;
+			}
+		}
 
-        return Message.NOTHING;
-    }
+		return Message.NOTHING;
+	}
 
-    private static String makeMessage(int strike, int ball, String message) {
+	private static String makeMessage(int strike, int ball, String message) {
 
-        if (ball == 0 && strike == 0) {
-            return Message.NOTHING;
-        }
+		if (ball == 0 && strike == 0) {
+			return Message.NOTHING;
+		}
 
-        if (ball > 0) {
-            message += ball + Message.BALL + " ";
-        }
+		if (ball > 0) {
+			message += ball + Message.BALL + " ";
+		}
 
-        if (strike > 0) {
-            message += strike + Message.STRIKE;
-        }
+		if (strike > 0) {
+			message += strike + Message.STRIKE;
+		}
 
-        return message;
-    }
+		return message;
+	}
 }
