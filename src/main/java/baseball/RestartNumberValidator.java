765fac39fd57ac4ac9eda5ad76f37c96d8ad3461
TaeHyeon Kim <kth990303@naver.com>
2021-11-25 20:47:43 +0900

    feat: RestartNumberValidator 클래스 구현

--- src/main/java/baseball/RestartNumberValidator.java
+++ src/main/java/baseball/RestartNumberValidator.java
@@ -5,4 +5,13 @@ package baseball;
  * 올바르지 않을 경우 예외를 발생시키고 종료시킨다.
  */
 public class RestartNumberValidator {
+    private String isRestart;
+
+    public RestartNumberValidator(String isRestart) {
+        this.isRestart=isRestart;
+    }
+
+    public boolean rightInputRestartCheck(){
+        return isRestart.equals(Game.RESTART) || isRestart.equals(Game.EXIT);
+    }
 }
