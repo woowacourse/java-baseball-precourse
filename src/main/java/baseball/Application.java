896526df47b4f765ec53f42337cee8f9e721ac6c
Ohzzi <clover7even@naver.com>
2021-11-25 15:04:29 +0900

    style: java 코딩 컨벤션에 맞게 수정
    LF와 CRLF 문제는 git 설정을 통해 수정하고, 코드 내에서는 indent 형식이 맞지 않는 부분들을 수정하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,9 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.run();
-    }
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+		BaseballGame baseballGame = new BaseballGame();
+		baseballGame.run();
+	}
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,21 +4,19 @@ import baseball.players.Computer;
 import baseball.players.Player;
 
 public class BaseballGame {
+	private Computer computer;
+	private Player player;
+	private int ballCount;
+	private int strikeCount;
 
-    private Computer computer;
-    private Player player;
-    private int ballCount;
-    private int strikeCount;
+	public BaseballGame() {
+		this.computer = new Computer();
+		this.player = new Player();
+		this.ballCount = 0;
+		this.strikeCount = 0;
+	}
 
-    public BaseballGame() {
-        this.computer = new Computer();
-        this.player = new Player();
-        this.ballCount = 0;
-        this.strikeCount = 0;
-    }
-
-    public void run() {
-        System.out.println("게임을 시작합니다.");
-    }
-
+	public void run() {
+		System.out.println("게임을 시작합니다.");
+	}
 }
