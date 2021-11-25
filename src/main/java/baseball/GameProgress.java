a268055c9c9027f4febc08da4a8f479338ece17d
minsulee <alkwen0996@naver.com>
2021-11-25 13:20:50 +0900

    feat: 입력값 정답 확인 기능 추가

--- src/main/java/baseball/GameProgress.java
+++ src/main/java/baseball/GameProgress.java
@@ -1,28 +1,21 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class GameProgress {
-    private static void printInputMessage(){
-        System.out.print("숫자를 입력해 주세요 : ");
-    }
 
-    private static String inputPlayerNumber(){
-        return Console.readLine();
-    }
-
-    private static int inputAnswerNumber(){
-        return Randoms.pickNumberInRange(0,3);
-    }
-
-    private static void gameProgress(){
-        int answer = inputAnswerNumber();
-        printInputMessage();
-        String inputNumber = inputPlayerNumber();
-    }
-
-    private static void GameStart(){
+    public void gameStart() {
         gameProgress();
+    }
+
+    public void gameProgress() {
+        User user = new User();
+
+        String answer = user.inputAnswerNumber();
+        user.printInputMessage();
+        String inputNumber = user.inputPlayerNumber();
+
+        while(!answer.equals(inputNumber)){
+            user.printInputMessage();
+            inputNumber = user.inputPlayerNumber();
+        }
     }
 }
