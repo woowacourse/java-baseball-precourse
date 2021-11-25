5917de4b50a94a2a3a7ffdf3ba6939e799784f8e
Seungmin Lim <seungmin9066@gmail.com>
2021-11-25 14:31:56 +0900

    [수정] README에 적합하도록 메서드 목록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,49 +1,69 @@
 package baseball;
 import java.util.*;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class Application {
 
     public static final String START_MESSAGE = "숫자를 입력해주세요 : ";
-    public static final String MESSAGE_ABOUT_WRONG_INPUT = "3자리 숫자를 입력해야 합니다.";
-    public static final String HINT_MESSAGE = "볼 , 스트라이크";
+    public static final String MESSAGE_ABOUT_WRONG_INPUT = "잘못된 입력입니다.";
+    public static final String HINT_MESSAGE = "%d볼 %d스트라이크";
+    public static final int LENGTH_OF_ANSWER = 3;
 
-    static int[] answerNumber = new int[3];
+    public static List<Integer> answerNumber = new ArrayList<>();
 
     public static void main(String[] args) {
-
-        setAnswerNumber();
-
         while (true) {
+            setAnswerNumber(); // 정답 설정 부분
             String number;
-            Scanner sc = new Scanner(System.in);
             System.out.print(START_MESSAGE);
-            number = sc.next();
-            if (checkWrongInput(number)){
-                System.out.println(MESSAGE_ABOUT_WRONG_INPUT);
-                continue;
-            }
-            compareNumber(number);
+            number = Console.readLine();
+            checkWrongInput(number);
+            giveHintMessage(number);
         }
     }
 
     public static void setAnswerNumber(){
-        List<Integer> possibleNumber = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for(int i=0; i<3; i++){
-            int randomValue = (int)(Math.random()*(9 - i));
-            answerNumber[i] = possibleNumber.get(randomValue);
-            possibleNumber.remove(randomValue);
+        answerNumber = Randoms.pickUniqueNumbersInRange(1, 10, 3);
+    }
+
+    public static void checkWrongInput(String number){
+        boolean isNumeric = number.matches("[+-]?\\d*(\\.\\d+)?");
+        if (!isNumeric || number.length() != LENGTH_OF_ANSWER){
+            throw new IllegalArgumentException(MESSAGE_ABOUT_WRONG_INPUT);
         }
     }
 
-    public static boolean checkWrongInput(String number){
-        int numberLength = number.length();
-        return numberLength == 3;
+    public static void giveHintMessage(String number) {
+        int[] info_hint = compareNumber(number);
+
     }
 
-    public static String compareNumber(String number){
-        int strikeNumber = 0;
+    public static int[] compareNumber(String number){
         int ballNumber = 0;
-        return "1";
+        int strikeNumber = 0;
+        for (int i=0; i<3; i++){
+            strikeNumber += countStrikeNumber(answerNumber.get(i), Character.getNumericValue(number.charAt(i)));
+            ballNumber += countBallNumber(i, Character.getNumericValue(number.charAt(i)));
+        }
+        return new int[] {ballNumber, strikeNumber};
     }
+
+    public static int countStrikeNumber(int answer, int user_input){
+        if (answer == user_input){
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int countBallNumber(int nth_order , int user_input){
+        if (answerNumber.contains(user_input) && answerNumber.get(nth_order) != user_input)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
 }
