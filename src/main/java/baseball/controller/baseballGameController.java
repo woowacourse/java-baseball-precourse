5a4445f4209c2cb76da42ab0439d261542536dc8
jaeyoungjang2 <lion85488@gmail.com>
2021-11-25 16:21:49 +0900

    feat: vew와 model 을 관리하는 controller 역할

--- src/main/java/baseball/controller/baseballGameController.java
+++ src/main/java/baseball/controller/baseballGameController.java
@@ -1,14 +1,46 @@
 package baseball.controller;
 
-import baseball.util.Random;
-import java.util.List;
-
+import baseball.domain.Computer;
+import baseball.domain.BaseBallNumbers;
+import baseball.view.InputView;
+import baseball.view.ResultView;
 
 public class baseballGameController {
 
     public static void run() {
-        List<Integer> randomNumbers = Random.extractNumbers();
-
+        runBaseBallGame();
+        checkContinue();
 
     }
+
+    private static void checkContinue() {
+        String continueMessage = InputView.askContinue();
+
+        if (continueMessage.equals("1")) {
+            runBaseBallGame();
+            return;
+        }
+
+        if (continueMessage.equals("2")) {
+            ResultView.printGameEnd();
+            return;
+        }
+
+        throw new IllegalArgumentException("잘못 된 입력입니다. (1또는 2를 입력해야 합니다.)");
+    }
+
+    private static void runBaseBallGame() {
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        Computer computer = new Computer();
+        while (strikeCount != 3) {
+            BaseBallNumbers baseBallNumbers = new BaseBallNumbers(InputView.requireBaseBallNumber());
+            ballCount = baseBallNumbers.calculateBallCount(computer.randomNumbers());
+            strikeCount = baseBallNumbers.calculateStrikeCount(computer.randomNumbers());
+            ResultView.printResult(ballCount, strikeCount);
+        }
+        ResultView.printResultInfoMessage();
+    }
+
 }
