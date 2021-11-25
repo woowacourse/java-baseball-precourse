453c8ac33a1b28887ed1a66618f3e583c145e90d
BeomsooKim <kbsat@naver.com>
2021-11-25 12:40:17 +0900

    test(RefereeTest): check() 테스트 수정

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -8,11 +8,11 @@ class RefereeTest {
 	@Test
 	public void check() throws Exception {
 		//given
-		Referee referee1 = new Referee(new int[] {1, 2, 3}, new int[] {1, 3, 2});
-		Referee referee2 = new Referee(new int[] {1, 2, 3}, new int[] {4, 5, 6});
+		Referee referee1 = new Referee(new int[] {1, 2, 3});
+		Referee referee2 = new Referee(new int[] {1, 2, 3});
 		//when
-		Result result1 = referee1.check();
-		Result result2 = referee2.check();
+		Result result1 = referee1.check(new int[] {1, 3, 2});
+		Result result2 = referee2.check(new int[] {4, 5, 6});
 		//then
 		assertEquals(result1.getStrikeCount(), 1);
 		assertEquals(result1.getBallCount(), 2);
