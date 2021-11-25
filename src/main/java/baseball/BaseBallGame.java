7f963440d22b3403071cb30b01acdc8143e82ddf
EastHShin <xldk78@gmail.com>
2021-11-25 16:06:03 +0900

    feat: 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시키는 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -7,10 +7,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class BaseBallGame {
 	private static final int NUM_OF_DIGIT = 3;
+	private static final char START_CHAR = '1';
+	private static final char END_CHAR = '9';
+	
 	public List<Integer> getInput() {
 		String readLine = Console.readLine();
 		List<Integer> inputNumbers = new ArrayList<>();
-		if (isValidInput(readLine)) {
+		if (!isValidInput(readLine)) {
 			throw new IllegalArgumentException();
 		}
 		for (int i = 0; i < NUM_OF_DIGIT; i++) {
@@ -18,5 +21,26 @@ public class BaseBallGame {
 			inputNumbers.add(num);
 		}
 		return inputNumbers;
+	}
+
+	private static boolean isValidInput(String readLine) {
+		int readLineLength = readLine.length();
+		if(readLineLength != NUM_OF_DIGIT){
+			return false;
+		}
+		for (int i = 0; i < NUM_OF_DIGIT; i++) {
+			char number = readLine.charAt(i);
+			if (!isValidNumber(number)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isValidNumber(char number) {
+		if (number < START_CHAR || number > END_CHAR) {
+			return false;
+		}
+		return true;
 	}
 }
