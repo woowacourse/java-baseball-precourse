e93316f1aa41cdc29765aea1aec8ee13d4f612a9
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-25 21:05:38 +0900

    style(BaseballCount, BaseballSystem): change some fomatting && value name

--- src/main/java/baseball/BaseballCount.java
+++ src/main/java/baseball/BaseballCount.java
@@ -6,26 +6,26 @@ public class BaseballCount {
 	public int[] userNums;
 	public int[] computerNums;
 
-	public  BaseballCount(int[] userNums, int[] computerNums) {
+	public BaseballCount(int[] userNums, int[] computerNums) {
 		this.userNums = userNums;
 		this.computerNums = computerNums;
-		count();
+		this.count();
 	}
 
 	public void count() {
 		for (int i = 0; i < 3; i++) {
 			if (userNums[i] == computerNums[i]) {
-				strikeCount ++;
+				strikeCount++;
 				continue;
 			}
 			if (isInArray(userNums[i], this.computerNums)) {
-				ballCount ++;
+				ballCount++;
 			}
 		}
 	}
 
 	public boolean isInArray(int num, int[] numArray) {
-		for (int arrayNum:numArray) {
+		for (int arrayNum : numArray) {
 			if (arrayNum == num) {
 				return true;
 			}
--- src/main/java/baseball/BaseballSystem.java
+++ src/main/java/baseball/BaseballSystem.java
@@ -10,19 +10,19 @@ public class BaseballSystem {
 	public void gameStart() {
 		do {
 			int[] userNums = new UserNumGenerator().nums;
-			BaseballCount baseballCount = new BaseballCount(userNums, computerNums);
-			if (baseballCount.strikeCount == 3) {
-				System.out.println("3스트라이크");
-				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			BaseballCount set = new BaseballCount(userNums, computerNums);
+
+			if (set.strikeCount == 3) {
+				System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 				break;
 			}
-			if (baseballCount.ballCount != 0) {
-				System.out.print(baseballCount.ballCount + "볼 ");
+			if (set.ballCount != 0) {
+				System.out.print(set.ballCount + "볼 ");
 			}
-			if (baseballCount.strikeCount != 0) {
-				System.out.print(baseballCount.strikeCount + "스트라이크");
+			if (set.strikeCount != 0) {
+				System.out.print(set.strikeCount + "스트라이크");
 			}
-			if (baseballCount.ballCount == 0 && baseballCount.strikeCount == 0) {
+			if (set.ballCount == 0 && set.strikeCount == 0) {
 				System.out.print("낫싱");
 			}
 			System.out.println();
