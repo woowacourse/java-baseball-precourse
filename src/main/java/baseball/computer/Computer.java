d608ae48394a9d363803205141aaeb50b2a0cae2
Mo0nl19ht <mool2480@gmail.com>
2021-11-25 21:27:10 +0900

    refactor:Computer target Type  Interger -> int 변경

--- src/main/java/baseball/computer/Computer.java
+++ src/main/java/baseball/computer/Computer.java
@@ -4,7 +4,7 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Computer {
     //맞춰야 할 숫자
-    public Integer[] target = new Integer[3];
+    public int[] target = new int[3];
 
     //Computer 인스턴스 생성시 target 만들어짐
     public Computer(){
@@ -12,7 +12,7 @@ public class Computer {
 
         for (int i=1;i<3;i++){
             //주어진 메소드를 활용하여 1~9사이의 수를 랜덤으로 불러온다
-            Integer number=pickNumberInRange(1,9);
+            int number=pickNumberInRange(1,9);
             if (target[i-1]==number){
                 i--;
                 continue;
