90c97efd7e71fe06fe97f818bb0424d5ad49a9a9
her0807 <her0807@naver.com>
2021-11-25 15:33:47 +0900

    fix(computer): 순서 유지를 위한 자료형 변경
    - HashSet -> LinkedHashSet 으로 변경하여 순서 보장
    - 예외사항 체크를 위한 메서드 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,8 @@
 package baseball;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -11,17 +12,20 @@ public class Computer {
     private static final int END_RANGE = 9;
     private static final int SIZE_RANGE = 3;
     private static Set<Integer> randomNumbers;
+    private static BoundaryChecker boundaryChecker;
 
     public Computer() {
-        this.randomNumbers = new HashSet<>();
+        this.randomNumbers = new LinkedHashSet<>();
+        this.boundaryChecker = new BoundaryChecker();
         this.setRandomNumbers();
     }
 
-    public static void setRandomNumbers() {
+    public void setRandomNumbers() {
         while (randomNumbers.size() < SIZE_RANGE) {
             int choice = Randoms.pickNumberInRange(START_RANGE, END_RANGE);
             randomNumbers.add(choice);
         }
+        boundaryChecker.checkRange(Integer.parseInt(toString()));
     }
 
     public ArrayList<Integer> convertToList() {
@@ -35,7 +39,8 @@ public class Computer {
         randomNumbers.clear();
     }
 
+    @Override
     public String toString() {
-        return randomNumbers.toString();
+        return randomNumbers.toString().replaceAll("[^0-9]", "");
     }
 }
