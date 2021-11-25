ad0cd13600d96d0129348515310ae7825c04017d
wannagohome <iwwuf7@gmail.com>
2021-11-25 11:00:01 +0900

    feat: calculate baseball game result and print
     - 유저 입력 값에 대한 야구 결과를 도출하고 출력한다
     - 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,8 +6,31 @@ public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         String targetNumber = NumberGenerator.generate();
+        System.out.println(targetNumber);
         String inputNumber = getConsoleInput();
         validateInput(inputNumber);
+
+        int ballCount = 0;
+        int strikeCount = 0;
+        for (int i = 0; i < inputNumber.length(); i++) {
+            if (inputNumber.charAt(i) == targetNumber.charAt(i)) {
+                strikeCount++;
+            } else if (targetNumber.contains(String.valueOf(inputNumber.charAt(i)))) {
+                ballCount++;
+            }
+        }
+
+        String result = "";
+        if (ballCount > 0) {
+            result += ballCount + "볼";
+        }
+        if (strikeCount > 0) {
+            result += strikeCount + "스트라이크";
+        }
+        if (ballCount == 0 && strikeCount == 0) {
+            result = "낫싱";
+        }
+        System.out.println(result);
     }
 
     private static String getConsoleInput() {
