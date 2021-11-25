612e95e8f476133529927e72810ac1be21c2d833
Seongwon97 <osw970325@naver.com>
2021-11-25 22:26:12 +0900

    feat : add gameStart func
    - create a game class that manages game-related methods
    - create a gameStart func to start a new game

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class Game {
+
+    public void gameStart() {
+        System.out.println("Game start");
+
+        int answer = 0;
+        int input = 0;
+        boolean correct = false;
+//        while (!correct) {
+//        }
+    }
+}
