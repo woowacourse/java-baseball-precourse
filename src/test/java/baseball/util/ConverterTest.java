1113ae46ab8dec95698620a6e92ba4885593ffff
BeomsooKim <kbsat@naver.com>
2021-11-25 11:11:42 +0900

    test(ConverterTest): Converter 단위테스트 추가

---
+++ src/test/java/baseball/util/ConverterTest.java
@@ -0,0 +1,17 @@
+package baseball.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConverterTest {
+	@Test
+	public void convertToIntArr() throws Exception {
+	    //given
+		String str = "1234567890";
+	    //when
+		int[] result = Converter.convertToIntArr(str);
+		//then
+		assertArrayEquals(result, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+	}
+}
