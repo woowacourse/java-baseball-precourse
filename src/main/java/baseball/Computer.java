e3640953bae673fa73b28db80935411f19953070
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-11-26 04:22:17 +0900

    fix(Computer): 중복 처리 로직 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,29 +2,25 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Computer {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
     private static final int LENGTH_NUMBER = 3;
 
-    public static String generateNumbers() {
-        int[] computerNumbers = new int[LENGTH_NUMBER];
-
+    public static String generateNumber() {
+        int[] computerNumber = new int[LENGTH_NUMBER];
         int i = 0;
         do {
-            computerNumbers[i] = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-            if (i > 0 && computerNumbers[i] == computerNumbers[i - 1]) {
+            int newNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (Arrays.stream(computerNumber).anyMatch(k -> k == newNumber)) {
                 continue;
             }
+            computerNumber[i] = newNumber;
             i = i + 1;
         } while (i < LENGTH_NUMBER);
 
-        return Arrays.toString(computerNumbers);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(generateNumbers());
+        return Arrays.toString(computerNumber).replaceAll("[^0-9]","");
     }
 }
