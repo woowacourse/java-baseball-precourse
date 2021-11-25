7ac390f33aca605e5148a4cfb2fea32d0e390bcf
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-11-26 02:50:26 +0900

    feat: 서로 다른 3자리 난수 생성

---
+++ src/main/java/baseball/RandomNumberGenerator.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+	private static final int MAX_LENGTH = 3;
+
+	public List<Integer> generate() {
+		List<Integer> answer = new ArrayList<>();
+		while (answer.size() < MAX_LENGTH) {
+			addNotDuplicatedNumber(answer);
+		}
+		return answer;
+	}
+
+	private void addNotDuplicatedNumber(List<Integer> answer) {
+		int number = Randoms.pickNumberInRange(1, 9);
+		if (answer.contains(number)) {
+			answer.add(number);
+		}
+		return;
+	}
+}
