32527ffeb923a8a532123cbf9a21460b3e27fe5f
EastHShin <xldk78@gmail.com>
2021-11-26 01:45:36 +0900

    feat: ball 개수를 구하는 기능 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -32,6 +32,31 @@ public class BaseBallGame {
 		return numOfStrike;
 	}
 
+	private int getBallCount() {
+		int numOfBall = 0;
+		for (int curPlayerIdx = 0; curPlayerIdx < NUM_OF_DIGIT; curPlayerIdx++) {
+			int playerNum = playerNumbers.get(curPlayerIdx);
+			if (sameNumWithoutCur(curPlayerIdx, playerNum)) {
+				numOfBall += 1;
+			}
+		}
+		return numOfBall;
+	}
+
+	private boolean sameNumWithoutCur(int curPlayerIdx, int playerNum) {
+		for (int i = 0; i < NUM_OF_DIGIT; i++) {
+			if (i == curPlayerIdx) {
+				continue;
+			}
+			int randomNum = randomNumbers.get(i);
+
+			if (randomNum == playerNum) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public List<Integer> getInput() {
 		String readLine = Console.readLine();
 		List<Integer> inputNumbers = new ArrayList<>();
