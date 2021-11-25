fbdff55f01d9e699f6edf0924735806de3afc86f
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-11-26 00:31:57 +0900

    feat(Guess): 사용자의 입력을 표현하는 일급 컬렉션 추가
    Guess 클래스 추가
    - 콘솔에서 입력받은 문자열이 규칙에 맞는지 검증한 후 리스트로 변환한다. 만약 규칙에 맞지 않는다면 IllegalArgumentException을 던진다.
    - 문자열이 모두 숫자로 이루어져있는지 검증한다.
    - 문자열에 중복된 요소가 있는지 검증한다.
    - 각 요소가 주어진 범위 사이에 있는지 검증한다.
    - 문자열이 주어진 크기와 같은지 검증한다.

---
+++ src/main/java/baseball/domain/Guess.java
@@ -0,0 +1,64 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Guess {
+
+    private final List<Integer> guess;
+
+    public Guess(String userInput) {
+        validateUserInput(userInput);
+        this.guess = toList(userInput);
+    }
+
+    private List<Integer> toList(String userInput) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (char aChar : userInput.toCharArray()) {
+            result.add(Character.getNumericValue(aChar));
+        }
+        return result;
+    }
+
+    private void validateUserInput(String userInput) {
+        validateSize(userInput);
+        validateIsNumericValue(userInput);
+        validateDuplicate(userInput);
+        validateRange(userInput);
+    }
+
+    private void validateSize(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("The length of the user input must be 3");
+        }
+    }
+
+    private void validateIsNumericValue(String userInput) {
+        for (char c : userInput.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("User input should be a numeric value");
+            }
+        }
+    }
+
+    private void validateDuplicate(String userInput) {
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < userInput.length(); i++) {
+            set.add(userInput.charAt(i));
+        }
+        if (set.size() != userInput.length()) {
+            throw new IllegalArgumentException("All digits should be unique");
+        }
+    }
+
+    private void validateRange(String userInput) {
+        for (char c : userInput.toCharArray()) {
+            int digit = Character.getNumericValue(c);
+            if (digit < 1 || digit > 9) {
+                throw new IllegalArgumentException(
+                    "All digits should not be less than 1 and should not be greater than 9");
+            }
+        }
+    }
+}
