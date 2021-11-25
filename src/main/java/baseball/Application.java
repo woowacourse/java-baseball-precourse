1033788e64cb704b5fc8c428526dcf2f1f0036ec
hyeonic <dev.hyeonic@gmail.com>
2021-11-25 21:52:21 +0900

    feat: 정답을 맞출 때 까지 게임을 진행할 수 있도록 GameMachine 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
+import baseball.domain.BaseballReferee;
 import baseball.domain.GameMachine;
+import baseball.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
-        GameMachine gameMachine = new GameMachine();
+        GameMachine gameMachine = new GameMachine(new BaseballReferee(), new InputView());
         gameMachine.run();
     }
 }
--- src/main/java/baseball/domain/GameMachine.java
+++ src/main/java/baseball/domain/GameMachine.java
@@ -1,6 +1,36 @@
 package baseball.domain;
 
+import java.util.List;
+
+import baseball.view.InputView;
+
 public class GameMachine {
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    private final BaseballReferee baseballReferee;
+    private final InputView inputView;
+
+    public GameMachine(BaseballReferee baseballReferee, InputView inputView) {
+        this.baseballReferee = baseballReferee;
+        this.inputView = inputView;
+    }
+
     public void run() {
+        start(new BaseballNumbers());
+    }
+
+    private void start(BaseballNumbers baseballNumbers) {
+        System.out.println(baseballNumbers.getNumbers());
+        while (true) {
+            List<Integer> inputNumbers = inputView.getInputNumbers();
+            BaseballState baseballState = baseballReferee.referee(baseballNumbers.getNumbers(), inputNumbers);
+
+            System.out.println(baseballState.getMessage());
+
+            if (baseballState.isSuccess()) {
+                System.out.println(END_MESSAGE);
+                break;
+            }
+        }
     }
 }
