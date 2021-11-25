c598dd9158b2f7c8f01d900c68b538213518d36d
devHudi <devhudi@gmail.com>
2021-11-25 14:52:04 +0900

    fix(hint): 낫싱 표기 누락 해결

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,9 +4,10 @@ import domain.Hint;
 
 public class OutputView {
 	private static final String INPUT_INSTRUCTION = "숫자를 입력해주세요 : ";
+	private static final String CONGRATULATIONS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	private static final String BALL = "볼";
 	private static final String STRIKE = "스트라이크";
-	private static final String CONGRATULATIONS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String NOTHING = "낫싱";
 
 	public static void printInstruction() {
 		System.out.print(OutputView.INPUT_INSTRUCTION);
@@ -15,13 +16,14 @@ public class OutputView {
 	public static void printHint(Hint hint) {
 		int ballCount = hint.getBall(), strikeCount = hint.getStrike();
 
-		if (ballCount > 0) {
+		if (ballCount > 0)
 			System.out.print(ballCount + BALL + " ");
-		}
 
-		if (strikeCount > 0) {
+		if (strikeCount > 0)
 			System.out.print(strikeCount + STRIKE);
-		}
+
+		if (ballCount + strikeCount == 0)
+			System.out.print(NOTHING);
 
 		System.out.println();
 
