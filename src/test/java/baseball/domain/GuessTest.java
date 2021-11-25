488000251042821b881a9afef2768afddc280bce
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-11-26 00:33:18 +0900

    test(Guess): Guess의 검증 로직을 테스트

---
+++ src/test/java/baseball/domain/GuessTest.java
@@ -0,0 +1,41 @@
+package baseball.domain;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GuessTest {
+
+    @Test
+    void input_must_be_a_numeric_value() {
+        assertThatThrownBy(() -> {
+            new Guess("a12");
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("numeric");
+    }
+
+    @Test
+    void all_digits_must_be_unique() {
+        assertThatThrownBy(() -> {
+            new Guess("111");
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("unique");
+    }
+
+    @Test
+    void test_validate_range() {
+        assertThatThrownBy(() -> {
+            new Guess("901");
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("less than");
+    }
+
+    @Test
+    void test_validate_size() {
+        assertThatThrownBy(() -> {
+            new Guess("12345");
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("length");
+    }
+}
