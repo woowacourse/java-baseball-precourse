f1a964b18c2e18a7e08c4b023092499687aeaa05
Hyunta <mohai2618@naver.com>
2021-11-25 19:40:52 +0900

    feat : 게임 재시작 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
+		BaseballGame baseballGame = new BaseballGame();
+		baseballGame.start();
 	}
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,22 +5,36 @@ import camp.nextstep.edu.missionutils.Console;
 public class BaseballGame {
 	private RandomNumberGenerator randomNumberGenerator;
 	private Hint hint;
+	private boolean running;
 
 	public BaseballGame() {
 		this.randomNumberGenerator = new RandomNumberGenerator();
 		this.hint = new Hint();
+		this.running = true;
 	}
 
 	public void start() {
 		do {
 			int[] answer = randomNumberGenerator.makeRandomNumber();
 			playGame(answer);
-		} while (true);//TODO 재시작 여부
+		} while (restartGame());//TODO 재시작 여부
+	}
 
+	private boolean restartGame() {
+		System.out.println(Message.REQUEST_RESTART_NUMBER);
+		String requestInput = Console.readLine();
+		Validation.checkRestartInput(requestInput);
+		int inputValue = Integer.parseInt(requestInput);
+		if (inputValue == 1) {
+			running = true;
+			return true;
+		}
+		System.out.println(Message.EXIT_GAME);
+		return false;
 	}
 
 	private void playGame(int[] answer) {
-		while (true) {
+		while (running) {
 			System.out.print(Message.REQUEST_ENTER_NUMBER);
 			String userInput = Console.readLine();
 			Validation.checkInput(userInput);
@@ -29,9 +43,8 @@ public class BaseballGame {
 
 			if (hint.getStrike() == Rules.NUMBER_LENGTH) {
 				System.out.println(Message.GAME_CLEAR);
+				running = false;
 			}
-
-
 		}
 	}
 }
