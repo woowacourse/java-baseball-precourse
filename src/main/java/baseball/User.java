715af1b0a1cb0975bf6f9faca1fc8d725303884f
iamsseaa <homingsea@gmail.com>
2021-11-25 18:03:34 +0900

    feat: 유저 입력이 3자리가 아닌 경우 예외 처리

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -8,10 +8,15 @@ public class User {
 
     // 사용자에게 3자리의 수(문자열 형태)로 리턴하는 메소드
     public static String inputNumber() {
+
         input = Console.readLine();
+
+
+        if(input.length() != 3) {
+            throw new IllegalArgumentException("잘못 입력하셨습니다.");
+        }
         return input;
     }
-
 
 
 }
