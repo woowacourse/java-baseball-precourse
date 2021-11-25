8f234898021b0368a362a920d99457cbccc81190
Jihyun <bjih1999@naver.com>
2021-11-25 18:07:17 +0900

    feat: 입력한 값과 랜덤 값을 비교한 결과를 출력하는 기능
    GameExecutor
     - printScore 메소드 추가 (볼과 스트라이크  결과를 출력)
     - printNothing 메소드 추가 (낫싱인지 여부를 출력)
     - printCorrect 메소드 추가 (정답인지 여부를 출력)
     - getUserSelection 메소드 추가(게임 종료 후 완전 종료 여부를 입력받음)

--- src/main/java/baseball/GameExecutor.java
+++ src/main/java/baseball/GameExecutor.java
@@ -30,4 +30,29 @@ public class GameExecutor {
         return numberList;
     }
 
+    public void printScore(int strikeCount, int ballCount) {
+        String result = "";
+
+        if (ballCount != 0) {
+            result = result.concat(ballCount+"볼 ");
+        }
+        if (strikeCount != 0) {
+            result = result.concat(strikeCount +"스트라이크");
+        }
+        System.out.println(result);
+    }
+
+    public void printNothing() {
+        System.out.println("낫싱");
+    }
+
+    public void printCorrect() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public int getUserSelection() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(Console.readLine());
+    }
+
 }
