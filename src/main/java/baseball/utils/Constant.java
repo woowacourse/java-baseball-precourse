d79d14b7beaa1e6ae417b94190f9867ccfab10af
Ohzzi <clover7even@naver.com>
2021-11-25 13:04:52 +0900

    feat: 랜덤으로 정답(서로 다른 3개의 수) 생성
    Computer 클래스의 인스턴스가 생성될 때 서로 다른 3개의 수로 이루어진 정답 숫자 배열을 생성하는 기능 추가

---
+++ src/main/java/baseball/utils/Constant.java
@@ -0,0 +1,11 @@
+package baseball.utils;
+
+public class Constant {
+
+    /* 랜덤 및 입력으로 받는 숫자의 길이 */
+    public static final int LENGTH_OF_NUMBERS = 3;
+    /* 각 자리의 숫자의 최솟값 및 최댓값 */
+    public static final int MIN_NUM = 1;
+    public static final int MAX_NUM = 9;
+
+}
