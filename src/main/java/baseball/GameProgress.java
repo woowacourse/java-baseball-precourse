7e1494da94f6139ec5a76d49e18671cd564c0f1c
minsulee <alkwen0996@naver.com>
2021-11-25 10:57:02 +0900

    feat: 유저 3자리 숫자 입력 기능 추가

--- src/main/java/baseball/GameProgress.java
+++ src/main/java/baseball/GameProgress.java
@@ -1,2 +1,22 @@
-package baseball;public class GameProgress {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameProgress {
+    private static void printInputMessage(){
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+    private static String inputPlayerNumber(){
+        return Console.readLine();
+    }
+
+    private static void gameProgress(){
+        printInputMessage();
+        String inputNumber = inputPlayerNumber();
+    }
+
+    private static void GameStart(){
+        gameProgress();
+    }
 }
