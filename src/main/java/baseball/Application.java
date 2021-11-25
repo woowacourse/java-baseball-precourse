3dad35db0f5d099b14825ced95ebe1126d23173d
iamsseaa <homingsea@gmail.com>
2021-11-25 20:53:07 +0900

    feat: 유저가 입력한 수를 정수형 배열로 저장하는 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,18 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+
+    }
+
+    // 입력받은 수를 String 에서 int array 로 저장
+    public static int[] stringToInt(String inputNumber) {
+        int[] userNumber = new int[3];
+
+        for(int i=0; i<3; i++) {
+            userNumber[i] = Character.getNumericValue(inputNumber.charAt(i));
+        }
+
+        return userNumber;
     }
 }
