c75ab63f3cf4785698a6d95700cd02ab2030d8eb
hyeonic <dev.hyeonic@gmail.com>
2021-11-25 22:25:21 +0900

    feat: 게임 재시작을 여부 반환을 위한 InputView isRestart 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,14 +8,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String INPUT_NUMBERS_DELIMITER = "";
     private static final int INPUT_NUMBERS_SIZE = 3;
+    private static final String INPUT_RESTART_COMMAND = "1";
+    private static final String INPUT_END_COMMAND = "2";
 
     public List<Integer> getInputNumbers() {
         System.out.print(INPUT_NUMBERS_MESSAGE);
         String inputNumbers = Console.readLine();
         validateSize(inputNumbers.length());
         return toList(inputNumbers);
+    }
+
+    public boolean isRestart() {
+        String command = inputCommand();
+        validateCommand(command);
+
+        if (command.equals(INPUT_RESTART_COMMAND)) {
+            return true;
+        }
+        return false;
     }
 
     private void validateSize(int size) {
@@ -28,5 +41,16 @@ public class InputView {
         return Arrays.stream(inputNumbers.split(INPUT_NUMBERS_DELIMITER))
             .map(Integer::parseInt)
             .collect(Collectors.toList());
+    }
+
+    private String inputCommand() {
+        System.out.println(INPUT_RESTART_MESSAGE);
+        return Console.readLine();
+    }
+
+    private void validateCommand(String command) {
+        if (!command.equals(INPUT_RESTART_COMMAND) && !command.equals(INPUT_END_COMMAND)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
