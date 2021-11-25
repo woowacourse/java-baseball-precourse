7112901123c58387c12f15210760448c77b86f7a
HyeonbinSa <shb1833@gmail.com>
2021-11-25 21:42:45 +0900

    feat: 사용자 숫자 입력 기능 구현
    Class : User

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,16 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer();
-        System.out.println(computer.createComputerNumber());
+        ArrayList<Integer> computerNumber = computer.createComputerNumber();
+        System.out.println(computerNumber);
+
+        User user = new User();
+        ArrayList<Integer> userNumber = user.inputUserNumber();
+        System.out.println(userNumber);
+
     }
 }
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,81 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    private static final int LENGTH = 3;
+    private static final char MAX = '9';
+    private static final char MIN = '1';
+    private static final String INPUT_MSG = "숫자를 입력해주세요 : ";
+    private static String inputNumber;
+    private static ArrayList<Integer> userNumberList;
+
+    public ArrayList<Integer> inputUserNumber() {
+        System.out.print(INPUT_MSG);
+        inputNumber = Console.readLine();
+        String[] inputNumberList = inputNumber.split("");
+        if (this.validateInputNumber(inputNumberList)) {
+            this.changeArrayList(inputNumberList);
+        }
+        return userNumberList;
+    }
+
+    private void changeArrayList(String[] inputNumberList) {
+        userNumberList = new ArrayList<Integer>();
+        for (String number : inputNumberList) {
+            userNumberList.add(Integer.parseInt(number));
+        }
+    }
+
+    private boolean validateInputNumber(String[] inputNumberList) {
+        /* Case 1. 입력한 문자열이 3자리가 아닐 경우 */
+        if (!this.validateNumberLength(inputNumberList)) {
+            return false;
+        }
+        /* Case 2. 1~9가 아닌 값이 들어간 경우 */
+        if (!this.validateOnlyNumberInString(inputNumberList)) {
+            return false;
+        }
+        /* Case 3. 중복되는 숫자가 존재할 경우 */
+        if (!this.checkDuplicateNumberInString(inputNumberList)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /* Case 1. 입력한 문자열이 3자리가 아닐 경우 */
+    private boolean validateNumberLength(String[] inputNumberList) {
+        if (inputNumberList.length == LENGTH) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Case 2. 1~9가 아닌 값이 들어간 경우 */
+    private boolean validateOnlyNumberInString(String[] inputNumberList) {
+        for (String userNumber : inputNumberList) {
+            if (userNumber.charAt(0) < MIN || userNumber.charAt(0) > MAX) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /* Case 3. 중복되는 숫자가 존재할 경우 */
+    private boolean checkDuplicateNumberInString(String[] inputNumberList) {
+        Set<String> numberSet = new HashSet<String>();
+        for (String userNumber : inputNumberList) {
+            numberSet.add(userNumber);
+        }
+        if (numberSet.size() == LENGTH) {
+            return true;
+        }
+        return false;
+    }
+
+}
