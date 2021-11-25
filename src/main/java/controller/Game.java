39e1874b368855a0aee4e274ef859305c3b90548
devHudi <devhudi@gmail.com>
2021-11-25 15:40:55 +0900

    feat: 게임 종료, 재시작 선택 기능 구현

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -8,6 +8,9 @@ import view.OutputView;
 
 public class Game {
 
+	private static final String RESTART_INPUT = "1";
+	private static final String QUIT_INPUT = "2";
+
 	private final TargetNumbers targetNumbers;
 
 	public Game() {
@@ -19,18 +22,38 @@ public class Game {
 		return InputView.getInput();
 	}
 
+	private boolean getQuitInput() {
+		OutputView.printQuitInstruction();
+		String input = InputView.getInput();
+
+		if (input.equals(QUIT_INPUT))
+			return true;
+		if (input.equals(RESTART_INPUT))
+			return false;
+
+		throw new IllegalArgumentException("1 또는 2 만 입력할 수 있습니다.");
+	}
+
 	public void start() {
 		while (true) {
-			String userInput = getNumberInput();
-			Numbers numbers = new Numbers(userInput);
-
-			Hint hint = targetNumbers.compare(numbers);
-
-			OutputView.printHint(hint);
-
-			if (hint.getStrike() == 3)
+			boolean isGameEnd = play();
+			if (isGameEnd)
 				break;
 		}
+	}
+
+	public boolean play() {
+		String userInput = getNumberInput();
+		Numbers numbers = new Numbers(userInput);
+
+		Hint hint = targetNumbers.compare(numbers);
+
+		OutputView.printHint(hint);
+
+		if (hint.getStrike() == 3)
+			return getQuitInput();
+
+		return false;
 	}
 
 }
