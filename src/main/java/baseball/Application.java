acdaa462af8322f250ee50a4c2ccc2be3cf747ec
SoulBriz <adoredia@naver.com>
2021-11-25 10:10:39 +0900

    feat: 맞혀야 할 임의의 값을 만드는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    public static String getRandomNumber() {
+        int firstNumber = Randoms.pickNumberInRange(1, 9);
+//        System.out.println(firstNumber);
+//        System.out.println();
+        int secondNumber;
+        int thirdNumber;
+
+        do {
+            secondNumber = Randoms.pickNumberInRange(1, 9);
+//            System.out.println(secondNumber);
+//            System.out.println();
+        } while (secondNumber == firstNumber);
+
+        do {
+            thirdNumber = Randoms.pickNumberInRange(1, 9);
+//            System.out.println(thirdNumber);
+//            System.out.println();
+        } while (thirdNumber == firstNumber || thirdNumber == secondNumber);
+
+//        System.out.println(firstNumber);
+//        System.out.println(secondNumber);
+//        System.out.println(thirdNumber);
+
+        String randomNumber = Integer.toString(firstNumber) + Integer.toString(secondNumber) + Integer.toString(thirdNumber);
+//        System.out.println(randomNumber);
+
+        return randomNumber;
+    }
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        String randomNumber = getRandomNumber();
+        System.out.println(randomNumber);
     }
 }
