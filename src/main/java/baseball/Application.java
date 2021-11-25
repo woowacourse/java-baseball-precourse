071982f13d265cf0edebd3ae855d588bb88916e9
juwon <58173061+juwon0605@users.noreply.github.com>
2021-11-26 00:21:24 +0900

    feat(src/main/java/baseball): make makeAnswer method
    makeAnswer method generate three digits with different numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		while (true) {
 			boolean reStartBaseballGame = BaseballGame.play();
-			if (reStartBaseballGame) {
+			if (!reStartBaseballGame) {
 				break;
 			}
 		}
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
 public class BaseballGame {
-	static boolean play() {
-		return true;
+
+	static final int START_NUMBER = 1;
+	static final int END_NUMBER = 9;
+	static final int ANSWER_LENGTH = 3;
+
+	public static boolean play() {
+		String answer = makeAnswer();
+
+		return false;
+	}
+
+	public static String makeAnswer() {
+		int[] duplicationCheckArray = new int[END_NUMBER - START_NUMBER + 1];
+		String answer = "";
+		while (answer.length() < ANSWER_LENGTH) {
+			int randomNumber = pickNumberInRange(START_NUMBER, END_NUMBER);
+			if (duplicationCheckArray[randomNumber - START_NUMBER] == 0) {
+				answer += Integer.toString(randomNumber);
+				duplicationCheckArray[randomNumber - START_NUMBER] = 1;
+			}
+		}
+		return answer;
 	}
 }
