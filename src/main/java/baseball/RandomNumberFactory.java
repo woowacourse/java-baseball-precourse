137f2a50c598ed8fe495db016f6fa1a99e6a4fb3
bong6981 <kbk5190@naver.com>
2021-11-26 08:37:51 +0900

    feat: 서로 다른 수로 구성된 3자리 수 생성
    1에서 9까지 서로 다른 임의의 수로 구성된 3자리 수 생성:
    - 난수를 생성하는 기능은 게임 진행의 메인 로직과 구분되어 별도의
    클래스로 분리
    - 게임 로직에서 매번 각 자리수에 대한 숫자 확인이 필요하기 때문에 해당
    숫자를 int배열로 반환
    - 자릿수 3, 들어가 수의 범위의 시작 수1, 끝 수 9에 대한 상수 선언
    - 랜덤하게 뽑힌 숫자들에 중복이 없어야 하기 때문에 LinkedHashSet 사용

---
+++ src/main/java/baseball/RandomNumberFactory.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RandomNumberFactory {
+    private static final int NUMBER_OF_DIGITS = 3;
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER = 9;
+
+    public static int[] generate() {
+        return getDigits().stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static Set<Integer> getDigits() {
+        Set<Integer> numbers = new LinkedHashSet<>();
+        while (numbers.size() < NUMBER_OF_DIGITS) {
+            numbers.add(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
+        }
+        return numbers;
+    }
+}
---
+++ src/test/java/baseball/RandomNumberFactoryTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomNumberFactoryTest {
+    private final int NUMBER_OF_DIGITS = 3;
+    private final int NUMBER_NOT_TO_INCLUDE = 0;
+    private int[] numbers;
+
+    @BeforeEach
+    void 난수_생성() {
+        numbers = RandomNumberFactory.generate();
+    }
+
+    @Test
+    void 생성된_난수는_3자리() {
+        assertThat(numbers).hasSize(NUMBER_OF_DIGITS);
+    }
+
+    @Test
+    void 생성된_난수는_서로_다른_수로_구성() {
+        assertThat(numbers).doesNotContain(NUMBER_NOT_TO_INCLUDE);
+        for (int number : numbers) {
+            assertThat(numbers).containsOnlyOnce(number);
+        }
+    }
+}
