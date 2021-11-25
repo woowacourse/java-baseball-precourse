dad04bc76cd9cd45f50c6040c421d187e542b17f
dongho <dongho108@naver.com>
2021-11-25 17:19:49 +0900

    refactor: update message

--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -6,6 +6,6 @@ public class UserInput {
     }
 
     public static void retryMessage() {
-        System.out.println("게임을 계속 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 다시 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
