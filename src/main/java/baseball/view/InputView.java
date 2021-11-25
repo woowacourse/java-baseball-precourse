bba1ce6a489dbcec2105c51f1c5c4ef6135c07b3
hyeonic <dev.hyeonic@gmail.com>
2021-11-25 20:16:12 +0900

    feat: 사용자에게 3자리 수 입력을 받기 위한 InputView 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,32 @@
+package baseball.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_NUMBERS_DELIMITER = "";
+    private static final int INPUT_NUMBERS_SIZE = 3;
+
+    public List<Integer> getInputNumbers() {
+        System.out.println(INPUT_NUMBERS_MESSAGE);
+        String inputNumbers = Console.readLine();
+        validateSize(inputNumbers.length());
+        return toList(inputNumbers);
+    }
+
+    private void validateSize(int size) {
+        if (size != INPUT_NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private List<Integer> toList(String inputNumbers) {
+        return Arrays.stream(inputNumbers.split(INPUT_NUMBERS_DELIMITER))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
+}
