1c1627311af8202b716ea299833a311d20a3f28b
SeungHan <jk05018@naver.com>
2021-11-25 18:38:23 +0900

    feat(SystemMessage): SystemMessage 상수 추가

--- src/main/java/constant/SystemMessage.java
+++ src/main/java/constant/SystemMessage.java
@@ -1,4 +1,20 @@
 package constant;
 
 public class SystemMessage {
+    public static final int REPEAT = 1;
+    public static final int STOP = 2;
+    public static final int FIRST_VALUE = 0;
+
+    public static final String NO_NUMBER_ERROR = "[ERROR] 숫자를 입력해야 합니다";
+    public static final String INVALID_NUMBER = "[ERROR] 1과 2 중 하나를 입력해야 합니다";
+    public static final String INVALID_RANGE = "[ERROR] 세자리수 여야 합니다";
+    public static final String CONTAIN_ZERO = "[ERROR] 1~9의 숫자만으로 아루어져야 합니다";
+
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+
+    public static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String ENTER_NEXT_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 }
