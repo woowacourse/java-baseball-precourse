10507c45cd05715296918057bcaf846fc148de96
Seongwon97 <osw970325@naver.com>
2021-11-26 02:37:10 +0900

    feat : add a restart func
    - create a startNewGame func to get user input and check that input is a valid value
    - implement that the game could be repeatedly played.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         Game game = new Game();
+        boolean flag = true;
+        do {
+            game.gameStart();
+            flag = startNewGame();
+        } while(flag);
+    }
 
-        game.gameStart();
+    private static boolean startNewGame() {
+        System.out.println(Message.RESTART_MESSAGE);
+        String answer = Console.readLine();
+
+        if (answer.length() > 1) {
+            throw new IllegalArgumentException(Message.INPUT_ERROR_INVALID_INPUT);
+        }
+
+        if (answer.charAt(0) != '1' || answer.charAt(0) != '0') {
+            throw new IllegalArgumentException(Message.INPUT_ERROR_INVALID_INPUT);
+        }
+
+        if (answer.charAt(0) == '1') {
+            return true;
+        }
+        return false;
     }
 }
