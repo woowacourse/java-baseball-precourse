00175d215e9e56dc38f4cd3e83e79bcad8de2fa6
zzambbang <zzambbang@gmail.com>
2021-11-25 16:30:29 +0900

    feat : user get playing status

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,22 @@
 package baseball;
 
-public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    }
+public class Application {
+	private static final int RESTART_CODE = 1;
+
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+		Scanner scanner = new Scanner(System.in);
+		int playingStatus;
+
+		do {
+			ArrayList<Integer> generationNumbers = Number.createRandomNumbers();
+			Game.play(scanner, generationNumbers);
+			playingStatus = Game.isContinue(scanner);
+		} while (playingStatus == RESTART_CODE);
+
+		scanner.close();
+	}
 }
