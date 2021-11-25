eaa7b3663a55295d719d307bf050cb67a350a06b
OzRagwort <ozragwort@gmail.com>
2021-11-25 20:29:42 +0900

    refactor(Computer): move validation method
    - Move Valid.class to Computer.class

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import static baseball.Valid.*;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 import java.util.ArrayList;
@@ -10,6 +9,8 @@ public class Computer {
 	public static final int NUMBER_DIGITS = 3;
 	public static final int MAX_NUMBER = 9;
 	public static final int MIN_NUMBER = 1;
+	private static final boolean VALID_SUCCESS = true;
+	private static final boolean VALID_FAILED = false;
 	private List<Integer> randomNumbers;
 
 	public Computer() {
@@ -18,7 +19,7 @@ public class Computer {
 	public void init() {
 		do {
 			generateNewRandomNumbers();
-		} while (validateNumbersList(randomNumbers) == VALID_FAILED);
+		} while (validateNumbers(randomNumbers) == VALID_FAILED);
 	}
 
 	private void generateNewRandomNumbers() {
@@ -29,6 +30,21 @@ public class Computer {
 				randomNumbers.add(number);
 			}
 		}
+	}
+
+	private boolean validateNumbers(List<Integer> numbers) {
+		if (numbers.size() != NUMBER_DIGITS) {
+			return VALID_FAILED;
+		}
+		for (int i = 0; i < NUMBER_DIGITS; i++) {
+			if (numbers.get(i) < MIN_NUMBER || MAX_NUMBER < numbers.get(i)) {
+				return VALID_FAILED;
+			}
+		}
+		if (numbers.stream().distinct().count() != NUMBER_DIGITS) {
+			return VALID_FAILED;
+		}
+		return VALID_SUCCESS;
 	}
 
 	public int checkBallCount(List<Integer> userNumbers) {
