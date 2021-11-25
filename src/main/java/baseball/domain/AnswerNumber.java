a92f226499d9f4100e23aee2a17d4a56a407a1cd
DESKTOP-QQ11LUI\choijoohee <choijoohee@naver.com>
2021-11-25 16:57:25 +0900

    feat: 랜덤값 생성 기능 구현

---
+++ src/main/java/baseball/domain/AnswerNumber.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class AnswerNumber {
+	private static List<Integer> answer;
+	public static final int ANSWER_LENGTH = 3;
+	public static final int ANSWER_MIN_VALUE = 1;
+	public static final int ANSWER_MAX_VALUE = 9;
+
+	private AnswerNumber() {
+	}
+
+	public static void setAnswer(List<Integer> numberList) {
+		answer = numberList;
+	}
+
+	public static List<Integer> getAnswer() {
+		return answer;
+	}
+}
