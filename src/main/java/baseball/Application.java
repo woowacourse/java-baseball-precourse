37c1f3b46e14211910da599cc7272f9873bfa971
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-25 14:53:50 +0900

    refactor : separate BaseballGame class from Application.main Application.main 간소화를 위한 클래스 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,21 +4,8 @@ import static util.GameConstant.*;
 
 public class Application {
     public static void main(String[] args) {
-        GamePlayer gamePlayer = new GamePlayer();
-        GameProvider gameProvider = new GameProvider();
-        while (gameProvider.getGameStatus().equals(ON_GOING)){
-            gamePlayer.writeAnswer();
-            GameScore gameScore = gameProvider.checkAnswer(gamePlayer.getPlayerAnswer());
-            gameScore.printResult();
+        BaseballGame baseballGame = new BaseballGame(new GamePlayer(), new GameProvider());
+        baseballGame.run();
+    }
 
-            if (gameProvider.getGameStatus().equals(CORRECT)){
-                printGuideMessage(THREE_STRIKE);
-                printGuideMessage(ASK_RESTART_OR_STOP);
-                gameProvider.askRestartOrStop();
-            }
-        }
-    }
-    private static void printGuideMessage(String message){
-        System.out.println(message);
-    }
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import static util.GameConstant.*;
+import static util.GameConstant.ASK_RESTART_OR_STOP;
+
+public class BaseballGame {
+    private GamePlayer gamePlayer;
+    private GameProvider gameProvider;
+
+    public BaseballGame(GamePlayer gamePlayer, GameProvider gameProvider) {
+        this.gamePlayer = gamePlayer;
+        this.gameProvider = gameProvider;
+    }
+
+    public void run() {
+        while (gameProvider.getGameStatus().equals(ON_GOING)) {
+            gamePlayer.writeAnswer();
+            GameScore gameScore = gameProvider.checkAnswer(gamePlayer.getPlayerAnswer());
+            gameScore.printResult();
+
+            if (gameProvider.getGameStatus().equals(CORRECT)) {
+                System.out.println(THREE_STRIKE);
+                System.out.println(ASK_RESTART_OR_STOP);
+                gameProvider.askRestartOrStop();
+            }
+        }
+    }
+}
