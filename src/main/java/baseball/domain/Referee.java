78aac6b3463e613226ebc6d20a9c75c5ded45a56
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-11-25 22:56:41 +0900

    refactor(Referee): indent depth가 최대 2를 넘지 않도록 수정

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -5,20 +5,41 @@ import java.util.List;
 public class Referee {
 
     public Result judge(final List<Integer> answer, final List<Integer> guess) {
-        int strikeCount = 0;
-        int ballCount = 0;
-
-        for (int i = 0; i < answer.size(); i++) {
-            for (int j = 0; j < guess.size(); j++) {
-                if (answer.get(i).equals(guess.get(j))) {
-                    if (i == j) {
-                        strikeCount++;
-                    } else {
-                        ballCount++;
-                    }
-                }
-            }
-        }
+        int strikeCount = countStrike(answer, guess);
+        int ballCount = countBall(answer, guess);
         return new Result(strikeCount, ballCount);
     }
+
+    private int countBall(List<Integer> answer, List<Integer> guess) {
+        int ballCount = 0;
+        for (int i = 0; i < answer.size(); i++) {
+            if (isAnySameValueWithDifferentIndex(answer.get(i), i, guess)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    private int countStrike(List<Integer> answer, List<Integer> guess) {
+        int strikeCount = 0;
+        for (int i = 0; i < answer.size(); i++) {
+            if (answer.get(i).equals(guess.get(i))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private boolean isAnySameValueWithDifferentIndex(Integer value, int index, List<Integer> target) {
+        for (int i = 0; i < target.size(); i++) {
+            if (i == index) {
+                continue;
+            }
+            if (value.equals(target.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
