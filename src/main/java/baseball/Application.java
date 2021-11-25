a52b2e973e12c02e1d900d0c362b724864c11ce3
wannagohome <iwwuf7@gmail.com>
2021-11-25 10:26:45 +0900

    feat: get console input from user
     - 어플리케이션이 가동되면 유저의 입력을 받는다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         String targetNumber = NumberGenerator.generate();
+        getConsoleInput();
+    }
+
+    private static void getConsoleInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
     }
 }
