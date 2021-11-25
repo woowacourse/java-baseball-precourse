9b28c3425ed1e7041bd2103ea482ee903314ce99
swcho <progress0407@gmail.com>
2021-11-26 01:19:29 +0900

    test: 실패하는 BallsTest 테스트 코드 작성

---
+++ src/test/java/baseball/BallsTest.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import baseball.model.Ball;
+import baseball.model.Balls;
+import baseball.model.PlayType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallsTest {
+
+    Balls computerBalls;
+    @BeforeEach
+    void setUp() {
+        computerBalls = new Balls(4, 6, 8);
+    }
+
+    @Test
+    void 스트라이크1() {
+        Ball myBall = new Ball(4, 0);
+        PlayType playType = computerBalls.play(myBall);
+        assertThat(playType).isEqualTo(PlayType.STRIKE);
+    }
+}
