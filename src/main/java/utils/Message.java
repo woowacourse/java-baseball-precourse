4702d900c8991e929ce0554c715ad1a44b50b7c5
Seongwon97 <osw970325@naver.com>
2021-11-25 23:42:07 +0900

    feat : add a Message class with output messages
    - create many output messages in Message.java

---
+++ src/main/java/utils/Message.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Message {
+    public static final String INPUT_ERROR_LENGTH = "3자리 이상의 수를 입력하셨습니다.";
+    public static final String INPUT_ERROR_DUPLICATE ="중복된 수가 있습니다";
+    public static final String INPUT_ERROR_HAVE_ZERO = "입력 숫자에 0이 포함되어 있습니다.";
+    public static final String INPUT_ERROR_ETC = "옳바르지 않은 입력값입니다.";
+
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String SUCCESS_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String RESULT_NOTHING_MESSAGE = "낫싱";
+
+}
