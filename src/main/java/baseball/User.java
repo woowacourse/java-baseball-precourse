3f283f643143a4befd650bfc3f4f9bf302e60d0c
minsulee <alkwen0996@naver.com>
2021-11-25 13:25:50 +0900

    refactor: 상대방(컴퓨터) 3자리 수(정답) 입력 기능 수정 기존 1자리 입력 -> stringBuilder를 이용한 3자리 입력

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,24 @@
-package baseball;public class User {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class User {
+    public void printInputMessage() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+
+    public String inputPlayerNumber() {
+        return Console.readLine();
+    }
+
+    public String inputAnswerNumber() {
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            stringBuilder.append(Randoms.pickNumberInRange(1, 9));
+        }
+
+        return stringBuilder.toString();
+    }
 }
