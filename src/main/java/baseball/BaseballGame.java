19cf1f1fe4c85f8827db21528ff3278d2a0094db
minsulee <alkwen0996@naver.com>
2021-11-25 20:08:55 +0900

    feat: 스트라이크 볼 판정 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 public class BaseballGame {
 
+    public static final String STRIKE_ENGLISH = "strike";
+    public static final String BALL_ENGLISH = "ball";
+
     public void startGame() {
         progressGame();
     }
@@ -16,7 +19,10 @@ public class BaseballGame {
         String guessNumber = user.inputPlayerNumber();
 
         while (user.checkAnswer(answer, guessNumber)) {
+            final Map<String, Integer> strikeBallCount = user.countStrikeBallNumber(answer, guessNumber);
 
+            int strikeCount = strikeBallCount.get(STRIKE_ENGLISH);
+            int ballCount = strikeBallCount.get(BALL_ENGLISH);
 
             user.printInputMessage();
             guessNumber = user.inputPlayerNumber();
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,11 +3,67 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class User {
 
     private static final int RANDOM_START_INCLUSIVE = 1;
     private static final int RANDOM_END_INCLUSIVE = 9;
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해 주세요 : ";
+    public static final int ZERO = 0;
+    public static final String STRIKE_ENGLISH = "strike";
+    public static final String BALL_ENGLISH = "ball";
+
+    public Map<String, Integer> countStrikeBallNumber(final String answer, final String guessNumber) {
+        final Map<String, Integer> strikeBallCount = new HashMap<>();
+
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < answer.length(); i++) {
+            int strikeBall = decideStrikeBall(answer, i, guessNumber);
+
+            strike += addStrikeBallCount(strike, ball, strikeBall);
+            ball += addStrikeBallCount(strike, ball, strikeBall);
+        }
+
+        strikeBallCount.put(STRIKE_ENGLISH, strike);
+        strikeBallCount.put(BALL_ENGLISH, ball);
+
+        return strikeBallCount;
+    }
+
+    private int addStrikeBallCount(int strike, int ball, final int strikeBall) {
+        if (strikeBall == 1) {
+            strike++;
+            return strike;
+        }
+
+        if (strike == 2) {
+            ball++;
+            return ball;
+        }
+
+        return ZERO;
+    }
+
+    private int decideStrikeBall(final String answer, final int i, final String guessNumber) {
+        int decide = 0;
+
+        char answerCharacter = answer.charAt(i);
+        char guessNumberCharacter = guessNumber.charAt(i);
+
+        if (answerCharacter == guessNumberCharacter) {
+            decide = 1;
+        }
+
+        if ((answerCharacter != guessNumberCharacter) && (answer.contains(String.valueOf(guessNumberCharacter)))) {
+            decide = 2;
+        }
+
+        return decide;
+    }
 
     public boolean checkAnswer(final String answer, final String guessNumber) {
         return answer.equals(guessNumber);
