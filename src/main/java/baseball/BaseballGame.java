43e5311cc44fc52b7559d7a4a444b31e42725632
Ohzzi <clover7even@naver.com>
2021-11-25 11:37:39 +0900

    feat: 게임을 생성
    전체 게임을 담당하는 BaseballGame 클래스를 만들고 main 메소드가 시작될 때 새 BaseballGame 인스턴스를 만들어 게임을 실행하는 기능 추가

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import baseball.players.Computer;
+import baseball.players.Player;
+
+public class BaseballGame {
+
+    private Computer computer;
+    private Player player;
+    private int ballCount;
+    private int strikeCount;
+
+    public BaseballGame() {
+        this.computer = new Computer();
+        this.player = new Player();
+        this.ballCount = 0;
+        this.strikeCount = 0;
+    }
+
+    public void run() {
+        System.out.println("게임을 시작합니다.");
+    }
+
+}
