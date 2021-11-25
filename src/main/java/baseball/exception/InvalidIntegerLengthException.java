20211040e07172e2983e53f6c8dea9c84f762cb2
jaeyoungjang2 <lion85488@gmail.com>
2021-11-25 16:23:23 +0900

    feat: 야구 게임에 사용되는 수가 3자리가 아닐 경우 발생하는 예외

--- src/main/java/baseball/exception/InvalidIntegerLengthException.java
+++ src/main/java/baseball/exception/InvalidIntegerLengthException.java
@@ -1,2 +1,10 @@
-package baseball.domain;public class InvalidIntegerLengthException {
+package baseball.exception;
+
+public class InvalidIntegerLengthException extends IllegalArgumentException {
+
+    private static String INVALID_INTEGER_LENGTH_MESSAGE = "3자리의 수를 입력해야 합니다.";
+
+    public InvalidIntegerLengthException() {
+        super(INVALID_INTEGER_LENGTH_MESSAGE);
+    }
 }
