aa4e596c63a2bbbdc8691d69e92af5f7a80b4be5
SeungCheol <ssc6839@gmail.com>
2021-11-25 19:42:41 +0900

    feat: 3자리 수 생성 기능 구현

---
+++ src/main/java/baseball/BaseballRandomNumberGenerator.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballRandomNumberGenerator {
+
+    public static final int BASEBALL_NUMBER_MIN_VALUE = 1;
+    public static final int BASEBALL_NUMBER_MAX_VALUE = 9;
+    public static final int BASEBALL_NUMBER_LENGTH = 3;
+
+    public List<Integer> generateRandomNumber() {
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() < BASEBALL_NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(BASEBALL_NUMBER_MIN_VALUE, BASEBALL_NUMBER_MAX_VALUE);
+
+            if (!checkDuplicate(randomNumbers, randomNumber)) {
+                System.out.println(randomNumber);
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
+
+    public boolean checkDuplicate(List<Integer> randomNumbers, int randomNumber) {
+        return randomNumbers.contains(randomNumber);
+    }
+}
