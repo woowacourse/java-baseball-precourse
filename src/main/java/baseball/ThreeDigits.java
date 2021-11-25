2b16e7181c34d3e4793f5488eae0b11bea9c214f
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-26 00:12:54 +0900

    [feat] checkBall(int numberInt) 볼인지 체크하는 함수 작성

--- src/main/java/baseball/ThreeDigits.java
+++ src/main/java/baseball/ThreeDigits.java
@@ -1,10 +1,10 @@
 package baseball;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class ThreeDigits {
     public String threeDigits = "123";
+    public boolean [] IsJudged = new boolean[3];
 
     public String getThreeDigits() {
         return threeDigits;
@@ -34,8 +34,25 @@ public class ThreeDigits {
         for (int i = 0; i < this.threeDigits.length(); i++) {
             if (numberStr.charAt(i) == this.threeDigits.charAt(i)) {
                 strikeCount++;
+                this.IsJudged[i] = true;
             }
         }
         return strikeCount;
+    }
+
+    public int checkBall(int numberInt) {
+        String numberStr = String.valueOf(numberInt);
+        int ballCount = 0;
+        for (int i = 0; i < this.threeDigits.length(); i++) {
+            if (this.IsJudged[i]) {
+                continue;
+            }
+
+            char character = numberStr.charAt(i);
+            if (this.threeDigits.indexOf(character) != -1) {
+                ballCount++;
+            }
+        }
+        return ballCount;
     }
 }
