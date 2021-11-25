3863021269e820d21bfc4d062bde2890370eb9dc
juwon <58173061+juwon0605@users.noreply.github.com>
2021-11-25 19:21:56 +0900

    feat(src/main/java/baseball): construct the structure of the game start function
    Application.java:
    - Let's start the game, and depending on the result, the game starts or ends again
    BaseballGame.java:
    - create play() method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+
+	public static void main(String[] args) {
+		while (true) {
+			boolean reStartBaseballGame = BaseballGame.play();
+			if (reStartBaseballGame) {
+				break;
+			}
+		}
+	}
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class BaseballGame {
+	static boolean play() {
+		return true;
+	}
+}
