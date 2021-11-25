cdacd24de22f2394d8f04809d1e851298000ba62
nailseong <nnis9857@gmail.com>
2021-11-26 02:39:16 +0900

    refactor(main): 게임 종료 메시지 출력 메서드 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,8 +10,7 @@ public class Application {
         while (true) {
             boolean isFinish = game.play();
             if (isFinish) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                game.printEndMessage();
                 String input = Console.readLine();
                 if (input.equals("2")) {
                     break;
