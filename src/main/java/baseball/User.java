d2e85b17d478c846522d68f2d7b716cc230747a5
her0807 <her0807@naver.com>
2021-11-25 20:00:58 +0900

    feat(user): 게임 입력 로직 구현
    - 맞출 값을 입력 받기 위한 inputNumbers
    - 정답일 경우, 재시작 여부를 받기 위한 isContinue
    - 입력 값 검증 로직

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,57 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+public class User {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static ArrayList<Integer> userNumbers;
+    private BoundaryChecker boundaryChecker;
+    private static String inputNumbers;
+
+    public User() {
+        boundaryChecker = new BoundaryChecker();
+        userNumbers = new ArrayList<>();
+    }
+
+    public void inputNumbers() {
+        clear();
+        System.out.print(INPUT_MESSAGE);
+        inputNumbers = Console.readLine();
+        checkList(inputNumbers);
+        userNumbers.addAll(convertStringToList());
+    }
+
+    public boolean isContinue() {
+        System.out.println(CONTINUE_MESSAGE);
+        String userInput = Console.readLine();
+        if (userInput.equals("1")) {
+            return true;
+        }
+        if (userInput.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public void checkList(String inputNumbers) {
+        boundaryChecker.convertInt(inputNumbers);
+        boundaryChecker.checkLength(inputNumbers);
+        boundaryChecker.checkDuplicate(convertStringToList());
+        boundaryChecker.checkRange(Integer.parseInt(inputNumbers));
+    }
+
+    public ArrayList<Integer> convertStringToList() {
+        String[] inputStr = inputNumbers.split("");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (String i : inputStr)
+            numbers.add(Integer.parseInt(i));
+        return numbers;
+    }
+
+    public void clear() {
+        userNumbers.clear();
+    }
+}
