a3fe2c0c6e7c15584610b7134a72b7ac15588f86
zzambbang <zzambbang@gmail.com>
2021-11-25 16:07:33 +0900

    feat: user input num check

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -31,4 +31,40 @@ public class Number {
 		return result;
 	}
 
+	//사용자로부터 입력받은 수 처리
+	public static ArrayList<Integer> splitNumber(Integer playerNumber) {
+		ArrayList<Integer> splitPlayerNumber = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			splitPlayerNumber.add(0, playerNumber % 10);
+			playerNumber /= 10;
+		}
+		return splitPlayerNumber;
+	}
+
+	public static void inputCheck(Integer number) {
+		is_Not_ThreeDigits(number);
+		is_Contain_Zero(number);
+		is_Overlap(splitNumber(number));
+	}
+
+	public static void is_Not_ThreeDigits(Integer number) {
+		if (number.toString().length() != 3)
+			throw new IllegalArgumentException("올바른 입력이 아닙니다!");
+	}
+
+	public static void is_Contain_Zero(Integer number) {
+		if (String.valueOf(number).contains("0"))
+			throw new IllegalArgumentException("올바른 입력이 아닙니다!");
+	}
+
+	public static void is_Overlap(ArrayList<Integer> number) {
+		ArrayList<Integer> temp = new ArrayList<>();
+
+		for (int i = 0; i < 3; i++) {
+			if (temp.contains(number.get(i)))
+				throw new IllegalArgumentException("올바른 입력이 아닙니다!");
+			temp.add(number.get(i));
+		}
+	}
+
 }
