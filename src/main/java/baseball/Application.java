687bd1b1ca0b86f3233ca48a029490f695987b8a
iamsseaa <homingsea@gmail.com>
2021-11-26 16:49:52 +0900

    feat: main 함수 게임 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,17 +4,13 @@ public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
 
+        int nextRound;
 
+        do {
+            BaseballGame.Round();
+            nextRound = BaseballGame.NextRound();
+        } while(nextRound == 1);
     }
 
-    // 입력받은 수를 String 에서 int array 로 저장
-    public static int[] stringToInt(String inputNumber) {
-        int[] userNumber = new int[3];
 
-        for(int i=0; i<3; i++) {
-            userNumber[i] = Character.getNumericValue(inputNumber.charAt(i));
-        }
-
-        return userNumber;
-    }
 }
