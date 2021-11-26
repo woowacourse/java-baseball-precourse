757dc77775bdcd1c61808b0b3b6e372ae7e42584
Ohzzi <clover7even@naver.com>
2021-11-26 17:46:44 +0900

    refactor: 숫자 자동 생성 코드 수정
    컴퓨터가 3개의 수를 자동으로 생성할 때 Randoms의 pickUniqueNumbersInRange()를 사용하면 더 간단하게 할 수 있어서 수정

--- src/main/java/baseball/players/Computer.java
+++ src/main/java/baseball/players/Computer.java
@@ -3,18 +3,18 @@ package baseball.players;
 import static baseball.utils.Constant.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer implements Player {
-	private final ArrayList<Integer> numbers;
+	private List<Integer> numbers;
 
 	/**
 	 * 생성자가 호출될 때 자동으로 숫자 배열을 생성하고
 	 * 배열 안에 들어갈 숫자들을 생성한다.
 	 */
 	public Computer() {
-		this.numbers = new ArrayList<>();
 		createNumbers();
 	}
 
@@ -23,13 +23,7 @@ public class Computer implements Player {
 	 * 각 자리수의 숫자는 서로 다른 수이며 각 자리수를 ArrayList 에 저장한다.
 	 */
 	public void createNumbers() {
-		numbers.clear();
-		do {
-			int number = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
-			if (!numbers.contains(number)) {
-				numbers.add(number);
-			}
-		} while (numbers.size() < LENGTH_OF_NUMBERS);
+		numbers = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, LENGTH_OF_NUMBERS);
 	}
 
 	@Override
