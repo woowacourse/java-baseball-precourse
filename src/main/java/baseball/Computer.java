52bf9c5adf09b2d555efd931d5951d24a486f8e0
iamsseaa <homingsea@gmail.com>
2021-11-26 14:12:31 +0900

    chore: 컴퓨터 랜덤 값 생성하는 메소드 static 제거

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,10 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
 
-    public static int[] target = new int[3];
+    public int[] target = new int[3];
 
     // 정답으로 쓰일 임의의 수 3개를 target array에 저장한다.
-    public static void makeTarget() {
+    public void makeTarget() {
         for(int i=0; i<3; i++) {
             target[i] = Randoms.pickNumberInRange(1, 9);
         }
