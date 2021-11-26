46f875b7bc15d4ea1f0fb60972f6de893fe48256
Gyuchool <balance0591@naver.com>
2021-11-26 13:16:40 +0900

    refactor(game): Game 생성자를 통해서 객체 초기화

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -3,9 +3,14 @@ package baseball.domain;
 import baseball.view.InputView;
 
 public class Game {
-	private final User user = new User();
-	private Computer computer = new Computer();
+	private final User user;
+	private final Computer computer;
 
+	public Game(){
+		user = new User();
+		computer = new Computer();
+
+	}
 	public void start() {
 		computer.generateAnswer();
 		user.setNumberOfUser(InputView.getInput());
