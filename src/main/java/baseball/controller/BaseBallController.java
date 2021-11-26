a0288a01be5349061f506fbd6d166838cd351744
swcho <progress0407@gmail.com>
2021-11-27 01:37:03 +0900

    feat: 랜덤 생성 구현 및 게임 종료 구현

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -1,21 +1,40 @@
 package baseball.controller;
 
 import baseball.model.Balls;
+import baseball.model.PlayType;
+import baseball.model.PlayTypes;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 import baseball.view.ScannerInputStrategy;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class BaseBallController {
 
+    private static final int BALL_ROUND = 3;
+
     public void run() {
-        boolean isEndGaem = false;
+        Balls computerBalls = getRandomBalls();
+        Balls playerBalls;
+        System.out.println("computerBalls = " + computerBalls);
         do {
-            Balls balls = InputView.inputNumber(new ScannerInputStrategy());
-            OutputView.printResult(balls);
-            isEndGaem = OutputView.isEndGame();
-        } while (!isEndGaem);
-        
+            playerBalls = InputView.inputNumber(new ScannerInputStrategy());
+            OutputView.printResult(computerBalls, playerBalls);
+        } while (!isEndGame(computerBalls, playerBalls));
+    }
+
+    private Balls getRandomBalls() {
+        int randomNumbers = Randoms.pickNumberInRange(100, 999);
+        return new Balls(randomNumbers);
+    }
+
+    boolean isEndGame(Balls computerBalls, Balls playerBalls) {
+        PlayTypes playTypes = computerBalls.play(playerBalls);
+        List<PlayType> playTypeList = playTypes.getPlayType();
+        long strikeCount = playTypeList.stream().filter(PlayType.STRIKE::equals).count();
+        return strikeCount == BALL_ROUND;
     }
 }
