7b478f9ab3b174ba453f3e6b1d8effca7d5cca03
EastHShin <xldk78@gmail.com>
2021-11-27 02:35:24 +0900

    feat(messages): 게임의 결과 메시지 출력하는 기능 구현

--- src/main/java/baseball/messages/GameMessage.java
+++ src/main/java/baseball/messages/GameMessage.java
@@ -3,6 +3,7 @@ package baseball.messages;
 public final class GameMessage {
 	public static final String BALL = "볼";
 	public static final String STRIKE = "스트라이크";
+	public static final String THREE_STRIKE = "3스트라이크";
 	public static final String NOTHING = "낫싱";
 	public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 	public static final String FINISH_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
--- src/main/java/baseball/messages/PrintResult.java
+++ src/main/java/baseball/messages/PrintResult.java
@@ -1,2 +1,30 @@
-package baseball.messages;public class PrintResult {
+package baseball.messages;
+
+public class PrintResult {
+
+	public void printEndGame(){
+		System.out.println(GameMessage.THREE_STRIKE);
+		System.out.println(GameMessage.FINISH_GAME);
+	}
+
+	public void printNothing() {
+		System.out.println(GameMessage.NOTHING);
+	}
+
+	public void printBall(int ballCount) {
+		String ballCntStr = Integer.toString(ballCount);
+		System.out.println(ballCntStr + GameMessage.BALL);
+	}
+
+	public void printStrike(int strikeCount) {
+		String strikeCntStr = Integer.toString(strikeCount);
+		System.out.println(strikeCntStr + GameMessage.STRIKE);
+	}
+
+	public void printBallAndStrike(int strikeCount, int ballCount){
+		String strikeCntStr = Integer.toString(strikeCount);
+		String ballCntStr = Integer.toString(ballCount);
+		System.out.println(ballCntStr + GameMessage.BALL + " " +
+			strikeCntStr + GameMessage.STRIKE);
+	}
 }
