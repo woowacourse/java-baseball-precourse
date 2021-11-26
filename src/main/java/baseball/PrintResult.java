ceee9be56487ddb43bbebd2ce512372cb2b690e6
iamsseaa <homingsea@gmail.com>
2021-11-26 13:53:47 +0900

    chore: 결과를 출력하는 메소드 리턴 값 수정

--- src/main/java/baseball/PrintResult.java
+++ src/main/java/baseball/PrintResult.java
@@ -37,18 +37,27 @@ public class PrintResult {
         return result;
     }
 
-    public static void printResult(int[] target, int[] userNum) {
+    // 결과 출력 시 printResult 메소드만 호출하면 됨
+    public static boolean printResult(int[] target, int[] userNum) {
         int[] result;
         result = countResult(target, userNum);
 
-        if(Arrays.stream(result).sum() == 0) {
-            System.out.println("낫싱");
-       } else if(result[0] == 0 && result[1] != 0) {
-            System.out.println(result[1]+"볼");
-        } else if(result[0] != 0 && result[1] == 0) {
-            System.out.println(result[0]+"스트라이크");
+        if(result[1] == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
         } else {
-            System.out.println(result[1]+"볼 "+result[0]+"스트라이크");
+            if (Arrays.stream(result).sum() == 0) {
+                System.out.println("낫싱");
+
+            } else if (result[0] == 0 && result[1] != 0) {
+                System.out.println(result[1] + "볼");
+            } else if (result[0] != 0 && result[1] == 0) {
+                System.out.println(result[0] + "스트라이크");
+            } else {
+                System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
+            }
+
+            return false;
         }
     }
 }
