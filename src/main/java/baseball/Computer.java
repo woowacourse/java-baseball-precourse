10470e3a775a0047c7b82114214fc2f341ef1045
steven <seokju73@gmail.com>
2021-11-27 00:09:01 +0900

    cln: Refactor 'generateRanNum' method

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,35 @@
 package baseball;
 
-public class Computer {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 
+public class Computer {
+	private static final int DIGIT = 3;
+	private String ranNum;
+	
+	public Computer () {
+		ranNum = "";
+	}
+	
+	public void initialize () {
+		ranNum = generateRanNum(DIGIT);
+	}
+	
+	private String generateRanNum(int digit) {
+		ArrayList <Character> list = new ArrayList<>();
+		String res = "";
+		while(list.size()<digit) {
+			char tmp = Character.forDigit(Randoms.pickNumberInRange(1, 9), 10);
+			
+			if(list.contains(tmp)) {
+				continue;
+			}
+			list.add(tmp);
+		}
+		for(char c : list) {
+			res += c;
+		}
+		return res;
+	}
+	
 }
