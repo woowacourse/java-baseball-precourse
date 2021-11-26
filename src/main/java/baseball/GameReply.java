d23dd151cda020b369091f462b910dbafff7aeb1
HyoTaek-Jang <hyotaek9812@gmail.com>
2021-11-27 01:56:32 +0900

    feat: 사용자 입력 유효성 검사 기능 추가

--- src/main/java/baseball/GameReply.java
+++ src/main/java/baseball/GameReply.java
@@ -13,7 +13,30 @@ public class GameReply {
 	public GameReply() {
 		System.out.print("숫자를 입력해주세요 : ");
 		String input = Console.readLine();
+		this.gameReply = Arrays.asList(makeIntArray(input));
+		validation();
 	}
 
+	public Integer[] makeIntArray(String input) {
+		try {
+			return Stream.of(input.split("")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+		} catch (NumberFormatException numberFormatException) {
+			throw (new IllegalArgumentException());
+		}
+	}
+
+	public void validation() {
+		if (this.gameReply.size() > 3) {
+			throw (new IllegalArgumentException());
+		}
+		for (Integer cur :
+			this.gameReply) {
+			int front = this.gameReply.indexOf(cur);
+			int tail = this.gameReply.lastIndexOf(cur);
+			if (front != tail) {
+				throw (new IllegalArgumentException());
+			}
+		}
+	}
 
 }
