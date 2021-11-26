a79e2873c5f4629ffc72d96215aee72e4b9e8e3d
pup-paw <wldusdl0310@gmail.com>
2021-11-26 23:09:58 +0900

    feat: 게임을 시작하기 위한 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.gameController.GameDriver;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+
+	public static void main(String[] args) {
+		GameDriver gameDriver = new GameDriver();
+		gameDriver.run();
+	}
 }
