718a19d281ae892b53d4d4772c46e95be2e00ef0
yezyvibe <yeji0501@gmail.com>
2021-11-26 12:02:23 +0900

    refactor(user input number): change return type array to array list

--- src/main/java/baseball/UserInterfaceView.java
+++ src/main/java/baseball/UserInterfaceView.java
@@ -1,28 +1,40 @@
 package baseball;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class View {
+public class UserInterfaceView {
 	private static final String NUMBER_INPUT = "숫자를 입력해주세요 : ";
 	private static final String THREE_STRIKE_GAME_OVER = "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	private static final String IS_BALL = "볼";
 	private static final String IS_STRIKE = "스트라이크";
 	private static final String IS_NOTHING = "낫싱";
 	private static final String GAME_RESTART_OR_NOT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private static final String NUMBER_LENGTH_ERROR_MESSAGE = "잘못된 길이의 숫자를 입력했습니다.";
+	private static final String NUMBER_OVERLAP_ERROR_MESSAGE = "동일한 숫자를 입력했습니다.";
 
 	public static void printNumberInputMessage() {
 		System.out.print(NUMBER_INPUT);
 	}
 
-	public static int[] inputUserNumbers() {
-		String tempInput = Console.readLine();
-		int[] userInputNumbers = Stream.of(tempInput.split("")).mapToInt(Integer::parseInt).toArray();
-		System.out.println(Arrays.toString(userInputNumbers));
+	public static ArrayList<Integer> inputUserNumbers() {
+		int[] tempInput = Stream.of(Console.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+		if (!isValidNumberLength(tempInput)) {
+			throw new IllegalArgumentException(NUMBER_LENGTH_ERROR_MESSAGE);
+		}
+		ArrayList<Integer> userInputNumbers = new ArrayList<>();
+		for (int i : tempInput) {
+			Integer number = new Integer(i);
+			userInputNumbers.add(number);
+		}
+		if (!isValidNumberOverlap(userInputNumbers)) {
+			throw new IllegalArgumentException(NUMBER_OVERLAP_ERROR_MESSAGE);
+		}
 		return userInputNumbers;
+
 	}
 
 	public static void printHint(HashMap<String, Integer> hintMap) {
