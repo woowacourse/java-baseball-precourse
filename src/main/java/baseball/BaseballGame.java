a4be11d196d92a791f3b2977b1703efd95c39f3d
iamsseaa <homingsea@gmail.com>
2021-11-26 14:08:12 +0900

    feat: 사용자의 입력과 결과 출력을 반복하는 메소드

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,4 +1,18 @@
 package baseball;
 
 public class BaseballGame {
+    // 게임 진행 : Round(Computer 랜덤 값 지정 -> Game(사용자 입력 -> 결과 출력))
+
+    public static void Game(int[] target) {
+        boolean result = false;
+
+        do{
+            // 사용자에게 입력 받기
+            System.out.print("숫자를 입력해주세요: ");
+            int[] userNumber = User.inputNumber();
+
+            // 결과 출력
+            result = PrintResult.printResult(target, userNumber);
+        }while(result == false);
+    }
 }
