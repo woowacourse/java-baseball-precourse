99f376b3a63cee0d5e8997fa2fcf3fb2d291dace
이민재 <lmj938@naver.com>
2021-11-26 21:09:24 +0900

    [refactor] 상수들을 Constant와 SystemMessage 클래스를 만들어 따로 모아주었음

--- src/main/java/baseball/constant/Constant.java
+++ src/main/java/baseball/constant/Constant.java
@@ -4,4 +4,7 @@ public class Constant {
     public final static int MIN_RANGE_NUM = 1;
     public final static int MAX_RANGE_NUM = 9;
     public final static int USER_NUMBER_LENGTH = 3;
+
+    public final static int BALL_INITIAL_VALUE = 0;
+    public final static int STRIKE_INITIAL_VALUE = 0;
 }
