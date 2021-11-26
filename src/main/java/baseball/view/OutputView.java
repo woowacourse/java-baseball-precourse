372c6bbac9ea9fd634d6fc0009353d40589bd687
swcho <progress0407@gmail.com>
2021-11-27 00:30:46 +0900

    style: isEndGame 오타 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,7 @@ import baseball.model.Balls;
 
 public class OutputView {
     public static void printResult(Balls balls) {
-        System.out.println("OutputView.printResult");
+
     }
 
     public static boolean isEndGame() {
