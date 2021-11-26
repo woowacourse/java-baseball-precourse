0974e581e7fc95f17623ee9e180fb2b816e6ee75
minsulee <alkwen0996@naver.com>
2021-11-26 14:33:42 +0900

    refactor: 정답 메시지 출력 내용 수정 printCorrectAnswerMessage -> 3스트라이크 문구 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -11,6 +11,8 @@ public class BaseballGame {
     public static final String COLLECT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_OR_NOT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String GAME_RESTART_NUMBER = "1";
+    public static final String THREE_STRIKE = "3스트라이크";
+    public static final String NEW_LINE = "\n";
 
     public void startGame() {
 
@@ -67,7 +69,7 @@ public class BaseballGame {
     }
 
     private void printCorrectAnswerMessage() {
-        System.out.println(COLLECT_ANSWER_MESSAGE);
+        System.out.println(THREE_STRIKE + NEW_LINE + COLLECT_ANSWER_MESSAGE);
     }
 
 }
