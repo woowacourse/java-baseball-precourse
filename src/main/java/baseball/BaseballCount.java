b3cc544512230780ff2ca29ade258896dd07addf
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-26 11:37:33 +0900

    refactor(BaseballCount): delete instance variables && change method parameter

--- src/main/java/baseball/BaseballCount.java
+++ src/main/java/baseball/BaseballCount.java
@@ -3,22 +3,18 @@ package baseball;
 public class BaseballCount {
 	public int ballCount = 0;
 	public int strikeCount = 0;
-	public int[] userNums;
-	public int[] computerNums;
 
 	public BaseballCount(int[] userNums, int[] computerNums) {
-		this.userNums = userNums;
-		this.computerNums = computerNums;
-		this.count();
+		this.count(userNums, computerNums);
 	}
 
-	public void count() {
+	public void count(int[] userNums, int[] computerNums) {
 		for (int i = 0; i < 3; i++) {
 			if (userNums[i] == computerNums[i]) {
 				strikeCount++;
 				continue;
 			}
-			if (isInArray(userNums[i], this.computerNums)) {
+			if (isInArray(userNums[i], computerNums)) {
 				ballCount++;
 			}
 		}
