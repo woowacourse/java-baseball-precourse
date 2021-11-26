a6156bc38593f695b3ef2f92b3be19110b0dd25d
qbang <vhfhddl0101@naver.com>
2021-11-26 15:00:33 +0900

    feat: pick 3 random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        List<Integer> answerNumbersList = pickUniqueNumbersInRange(1, 9, 3);
     }
 }
