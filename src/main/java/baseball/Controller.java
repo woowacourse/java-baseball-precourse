c7aae358a7e0f007998d052d75e14ab8b2bf6014
dongho <dongho108@naver.com>
2021-11-26 14:43:42 +0900

    refactor: easy to understand

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -8,6 +8,9 @@ import baseball.view.Output;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Controller {
+    final int SIZE = 3;
+    final int START_INCLUSIVE = 1;
+    final int END_INCLUSIVE = 9;
 
     Parser parser = new Parser();
     Compute compute = new Compute();
@@ -20,37 +23,24 @@ public class Controller {
     }
 
     private void gameSet() {
-        gameNumber = RandomUtils.getRandomNumbers(3, 1, 9);
+        gameNumber = RandomUtils.getRandomNumbers(SIZE, START_INCLUSIVE, END_INCLUSIVE);
     }
 
     private void gameStart() throws IllegalArgumentException{
-//        System.out.println(gameNumber[0]+""+gameNumber[1]+""+gameNumber[2]);
+        System.out.println(gameNumber[0]+""+gameNumber[1]+""+gameNumber[2]);
         play();
         gameOver();
     }
 
-    private void gameOver() throws IllegalArgumentException{
-        output.gameSetMessage();
-        UserInput.retryMessage();
-        askRetry();
-    }
-
-    private void askRetry() throws IllegalArgumentException{
-        String input = Console.readLine();
-        int inputNum = Integer.parseInt(input);
-        if (inputNum == 0 || inputNum > 2) {
-            throw new IllegalArgumentException();
-        }
-        if (inputNum == 1) {
-            run();
-        }
-    }
-
+    /**
+     * score[0] : ball 개수
+     * score[1] : strike 개수
+     */
     private void play() throws IllegalArgumentException{
         int strike = 0;
         while (strike != 3) {
             int userNumber[] = getUserNumber();
-            int[] score = compute.computeScore(gameNumber, userNumber, 3);
+            int[] score = compute.computeScore(gameNumber, userNumber, SIZE);
             output.outputScore(score[0], score[1]);
             strike = score[1];
         }
@@ -63,5 +53,27 @@ public class Controller {
         int inputNum[] = parser.userInputParser(input);
 
         return inputNum;
+    }
+
+    private void gameOver() throws IllegalArgumentException{
+        output.gameSetMessage();
+        UserInput.retryMessage();
+        askRetry();
+    }
+
+    /**
+     * 유저입력이 (문자 or 0 or 3 이상) : Exception
+     * 유저입력이 (1) : 재시작
+     * 유저입력이 (2) : 종료
+     */
+    private void askRetry() throws IllegalArgumentException{
+        String input = Console.readLine();
+        int inputNum = Integer.parseInt(input);
+        if (inputNum == 0 || inputNum > 2) {
+            throw new IllegalArgumentException();
+        }
+        if (inputNum == 1) {
+            run();
+        }
     }
 }
