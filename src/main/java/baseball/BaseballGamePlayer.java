6b92f35153ccd89d78ef8a07509409c60115c418
yangdongjue5510 <djwhy5510@gmail.com>
2021-11-26 13:27:57 +0900

    feat: 입력 문자열과 정답 문자열을 비교해서 스트라이크 볼 갯수를 판별하는 기능

---
+++ src/main/java/baseball/BaseballGamePlayer.java
@@ -0,0 +1,33 @@
+package baseball;
+
+/**
+ * 난수를 생성 및 저장, 스트라이크 볼 판별, 결과 출력, 게임 재시작
+ * 야구 게임 관련 책임을 가진 클래스
+ */
+public class BaseballGamePlayer {
+    private int strikeCount = 0;
+    private int ballCount = 0;
+    private String goalNumberString = null;
+
+    public void setRandomGoalNumberString() {
+        goalNumberString = RandomNumberStringMaker.getRandomNumberString();
+    }
+
+    /**
+     * 사용자의 문자열과 목표 문자열을 비교해서 스트라이크, 볼 필드를 초기화.
+     * @param userString
+     */
+    public void compareString(String userString) {
+        strikeCount = 0;
+        ballCount = 0;
+
+        for (int i = 0 ; i < goalNumberString.length() ; i++) {
+            char number = goalNumberString.charAt(i);
+            if (userString.charAt(i)==number) {
+                strikeCount++;
+            } else if (userString.contains(String.valueOf(number))) {
+                ballCount++;
+            }
+        }
+    }
+}
