e8f09118f9e5926753ba117913ec8a64d04555fc
hyeonic <dev.hyeonic@gmail.com>
2021-11-27 00:58:04 +0900

    feat: InputView 잘못된 입력에 대한 검증 및 예외 처리 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,7 +1,9 @@
 package baseball.view;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -13,12 +15,19 @@ public class InputView {
     private static final int INPUT_NUMBERS_SIZE = 3;
     private static final String INPUT_RESTART_COMMAND = "1";
     private static final String INPUT_END_COMMAND = "2";
+    private static final int INPUT_MIN_NUMBER = 1;
+    private static final int INPUT_MAX_NUMBER = 9;
 
     public List<Integer> getInputNumbers() {
         System.out.print(INPUT_NUMBERS_MESSAGE);
         String inputNumbers = Console.readLine();
         validateSize(inputNumbers.length());
-        return toList(inputNumbers);
+        validateDigit(inputNumbers);
+
+        List<Integer> numbers = toList(inputNumbers);
+        validateDuplicate(numbers);
+        validateNumberRange(numbers);
+        return numbers;
     }
 
     public boolean isRestart() {
@@ -37,10 +46,35 @@ public class InputView {
         }
     }
 
+    private void validateDigit(String inputNumbers) {
+        try {
+            Integer.parseInt(inputNumbers);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     private List<Integer> toList(String inputNumbers) {
         return Arrays.stream(inputNumbers.split(INPUT_NUMBERS_DELIMITER))
             .map(Integer::parseInt)
             .collect(Collectors.toList());
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> noneDuplicatedNumbers = new HashSet<>(numbers);
+        if (noneDuplicatedNumbers.size() != INPUT_NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        long size = numbers.stream()
+            .filter(i -> i >= INPUT_MIN_NUMBER && i <= INPUT_MAX_NUMBER)
+            .count();
+
+        if (size != INPUT_NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private String inputCommand() {
