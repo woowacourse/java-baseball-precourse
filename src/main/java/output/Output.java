954ed59565c1e04ddaedd212bd803f982d60edc0
jurlring <78091011+jurlring@users.noreply.github.com>
2021-11-26 18:51:20 +0900

    refactor : 변수, 메소드명 수정

--- src/main/java/output/Output.java
+++ src/main/java/output/Output.java
@@ -1,40 +1,53 @@
 package output;
 
 public class Output {
-    private static final int UPPER_BOUND=3;
-    private static final int LOWER_BOUND=0;
-    private static final String BALL_STRING="볼";
-    private static final String STRIKE_STRING="스트라이크";
-    private static final String INPUT_MESSAGE="숫자를 입력해 주세요 : ";
-    private static final String NOTHTING="낫싱";
-    private static final String ENTER="\n";
-    private static final String FINISH="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String SELECT="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final int UPPER_BOUND = 3;
+    private static final int LOWER_BOUND = 0;
+    private static final String BALL_STRING = "볼";
+    private static final String STRIKE_STRING = "스트라이크";
+    private static final String INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
+    private static final String NOTHTING_STRING = "낫싱";
+    private static final String ENTER = "\n";
+    private static final String FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String SELECT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printMessage(String message) {
         System.out.print(message);
     }
 
-    public static void pleaseInput(){
+    public static void pleaseInput() {
         printMessage(INPUT_MESSAGE);
+    }
+
+    public static void printResult(int ball, int strike) {
+        printBall(ball);
+        printStrike(strike);
+        printNothing(ball, strike);
+        printEnter();
+        isFinish(strike);
     }
 
     public static void printEnter() {
         printMessage(ENTER);
     }
 
-    public static void printResult(int ball, int strike){
-        printBall(ball);
-        printStrike(strike);
-        printNothing(ball,strike);
-        printEnter();
-        IsStrike(strike);
-
-
+    public static void printBall(int ball) {
+        if (ball != LOWER_BOUND)
+            printMessage(ball + BALL_STRING + " ");
     }
 
-    public static void IsStrike(int strike) {
-        if (strike==UPPER_BOUND) {
+    public static void printStrike(int strike) {
+        if (strike != LOWER_BOUND)
+            printMessage(strike + STRIKE_STRING);
+    }
+
+    public static void printNothing(int ball, int strike) {
+        if (ball == LOWER_BOUND && strike == LOWER_BOUND)
+            printMessage(NOTHTING_STRING);
+    }
+
+    public static void isFinish(int strike) {
+        if (strike == UPPER_BOUND) {
             printFinish();
             printEnter();
             printSelect();
@@ -49,21 +62,5 @@ public class Output {
     public static void printFinish() {
         printMessage(FINISH);
     }
-
-    public static void printNothing(int ball, int strike) {
-        if (ball==LOWER_BOUND&&strike==LOWER_BOUND)
-            printMessage(NOTHTING);
-    }
-
-    public static void printStrike(int strike) {
-        if (strike!=LOWER_BOUND)
-            printMessage(strike+STRIKE_STRING);
-    }
-
-    public static void printBall(int ball) {
-        if (ball!=LOWER_BOUND)
-            printMessage(ball+BALL_STRING+" ");
-    }
-
 
 }
