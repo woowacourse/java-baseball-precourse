141e9064a816b0c9495aa574b0709f689ecaf86e
skennetho <thrkd613@naver.com>
2021-11-26 21:33:16 +0900

    test: additional testcase for when userInput is out of range
    또다른 예외로 범위 바깥을 넣었을때를 테스트 해보았고 결과는 통과였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,9 +6,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
+		String userInput = readLine();
+		checkUserInput(userInput);
 	}
 
-	//TODO: Target클래스에 맞춰서 범위와 길이를 맞춰야 함.
+	//TODO: Target클래스에 맞춰서 범위와 길이를 맞춰야함.
 	static void checkUserInput(String userInput) {
 		if (userInput.length() > 3) {
 			throw new IllegalArgumentException();
