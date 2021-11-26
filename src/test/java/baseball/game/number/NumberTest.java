2c5e94ac871c8381c63c109ee78dad9ff0e45b68
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-26 22:07:26 +0900

    test<NumberTest>: 난수 생성 및 검증에 대한 테스트 수행
    - 난수가 생성되는 것은 이미 패키지에서 주어진 메소드이므로 생략
    - 난수를 생성했을 때, 검증을 마치고 반환된 숫자들이 규칙을 지키는지 확인
    - 반복문을 통해 10000번 테스트 수행
    
    난수 생성 이후 중복된 숫자나 0의 사용 유무 검증 테스트를 수행하였고 통과하였다.

---
+++ src/test/java/baseball/game/number/NumberTest.java
@@ -0,0 +1,23 @@
+package baseball.game.number;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class NumberTest {
+
+	@Test
+	public void 난수_검증() throws Exception {
+		for (int i = 0; i < 10000; i++) {
+			int randomNumber = Number.createRandomNumber();
+			assertThat(String.valueOf(randomNumber)).doesNotContain("0");
+			assertThat(Arrays.stream(String.valueOf(randomNumber)
+					.split(""))
+				.distinct()
+				.count())
+				.isEqualTo(3);
+		}
+	}
+}
