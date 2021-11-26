2cc95b53456e3ce2c3492917eb1cac93c6dc23a3
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-26 15:42:16 +0900

    refactor(Application): add final values - RESTART,QUIT

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application extends IllegalArgumentException {
+	public static final String RESTART = "1";
+	public static final String QUIT = "2";
+
 	public static void main(String[] args) {
 		do {
 			BaseballSystem system = new BaseballSystem();
@@ -12,10 +16,10 @@ public class Application extends IllegalArgumentException {
 	public static boolean exit() {
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 		String exitNum = Console.readLine();
-		if (exitNum.equals("1")) {
+		if (exitNum.equals(RESTART)) {
 			return false;
 		}
-		if (exitNum.equals("2")) {
+		if (exitNum.equals(QUIT)) {
 			return true;
 		}
 		throw new IllegalArgumentException();
