58b25ad11b748a67dc705628045a0b827210a702
dongho <dongho108@naver.com>
2021-11-26 17:20:49 +0900

    style: import convention

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -7,6 +7,7 @@ import baseball.utils.Parser;
 import baseball.utils.RandomUtils;
 import baseball.view.RequestInput;
 import baseball.view.SystemMessage;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Controller {
