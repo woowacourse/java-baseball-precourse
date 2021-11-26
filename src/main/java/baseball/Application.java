db51d2f5b408ebacddde5dea10eb20931db8f780
bcc0830 <bcc0830@naver.com>
2021-11-26 21:05:54 +0900

    feat : 입력 유효성 확인 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,21 @@
 package baseball;
 
 import java.util.HashSet;
+import java.lang.IllegalArgumentException;
 
 public class Application {
 	static int RANDOM_NUMBER_START = 1;
 	static int RANDOM_NUMBER_END = 9;
 	static int RANDOM_NUMBER_SIZE = 3;
+	static char GAME_INPUT_START = '1';
+	static char GAME_INPUT_END = '9';
+	static int GAME_INPUT_SIZE = 3;
+	static int TOTAL_NUM = 10;
 
 	public static void main(String[] args) {
-
+		System.out.print("숫자를 입력해주세요 : ");
+		String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+		checkInput(userInput, GAME_INPUT_START, GAME_INPUT_END, GAME_INPUT_SIZE);
 	}
 
 	public static String generateNumber() {
@@ -22,5 +29,21 @@ public class Application {
 			randomNumber += eachNum;
 		}
 		return randomNumber;
+	}
+
+	public static void checkInput(String input, char start, char end, int size) throws IllegalArgumentException {
+		boolean numberCondition = true;
+		int[] numberCheck = new int[TOTAL_NUM];
+		for (int index = 0; index < input.length(); index++) {
+			char nowInput = input.charAt(index);
+			if (!(start <= nowInput && nowInput <= end) || numberCheck[nowInput - '0'] != 0) {
+				numberCondition = false;
+				break;
+			}
+			numberCheck[nowInput - '0'] += 1;
+		}
+		if (input.length() != size || !numberCondition) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
