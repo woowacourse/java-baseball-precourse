bebd83775f20f718e7462921a0af1a6532a4c613
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-26 22:22:02 +0900

    feat<GuideMessage>: 게임 안내 멘트 반환 및 검증 메소드 구현
    - 숫자를 입력해달라는 메세지
    - 틀린 경우
      - 낫싱 메시지
      - 볼만 출력하는 메세지
      - 스트라이크만 출력하는 메세지
      - 볼과 스트라이크 모두 출력하는 메세지
    - 정답인 경우
      - 정답 메세지
      - 재시작 또는 종료 안내 메시지
    
    - 각 메세지들에 대한 검증도 구현
      - 낫싱 메세지 : 0볼 0스트라이크여야만 한다
      - 볼만 출력 메세지 : 스트라이크는 0, 볼은 1, 2, 3 중 하나여야 한다
      - 스트라이크만 출력 메세지 : 볼은 0, 스트라이크는 1, 2 중 하나여야 한다
      - 볼 스트라이크 출력 메세지 : 1볼 1스트라이크 또는 2볼 1스트라이크여야 한다
      - 정답 메세지 : 0볼 3스트라이크여야만 한다
    
    진행 멘트 문자열을 반환해주는 메소드 및 검증 메소드 생성.

---
+++ src/main/java/baseball/game/service/GuideMessage.java
@@ -0,0 +1,60 @@
+package baseball.game.service;
+
+public class GuideMessage {
+
+	public static String enterNumberMessage() {
+		return "숫자를 입력해주세요 : ";
+	}
+
+	public static String wrongNumberMessage(int ball, int strike) {
+		if (ball == 0 && strike == 0) {
+			return nothingMessage(ball, strike);
+		} else if (strike == 0) {
+			return ballOnlyMessage(ball, strike);
+		} else if (ball == 0) {
+			return strikeOnlyMessage(ball, strike);
+		} else {
+			return ballAndStrikeMessage(ball, strike);
+		}
+	}
+
+	public static String nothingMessage(int ball, int strike) {
+		if (ball == 0 && strike == 0) {
+			return "낫싱";
+		} else {
+			throw new IllegalArgumentException("낫싱 출력에 잘못된 인수가 전달되었습니다");
+		}
+	}
+
+	public static String ballOnlyMessage(int ball, int strike) {
+		if (strike == 0 && (ball == 1 || ball == 2 || ball == 3)) {
+			return ball + "볼";
+		} else {
+			throw new IllegalArgumentException("볼만 출력에 잘못된 인수가 전달되었습니다");
+		}
+	}
+
+	public static String strikeOnlyMessage(int ball, int strike) {
+		if (ball == 0 && (strike == 1 || strike == 2)) {
+			return strike + "스트라이크";
+		} else {
+			throw new IllegalArgumentException("스트라이크만 출력에 잘못된 인수가 전달되었습니다");
+		}
+	}
+
+	public static String ballAndStrikeMessage(int ball, int strike) {
+		if (ball == 1 && strike == 1 || ball == 2 && strike == 1) {
+			return ball + "볼 " + strike + "스트라이크";
+		} else {
+			throw new IllegalArgumentException("볼과 스트라이크 출력에 잘못된 인수가 전달되었습니다");
+		}
+	}
+
+	public static String correctNumberMessage(int ball, int strike) {
+		if (ball == 0 && strike == 3) {
+			return "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+		} else {
+			throw new IllegalArgumentException("정답 메세지 출력에 잘못된 인수가 전달되었습니다");
+		}
+	}
+}
