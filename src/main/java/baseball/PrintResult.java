78d5ffd94e5ed50b68012c2b2e76a6f57404b733
iamsseaa <homingsea@gmail.com>
2021-11-26 12:14:51 +0900

    feat: 두 수가 같은지 확인하는 메소드

--- src/main/java/baseball/PrintResult.java
+++ src/main/java/baseball/PrintResult.java
@@ -2,8 +2,13 @@ package baseball;
 
 public class PrintResult {
 
+    public static boolean validateSameNumber(int target, int num) {
+        if(target == num) {
+            return true;
+        }
+        return false;
+    }
 
-
-
+    
 
 }
