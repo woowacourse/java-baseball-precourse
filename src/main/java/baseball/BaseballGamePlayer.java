81956b7f2b79c23f6e7199ef0a0c6820caf9c226
yangdongjue5510 <djwhy5510@gmail.com>
2021-11-26 13:28:51 +0900

    feat: 스트라이크 볼 갯수에 따라 결과를 출력하는 기능

--- src/main/java/baseball/BaseballGamePlayer.java
+++ src/main/java/baseball/BaseballGamePlayer.java
@@ -30,4 +30,16 @@ public class BaseballGamePlayer {
             }
         }
     }
+
+    public void printResult() {
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCount == 0) {
+            System.out.println(ballCount+"볼");
+        } else if (ballCount == 0) {
+            System.out.println(strikeCount+"스트라이크");
+        } else if (ballCount != 0 && strikeCount != 0) {
+            System.out.println(ballCount+"볼 "+strikeCount+"스트라이크");
+        }
+    }
 }
