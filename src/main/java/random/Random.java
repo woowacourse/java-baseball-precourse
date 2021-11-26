765e11d59de72bfae9e432e799bb105e1956b20b
DK <ldk980130@gmail.com>
2021-11-26 20:59:38 +0900

    fix : 난수 생성하는 범위 1, 9 위치 바뀐 것 수정

--- src/main/java/random/Random.java
+++ src/main/java/random/Random.java
@@ -8,16 +8,16 @@ public class Random {
 
 	public static String createAnswer() {
 
-		int firstNumber = Randoms.pickNumberInRange(MAX_NUMBER, MIN_NUMBER);
+		int firstNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 		int secondNumber;
 		int thirdNumber;
 
 		do {
-			secondNumber = Randoms.pickNumberInRange(MAX_NUMBER, MIN_NUMBER);
+			secondNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 		} while (secondNumber == firstNumber);
 
 		do {
-			thirdNumber = Randoms.pickNumberInRange(MAX_NUMBER, MIN_NUMBER);
+			thirdNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 		} while (thirdNumber == secondNumber || thirdNumber == firstNumber);
 
 		return firstNumber + "" + secondNumber + thirdNumber;
