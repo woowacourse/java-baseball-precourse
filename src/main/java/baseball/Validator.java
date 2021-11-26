08b73559100ea7845a105f51c621f1def74292de
AlphaTechnic <alphatechnicist@gmail.com>
2021-11-26 13:21:00 +0900

    [feat] 사용자 입력 내용 검증을 위한 Validator 클래스 생성, validateNumberInput() 구현

---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,43 @@
+package baseball;
+
+public class Validator {
+    public boolean [] numberChecked = new boolean[10];
+
+    public void validateNumberInput(String numberInputStr) {
+        this.initNumberChecked();
+
+        int length = numberInputStr.length();
+        if (length != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        numberChecked[0] = true;  // 0은 true로 set
+        for (int i = 0; i < length; i++) {
+            char ch = numberInputStr.charAt(i);
+            int digit = ch - '0';
+
+            // 1부터 9까지의 숫자가 아닌 digit => 유효하지 않음
+            if (!(1 <= digit && digit <= 9)) {
+                throw new IllegalArgumentException();
+            }
+
+            // 중복된 digit의 등장 => 유효하지 않음
+            if (numberChecked[digit]) {
+                throw new IllegalArgumentException();
+            }
+            else {
+                numberChecked[digit] = true;
+            }
+        }
+    }
+
+    public void validateDecisionInput(String decisionInputStr) {
+
+    }
+
+    public void initNumberChecked() {
+        for (int i = 1; i <= 9; i++) {
+            numberChecked[i] = false;
+        }
+    }
+}
