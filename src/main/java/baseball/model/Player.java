7b47724e799dec4abde42a40abecfaf4d45d4105
이민재 <lmj938@naver.com>
2021-11-26 21:04:10 +0900

    [feat] Player 클래스 추가

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,9 @@
+package baseball.model;
+
+public class Player {
+    private PlayerNumber playerNumber = new PlayerNumber();
+
+    public void setPlayerNumber(String number) {
+        playerNumber.setPlayerNumber(number);
+    }
+}
