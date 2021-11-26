ead5e1f138d3b699c71309a50ebeb130d90c33e2
bcc0830 <bcc0830@naver.com>
2021-11-26 20:51:01 +0900

    feat : 세자리 랜덤 난수 생성 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import java.util.HashSet;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	static int RANDOM_NUMBER_START = 1;
+	static int RANDOM_NUMBER_END = 9;
+	static int RANDOM_NUMBER_SIZE = 3;
+
+	public static void main(String[] args) {
+
+	}
+
+	public static String generateNumber() {
+		HashSet<Integer> numberSet = new HashSet<>();
+		String randomNumber = "";
+		while (numberSet.size() < RANDOM_NUMBER_SIZE) {
+			numberSet.add(
+				camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(RANDOM_NUMBER_START, RANDOM_NUMBER_END));
+		}
+		for (int eachNum : numberSet) {
+			randomNumber += eachNum;
+		}
+		return randomNumber;
+	}
 }
