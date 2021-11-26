31e255017a59fdbb760aba9e66a3a57d9f517472
pup-paw <wldusdl0310@gmail.com>
2021-11-26 22:22:37 +0900

    feat: Input Validator 의 예외 처리 코드 추가

--- src/main/java/baseball/gameController/GameDriver.java
+++ src/main/java/baseball/gameController/GameDriver.java
@@ -2,10 +2,12 @@ package baseball.gameController;
 
 import java.util.HashSet;
 
+import baseball.errorHandler.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameDriver {
 	NumberGenerator numberGenerator = new NumberGenerator();
+	InputValidator inputValidator = new InputValidator();
 	HintGiver hintGiver = new HintGiver();
 	String answer = "";
 
@@ -22,10 +24,11 @@ public class GameDriver {
 		int playerAnswer;
 		while (!hintGiver.hint.equals("3스트라이크")) {
 			System.out.print("숫자를 입력해주세요 : ");
-			playerAnswer = Integer.parseInt(Console.readLine());
+			this.answer = Console.readLine();
+			inputValidator.checkPlayerAnswer(this.answer);
+			playerAnswer = Integer.parseInt(this.answer);
 			hintGiver.setScore(realAnswer, playerAnswer);
 			hintGiver.getHint();
-			System.out.println(realAnswer);
 		}
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
@@ -33,5 +36,6 @@ public class GameDriver {
 	private void askResume() {
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
 		this.answer = Console.readLine();
+		inputValidator.checkResumeAnswer(this.answer);
 	}
 }
