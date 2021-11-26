8e03b353878af66cfa25992c5a0a933e9838eb16
이민재 <lmj938@naver.com>
2021-11-26 15:35:01 +0900

    [feat] 예외 발생 시SystemMessage 출력 기능 추가

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,6 +1,7 @@
 package baseball.model;
 
 import baseball.constant.Constant;
+import baseball.constant.SystemMessage;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,14 +18,14 @@ public class UserNumber {
     }
 
     public void setUserNumber(String userNumber) {
-        if (isStringLengthThree(userNumber) && isDigitUserNumber(userNumber) && isSameAllUserNumber(userNumber)) {
+        if (isStringLengthCorrect(userNumber) && isDigitUserNumber(userNumber) && isDifferentUserNumber(userNumber)) {
             this.userNumber = userNumber;
         }
     }
 
-    public static boolean isStringLengthThree(String word) throws IllegalArgumentException {
+    public static boolean isStringLengthCorrect(String word) throws IllegalArgumentException {
         if (word.length() != Constant.USER_NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SystemMessage.NUMBER_LENGTH_NOT_CORRECT);
         }
         return true;
     }
@@ -36,21 +37,21 @@ public class UserNumber {
         return true;
     }
 
-    public static boolean isSameAllUserNumber(String word) throws IllegalArgumentException {
+    public static boolean isDifferentUserNumber(String word) throws IllegalArgumentException {
         Set<Character> set = new HashSet<>();
 
         for (int i = 0; i < word.length(); i++) {
             set.add(word.charAt(i));
         }
         if (set.size() != word.length()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SystemMessage.HAS_SAME_NUMBER);
         }
         return true;
     }
 
     public static boolean isDigitCharInString(String word, int index) throws IllegalArgumentException {
         if (!Character.isDigit(word.charAt(index))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SystemMessage.STRING_IS_NOT_NUMBER);
         }
         return true;
     }
