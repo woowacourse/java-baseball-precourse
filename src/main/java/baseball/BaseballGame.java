86f51f295f37da051de09b2bca5dbf451215a6d0
iamsseaa <homingsea@gmail.com>
2021-11-26 14:21:00 +0900

    feat: 다음 경기의 여부를 묻는 메소드 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -22,4 +22,12 @@ public class BaseballGame {
             result = PrintResult.printResult(target, userNumber);
         }while(result == false);
     }
+
+    public static int NextRound() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        int nextRound = User.choiceNextRound();
+
+        return nextRound;
+    }
 }
