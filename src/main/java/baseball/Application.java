8114dfcf0fc4f31a755f1bef8f3d1030c2cceb33
HyeonbinSa <shb1833@gmail.com>
2021-11-26 19:29:13 +0900

    feat: 게임 시작 기능 및 재시작 or 종료 선택 기능 구현
    Class : BaseballGame
    기능 1. 게임 시작 : startGame()
    기능 2. 게임 반복 : playBaseBallGame()
    기능 3. 게임 종료 시 선택지 입력 및 입력값 검증 : selectRestartOrQuit(), validateUserSelection()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,15 +4,7 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        Computer computer = new Computer();
-        ArrayList<Integer> computerNumberList = computer.createComputerNumber();
-        System.out.println(computerNumberList);
-
-        User user = new User();
-        ArrayList<Integer> userNumberList = user.inputUserNumber();
-        System.out.println(userNumberList);
-
-        GameReferee gameReferee = new GameReferee();
-        gameReferee.decideResult(computerNumberList, userNumberList);
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.startGame();
     }
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,59 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+public class BaseballGame {
+    private final static String RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final static String RESTART = "1";
+    private final static String QUIT = "2";
+    private Computer computer;
+    private User user;
+    private GameReferee gameReferee;
+    private ArrayList<Integer> computerNumberList;
+    private ArrayList<Integer> userNumberList;
+
+    public BaseballGame() {
+        this.computer = new Computer();
+        this.user = new User();
+        this.gameReferee = new GameReferee();
+    }
+
+    public void startGame() {
+        while (this.playBaseBallGame()) {
+            computerNumberList.clear();
+            userNumberList.clear();
+        }
+    }
+    
+    private boolean playBaseBallGame() {
+        computerNumberList = computer.createComputerNumber();
+        userNumberList = user.inputUserNumber();
+
+        while (!gameReferee.decideResult(computerNumberList, userNumberList)) {
+            userNumberList = user.inputUserNumber();
+        }
+
+        return this.selectRestartOrQuit();
+    }
+
+    /* 재시작 및 종료 선택 함수 */
+    private boolean selectRestartOrQuit() {
+        System.out.println(RESTART_MSG);
+        String userSelection = Console.readLine();
+
+        return validateUserSelection(userSelection);
+    }
+
+    /* 입력값 검증 함수 */
+    private boolean validateUserSelection(String userSelection) {
+        if (userSelection.equals(RESTART) && userSelection.equals(QUIT)) {
+            return false;
+        }
+        if (userSelection.equals(QUIT)) {
+            return false;
+        }
+        return true;
+    }
+}
