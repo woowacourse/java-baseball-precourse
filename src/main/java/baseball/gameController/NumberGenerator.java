6c39f7cea19ca3baad831ad6710038ff036d1d2f
pup-paw <wldusdl0310@gmail.com>
2021-11-26 18:21:37 +0900

    feat: Math.random 대신 Random.pickNumberInRange 사용

--- src/main/java/baseball/gameController/NumberGenerator.java
+++ src/main/java/baseball/gameController/NumberGenerator.java
@@ -6,14 +6,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
 
-	private HashSet<Integer> generateNumSet() {
+	public HashSet<Integer> generateNumSet() {
 		HashSet<Integer> selectedNum = new HashSet<>();
 
 		while (selectedNum.size() < 3) {
 			int randomNum = Randoms.pickNumberInRange(1, 9);
 			selectedNum.add(randomNum);
 		}
-		
 		return selectedNum;
 	}
 }
