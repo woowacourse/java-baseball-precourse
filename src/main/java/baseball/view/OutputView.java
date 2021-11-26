c981c63cbef6a6cfe79fe274b1148309502937b6
sinb57 <sinb57@naver.com>
2021-11-26 20:35:47 +0900

    feat(view) : 메시지 출력 메소드 구현
    - OutputView : printMessage / printMessageWithLine

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static void printMessage(String message) {
+        System.out.print(message);
+    }
+
+    public static void printMessageWithLine(String message) {
+        System.out.println(message);
+    }
+
+}
