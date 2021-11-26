2650b134b61487e7c8082118b7ce824e40cf6955
dongho <dongho108@naver.com>
2021-11-26 14:19:31 +0900

    refactor: throw exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,10 +4,6 @@ public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         Controller controller = new Controller();
-        try {
-            controller.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        controller.run();
     }
 }
