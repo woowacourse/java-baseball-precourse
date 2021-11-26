d6da435d311cff70d12ac3b5f5de2e3dd851e6ed
zzambbang <zzambbang@gmail.com>
2021-11-26 22:31:51 +0900

    refactor(Application): change method main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,20 +3,18 @@ package baseball;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 	private static final int RESTART_CODE = 1;
 
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
-		Scanner scanner = new Scanner(System.in);
 		int playingStatus;
 
-		// do {
-		// 	ArrayList<Integer> generationNumbers = Number.createRandomNumbers();
-		// 	Game.play(scanner, generationNumbers);
-		// 	playingStatus = Game.isContinue(scanner);
-		// } while (playingStatus == RESTART_CODE);
-
-		scanner.close();
+		do {
+			Game.play();
+			playingStatus = Game.isContinue();
+		} while (playingStatus == RESTART_CODE);
 	}
 }
