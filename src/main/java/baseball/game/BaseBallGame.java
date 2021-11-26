0d7e7a535eab5aaf2048d80f046d4144fd339098
EastHShin <xldk78@gmail.com>
2021-11-27 02:09:33 +0900

    feat(baseball): 결과에 따라 게임을 반복하는 기능 구현

--- src/main/java/baseball/game/BaseBallGame.java
+++ src/main/java/baseball/game/BaseBallGame.java
@@ -3,12 +3,15 @@ package baseball.game;
 import java.util.ArrayList;
 import java.util.List;
 
+import baseball.messages.GameMessage;
 import baseball.utils.PlayerInput;
 import baseball.utils.RandomNumbers;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseBallGame {
 	private static final int NUM_OF_DIGIT = 3;
+	private static final String GAME_CONTINUE = "1";
+	private static final String GAME_FINISH = "2";
 
 	private static List<Integer> randomNumbers;
 	private static List<Integer> playerNumbers;
@@ -22,8 +25,27 @@ public class BaseBallGame {
 		baseBallReferee = new BaseBallReferee(NUM_OF_DIGIT);
 	}
 
-	public static void playGame(){
-		playerNumbers = playerInput.getInput();
+	public static boolean playGame(){
+		int strikeCount;
+		int ballCount;
+		do {
+			System.out.print(GameMessage.INPUT_MESSAGE);
+			playerNumbers = playerInput.getInput();
+			strikeCount = baseBallReferee.getStrikeCount(randomNumbers, playerNumbers);
+			ballCount = baseBallReferee.getBallCount(randomNumbers, playerNumbers);
+		} while(resultOfGame(strikeCount, ballCount));
+
+		String decisionOfPlayer = playerInput.getIsContinue();
+		if(decisionOfPlayer.equals(GAME_FINISH)){
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean resultOfGame(int strikeCount, int ballCount) {
+
+		return false;
 	}
 
 
