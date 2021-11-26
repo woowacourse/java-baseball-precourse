e1eb1124548fe4b784bf3b02bc6239e93979cc61
nailseong <nnis9857@gmail.com>
2021-11-26 18:06:25 +0900

    refactor(main): play 메서드 리팩토링으로 인한 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,20 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game();
         game.init();
-
-        while (true) {
-            boolean isFinish = game.play();
-            if (!isFinish) {
-                continue;
-            }
-
-            game.printEndMessage();
-            boolean isContinue = game.askRestart();
-            if (!isContinue) {
-                break;
-            }
-
-            game.init();
-        }
+        game.play();
     }
 }
+
