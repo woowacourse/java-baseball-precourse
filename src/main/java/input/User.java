d20c80ba58361ac1326134076ec804adf22d4d8a
jurlring <78091011+jurlring@users.noreply.github.com>
2021-11-26 19:00:01 +0900

    refactor : 사용자 입력 값을 List로 변환

--- src/main/java/input/User.java
+++ src/main/java/input/User.java
@@ -3,37 +3,45 @@ package input;
 import camp.nextstep.edu.missionutils.Console;
 import output.Output;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
-    private static final int BALL_SIZE=3;
-    private static final char UPPER_BOUND='9';
-    private static final char LOWER_BOUND='1';
+    private static final int BALL_MAX_SIZE = 3;
+    private static final char BALL_UPPER_BOUND = '9';
+    private static final char BALL_LOWER_BOUND = '1';
 
-    Output output=new Output();
-
-    public static String userInput() {
+    public static List<Integer> readUserInput() {
         Output.pleaseInput();
-        String userInput=Console.readLine();
+        String userInput = Console.readLine();
         checkException(userInput);
-        return userInput;
+        return convertStringToList(userInput);
+    }
+
+    private static List<Integer> convertStringToList(String userInput) {
+        List<Integer> userInputList = new ArrayList<>();
+        userInput.chars().forEach(
+                c -> userInputList.add(c - '0')
+        );
+        return userInputList;
     }
 
     public static void checkException(String userInput) {
-        if (!checkSize(userInput)||!checkRange(userInput.toCharArray()))
+        if (!checkSize(userInput) || !checkRange(userInput.toCharArray()))
             throw new IllegalArgumentException();
     }
 
     private static boolean checkRange(char[] toCharArray) {
-        for(char c:toCharArray){
-            if (c>UPPER_BOUND||c<LOWER_BOUND)
+        for (char c : toCharArray) {
+            if (c > BALL_UPPER_BOUND || c < BALL_LOWER_BOUND)
                 return false;
         }
         return true;
     }
 
-
     private static boolean checkSize(String userInput) {
-        if (userInput.length()==BALL_SIZE)
+        if (userInput.length() == BALL_MAX_SIZE)
             return true;
         return false;
     }
