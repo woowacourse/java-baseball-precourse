a92d87414e486ed495a3c9306894a456923e350c
이한울 <dkfghg3@naver.com>
2021-11-27 00:48:19 +0900

    feat: 플레이어가 3자리 수를 입력하는 기능 구현 비정상적인 입력에 대한 예외 처리 기능 구현

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,62 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import baseball.type.ExceptionType;
+import baseball.type.NumberRangeType;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+	private static List<Integer> playerNumbers;
+
+	public Player() {
+		playerNumbers = new ArrayList<>(NumberRangeType.ANSWER_SIZE.getNumberRange());
+	}
+
+	public void generatePlayerNumber() {
+		String enterPlayerNumber = Console.readLine();
+		validatePlayerNumber(enterPlayerNumber);
+		setPlayerNumber(enterPlayerNumber);
+	}
+
+	private void setPlayerNumber(String enterPlayerNumber) {
+		for (char number : enterPlayerNumber.toCharArray()) {
+			playerNumbers.add(number - '0');
+		}
+	}
+
+	private void validatePlayerNumber(String enterPlayerNumber) {
+		validatePlayerNumberLength(enterPlayerNumber.length());
+		validatePlayerNumberType(enterPlayerNumber);
+		validatePlayerNumberDuplicate(enterPlayerNumber);
+	}
+
+	private void validatePlayerNumberType(String enterPlayerNumber) {
+		for (char number : enterPlayerNumber.toCharArray()) {
+			if (number < NumberRangeType.MIN_NUMBER_ASCII.getNumberRange()
+				|| number > NumberRangeType.MAX_NUMBER_ASCII.getNumberRange()) {
+				throw new IllegalArgumentException(ExceptionType.WRONG_TYPE_INPUT.getMessage());
+			}
+		}
+	}
+
+	private void validatePlayerNumberDuplicate(String enterPlayerNumber) {
+		Set<Character> checkList = new HashSet<>();
+		for (char number : enterPlayerNumber.toCharArray()) {
+			if (checkList.contains(number)) {
+				throw new IllegalArgumentException(ExceptionType.DUPLICATE.getMessage());
+			}
+			checkList.add(number);
+		}
+	}
+
+	private void validatePlayerNumberLength(int enterPlayerNumberLength) {
+		if (enterPlayerNumberLength != NumberRangeType.ANSWER_SIZE.getNumberRange()) {
+			throw new IllegalArgumentException(ExceptionType.OUT_OF_LENGTH.getMessage());
+		}
+	}
+}
