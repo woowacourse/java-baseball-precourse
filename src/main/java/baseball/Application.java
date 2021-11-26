1ed3db9cdb541c94bb32f1c00b8b9b110ce75507
khj1 <dhepd8849@gmail.com>
2021-11-27 01:18:30 +0900

    feat: 게임 실행 기능 구현
    새롭게 구현된 기능들:
    - 사용자가 3 스트라이크를 만들어낼 때 까지 게임을 반복한다.
    - 정답을 맞춘 후 사용자가 1을 입력하면 새로운 게임을 시작한다.
    - 정답을 맞춘 후 사용자가 2를 입력하면 게임이 완전히 종료된다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
 public class Application {
+    private static final String FAILED = "failed";
+    private static final String RESTART = "1";
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        String status = FAILED;
+        Balls computerBalls = new Balls();
+
+        while (status.equals(FAILED) || status.equals(RESTART)) {
+            Balls userBalls = new Balls(inputView.attempt());
+            GameResult result = new GameResult(computerBalls, userBalls);
+
+            outputView.printResult(result);
+            status = setStatus(result);
+            computerBalls = setComputerBalls(status, computerBalls);
+        }
+    }
+
+    private static String setStatus(GameResult result) {
+        if (result.isEnd()) {
+            return inputView.askRestart();
+        }
+        return FAILED;
+    }
+
+    private static Balls setComputerBalls(String status, Balls computerBalls) {
+        if (status.equals(RESTART)) {
+            return new Balls();
+        }
+        return computerBalls;
     }
 }
