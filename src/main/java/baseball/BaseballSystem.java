8a7ae2c16cd649c47c6b4c5c6ccc2d7478677c8f
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-26 16:38:39 +0900

    refactor(BaseballSystem): refactory some code in class to method

--- src/main/java/baseball/BaseballSystem.java
+++ src/main/java/baseball/BaseballSystem.java
@@ -8,24 +8,50 @@ public class BaseballSystem {
 	}
 
 	public void gameStart() {
+		boolean threeStrike;
 		do {
-			int[] userNums = new UserNumGenerator().nums;
-			BaseballCount set = new BaseballCount(userNums, computerNums);
+			BaseballCount set = new BaseballCount(new UserNumGenerator().nums, computerNums);
+			printGameResult(set);
+			threeStrike = isThreeStrike(set);
+		} while (!threeStrike);
+	}
 
-			if (set.strikeCount == 3) {
-				System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-				break;
-			}
-			if (set.ballCount != 0) {
-				System.out.print(set.ballCount + "볼 ");
-			}
-			if (set.strikeCount != 0) {
-				System.out.print(set.strikeCount + "스트라이크");
-			}
-			if (set.ballCount == 0 && set.strikeCount == 0) {
-				System.out.print("낫싱");
-			}
-			System.out.println();
-		} while (true);
+	public void printGameResult(BaseballCount set) {
+		printOnlyBall(set);
+		printOnlyStrike(set);
+		printBallAndStrike(set);
+		printNoting(set);
+	}
+
+	public boolean isThreeStrike(BaseballCount set) {
+		if (set.strikeCount == 3) {
+			System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			return false;
+		}
+		return true;
+	}
+
+	public void printOnlyBall(BaseballCount set) {
+		if (set.ballCount != 0 && set.strikeCount == 0) {
+			System.out.println(set.ballCount + "볼");
+		}
+	}
+
+	public void printOnlyStrike(BaseballCount set) {
+		if (set.ballCount == 0 && set.strikeCount != 0) {
+			System.out.println(set.strikeCount + "스트라이크");
+		}
+	}
+
+	public void printBallAndStrike(BaseballCount set) {
+		if (set.ballCount != 0 && set.strikeCount != 0) {
+			System.out.print(set.ballCount + "볼 " + set.strikeCount + "스트라이크");
+		}
+	}
+
+	public void printNoting(BaseballCount set) {
+		if (set.ballCount == 0 && set.strikeCount == 0) {
+			System.out.println("낫싱");
+		}
 	}
 }
