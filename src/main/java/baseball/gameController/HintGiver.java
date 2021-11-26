836821ce66e7a410c1b73a5c1424f8f13b6363a8
pup-paw <wldusdl0310@gmail.com>
2021-11-26 18:18:12 +0900

    feat: 힌트 제공기 초기화 기능 추가

--- src/main/java/baseball/gameController/HintGiver.java
+++ src/main/java/baseball/gameController/HintGiver.java
@@ -4,14 +4,20 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 public class HintGiver {
-	String ball = "볼";
-	String strike = "스트라이크";
+	final String ball = "볼";
+	final String strike = "스트라이크";
 	int ballCount = 0;
 	int strikeCount = 0;
+	String hint = "";
+
+	public void initHint() {
+		this.ballCount = 0;
+		this.strikeCount = 0;
+		this.hint = "";
+	}
 
 	private void checkStrike(HashSet<Integer> realAnswer, int[] playerAnswer) {
 		Iterator<Integer> iterator = realAnswer.iterator();
-		// int num = 0;
 		for (int answer : playerAnswer) {
 			if (iterator.next() == answer) {
 				this.strikeCount++;
@@ -29,23 +35,37 @@ public class HintGiver {
 		this.ballCount -= this.strikeCount;
 	}
 
-	public void setScore(HashSet<Integer> realAnswer, int[] playerAnswer) {
+	public void setScore(HashSet<Integer> realAnswer, int input) {
+		initHint();
+		int[] playerAnswer = splitInt(input);
 		checkStrike(realAnswer, playerAnswer);
 		if (this.strikeCount < 2) {
 			checkBall(realAnswer, playerAnswer);
 		}
+		setHint();
 	}
 
-	public void getHint() {
+	public void setHint() {
 		if (this.ballCount + this.strikeCount == 0) {
-			System.out.println("낫싱");
+			hint += "낫싱";
+			// System.out.println("낫싱");
 			return;
 		}
 		if (this.ballCount > 0) {
-			System.out.print(ballCount + ball + " ");
+			hint += ballCount + ball;
+			// System.out.print(ballCount + ball + " ");
 		}
 		if (this.strikeCount > 0) {
-			System.out.println(strikeCount + strike);
+			hint += strikeCount + strike;
+			// System.out.println(strikeCount + strike);
 		}
+	}
+
+	public void getHint() {
+		System.out.println(hint);
+	}
+
+	public int[] splitInt(int input) {
+		return new int[] {input / 100, input % 100 / 10, input % 10};
 	}
 }
