111239b1a26f413ce032625851e7558c688a330c
yeongunheo <gjduddns@gmail.com>
2021-11-26 23:38:12 +0900

    feat: 1에서 9까지 서로 다른 수로 이루어진 3자리의 임의의 수를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+	}
+
+	public String createRandomNumber() {
+		String randomNumber = "";
+		while (randomNumber.length() < 3) {
+			String num = String.valueOf(Randoms.pickNumberInRange(1, 10));
+			if (!randomNumber.contains(num)) {
+				randomNumber += num;
+			}
+		}
+		return randomNumber;
+	}
 }
