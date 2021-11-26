695bbfe5d36f19e4251da559f59a270b5eeb1830
wannagohome <iwwuf7@gmail.com>
2021-11-26 21:48:12 +0900

    refactor(game): change target number as global variable
     - 컴퓨터의 숫자를 전역변수로 관리

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -3,24 +3,32 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseBallGame {
+    private String targetNumber;
     private static final String BASEBALL_INPUT_REQUEST_TEXT = "숫자를 입력해주세요 : ";
     private static final String RESTART_INPUT_REQUEST_TEXT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void start() {
-        String targetNumber = NumberGenerator.generate();
+        generateTarget();
         System.out.println(targetNumber);
-        requestInputUntilCorrectAnswer(targetNumber);
-
+        requestInputUntilCorrectAnswer();
         configureRestartGame();
     }
 
-    private void requestInputUntilCorrectAnswer(String targetNumber) {
+    private void restart() {
+        start();
+    }
+
+    private void generateTarget() {
+        targetNumber = NumberGenerator.generate();
+    }
+
+    private void requestInputUntilCorrectAnswer() {
         String inputNumber = "";
 
         while (!inputNumber.equals(targetNumber)) {
             inputNumber = requestBaseBallInput();
-            printCalculatedHint(targetNumber, inputNumber);
+            printCalculatedHint(inputNumber);
         }
     }
 
@@ -29,21 +37,21 @@ public class BaseBallGame {
         BaseBallValidator.validateRestart(restart);
 
         if (RestartInput.byNumber(restart) == RestartInput.RESTART) {
-            start();
+            restart();
         }
     }
 
-    private void printCalculatedHint(String targetNumber, String inputNumber) {
-        BaseBallHint hint = calculateHint(targetNumber, inputNumber);
+    private void printCalculatedHint(String inputNumber) {
+        BaseBallHint hint = calculateHint(inputNumber);
         System.out.println(hint);
     }
 
-    private BaseBallHint calculateHint(String target, String input) {
+    private BaseBallHint calculateHint(String input) {
         BaseBallHint result = new BaseBallHint();
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == target.charAt(i)) {
+            if (input.charAt(i) == targetNumber.charAt(i)) {
                 result.strikeCount++;
-            } else if (target.contains(String.valueOf(input.charAt(i)))) {
+            } else if (targetNumber.contains(String.valueOf(input.charAt(i)))) {
                 result.ballCount++;
             }
         }
