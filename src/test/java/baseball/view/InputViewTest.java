3d1ce11ff33a1bee4f3917325ecba17d6444688e
swcho <progress0407@gmail.com>
2021-11-27 00:10:06 +0900

    test(InputViewTest): 정상값, 비정상값 테스트 코드 작성

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,7 +1,9 @@
 package baseball.view;
 
+import baseball.model.Balls;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +12,13 @@ class InputViewTest {
     @Test
     void 잘못된값_입력() {
         assertThatThrownBy(() -> InputView.inputNumber(new FixedInputStrategy("1234")));
+        assertThatThrownBy(() -> InputView.inputNumber(new FixedInputStrategy("12")));
+        assertThatThrownBy(() -> InputView.inputNumber(new FixedInputStrategy("abc")));
+    }
+
+    @Test
+    void 정상값입력후_Balls객체_생성여부() {
+        Balls balls = InputView.inputNumber(new FixedInputStrategy("456"));
+        assertThat(balls).isEqualTo(new Balls(4, 5, 6));
     }
 }
