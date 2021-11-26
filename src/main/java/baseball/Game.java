649a8224630d067a367dd97214c64d94fe3467ab
jochungbeom <cndqjacndqja@gmail.com>
2021-11-26 16:24:00 +0900

    feat: 3자리 수, 게임 끝난 경우 재시작/종료를 구분하는 입력문 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,6 +8,12 @@ public class Game {
 	Referee referee = new Referee(computerBalls);
 	boolean game = true;
 
+	public Boolean decideWhetherRestart() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		int input = Integer.parseInt(Console.readLine());
+		return input == 1;
+	}
+
 	public void run() {
 		while (game) {
 			System.out.print("숫자를 입력해주세요 : ");
@@ -16,6 +22,20 @@ public class Game {
 			BallRepository userBalls = user.getBalls();
 			referee.judge(userBalls.getBalls());
 			Output.printResult(referee.getStrikeCount(), referee.getBallCount());
+			if (referee.getStrikeCount() == 3) {
+				setForRestarting();
+			}
+			referee.clear();
+		}
+	}
+
+	public void setForRestarting() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		game = decideWhetherRestart();
+		if (game) {
+			computer = new Computer();
+			computerBalls = computer.getBalls();
+			referee = new Referee(computerBalls);
 		}
 	}
 }
