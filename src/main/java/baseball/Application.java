16d4ad31184946a996631caac638382a2e2215ef
skennetho <thrkd613@naver.com>
2021-11-26 21:04:36 +0900

    feat: add checkUserInput() to filter the illegal userInput
    함수에서 범위와 길이를 체크하여 부적절하면 illegalArgumentException이 나게끔 구현하였다.
    readLine()한 string을 넣어 예외_테스트()를 진행한 결과 통과함 또한 확인하였다.
    그러나 추후에 Target 클래스의 멤버변수에 맞게 범위와 길이를 수정해야하고 이를 TODO에 적어두었다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+	}
+
+	//TODO: Target클래스에 맞춰서 범위와 길이를 맞춰야 함.
+	static void checkUserInput(String userInput) {
+		if (userInput.length() > 3) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = 0; i < userInput.length(); i++) {
+			if (userInput.charAt(i) - '0' < 1 && userInput.charAt(i) - '0' > 9) {
+				throw new IllegalArgumentException();
+			}
+		}
+	}
 }
