bd9891baa2ce6c80562a49929bdf2ea5008f2edf
iamsseaa <homingsea@gmail.com>
2021-11-26 16:48:17 +0900

    feat: 컴퓨터가 타겟 랜덤 수를 선택할 때 중복되는 경우 다시 선택

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,15 +2,33 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
+
 public class Computer {
 
-    public int[] target = new int[3];
+    public int[] target = {0, 0, 0};
+    int[] number_check = {0,0,0,0,0,0,0,0,0,0};
 
     // 정답으로 쓰일 임의의 수 3개를 target array에 저장한다.
     public void makeTarget() {
-        for(int i=0; i<3; i++) {
-            target[i] = Randoms.pickNumberInRange(1, 9);
+        int i = 0;
+
+        while(true) {
+            if(target[2] != 0) {
+                break;
+            }
+
+            int num = Randoms.pickNumberInRange(1, 9);
+
+            if(number_check[num] == 0) {
+                target[i] = num;
+                number_check[num]++;
+                i++;
+            }
         }
     }
+
 }
 
