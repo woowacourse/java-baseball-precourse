d41b062b273749f062dac829220355403717de34
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-11-26 17:17:39 +0900

    feat: 공격 숫자 입력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,49 @@
+package baseball.view;
+
+import java.util.Objects;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final int CORRECT_NUMBER_LENGTH = 3;
+
+    private static final String NUMERIC_FILTER = "[1-9]+";
+
+    public String guessNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        final String input = readLine();
+        validateAttackInput(input);
+        return input;
+    }
+
+    private String readLine() {
+        return Console.readLine();
+    }
+
+    private void validateAttackInput(String input) {
+        if (isContainsZero(input)) {
+            throw new IllegalArgumentException("1 ~ 9 까지의 숫자만 입력할 수 있습니다 ! 게임 종료");
+        }
+
+        if (!isCorrectLength(input)) {
+            throw new IllegalArgumentException("세 개의 숫자만 입력할 수 있습니다 ! 게임 종료");
+        }
+
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다 ! 게임 종료");
+        }
+    }
+
+    private boolean isContainsZero(String input) {
+        return input.contains("0");
+    }
+
+    private boolean isCorrectLength(String input) {
+        return Objects.equals(input.length(), CORRECT_NUMBER_LENGTH);
+    }
+
+    private boolean isNumeric(String input) {
+        return input.matches(NUMERIC_FILTER);
+    }
+}
