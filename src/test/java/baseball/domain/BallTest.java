abb795e4b58ea2292b11f886df79f5119e9e1025
yoon <duddbs774@gmail.com>
2021-11-27 17:10:43 +0900

    test: baseball.domain.BallTest 사용하지 않는 api 삭제

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class BallTest {
 	@DisplayName("스트라이크 테스트")
