e1d9e1b78e97b92f1aea5044f427963ce9b90a06
juwon <58173061+juwon0605@users.noreply.github.com>
2021-11-27 23:40:12 +0900

    refactor(src/main/java/baseball): use private instead of unneccessary static

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,7 @@ package baseball;
 public class Application {
 
 	public static void main(String[] args) {
-		while (true) {
-			boolean finishBaseballGame = BaseballGame.play();
-			if (finishBaseballGame) {
-				break;
-			}
-		}
+		BaseballGame baseballGame = new BaseballGame();
+		baseballGame.play();
 	}
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,29 +5,36 @@ import static camp.nextstep.edu.missionutils.Randoms.*;
 
 public class BaseballGame {
 
-	static final int START_NUMBER = 1;
-	static final int END_NUMBER = 9;
-	static final int ANSWER_LENGTH = 3; // 1 <= ANSWER_LENGTH <= END_NUMBER - START_NUMBER + 1
-	static final int RESTART_NUMBER = 1;
-	static final int FINISH_NUMBER = 2;
+	private final int START_NUMBER = 1; // 1 <= START_NUMBER <= 9
+	private final int END_NUMBER = 9; // START_NUMBER <= END_NUMBER <= 9
+	private final int ANSWER_LENGTH = 3; // 1 <= ANSWER_LENGTH <= END_NUMBER - START_NUMBER + 1
+	private final String RESTART_OPTION = "1";
+	private final String FINISH_OPTION = "2";
+	private final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	private final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private String answer;
+	private String userInput;
+	private int ball;
+	private int strike;
+	private boolean startBaseballGame = true;
 
-	public static boolean play() {
-		String answer = makeAnswer();
-		while (true) {
-			System.out.print("숫자를 입력해주세요 : ");
-			String input = readLine();
-			checkInput(input);
-			int[] ballAndStrikeCount = countBallAndStrike(answer, input);
-			boolean isAnswer = printBaseballGameResult(ballAndStrikeCount);
-			if (isAnswer) {
-				break;
+	public void play() {
+		while (startBaseballGame) {
+			answer = makeAnswer();
+			userInput = "";
+			while (!answer.equals(userInput)) {
+				initializeBallAndStrike();
+				System.out.print(INPUT_MESSAGE);
+				userInput = readLine();
+				checkInput();
+				countBallAndStrike();
+				printBaseballGameResult();
 			}
+			askRestartBaseballGame();
 		}
-		boolean finishBaseballGame = askFinishBaseballGame();
-		return finishBaseballGame;
 	}
 
-	public static String makeAnswer() {
+	private String makeAnswer() {
 		int[] duplicationCheckArray = new int[END_NUMBER - START_NUMBER + 1];
 		String answer = "";
 		while (answer.length() < ANSWER_LENGTH) {
@@ -40,30 +47,32 @@ public class BaseballGame {
 		return answer;
 	}
 
-	public static void checkInput(String input) {
-		if (input.length() != ANSWER_LENGTH) {
+	private void initializeBallAndStrike() {
+		ball = 0;
+		strike = 0;
+	}
+
+	private void checkInput() {
+		if (userInput.length() != ANSWER_LENGTH) {
 			throw new IllegalArgumentException();
 		}
 		for (int i = 0; i < ANSWER_LENGTH; i++) {
-			int number = input.charAt(i) - '0';
+			int number = userInput.charAt(i) - '0';
 			if (!(number >= START_NUMBER && number <= END_NUMBER)) {
 				throw new IllegalArgumentException();
 			}
 		}
 		int[] duplicationCheckArray = new int[END_NUMBER - START_NUMBER + 1];
 		for (int i = 0; i < ANSWER_LENGTH; i++) {
-			int number = input.charAt(i) - '0';
-			duplicationCheckArray[number]++;
-			if (duplicationCheckArray[number] != 1) {
+			int number = userInput.charAt(i) - '0';
+			duplicationCheckArray[number - START_NUMBER]++;
+			if (duplicationCheckArray[number - START_NUMBER] != 1) {
 				throw new IllegalArgumentException();
 			}
 		}
 	}
 
-	public static int[] countBallAndStrike(String answer, String input) {
-		int[] ballAndStrikeCount = new int[2];
-		int ballCount = 0;
-		int strikeCount = 0;
+	private void countBallAndStrike() {
 		int[] answerNumberCount = new int[END_NUMBER - START_NUMBER + 1];
 		for (int i = 0; i < ANSWER_LENGTH; i++) {
 			int answerNumber = answer.charAt(i) - '0';
@@ -71,48 +80,41 @@ public class BaseballGame {
 		}
 		for (int i = 0; i < ANSWER_LENGTH; i++) {
 			int answerNumber = answer.charAt(i) - '0';
-			int inputNumber = input.charAt(i) - '0';
+			int inputNumber = userInput.charAt(i) - '0';
 			if (answerNumber == inputNumber) {
-				strikeCount++;
+				strike++;
 			} else if (answerNumberCount[inputNumber - START_NUMBER] > 0) {
-				ballCount++;
+				ball++;
 			}
 		}
-		ballAndStrikeCount[0] = ballCount;
-		ballAndStrikeCount[1] = strikeCount;
-		return ballAndStrikeCount;
 	}
 
-	public static boolean printBaseballGameResult(int[] ballAndStrikeCount) {
-		boolean isAnswer = false;
-		int ballCount = ballAndStrikeCount[0];
-		int strikeCount = ballAndStrikeCount[1];
-		if (strikeCount == ANSWER_LENGTH) {
+	private void printBaseballGameResult() {
+		if (strike == ANSWER_LENGTH) {
 			System.out.println(ANSWER_LENGTH + "스트라이크");
 			System.out.println(ANSWER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
-			isAnswer = true;
-		} else if (ballCount > 0 && strikeCount > 0) {
-			System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
-		} else if (ballCount > 0) {
-			System.out.println(ballCount + "볼");
-		} else if (strikeCount > 0) {
-			System.out.println(strikeCount + "스트라이크");
+		} else if (ball > 0 && strike > 0) {
+			System.out.println(ball + "볼 " + strike + "스트라이크");
+		} else if (ball > 0) {
+			System.out.println(ball + "볼");
+		} else if (strike > 0) {
+			System.out.println(strike + "스트라이크");
 		} else {
 			System.out.println("낫싱");
 		}
-		return isAnswer;
 	}
 
-	public static boolean askFinishBaseballGame() {
-		boolean finishBaseballGame = true;
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-		String input = readLine();
-		if (!(input.equals(RESTART_NUMBER) || input.equals(FINISH_NUMBER))) {
+	private void askRestartBaseballGame() {
+		System.out.println(RESTART_MESSAGE);
+		String userChoice = readLine();
+		if (!(userChoice.equals(RESTART_OPTION) || userChoice.equals(FINISH_OPTION))) {
 			throw new IllegalArgumentException();
 		}
-		if (input.equals(RESTART_NUMBER)) {
-			finishBaseballGame = false;
+		if (userChoice.equals(RESTART_OPTION)) {
+			startBaseballGame = true;
 		}
-		return finishBaseballGame;
+		if (userChoice.equals(FINISH_OPTION)) {
+			startBaseballGame = false;
+		}
 	}
 }
