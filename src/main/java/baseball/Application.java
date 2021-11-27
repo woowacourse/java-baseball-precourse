d7a6163a6880d28ec2f43f9dff0ff34e032dd98e
Sanggu <rkdtkdrn93@naver.com>
2021-11-27 19:35:33 +0900

    feat: 숫자를 입력받는 기능 구현
    입력받은 숫자를 비교하기 편하게 List로 변경.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import net.bytebuddy.pool.TypePool.Resolution.Illegal;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        System.out.println("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        List<Integer> userNumbers = makeList(input);
+        System.out.println(input);
     }
+
+    public static List<Integer> makeList(String input) {
+        String[] arrayInput = input.split("");
+        List<Integer> userNumbers = new ArrayList<>();
+        for (int i=0; i<input.length();i++) {
+            userNumbers.add(Integer.parseInt(arrayInput[i]));
+        }
+        return userNumbers;
+    }
+
+
 }
