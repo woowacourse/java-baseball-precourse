1f0410190408ddc133e5601b0e795a1c793f0193
최창준 <aowert6518@gmail.com>
2021-11-27 21:50:13 +0900

    [feat] 사용자가 잘못입력했을 경우 예외처리하기
    - 숫자 0 제거 및 중복 제거
    - 100~999까지로 했다가 1~9로 하여 하나씩 받아오고 ArrayList의 contains를 사용하여 중복을 제거함
    
    - 숫자가 3자리가 아닐 시 예외처리
    - 숫자가 아닌 문자입력 시 예외처리
    
    - 재시작 1, 종료 2가 아닐 시 예외처리

--- src/main/java/baseball/domain/Controller.java
+++ src/main/java/baseball/domain/Controller.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Controller {
     private String randomNumber;
@@ -16,18 +17,39 @@ public class Controller {
     }
 
     private String createRandomNumber() {
-        return String.valueOf(Randoms.pickNumberInRange(100, 1000));
+        ArrayList<Integer> randomNum = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        while (randomNum.size() != 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if (!randomNum.contains(num)) {
+                randomNum.add(num);
+                sb.append(num);
+            }
+        }
+        return sb.toString();
     }
 
     public void getUserNumber() {
         Viewer.inputNumber();
         userNumber = Console.readLine();
+
+        if (userNumber.length() != 3) {
+            throw new IllegalArgumentException("3자리의 숫자를 입력해주세요!");
+        }
+        for (char num : userNumber.toCharArray()) {
+            if (!Character.isDigit(num)) {
+                throw new IllegalArgumentException("숫자를 입력해주세요!");
+            }
+        }
     }
 
     private void getEndByUser(){
         String userInput = Console.readLine();
         if (userInput.equals("2")) {
             endFlag = true;
+        } else if (!userInput.equals("1")) {
+            throw new IllegalArgumentException("재시작은 1, 종료는 2를 입력해주세요!");
         }
     }
 
