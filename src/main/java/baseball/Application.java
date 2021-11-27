e1dd177f1ec298faa8d6eaadcdfe9282d19872e4
sha11 <sha1119@naver.com>
2021-11-27 18:31:08 +0900

    feat(strikeCheck, ballCheck): 기능 추가
    스트라이크, 볼 카운팅 기능 추가
    
    자릿수 비교, 같은 수 존재유무 비교로 카운팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,15 +9,48 @@ import java.io.InputStreamReader;
 public class Application {
     private static int ballCount;
     private static int strikeCount;
+    private static int X;
+    private static int Y;
+    private static int Z;
+    private static int N;
+    private static int x;
+    private static int y;
+    private static int z;
+
 
     public static void main(String[] args) throws IOException {
         //TODO: 숫자 야구 게임 구현
 
-        int X = Randoms.pickNumberInRange(1, 9);
-        int Y = Randoms.pickNumberInRange(1, 9);
-        int Z = Randoms.pickNumberInRange(1, 9);
+        X = Randoms.pickNumberInRange(1, 9);
+        Y = Randoms.pickNumberInRange(1, 9);
+        Z = Randoms.pickNumberInRange(1, 9);
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(bufferedReader.readLine());
+
+        x = N / 100;
+        y = (N / 10) % 10;
+        z = N % 10;
+
+        ballCount = 0;
+        strikeCount = 0;
+
+        strikeCheck();
+        ballCheck();
+        print();
+    }
+
+    public static void strikeCheck() {
+        if (X == x || Y == y || Z == z) {
+            strikeCount++;
+        }
+    }
+
+    public static void ballCheck() {
+        if (X == y || X == z || Y == x || Y == z || Z == x || Z == y) {
+            ballCount++;
+        }
     }
 
     public static void print() {
