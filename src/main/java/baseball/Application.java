7edbe6316c067b76a5c1111fae3e012f8fe41d84
SheepEatLion <gowjr207@gmail.com>
2021-11-27 15:45:29 +0900

    feat(Application): main
    now User can input data

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,12 +4,17 @@ import camp.nextstep.edu.missionutils.*;
 
 public class Application {
     public static void main(String[] args) {
-
+        // 기능 1: 랜덤 값 생성
         String targetNumber = randomNumber();
 
+        while(true){
+            //  기능 2: 사용자로부터 값 입력받기
+            System.out.print("숫자를 입력해주세요: ");
+            String inputNumber = Console.readLine();
+        }
     }
 
-    // 1~9 사이의 랜덤 수 출력해서 3자리의 수로 리턴
+
     public static String randomNumber(){
 
         String firstNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
