ee31e7f1fd7fdc0bd81cfd5c6ee9fa3dffc63151
skullKim <kyk990328@gmail.com>
2021-11-27 18:08:40 +0900

    fix(RandomNumber.java): 3자리 난수의 각 자리수가 서로다른 수를 갖게 수정
    숫자야구게임 요구사항에 맞추기 위해 RandomNumber 생성자에서 생성되는 3자리
    난수의 각 자리수가 서로다른 3가지 수를 갖을 수 있게 수정.
    
    BREAKING CHANGES: RandomNumber인스턴스 생성자 내부 로직 수정

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -1,29 +1,38 @@
 package baseball;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 /**
  * <h1>야구게임에서 컴퓨터가 사용할 3자리 난수를 생성한다</h1>
  *
  * @author yunki kim
- * @version 1.0
+ * @version 2.0
  * @since 2021-11-25(V1.0)
  */
 
 public class RandomNumber {
-	/** 생성자에서 3자리 난수 생성에 사용되는 난수 범위 최대값 */
-	public static final Integer RANDOM_NUMBER_MAX_RANGE = 1000;
+	/** 생성자에서 n자리 난수 생성에 사용되는 난수 범위 최대값 */
+	public static final Integer RANDOM_NUMBER_MAX_RANGE = 9;
 
-	/** 생성자에서 3자리 난수 생성에 사용되는 난수 범위 최소값 */
-	public static final Integer RANDOM_NUMBER_MIN_RANGE = 100;
+	/** 생성자에서 n자리 난수 생성에 사용되는 난수 범위 최소값 */
+	public static final Integer RANDOM_NUMBER_MIN_RANGE = 1;
+
+	/** 생성자에서 n자리 난수 생성에 사용되는 값 n */
+	public static final Integer RANDOM_NUMBER_SIZE = 3;
 
 	/** 생성자에서 만든 3자리 난수를 문자열로 저장 */
 	private String randomNumber;
 
+	/** RandomNumber 인스턴스를 만드는 생성자 */
 	public RandomNumber() {
-		this.randomNumber = Integer.toString(
-			Randoms.pickNumberInRange(RANDOM_NUMBER_MIN_RANGE, RANDOM_NUMBER_MAX_RANGE)
-		);
+		final List<Integer> randomNumberList = Randoms.pickUniqueNumbersInRange(RANDOM_NUMBER_MIN_RANGE,
+			RANDOM_NUMBER_MAX_RANGE, RANDOM_NUMBER_SIZE);
+		this.randomNumber = randomNumberList.stream()
+			.map(number -> Integer.toString(number))
+			.collect(Collectors.joining());
 	}
 
 	/**
