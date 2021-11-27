dbcc2a33761f6dd648f55a57a51b9a6048751115
Gibeom <delphox60@gmail.com>
2021-11-28 08:45:38 +0900

    fix(OpponentPlayer): 유효하지 않은 3자리 수를 생성하는 오류 수정

--- src/main/java/baseball/OpponentPlayer.java
+++ src/main/java/baseball/OpponentPlayer.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.*;
 
 public class OpponentPlayer {
@@ -9,13 +10,32 @@ public class OpponentPlayer {
 
     }
 
+    private boolean isUnique(List<Integer> threeNumList) {
+        if (threeNumList.get(0).equals(threeNumList.get(1))) {
+            return false;
+        }
+        if (threeNumList.get(1).equals(threeNumList.get(2))) {
+            return false;
+        }
+        if (threeNumList.get(0).equals(threeNumList.get(2))) {
+            return false;
+        }
+        return true;
+    }
+
     public String pickRandom3DigitNum() {
         StringBuilder stringBuilder = new StringBuilder();
-        List<Integer> random3NumsList = Randoms.pickUniqueNumbersInRange(0, 9, 3);
-        for (int i = 0; i < 3; i++) {
-             stringBuilder.append(Integer.toString(random3NumsList.get(i)));
+        List<Integer> random3NumsList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        while (true)
+        {
+            for (int i = 0; i < 3; i++) {
+                stringBuilder.append(Integer.toString(random3NumsList.get(i)));
+            }
+            if (isUnique(random3NumsList))
+                break;
         }
-        return stringBuilder.toString();
+        String random3DigitNum = stringBuilder.toString();
+        return random3DigitNum;
     }
 
 }
