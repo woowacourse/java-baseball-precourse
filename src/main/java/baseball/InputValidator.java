3130e8245cb05219c13d02b4ad8c4670e7990cae
xfrnk2 <xfrnk2@gmail.com>
2021-11-27 18:40:30 +0900

    feat: 테스트를 통과하는 사용자 입력 검사 기능 구현

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -1,8 +1,26 @@
 package baseball;
 
 public class InputValidator {
+    private static final int DIGIT_NUMBER = 3;
+
     InputValidator() {
     }
-    public static boolean checkUserInput(final String userInput){
-        return false;
-    }}
+
+    public static boolean checkUserInput(final String userInput) {
+
+        // 제한된 자리수를 초과할 경우 false 리턴
+        if (DIGIT_NUMBER != userInput.length()) {
+            return false;
+        }
+
+        // 정수가 아니거나 0인 경우 false 리턴
+        for (int i = 0; i < DIGIT_NUMBER; i++) {
+            char temp = userInput.charAt(i);
+
+            if (!Character.isDigit(temp) || temp == '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
