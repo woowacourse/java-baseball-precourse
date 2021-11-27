4dbf837a96ac6fc71283923bdf7faf6d0be1dcaf
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-27 22:43:28 +0900

    refactor(BaseballCount): refactory some code in class to method

--- src/main/java/baseball/BaseballCount.java
+++ src/main/java/baseball/BaseballCount.java
@@ -1,25 +1,33 @@
 package baseball;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class BaseballCount {
 	public int ballCount = 0;
 	public int strikeCount = 0;
 
 	public BaseballCount(int[] userNums, int[] computerNums) {
-		for (int i = 0; i < Constant.NUMS_LENGTH; i++) {
-			if (userNums[i] == computerNums[i]) {
-				strikeCount++;
-				continue;
-			}
-			if (isInArray(userNums[i], computerNums)) {
-				ballCount++;
-			}
-		}
+		setBallCount(userNums, computerNums);
+		setStrikeCount(userNums, computerNums);
+	}
+
+	private void setBallCount(int[] userNums, int[] computerNums) {
+		ballCount = (int)Stream.iterate(0, i -> i + 1)
+			.limit(Constant.NUMS_LENGTH)
+			.filter(i -> userNums[i] != computerNums[i] && isInArray(userNums[i], computerNums))
+			.count();
+	}
+
+	private void setStrikeCount(int[] userNums, int[] computerNums) {
+		strikeCount = (int)Stream.iterate(0, i -> i + 1)
+			.limit(Constant.NUMS_LENGTH)
+			.filter(i -> userNums[i] == computerNums[i])
+			.count();
 	}
 
 	private static boolean isInArray(int num, int[] nums) {
-		return Arrays.stream(nums)
-			.anyMatch(arrayNum -> arrayNum == num);
+		return Arrays.stream(nums).anyMatch(arrayNum -> arrayNum == num);
 	}
 }
+
