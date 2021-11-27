b60546c254ee2c3f7ae9d63acb83cef99cace0db
Gibeom <delphox60@gmail.com>
2021-11-28 08:22:23 +0900

    feat(Application): Application에서 Game System을 실행하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        GameSystem gameSystem = new GameSystem();
+        gameSystem.executeGameSystem();
     }
 }
