54981e162643d18e461df649e76f8fcec1028cba
bugoverdose <bugod96@gmail.com>
2021-11-27 14:56:19 +0900

    feat(Game): 사용자로부터 숫자 3개 입력받아 정답 여부 출력
    - Game 클래스 내부에서 start 메서드로 게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,6 @@ import domain.Game;
 public class Application {
     public static void main(String[] args) {
         Game newGame = new Game();
-        System.out.println(newGame.answerNumbers);
+        newGame.start();
     }
 }
--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,19 +1,22 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Game {
 
-    public List<Integer> answerNumbers;
+    private List<Integer> answerNumbers;
 
     public Game() {
         reset();
     }
 
-    public void reset() {
+    private void reset() {
         List<Integer> newList = new ArrayList<>();
         boolean[] visited = new boolean[10];
 
@@ -27,5 +30,24 @@ public class Game {
         }
 
         this.answerNumbers = newList;
+    }
+
+    public void start() {
+        //TODO: should be removed
+        System.out.println("answer : " + this.answerNumbers);
+
+        System.out.print("숫자를 입력해주세요 : ");
+        List<String> input = Arrays.asList(Console.readLine().split(""));
+
+        int strikeCount = 0;
+        int idx;
+        for (idx = 0; idx < 3; idx++){
+            if (!Objects.equals(input.get(idx), this.answerNumbers.get(idx).toString())) break;
+            strikeCount++;
+        }
+        if (strikeCount == 3){
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 }
