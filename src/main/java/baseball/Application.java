1b6fbcf15d498f30268e4e95626a1d57ad5c440a
xi_jjun <rlawowns97@naver.com>
2021-11-27 23:02:24 +0900

    feat: 숫자 야구 게임 Application 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.controller.GameHandler;
+
 public class Application {
+    private static final int PROGRAM_EXIT = 2;
+    private static final GameHandler gameHandler = GameHandler.getInstance();
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        while (true) {
+            int mode = gameHandler.start();
+            if (mode == PROGRAM_EXIT) {
+                break;
+            }
+        }
     }
 }
