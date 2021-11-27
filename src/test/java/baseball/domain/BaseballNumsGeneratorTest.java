3f18d7d0c94976bdffe4108c592b8c433364e909
Wishoon <heonga2@gmail.com>
2021-11-27 22:07:15 +0900

    test: 입력한 3자리 숫자 생성기능 및 유효성 테스트 작성

--- src/test/java/baseball/domain/BaseballNumsGeneratorTest.java
+++ src/test/java/baseball/domain/BaseballNumsGeneratorTest.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +15,22 @@ class BaseballNumsGeneratorTest {
 
         // then
         assertEquals(3, randomBaseballNums.getBaseballNums().size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "234", "426", "326"})
+    public void 입력한_3자리_숫자_생성_테스트(String inputNum) {
+        // given // when
+        BaseballNums inputBaseballNums = BaseballNumsGenerator.generateInputBaseballNums(inputNum);
+        // then
+        assertEquals(3, inputBaseballNums.getBaseballNums().size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"122", "1234", "111", "xfe", "ㄹㄷㄹ", "$$%@"})
+    public void 입력한_3자리_숫자_유효성_테스트(String inputNum) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            BaseballNumsGenerator.generateInputBaseballNums(inputNum);
+        });
     }
 }
