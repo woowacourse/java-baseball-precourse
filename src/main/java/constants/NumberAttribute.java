59af5c0cc5bede669f2a59357d1649ec912eb89d
xfrnk2 <xfrnk2@gmail.com>
2021-11-27 22:10:09 +0900

    feat: 요구 조건을 따르는 숫자의 속성을 담고 있는 열거체 추가

---
+++ src/main/java/constants/NumberAttribute.java
@@ -0,0 +1,18 @@
+package constants;
+
+public enum NumberAttribute {
+    DIGIT_NUMBER(3),
+    RADIX(10),
+    MIN_RANGE(1),
+    MAX_RANGE(9);
+
+    private final int value;
+
+    NumberAttribute(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
