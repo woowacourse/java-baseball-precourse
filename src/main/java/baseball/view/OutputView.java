8b34ab0e69242ee7002eb5aac41732625953bb73
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-11-27 12:46:24 +0900

    refactor: 메서드 네이밍 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,14 +1,14 @@
 package baseball.view;
 
-import baseball.domain.vo.BallStrikeCount;
+import baseball.domain.vo.JudgementResult;
 
 public class OutputView {
 
-    public void printResult(BallStrikeCount result) {
+    public void printResult(JudgementResult result) {
         System.out.println(result);
     }
 
-    public void exitGame() {
+    public void gameOver() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
