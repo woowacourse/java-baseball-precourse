6c95e20cc6ce32dbfaff855d67f08248a8333a11
yeongunheo <gjduddns@gmail.com>
2021-11-27 12:00:54 +0900

    feat: 숫자야구게임 전체 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,6 +17,19 @@ public class Application {
 
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
+		randomNumber = createRandomNumber();
+		while (!isFinished) {
+			while (!isThreeStrike) {
+				System.out.println("숫자를 입력해주세요 : ");
+				inputNumber = Console.readLine();
+				printBallAndStrikeCount();
+				isThreeStrike();
+			}
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			checkRestartOrFinished = Console.readLine();
+			isFinished();
+		}
 	}
 
 	public static String createRandomNumber() {
