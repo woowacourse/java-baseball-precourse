58cab8f0d38cd0b601c88048a8361329d3a5aab8
xfrnk2 <xfrnk2@gmail.com>
2021-11-27 18:34:02 +0900

    test: 사용자 입력 검사 테스트 케이스 작성 (테스트가 실패한다)

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -3,4 +3,6 @@ package baseball;
 public class InputValidator {
     InputValidator() {
     }
-}
+    public static boolean checkUserInput(final String userInput){
+        return false;
+    }}
---
+++ src/test/java/baseball/InputValidatorTest.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class InputValidatorTest {
+
+    public static final HashMap<String, Boolean> testCase = new HashMap<String, Boolean>() {{//초기값 지정
+
+        put("123", true);
+        put("987", true);
+        put("999", true);
+
+        put("1234", false);
+        put("780", false);
+        put("a91", false);
+        put("d", false);
+        put("55", false);
+        put("", false);
+
+    }};
+
+    @Test
+    public void checkUserInput() {
+
+        for (Map.Entry<String, Boolean> entry : testCase.entrySet()) {
+
+            String key = entry.getKey();
+            Boolean result = entry.getValue();
+
+            Assertions.assertEquals(result, InputValidator.checkUserInput(key), "요구조건을 충족하지 않음");
+
+        }
+    }
+
+}
