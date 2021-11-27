280853880c6160119512ba78aaabc3e8606862ff
이한울 <dkfghg3@naver.com>
2021-11-28 01:44:59 +0900

    refactor: 예외처리에 대한 enum Type 추가 재시작 1, 종료2 가 아닌 값이 입력될 경우 예외

--- src/main/java/baseball/type/ExceptionType.java
+++ src/main/java/baseball/type/ExceptionType.java
@@ -4,6 +4,7 @@ public enum ExceptionType {
 	DUPLICATE("중복된 수가 있습니다."),
 	OUT_OF_LENGTH("3자리의 숫자로 입력해주세요."),
 	WRONG_TYPE_INPUT("1 ~ 9가 아닌 수가 포함되어 있습니다."),
+	NOT_RESTART_AND_FINISH("입력한 수가 잘못되었습니다. 게임을 새로 시작하려면 1을, 종료하려면 2를 입력해주세요."),
 	;
 
 	private String message;
