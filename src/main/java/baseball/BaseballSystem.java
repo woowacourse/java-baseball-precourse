ff4c8964acf3ac622a57e3e33b32994f5806497e
BETTERFUTURE4 <bumkyu11@naver.com>
2021-11-27 16:22:59 +0900

    fix: change print() to println()

--- src/main/java/baseball/BaseballSystem.java
+++ src/main/java/baseball/BaseballSystem.java
@@ -11,14 +11,14 @@ public class BaseballSystem {
 		} while (!gameClear);
 	}
 
-	public static void printGameResult(BaseballCount set) {
+	private static void printGameResult(BaseballCount set) {
 		printOnlyBall(set);
 		printOnlyStrike(set);
 		printBallAndStrike(set);
 		printNoting(set);
 	}
 
-	public boolean isMaxStrike(BaseballCount set) {
+	private static boolean isMaxStrike(BaseballCount set) {
 		if (set.strikeCount == Constant.NUMS_LENGTH) {
 			System.out.println(Constant.NUMS_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
 			return true;
@@ -26,28 +26,27 @@ public class BaseballSystem {
 		return false;
 	}
 
-	public static void printOnlyBall(BaseballCount set) {
+	private static void printOnlyBall(BaseballCount set) {
 		if (set.ballCount != 0 && set.strikeCount == 0) {
 			System.out.println(set.ballCount + "볼");
 		}
 	}
 
-	public static void printOnlyStrike(BaseballCount set) {
+	private static void printOnlyStrike(BaseballCount set) {
 		if (set.ballCount == 0 && set.strikeCount != 0) {
 			System.out.println(set.strikeCount + "스트라이크");
 		}
 	}
 
-	public static void printBallAndStrike(BaseballCount set) {
+	private static void printBallAndStrike(BaseballCount set) {
 		if (set.ballCount != 0 && set.strikeCount != 0) {
-			System.out.print(set.ballCount + "볼 " + set.strikeCount + "스트라이크");
+			System.out.println(set.ballCount + "볼 " + set.strikeCount + "스트라이크");
 		}
 	}
 
-	public static void printNoting(BaseballCount set) {
+	private static void printNoting(BaseballCount set) {
 		if (set.ballCount == 0 && set.strikeCount == 0) {
 			System.out.println("낫싱");
 		}
 	}
 }
-
