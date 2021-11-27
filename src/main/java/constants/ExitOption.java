8689810285dd941deb7dd526070555df33f5f6db
xfrnk2 <xfrnk2@gmail.com>
2021-11-27 22:09:09 +0900

    feat: 재시작 및 종료 커맨드를 담고있는 열거체 추가

---
+++ src/main/java/constants/ExitOption.java
@@ -0,0 +1,16 @@
+package constants;
+
+public enum ExitOption {
+    RESTART("1"),
+    END("2");
+
+    private final String command;
+
+    ExitOption(String command) {
+        this.command = command;
+    }
+
+    public String command() {
+        return command;
+    }
+}
