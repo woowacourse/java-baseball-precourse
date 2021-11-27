07b361b73fc8b5df91a9c6a47a9e86bfb26c65c4
maprk <zldndlzl@naver.com>
2021-11-27 19:52:51 +0900

    refactor: 폴더 구조 변경으로 인한 import 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import baseball.game.BaseballGameManager;
+
 public class Application {
 	public static void main(String[] args) {
 		BaseballGameManager baseballGameManager = new BaseballGameManager();
