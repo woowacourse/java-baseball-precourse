e7b15b0e396afdced14f5da6244b23643557fbfe
xfrnk2 <xfrnk2@gmail.com>
2021-11-27 11:27:10 +0900

    feat: 숫자 요구 조건을 정의

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,10 @@
 package baseball;
 
 public class Game {
+  private final int MIN_RANGE = 1;
+  private final int MAX_RANGE = 9;
+  private final int DIGIT_NUM = 3;
+  private final int RADIX = 10;
 
   Game () {
   }
