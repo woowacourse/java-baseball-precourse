4dd7f9009f4ae887af1e124ad33edf5a836da3ae
pup-paw <wldusdl0310@gmail.com>
2021-11-27 14:36:04 +0900

    feat : 프로젝트 내에서 사용되는 상수 처리

---
+++ src/main/java/baseball/constantStorage/Constant.java
@@ -0,0 +1,15 @@
+package baseball.constantStorage;
+
+public class Constant {
+	public static final int NUMBER_LENGTH = 3;
+	public static final int MIN_NUMBER = 1;
+	public static final int MAX_NUMBER = 9;
+	public static final String BALL = "볼";
+	public static final String STRIKE = "스트라이크";
+	public static final String NOTHING = "낫싱";
+	public static final String ASK_ANSWER = "숫자를 입력해주세요 : ";
+	public static final String YOU_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public static final String ASK_RESUME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String RESUME = "1";
+	public static final String EXIT = "2";
+}
