fc39488eac56d3275389e17712595af945f362d0
Gibeom <delphox60@gmail.com>
2021-11-27 09:42:38 +0900

    feat(GameSystem): User로부터 3자리 수를 입력받는 기능 구현

--- src/main/java/baseball/GameSystem.java
+++ src/main/java/baseball/GameSystem.java
@@ -2,10 +2,13 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class GameSystem {
 
     private static final int SIG_GAME_CONTINUE = 1;
     private static final int SIG_GAME_END = 2;
+    private static final String THREE_DIGIT_PATTERN = "^ddd$";
 
     private int signal;
 
@@ -23,10 +26,19 @@ public class GameSystem {
     private void playGame() {
     }
 
+    private String getUser3DigitNum() {
+        String input3DigitNum = Console.readLine();
+        if (Pattern.matches(THREE_DIGIT_PATTERN, input3DigitNum)) {
+            throw new IllegalArgumentException();
+        }
+        return input3DigitNum;
+    }
+
     private void determineContinueGame() {
         String users_signal = Console.readLine();
-        if (users_signal.equals("2"))
+        if (users_signal.equals("2")) {
             signal = 2;
+        }
         try {
             if (!users_signal.equals("1")) {
                 throw new IllegalArgumentException();
