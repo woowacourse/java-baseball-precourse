b6933a756473e93f4fcbc6e5556eb8fcdbe1dc79
Sanggu <rkdtkdrn93@naver.com>
2021-11-28 00:33:01 +0900

    feat: 게임 종료 후 재시작 선택 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.List;
-import net.bytebuddy.pool.TypePool.Resolution.Illegal;
-
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         GameSimulation gameSimulation = new GameSimulation();
-        gameSimulation.playGame();
+        while (gameSimulation.gameEndCheck()) {
+            gameSimulation.playGame();
+        }
     }
 
 
--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -22,22 +22,17 @@ public class GameSimulation {
 	}
 
 	public void init() {
-		this.comNum = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+		comNum = Randoms.pickUniqueNumbersInRange(1, 9, 3);
 	}
 
 	public void playGame() {
-		System.out.println(comNum);
 		System.out.println("숫자를 입력해주세요 : ");
 		String userInput = Console.readLine();
 		this.userNum = makeList(userInput);
 		System.out.println(userNum);
 		validateUserNum();
 		compareNumbers();
-		System.out.println(strike);
-		System.out.println(ball);
-		message = compareResultReturn();
-		System.out.println(message);
-
+		System.out.println(compareResultReturn());
 	}
 
 	public List<Integer> makeList(String userInput) {
@@ -79,15 +74,33 @@ public class GameSimulation {
 
 	public String compareResultReturn() {
 		if (ball != 0 && strike != 0) {
-			return(ball+"볼"+" "+strike+"스트라이크");
+			return (ball + "볼" + " " + strike + "스트라이크");
 		}
 		if (ball == 0 && strike != 0) {
-			return(strike+"스트라이크");
+			return (strike + "스트라이크");
 		}
 		if (ball != 0 && strike == 0) {
-			return(ball+"볼");
+			return (ball + "볼");
 		}
-		return("낫싱");
+		return ("낫싱");
+	}
+
+	public boolean gameEndCheck() {
+		if (strike != null && strike == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
+
+			String response = Console.readLine();
+			if (response.equals("1")) {
+				init();
+				return true;
+			}
+			if (response.equals("2")) {
+				return false;
+			}
+			throw new IllegalArgumentException("올바른 숫자가 아닙니다. 1 또는 2를 입력하세요.");
+		}
+		return true;
 	}
 
 }
