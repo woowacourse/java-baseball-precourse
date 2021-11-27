bd86765f98493384425a42093eb1ec5f07cbe1f1
이민재 <lmj938@naver.com>
2021-11-27 13:34:29 +0900

    [style] SystemMessage 공백 변경

--- src/main/java/baseball/constant/SystemMessage.java
+++ src/main/java/baseball/constant/SystemMessage.java
@@ -4,7 +4,6 @@ public class SystemMessage {
     public final static String NUMBER_LENGTH_NOT_CORRECT = "입력한 숫자의 길이가 올바르지 않습니다.";
     public final static String HAS_SAME_NUMBER = "중복된 숫자가 포함되어있습니다.";
     public final static String STRING_IS_NOT_NUMBER = "숫자가 아닌 문자가 포함되어 있습니다.";
-
     public final static String SET_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public final static String BALL_MESSAGE = "볼";
