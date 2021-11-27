6dfe81c3156f2fa98740787c600b3dc534bfcb83
xfrnk2 <xfrnk2@gmail.com>
2021-11-28 01:36:43 +0900

    feature: 게임 종료 조건 만족시 재시작 또는 종료 여부를 판별하는 동작 수행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-        Game baseballGame = new Game();
+        Game game = new Game();
+        do {
+            game.init();
+            game.playGame();
+        } while (game.checkUserWantRestart());
     }
 }
