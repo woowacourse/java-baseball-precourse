eebd6e5a82ea08ca961dcd3349f371425fa885d3
xi_jjun <rlawowns97@naver.com>
2021-11-27 22:47:16 +0900

    refactor: playerInput method

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+public class Player {
+    private static Player instance;
+    private int[] tryNumber = new int[3];
+
+    private Player() {
+    }
+
+    public static Player getInstance() {
+        if (instance == null) {
+            return new Player();
+        }
+        return instance;
+    }
+
+    public int[] submitTryNumber() {
+        return tryNumber;
+    }
+
+    public void playerInput(String playerInput) {
+        int input = Integer.parseInt(playerInput);
+        tryNumber[0] = input / 100;
+        tryNumber[1] = (input % 100) / 10;
+        tryNumber[2] = input % 10;
+    }
+}
