37f03f2940d83ff2533835042a231498b60b6b78
dongho <dongho108@naver.com>
2021-11-27 17:58:40 +0900

    refactor: update baseball position

--- src/main/java/baseball/service/Computer.java
+++ src/main/java/baseball/service/Computer.java
@@ -4,16 +4,16 @@ import baseball.domain.BaseBall;
 
 public class Computer {
 
-    public BaseBall computeScore(int[] gameNumber, int[] userNumber, int size) {
-        BaseBall baseBall = new BaseBall();
+    BaseBall baseBall = new BaseBall();
 
+    public BaseBall computeScore(int[] gameNumber, int[] userNumber, int size) {
         for (int i = 0; i < size; i++) {
-            compute(baseBall, gameNumber, userNumber, i);
+            compute(gameNumber, userNumber, i);
         }
         return baseBall;
     }
 
-    private void compute(BaseBall baseBall, int[] gameNumber, int[] userNumber, int index) {
+    private void compute(int[] gameNumber, int[] userNumber, int index) {
         int temp = -1;
         for (int i = 0; i < gameNumber.length; i++) {
             if (gameNumber[i] == userNumber[index]) {
@@ -21,10 +21,10 @@ public class Computer {
                 break;
             }
         }
-        incCount(baseBall, index, temp);
+        incCount(index, temp);
     }
 
-    private void incCount(BaseBall baseBall, int index, int temp) {
+    private void incCount(int index, int temp) {
         if (temp != index && temp != -1) {
             baseBall.incBallCount();
         }
