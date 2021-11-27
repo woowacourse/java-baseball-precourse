b2d82899508de9060c8b192934fc7f9d44e5a5ca
yeon-06 <rnjstldus2@hs.ac.kr>
2021-11-27 18:58:44 +0900

    refactor: 종료 조건 입력 메소드와 로직 분리, 하드 코딩 제거

--- src/main/java/baseball/GameSystem.java
+++ src/main/java/baseball/GameSystem.java
@@ -5,10 +5,15 @@ import java.util.regex.Pattern;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameSystem {
-	private static final String BALL_STR = "볼";
+	private static final int CNT_NUMBER = 3;
+	private static final int STRIKE_CODE= 2;
+	private static final int BALL_CODE= 1;
+	private static final String BALL_STR = "볼 ";
 	private static final String STRIKE_STR = "스트라이크";
 	private static final String NOTHING = "낫싱";
-	private static final int CNT_NUMBER = 3;
+	private static final String EXIT_CODE = "2";
+	private static final String ALL_STRIKE_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String EXIT_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 	
 	private int[] player;
 	private int[] computer;
@@ -20,12 +25,13 @@ public class GameSystem {
 	public GameSystem() {}
 
 	public void startGame() {
+		Number playerNumber = new Number();
 		Number computerNumber = new Number();
+
 		computerNumber.setRandomNumber();
 		computer = computerNumber.getDigits();
-		Number playerNumber = new Number();
 		
-		do {
+		while(!exit) {
 			playerNumber.inputAnswer();
 			player = playerNumber.getDigits();
 			
@@ -33,20 +39,20 @@ public class GameSystem {
 			calculateScore();
 			printResult();
 			
-			if(strike == 3) {
-				inputExit();
-				computerNumber.setRandomNumber();
-				computer = computerNumber.getDigits();
+			if(strike == CNT_NUMBER) {
+				readyToExit(computerNumber);
 			}
-		} while(!exit);
+		}
 	}
 	
 	private void calculateScore() {
 		for(int i=0;i<CNT_NUMBER;i++) {
 			int score = isStrikeOrBall(player[i], i);
-			if(score == 1) {
+			if(score == BALL_CODE) {
 				ball++;
-			} else if(score == 2) {
+				continue;
+			}
+			if(score == STRIKE_CODE) {
 				strike++;
 			}
 		}
@@ -57,15 +63,23 @@ public class GameSystem {
 		ball = 0;
 	}
 	
-	private void inputExit() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+	private void readyToExit(Number computerNumber) {
+		String input = inputExit();
+		if(EXIT_CODE.equals(input)) {
+			exit = true;
+		} else {
+			computerNumber.setRandomNumber();
+			computer = computerNumber.getDigits();
+		}
+	}
+	
+	private String inputExit() {
+		System.out.println(EXIT_SENTENCE);
 		String input = Console.readLine();
 		if(!isRightAnswer(input)) {
 			throw new IllegalArgumentException();
 		}
-		if("2".equals(input)) {
-			exit = true;
-		}
+		return input;
 	}
 	
 	private boolean isRightAnswer(String input) {
@@ -76,10 +90,10 @@ public class GameSystem {
 	private int isStrikeOrBall(int num, int index) {
 		for(int i=0;i<CNT_NUMBER;i++) {
 			if(computer[i] == num && i == index) {
-				return 2;
+				return STRIKE_CODE;
 			}
 			if(computer[i] == num) {
-				return 1;
+				return BALL_CODE;
 			}
 		}
 		return -1;
@@ -87,8 +101,8 @@ public class GameSystem {
 	
 	private void printResult() {
 		System.out.println(getResultString());
-		if(strike == 3) {
-			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		if(strike == CNT_NUMBER) {
+			System.out.println(ALL_STRIKE_SENTENCE);
 		}
 	}
 	
