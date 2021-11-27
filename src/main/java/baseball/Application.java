157f8ee0d6be4717b6150ac3706fde5000828c42
sha11 <sha1119@naver.com>
2021-11-27 23:44:46 +0900

    refactor(makeComputerNumbers): 리팩토링
    랜덤 생성 수 중복 값 제거
    
    중복 값 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,23 +20,21 @@ public class Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public static void startGame() throws IOException {
         makeComputerNumbers();
-        System.out.println("숫자를 입력하세요.");
+        System.out.print("숫자를 입력하세요 : ");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        ballCount = 0;
+        strikeCount = 0;
 
         while (strikeCount != 3) {
             N = Integer.parseInt(bufferedReader.readLine());
-
             inputNumbers[0] = N / 100;
             inputNumbers[1] = (N / 10) % 10;
             inputNumbers[2] = N % 10;
-
-            ballCount = 0;
-            strikeCount = 0;
 
             input();
             ballCheck();
@@ -46,9 +44,13 @@ public class Application {
     }
 
     public static void makeComputerNumbers() {
-        for (int i = 0; i < 3; i++) {
-            computerNumbers[i] = Randoms.pickNumberInRange(1, 9);
+        while (computerNumbers[0] == computerNumbers[1] && computerNumbers[1] == computerNumbers[2]) {
+            computerNumbers[0] = Randoms.pickNumberInRange(1, 9);
+            computerNumbers[1] = Randoms.pickNumberInRange(1, 9);
+            computerNumbers[2] = Randoms.pickNumberInRange(1, 9);
         }
+
+        System.out.println("answer: " + computerNumbers[0] + computerNumbers[1] + computerNumbers[2]);
     }
 
     public static void input() {
@@ -62,28 +64,30 @@ public class Application {
     }
 
     public static void restart() throws IOException {
-        if (strikeCount == 3) {
-            System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int reGameQuestion = Integer.parseInt(bufferedReader.readLine());
 
-            int reGameQuestion = Integer.parseInt(bufferedReader.readLine());
+        if (reGameQuestion == 1) {
+            System.out.println("게임을 재시작합니다.");
 
-            if (reGameQuestion == 1) {
-                System.out.println("게임을 재시작합니다.");
-
+            try {
+                startGame();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
 
-            if (reGameQuestion == 2) {
-                System.out.println("게임을 종료합니다.");
-            }
+        if (reGameQuestion == 2) {
+            System.out.println("게임을 종료합니다.");
         }
     }
 
     public static void strikeCheck() {
         for (int i = 0; i < 3; i++) {
             if (computerNumbers[i] == inputNumbers[i]) strikeCount++;
-         }
+        }
     }
 
     public static void ballCheck() {
@@ -96,7 +100,7 @@ public class Application {
     }
 
     public static void print() throws IOException {
-        if (ballCount < 3 && ballCount !=0 && strikeCount == 0) {
+        if (ballCount < 3 && ballCount != 0 && strikeCount == 0) {
             System.out.println(ballCount + "볼");
         } else if (strikeCount < 3 && strikeCount != 0 && ballCount == 0) {
             System.out.println(strikeCount + "스트라이크");
