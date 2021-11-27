de41c791907ca76b3ef8483ed2c9009e0c0bdc85
zmfl1230 <zmfl123097@gmail.com>
2021-11-27 16:13:21 +0900

    feat: develop convertNumberToArray function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,68 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    static final int STRIKE = 0;
-    static final int BALL = 1;
-    static final int NOTHING = 2;
 
-    public static void main(String[] args){
-        boolean flag = true;
-        String[] resultType = new String[] {"스크라이크", "볼", "낫싱"};
+    public static void main(String[] args) {
 
-        while (flag) {
-            // TODO: 랜덤 수 생성
-            int randomValue = Randoms.pickNumberInRange(111, 999);
-
-            // TODO: 생성 된 값 배열로 변환
-            int[] randomValueArray = convertNumberToArray(randomValue);
-
-            boolean correct = false;
-            while (!correct) {
-                // TODO: 사용자 입력 받기
-                System.out.print("숫자를 입력해주세요 : ");
-                int userInput = Integer.parseInt(Console.readLine());
-
-                // TODO: 사용자 입력 값 배열로 변환
-                int[] userInputArray = convertNumberToArray(userInput);
-
-                // TODO: 랜덤 값과 사용자 입력 값 비교
-                int[] result = compareRandomValueAndInputValue(randomValueArray, userInputArray);
-
-                // TODO: 비교 결과에 대한 값 출력
-                StringBuffer stringBuffer = new StringBuffer();
-                // strike = 0, ball = 0
-                if((result[STRIKE]|result[BALL]) == 0) System.out.println(resultType[NOTHING]);
-                else {
-                    for (int index = 0; index < result.length; index++) {
-                        if (result[index]==0) continue;
-                        if (index > 0) stringBuffer.append(" ");
-                        stringBuffer.append(result[index]);
-                        stringBuffer.append(resultType[index]);
-                    }
-
-                    // strike = 3, ball = 0
-                    if((result[STRIKE]^randomValueArray.length) == 0) {
-                        stringBuffer.append('\n');
-                        stringBuffer.append("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-                        // TODO: 비교 결과에 따른 correct 변수 수정
-                        correct = true;
-                    }
-                }
-                System.out.println(stringBuffer);
-
-            }
-
-        }
-
-        // TODO: 사용자로부터 게임 재게 여부 확인 (1 or 2)
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        int restart = Integer.parseInt(Console.readLine());
-
-        // TODO: 재게 여부에 따른 flag 변수 수정
-        if(restart == 2) flag = false;
     }
-
     public static int[] convertNumberToArray(int number) {
         /*
             [definition]
@@ -90,39 +32,5 @@ public class Application {
 
     }
 
-    public static int[] compareRandomValueAndInputValue(int[] randomValue, int[] inputValue) {
-        /*
-            [definition]
-            게임 시작과 동시에 생성한 랜덤 값과 사용자에게 입력받은 입력 값을 비교하는 합수입니다.
-
-            [parameters]
-            randomValue: 콤퓨터가 게임의 시작과 동시에 생성한 랜덤 값이 담긴 배열
-            inputValue: 사용자에게 입력받은 입력 값이 담긴 배열
-
-            - 111 ~ 999 사이의 값이 매개변수로 전달됨을 보장합니다.
-            - 값이 자릿수 별로 배열에 담겨 배열의 형태로 전달됩니다.
-
-            [return]
-            스트라이크, 볼 순으로 각각의 개수를 담은 배열
-            만약 하나도 같은 것이 없다면 초기값({0, 0})으로 반환
-        **/
-        int[] answer = new int[2];
-
-        for(int randomValueIndex = 0; randomValueIndex < randomValue.length; randomValueIndex++) {
-            for (int inputValueIndex = 0; inputValueIndex < inputValue.length; inputValueIndex++) {
-
-                // TODO: 기능 분리 필요, depth 3 이상
-                // 같은 위치의 값을 배타연산해서 값이 0인 경우, 해당 값들은 같다고 볼 수 있음
-                if ((randomValue[randomValueIndex] ^ inputValue[inputValueIndex]) == 0) {
-                    // 값이 같은데 인덱스가 같다면, 스트라이크 카운팅
-                    if ((randomValueIndex ^ inputValueIndex) == 0) answer[0] += 1;
-                        // 같은 같은데 인덱스가 다르다면, 볼 카운팅
-                    else answer[1] += 1;
-                }
-            }
-        }
-        return answer;
-
-    }
 
 }
