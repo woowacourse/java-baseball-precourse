1f526fc035abf2761d87b4056afeea1708a4899d
skullKim <kyk990328@gmail.com>
2021-11-27 19:06:36 +0900

    fix(Player.java): 플레이어가 입력한 숫자 검증 로직 수정
    플레이어가 입력한 숫자에 대해 0포함 여부, 중복된 수 존재 여부 검증 단계
    추가
    
    BREAKING CHNAGE: 플레이어가 입려한 수에 중복된 숫자가 존재하거나 0이
    포함된다면 validateSelectedNumber에서 false 반환

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 /**
@@ -7,7 +8,7 @@ import java.util.regex.Pattern;
  * 플레이어를 만들떄 이 인터페이스를 사용해 만든다
  *
  * @author yunki kim
- * @version 1.0
+ * @version 2.0
  * @since 2021-11-25
  */
 
@@ -22,17 +23,30 @@ public interface Player {
 
 	/**
 	 * 플레이어가 입력한 수를 검증한다
-	 * 입력한 수가 3자리 자연수 형태의 스트링이면 통과
+	 * 입력한 수의 각 자리수의 숫자가 서로 다르고 3자리 자연수 형태의 스트링이면 통과
 	 *
 	 * @param selectedNumber 유저가 입력한 수
 	 * @return Boolean 플레이어가 입력한 수가 조건에 만족하면 true, 아니면 false
 	 */
 	static Boolean validateSelectedNumber(final String selectedNumber) {
-		final String numberPattern = "^[0-9]*$";
+		final String numberPattern = "^[1-9]*$";
+		Boolean[] usedNumber = new Boolean[11];
+		Arrays.fill(usedNumber, false);
 		if(selectedNumber.length() != 3) {
 			return false;
 		}
-		return Pattern.matches(numberPattern, selectedNumber);
+		if(!Pattern.matches(numberPattern, selectedNumber)) {
+			return false;
+		}
+		for(int idx = 0; idx < selectedNumber.length(); idx++) {
+			int number = Character.getNumericValue(
+				selectedNumber.charAt(idx));
+			if(usedNumber[number]) {
+				return false;
+			}
+			usedNumber[number] = true;
+		}
+		return true;
 	}
 
 	/**
