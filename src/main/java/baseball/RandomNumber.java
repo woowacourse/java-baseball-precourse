ea1abc918a41b780423b518d53e51345b0b43aca
skullKim <kyk990328@gmail.com>
2021-11-27 22:42:45 +0900

    fix(RandomNumber.java): 난수생성하는 로직 수정
    난수 로직이 잘못되서 게임종료_후_재시작() 테스트코드가 통과되지 않는
    문제 수정
    makeRandomNumber() private 메서드를 만들고 거기에 난수 생성 로직 구현
    
    BREAKING CHNAGE: 난수 생성 로직 변경

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,30 +12,44 @@ import camp.nextstep.edu.missionutils.Randoms;
  * <h1>야구게임에서 컴퓨터가 사용할 3자리 난수를 생성한다</h1>
  *
  * @author yunki kim
- * @version 2.0
+ * @version 3.0
  * @since 2021-11-25(V1.0)
  */
 
 public class RandomNumber {
-	/** 생성자에서 n자리 난수 생성에 사용되는 난수 범위 최대값 */
+	/** 생성자에서 3자리 난수 생성에 사용되는 난수 범위 최대값 */
 	public static final Integer RANDOM_NUMBER_MAX_RANGE = 9;
 
-	/** 생성자에서 n자리 난수 생성에 사용되는 난수 범위 최소값 */
+	/** 생성자에서 3자리 난수 생성에 사용되는 난수 범위 최소값 */
 	public static final Integer RANDOM_NUMBER_MIN_RANGE = 1;
-
-	/** 생성자에서 n자리 난수 생성에 사용되는 값 n */
-	public static final Integer RANDOM_NUMBER_SIZE = 3;
 
 	/** 생성자에서 만든 3자리 난수를 문자열로 저장 */
 	private String randomNumber;
 
+	/**
+	 * 3자리 난수를 만들어 반환한다
+	 *
+	 * @return String 만들어진 3자리 난수
+	 */
+	private String makeRandomNumber() {
+		Boolean[] usedNumber = new Boolean[11];
+		Arrays.fill(usedNumber, false);
+		String randomNumber = "";
+		while(randomNumber.length() < 3) {
+			int tempNumber =
+				pickNumberInRange(RANDOM_NUMBER_MIN_RANGE, RANDOM_NUMBER_MAX_RANGE);
+			if(usedNumber[tempNumber]) {
+				continue;
+			}
+			usedNumber[tempNumber] = true;
+			randomNumber = randomNumber.concat(Integer.toString(tempNumber));
+		}
+		return randomNumber;
+	}
+
 	/** RandomNumber 인스턴스를 만드는 생성자 */
 	public RandomNumber() {
-		final List<Integer> randomNumberList = Randoms.pickUniqueNumbersInRange(RANDOM_NUMBER_MIN_RANGE,
-			RANDOM_NUMBER_MAX_RANGE, RANDOM_NUMBER_SIZE);
-		this.randomNumber = randomNumberList.stream()
-			.map(number -> Integer.toString(number))
-			.collect(Collectors.joining());
+		this.randomNumber = makeRandomNumber();
 	}
 
 	/**
