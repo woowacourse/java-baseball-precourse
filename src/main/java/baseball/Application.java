297416fb8fb9d31622198af472c2417f42a3b081
SheepEatLion <gowjr207@gmail.com>
2021-11-27 16:11:46 +0900

    feat(Application): main
    added new method to Application
    - validation
    can validate between input number and random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.*;
 
 public class Application {
     public static void main(String[] args) {
+
         // 기능 1: 랜덤 값 생성
         String targetNumber = randomNumber();
 
@@ -11,16 +12,36 @@ public class Application {
             //  기능 2: 사용자로부터 값 입력받기
             System.out.print("숫자를 입력해주세요: ");
             String inputNumber = Console.readLine();
+            //  기능 3: 사용자 입력값과 상대(컴퓨터)값 비교
+            System.out.println(validation(inputNumber, targetNumber));
+
+            break;
         }
     }
-
-
     public static String randomNumber(){
-
         String firstNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
         String secondNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
         String thirdNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
-
         return firstNumber + secondNumber + thirdNumber;
+    }
+    public static String validation(String number, String target){
+        String resultMessage = "";
+        int strikeCount = 0;
+        int ballCount = 0;
+        for(int i = 0; i < number.length(); i++){
+            if(number.charAt(i) == target.charAt(i)){
+                strikeCount++;
+            } else if (target.contains(String.valueOf(number.charAt(i)))){
+                ballCount++;
+            }
+        }
+        if(strikeCount == 0 && ballCount == 0){
+            resultMessage = "낫싱";
+        } else if (ballCount != 0) {
+            resultMessage += ballCount + "볼";
+        } else if (strikeCount != 0){
+            resultMessage += strikeCount + "스트라이크";
+        }
+        return resultMessage;
     }
 }
