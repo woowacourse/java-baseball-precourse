3ec92a776c188fa8117d9eeea311e24c00efeb8d
YuJeong <yjfflq4872@gmail.com>
2021-11-28 01:26:42 +0900

    fix: 필드 초기화 메소드 추가

--- src/main/java/logic/BaseballGameJudge.java
+++ src/main/java/logic/BaseballGameJudge.java
@@ -4,15 +4,24 @@ import java.util.List;
 
 public class BaseballGameJudge {
     private static final int NUMBER_COUNT = 3;
-    private static Boolean isEnd = false;
+    private static final int INIT_COUNT = 0;
+
+    private static Boolean isEnd;
     private static int ballCount;
     private static int strikeCount;
 
     public GameResultObject judgeGameResult(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        init();
         getCount(computerNumbers, userNumbers);
         judgeGameOver();
 
         return new GameResultObject(isEnd, ballCount, strikeCount);
+    }
+
+    public void init() {
+        isEnd = false;
+        ballCount = INIT_COUNT;
+        strikeCount = INIT_COUNT;
     }
 
     private void getCount(List<Integer> computerNumbers, List<Integer> userNumbers) {
