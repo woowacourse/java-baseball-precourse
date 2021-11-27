b46e168b42722a07034015f1de2480ba2d7651ca
xi_jjun <rlawowns97@naver.com>
2021-11-27 22:49:12 +0900

    feat: add GameHandler
    싱글톤 패턴으로 구현
    start() : 게임 시작 method. 게임이 종료되면 다시할 것인지에 대한 값을 return
    playing()
    	게임 진행 method. 정답을 맞추게 되면 return GAME_EXIT
    	정답을 틀리게 되면 return KEEP_GOING
    	적절하지 못한 입력이 주어지면 throw IllegalArgumentException
    getPlayerInput() : 사용자로부터 playerInput을 Console로 받아서 return
    isRestart() : player의 입력을 받아서 다시 게임을 할 것인지에 대한 값을 return
    checkValidInput(String playerInput)
    	playerInput 이 알맞은 값인지 판단. 적절하지 않은 값이라면 throw IllegalArgumentException
    checkDuplicated(String playerInput)
    	playerInput 중에 같은 숫자가 있으면 return true. 모두 다 다른 숫자면 return false
    checkIsNumber(String playerInput)
    	playerInput 중에 숫자가 아닌 것이 있으면 return false
    showHint(int[] hint) : Computer로 부터 받은 힌트를 화면에 출력하여 보여줌
    isAnswer(int[] hint) : hint가 3 Strike 라면, return ture

---
+++ src/main/java/baseball/controller/GameHandler.java
@@ -0,0 +1,119 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameHandler {
+    private static GameHandler instance;
+    private final int KEEP_GOING = 0;
+    private final int RESTART = 1;
+    private final int PROGRAM_EXIT = 2;
+    private final int GAME_EXIT = 3;
+    private int mode;
+    private Player player;
+    private Computer computer;
+
+    private GameHandler() {
+    }
+
+    public static GameHandler getInstance() {
+        if (instance == null) {
+            return new GameHandler();
+        }
+
+        return instance;
+    }
+
+    public int start() {
+        mode = KEEP_GOING;
+        player = Player.getInstance();
+        computer = Computer.getInstance();
+
+        while (mode == KEEP_GOING) {
+            mode = playing();
+        }
+
+        return isRestart();
+    }
+
+    private int isRestart() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if (!checkIsNumbers(input) || input.length() != 1) {
+            throw new IllegalArgumentException();
+        }
+        if (input.charAt(0) == 49) {
+            return RESTART;
+        }
+        return PROGRAM_EXIT;
+    }
+
+    private boolean isAnswer(int[] hint) {
+        return hint[1] == 3;
+    }
+
+    private int playing() {
+        try {
+            String playerInput = getPlayerInput();
+            checkValidInput(playerInput);
+            player.playerInput(playerInput);
+            int[] hint = computer.generateHint(player.submitTryNumber());
+            showHint(hint);
+            if (isAnswer(hint)) {
+                return GAME_EXIT;
+            }
+        } catch (Exception e) {
+            System.exit(1);
+        }
+
+        return KEEP_GOING;
+    }
+
+    private String getPlayerInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    /**
+     * @param hint hint[0] : Ball 개수
+     *             hint[1] : Strike 개수
+     */
+    private void showHint(int[] hint) {
+        if (hint[0] + hint[1] == 0) {
+            System.out.println("낫싱");
+        }
+
+        if (hint[0] == 0 && hint[1] != 0) {
+            System.out.println(hint[1] + "스트라이크");
+        }
+
+        if (hint[0] != 0 && hint[1] == 0) {
+            System.out.println(hint[0] + "볼");
+        }
+
+        if (hint[0] != 0 && hint[1] != 0) {
+            System.out.println(hint[0] + "볼 " + hint[1] + "스트라이크");
+        }
+    }
+
+    private void checkValidInput(String playerInput) {
+        if (checkDuplicated(playerInput) || playerInput.length() != 3 || !checkIsNumbers(playerInput)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkIsNumbers(String playerInput) {
+        for (String input : playerInput.split("")) {
+            if (!(48 < input.charAt(0) && input.charAt(0) < 58)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkDuplicated(String playerInput) {
+        return playerInput.charAt(0) == playerInput.charAt(1) || playerInput.charAt(0) == playerInput.charAt(2) || playerInput.charAt(1) == playerInput.charAt(2);
+    }
+}
