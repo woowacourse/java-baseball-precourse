ff0a1bfee62ae6ad4498aa5a64ce33a404a64b9b
= <ulim130@naver.com>
2021-11-27 20:53:55 +0900

    feat : 상수

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,27 @@
+package baseball;
+
+public class Constant {
+
+    public static final int MIN_VALUE = 1;
+
+    public static final int MAX_VALUE = 9;
+
+    public static final int NUMBER_OF_NUMBERS = 3;
+
+    public static final int RESTART_VALUE = 1;
+
+    public static final int EXIT_VALUE = 2;
+
+    public static final String PLEASE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+
+    public static final String SUCCESS = NUMBER_OF_NUMBERS + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String STRIKE = "스트라이크 ";
+
+    public static final String BALL = "볼 ";
+
+    public static final String NOTHING = "낫싱";
+
+    public static final String RESTART_OR_EXIT = "게임을 새로 시작하려면 " + RESTART_VALUE + ", 종료하려면 " + EXIT_VALUE + "를 입력하세요.";
+
+}
