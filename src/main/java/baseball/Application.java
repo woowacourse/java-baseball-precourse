e521d647a93e880c5356152eb25f91772c88446b
SheepEatLion <gowjr207@gmail.com>
2021-11-27 15:38:55 +0900

    feat(Application): randomNumber
    Added new method to Application
    - randomNumber()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+
+        String targetNumber = randomNumber();
+
+    }
+
+    // 1~9 사이의 랜덤 수 출력해서 3자리의 수로 리턴
+    public static String randomNumber(){
+
+        String firstNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+        String secondNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+        String thirdNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+
+        return firstNumber + secondNumber + thirdNumber;
     }
 }
