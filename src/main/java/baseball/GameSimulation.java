de623876aa51bb33eb18f7b61a5e94b1749fa3b6
Sanggu <rkdtkdrn93@naver.com>
2021-11-27 21:18:51 +0900

    feat: 입력값 유효성 검사 구현

--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -19,20 +20,35 @@ public class GameSimulation {
 	}
 
 	public void playGame() {
+		System.out.println("숫자를 입력해주세요 : ");
 		String userInput = Console.readLine();
 		this.userNum = makeList(userInput);
 		System.out.println(userNum);
+		validateUserNum();
 
 	}
 
 	public List<Integer> makeList(String userInput) {
 		String[] arrayInput = userInput.split("");
 		List<Integer> userNumbers = new ArrayList<>();
-		for (int i=0; i<userInput.length();i++) {
+		for (int i = 0; i < userInput.length(); i++) {
 			userNumbers.add(Integer.parseInt(arrayInput[i]));
 		}
 		return userNumbers;
 	}
 
+	public void validateUserNum() {
+		if (userNum.size() != 3) {
+			throw new IllegalArgumentException("올바른 숫자가 아닙니다. 세자리 숫자를 입력해주세요.");
+		}
+		if (userNum.contains(0)) {
+			throw new IllegalArgumentException("올바른 숫자가 아닙니다. 0을 제외한 숫자를 입력해주세요.");
+		}
+		for (int i = 0; i < userNum.size(); i++) {
+			if (Collections.frequency(userNum, userNum.get(i)) != 1) {
+				throw new IllegalArgumentException("올바른 숫자가 아닙니다. 서로 다른 숫자로 이루어져야 합니다.");
+			}
+		}
+	}
 
 }
