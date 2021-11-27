e1d45a447760e00759cf564eabc7ccb95932a8d4
xfrnk2 <xfrnk2@gmail.com>
2021-11-27 15:42:11 +0900

    feat: 테스트를 통과하는 난수 생성 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import java.util.Set;
+import java.util.HashSet;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Game {
     private final int MIN_RANGE = 1;
     private final int MAX_RANGE = 9;
@@ -10,7 +15,25 @@ public class Game {
     }
 
     public String generateRandomNumber() {
-        return "test";
+        String result = "";
+        Set<Character> randomNumberSet = new HashSet<>();
+
+        int numberLength = 0;
+        while (numberLength < DIGIT_NUM) {
+            int randomDigit = Randoms.pickNumberInRange(MIN_RANGE,
+                    MAX_RANGE);
+            char number = Character.forDigit(randomDigit, RADIX);
+
+            if (randomNumberSet.contains(number)) {
+                continue;
+            }
+            numberLength++;
+            result += number;
+            randomNumberSet.add(number);
+        }
+
+        return result;
+
     }
 
 }
