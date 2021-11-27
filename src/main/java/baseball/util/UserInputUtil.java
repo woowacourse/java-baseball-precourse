ba91798944a411503f0f68f68ed0a44cc692eff4
Donggyu Kim <a29661498@gmail.com>
2021-11-27 19:04:21 +0900

    feat: 사용자 입력 받기
    3자리 정수를 입력받는 getThreeDigitInput() 메소드 구현

--- src/main/java/baseball/util/UserInputUtil.java
+++ src/main/java/baseball/util/UserInputUtil.java
@@ -7,6 +7,24 @@ public class UserInputUtil {
     private UserInputUtil() {
     }
 
+    public static int[] getThreeDigitInput() {
+        int[] userNumber = new int[3];
+
+        String input = Console.readLine();
+
+        if (input.length() == 3) {
+            throw new IllegalArgumentException();
+        }
+
+        String[] stringArray = input.split("");
+
+        for(int i = 0; i < 3; i++) {
+            userNumber[i] = toInteger(stringArray[i]);
+        }
+
+        return userNumber;
+    }
+
     private static int toInteger(String input) {
         try{
             return Integer.parseInt(input);
