03cb2eab0e8407abc2481e1ef58cfbac748dd7ef
최창준 <aowert6518@gmail.com>
2021-11-27 22:02:18 +0900

    [fix] 낫싱 추가 및 Viewer 문구 변경
    - strike와 ball 둘다 없을 때 낫싱 출력
    
    - 게임 종료 등 요구사항에 나온대로 수정

--- src/main/java/baseball/domain/Viewer.java
+++ src/main/java/baseball/domain/Viewer.java
@@ -2,7 +2,7 @@ package baseball.domain;
 
 public class Viewer {
     public static void inputNumber() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public static void printStrikeBall(int strike, int ball) {
@@ -17,13 +17,17 @@ public class Viewer {
             sb.append("스트라이크");
         }
         if (strike == 3) {
-            sb.append("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+            sb.append("\n");
+            sb.append("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        if (strike == 0 && ball == 0) {
+            sb.append("낫싱");
         }
         System.out.println(sb.toString());
     }
 
     public static void printEndMessage() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
 
