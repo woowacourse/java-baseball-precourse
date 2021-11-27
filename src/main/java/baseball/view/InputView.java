a262c0749e518ee50499afa397bd700eb7458f8e
swcho <progress0407@gmail.com>
2021-11-27 12:11:18 +0900

    style: 메세지 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,18 +1,19 @@
 package baseball.view;
 
 import baseball.model.Balls;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 
 public class InputView {
 
     private static final int BALL_SIZE = 3;
+    private static final String ERROR_WRONG_INPUT = "잘못된 값을 입력하였습니다.";
 
     public static Balls inputNumber(InputStrategy inputStrategy) {
         String input = inputStrategy.getInput();
         validateInput(input);
-        Balls balls = convertBalls(input);
-        return balls;
+        return convertBalls(input);
     }
 
     private static Balls convertBalls(String input) {
@@ -29,10 +30,27 @@ public class InputView {
         try {
             number = Integer.parseInt(input);
         } catch (Exception exception) {
-            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+            throw new IllegalArgumentException(ERROR_WRONG_INPUT);
         }
         if (number < 100 || number > 999) {
-            throw new IllegalArgumentException("잘못된 값을 입력하였습니다.");
+            throw new IllegalArgumentException(ERROR_WRONG_INPUT);
         }
+    }
+
+    public static int inputChoiceAfterEnd() {
+        String inputString = Console.readLine();
+        int inputInteger;
+
+        try {
+            inputInteger = Integer.parseInt(inputString);
+        } catch (Exception exception) {
+            throw new IllegalArgumentException(ERROR_WRONG_INPUT);
+        }
+
+        if (inputInteger < 1 || inputInteger > 2) {
+            throw new IllegalArgumentException(ERROR_WRONG_INPUT);
+        }
+
+        return inputInteger;
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,10 +3,21 @@ package baseball.view;
 import baseball.model.Balls;
 import baseball.model.PlayTypes;
 
+import static java.lang.System.out;
+
 public class OutputView {
+
+    private static final String CONGRATULATION_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String CHOICE_CONTINUE_OR_EXIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public static void printResult(Balls computerBalls, Balls balls) {
         PlayTypes playTypes = computerBalls.play(balls);
         String playResult = playTypes.getPlayResult();
         System.out.println(playResult);
+    }
+
+    public static void displayAfterEnd() {
+        out.println(CONGRATULATION_MESSAGE);
+        out.println(CHOICE_CONTINUE_OR_EXIT_MESSAGE);
     }
 }
