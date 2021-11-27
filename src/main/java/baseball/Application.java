97c1dccafd210c099b9844da21b01dcb5c7392b5
최창준 <aowert6518@gmail.com>
2021-11-27 22:03:12 +0900

    [style] 주석 제거 및 들여쓰기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
         Controller game = new Controller();
         while (!game.isEnd()) {
             game.initGame();
