80d986d20a509d8502f6570542e784522e01c973
xfrnk2 <xfrnk2@gmail.com>
2021-11-28 01:39:06 +0900

    refactor(Game): 직관적 구조로 리팩토링

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,27 +1,33 @@
 package baseball;
 
+import constants.ExitOption;
 import constants.NumberAttribute;
 import camp.nextstep.edu.missionutils.Console;
 
 import constants.HintMessage;
 import utils.NumberGenerator;
 
+import static baseball.InputValidator.checkExitCommandIsValid;
+
 public class Game {
     private static String CLEAR_CONDITION_MESSAGE;
     Umpire umpire;
 
-    Game() {
-         CLEAR_CONDITION_MESSAGE = NumberAttribute.DIGIT_NUMBER.getValue() + HintMessage.STRIKE.message();
-         String randomNumber = NumberGenerator.generateRandomNumber();
-         umpire = new Umpire(randomNumber);
+    Game () {
+        CLEAR_CONDITION_MESSAGE = NumberAttribute.DIGIT_NUMBER.getValue() + HintMessage.STRIKE.message();
+    }
 
+    public void init() {
+        umpire = new Umpire(NumberGenerator.generateRandomNumber());
     }
 
     public void playGame() {
         boolean isGameContinue = true;
         do {
             String userNumber = Console.readLine();
-            inputValidation(userNumber);
+            if (!InputValidator.checkUserInputIsValid(userNumber)) {
+                throw new IllegalArgumentException("사용자 입력이 잘못된 값이므로 게임을 종료합니다");
+            }
 
             String result = umpire.judgeRound(userNumber);
             isGameContinue = !result.equals(CLEAR_CONDITION_MESSAGE);
@@ -29,11 +35,12 @@ public class Game {
         } while (isGameContinue);
     }
 
-    public void inputValidation (final String userInput){
-        boolean isInvalid = !InputValidator.checkUserInput(userInput);
-        if(isInvalid){
-            throw new IllegalArgumentException("사용자 입력이 잘못된 값이므로 게임을 종료합니다");
+    public boolean checkUserWantRestart() {
+        String userInput = Console.readLine();
+        if (!checkExitCommandIsValid(userInput)) {
+            throw new IllegalArgumentException("게임 종료 옵션 커맨드가 잘못된 값이므로 게임을 종료합니다");
         }
+        return userInput.equals(ExitOption.RESTART.command());
     }
-
 }
+
