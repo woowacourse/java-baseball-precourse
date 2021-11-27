b05fc914a7596825bebb0bd598c036e38e2ce375
iamtaehoon <kimth9981@naver.com>
2021-11-27 16:29:40 +0900

    rename: Message 클래스 GameConstant로 이름 변경

---
+++ src/main/java/util/GameConstant.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Message {
+    //게임 진행 메세지
+    public static final String INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String ASK_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    //예외 케이스 메세지
+    public static final String NOT_THREE_DIGIT_INTEGER_MESSAGE = "세 자리 정수를 입력하세요.";
+    public static final String NOT_RELATED_ABOUT_RESTART_MESSAGE = "1 또는 2 이외의 값을 입력하셨습니다.";
+}
