4c359d63fe7868d76427a030fc9eb14b8881bfa4
SheepEatLion <gowjr207@gmail.com>
2021-11-27 16:23:24 +0900

    feat(Application): main
    added new method to Application
    - inputException
    if input data by user isn't number will be occurred illegalArgumentException.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,11 +7,15 @@ public class Application {
 
         // 기능 1: 랜덤 값 생성
         String targetNumber = randomNumber();
-
+        System.out.println(targetNumber);
         while(true){
             //  기능 2: 사용자로부터 값 입력받기
             System.out.print("숫자를 입력해주세요: ");
             String inputNumber = Console.readLine();
+
+            //  기능 4: 사용자 입력에 대한 예외처리
+            inputException(inputNumber);
+
             //  기능 3: 사용자 입력값과 상대(컴퓨터)값 비교
             System.out.println(validation(inputNumber, targetNumber));
 
@@ -24,6 +28,7 @@ public class Application {
         String thirdNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
         return firstNumber + secondNumber + thirdNumber;
     }
+
     public static String validation(String number, String target){
         String resultMessage = "";
         int strikeCount = 0;
@@ -43,5 +48,12 @@ public class Application {
             resultMessage += strikeCount + "스트라이크";
         }
         return resultMessage;
+    }
+
+    public static void inputException(String number){
+        String regExp = "^[0-9]+$";
+        if(!number.matches(regExp)){
+            throw new IllegalArgumentException();
+        }
     }
 }
