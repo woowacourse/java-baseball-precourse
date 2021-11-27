1004c0da1108d2e0bf078d3fa789b5ff20dcfbd2
Wishoon <heonga2@gmail.com>
2021-11-27 21:37:52 +0900

    feat(BaseballNums, BaseballNumsGenerator): 입력한 3자리 숫자 생성기능 추가 및 검증로직 추가

--- src/main/java/baseball/domain/BaseballNums.java
+++ src/main/java/baseball/domain/BaseballNums.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaseballNums {
     public static final int MIN_BASEBALL_NUMBER = 1;
@@ -9,7 +11,22 @@ public class BaseballNums {
     private final List<Integer> baseballNums;
 
     public BaseballNums(List<Integer> baseballNums) {
+        isValidateLength(baseballNums);
+        isValidateDuplicated(baseballNums);
         this.baseballNums = baseballNums;
+    }
+
+    private void isValidateLength(List<Integer> baseballNums) {
+        if (baseballNums.size() != BASEBALL_NUMBERS_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isValidateDuplicated(List<Integer> baseballNums) {
+        Set<Integer> tempBaseballNums = new HashSet<>(baseballNums);
+        if (tempBaseballNums.size() != BASEBALL_NUMBERS_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public List<Integer> getBaseballNums() {
--- src/main/java/baseball/domain/BaseballNumsGenerator.java
+++ src/main/java/baseball/domain/BaseballNumsGenerator.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class BaseballNumsGenerator {
+    private final static String NOT_NUMBER_RANGE_ONE_TO_NINE = "1에서 9사이의 숫자가 아닙니다.";
+
     public static BaseballNums generateRandomBaseballNums() {
         Set<Integer> randomBaseballNums = new HashSet<>();
         while (randomBaseballNums.size() != BaseballNums.BASEBALL_NUMBERS_LENGTH) {
@@ -13,5 +15,20 @@ public class BaseballNumsGenerator {
                     BaseballNums.MAX_BASEBALL_NUMBER));
         }
         return new BaseballNums(new ArrayList<>(randomBaseballNums));
+    }
+
+    public static BaseballNums generateInputBaseballNums(String inputNum) {
+        List<Integer> inputBaseBallNums = new ArrayList<>();
+        for (int i = 0; i < inputNum.length(); i++) {
+            isValidateRangeNumOneToNine(inputNum.charAt(i) - '0');
+            inputBaseBallNums.add(inputNum.charAt(i) - '0');
+        }
+        return new BaseballNums(inputBaseBallNums);
+    }
+
+    private static void isValidateRangeNumOneToNine(int number) {
+        if (BaseballNums.MIN_BASEBALL_NUMBER > number || number > BaseballNums.MAX_BASEBALL_NUMBER) {
+            throw new IllegalArgumentException(NOT_NUMBER_RANGE_ONE_TO_NINE);
+        }
     }
 }
