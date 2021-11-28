c13cd03aeac65d10eaa23acfb8398d4fd1124710
윤이진 <483759@naver.com>
2021-11-28 22:03:58 +0900

    feat: 사용자 입력을 GameBall 인스턴스로 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.domain.GameBalls;
+import baseball.domain.GameGuidePrinter;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+
+    public static void main(String[] args) throws IllegalArgumentException {
+        GameGuidePrinter.inputUserNumber();
+        String userNumber = Console.readLine();
+
+        GameBalls balls = new GameBalls(userNumber);
     }
 }
