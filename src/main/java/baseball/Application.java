6ab83c2dcb1955efce7b2e8c6e76703e1a869b8b
이민재 <lmj938@naver.com>
2021-11-28 18:05:32 +0900

    [style] Application 클래스 공백 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,6 @@ import baseball.view.InputView;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-
         do {
             BaseballGame baseballGame = new BaseballGame();
             baseballGame.start();
