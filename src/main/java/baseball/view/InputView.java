286c0982613a2f5d9c51ee763b13c299b6aba4d4
RIANAEH <ririhan217@gmail.com>
2021-11-28 14:52:42 +0900

    feat(inputView): add exit or restart input view
    사용자로부터 게임을 새로 시작하거나 재시작 숫자 입력을 받는 view를 추가하였다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
 	private String inputBallNumbers;
+	private String inputExitOrRestart;
 
 	public void printStartMessage() {
 		System.out.print("숫자를 입력해주세요 : ");
@@ -16,5 +17,17 @@ public class InputView {
 
 	public String getInputBallNumbers() {
 		return inputBallNumbers;
+	}
+
+	public void printExitOrRestartMessage() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+	}
+
+	public void setInputExitOrRestart() {
+		inputExitOrRestart = Console.readLine();
+	}
+
+	public String getInputExitOrRestart() {
+		return inputExitOrRestart;
 	}
 }
