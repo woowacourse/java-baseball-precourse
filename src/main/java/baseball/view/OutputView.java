3da71df48d092430e24b245c331f14fd87330a5a
Wishoon <heonga2@gmail.com>
2021-11-28 19:46:31 +0900

    refactor(OutputView): 하드코딩 값을 상수로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,19 +3,20 @@ package baseball.view;
 import baseball.domain.GameBoard;
 
 public class OutputView {
+	private static final int ZERO = 0;
 	private static final String BALL_COUNT_MESSAGE = "%d볼 ";
 	private static final String STRIKE_COUNT_MESSAGE = "%d스트라이크";
 	private static final String NOTHING_COUNT_MESSAGE = "낫싱";
 	private static final String GAME_RESULT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
 	public static void showHintMessage(GameBoard gameBoard) {
-		if (gameBoard.getBallCount() > 0) {
+		if (gameBoard.getBallCount() > ZERO) {
 			System.out.printf(BALL_COUNT_MESSAGE, gameBoard.getBallCount());
 		}
-		if (gameBoard.getStrikeCount() > 0) {
+		if (gameBoard.getStrikeCount() > ZERO) {
 			System.out.printf(STRIKE_COUNT_MESSAGE, gameBoard.getStrikeCount());
 		}
-		if (gameBoard.getBallCount() == 0 && gameBoard.getStrikeCount() == 0) {
+		if (gameBoard.getBallCount() == ZERO && gameBoard.getStrikeCount() == ZERO) {
 			System.out.print(NOTHING_COUNT_MESSAGE);
 		}
 		System.out.println();
