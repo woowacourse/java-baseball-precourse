e1b466a36a9d950a52053db4736ca7ad404b43bb
윤이진 <483759@naver.com>
2021-11-28 21:55:10 +0900

    feat: 문자열 형태의 입력을 Ball 배열로 변환하여 저장

--- src/main/java/baseball/domain/GameBalls.java
+++ src/main/java/baseball/domain/GameBalls.java
@@ -7,6 +7,38 @@ public class GameBalls {
     public static final int NUMBEROFBALLS = 3;
     private Ball[] sequence;
 
+    public GameBalls(String userInput) throws IllegalArgumentException {
+        int toInteger = convertStringToInteger(userInput);
+        int[] numberArray = convertIntegerToNumberArray(toInteger);
+
+        this.sequence = new Ball[NUMBEROFBALLS];
+        for (int i = 0; i < NUMBEROFBALLS; i++) {
+            this.sequence[i] = new Ball(numberArray[i]);
+        }
+    }
+
+    public int convertStringToInteger(String userInput) throws IllegalArgumentException {
+        if (userInput.length() != NUMBEROFBALLS) {
+            throw new IllegalArgumentException(NUMBEROFBALLS + " 자리 숫자를 입력하세요");
+        }
+
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBEROFBALLS + "자리 숫자를 입력하세요");
+        }
+    }
+
+    public int[] convertIntegerToNumberArray(int userNumber) {
+        int[] numbers = new int[NUMBEROFBALLS];
+
+        for (int i = NUMBEROFBALLS - 1; i >= 0; i--) {
+            numbers[i] = userNumber % 10;
+            userNumber /= 10;
+        }
+        return numbers;
+    }
+
     @Override
     public String toString() {
         return "GameBalls{" +
