fcdc51d750ac35d92185aa78cf3a2ba1499b5241
KBY-TECH <soccer_by@naver.com>
2021-11-28 23:51:54 +0900

    feat: 게임 종료 후 사용자 응답에 대한 재시작 기능

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import baseball.BaseballSystem.BaseballRule;
 import baseball.BaseballSystem.BaseballValidationImpl;
 import baseball.BaseballSystem.Validation;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGame {
+
+    private static final boolean OK = true;
+    private static final boolean NO = false;
 
     private final Validation validation;
 
@@ -25,6 +29,24 @@ public class BaseballGame {
             System.out.print("Business Logic 처리 완료됨 가정");
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean continueGame() {
+        try {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String validContinuePlayerInput = validation.isValidContinueInput(Console.readLine());
+            return isContinue(validContinuePlayerInput);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isContinue(String continuePlayerInput) {
+        if (BaseballRule.CONTINUE_GAME.getValue().equals(continuePlayerInput)) {
+            return OK;
+        } else {
+            return NO;
         }
     }
 }
