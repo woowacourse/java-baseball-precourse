c6a69b29213ace8ff6650baa1ff56b368fe550df
shindong96 <2006peter@naver.com>
2021-11-29 00:57:44 +0900

    refact(Application) : refactor main method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,11 @@ package baseball;
 
 public class Application {
 	public static void main(String[] args) {
-		
+		boolean restartGame = true;
+		while (restartGame) {
+			Game game = new Game();
+			restartGame = game.playGame();
+		}
 		//TODO: 숫자 야구 게임 구현
 	}
 }
