561c77d86fbdd530c680a1af6e043a0846a499b4
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-11-28 18:08:50 +0900

    style: 제한자 순서 수정
    Change Access Modifier from final static to static final

---
+++ src/main/java/baseball/game/constants/BaseballGameConstants.java
@@ -0,0 +1,15 @@
+package baseball.game.constants;
+
+public class BaseballGameConstants {
+    public static final int RANDOM_NUMBER_LENGTHS = 3;   // 맞출 숫자와 입력 숫자 배열의 크기
+    public static final int RESTART_INPUT_LENGTHS = 1;   // 재시작 입력 문자열 크기
+    public static final int ONE = 1;
+    public static final int NINE = 9;
+    public static final int ASCII_ONE = 49;
+    public static final int ASCII_NINE = 57;
+    public static final int ASCII_TWO = 50;
+    public static final int RESTART = 1;
+    public static final int GAMEOVER = 2;
+
+
+}
