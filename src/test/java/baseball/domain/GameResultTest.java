9c66ffba42d0063e55aa5119f85a75e54ef35ae0
Wishoon <heonga2@gmail.com>
2021-11-28 12:52:15 +0900

    test: 생성된 각 3자리 숫자의 비교기능 테스트 변경

--- src/test/java/baseball/domain/GameResultTest.java
+++ src/test/java/baseball/domain/GameResultTest.java
@@ -1,32 +1,74 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameResultTest {
-    BaseballNums answerBaseballNums;
-    BaseballNums playerBaseballNums;
     GameResult gameResult;
 
     @BeforeEach
-    void 게임결과_테스트_기본세팅() {
-        answerBaseballNums = new BaseballNums(Arrays.asList(4, 2, 3));
-        playerBaseballNums = new BaseballNums(Arrays.asList(2, 4, 3));
+    void 게임결과_테스트_기본설정() {
         gameResult = new GameResult();
     }
 
-    @Test
-    void 스트라이크_볼_계산로직_검증() {
-        // given // when
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "234", "789"})
+    void 스트라이크_3_검증(String value) {
+        BaseballNums answerBaseballNums = new BaseballNums(makeConvertStringToInt(value));
+        BaseballNums playerBaseballNums = new BaseballNums(makeConvertStringToInt(value));
         gameResult.checkResult(answerBaseballNums, playerBaseballNums);
+        assertAll(() -> {
+            assertEquals(gameResult.getStrikeCount(), 3);
+            assertEquals(gameResult.getBallCount(), 0);
+        });
+    }
 
-        // then
+    @ParameterizedTest
+    @CsvSource(value = {"123:132", "789: 879"}, delimiter = ':')
+    void 스트라이크_1_볼_2_검증(String answerValue, String playerValue) {
+        BaseballNums answerBaseballNums = new BaseballNums(makeConvertStringToInt(answerValue));
+        BaseballNums playerBaseballNums = new BaseballNums(makeConvertStringToInt(playerValue));
+        gameResult.checkResult(answerBaseballNums, playerBaseballNums);
         assertAll(() -> {
             assertEquals(gameResult.getStrikeCount(), 1);
             assertEquals(gameResult.getBallCount(), 2);
         });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"416:641", "234:423"}, delimiter = ':')
+    void 볼_3_검증(String answerValue, String playerValue) {
+        BaseballNums answerBaseballNums = new BaseballNums(makeConvertStringToInt(answerValue));
+        BaseballNums playerBaseballNums = new BaseballNums(makeConvertStringToInt(playerValue));
+        gameResult.checkResult(answerBaseballNums, playerBaseballNums);
+        assertAll(() -> {
+            assertEquals(gameResult.getStrikeCount(), 0);
+            assertEquals(gameResult.getBallCount(), 3);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:456", "357:468"}, delimiter = ':')
+    void 낫싱_검증(String answerValue, String playerValue) {
+        BaseballNums answerBaseballNums = new BaseballNums(makeConvertStringToInt(answerValue));
+        BaseballNums playerBaseballNums = new BaseballNums(makeConvertStringToInt(playerValue));
+        gameResult.checkResult(answerBaseballNums, playerBaseballNums);
+        assertAll(() -> {
+            assertEquals(gameResult.getStrikeCount(), 0);
+            assertEquals(gameResult.getBallCount(), 0);
+        });
+    }
+
+    private List<Integer> makeConvertStringToInt(String value) {
+        return Arrays.asList(value.split("")).stream()
+                .map(Integer::valueOf).collect(Collectors.toList());
     }
 }
