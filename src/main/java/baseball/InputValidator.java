f59fb831c4b85e9be605814f9ca5547d8bf35fb6
hyewoncc <hyewoncc@gmail.com>
2021-11-28 21:19:53 +0900

    feat: 게임 재시작/종료 입력값 검사
    게임 재시작 또는 종료 시 올바른 입력값이 주어졌는지 검사

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -1,11 +1,19 @@
 package baseball;
 
 import static baseball.BaseballGame.TARGET_SIZE;
+import static baseball.Computer.*;
 
 public class InputValidator {
 
 	public static boolean isRightGuessInput(String input) {
 		if (hasOnlyNums(input) && isRightGuessLength(input)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isRightRestartInput(String input) {
+		if (isRightRestartLength(input) && isRightRestartOption(input)){
 			return true;
 		}
 		return false;
@@ -25,5 +33,22 @@ public class InputValidator {
 			return false;
 		}
 		return true;
+	}
+
+	private static boolean isRightRestartLength(String input) {
+		if (input.length() != RESTART_OPTION_SIZE) {
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean isRightRestartOption(String input) {
+		if (hasOnlyNums(input)) {
+			if (Integer.parseInt(input) == RESTART_YES || Integer.parseInt(input) == RESTART_NO) {
+				return true;
+			}
+			return false;
+		}
+		return false;
 	}
 }
