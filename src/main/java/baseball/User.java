268d021160cd6232d741ad98ba3cd0a34adfad13
shindong96 <2006peter@naver.com>
2021-11-29 00:20:34 +0900

    feat(User) : add User class and add chooseNumber method
    checkReduplication method and reset method are added in User class, too

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,46 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+	public int[] number = {0, 0, 0};
+
+	public void reset() {
+		for (int i = 0; i < 3; i++) {
+			number[i] = 0;
+		}
+	}
+
+	public void chooseNumber() {
+		reset();
+		System.out.print("숫자를 입력해주세요 : ");
+		String numberStr = Console.readLine();
+
+		if (numberStr.length() != 3) {
+			throw new IllegalStateException();
+		}
+
+		for (int i = 0; i < 3; i++) {
+			char charIdxNum = numberStr.charAt(i);
+
+			if (!(charIdxNum - '0' >= 1 && charIdxNum - '0' <= 9)) {
+				throw new IllegalStateException();
+			}
+
+			if (!checkReduplication(charIdxNum - '0', i)) {
+				throw new IllegalStateException();
+			}
+
+			number[i] = charIdxNum - '0';
+		}
+	}
+
+	public boolean checkReduplication(int num, int idx) {
+		for (int i = 0; i < idx; i++) {
+			if (number[i] == num) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
