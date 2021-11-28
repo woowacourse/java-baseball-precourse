dae6fe6bd0963f286aad69a36937cdce8d4a5702
lomuto <kangys1245@gmail.com>
2021-11-28 14:04:23 +0900

    feature(Score) Add score class

---
+++ src/main/java/baseball/GameManager/Score.java
@@ -0,0 +1,19 @@
+package baseball.GameManager;
+
+public class Score {
+    private int strike;
+    private int ball;
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
