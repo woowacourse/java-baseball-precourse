61ff88c8fc895673458961f5606e7e8bb00713ff
RIANAEH <ririhan217@gmail.com>
2021-11-28 14:43:36 +0900

    feat(randomNumberPicker): add random number picker
    난수를 생성하는 기능을 추가하였다.
    - 1~9 숫자 중 하나를 선택한다.
    - 중복되는 숫자가 없게한다.

---
+++ src/main/java/baseball/RandomNumberPicker.java
@@ -0,0 +1,43 @@
+package baseball;
+
+import java.util.HashSet;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberPicker {
+
+	private static final int RANDOM_START_INCLUSIVE = 1;
+	private static final int RANDOM_END_INCLUSIVE = 9;
+	private static final int BALL_NUMBERS_LENGTH = 3;
+	private String randomBallNumbers;
+	private HashSet<Integer> randomBallNumbersSet;
+
+	public int pickOneRandomNumber() {
+		return Randoms.pickNumberInRange(RANDOM_START_INCLUSIVE, RANDOM_END_INCLUSIVE);
+	}
+
+	public void setRandomBallNumbers() {
+		StringBuilder stringBuilder = new StringBuilder();
+		randomBallNumbersSet = new HashSet<>();
+		while (stringBuilder.length() < BALL_NUMBERS_LENGTH) {
+			int number = pickOneRandomNumber();
+			if(checkRandomBallNumberDuplicate(number)) {
+				stringBuilder.append(number);
+			}
+		}
+		randomBallNumbers = stringBuilder.toString();
+	}
+
+	public boolean checkRandomBallNumberDuplicate(int number) {
+		if (randomBallNumbersSet.contains(number)) {
+			return false;
+		} else {
+			randomBallNumbersSet.add(number);
+			return true;
+		}
+	}
+
+	public String getRandomBallNumbers() {
+		return randomBallNumbers;
+	}
+}
