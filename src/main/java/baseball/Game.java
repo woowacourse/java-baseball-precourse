c90ccce967a649fef5dff1afa477a9377d48a63f
soominsohn <soomin.sohn.dev@gmail.com>
2021-11-28 18:23:25 +0900

    feat: add continue the game function - add new method isEndGame()

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,8 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
 	public void gameStart() {
+
+		boolean gameContinueFlag;
+
+		do {
+			gameContinueFlag = isEndGame();
+		} while (gameContinueFlag);
+
+	}
+
+	public boolean isEndGame() {
+
+		String playerInput;
+
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+		playerInput = Console.readLine();
+
+		if (playerInput.equals("1")) {
+			return true;
+		}
+
+		if (playerInput.equals("2")) {
+			return false;
+		}
+		//need: 사용자 입력 예외처리 작업
+		return false;
 
 	}
 }
