1a5817f43f5b166777d183b405a5c4388931e33d
Donggyu Kim <a29661498@gmail.com>
2021-11-28 21:49:23 +0900

    feat: 컴퓨터 난수 생성시 중복 검증
    총 3개의 `서로 다른` 랜덤한 숫자를 생성해야 하므로 이를 검증하는 assginNumber(), checkContains() 메소드를 RandomUtil.java에 추가로 구현하였습니다.

--- src/main/java/baseball/util/RandomUtil.java
+++ src/main/java/baseball/util/RandomUtil.java
@@ -11,13 +11,39 @@ public class RandomUtil {
         int[] computerNumbers = new int[3];
 
         for (int i =0; i < 3; i++) {
-            computerNumbers[i] = getRandomNumber();
+            computerNumbers[i] = assignNumber(computerNumbers);
         }
 
         return computerNumbers;
     }
 
+    private static int assignNumber(int[] computerNumbers) {
+        while (true) {
+            int randomNumber = getRandomNumber();
+
+            boolean checkContain = checkContains(computerNumbers, randomNumber);
+
+            if(checkContain == true) {
+                return randomNumber;
+            }
+        }
+    }
+
     private static int getRandomNumber() {
         return Randoms.pickNumberInRange(1, 9);
+    }
+
+    private static boolean checkContains(int[] computerNumbers, int randomNumber) {
+
+        for (int i = 0; i < 3; i++) {
+            if (computerNumbers[i] == 0) {
+                break;
+            }
+            if (computerNumbers[i] == randomNumber) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
