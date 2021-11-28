306d5a132f14decca574a024e924236eed2a3578
CptBluebear <cptbluebear@naver.com>
2021-11-29 02:02:06 +0900

    feat(GameData.java): 게임 재시작 문자열 추가

--- src/main/java/baseball/data/GameData.java
+++ src/main/java/baseball/data/GameData.java
@@ -8,5 +8,6 @@ public class GameData {
     public static final String STRIKE_MESSAGE = "스트라이크";
     public static final String BALL_MESSAGE = "볼";
     public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 }
