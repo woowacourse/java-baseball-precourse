51fe5c391415d91410da47d13c0c326e220e06bf
dls4585 <dls4585@naver.com>
2021-11-29 02:38:39 +0900

    feat: 맞힐 때까지 반복하도록 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,14 +11,27 @@ public class Application {
         NumberValidator validator = new ThreeDigitValidator();
         Ball ball = new Ball(new ThreeDigitGenerator(), validator);
         User user = new User();
+        Checker checker = new GameChecker();
 
         ball.generateNumber();
-        String inputString = Console.readLine();
 
-        List<Integer> inputNumbers = parseInput(inputString);
-        validateInput(validator, inputNumbers);
-        user.setNumbers(inputNumbers);
-        
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputString = Console.readLine();
+
+            List<Integer> inputNumbers = parseInput(inputString);
+            validateInput(validator, inputNumbers);
+            user.setNumbers(inputNumbers);
+
+            int strikes = checker.checkStrike(ball.getNumber(), user.getNumbers());
+            int balls = checker.checkBall(ball.getNumber(), user.getNumbers());
+            printResult(strikes, balls);
+            if(strikes == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+
+        } while(true);
     }
 
     public static void validateInput(NumberValidator validator, List<Integer> inputNumbers) {
@@ -41,6 +54,18 @@ public class Application {
     public static void isDigit(char character) {
         if(!Character.isDigit(character)) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    public static void printResult(int strikes, int balls) {
+        if (strikes == 0 && balls == 0) {
+            System.out.println("낫싱");
+        } else if (strikes > 0 && balls == 0) {
+            System.out.println(strikes + "스트라이크");
+        } else if (strikes == 0 && balls > 0) {
+            System.out.println(balls + "볼");
+        } else if (strikes > 0 && balls > 0) {
+            System.out.println(balls + "볼 " + strikes + "스트라이크");
         }
     }
 }
