a14b7a5f76072e3214c3ac106ab6c0147de700c1
shindong96 <2006peter@naver.com>
2021-11-29 00:52:43 +0900

    feat(Game) : add restart method

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 	private int strike = 0;
 	private int ball = 0;
@@ -71,5 +73,13 @@ public class Game {
 	}
 
 	public boolean restart() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String str = Console.readLine();
+
+		if (str.equals("1")) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
