0fdca927594046f45479295e141d7540e308620b
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-11-28 20:38:17 +0900

    feat: 컴퓨터가 랜덤한 세자리 숫자 생성하는 기능 추가

---
+++ src/main/java/baseball/BaseballGameUtils.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.*;
+
+public class BaseballGameUtils {
+
+	public BaseballGameUtils() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static int makeThreeLengthRandomNumber() {
+		int number = 0;
+		number = Randoms.pickNumberInRange(1, 9) * 100
+				+ Randoms.pickNumberInRange(1, 9) * 10
+				+ Randoms.pickNumberInRange(1, 9);
+		return number;
+	}
+}
