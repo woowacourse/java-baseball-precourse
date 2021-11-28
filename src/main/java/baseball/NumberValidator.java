42e0b4df399196cffc589d436dc3ee8372bf6e76
dls4585 <dls4585@naver.com>
2021-11-29 01:19:44 +0900

    feat : 검증 메소드 오버로딩
    사용자 입력을 위해 List를 검증하는 메소드 오버로딩

--- src/main/java/baseball/NumberValidator.java
+++ src/main/java/baseball/NumberValidator.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.List;
+
 public interface NumberValidator {
     boolean validateNumber(int number);
+    boolean validateNumber(List<Integer> numbers);
 }
--- src/main/java/baseball/ThreeDigitValidator.java
+++ src/main/java/baseball/ThreeDigitValidator.java
@@ -1,9 +1,16 @@
 package baseball;
 
+import java.util.List;
+
 public class ThreeDigitValidator implements NumberValidator {
 
     @Override
     public boolean validateNumber(int number) {
         return number < 1000 && number >= 100;
+    }
+
+    @Override
+    public boolean validateNumber(List<Integer> numbers) {
+        return numbers.size() == 3;
     }
 }
