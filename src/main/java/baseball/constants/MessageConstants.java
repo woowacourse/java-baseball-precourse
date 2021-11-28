c539f1fd1c7cdb3b9ba6a91e8a98e45977ba0e75
overnew <ehdls1303@gmail.com>
2021-11-28 23:07:24 +0900

    Feat: constants 패키지 클래스 구현
    MessageConstants Class 구현
     - 프로그램 실행 중 출력하는 상수 문자열 집합
    
    ValueConstants Class 구현
     - 프로그램에 사용되는 상수 숫자 값의 집합

---
+++ src/main/java/baseball/constants/MessageConstants.java
@@ -0,0 +1,17 @@
+package baseball.constants;
+
+public final class MessageConstants {
+    //게임 플레이
+    public static final String NUMBER_INPUT_REQUEST_MESSAGE= "숫자를 입력해주세요 : ";
+    public static final String GAME_END_MESSAGE = "게임 종료";
+    public static final String BALL_MESSAGE = "볼 ";
+    public static final String STRIKE_MESSAGE = "스트라이크 ";
+    public static final String CORRECT_NUMBER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! ";
+    public static final String MISS_ALL_NUMBER_MESSAGE = "낫싱";
+    public static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    //예외 처리
+    public static final String DIFF_NUMBER_INPUT_MESSAGE = "3자리의 숫자를 입력해 주세요.";
+    public static final String DIFF_OPTION_INPUT_MESSAGE = "1 혹은 2를 입력해 주세요.";
+
+}
---
+++ src/main/java/baseball/constants/ValueConstants.java
@@ -0,0 +1,12 @@
+package baseball.constants;
+
+public class ValueConstants {
+    public static final int GAME_NUMBER_LEN = 3;
+    public static final int GAME_NUMBER_BOUND_START = 1;
+    public static final int GAME_NUMBER_BOUND_END = 9;
+
+    public static final int GAME_OPTION_LEN = 1;
+    public static final int GAME_OPTION_BOUND_START = 1;
+    public static final int GAME_OPTION_BOUND_END = 2;
+
+}
