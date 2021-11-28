7abbf083eb509148a6ee02431ce9e74eb12ab989
이민재 <lmj938@naver.com>
2021-11-28 13:32:54 +0900

    [refactor] PlayerNumber 클래스 리팩토링

---
+++ src/main/java/baseball/model/Number/AnswerNumber.java
@@ -0,0 +1,38 @@
+package baseball.model.Number;
+
+import baseball.constant.Constant;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+public class AnswerNumber {
+    private String answerNumber;
+
+    public AnswerNumber() {
+        getNewAnswerNumber();
+    }
+
+    public String getAnswerNumber() {
+        return answerNumber;
+    }
+
+    public void getNewAnswerNumber() {
+        ArrayList<String> answerNumberList = new ArrayList<String>();
+
+        while (answerNumberList.size() < Constant.USER_NUMBER_LENGTH) {
+            isNumberInList(getStringRandomNumber(), answerNumberList);
+        }
+        this.answerNumber = String.join("", answerNumberList);
+    }
+
+    public static void isNumberInList(String number, ArrayList<String> list) {
+        if (!list.contains(number)) {
+            list.add(number);
+        }
+
+    }
+
+    public static String getStringRandomNumber() {
+        return Integer.toString(Randoms.pickNumberInRange(Constant.MIN_RANGE_NUM, Constant.MAX_RANGE_NUM));
+    }
+}
---
+++ src/main/java/baseball/model/Number/PlayerNumber.java
@@ -0,0 +1,54 @@
+package baseball.model.Number;
+
+import baseball.constant.Constant;
+import baseball.constant.SystemMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlayerNumber {
+    private String playerNumber;
+
+    public PlayerNumber(String playerNumber) {
+
+        isStringLengthCorrect(playerNumber);
+        isDigitPlayerNumber(playerNumber);
+        isDifferentPlayerNumber(playerNumber);
+
+        this.playerNumber = playerNumber;
+
+    }
+
+    public String getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public static void isStringLengthCorrect(String word) throws IllegalArgumentException {
+        if (word.length() != Constant.USER_NUMBER_LENGTH) {
+            throw new IllegalArgumentException(SystemMessage.NUMBER_LENGTH_NOT_CORRECT);
+        }
+    }
+
+    public void isDigitPlayerNumber(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            isDigitCharInString(word, i);
+        }
+    }
+
+    public static void isDifferentPlayerNumber(String word) throws IllegalArgumentException {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            set.add(word.charAt(i));
+        }
+        if (set.size() != word.length()) {
+            throw new IllegalArgumentException(SystemMessage.HAS_SAME_NUMBER);
+        }
+    }
+
+    public static void isDigitCharInString(String word, int index) throws IllegalArgumentException {
+        if (!Character.isDigit(word.charAt(index))) {
+            throw new IllegalArgumentException(SystemMessage.STRING_IS_NOT_NUMBER);
+        }
+    }
+}
