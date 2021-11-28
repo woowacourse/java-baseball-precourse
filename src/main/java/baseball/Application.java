f1737b87938452a6271aaa8b6c83b19c0c766050
zmfl1230 <zmfl123097@gmail.com>
2021-11-28 14:48:08 +0900

    fix: receive value to know whether restart or not

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -57,11 +57,14 @@ public class Application {
 
             }
 
-        }
-
             // TODO: 사용자로부터 게임 재게 여부 확인 (1 or 2)
+            System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int restart = Integer.parseInt(Console.readLine());
 
             // TODO: 재게 여부에 따른 flag 변수 수정
+            if(restart == 2) flag = false;
+        }
+
     }
 
     public static int[] convertNumberToArray(int number) {
