472a213f300799d76da84760193c043bb5527f60
Donggyu Kim <a29661498@gmail.com>
2021-11-28 21:00:41 +0900

    feat: 사용자 입력 전체에 따른 힌트 메시지 제공
    앞서 하나의 값에 대해 ball, strike, nothing 을 구별하는 메소드를 구현하였는데 이를 전부 합산하여 최종 메시지를 전달하기 위한 makeMessage() 메소드를 구현하였습니다.

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -5,7 +5,7 @@ public class Baseball {
     private String checkNumber(int[] computerNumbers, int userInput, int index) {
 
         for (int i = 0; i < 3; i++) {
-            if(computerNumbers[i] == userInput && i == index) {
+            if (computerNumbers[i] == userInput && i == index) {
                 return "스트라이크";
             } else if (computerNumbers[i] == userInput) {
                 return "볼";
@@ -13,5 +13,24 @@ public class Baseball {
         }
 
         return "낫싱";
+    }
+
+    private String makeMessage(int strike, int ball) {
+
+        String message = "";
+
+        if (strike == 0 && ball == 0) {
+            message = "낫싱";
+        }
+
+        if (ball > 0) {
+            message = ball + "볼 ";
+        }
+
+        if (strike > 0) {
+            message = strike + "스트라이크";
+        }
+
+        return message;
     }
 }
