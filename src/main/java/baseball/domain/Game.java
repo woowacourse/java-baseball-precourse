6e82123187717c6638bbc44d8f4be2807df2cf7f
Wishoon <heonga2@gmail.com>
2021-11-28 14:37:32 +0900

    feat(Application, Game): 게임 동작, 게임 실행, 게임 재실행 기능 추가

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,16 +1,41 @@
 package baseball.domain;
 
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Game {
     private static final String RESTART_GAME = "1";
     private static final String END_GAME = "2";
     private static final String NOT_NUMBER_INVALID_ONE_OR_TWO = "1 혹은 2의 입력이 아닌 올바르지 않은 입력입니다.";
+    private BaseballNums answerBaseballNums;
+    private BaseballNums playerBaseballNums;
+    private GameResult gameResult;
+
+    public void processRun() {
+        processInit();
+        do {
+            processPlay();
+        } while (gameResult.isValidateWinner());
+        OutputView.showGameResultMessage();
+        processResume();
+    }
+
+    public void processInit() {
+        gameResult = new GameResult();
+        answerBaseballNums = BaseballNumsGenerator.generateRandomBaseballNums();
+    }
+
+    public void processPlay() {
+        playerBaseballNums = BaseballNumsGenerator.generateInputBaseballNums(InputView.inputBaseballNumbers());
+        gameResult.checkResult(answerBaseballNums, playerBaseballNums);
+        OutputView.showHintMessage(gameResult);
+    }
 
     public void processResume() {
         String inputMessage = InputView.inputResumeOrEnd();
 
         if (inputMessage.equals(RESTART_GAME)) {
+            processRun();
             return;
         }
         if (inputMessage.equals(END_GAME)) {
