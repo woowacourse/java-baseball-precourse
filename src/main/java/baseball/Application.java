274667be5be025cd22b0f7fd7a74c1dd4eff9894
Donggyu Kim <a29661498@gmail.com>
2021-11-28 21:15:32 +0900

    feat: Application main()에 Baseball 객체 생성 후 startGame() 메소드 호출
    프로그램의 시작점에 해당하는 main() 메소드에서 Baseball 객체 생성 이후 startGame() 메소드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.game.Baseball;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        Baseball baseball = new Baseball();
+        baseball.startGame();
     }
 }
