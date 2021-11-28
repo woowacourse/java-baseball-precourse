cc28eb078fe1220eedbe586027390642e2141b06
lymchgmk <lymchgmk@gmail.com>
2021-11-28 22:35:28 +0900

    feat(Constants): 상수 보관 class 구현
    게임 내 메시지 및 관련 상수들을 모은 class

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,23 @@
+package baseball;
+
+public class Constants {
+    public static final String GAME_START_MSG = "게임을 시작하겠습니다.";
+    public static final String GAME_END_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String GAME_EXIT_MSG = "게임을 종료하겠습니다. 감사합니다.";
+    
+    public static final String INPUT_NUMBER_MSG = "숫자를 입력해주세요 : ";
+    
+    public static final String INPUT_ERROR_MSG = "잘못된 입력입니다.";
+    
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    
+    public static final String RESTART = "1";
+    public static final String EXIT = "2";
+    
+    public static final int[] DEFAULT_ANSWER = {0, 0, 0};
+    public static final int ANSWER_LENGTH = 3;
+    public static final int EXIT_OR_CONTINUE_LENGTH = 1;
+}
