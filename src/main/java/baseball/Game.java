fd91d3bd05bf7655e9083934f4aa452a5fe9c2ee
jochungbeom <cndqjacndqja@gmail.com>
2021-11-28 14:46:36 +0900

    refactor: 출력문 Output 클래스의 메소드로 분리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,17 +8,10 @@ public class Game {
 	User user = new User();
 	boolean game = true;
 
-	public Boolean decideWhetherRestart() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-		int input = Integer.parseInt(Console.readLine());
-		return input == 1;
-	}
-
 	public void run() {
 		while (game) {
-			System.out.print("숫자를 입력해주세요 : ");
-			String input = Console.readLine();
-			user.insertNumbers(input);
+			Output.printInputView();
+			user.insertNumbers(Console.readLine());
 			referee.judge(user.getBalls());
 			Output.printResult(referee.getStrikeCount(), referee.getBallCount());
 			if (referee.getStrikeCount() == 3) {
@@ -29,11 +22,17 @@ public class Game {
 	}
 
 	public void setForRestarting() {
-		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		Output.printFinishView();
 		game = decideWhetherRestart();
 		if (game) {
 			computer.createNewNumbers();
 			referee.setBalls(computer.getBalls());
 		}
+	}
+
+	public Boolean decideWhetherRestart() {
+		Output.printRequestRestartOrFinish();
+		int input = Integer.parseInt(Console.readLine());
+		return input == 1;
 	}
 }
--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -13,4 +13,16 @@ public class Output {
 			System.out.println("낫싱");
 		}
 	}
+
+	public static void printInputView() {
+		System.out.print("숫자를 입력해주세요 : ");
+	}
+
+	public static void printFinishView() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+	}
+
+	public static void printRequestRestartOrFinish() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+	}
 }
