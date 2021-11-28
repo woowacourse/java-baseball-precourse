545006021fed6ac6a9249476f963df6f984c9e39
Garrrruii <hishally012@gmail.com>
2021-11-28 21:23:18 +0900

    [feat] count strike 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,76 +18,76 @@ public class Application {
         }
     }
 
-    static boolean GAME_ERROR=false;
-    static boolean GAME_NORMAL=true;
+    static boolean GAME_ERROR = false;
+    static boolean GAME_NORMAL = true;
 
-    static boolean CORRECT_INPUT=true;
-    static boolean WRONG_INPUT=false;
+    static boolean CORRECT_INPUT = true;
+    static boolean WRONG_INPUT = false;
 
-    static boolean PlayGame(){
+    static boolean PlayGame() {
         String keyNumbers = SelectRandomNumbers();
-        while(true){
+        while (true) {
             String inputNumbers = GetInputNumbers();
 
-            if(IsInputError(inputNumbers)){
+            if (IsInputError(inputNumbers)) {
                 return GAME_ERROR;
             }
 
-            boolean inputResult = CheckInputNumbers(keyNumbers,inputNumbers);
-            if(inputResult==CORRECT_INPUT){
+            boolean inputResult = CheckInputNumbers(keyNumbers, inputNumbers);
+            if (inputResult == CORRECT_INPUT) {
                 return GAME_NORMAL;
             }
         }
     }
 
 
-    static int MAX_NUMBER=9;
-    static int MIN_NUMBER=1;
-    static int NUMBER_COUNT=MAX_NUMBER-MIN_NUMBER+1;
-    static int NUMBER_SIZE=3;
+    static int MAX_NUMBER = 9;
+    static int MIN_NUMBER = 1;
+    static int NUMBER_COUNT = MAX_NUMBER - MIN_NUMBER + 1;
+    static int NUMBER_SIZE = 3;
 
-    static String SelectRandomNumbers(){
+    static String SelectRandomNumbers() {
         boolean[] existence = new boolean[NUMBER_COUNT];
         int[] randomNumber = new int[NUMBER_SIZE];
-        int randomNumberInt=0;
+        int randomNumberInt = 0;
 
-        for(int i=0;i<NUMBER_SIZE;++i){
-            randomNumber[i]=pickNumberInRange(MIN_NUMBER,MAX_NUMBER);
-            if(existence[randomNumber[i]-MIN_NUMBER]){
+        for (int i = 0; i < NUMBER_SIZE; ++i) {
+            randomNumber[i] = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (existence[randomNumber[i] - MIN_NUMBER]) {
                 i--;
-            } else{
-                existence[randomNumber[i]-MIN_NUMBER]=true;
+            } else {
+                existence[randomNumber[i] - MIN_NUMBER] = true;
             }
         }
 
-        for(int i=0;i<NUMBER_SIZE;++i){
-            randomNumberInt*=10;
-            randomNumberInt+=randomNumber[i];
+        for (int i = 0; i < NUMBER_SIZE; ++i) {
+            randomNumberInt *= 10;
+            randomNumberInt += randomNumber[i];
         }
 
-        String keyNumbers=Integer.toString(randomNumberInt);
+        String keyNumbers = Integer.toString(randomNumberInt);
         return keyNumbers;
     }
 
-    static String GetInputNumbers(){
+    static String GetInputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
 
         String inputNumbers = readLine();
         return inputNumbers;
     }
 
-    static boolean INPUT_ERROR=true;
-    static boolean NON_INPUT_ERROR=false;
+    static boolean INPUT_ERROR = true;
+    static boolean NON_INPUT_ERROR = false;
 
-    static boolean IsInputError(String inputNumbers){
-        if(inputNumbers.length()!=NUMBER_SIZE){
+    static boolean IsInputError(String inputNumbers) {
+        if (inputNumbers.length() != NUMBER_SIZE) {
             return INPUT_ERROR;
         }
 
-        boolean[] existence=new boolean[NUMBER_COUNT];
+        boolean[] existence = new boolean[NUMBER_COUNT];
 
-        for(int i=0;i<NUMBER_SIZE;++i){
-            if(inputNumbers.charAt(i)<'1' || '9'<inputNumbers.charAt(i)){
+        for (int i = 0; i < NUMBER_SIZE; ++i) {
+            if (inputNumbers.charAt(i) < '1' || '9' < inputNumbers.charAt(i)) {
                 return INPUT_ERROR;
             }
         }
@@ -95,34 +95,38 @@ public class Application {
         return NON_INPUT_ERROR;
     }
 
-    static boolean CheckInputNumbers(String keyNumbers,String inputNumbers){
+    static boolean CheckInputNumbers(String keyNumbers, String inputNumbers) {
         int strikeCount = 0;
-        int ballCount=0;
+        int ballCount = 0;
 
-        for(int i=0;i<NUMBER_SIZE;++i){
-            if(IsStrike(keyNumbers.charAt(i),inputNumbers.charAt(i))){
+        for (int i = 0; i < NUMBER_SIZE; ++i) {
+            if (IsStrike(keyNumbers.charAt(i), inputNumbers.charAt(i))) {
                 strikeCount++;
-            } else if(IsBall(keyNumbers,inputNumbers.charAt(i))){
+            } else if (IsBall(keyNumbers, inputNumbers.charAt(i))) {
                 ballCount++;
             }
         }
 
-        return ReturnInputResult(strikeCount,ballCount);
+        return ReturnInputResult(strikeCount, ballCount);
     }
 
-    static boolean IsStrike(char keyNumbersChar, char inputNumbersChar){
-
-    }
-    static boolean IsBall(String keyNumbers, char inputNumbersChar){
-
-    }
-
-    static boolean ReturnInputResult(int strikeCount,int ballCount){
-
+    static boolean IsStrike(char keyNumbersChar, char inputNumbersChar) {
+        if(keyNumbersChar==inputNumbersChar){
+            return true;
+        }
+        return false;
     }
 
+    static boolean IsBall(String keyNumbers, char inputNumbersChar) {
+        
+    }
 
-    static boolean DecideReplayOrExit(String gameResult){
+    static boolean ReturnInputResult(int strikeCount, int ballCount) {
+
+    }
+
+
+    static boolean DecideReplayOrExit(String gameResult) {
 
     }
 
