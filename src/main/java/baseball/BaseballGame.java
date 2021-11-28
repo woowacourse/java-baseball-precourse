d04b4602b5d713632d595f36514e0bf0494ad442
Leeyerimearth <50127628+Leeyerimearth@users.noreply.github.com>
2021-11-28 22:16:16 +0900

    feat: 5. 입력받은 숫자에 대한 결과를 출력하는 기능
    볼/스트라이크 개수 판단 기능 개발이전, 출력에 대한 기능 선개발

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,10 +3,42 @@ package baseball;
 
 public class BaseballGame {
 	
+	private static final int BALL_CHECK_NUMBER = 3;
+	private static final int STRIKE_CHECK_NUMBER = 3;
+	
 	public BaseballGame(){
 		//BaseballGameUtils.makeThreeLengthRandomNumber();
-		BaseballGameUtils.getNumberFromUser();
+		//int number = BaseballGameUtils.getNumberFromUser();
 	}
 	
+	public void checkScore() {
+		int countBalls = 0;
+		int countStrikes = 0;
+		countBalls = checkBall();
+		countStrikes = checkStrike();
+		
+		if (countBalls + countStrikes == 0) {
+			System.out.println("낫싱");
+		}
+		if (countBalls != 0) {
+			System.out.println(countBalls+"볼");
+		}
+		if (countStrikes > 0 && countStrikes <= STRIKE_CHECK_NUMBER) {
+			System.out.println(countStrikes+"스트라이크");
+		}
+		if (countStrikes == STRIKE_CHECK_NUMBER) {
+			System.out.println(STRIKE_CHECK_NUMBER+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		}
+	}
+	
+	private int checkBall() {
+		int number = 0;
+		return number;
+	}
+	
+	private int checkStrike() {
+		int number = 0;
+		return number;
+	}
 
 }
