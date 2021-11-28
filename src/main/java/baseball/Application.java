2982cc67e6a8f68adde8aae79541e4f55eba5cb9
CptBluebear <cptbluebear@naver.com>
2021-11-29 02:16:56 +0900

    feat(Application.java): 게임 재시작 및 메인 메소드 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import baseball.data.GameData;
+import baseball.engine.GameEngine;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        Application application = new Application();
+        application.run();
+    }
+
+    public void run() {
+        GameEngine gameEngine = new GameEngine();
+
+        do {
+            gameEngine.start();
+        } while (checkRestart());
+    }
+
+    private boolean checkRestart() {
+        System.out.println(GameData.GAME_RESTART_MESSAGE);
+        String userInput = Console.readLine();
+
+        if(userInput.equals("1")) {
+            return true;
+        } else if(userInput.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException(userInput);
+        }
     }
 }
