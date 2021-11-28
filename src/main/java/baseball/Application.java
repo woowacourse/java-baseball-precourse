244b7c0870680ed227d6c96a1a7219c28f447efa
ayoung0073 <ayong0310@naver.com>
2021-11-29 00:57:14 +0900

    feat: 정답(랜덤 숫자) 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.util.LinkedHashSet;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    private static final int NUMBER_LENGTH = 3;
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        System.out.println(generateRandomNumber());
+    }
+
+    public static String generateRandomNumber() {
+        LinkedHashSet<Integer> randomNumberSet = new LinkedHashSet<>();
+
+        while (randomNumberSet.size() < NUMBER_LENGTH) {
+            int pickedNumber = Randoms.pickNumberInRange(1, 9);
+            randomNumberSet.add(pickedNumber);
+        }
+
+        StringBuilder randomNumber = new StringBuilder();
+        for (Integer number : randomNumberSet) {
+            randomNumber.append(number);
+        }
+
+        return randomNumber.toString();
     }
 }
