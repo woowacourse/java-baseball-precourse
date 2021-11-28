19bd4864fb5dc8e82ceb30a5e4a4faffef80cb45
Juhyung990122 <harukawa99@naver.com>
2021-11-29 02:02:29 +0900

    feat: 입력값 중복여부 체크로직 구현

--- src/main/java/utils/Validation.java
+++ src/main/java/utils/Validation.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.stream.Stream;
+
 public class Validation {
     public static void validate(String input) {
         if (input.length() != 3) {
@@ -9,6 +11,15 @@ public class Validation {
         if (input.charAt(0) == '0') {
             throw new IllegalArgumentException();
         }
+
+        String[] inputDigitSet = Stream.of(input.split(""))
+                .distinct()
+                .toArray(String[]::new);
+
+        if (inputDigitSet.length != 3){
+            throw new IllegalArgumentException();
+        }
+
     }
 }
 
