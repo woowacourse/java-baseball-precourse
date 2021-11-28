643942e018f60893de956a44a70a8f278119671a
audgnssweet <audgnssweet@naver.com>
2021-11-29 00:35:08 +0900

    feat(Player): 3자리수 입력을 통한 정답 예측기능 추가
    - Player 클래스에 predictBalls 메서드 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
@@ -17,5 +20,31 @@ public class Player {
 			return false;
 		}
 		throw new IllegalArgumentException("입력은 1 혹은 2 여야 합니다.");
+	}
+
+	public static String predictBalls() {
+		System.out.print("숫자를 입력해주세요 : ");
+		String input = Console.readLine();
+		if (input.length() != 3) {
+			throw new IllegalArgumentException("입력된 숫자는 3자리여야 합니다.");
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (!Character.isDigit(input.charAt(i))) {
+				throw new IllegalArgumentException("입력은 반드시 숫자여야 합니다.");
+			}
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == '0') {
+				throw new IllegalArgumentException("숫자는 0일 수 없습니다.");
+			}
+		}
+		Set<Character> set = new HashSet<>();
+		for (int i = 0; i < input.length(); i++) {
+			set.add(input.charAt(i));
+		}
+		if (set.size() != 3) {
+			throw new IllegalArgumentException("숫자가 중복될 수 없습니다.");
+		}
+		return input;
 	}
 }
