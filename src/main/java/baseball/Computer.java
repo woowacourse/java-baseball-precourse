160787d0687c872e3073c41d4bce2ef6b30f42b6
audgnssweet <audgnssweet@naver.com>
2021-11-29 00:37:49 +0900

    feat(Computer): 컴퓨터가 정답을 결정하는 기능 추가
    - Computer 클래스의 determineAnswerBalls 메서드 추가
     판마다 새로운 정답을 결정하기 위해 메서드로 분리

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+
+	private String answerBalls;
+
+	public Computer() {
+		this.answerBalls = "";
+	}
+
+	public void determineAnswerBalls() {
+		List<Integer> randoms = new ArrayList<>();
+		while (randoms.size() < 3) {
+			int random = Randoms.pickNumberInRange(1, 9);
+			if (!randoms.contains(random)) {
+				randoms.add(random);
+			}
+		}
+		String answerBalls = "";
+		for (Integer random : randoms) {
+			answerBalls += random;
+		}
+
+		System.out.println("answerBalls = " + answerBalls);
+		this.answerBalls = answerBalls;
+	}
+}
