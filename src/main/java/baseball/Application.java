532918c3afe756be79f7944cd1edd9a2b10c94fa
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-28 15:58:00 +0900

    refactor : Answer Class를 통하여 정답 숫자를 관리하도록 변경
    Answer Class 생성
    - 랜덤한 값의 정답을 생성해주는 일을 담당.
    
    Game 클래스 수정
    - start를 통하여 값을 새로 발급받을 수 있도록 지정
    - 새로 시작마다 new 키워드를 통한 객체를 다시 정의하지 않도록 하기 위함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,26 +10,36 @@ public class Application {
 
     public static void main(String[] args) {
         int gameStatus = START_GAME_VALUE;
+        Game game = new Game();
 
         while (gameStatus != END_GAME_VALUE) {
-
+            game.start();
         }
     }
 }
 
 class Game {
-    List<Integer> answer;
+    Answer answer;
 
     public Game() {
+
     }
 
-    public void startGame() {
+    public void start() {
+        this.answer = new Answer();
+    }
+}
+
+class Answer {
+    List<Integer> numbers;
+
+    public Answer() {
         initAnswer();
     }
 
     private void initAnswer() {
         for (int i = 0; i < 3; i++) {
-            answer.add(getUniqueNumber());
+            numbers.add(getUniqueNumber());
         }
     }
 
@@ -39,7 +49,7 @@ class Game {
         while(true) {
             number = Randoms.pickNumberInRange(0, 9);
 
-            if (!answer.contains(number)) return number;
+            if (!numbers.contains(number)) return number;
         }
     }
 }
