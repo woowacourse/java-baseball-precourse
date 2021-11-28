37390aabd5b4ddc4858f59c4190db4d03d47d7cf
RIANAEH <ririhan217@gmail.com>
2021-11-28 15:43:19 +0900

    feat(resultView): add print result
    볼, 스트라이크 게수에 따라 결과를 다르게 출력하는 기능을 추가하였다.

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -5,4 +5,17 @@ public class ResultView {
 	public void printEndMessage() {
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
+
+	public void printResult(int ball, int strike) {
+		if (ball > 0) {
+			System.out.print(ball + "볼 ");
+		}
+		if (strike > 0) {
+			System.out.print(strike + "스트라이크");
+		}
+		if (ball == 0 && strike == 0) {
+			System.out.print("낫싱");
+		}
+		System.out.println("");
+	}
 }
