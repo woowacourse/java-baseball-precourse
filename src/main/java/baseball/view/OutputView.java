68f66475b8166cd2886d14d7398fc8b3216d8c80
Wishoon <heonga2@gmail.com>
2021-11-28 13:01:40 +0900

    feat(OutputView): 승리 시 승리 스크립트 출력기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
     private static final String BALL_COUNT_MESSAGE = "%d볼 ";
     private static final String STRIKE_COUNT_MESSAGE = "%d스트라이크";
     private static final String NOTHING_COUNT_MESSAGE = "낫싱";
+    private static final String GAME_RESULT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void showHintMessage(GameResult result) {
         if (result.getBallCount() > 0) {
@@ -18,5 +19,9 @@ public class OutputView {
             System.out.print(NOTHING_COUNT_MESSAGE);
         }
         System.out.println();
+    }
+
+    public static void showGameResultMessage() {
+        System.out.println(GAME_RESULT_MESSAGE);
     }
 }
