5c17effb417cdd0887827639621c45d7f6229dba
nayoung <evelyn82ny@gmail.com>
2021-11-28 18:23:16 +0900

    test: 입력 검증 방식 분리에 따른 테스트 작성

--- src/test/java/baseball/utils/InputValidatorTest.java
+++ src/test/java/baseball/utils/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package baseball.utils;
 
+import baseball.BaseballGameSystem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,30 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class InputValidatorTest {
 
     @Test
-    void 입력값_숫자_테스트() {
+    void 입력값_범위_테스트() {
 
         String positiveNumber = "123";
         String negativeNumber = "-758";
         String includeZero = "905";
         String includeChar = "a12";
 
-        assertDoesNotThrow(() -> InputValidator.validateDigit(positiveNumber));
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateDigit(negativeNumber));
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateDigit(includeZero));
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateDigit(includeChar));
+        assertDoesNotThrow(() -> InputValidator.validateGameDigitInRange(positiveNumber, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateGameDigitInRange(negativeNumber, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateGameDigitInRange(includeZero, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateGameDigitInRange(includeChar, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
     }
 
     @Test
     void 입력값_길이_테스트() {
+        String digit = "3";
         String threeDigits = "486";
         String negativeThreeDigits = "-486";
         String lessThanThreeDigits = "23";
         String moreThanThreeDigits = "3657";
 
-        assertDoesNotThrow(() -> InputValidator.validateLength(threeDigits));
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateLength(negativeThreeDigits));
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateLength(lessThanThreeDigits));
-        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateLength(moreThanThreeDigits));
+        assertDoesNotThrow(() -> InputValidator.validateLength(digit, BaseballGameSystem.GAME_MENU_LENGTH));
+        assertDoesNotThrow(() -> InputValidator.validateLength(threeDigits, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateLength(negativeThreeDigits, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateLength(lessThanThreeDigits, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateLength(moreThanThreeDigits, RandomDigitsGenerator.RANDOM_DIGIT_COUNT));
     }
 
     @Test
@@ -46,8 +49,12 @@ class InputValidatorTest {
     void 모든_검증_적합_테스트() {
         String properInput = "457";
         String improperInput = "161r";
+        String properGameMenuInput = "1";
+        String improperGameMenuInput = "5";
 
         assertDoesNotThrow(() -> InputValidator.validateGameDigits(properInput));
         assertThrows(IllegalArgumentException.class, () -> InputValidator.validateGameDigits(improperInput));
+        assertDoesNotThrow(() -> InputValidator.validateGameMenu(properGameMenuInput));
+        assertThrows(IllegalArgumentException.class, () -> InputValidator.validateGameMenu(improperGameMenuInput));
     }
 }
