87e998f334f7262a87311b36832fdc97720aed67
Donggyu Kim <a29661498@gmail.com>
2021-11-28 21:03:51 +0900

    feat: 하나의(단일) 게임에 대해서 진행하는 progressGame() 구현
    사용자에게 계속 게임을 묻는 부분을 제외한 하나의 게임 진행에 대한 로직을 progressGame() 메소드에 구현하였습니다.

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -1,6 +1,41 @@
 package baseball.game;
 
+import static baseball.util.RandomUtil.createComputerNumbers;
+import static baseball.util.UserInputUtil.getThreeDigitInput;
+
 public class Baseball {
+
+    private void progressGame() {
+
+        int[] computerNumbers = createComputerNumbers();
+
+        int strike = 0;
+        int ball = 0;
+
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+
+            int[] userInput = getThreeDigitInput();
+
+            for (int i = 0; i < 3; i++) {
+                String result = checkNumber(computerNumbers, userInput[i], i);
+
+                if (result.equals("스트라이크")) {
+                    strike++;
+                } else if (result.equals("볼")) {
+                    ball++;
+                }
+            }
+
+            System.out.println(makeMessage(strike, ball));
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+
+    }
 
     private String checkNumber(int[] computerNumbers, int userInput, int index) {
 
