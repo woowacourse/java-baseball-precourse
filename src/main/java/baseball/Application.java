c43aeecef765517ca9ed06873a103cec50cf6857
summerlunaa <rhgus1085@gmail.com>
2021-11-29 04:33:12 +0900

    feat: 임의의 수 생성을 위한 randomNumberGenerator 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+
 public class Application {
+    static final int MIN_NUM = 1;
+    static final int MAX_NUM = 9;
+    static final int INPUT_LEN = 3;
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        randomNumberGenerator(MIN_NUM, MAX_NUM, INPUT_LEN);
+
+    }
+
+    public static String randomNumberGenerator(int startInclusive, int endInclusive, int count) {
+        LinkedHashSet<Integer> randomNumberSet = new LinkedHashSet<>();
+        while (randomNumberSet.size() < count) {
+            randomNumberSet.add(Integer.valueOf(Randoms.pickNumberInRange(startInclusive, endInclusive)));
+        }
+
+        Iterator<Integer> randomNumberIter = randomNumberSet.iterator();
+        String randomNumberStr = "";
+        while (randomNumberIter.hasNext()) {
+            randomNumberStr += randomNumberIter.next() + "";
+        }
+
+        return randomNumberStr;
     }
 }
