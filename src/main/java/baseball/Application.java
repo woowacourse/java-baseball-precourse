1b377ee10899004474cab95fe913c7af724fb2a2
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-28 15:36:38 +0900

    feat : 게임을 반복하여 진행할 수 있도록 while문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
+    private static final int START_GAME_VALUE = 1;
+    private static final int END_GAME_VALUE = 2;
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        int gameStatus = START_GAME_VALUE;
+
+        while (gameStatus != END_GAME_VALUE) {
+
+        }
     }
 }
