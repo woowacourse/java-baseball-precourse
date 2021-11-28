4036824efc93a8730384201d261b6f932b68b076
qbang <vhfhddl0101@naver.com>
2021-11-28 16:02:59 +0900

    feat: ask if user want to restart

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         List<Integer> answerNumbersList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        System.out.println(answerNumbersList);
         while (true) {
-            String input = Console.readLine();
+            String input = readUserInput();
             validateType(input);
             validateRange(input);
             validateNumber(input);
@@ -17,11 +18,15 @@ public class Application {
             int[] compareResult = compareNumbersList(answerNumbersList, inputNumbersList);
             printHintMessage(compareResult);
             boolean finishGame = isAllNumbersCorrect(compareResult[2]);
-            if (finishGame) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            if(finishGame && finishOrNot()){
                 break;
             }
         }
+    }
+
+    // 사용자로부터 값을 입력받는다.
+    private static String readUserInput(){
+        return Console.readLine();
     }
 
     // 인풋에 문자가 포함되어 있는지 확인한다.
@@ -94,5 +99,19 @@ public class Application {
             return true;
         }
         return false;
+    }
+
+    // 3스트라이크일 때 게임을 끝낼 것인지 확인한다.
+    private static boolean finishOrNot(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = readUserInput();
+        if (input.equals("1")) {
+            return false;
+        } else if (input.equals("2")) {
+            return true;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
