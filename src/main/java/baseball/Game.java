2f40183aceeca2400d81521ff7969510f2fbc37f
wooseok <diddmlvkf@gmail.com>
2021-11-28 17:41:11 +0900

    feat(Game) : 사용자 입력으로 게임 진행 결정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -5,11 +5,13 @@ public class Game {
     private final String START = "1";
     private final String END = "2";
 
+    private boolean play = true;
+
     public Game() {
     }
 
     public void validateInput(String input) {
-        if (!(input.equals(START)) || !(input.equals(END))) {
+        if (!(input.equals(START)) && !(input.equals(END))) {
             throw new IllegalArgumentException();
         }
     }
@@ -19,5 +21,16 @@ public class Game {
             return true;
         }
         return false;
+    }
+
+    public void choicePlay(String input) {
+        validateInput(input);
+        if (input.equals(END)) {
+            play = false;
+        }
+    }
+
+    public boolean isPlay() {
+        return play;
     }
 }
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,14 +1,12 @@
 package baseball;
 
 // - [ ] 게임이 생성되면 임의의 수를 가진 Balls 생성
-//     - [ ] Hint 객체를 받아 사용자가 정답을 맞췄는지 확인
-//     - [ ] 사용자가 정답을 맞췄을 때 게임 실행 여부를 입력받는다.
+//     - [ ] 사용자 입력으로 게임 진행 결정
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -32,4 +30,15 @@ public class GameTest {
         hint.setHint(user);
         assertThat(game.checkhint(hint)).isTrue();
     }
+
+    @DisplayName("사용자 입력으로 게임 진행 결정")
+    @Test
+    void choicePlay() {
+        Game game = new Game();
+        game.choicePlay("1");
+        assertThat(game.isPlay()).isTrue();
+        game.choicePlay("2");
+        assertThat(game.isPlay()).isFalse();
+    }
+
 }
