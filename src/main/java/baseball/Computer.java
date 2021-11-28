2354116524ab5b00a6f3ff4b57083551ac22be1b
hyewoncc <hyewoncc@gmail.com>
2021-11-28 21:32:58 +0900

    feat: 입력받은 값 변환
    플레이어에게 입력받은 문자열 값을 리스트로 변환

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.BaseballGame.TARGET_SIZE;
+
 public class Computer {
 	public static final int RESTART_OPTION_SIZE = 1;
 	public static final int RESTART_YES = 1;
@@ -15,5 +20,13 @@ public class Computer {
 		if (!InputValidator.isRightGuessInput(input)) {
 			throw new IllegalArgumentException();
 		}
+	}
+
+	public List<Integer> parseGuessInput(String guessInput) {
+		List<Integer> guessNums = new ArrayList<>();
+		for (int i = 0; i < TARGET_SIZE; i++) {
+			guessNums.add(new Integer(guessInput.substring(i, i+1)));
+		}
+		return guessNums;
 	}
 }
