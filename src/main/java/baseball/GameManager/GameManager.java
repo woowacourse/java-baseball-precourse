f7cd8cdc6904ee10c8292f4b27aa3a8ca69db1d4
lomuto <kangys1245@gmail.com>
2021-11-28 13:18:13 +0900

    feature(GameManger) Add GameManager Initializer

--- src/main/java/baseball/GameManager/GameManager.java
+++ src/main/java/baseball/GameManager/GameManager.java
@@ -1,2 +1,32 @@
-package baseball.GameManager;public class GameManager {
+package baseball.GameManager;
+
+import baseball.Result.BallResult;
+import baseball.Result.NothingResult;
+import baseball.Result.ResultBase;
+import baseball.Result.StrikeAndBallResult;
+import baseball.Result.StrikeResult;
+
+public class GameManager {
+    private static GameManager instance = null;
+
+    public ResultBase result;
+
+    private GameManager() {
+        result = new StrikeResult();
+        result.appendNext(
+                new StrikeAndBallResult().appendNext(
+                        new BallResult().appendNext(
+                                new NothingResult()
+                        )
+                )
+        );
+    }
+
+    public static GameManager getInstance() {
+        if(GameManager.instance == null) {
+            GameManager.instance = new GameManager();
+        }
+
+        return GameManager.instance;
+    }
 }
