1885749e4a5db3faf352c6fe071726dfbf7602e3
hyewoncc <hyewoncc@gmail.com>
2021-11-29 02:12:58 +0900

    feat: 게임 재시작 여부 입력

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,10 +2,17 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.Computer.*;
+
 public class Player {
 
 	public static String getGuessInput() {
 		System.out.print("숫자를 입력해주세요 : ");
+		return Console.readLine();
+	}
+
+	public static String getRestartInput() {
+		System.out.printf("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.\n", RESTART_YES, RESTART_NO);
 		return Console.readLine();
 	}
 }
