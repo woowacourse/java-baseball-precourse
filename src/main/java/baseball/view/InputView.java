a1d98bcd959b623346ebf67e18b7df1e44e02d2a
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-11-29 05:25:17 +0900

    feat: InputView 매직 리터럴 -> 상수

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -7,13 +7,16 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
+    private static String INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static String INPUT_NUMBERS_DELIMITER = "";
+
     private InputView() {
     }
 
     public static List<Integer> inputNumbers() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_NUMBERS_MESSAGE);
         String inputBaseBallNumbers = Console.readLine();
-        return Arrays.stream(inputBaseBallNumbers.split(""))
+        return Arrays.stream(inputBaseBallNumbers.split(INPUT_NUMBERS_DELIMITER))
             .map(Integer::new)
             .collect(Collectors.toList());
     }
