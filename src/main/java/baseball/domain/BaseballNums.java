8832b86be42c76a857f60c96784eb3d0a6990008
Wishoon <heonga2@gmail.com>
2021-11-28 20:35:32 +0900

    feat(BaseballNums): BaseballNums의 유효성 검증 에러메시지 추가

--- src/main/java/baseball/domain/BaseballNums.java
+++ src/main/java/baseball/domain/BaseballNums.java
@@ -8,6 +8,8 @@ public class BaseballNums {
 	public static final int MIN_BASEBALL_NUMBER = 1;
 	public static final int MAX_BASEBALL_NUMBER = 9;
 	public static final int BASEBALL_NUMBERS_LENGTH = 3;
+	private static final String INPUT_DIGIT_NUMBER_ERROR_MESSAGE = "숫자의 자릿수가 올바르지 않은 입력입니다.";
+	private static final String INPUT_SAME_NUMBER_ERROR_MESSAGE = "동일한 숫자가 중복 입력되었습니다.";
 	private final List<Integer> baseballNums;
 
 	public BaseballNums(List<Integer> baseballNums) {
@@ -18,14 +20,14 @@ public class BaseballNums {
 
 	private void isValidateLength(List<Integer> baseballNums) {
 		if (baseballNums.size() != BASEBALL_NUMBERS_LENGTH) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(INPUT_DIGIT_NUMBER_ERROR_MESSAGE);
 		}
 	}
 
 	private void isValidateDuplicated(List<Integer> baseballNums) {
 		Set<Integer> tempBaseballNums = new HashSet<>(baseballNums);
 		if (tempBaseballNums.size() != BASEBALL_NUMBERS_LENGTH) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException(INPUT_SAME_NUMBER_ERROR_MESSAGE);
 		}
 	}
 
