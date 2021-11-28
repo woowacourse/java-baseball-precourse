f9e2fe93d0d45de7bbcd1810724808d098f2762e
KBY-TECH <soccer_by@naver.com>
2021-11-29 00:09:00 +0900

    refactor : 입력 값 player 객체 삽입

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,6 +3,7 @@ package baseball;
 import baseball.BaseballSystem.BaseballRule;
 import baseball.BaseballSystem.BaseballValidationImpl;
 import baseball.BaseballSystem.Validation;
+import baseball.domain.Player;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGame {
@@ -26,6 +27,7 @@ public class BaseballGame {
         try {
             System.out.print("숫자를 입력해주세요 : ");
             String validPlayerInput = validation.isValidInput(Console.readLine());
+            Player player=new Player(validPlayerInput);
             System.out.print("Business Logic 처리 완료됨 가정");
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException();
---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+public class Player {
+
+    private final String inputNumber;
+
+    public Player(String inputNumber) {
+        this.inputNumber = inputNumber;
+    }
+
+    public String getInputNumber() {
+        return inputNumber;
+    }
+}
