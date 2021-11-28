9f0f67ad4586399eace60334590b4db9a0450dda
her0807 <her0807@naver.com>
2021-11-28 21:19:17 +0900

    test(Exception/answerChecker): 예외처리 테스트 로직과 answerChecker 테스트 로직
    - 0이 포함된 경우
    - 중복 값이 포함된 경우
    - 정답확인 로직
    - 게임종료 에러 검증 로직
    - 게임종료검증 로직

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -8,29 +9,86 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class ApplicationTest extends NsTest {
 
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
+            () -> {
+                run("246", "135", "1", "597", "589", "2");
+                assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+            },
+            1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    void 게임종료_검증() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("274", "2");
+                assertThat(output()).contains("3스트라이크", "게임 종료");
+            },
+            2, 7, 4
+        );
+    }
+
+    @Test
+    void 게임종료_에러_검증() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    run("274", "3");
                 },
-                1, 3, 5, 5, 8, 9
+                2, 7, 4
+            )).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 정답확인_로직_검증() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                Computer computer = new Computer();
+                computer.pickUniqueThreeNumbersInRange();
+                AnswerChecker answerChecker = new AnswerChecker(computer.convertToList());
+                answerChecker.calculateBallOrStrike(new ArrayList<>(Arrays.asList(1, 5, 3)));
+                assertThat(answerChecker.getBallCount() == 2);
+                assertThat(answerChecker.getStrikeCount() == 1);
+            },
+            1, 3, 5
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_0이_포함된_경우() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("305"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_중복된_값이_포함된_경우() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("466"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[] {});
     }
 }
