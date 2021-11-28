10970ae8c0eb6123ad1468de0c5f20d5b40cc781
soominsohn <soomin.sohn.dev@gmail.com>
2021-11-28 18:36:59 +0900

    feat: add get player number function - add new class Player.java
    - add new methods in Player
    getNumberFromPlayer()
    checkException()

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+	public int[] getNumberFromPlayer() {
+		String tempInput = Console.readLine();
+		int[] playerNumber;
+
+		System.out.print("숫자를 입력해 주세요 : ");
+
+		tempInput = Console.readLine();
+
+		playerNumber = new int[tempInput.length()];
+
+		for (int i = 0; i < tempInput.length(); i++) {
+			playerNumber[i] = tempInput.charAt(i) - '0';
+		}
+
+		checkException(playerNumber);
+
+		return playerNumber;
+	}
+
+	//need: 사용자 입력 예외 처리
+	public void checkException(int[] playerNumber) {
+
+	}
+}
