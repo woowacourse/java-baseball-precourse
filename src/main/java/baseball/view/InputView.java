77c95dfd2e04587f673120d96a03bf0fdd53fc0a
Wishoon <heonga2@gmail.com>
2021-11-28 14:32:42 +0900

    feat(InputView): 재시작 혹은 프로그램 종료를 위한 입력기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String REQUEST_INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String REQUEST_INPUT_RESUME_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String inputBaseballNumbers() {
         System.out.print(REQUEST_INPUT_NUMBERS_MESSAGE);
         return Console.readLine();
+    }
+
+    public static String inputResumeOrEnd() {
+        System.out.println(REQUEST_INPUT_RESUME_OR_END_MESSAGE);
+        final String userInput = Console.readLine();
+        return userInput;
     }
 }
