165289898fc48279c21c9da22bbd61de0c9e6d54
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-29 01:09:25 +0900

    perf: 난수 생성을 한자리씩 생성
    변경 전 : 세자리를 한번에 난수로 받고 세자리에 0이 포함되어 있거나 같은 숫자가 중복되어 있을 시 다시 생성하도록 함.
    따라서 stackoverflow가 발생하였다.
    
    변경 후 : 한자리의 난수를 만들고 boolean 배열을 활용해 만들어지지 않은 숫자만 생성하고 int 배열 자체를 반환하였다.
    검증 단계가 사라지고 이후 실행되는 메소드에서 파싱 단계가 필요 없어지면서 프로그램 성능 또한 향상되었다.
    
    세자리수 난수를 생성하는 방식을 변경하였고, 성능 향상에 기여하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,9 @@ public class Application {
 	public static void main(String[] args) {
 		boolean keepPlay = true;
 		while (keepPlay) {
-			int answer = NumberCreation.createRandomNumber();
-			keepPlay = GameProgress.startGame(answer);
+			int[] answerArr = NumberCreation.createRandomNumber();
+			keepPlay = GameProgress.startGame(answerArr);
 		}
 	}
-	
+
 }
