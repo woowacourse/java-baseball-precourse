772660a16bd635417092919c36c02475cfbd06c8
harinn1014 <gkfla7555@gmail.com>
2021-11-28 16:43:24 +0900

    build(gradle): add utf-8 for encoding setting

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import java.util.ArrayList;
+
+public class Computer {
+	public static final int NUMBER_DIGIT= 3;
+	public static final int MIN_RANGE= 1;
+	public static final int MAX_RANGE= 9;
+
+	public Computer() {
+	}
+	
+	//generate random numbers > ranNumList
+	private void genRandomNumber() {
+		ArrayList<Integer> ranNumList= new ArrayList<>();
+		while(ranNumList.size()<NUMBER_DIGIT) {
+			int num= (pickNumberInRange(1, 9));
+			if(!ranNumList.contains(num)) {
+				ranNumList.add(num);
+			}
+		}
+	}	
+
+}
