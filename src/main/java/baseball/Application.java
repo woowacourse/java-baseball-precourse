f24496e5235f5a23bb45442a5543243197389b7f
Garrrruii <hishally012@gmail.com>
2021-11-28 21:03:53 +0900

    [feat] main 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    static boolean EXIT = false;
+    static boolean REPLAY = true;
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        while (true) {
+            boolean gameResult = PlayGame();
+            boolean replayOrExit = DecideReplayOrExit(gameResult);
+
+            if (replayOrExit == EXIT) {
+                break;
+            }
+        }
     }
+
 }
