369a22085e9f1a071ad48d6d7edfa9f2ae394bac
이민재 <lmj938@naver.com>
2021-11-28 14:42:42 +0900

    [refactor] Referee 클래스 리팩토링

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -1,35 +1,13 @@
 package baseball.model;
 
-import baseball.constant.Constant;
-
 public class Referee {
-    public int[] getHint(String playerNumber, String answerNumber){
+    private final Hint hint;
 
-        int ballCount = CountingBall(playerNumber, answerNumber);
-        int strikeCount = CountingStrike(playerNumber, answerNumber);
-
-        return new int[]{ballCount - strikeCount, strikeCount};
+    public Referee() {
+        hint = new Hint();
     }
 
-    public static int CountingBall(String playerNumber, String answerNumber) {
-        int ball = Constant.BALL_INITIAL_VALUE;
-
-        for (int i = 0; i < playerNumber.length(); i++) {
-            if (answerNumber.contains(Character.toString(playerNumber.charAt(i)))) {
-                ball += 1;
-            }
-        }
-        return ball;
-    }
-
-    public static int CountingStrike(String playerNumber, String answerNumber) {
-        int strike = Constant.STRIKE_INITIAL_VALUE;
-
-        for (char userNum : playerNumber.toCharArray()) {
-            if (answerNumber.indexOf(userNum) == playerNumber.indexOf(userNum)) {
-                strike += 1;
-            }
-        }
-        return strike;
+    public int[] getHint(String answerNumber, String playerNumber){
+        return hint.getHint(answerNumber, playerNumber);
     }
 }
