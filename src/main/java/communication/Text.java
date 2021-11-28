a1572407a62d0596408af51292473ae890c9a721
usuyn <usuyn000@gmail.com>
2021-11-29 03:49:12 +0900

    feat(Text): 출력 문자열들을 상수로 선언

---
+++ src/main/java/communication/Text.java
@@ -0,0 +1,12 @@
+package communication;
+
+public final class Text {
+    public static final String INPUT_REQUEST = "숫자를 입력해주세요 : ";
+    public static final String NOTHING = "낫싱";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART = "1";
+    public static final String END = "2";
+}
