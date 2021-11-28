2e8ba6a64f7246510bb3f4e32a7150356d463525
jurlring <78091011+jurlring@users.noreply.github.com>
2021-11-28 17:42:02 +0900

    test : 사용자 입력 예외 테스트

--- src/test/java/input/UserTest.java
+++ src/test/java/input/UserTest.java
@@ -1,9 +1,15 @@
 package input;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+
 //TDD
-class UserTest {
+class UserTest extends NsTest {
 
 	@Test
 	void 사용자_입력_테스트() {
@@ -15,23 +21,25 @@ class UserTest {
 		user.checkException(userInput);
 	}
 
-	@Test
-	void 사용자_입력_테스트_크기() {
-		// given
-		User user = new User();
-		// when
-		String userInput = "11234";
-		// then
-		user.checkException(userInput);
+	@Override
+	protected void runMain() {
+		Application.main(new String[] {});
 	}
 
 	@Test
-	void 사용자_입력_테스트_범위() {
-		// given
-		User user = new User();
-		// when
-		String userInput = "1a0";
-		// then
-		user.checkException(userInput);
+	void 예외_테스트_입력크기() {
+		assertSimpleTest(() ->
+			assertThatThrownBy(() -> runException("12"))
+				.isInstanceOf(IllegalArgumentException.class)
+		);
 	}
+
+	@Test
+	void 예외_테스트_잘못된값() {
+		assertSimpleTest(() ->
+			assertThatThrownBy(() -> runException("1a0"))
+				.isInstanceOf(IllegalArgumentException.class)
+		);
+	}
+
 }
