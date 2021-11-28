cffc367afcdb7ce26ade7efb6385a87e1feace35
hwihwi99 <chamgrace@naver.com>
2021-11-28 22:53:57 +0900

    메인함수에서 게임 실행(test 통과 확인 완료)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        PlayGame p = new PlayGame();
+        p.baseballGame();
     }
 }
