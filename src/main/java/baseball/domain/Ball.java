03aabd1cfa241bccd7c708138755e838a2de109f
윤이진 <483759@naver.com>
2021-11-28 21:50:37 +0900

    feat: Ball 클래스에 toString() 메서드 추가

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -4,11 +4,17 @@ public class Ball {
 
     private int number;
 
-    public Ball(int number) throws IllegalArgumentException{
+    public Ball(int number) throws IllegalArgumentException {
         if (number < 1 || number > 9) {
             throw new IllegalArgumentException("잘못 된 숫자 범위 입니다");
         }
         this.number = number;
     }
 
+    @Override
+    public String toString() {
+        return "Ball{" +
+            "number=" + number +
+            '}';
+    }
 }
