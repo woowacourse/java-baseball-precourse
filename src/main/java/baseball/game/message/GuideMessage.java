ee280257d6bcd4738fd9ee804aa8320752cb3506
yongyongkr <67625677+yongyongkr@users.noreply.github.com>
2021-11-29 00:24:09 +0900

    chore: 필요없는 메소드 제거

--- src/main/java/baseball/game/message/GuideMessage.java
+++ src/main/java/baseball/game/message/GuideMessage.java
@@ -13,10 +13,6 @@ public class GuideMessage {
 	public static final String WIN_GAME_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 	public static final String ERR_CORRECT_NUMBER_PARAM = "정답 메세지 출력에 잘못된 인수가 전달되었습니다";
 
-	public static String enterNumberMessage() {
-		return ENTER_NUM_MESSAGE;
-	}
-
 	public static String wrongNumberMessage(int ball, int strike) {
 		if (ball == 0 && strike == 0) {
 			return nothingMessage(ball, strike);
