30ac76ec33f4f2c962f2e5f04d33a50885f6a40e
shindong96 <2006peter@naver.com>
2021-11-28 23:59:32 +0900

    refactor(Computer) : upgrade  checkReduplication method
    add index in argument

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,7 +9,7 @@ public class Computer {
 		for (int i = 0; i < 3; i++) {
 			int num = Randoms.pickNumberInRange(1, 9);
 
-			if (checkReduplication(num)) {
+			if (checkReduplication(num, i)) {
 				answer[i] = num;
 			} else {
 				i--;
@@ -17,8 +17,8 @@ public class Computer {
 		}
 	}
 
-	public boolean checkReduplication(int num) {
-		for (int i = 0; i < 3; i++) {
+	public boolean checkReduplication(int num, int idx) {
+		for (int i = 0; i < idx; i++) {
 			if (answer[i] == num) {
 				return false;
 			}
