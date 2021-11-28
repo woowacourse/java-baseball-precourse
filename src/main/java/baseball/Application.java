215345c3bfe0b79aa72315ceb9ee045eec9bb68b
lomuto <kangys1245@gmail.com>
2021-11-28 15:10:48 +0900

    feature(Application) Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.GameManager.GameManager;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        GameManager gameManager = GameManager.getInstance();
+        gameManager.run();
     }
 }
