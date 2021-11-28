74931849043e06185a15bb7c433cf7829595e7d2
lomuto <kangys1245@gmail.com>
2021-11-28 13:11:18 +0900

    feature(NothingResult) Add NothingResult class

--- src/main/java/baseball/Result/NothingResult.java
+++ src/main/java/baseball/Result/NothingResult.java
@@ -1,2 +1,19 @@
-package baseball.Result;public class NothingResult {
+package baseball.Result;
+
+public class NothingResult extends ResultBase {
+    public NothingResult() {
+        super(
+                ResultType.NOTHING,
+                "낫싱" + System.lineSeparator()
+        );
+    }
+
+    @Override
+    public final String getMessage(int strikeCount, int ballCount, ResultType resultType) {
+        if (this.resultType.equals(resultType)) {
+            return message;
+        }
+
+        return this.next.getMessage(strikeCount, ballCount, resultType);
+    }
 }
