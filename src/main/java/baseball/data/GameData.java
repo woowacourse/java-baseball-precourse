e465a9d38611c6725010815db3d0fcaa3920ae13
CptBluebear <cptbluebear@naver.com>
2021-11-29 01:22:58 +0900

    feat(GameData.java): 게임에 필요한 상수 데이터 추가

---
+++ src/main/java/baseball/data/GameData.java
@@ -0,0 +1,12 @@
+package baseball.data;
+
+public class GameData {
+
+    public static final int NUM_OF_ANSWER = 3;
+
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String BALL_MESSAGE = "볼";
+    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+}
