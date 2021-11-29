6a0a363883a38aeb7dbb1dbe212c110bdb5315cc
xi_jjun <rlawowns97@naver.com>
2021-11-29 22:29:43 +0900

    feat: 메세지, 상수class 추가

---
+++ src/main/java/baseball/Constants/Constant.java
@@ -0,0 +1,13 @@
+package baseball.Constants;
+
+public class Constant {
+    public static final String BALL = "BALL";
+    public static final String STRIKE = "STRIKE";
+
+    // ===Game Handler===
+    public static final int KEEP_GOING = 0;
+    public static final int START = 1;
+    public static final int RESTART = 1;
+    public static final int PROGRAM_EXIT = 2;
+    public static final int GAME_EXIT = 3;
+}
