31c2c397153e4bb36631b2be6194a4770d65ca4f
Kim Seunglae <obey1342@gmail.com>
2021-11-30 00:32:33 +0900

    refactor: 서로 다른 3자리 수 생성하는 기능 String형으로 변경

--- src/main/java/baseball/BallGenerator.java
+++ src/main/java/baseball/BallGenerator.java
@@ -1,22 +1,19 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BallGenerator {
 	private static int START_NUMBER = 1;
 	private static int END_NUMBER = 9;
 
-	public List<Integer> createRandomBalls() {
-		List<Integer> balls = new ArrayList<>();
-		while (balls.size() < 3) {
-			int ball = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
-			if (!balls.contains(ball)) {
-				balls.add(ball);
+	public String createRandomBalls() {
+		StringBuilder balls = new StringBuilder();
+		while (balls.length() < 3) {
+			String ball = String.valueOf(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
+			if (!balls.toString().contains(ball)) {
+				balls.append(ball);
 			}
 		}
-		return balls;
+		return balls.toString();
 	}
 }
