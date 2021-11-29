8713ed05baa32258148a0d61f18b7f8806af181c
soominsohn <soomin.sohn.dev@gmail.com>
2021-11-29 11:41:55 +0900

    feat: add Constant class to save game message

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class Constant {
+
+	public static final int RANDOM_NUMBER_START = 1;
+	public static final int RANDOM_NUMBER_END = 9;
+	public static final int RANDOM_NUMBER_LENGTH = 3;
+
+	public static final String INPUT_MESSAGE = "숫자를 입력해 주세요: ";
+	public static final String CONTINUE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String END_MESSAGE = "게임 종료";
+	public static final String BALL = "볼";
+	public static final String STRIKE = "스트라이크";
+	public static final String NOTHING = "낫싱";
+	public static final String RESTART = "1";
+	public static final String END = "2";
+
+}
