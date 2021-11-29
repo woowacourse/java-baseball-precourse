fa7996607fe7e5a7cde7b8b8f200430183f224a2
koo <gudonghee2000@gmail.com>
2021-11-29 15:55:27 +0900

    feat : 플레이어 게임 재시작 / 종료 입력숫자 검사 기능 구현

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -16,6 +16,24 @@ public class InputController {
 
 	public static int chooseGameFlag() {
 		String gameFlag = Console.readLine();
-		return Integer.parseInt(gameFlag);
+		return isValidationGameFlag(gameFlag);
+	}
+
+	private static int isValidationGameFlag(String gameFlag) {
+		int inputNumber = Integer.parseInt(gameFlag);
+
+		if (!Pattern.matches(NUMBER_PATTERN, gameFlag)) {
+			throw new IllegalArgumentException("입력한 숫자중 잘못된 숫자(문자, 음수 등)이 있습니다.");
+		}
+
+		if (gameFlag.length() != 1) {
+			throw new IllegalArgumentException("입력한 숫자가 한자리가 아닙니다.");
+		}
+
+		if (inputNumber != 1 && inputNumber != 2) {
+			throw new IllegalArgumentException("입력한 숫자가 1 또는 2가 아닙니다.");
+		}
+
+		return inputNumber;
 	}
 }
