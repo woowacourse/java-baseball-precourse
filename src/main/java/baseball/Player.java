e159316aee9a128c8df85cf2c251ff106dc6ff6b
Kim Seunglae <obey1342@gmail.com>
2021-11-29 18:50:13 +0900

    feat: 사용자가 재시작,종료 입력하는 기능 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,6 +5,9 @@ import static resource.MessageResource.*;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
+	private static final int RESTART = 1;
+	private static final int END = 2;
+
 	public String inputBallNumber() {
 		System.out.print(INPUT_NUMBER_MESSAGE);
 		String ballNumber = Console.readLine();
@@ -12,5 +15,17 @@ public class Player {
 			throw new IllegalArgumentException(ERROR_INPUT_NUMBER);
 		}
 		return ballNumber;
+	}
+
+	public boolean isRestart() {
+		System.out.println(SELECT_RESTART_MESSAGE);
+		int button = Integer.parseInt(Console.readLine());
+		if (button == RESTART) {
+			return true;
+		}
+		if (button == END) {
+			return false;
+		}
+		throw new IllegalArgumentException(ERROR_SELECT_RESTART);
 	}
 }
--- src/main/java/resource/MessageResource.java
+++ src/main/java/resource/MessageResource.java
@@ -2,6 +2,8 @@ package resource;
 
 public class MessageResource {
 	public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	public static final String SELECT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public static final String ERROR_INPUT_NUMBER = "3자리 숫자만 입력 가능합니다.";
+	public static final String ERROR_SELECT_RESTART = "1,2 두 숫자중에 선택해야 합니다.";
 }
