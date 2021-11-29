b8de1ce4d1a6a83a5e7d3ba51403c0fa49d71b39
Dong Geun <dlehdrms978@gmail.com>
2021-11-29 23:01:55 +0900

    모든 기능 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseballgame.gamestarter.GameStarter;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        GameStarter gameStarter = new GameStarter();
+        gameStarter.start();
     }
 }
