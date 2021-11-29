73f8300aaa4ec80899e8cb46091d76c4ac979428
asebn1 <asebn1@khu.ac.kr>
2021-11-29 22:41:00 +0900

    feat: 입력값이 1 ~ 9 사이의 숫자로 이루어져있는지 확인

--- src/main/java/baseball/game/Validation.java
+++ src/main/java/baseball/game/Validation.java
@@ -3,13 +3,27 @@ package baseball.game;
 public class Validation {
 
     public static void isValidInput(String input) {
-        if (!isRightLength(input)){
+        if (!isRightLength(input)) {
             throw new IllegalArgumentException("잘못된 길이의 숫자입니다.");
+        }
+        if (!isRightRange(input)) {
+            throw new IllegalArgumentException("잘못된 숫자입니다.");
         }
     }
 
     private static boolean isRightLength(String input) {
         return input.length() == 3;
+    }
+
+    private static boolean isRightRange(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            int num = Character.getNumericValue(input.charAt(i));
+            if (num<1 || num>9){
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
