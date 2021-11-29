d845fab6fe4d2b36738f0901023c78353f1a6bd5
HYUNAHSHIM <hyunahshim@naver.com>
2021-11-30 00:26:49 +0900

    feat: 스트라이크3이 될때까지 계속 입력을 받는 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -23,4 +23,52 @@ public class Game {
 		computerNumber = Arrays.toString(computer).replaceAll("[^0-9]", "");
 	}
 
+	/**
+	 * 컴퓨터가 입력한 수와 사용자가 입력한 수를 비교해서 볼, 스트라이크의 개수를 카운트하는 메서드
+	 * @param inputNumber
+	 * @return int[]
+	 */
+	public int[] compareNumber(String inputNumber) {
+		// TODO: 컴퓨터가 입력한 수와 사용자가 입력한 수를 비교해서 볼, 스트라이크의 개수를 카운트하는 메서드
+		int[] tmp = {0, 3};
+		return tmp;
+	}
+
+	/**
+	 * 카운트한 결과를 출력하는 메서드
+	 * @param result
+	 */
+	public void printResult(int[] result) {
+		// TODO: 카운트한 결과를 출력하는 메서드
+	}
+
+	/**
+	 * 정답을 맞췄을 때 게임 종료여부를 묻는 입력을 받는 메서드
+	 * @return int
+	 */
+	public int askToRestart() {
+		// TODO: 정답을 맞췄을 때 게임 종료여부를 묻는 입력을 받는 기능
+		return 2;
+	}
+
+	/**
+	 * 게임을 시작해서 스트라이크3이 나올때까지 계속 입력을 받는 기능
+	 * @return int
+	 */
+	public int startGame() {
+		while (true) {
+			System.out.println("숫자를 입력해주세요 : ");
+			String inputNumber = camp.nextstep.edu.missionutils.Console.readLine();
+			InputChecker.checkStringIsThreeNumbers(inputNumber);
+			int[] result = compareNumber(inputNumber);
+			printResult(result);
+
+			if (result[1] == 3) {
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+					+ "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+				return askToRestart();
+			}
+		}
+	}
+
 }
---
+++ src/main/java/baseball/InputChecker.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class InputChecker {
+	/**
+	 * 특정 문자열이 3자리 수인지 확인하는 메서드
+	 * @param inputNumber
+	 * @return boolean
+	 */
+	public static boolean checkStringIsThreeNumbers(String inputNumber) {
+		// TODO: 특정 문자열이 3자리 수인지 확인하는 기능
+		return true;
+	}
+}
