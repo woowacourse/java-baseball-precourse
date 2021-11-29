be537278fa881b23f46dcdeb11d2e7ea4006784a
summerlunaa <rhgus1085@gmail.com>
2021-11-29 15:34:27 +0900

    feat(Judge): 입력값이 볼인지 스트라이크인지 판단하는 기능 구현

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,10 +1,21 @@
 package baseball;
 
 public class Judge {
-    public static boolean IsBall(){
-
+    public static boolean IsBall(String rightAnswer, char num, int idx){
+        for (int i=0; i<rightAnswer.length(); i++) {
+            if (i == idx) {
+                continue;
+            }
+            if (rightAnswer.charAt(idx) == num) {
+                return true;
+            }
+        }
+        return false;
     }
-    public static boolean IsStrike(){
-
+    public static boolean IsStrike(String rightAnswer, char num, int idx){
+        if (rightAnswer.charAt(idx) == num) {
+            return true;
+        }
+        return false;
     }
 }
