bdc8d0d0f5651a0a3f2fa0b257915ac88d30d3d1
grace_goose <choieungi@gm.gist.ac.kr>
2021-11-29 22:48:13 +0900

    feat: 게임을 재시작할지 정하는 역할 구현

--- src/main/java/baseball/OutputRole.java
+++ src/main/java/baseball/OutputRole.java
@@ -8,6 +8,7 @@ public class OutputRole {
     private static final String STRIKE = "스트라이크 ";
     private static final String BALL = "볼 ";
     private static final String SUCCESS_MISSIOLN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료 ";
+    private static final String RESTART_INSTRUCTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 
     public boolean printResult(int strikeNumber, int ballNumber) {
@@ -24,6 +25,7 @@ public class OutputRole {
         System.out.println();
         if (strikeNumber == 3) {
             System.out.println(SUCCESS_MISSIOLN);
+            allowRestart();
         }
         return false;
 
@@ -35,5 +37,17 @@ public class OutputRole {
     }
 
 
+    private boolean allowRestart(){
+        System.out.println(RESTART_INSTRUCTION);
+        String input = Console.readLine();
+        if (input.equals("1")) {
+            return true;
+        }
+        if (input.equals("2")) {
+            return false;
+        }
 
+        throw new IllegalArgumentException("잘못된 값을 입력했습니다. 1 또는 2를 입력해주세요.");
+
+    }
 }
