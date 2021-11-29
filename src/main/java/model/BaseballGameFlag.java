7841dff744f6951e193bda975dd966df722adbe0
koo <gudonghee2000@gmail.com>
2021-11-29 23:34:04 +0900

    refactor : BaseballGameFlag에 매직넘버를 설정

--- src/main/java/model/BaseballGameFlag.java
+++ src/main/java/model/BaseballGameFlag.java
@@ -4,14 +4,16 @@ import java.util.regex.Pattern;
 
 public class BaseballGameFlag {
 	private static final String NUMBER_PATTERN = "^[1-2]";
+	private static final int NUMBER_SIZE = 1;
+	private static final int START_GAME = 1;
 	private int gameFlag;
 
 	public BaseballGameFlag() {
-		gameFlag = 1;
+		gameFlag = START_GAME;
 	}
 
 	public boolean isStartGameFlag() {
-		if (gameFlag == 1) {
+		if (gameFlag == START_GAME) {
 			return true;
 		}
 		return false;
@@ -27,7 +29,7 @@ public class BaseballGameFlag {
 			throw new IllegalArgumentException("입력한 숫자중 잘못된 숫자(문자, 음수 등)이 있습니다.");
 		}
 
-		if (number.length() != 1) {
+		if (number.length() != NUMBER_SIZE) {
 			throw new IllegalArgumentException("입력한 숫자가 한자리가 아닙니다.");
 		}
 	}
