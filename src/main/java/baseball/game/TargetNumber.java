c4e5e97b7cd4c8af1d4ef96c19168173d318f314
monk <2012.monk@gmail.com>
2021-11-29 16:28:16 +0900

    refactor: TargetNumber 검증로직 분리

--- src/main/java/baseball/game/TargetNumber.java
+++ src/main/java/baseball/game/TargetNumber.java
@@ -1,7 +1,6 @@
 package baseball.game;
 
-import baseball.NumbersConstraints;
-import java.util.HashSet;
+import baseball.utils.GameValidator;
 import java.util.List;
 
 public class TargetNumber {
@@ -9,22 +8,10 @@ public class TargetNumber {
     private final List<Integer> numbers;
 
     public TargetNumber(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplicate(numbers);
+        GameValidator.validateRange(numbers);
+        GameValidator.validateSize(numbers);
+        GameValidator.validateDuplicate(numbers);
         this.numbers = numbers;
-    }
-
-    public void validateSize(List<Integer> numbers) throws IllegalArgumentException {
-        if (numbers.size() != NumbersConstraints.NUMBER_LENGTH.value()) {
-            throw new IllegalArgumentException("wrong size");
-        }
-    }
-
-    public void validateDuplicate(List<Integer> numbers) throws IllegalArgumentException {
-        HashSet<Integer> set = new HashSet<>(numbers);
-        if (set.size() != numbers.size()) {
-            throw new IllegalArgumentException("number is duplicated");
-        }
     }
 
     public boolean isStrike(int position, int number) {
