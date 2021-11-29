84f9efcf181d6dfe3a328a12c22bf61040eeaba1
eonju97 <eonju97.dev@gmail.com>
2021-11-30 08:36:29 +0900

    feat: 결과 만드는 기능 추가

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,47 @@
+package baseball;
+
+public class Result {
+
+    private final String randomNumber;
+    private String inputNumber;
+    private int ball;
+    private int strike;
+
+    public Result(String randomNumber) {
+        this.randomNumber = randomNumber;
+        this.ball = 0;
+        this.strike = 0;
+    }
+
+    public void makeResult(String input) {
+        this.inputNumber = input;
+        countBall();
+        countStrike();
+    }
+
+    private void countBall() {
+        String[] inputNumbers = inputNumber.split("");
+        for (int i = 0; i < inputNumbers.length; i++) {
+            String target = inputNumbers[i];
+            if (randomNumber.contains(target) && randomNumber.indexOf(target) != i) {
+                ball++;
+            }
+        }
+    }
+
+    private void countStrike() {
+        for (int i = 0; i < inputNumber.length(); i++) {
+            if (inputNumber.charAt(i) == randomNumber.charAt(i)) {
+                this.strike++;
+            }
+        }
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+}
