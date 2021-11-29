91f808fd10a928829e58a2a545b03e8c129e2549
overnew <ehdls1303@gmail.com>
2021-11-29 21:46:59 +0900

    fix:  서로 같은 난수가 나오는 오류 수정
    Opponent class의 필드값 numbers에 같은 난수가 들어가는 오류
     - 중복된 값은 numbers에 추가하지 않고 다시 난수 생성
     - Randoms.pickNumberInRange()는 같은 임의의 수를 생성할 수 있음을
       간과하여 발생

--- src/main/java/baseball/gamesource/Opponent.java
+++ src/main/java/baseball/gamesource/Opponent.java
@@ -8,12 +8,17 @@ import baseball.constants.ValueConstants;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Opponent {
-    private List<Integer> numbers = new ArrayList<>();
+    private List<Integer> numbers= new ArrayList<>();
 
-    public Opponent(){  //생성자에서 랜덤 수 생성
+    public Opponent(){  //생성자에서 서로 다른 세가지 난수 생성
 
         for(int i=0; i< ValueConstants.GAME_NUMBER_LEN ; ++i){
-            int randomNum = Randoms.pickNumberInRange(ValueConstants.GAME_NUMBER_BOUND_START,ValueConstants.GAME_NUMBER_BOUND_END);
+            int randomNum;
+
+            do{ // 중복 값 배제
+                randomNum = Randoms.pickNumberInRange(ValueConstants.GAME_NUMBER_BOUND_START,ValueConstants.GAME_NUMBER_BOUND_END);
+            }while(numbers.contains(randomNum));
+
             numbers.add(randomNum);
         }
 
