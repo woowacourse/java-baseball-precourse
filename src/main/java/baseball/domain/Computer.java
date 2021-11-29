bd3fd83d87ab49d4f897e066e629f51b39fee6fc
xi_jjun <rlawowns97@naver.com>
2021-11-29 20:46:32 +0900

    !hotfix: ApplicationTest 통과 안되는 버그 수정
    Computer 객체에서 처음에 2번의 난수를 발생시켰기에 테스트에서 원하는대로 진행이 안됨
    따라서 처음 1번만 난수를 발생시키게 만듦
    Computer의 instance 변수를 null로 선언하여 첫 생성시에 1번의 난수만 발생시키게 만듦
    
    난수발생시키는 클래스를 지운뒤, Computer 클래스의 메소드로 병합
    -> generateRandomAnswer()

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,28 +1,38 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
-    private static RandomGenerator randomGenerator;
-    private static Computer instance = new Computer();
-    private static int[] answer;
+    private static Computer instance;
+    private static List<Integer> answer = new ArrayList<>();
     private final int BALL = 0;
     private final int STRIKE = 1;
 
     private Computer() {
-        randomGenerator = RandomGenerator.getInstance();
-        answer = randomGenerator.getAnswer();
+        generateRandomAnswer();
     }
 
     public static Computer getInstance() {
         if (instance == null) {
             return new Computer();
         }
-        getNewAnswer();
+        generateRandomAnswer();
         return instance;
     }
 
-    private static void getNewAnswer() {
-        randomGenerator.reGenerate();
-        answer = randomGenerator.getAnswer();
+    private static void generateRandomAnswer() {
+        answer = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (answer.contains(number)) {
+                i--;
+                continue;
+            }
+            answer.add(number);
+        }
     }
 
     public int[] generateHint(int[] playerNumber) {
@@ -38,13 +48,11 @@ public class Computer {
 
     private int countBall(int[] playerNumber) {
         int count = 0;
-        if (playerNumber[0] == answer[1] || playerNumber[0] == answer[2]) {
+        if (playerNumber[0] == answer.get(1) || playerNumber[0] == answer.get(2)) {
             count++;
-        }
-        if (playerNumber[1] == answer[0] || playerNumber[1] == answer[2]) {
+        } else if (playerNumber[1] == answer.get(0) || playerNumber[1] == answer.get(2)) {
             count++;
-        }
-        if (playerNumber[2] == answer[0] || playerNumber[2] == answer[1]) {
+        } else if (playerNumber[2] == answer.get(0) || playerNumber[2] == answer.get(1)) {
             count++;
         }
 
@@ -54,7 +62,7 @@ public class Computer {
     private int countStrike(int[] playerNumber) {
         int count = 0;
         for (int i = 0; i < 3; i++) {
-            if (playerNumber[i] == answer[i]) {
+            if (playerNumber[i] == answer.get(i)) {
                 count++;
             }
         }
