6827a4291fb362005de67d3bde570d8739b30f87
Byeongju-Kong <bjuuuu98@gmail.com>
2021-11-29 19:42:48 +0900

    style : 대괄호 뒤에 공백 삽입

--- src/test/java/model/NumberValidatorTest.java
+++ src/test/java/model/NumberValidatorTest.java
@@ -12,7 +12,7 @@ class NumberValidatorTest {
     @Test
     @DisplayName("정수가 아닌 값이 있으면 예외를 발생시킨다.")
     void evokeExceptionByNonInteger() {
-        String[] wrongInputTokens = new String[]{"1", "?", "3"};
+        String[] wrongInputTokens = new String[] {"1", "?", "3"};
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> NumberValidator.getValidatedNumbers(wrongInputTokens))
                 .withMessage("정수만 입력하셔야합니다.");
@@ -21,7 +21,7 @@ class NumberValidatorTest {
     @Test
     @DisplayName("문자열 배열로 입력된 값을 정수 유효성 검사 후, 정수 타입의 List로 반환한다.")
     void getValidatedNumbers() {
-        String[] inputTokens = new String[]{"1", "2", "3"};
+        String[] inputTokens = new String[] {"1", "2", "3"};
         List<Integer> actual = NumberValidator.getValidatedNumbers(inputTokens);
         List<Integer> expected = Arrays.asList(1, 2, 3);
         assertThat(actual).isEqualTo(expected);
