48019d52b89830e14192fb70be36004221d22abd
hyunrrr <whgusrms96@gmail.com>
2021-11-29 17:10:10 +0900

    feat(Player): Player에게 질문하고 답 리턴하는 기능 추가

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Player {
+
+	public String askQuestionAndGetAnswer(String question) {
+
+		System.out.println(question);
+		return camp.nextstep.edu.missionutils.Console.readLine();
+	}
+}
