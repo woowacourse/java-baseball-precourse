e872371c088a1a24e1d14d3653eb8dd75cafe69c
is2js <tingstyle1@gmail.com>
2021-11-29 22:46:17 +0900

    refactor(RefreeTest): 단위테스트 메소드명 변경 및 반복되는 코드 처리함 + 테스트 케이스 추가

--- src/test/java/baseball/domain/RefreeTest.java
+++ src/test/java/baseball/domain/RefreeTest.java
@@ -15,6 +15,7 @@ import baseball.utils.Constant;
 class RefreeTest {
 	public static final String RESULT_2BALL_1STRIKE = "2볼 1스트라이크";
 	Refree refree;
+	List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
 
 	@BeforeEach
 	void setUp() {
@@ -22,8 +23,7 @@ class RefreeTest {
 	}
 
 	@Test
-	void 쓰리스트라이크_테스트() {
-		List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
+	void compare_쓰리스트라이크_테스트() {
 		List<Integer> playerNumbers = Arrays.asList(1, 2, 3);
 
 		String result = refree.compare(computerNumbers, playerNumbers);
@@ -32,8 +32,7 @@ class RefreeTest {
 	}
 
 	@Test
-	void 낫싱_테스트() {
-		List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
+	void compare_낫싱_테스트() {
 		List<Integer> playerNumbers = Arrays.asList(4, 5, 7);
 
 		String result = refree.compare(computerNumbers, playerNumbers);
@@ -42,8 +41,7 @@ class RefreeTest {
 	}
 
 	@Test
-	void 볼2_스트라이크1_테스트() {
-		List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
+	void compare_볼2_스트라이크1_테스트() {
 		List<Integer> playerNumbers = Arrays.asList(1, 3, 2);
 
 		String result = refree.compare(computerNumbers, playerNumbers);
@@ -52,9 +50,10 @@ class RefreeTest {
 	}
 
 	@ParameterizedTest
-	@CsvSource({"1,3,4,1볼 1스트라이크,3,2,1,3볼"})
+	@CsvSource({"1,3,4,1볼 1스트라이크", "3,2,1,2볼", "4,5,6,낫싱"})
 	public void compare_여러테스트(int number1, int number2, int number3, String expected) {
-		String actual = refree.compare(Arrays.asList(1, 2, 3), Arrays.asList(number1, number2, number3));
-		assertThat(expected).isEqualTo(actual);
+		String actual = refree.compare(computerNumbers, Arrays.asList(number1, number2, number3));
+
+		assertThat(actual).isEqualTo(expected);
 	}
 }
