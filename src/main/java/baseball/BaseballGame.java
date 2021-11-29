59f22fb9bd622c85eb5213b0fd7733c00f845916
hyunrrr <whgusrms96@gmail.com>
2021-11-29 19:11:49 +0900

    feat(BaseballGame): 야구게임 정답 포맷 규칙 확인하는 기능 추가

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import java.util.Arrays;
+
+public class BaseballGame {
+
+	private boolean checkLengthIsThree(String answer) {
+
+		return answer.length() == BaseballGameConstants.LENGTH_RULE;
+	}
+
+	private boolean checkConsistOfOneToNine(String answer) {
+
+		for (int index = 0; index < BaseballGameConstants.LENGTH_RULE; ++index) {
+			char charAtIndex = answer.charAt(index);
+			if (charAtIndex < '1' || charAtIndex > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private int convertCharToInt(char targetChar) {
+
+		return targetChar - '0';
+	}
+
+	private boolean checkNoOverlapCharacter(String answer) {
+
+		boolean isNumUsed[] = new boolean[10];
+		Arrays.fill(isNumUsed, false);
+
+		for (int index = 0; index < BaseballGameConstants.LENGTH_RULE; ++index) {
+			int intAtIndex = convertCharToInt(answer.charAt(index));
+			if (isNumUsed[intAtIndex]) {
+				return false;
+			}
+			isNumUsed[intAtIndex] = true;
+		}
+		return true;
+	}
+
+	private boolean checkBaseballGameAnswerFormatRules(String answer) {
+
+		return checkLengthIsThree(answer)
+			&& checkConsistOfOneToNine(answer)
+			&& checkNoOverlapCharacter(answer);
+	}
+}
---
+++ src/main/java/baseball/BaseballGameConstants.java
@@ -0,0 +1,6 @@
+package baseball;
+
+public class BaseballGameConstants {
+
+	final static int LENGTH_RULE = 3;
+}
