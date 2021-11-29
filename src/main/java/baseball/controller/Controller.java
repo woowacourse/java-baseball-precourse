01a7b8bfdef28081c5476668fff9e3fdeb044b29
is2js <tingstyle1@gmail.com>
2021-11-29 13:06:37 +0900

    feat(domain): 1에서 9까지 서로 다른 임의의 수 3개(플레이어 수)를 입력받기 구현
    플레이어 수를 검증하고 변환하는 기능 구현
     - 4가지 검증(길이, 포맷, 범위, 중복) 메서드 기능 구현
     - 검증후 String -> Integer 타입으로 변환한 list 반환하는 기능 구현

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -3,15 +3,23 @@ package baseball.controller;
 import java.util.List;
 
 import baseball.domain.Computer;
+import baseball.domain.Refree;
 import baseball.view.InputView;
 
 public class Controller {
+
+	public static final String ON_GOING = "1";
+	public static final String STOP = "2";
+	String status = ON_GOING;
+
 	public void runGame() {
 		Computer computer = new Computer();
 		List<Integer> computerNumbers = computer.createRandomNumbers();
-		System.out.println(computerNumbers);
-		// while (true) {
-		// 	String input = InputView.getInput();
-		// }
+		while (!status.equals(STOP)) {
+			Refree refree = new Refree();
+			List<Integer> playerNumbers = refree.askNumbers(InputView.getInput());
+			System.out.println(playerNumbers);
+		}
+
 	}
 }
--- src/main/java/baseball/domain/Refree.java
+++ src/main/java/baseball/domain/Refree.java
@@ -1,21 +1,73 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Refree {
-	public void askNumbers(){
+	public List<Integer> askNumbers(String stringNumbers) {
+		return checkValidationAndConvertType(stringNumbers);
+	}
+
+	public List<Integer> checkValidationAndConvertType(String stringNumbers) {
+		// check -> is ? true 반환 하다가...  format같은 경우에는 체크와 동시에 변환하도록 하자.
+		// format은 .charAt(idx) -> Character.isDigit() 로 발견시 return 후 thr 하면되는데,,, 차라리 확인과 동시에 변환까지 시키자.
+		//  void thr던지는 check메서드() -> 내부 if !(맞는조건)시 -> thr 패턴 + 맞는 조건도 t/f의 is~메서드로 구현
+		checkLength(stringNumbers);
+		List<Integer> integerNumbers = checkFormatAndConvert(stringNumbers);
+		checkRange(integerNumbers);
+		checkDuplicated(integerNumbers);
+
+		return integerNumbers;
 
 	}
 
-	public List<Integer> validAndConvertIntegerNumbers(String stringNumbers) {
-		return null;
+	// 중복확인은 원본길이와 set한 길이를 이용하는데, list-> stream을 이용해서 편하게 한다.
+	private void checkDuplicated(List<Integer> integerNumbers) {
+		if (isDuplicated(integerNumbers)) {
+			throw new IllegalArgumentException("같은 수를 2번이상 입력하였습니다.");
+		}
+	}
+
+	private boolean isDuplicated(List<Integer> integerNumbers) {
+		return integerNumbers.stream().distinct().count() != integerNumbers.size();
+	}
+
+	private List<Integer> checkFormatAndConvert(String stringNumbers) {
+		List<Integer> integerNumbers = new ArrayList<>();
+		for (String stringNumber : stringNumbers.split("")) {
+			try {
+				integerNumbers.add(Integer.valueOf(stringNumber));
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException("숫자를 입력하세요.");
+			}
+		}
+		return integerNumbers;
+	}
+
+	private void checkLength(String stringNumbers) {
+		if (!isValidLength(stringNumbers)) {
+			throw new IllegalArgumentException("숫자 3개를 입력해주세요.");
+		}
+	}
+
+	private boolean isValidLength(String stringNumbers) {
+		return stringNumbers.length() == 3;
+	}
+
+	private void checkRange(List<Integer> integerNumbers) {
+		for (Integer number : integerNumbers) {
+			if (!(1 <= number && number <= 9)) {
+				throw new IllegalArgumentException("1~9사이의 값을 입력해주세요.");
+			}
+		}
+
 	}
 
 	public String compare(List<Integer> computerNumbers, List<Integer> playerNumbers) {
 		return "";
 	}
 
-	public Integer countSameNumber(List<Integer> computerNumbers, List<Integer> playerNumbers){
+	public Integer countSameNumber(List<Integer> computerNumbers, List<Integer> playerNumbers) {
 		return 0;
 	}
 
