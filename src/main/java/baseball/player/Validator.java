0f3bbc187d8e0e69aacf2f9d32cbe4369a8598f5
bomi <bonita9931@gmail.com>
2021-11-30 06:54:52 +0900

    feat - 사용자 입력 유효성 검사 기능 추가

---
+++ src/main/java/baseball/player/Validator.java
@@ -0,0 +1,23 @@
+package baseball.player;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+	public static boolean validateUserAnswer(String userAnswer) {
+		String regExp = "^[1-9]{3}$";
+		String removeDistinct = Arrays.stream(userAnswer.split("")).distinct().toString();
+
+		if (!Pattern.matches(regExp, userAnswer)) {
+			throw new IllegalArgumentException("잘못된 사용자 입력입니다!");
+		}
+
+		if (removeDistinct.length() != userAnswer.length()) {
+			throw  new IllegalArgumentException("잘못된 사용자 입력입니다!");
+		}
+
+		return true;
+	}
+}
