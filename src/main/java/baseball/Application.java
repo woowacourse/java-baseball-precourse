eda167458fa71f031304459ff06c4793d1054cf1
eonju97 <eonju97.dev@gmail.com>
2021-11-30 08:52:28 +0900

    feat: 결과 출력 추가 및 상수 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,19 +3,45 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    private final static int DIGIT = 3;
+    private final static int CORRECT_NOTING = 0;
+
     public static void main(String[] args) {
 
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
         Printer printer = new Printer();
         Validator validator = new Validator();
 
-        String randomNumber = String.valueOf(randomNumberGenerator.makeRandomNumber(3));
+        String randomNumber = String.valueOf(randomNumberGenerator.makeRandomNumber(DIGIT));
+        System.out.println(randomNumber);
+        Result result = new Result(randomNumber);
 
         while (true) {
             printer.printNumberInputMessage();
             String input = Console.readLine();
             validator.validateInputNumber(input);
 
+            result.makeResult(input);
+            int ball = result.getBall();
+            int strike = result.getStrike();
+
+            if (ball == CORRECT_NOTING && strike == CORRECT_NOTING) {
+                printer.printResultNoting();
+            }
+
+            if (ball != CORRECT_NOTING) {
+                printer.printResultBall(ball);
+            }
+
+            if (strike != CORRECT_NOTING) {
+                printer.printResultStrike(strike);
+            }
+
+            if (strike == DIGIT) {
+                printer.printResultGameEnd();
+                break;
+            }
 
         }
 
