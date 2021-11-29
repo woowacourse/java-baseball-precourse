04262e72ae1ed4cf473c20459230ecebbdea23c5
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-11-30 01:36:59 +0900

    feat: 게임 결과가 정답인 경우 게임을 계속할지 종료시킬지 입력

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -6,9 +6,20 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	private static final String ENTER_THE_PREDICTION = "숫자를 입력해주세요 : ";
+	private static final String ENTER_THE_QUIT_OR_NOT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public List<Integer> getPredictionNumber() {
 		System.out.print(ENTER_THE_PREDICTION);
 		return InputValidation.validatePrediction(Console.readLine());
+	}
+
+	public boolean getQuitOrNot() {
+		System.out.println(ENTER_THE_QUIT_OR_NOT);
+		String quitOrNot = Console.readLine();
+		if (quitOrNot.equals("1")) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
