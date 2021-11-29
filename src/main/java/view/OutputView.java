1bcb587d02dcf473ee177a355c98a03737dc65a1
devHudi <devhudi@gmail.com>
2021-11-30 01:46:11 +0900

    refactor: 상수 이름을 의미가 명확하게 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,7 +4,7 @@ import domain.PredictionResult;
 
 public class OutputView {
 	private static final String INPUT_INSTRUCTION = "숫자를 입력해주세요 : ";
-	private static final String QUIT_INPUT_INSTRUCTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private static final String RESTART_OR_QUIT_INPUT_INSTRUCTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 	private static final String CONGRATULATIONS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	private static final String BALL = "볼";
 	private static final String STRIKE = "스트라이크";
@@ -15,7 +15,7 @@ public class OutputView {
 	}
 
 	public static void printQuitInstruction() {
-		System.out.println(OutputView.QUIT_INPUT_INSTRUCTION);
+		System.out.println(OutputView.RESTART_OR_QUIT_INPUT_INSTRUCTION);
 	}
 
 	public static void printHint(PredictionResult predictionResult) {
