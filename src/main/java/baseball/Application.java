19c04fdc967c41cb5225f1a26848c30708b9483d
overnew <ehdls1303@gmail.com>
2021-11-29 23:03:08 +0900

    Refactor: Application.main() 리펙토링
    주석 추가
    
    문자열의 null 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,19 +4,26 @@ import baseball.constants.MessageConstants;
 import baseball.gamesource.BaseBallGame;
 import baseball.util.InputCheck;
 
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input;
+        String input = null;
+
         do{
             BaseBallGame newGame = new BaseBallGame();
+
+            //게임 시작
             newGame.gameStart();
+
+            //게임 종료
             System.out.println(MessageConstants.RESTART_OR_END_MESSAGE);
             input = Console.readLine();
+
+            //입력 확인
             InputCheck.checkOptionNumber(input);
 
         }while(input.equals("1"));
+
     }
 }
