26a3b2662030caad98a44d2cffa3beb85a6f416a
minsulee <alkwen0996@naver.com>
2021-11-29 21:36:22 +0900

    refactor: 출력기능 GameDisplay 클래스 작성

--- src/main/java/baseball/view/GameDisplay.java
+++ src/main/java/baseball/view/GameDisplay.java
@@ -1,2 +1,23 @@
-package baseball.view;public class GameDisplay {
+package baseball.view;
+
+import baseball.domain.Messages;
+
+public class GameDisplay {
+    private static final String NEW_LINE = "\n";
+
+    public static void printRestartOrNotMessage() {
+        System.out.println(Messages.RESTART_OR_NOT_MESSAGE.printMessages());
+    }
+
+    public static void printCorrectAnswerMessage() {
+        System.out.println(Messages.THREE_STRIKE_MESSAGE.printMessages() + NEW_LINE + Messages.COLLECT_ANSWER_MESSAGE.printMessages());
+    }
+
+    public static void printInputMessage() {
+        System.out.print(Messages.INPUT_NUMBER_MESSAGE.printMessages());
+    }
+
+    public static void printHintMessage(final String hintMessage) {
+        System.out.println(hintMessage);
+    }
 }
