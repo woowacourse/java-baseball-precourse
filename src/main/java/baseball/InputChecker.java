fac9d0ad2d9fa0bee940596f289f4bfb04f66b16
HYUNAHSHIM <hyunahshim@naver.com>
2021-11-30 00:36:40 +0900

    feat: 특정 문자열이 3자리 수인지 확인하는 기능

--- src/main/java/baseball/InputChecker.java
+++ src/main/java/baseball/InputChecker.java
@@ -4,10 +4,18 @@ public class InputChecker {
 	/**
 	 * 특정 문자열이 3자리 수인지 확인하는 메서드
 	 * @param inputNumber
-	 * @return boolean
 	 */
-	public static boolean checkStringIsThreeNumbers(String inputNumber) {
-		// TODO: 특정 문자열이 3자리 수인지 확인하는 기능
-		return true;
+	public static void checkStringIsThreeNumbers(String inputNumber) {
+		// inputNumber가 3자리인지 확인
+		if (inputNumber.length() != 3) {
+			throw new IllegalArgumentException("3자리 수가 아닙니다.");
+		}
+
+		// inputNumber가 숫자로 이루어진 문자열인지 확인
+		for (int i = 0; i < inputNumber.length(); i++) {
+			if (!Character.isDigit(inputNumber.charAt(i))) {
+				throw new IllegalArgumentException("숫자가 아닙니다.");
+			}
+		}
 	}
 }
