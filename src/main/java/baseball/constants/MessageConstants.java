d6111ed3ccd94a3adcebb21285685769bc703e8d
overnew <ehdls1303@gmail.com>
2021-11-29 23:14:22 +0900

    Refactor: MessgaeConstants 상수 문자열 및 주석 추가
    GAME_RESTART_NUM 추가
     - 게임 재시작시 확인하는 문자를 상수화
    
    분류를 위해 주석 추가

--- src/main/java/baseball/constants/MessageConstants.java
+++ src/main/java/baseball/constants/MessageConstants.java
@@ -1,14 +1,19 @@
 package baseball.constants;
 
 public final class MessageConstants {
-    //게임 플레이
-    public static final String NUMBER_INPUT_REQUEST_MESSAGE= "숫자를 입력해주세요 : ";
+    //게임 플레이 안내
     public static final String GAME_END_MESSAGE = "게임 종료";
     public static final String BALL_MESSAGE = "볼 ";
     public static final String STRIKE_MESSAGE = "스트라이크 ";
     public static final String CORRECT_NUMBER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! ";
     public static final String MISS_ALL_NUMBER_MESSAGE = "낫싱";
+
+    //입력 요청 메시지
+    public static final String NUMBER_INPUT_REQUEST_MESSAGE= "숫자를 입력해주세요 : ";
     public static final String RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    //게임 재시작 문자
+    public static final String GAME_RESTART_NUM= "1";
 
     //예외 처리
     public static final String DIFF_NUMBER_INPUT_MESSAGE = "3자리의 숫자를 입력해 주세요.";
