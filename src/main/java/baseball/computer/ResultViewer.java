8546cf20e3e2bef8aafdeb642e44d3f709ba8135
bomi <bonita9931@gmail.com>
2021-11-30 07:08:39 +0900

    feat - 볼/스트라이크 개수에 따라 문자열 보여주는 기능 추가

---
+++ src/main/java/baseball/computer/ResultViewer.java
@@ -0,0 +1,17 @@
+package baseball.computer;
+
+public class ResultViewer {
+
+	public String showResult(int strikeCnt, int ballCnt) {
+		if (ballCnt == 0 && strikeCnt == 0) {
+			return "낫싱";
+		}
+		if (strikeCnt == 0) {
+			return ballCnt + "볼";
+		}
+		if (ballCnt == 0) {
+			return strikeCnt + "스트라이크";
+		}
+		return ballCnt + "볼" + strikeCnt + "스트라이크";
+	}
+}
