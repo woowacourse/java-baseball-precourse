87df86b6c26470227e365e670ecaaf8e94e4a547
Kim Seunglae <obey1342@gmail.com>
2021-11-30 03:31:49 +0900

    style: 포맷팅 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,9 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        Player player = new Player();
-        BaseBallController game = new BaseBallController(player);
-        game.start();
-    }
+	public static void main(String[] args) {
+		Player player = new Player();
+		BaseBallController game = new BaseBallController(player);
+		game.start();
+	}
 }
