570fd0415a98856478f044ca89009332a7aba6f2
June <tkdwns159@naver.com>
2021-11-29 17:58:57 +0900

    feat: 상대 숫자 랜덤 배정
    상대의 숫자를 랜덤하게 배정하는 기능 추가.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,39 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static class Game {
+		public List<Integer> answer;
+
+		Game() {
+			startGame();
+		}
+
+		private boolean checkIsDuplicated(int num) {
+			for (Integer i : answer) {
+				if (i == num) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private void startGame() {
+			answer = new ArrayList<>();
+			for (int i = 0; i < 3; i++) {
+				int randomNum = Randoms.pickNumberInRange(1, 9);
+				while (checkIsDuplicated(randomNum)) {
+					randomNum = Randoms.pickNumberInRange(1, 9);
+				}
+				answer.add(randomNum);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+	}
 }
