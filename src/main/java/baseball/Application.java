85e4cc3cb6f84193e2232ff6c865ae0c780802c5
woojaang <woojaang@naver.com>
2021-11-30 02:28:40 +0900

    정답 숫자 만들기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,39 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    String answer = "";
+
+    public void makeAnswer() {
+        this.answer = "";
+        int[] answerArray = new int[9];
+        int temp;
+        int randomNumber;
+        for (int i=0; i<9; i++) {
+            answerArray[i] = i+1;
+        }
+        for (int i=0; i<3; i++) {
+            randomNumber = Randoms.pickNumberInRange(0,8);
+            temp = answerArray[i];
+            answerArray[i] = answerArray[randomNumber];
+            answerArray[randomNumber] = temp;
+        }
+        for (int i=0; i<3; i++) {
+            this.answer += answerArray[i];
+        }
+    }
+
     public static void main(String[] args) {
+        /*
         System.out.println("test");
-        int a = Randoms.pickNumberInRange(1,5);
-        System.out.println(a);
+        Application test = new Application();
+        for (int i=0; i<10; i++) {
+            test.makeAnswer();
+            System.out.println(test.answer);
+        }
+         */
     }
 }
