7bcc8ad049bf8adaa8a620426d2609c2e834a9bd
RIANAEH <ririhan217@gmail.com>
2021-11-30 00:48:41 +0900

    refactor(resultView): declare constants
    상수를 따로 선언하여 사용하는 방식으로 변경하였다.

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -2,21 +2,26 @@ package baseball.view;
 
 public class ResultView {
 
-	public void printEndMessage() {
-		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+	static final String CORRECT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	static final String BALL_MESSAGE = "볼";
+	static final String STRIKE_MESSAGE = "스트라이크";
+	static final String NOTHING_MESSAGE = "낫싱";
+
+	public void printCorrectMessage() {
+		System.out.println(CORRECT_MESSAGE);
 	}
 
 	public void printResult(int ball, int strike) {
 		StringBuilder stringBuilder = new StringBuilder();
 
 		if (ball > 0) {
-			stringBuilder.append(ball + "볼 ");
+			stringBuilder.append(ball).append(BALL_MESSAGE).append(" ");
 		}
 		if (strike > 0) {
-			stringBuilder.append(strike + "스트라이크");
+			stringBuilder.append(strike).append(STRIKE_MESSAGE);
 		}
 		if (ball == 0 && strike == 0) {
-			stringBuilder.append("낫싱");
+			stringBuilder.append(NOTHING_MESSAGE);
 		}
 		System.out.println(stringBuilder.toString().trim());
 	}
