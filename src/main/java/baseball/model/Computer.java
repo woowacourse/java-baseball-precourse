5e1b6ddb506fbba470ad30814ec1c872b9d8fa56
YJeongKim <yjeongisme@gmail.com>
2021-11-29 20:35:54 +0900

    feat(Computer): 랜덤 숫자 생성 기능 구현

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,7 +1,12 @@
 package baseball.model;
 
+import baseball.util.AnswerRange;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
     private int[] answer;
+    private boolean[] duplicateCheck;
 
     public Computer() {
     }
@@ -11,6 +16,31 @@ public class Computer {
     }
 
     public void createAnswer() {
-        // ToDo: 랜덤 수 생성
+        initAnswer();
+        initDuplicateCheck();
+
+        for (int i = 0; i < AnswerRange.DIGIT; i++) {
+            int randomNumber = pickRandomNumber();
+            answer[i] = randomNumber;
+            duplicateCheck[randomNumber] = true;
+        }
+    }
+
+    private void initAnswer() {
+        answer = new int[AnswerRange.DIGIT];
+    }
+
+    private void initDuplicateCheck() {
+        duplicateCheck = new boolean[AnswerRange.MAX + 1];
+    }
+
+    private int pickRandomNumber() {
+        int randomNumber;
+
+        do {
+            randomNumber = Randoms.pickNumberInRange(AnswerRange.MIN, AnswerRange.MAX);
+        } while (duplicateCheck[randomNumber]);
+
+        return randomNumber;
     }
 }
---
+++ src/main/java/baseball/util/AnswerRange.java
@@ -0,0 +1,7 @@
+package baseball.util;
+
+public class AnswerRange {
+    public static final int MIN = 1;
+    public static final int MAX = 9;
+    public static final int DIGIT = 3;
+}
