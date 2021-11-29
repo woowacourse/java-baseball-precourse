7f10d63158bb3ef5f9358cac4efe53e8e918e93d
grace_goose <choieungi@gm.gist.ac.kr>
2021-11-29 22:18:13 +0900

    feat: 스트라이크와 볼 개수 계산 기능 구현

--- src/main/java/baseball/GameRole.java
+++ src/main/java/baseball/GameRole.java
@@ -9,8 +9,7 @@ public class GameRole {
     public static void startGame() {
         while (true) {
             InputRole inputRole = new InputRole();
-            String a = inputRole.getInputValue();
-            System.out.println("getRandomNumber() = " + getRandomNumber());
+            checkGameResult(inputRole.getInputValue());
             return;
         }
     }
@@ -18,6 +17,28 @@ public class GameRole {
     public static String getRandomNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         return numbers.toString().replaceAll("[^0-9]", "");
+    }
+
+    private static void checkGameResult(String input) {
+        int ball = 0;
+        int strike = 0;
+        String computerNumber = getRandomNumber();
+
+        for (int i = 0; i < input.length(); i++) {
+            char number = computerNumber.charAt(i);
+            if (number == input.charAt(i)) {
+                strike++;
+                continue;
+            }
+            if (input.contains(String.valueOf(number))){
+                ball++;
+            }
+        }
+        System.out.println("computerNumber = " + computerNumber);
+        System.out.println("input = " + input);
+        System.out.println("strike = " + strike);
+        System.out.println("ball = " + ball);
+
     }
 
 
