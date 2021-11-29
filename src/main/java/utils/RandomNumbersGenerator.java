0d98299700641bf98f5a4a68eb155e7e3eea04d2
koo <gudonghee2000@gmail.com>
2021-11-29 23:39:30 +0900

    refactor : RandomNumbersGenerator 매직넘버 이름 변경

--- src/main/java/utils/RandomNumbersGenerator.java
+++ src/main/java/utils/RandomNumbersGenerator.java
@@ -8,15 +8,15 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumbersGenerator {
 	private static final int MAX_NUMBER = 9;
 	private static final int MIN_NUMBER = 1;
-	private static final int MAX_SIZE = 3;
+	private static final int NUMBER_SIZE = 3;
 
 	private RandomNumbersGenerator() {
 	}
 
 	public static int[] makeThreeRandomNumbers() {
-		List<Integer> randomNumbers = new ArrayList<>(MAX_SIZE);
+		List<Integer> randomNumbers = new ArrayList<>(NUMBER_SIZE);
 
-		while (randomNumbers.size() < MAX_SIZE) {
+		while (randomNumbers.size() < NUMBER_SIZE) {
 			int randomNumber = makeRandomNumber();
 			if (!randomNumbers.contains(randomNumber)) {
 				randomNumbers.add(randomNumber);
