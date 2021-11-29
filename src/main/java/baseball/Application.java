7183a8278d807b370eb746b0474403d4567ff515
woojaang <woojaang@naver.com>
2021-11-29 10:11:26 +0900

    구현할 기능 목록

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         System.out.println("test");
+        int a = Randoms.pickNumberInRange(1,5);
+        System.out.println(a);
     }
 }
