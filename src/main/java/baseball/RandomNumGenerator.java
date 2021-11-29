1d388cdf66a4d33c30c3bb3667d69da4f20adb3a
bonkwon <rnjsqhsrnjs@naver.com>
2021-11-30 02:44:06 +0900

    feat : 1. 3자리 난수 생성

---
+++ src/main/java/baseball/RandomNumGenerator.java
@@ -0,0 +1,43 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumGenerator {
+
+	private static final int NUM_LENGTH = 3;
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 9;
+
+	private int[] randomNumber;
+
+	public RandomNumGenerator() {}
+
+	public int[] getNumber(){
+		makeRandomNumber();
+		return this.randomNumber;
+	}
+
+	public void makeRandomNumber(){
+		int currentNumber = 0;
+		while (currentNumber < NUM_LENGTH) {
+			int temp = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+			if(validateRandomNumber(currentNumber, temp) == true){
+				randomNumber[currentNumber] = temp;
+				currentNumber++;
+			}
+
+
+		}
+	}
+
+	public boolean validateRandomNumber(int currentNumber, int temp){
+		for(int i = 0; i < currentNumber; i++){
+			if(randomNumber[i] == temp){
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
