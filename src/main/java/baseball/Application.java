700d5eee03c3d63d84f477d431b73320f9ac7d98
johrae99 <70756680+johrae99@users.noreply.github.com>
2021-11-29 12:10:36 +0900

    feat : 사용자 3자리수 입력(예외 포함)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,23 @@
 package baseball;
 
 public class Application {
+	public Computer computer;
+	public Player player;
+	public boolean continueGame;
+
+	public Application(Computer computer, Player player) {
+		this.computer = computer;
+		this.player = player;
+		this.continueGame = true;
+	}
+
 	public static void main(String[] args) {
 		Computer computer = new Computer();
 		Player player = new Player();
-		computer.makeRandomNumber();
-		player.scanInputNumber();
+		Application application = new Application(computer, player);
+		application.computer.makeRandomNumber();
+		while (application.continueGame) {
+			application.player.scanInputNumber();
+		}
 	}
 }
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -9,9 +9,40 @@ public class Player {
 		System.out.print("숫자를 입력해주세요 : ");
 		String inputNumber = Console.readLine();
 		saveInputNumber(inputNumber);
+		isValidInput();
 	}
 
 	public void saveInputNumber(String inputNumber) {
 		this.inputNumber = inputNumber.toCharArray();
+	}
+
+	public void isValidInput() throws IllegalArgumentException {
+		if (inputNumber.length != Computer.NUMBER_SIZE || hasDuplicateNumber() || hasZero() || !isNumber()) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public boolean isNumber() {
+		for (char eachNumber : inputNumber) {
+			if (!Character.isDigit(eachNumber))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean hasDuplicateNumber() {
+		if (inputNumber[0] == inputNumber[1] || inputNumber[1] == inputNumber[2] || inputNumber[0] == inputNumber[2]) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean hasZero() {
+		for (int i = 0; i < Computer.NUMBER_SIZE; i++) {
+			if (inputNumber[i] == '0') {
+				return true;
+			}
+		}
+		return false;
 	}
 }
