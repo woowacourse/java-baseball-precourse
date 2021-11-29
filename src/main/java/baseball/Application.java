53eb218485d03245b581a6b41fa235768938b91e
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-11-30 03:03:12 +0900

    feat: 정답을 맞춘 후 재시작일 경우 다시 게임을 진행/ 종료일 경우 프로그램 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+		while (true) {
+			BaseballGame baseballGame = new BaseballGame();
+			baseballGame.playGame();
+			if (baseballGame.isQuit()) {
+				break;
+			}
+		}
+	}
 }
