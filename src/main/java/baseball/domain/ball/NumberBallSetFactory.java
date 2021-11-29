129ecaa4af0698bb48a4a038d97e05af2ac803f8
cjlee38 <gptpem38@gmail.com>
2021-11-29 22:50:40 +0900

    feat: validate user input for prediction
    사용자 입력 숫자 검증 로직 구현
    1. 길이가 올바른지(현재는 3)
    2. 모두 숫자인지(0 ~ 9, 0의 경우 NumberBall에서 구현 예정)
    3. 중복되는 숫자는 없는지

--- src/main/java/baseball/domain/ball/NumberBallSetFactory.java
+++ src/main/java/baseball/domain/ball/NumberBallSetFactory.java
@@ -10,6 +10,9 @@ import baseball.AppConfig;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberBallSetFactory {
+	private static final String ERROR_NON_DIGIT = "숫자가 아닌 값이 들어있습니다.";
+	private static final String ERROR_LENGTH = String.format("길이는 %d 이어야 합니다", NumberBallSet.NUMBER_COUNT);
+	private static final String ERROR_DUPLICATION = "중복된 숫자가 있습니다.";
 
 	private NumberBallSetFactory() {
 	}
@@ -23,6 +26,31 @@ public class NumberBallSetFactory {
 	}
 
 	protected static List<Integer> generateNumbersFromInput(String input) {
+		validateAll(input);
 		return input.chars().mapToObj(x -> Character.digit(x, AppConfig.RADIX)).collect(Collectors.toList());
+	}
+
+	private static void validateAll(String input) {
+		validateLength(input);
+		validateDigit(input);
+		validateNonDuplication(input);
+	}
+
+	private static void validateLength(String input) {
+		if (input.length() != NumberBallSet.NUMBER_COUNT) {
+			throw new IllegalArgumentException(ERROR_LENGTH);
+		}
+	}
+
+	private static void validateDigit(String input) {
+		if (!input.chars().allMatch(Character::isDigit)) {
+			throw new IllegalArgumentException(ERROR_NON_DIGIT);
+		}
+	}
+
+	private static void validateNonDuplication(String input) {
+		if (input.chars().distinct().reduce(0, (x, y) -> x + 1) != NumberBallSet.NUMBER_COUNT) {
+			throw new IllegalArgumentException(ERROR_DUPLICATION);
+		}
 	}
 }
