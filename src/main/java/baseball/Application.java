e8a8ce9853133790c6e5149027c0ae622dddceb4
sha11 <sha1119@naver.com>
2021-11-29 22:11:27 +0900

    refactor(Member Variable): 개선
    멤버변수를 제거하고 파라미터로 받아서 개선
    
    개선

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,11 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Application {
-    private static int ballCount;
-    private static int strikeCount;
-    private static int[] computerNumbers;
-    private static int N;
-    private static int[] inputNumbers;
-
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
         try {
@@ -23,37 +16,42 @@ public class Application {
     }
 
     public static void startGame() throws IOException {
-        makeComputerNumbers();
+        int[] inputNumbers = new int[3];
+        int ballCount = 0;
+        int strikeCount = 0;
+        int[] computerNumbers = new int[3];
+
+        makeComputerNumbers(computerNumbers);
         System.out.print("숫자를 입력하세요 : ");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        ballCount = 0;
-        strikeCount = 0;
-
         while (strikeCount != 3) {
-            N = Integer.parseInt(bufferedReader.readLine());
+            int N = Integer.parseInt(bufferedReader.readLine());
+
+            ballCount = 0;
+            strikeCount = 0;
+
             inputNumbers[0] = N / 100;
             inputNumbers[1] = (N / 10) % 10;
             inputNumbers[2] = N % 10;
 
-            inputNumbers();
-            checkBall();
-            checkStrike();
-            print();
+            inputNumbers(computerNumbers, N);
+            ballCount = checkBall(ballCount, computerNumbers, inputNumbers);
+            strikeCount = checkStrike(strikeCount, computerNumbers, inputNumbers);
+            print(ballCount, strikeCount);
         }
     }
 
-    public static void makeComputerNumbers() {
-        computerNumbers = new int[3];
-        inputNumbers = new int[3];
+    public static void makeComputerNumbers(int[] computerNumbers) {
         while (computerNumbers[0] == computerNumbers[1] || computerNumbers[1] == computerNumbers[2] || computerNumbers[0] == computerNumbers[2]) {
             computerNumbers[0] = Randoms.pickNumberInRange(1, 9);
             computerNumbers[1] = Randoms.pickNumberInRange(1, 9);
             computerNumbers[2] = Randoms.pickNumberInRange(1, 9);
         }
+        System.out.println("answer :" + computerNumbers[0] + computerNumbers[1] + computerNumbers[2]);
     }
 
-    public static void inputNumbers() {
+    public static void inputNumbers(int[] computerNumbers, int N) {
         if (computerNumbers[0] == computerNumbers[1] || computerNumbers[0] == computerNumbers[2] || computerNumbers[1] == computerNumbers[2]) {
             throw new IllegalArgumentException("조건에 맞는 수를 입력해주세요.");
         }
@@ -84,25 +82,26 @@ public class Application {
         }
     }
 
-    public static void checkStrike() {
-        strikeCount = 0;
+    public static int checkStrike(int strikeCount, int[] computerNumbers, int[] inputNumbers) {
         for (int i = 0; i < 3; i++) {
             if (computerNumbers[i] == inputNumbers[i]) strikeCount++;
         }
+
+        return strikeCount;
     }
 
-    public static void checkBall() {
-        ballCount = 0;
-        strikeCount = 0;
+    public static int checkBall(int ballCount, int[] computerNumbers, int[] inputNumbers) {
         if (computerNumbers[0] == inputNumbers[1]) ballCount++;
         if (computerNumbers[0] == inputNumbers[2]) ballCount++;
         if (computerNumbers[1] == inputNumbers[0]) ballCount++;
         if (computerNumbers[1] == inputNumbers[2]) ballCount++;
         if (computerNumbers[2] == inputNumbers[0]) ballCount++;
         if (computerNumbers[2] == inputNumbers[1]) ballCount++;
+
+        return ballCount;
     }
 
-    public static void print() throws IOException {
+    public static void print(int ballCount, int strikeCount) throws IOException {
         if (ballCount < 3 && ballCount != 0 && strikeCount == 0) {
             System.out.print(ballCount + "볼" + '\n' + "숫자를 입력하세요 : ");
         }
