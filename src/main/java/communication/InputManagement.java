e47d8327b7ecee1483f5de6302c2b8f0e1504fea
usuyn <usuyn000@gmail.com>
2021-11-29 18:56:45 +0900

    feat(InputManagement): 사용자 숫자 검사

--- src/main/java/communication/InputManagement.java
+++ src/main/java/communication/InputManagement.java
@@ -3,6 +3,9 @@ package communication;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputManagement {
+    private final int ESSENTIAL_NUMBER_LENGTH = 3;
+    private final String REGEX_NUMBER = "^[1-9]*$";
+
     public void requestUserInput() {
         System.out.println(Text.INPUT_REQUEST);
     }
@@ -11,9 +14,34 @@ public class InputManagement {
         return Console.readLine();
     }
 
+    private boolean isValidUsersNumber(String usersNumber) {
+
+        if (usersNumber.length() != ESSENTIAL_NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        if (usersNumber.matches(REGEX_NUMBER)) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = ESSENTIAL_NUMBER_LENGTH - 1; i >= 0; i--) {
+
+            if (usersNumber.indexOf(usersNumber.charAt(i)) != 1) {
+                throw new IllegalArgumentException();
+            }
+
+        }
+
+        return true;
+    }
+
     public String provideUserInput() {
         String usersNumber = receiveUserInput();
 
-        return usersNumber;
+        if (isValidUsersNumber(usersNumber)) {
+            return usersNumber;
+        }
+
+        return null;
     }
 }
