c6ff7e4e55d9b78200050f13dff4745815b34743
Kim Seunglae <obey1342@gmail.com>
2021-11-30 03:03:55 +0900

    refactor:  상수부분 수정

--- src/main/java/baseball/BallGenerator.java
+++ src/main/java/baseball/BallGenerator.java
@@ -1,14 +1,16 @@
 package baseball;
 
+import static baseball.Constants.*;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BallGenerator {
-	private static int START_NUMBER = 1;
-	private static int END_NUMBER = 9;
+	private static final int START_NUMBER = 1;
+	private static final int END_NUMBER = 9;
 
 	public String createRandomBalls() {
 		StringBuilder balls = new StringBuilder();
-		while (balls.length() < 3) {
+		while (balls.length() < MAX_LENGTH) {
 			String ball = String.valueOf(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
 			if (!balls.toString().contains(ball)) {
 				balls.append(ball);
