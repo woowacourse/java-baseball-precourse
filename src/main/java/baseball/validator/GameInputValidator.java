28cd6a2d9594880d072b5f1c7bc5161d31aba953
jojogreen91 <iawbg13@gmail.com>
2021-11-29 10:54:39 +0900

    refactor(GameInputValidator): refactor method
    예외가 발생하는 각각의 경우를 메서드로 분리시켜서 꺼냈다.

--- src/main/java/baseball/validator/GameInputValidator.java
+++ src/main/java/baseball/validator/GameInputValidator.java
@@ -8,14 +8,23 @@ public class GameInputValidator implements InputValidator {
 
 	@Override
 	public void validateInput(String input) throws IllegalArgumentException {
-		int lengthRule = 3;
-
 		// input 의 길이가 3이 아닐때 예외 발생
-		if (input.length() != lengthRule) {
-			throw new IllegalArgumentException();
-		}
+		keepLengthThree(input);
 
 		// input 에 숫자가 아닌 값 혹은 0이 있을 때 예외 발생
+		keepProperValue(input);
+
+		// input 에 중복되는 숫자가 입력될 때
+		keepUniqueValue(input);
+	}
+
+	private void keepLengthThree(String input) throws IllegalArgumentException {
+		if (input.length() != 3) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void keepProperValue(String input) throws IllegalArgumentException {
 		for (int i = 0; i < input.length(); i++) {
 			char c = input.charAt(i);
 
@@ -23,8 +32,9 @@ public class GameInputValidator implements InputValidator {
 				throw new IllegalArgumentException();
 			}
 		}
+	}
 
-		// input 에 중복되는 숫자가 입력될 때
+	private void keepUniqueValue(String input) throws IllegalArgumentException {
 		ArrayList<Character> inputCharList = new ArrayList<>();
 		for (char c : input.toCharArray()) {
 			inputCharList.add(c);
