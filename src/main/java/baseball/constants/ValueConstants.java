64c99e997cdc2cd0306302854390b274a9ffad31
overnew <ehdls1303@gmail.com>
2021-11-29 23:12:51 +0900

    Refactor: constants.ValueConstants 주석 추가
    주석을 추가해 종류 분류

--- src/main/java/baseball/constants/ValueConstants.java
+++ src/main/java/baseball/constants/ValueConstants.java
@@ -1,12 +1,13 @@
 package baseball.constants;
 
 public class ValueConstants {
+    //게임숫자
     public static final int GAME_NUMBER_LEN = 3;
     public static final int GAME_NUMBER_BOUND_START = 1;
     public static final int GAME_NUMBER_BOUND_END = 9;
 
+    //게임 재시작 옵션
     public static final int GAME_OPTION_LEN = 1;
     public static final int GAME_OPTION_BOUND_START = 1;
     public static final int GAME_OPTION_BOUND_END = 2;
-
 }
