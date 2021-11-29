34f7d07c8f521a7981d32366909e38852f4d818c
Your Name Here <learningssik@gmail.com>
2021-11-30 00:10:39 +0900

    feat: Random number 생성

---
+++ src/main/java/baseball/Random.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
+public class Random {
+
+    public static String random_Num(){
+        int[] computerNumber = new int[3];
+        int i = 0;
+        do {
+            int newNumber = Randoms.pickNumberInRange(1, 9);
+            if (isOverlap(computerNumber, newNumber)) {
+                continue;
+            }
+            computerNumber[i] = newNumber;
+            i = i + 1;
+        } while (i < 3);
+
+        System.out.println(Arrays.toString(computerNumber).replaceAll("[^0-9]",""));
+        return Arrays.toString(computerNumber).replaceAll("[^0-9]", "");
+    }
+
+    private static boolean isOverlap(int[] computerNumber, int newNumber) {
+        return Arrays.stream(computerNumber).anyMatch(k -> k == newNumber);
+    }
+}
