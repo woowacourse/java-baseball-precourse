412696fbc9a1f1ff3e5a0a3a37ef003c1078deb1
Gibeom <delphox60@gmail.com>
2021-11-29 22:23:27 +0900

    refactor(Judge): 숫자가 ball인지 판별하는 메소드를 가독성을 위해 재구성

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -36,8 +36,12 @@ public class Judge {
 
 	private boolean isBall(final int indexOfNum, final String targetNum) {
 		char curChar = targetNum.charAt(indexOfNum);
-        return curChar == rightAnswer.charAt((indexOfNum + 1) % 3) || curChar == rightAnswer.charAt((indexOfNum + 2) % 3);
-    }
+		if (curChar == rightAnswer.charAt((indexOfNum + 1) % 3))
+			return true;
+		if (curChar == rightAnswer.charAt((indexOfNum + 2) % 3))
+			return true;
+		return false;
+	}
 
 	public void makeJudgement(final String target) {
 		for (int i = 0; i < 3; i++) {
