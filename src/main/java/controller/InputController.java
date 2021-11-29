6e3efd71e0b8d9dd32ff58ccac8b17dac2114186
koo <gudonghee2000@gmail.com>
2021-11-29 16:48:43 +0900

    refactor : InputController의 isValidationGameFlag 함수 수정

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -20,8 +20,6 @@ public class InputController {
 	}
 
 	private static int isValidationGameFlag(String gameFlag) {
-		int inputNumber = Integer.parseInt(gameFlag);
-
 		if (!Pattern.matches(NUMBER_PATTERN, gameFlag)) {
 			throw new IllegalArgumentException("입력한 숫자중 잘못된 숫자(문자, 음수 등)이 있습니다.");
 		}
@@ -30,10 +28,6 @@ public class InputController {
 			throw new IllegalArgumentException("입력한 숫자가 한자리가 아닙니다.");
 		}
 
-		if (inputNumber != 1 && inputNumber != 2) {
-			throw new IllegalArgumentException("입력한 숫자가 1 또는 2가 아닙니다.");
-		}
-
-		return inputNumber;
+		return Integer.parseInt(gameFlag);
 	}
 }
