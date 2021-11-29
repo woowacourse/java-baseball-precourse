4741cfaa4158aba399a251750dcfb33e408e9567
overnew <ehdls1303@gmail.com>
2021-11-29 20:10:14 +0900

    refactor: 코드 컨벤션 유지
    코드 컨벤션 유지를 위해 일부 코드 수정

--- src/main/java/baseball/gamesource/Opponent.java
+++ src/main/java/baseball/gamesource/Opponent.java
@@ -11,10 +11,12 @@ public class Opponent {
     private List<Integer> numbers = new ArrayList<>();
 
     public Opponent(){  //생성자에서 랜덤 수 생성
+
         for(int i=0; i< ValueConstants.GAME_NUMBER_LEN ; ++i){
-            int randomNum = (Randoms.pickNumberInRange(ValueConstants.GAME_NUMBER_BOUND_START,ValueConstants.GAME_NUMBER_BOUND_END));
+            int randomNum = Randoms.pickNumberInRange(ValueConstants.GAME_NUMBER_BOUND_START,ValueConstants.GAME_NUMBER_BOUND_END);
             numbers.add(randomNum);
         }
+
     }
 
     List<Integer> getNumbers(){
