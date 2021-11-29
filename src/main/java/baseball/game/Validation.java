06f38736f8107c2377fea96f6dd486a4214a6aef
asebn1 <asebn1@khu.ac.kr>
2021-11-29 22:50:08 +0900

    feat: 유저의 입력에 중복된 숫자가 있는지 확인

--- src/main/java/baseball/game/Validation.java
+++ src/main/java/baseball/game/Validation.java
@@ -9,6 +9,9 @@ public class Validation {
         if (!isRightRange(input)) {
             throw new IllegalArgumentException("잘못된 숫자입니다.");
         }
+        if (!isDuplicate(input)) {
+            throw new IllegalArgumentException("중복된 숫자를 입력하였습니다.");
+        }
     }
 
     private static boolean isRightLength(String input) {
@@ -16,14 +19,34 @@ public class Validation {
     }
 
     private static boolean isRightRange(String input) {
+        int num;
+
         for (int i = 0; i < input.length(); i++) {
-            int num = Character.getNumericValue(input.charAt(i));
-            if (num<1 || num>9){
+            num = Character.getNumericValue(input.charAt(i));
+            if (num < 1 || num > 9) {
                 return false;
             }
         }
 
         return true;
     }
+
+    private static boolean isDuplicate(String input) {
+        int num;
+        boolean[] visited = new boolean[10];
+
+        for(int i=0; i<input.length(); i++){
+            num = Character.getNumericValue(input.charAt(i));
+
+            if (!visited[num]){
+                visited[num] = true;
+                continue;
+            }
+            return false;
+        }
+
+        return true;
+    }
+
 
 }
