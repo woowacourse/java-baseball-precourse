29f2dfe61758affa24a3c629bd96a012dc5ae83b
윤이진 <483759@naver.com>
2021-11-29 22:31:57 +0900

    feat: 서로 다른 숫자만을 가지도록 조건 추가

--- src/main/java/baseball/domain/GameBalls.java
+++ src/main/java/baseball/domain/GameBalls.java
@@ -1,11 +1,29 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 
 public class GameBalls {
 
     public static final int NUMBEROFBALLS = 3;
     private Ball[] sequence;
+
+    public GameBalls() {
+        this.sequence = new Ball[NUMBEROFBALLS];
+        boolean[] isUsedNumber = new boolean[10];
+
+        int index = 0;
+        while (index != NUMBEROFBALLS) {
+            int pickNumberInRange = Randoms.pickNumberInRange(1, 9);
+            if (isUsedNumber[pickNumberInRange]) {
+                continue;
+            }
+
+            this.sequence[index] = new Ball(pickNumberInRange);
+            isUsedNumber[pickNumberInRange] = true;
+            index++;
+        }
+    }
 
     public GameBalls(String userInput) throws IllegalArgumentException {
         int toInteger = convertStringToInteger(userInput);
@@ -29,12 +47,17 @@ public class GameBalls {
         }
     }
 
-    public int[] convertIntegerToNumberArray(int userNumber) {
+    public int[] convertIntegerToNumberArray(int userNumber) throws IllegalArgumentException {
         int[] numbers = new int[NUMBEROFBALLS];
+        boolean[] isUsedNumber = new boolean[10];
 
         for (int i = NUMBEROFBALLS - 1; i >= 0; i--) {
             numbers[i] = userNumber % 10;
             userNumber /= 10;
+            if (isUsedNumber[numbers[i]]) {
+                throw new IllegalArgumentException("서로 다른 숫자를 입력하세요");
+            }
+            isUsedNumber[numbers[i]] = true;
         }
         return numbers;
     }
