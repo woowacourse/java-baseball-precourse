3b600f4936072e3554933aa12931cb8350c557cc
Dong Geun <dlehdrms978@gmail.com>
2021-11-29 22:06:24 +0900

    야구 게임에서 사용할 상수 등록

---
+++ src/main/java/baseballgame/CommonConstant.java
@@ -0,0 +1,12 @@
+package baseballgame;
+
+public final class CommonConstant {
+    private CommonConstant(){
+
+    }
+    public static int MIN_NUMBER = 1;
+    public static int MAX_NUMBER = 9;
+    public static String NEW_GAME_STATE="1";
+    public static String FINISH_GAME_STATE="2";
+}
+
