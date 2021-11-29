2d7696949f507b1b04e4517a21b3a30d547a26f7
summerlunaa <rhgus1085@gmail.com>
2021-11-29 17:05:39 +0900

    fix(Game): equals 메소드 대신 등호를 사용하여 생긴 오류 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -57,7 +57,7 @@ public class Game {
         String input = Input.getNumber(
                 "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
                 1, 2, 1);
-        if (input == "1") {
+        if (input.equals("1")) {
             return true;
         }
         return false;
