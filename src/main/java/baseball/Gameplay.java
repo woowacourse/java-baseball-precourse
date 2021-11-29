32109ae7a98e674c8d721847cc17919dde1f4aa5
sejung-hong <sejungh@naver.com>
2021-11-30 01:10:46 +0900

    feat : 게임플레이를 위한 초기 세팅

--- src/main/java/baseball/Gameplay.java
+++ src/main/java/baseball/Gameplay.java
@@ -1,4 +1,19 @@
 package baseball;
 
 public class Gameplay {
+
+    Player player;
+    Computer computer;
+
+    //player와 computer 생성하는 함수
+    public void GameSetting(){
+        player = new Player();
+        computer = new Computer();
+    }
+    
+    //게임 플레이하는 함수
+    public void Play() {
+        
+    }
+    
 }
