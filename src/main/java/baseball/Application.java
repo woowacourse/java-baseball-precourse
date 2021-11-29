c831cd4bb0a23414ea685cf718ba7c802fd8d616
cjlee38 <gptpem38@gmail.com>
2021-11-30 00:42:33 +0900

    feat: implement main method(starting point)
    전체 프로그램 실행 시작점 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.domain.game.Game;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		//TODO: 숫자 야구 게임 구현
+		AppConfig config = new AppConfig();
+		Application app = new Application();
+		Game game = config.getNumberBaseballGame();
+		app.run(game);
+	}
+
+	public void run(Game game) {
+		game.run();
+	}
 }
