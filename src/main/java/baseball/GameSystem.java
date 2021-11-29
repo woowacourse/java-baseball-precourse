24612739149acb4dbe339fff5c23cadf55facc59
Gibeom <delphox60@gmail.com>
2021-11-29 20:42:58 +0900

    fix(GameSystem): 게임 결과를 출력하는 기능에서 ball과 strike 개수를 출력할 때 엔드라인이 출력되는 오류 수정

--- src/main/java/baseball/GameSystem.java
+++ src/main/java/baseball/GameSystem.java
@@ -25,17 +25,18 @@ public class GameSystem {
 
     private void printResult(final int strikes, final int balls) {
         if (balls != 0) {
-            System.out.println(balls + "볼 ");
+            System.out.print(balls + "볼 ");
         }
         if (strikes != 0) {
-            System.out.println(strikes + "스트라이크");
+            System.out.print(strikes + "스트라이크");
         }
+        if (balls == 0 && strikes == 0) {
+            System.out.print("낫싱");
+        }
+        System.out.print("\n");
         if (strikes == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return;
-        }
-        if (balls == 0 && strikes == 0) {
-            System.out.println("낫싱");
         }
     }
 
