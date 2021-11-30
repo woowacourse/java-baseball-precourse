d5f4b1d3cfbb4b9bc6641687aa48a59374bdfb2c
Mo0nl19ht <mool2480@gmail.com>
2021-11-30 11:55:41 +0900

    refactor: ComputerTest 모듈화

--- src/test/java/baseball/computer/ComputerTest.java
+++ src/test/java/baseball/computer/ComputerTest.java
@@ -1,5 +1,6 @@
 package baseball.computer;
 
+import static baseball.Constants.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Arrays;
@@ -11,23 +12,32 @@ import org.junit.jupiter.api.Test;
 public class ComputerTest {
 	@Test
 	void checkTarget() {
-		//타겟 넘버 생성
+		// 타겟 넘버 생성
 		Computer computer = new Computer();
 
-		//서로 다른 수 확인을 위한 set 자료구조 생성
+		// 서로 다른 수 확인을 위한 set 자료구조 생성
 		Set<Integer> set = new HashSet<>();
 		//타켓 넘버들을 set에 넣는다
 		for (int number : computer.target) {
 			set.add(number);
 		}
-		// set의 크기가 3인지 확인하여 모두 서로 다른 수 인지 확인
-		assertThat(set.size()).isEqualTo(3);
+		// 테스트 진행
+		checkDuplicates(set);
+		checkNumRange(computer.target, set);
+	}
 
-		//타켓 넘버들이 1~9사이의 숫자인지 확인
-		Arrays.stream(computer.target).forEach(number -> {
+	// set의 크기가 3인지 확인하여 모두 서로 다른 수 인지 확인
+	void checkDuplicates(Set<Integer> set) {
+		assertThat(set.size()).isEqualTo(maxNumberLen);
+	}
+
+	//타켓 넘버들이 1~9사이의 숫자인지 확인
+	void checkNumRange(int[] target, Set<Integer> set) {
+		Arrays.stream(target).forEach(number -> {
 			assertThat(number)
 				.isGreaterThanOrEqualTo(1)
 				.isLessThanOrEqualTo(9);
 		});
 	}
+
 }
