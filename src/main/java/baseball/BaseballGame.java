52ab7bb07090d5ab46497574fc107ac70c7f1441
SoulBriz <adoredia@naver.com>
2021-11-30 20:36:01 +0900

    style: 불필요한 출력문 제거

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -6,7 +6,7 @@ public class BaseballGame {
             int[] compareValues = new int[2];
             String randomNumber;
             randomNumber = baseball.Computer.getRandomNumber();
-            System.out.println(randomNumber);
+
             while (compareValues[0] != 3) {
                 String inputNumber = baseball.User.pleaseInputNumber();
                 compareValues = baseball.Referee.compareValues(randomNumber, inputNumber);
