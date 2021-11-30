23cb2f19853cdb4550e7d06a1c58ac9e608f30c9
mac_daehwan2yo <daehwan7989@naver.com>
2021-11-30 20:01:15 +0900

    [test] Valid : 사용자 입력값을 List로 바꾸면서 수정된 Valid 과정 재 테스트
    ## 목적
    기존에는 String 으로 입력받고, Valid를 int 타입으로 매개받아 Valid를 진행했으나,
    int로의 형변환 없이 그대로 String을 활용한다.

--- src/test/java/baseball/valid/ValidTest.java
+++ src/test/java/baseball/valid/ValidTest.java
@@ -12,11 +12,11 @@ public class ValidTest {
     @DisplayName("숫자_내_중복을_확인한다")
     void 숫자_내_중복() {
         // 123은 숫자 내 중복이 없다.
-        boolean result = valid.duplicate(123);
+        boolean result = valid.duplicate("123");
         assertThat(result).isFalse();
 
         // 111은 숫자 내 중복이 존재한다.
-        result = valid.duplicate(111);
+        result = valid.duplicate("111");
         assertThat(result).isTrue();
     }
 
@@ -24,15 +24,15 @@ public class ValidTest {
     @DisplayName("숫자의_범위를_확인한다")
     void 숫자는_3자리수() {
         // 123은 3자리 수 범위 내에 있다.
-        boolean result = valid.inRange(123);
+        boolean result = valid.inRange("123");
         assertThat(result).isTrue();
 
         // 99은 3자리 수 범위 밖에 있다.
-        result = valid.inRange(99);
+        result = valid.inRange("99");
         assertThat(result).isFalse();
 
         // 1000은 3자리 수 범위 밖에 있다.
-        result = valid.inRange(1000);
+        result = valid.inRange("1000");
         assertThat(result).isFalse();
     }
 
@@ -40,11 +40,11 @@ public class ValidTest {
     @DisplayName("숫자내에 0이 있는지 확인한다.")
     void 숫자에_0이_있는가() {
         // 123에는 0이 포함되어 있지 않다.
-        boolean result = valid.hasZero(123);
+        boolean result = valid.hasZero("123");
         assertThat(result).isFalse();
 
         // 103에는 0포함 되어있다.
-        result = valid.hasZero(103);
+        result = valid.hasZero("103");
         assertThat(result).isTrue();
     }
 }
