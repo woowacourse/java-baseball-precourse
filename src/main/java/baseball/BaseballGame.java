9b8e41e6f7553e5537da3b2a9fe2a505836c01be
GYUTAEKIM <kkt219a@gmail.com>
2021-11-30 21:38:08 +0900

    feat: 입출력 처리 시스템과 게임 진행 로직 연동
    - 입출력을 통해 게임을 진행할 수 있도록 연동 메소드 구현
    - 야구게임을 시작을 위한 main 메소드 조정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,20 +1,47 @@
 package baseball;
 
+import baseball.io.InputProcessor;
+import baseball.io.OutputProcessor;
 import baseball.number.Numbers;
+import baseball.type.GameStatus;
 
 public class BaseballGame {
-	private final Numbers computer;
+	private Numbers computer;
+	private GameStatus status;
 
 	private BaseballGame() {
 		this.computer = Numbers.create();
+		this.status = GameStatus.OPERATION;
 	}
 
 	public static BaseballGame create(){
 		return new BaseballGame();
 	}
 
-	public Results start(String input){
-		Numbers player = Numbers.from(input);
-		return Results.of(this.computer,player);
+	public void start(){
+		while(this.status != GameStatus.EXIT){
+			String input = InputProcessor.readValue();
+			Numbers player = Numbers.from(input);
+			Results results = Results.of(this.computer, player);
+			OutputProcessor.printResult(results);
+			validateEnd(results);
+		}
+	}
+
+	private void validateEnd(Results results){
+		if(results.isCorrect()){
+			OutputProcessor.printCorrect();
+			GameStatus status = InputProcessor.readGameStatus();
+			changeStatus(status);
+		}
+	}
+
+	private void changeStatus(GameStatus gameStatus){
+		if(gameStatus==GameStatus.RESTART){
+			this.computer = Numbers.create();
+			this.status = GameStatus.OPERATION;
+		} else if(gameStatus == GameStatus.EXIT){
+			this.status = GameStatus.EXIT;
+		}
 	}
 }
