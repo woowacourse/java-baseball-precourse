b56f4408c19a6f027b6550274967c91a6fd02ff1
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-30 17:54:51 +0900

    fix : gameController static 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,10 @@
 package baseball;
 
 public class Application {
-    private static final GameController gameController = new GameController();
 
     public static void main(String[] args) {
+        final GameController gameController = new GameController();
         gameController.run();
     }
-
-
 }
 
