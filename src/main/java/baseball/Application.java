a5ffffec29e0b7ae8180b4852c34bb0c11c372c5
YJ <soltwood6862@naver.com>
2021-11-30 23:00:30 +0900

    feat(Application.java) : Game 클래스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        Game game = new Game(Game.getRandomValue());
     }
 }
