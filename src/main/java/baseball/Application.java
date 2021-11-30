1f447df80eb46e0209fe34749f4b21c881cc8df5
sejung-hong <sejungh@naver.com>
2021-11-30 21:12:29 +0900

    feat : 게임플레이를 위한 초기 세팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-
+        Gameplay game = new Gameplay();
+        game.Play();
     }
 }
--- src/main/java/baseball/Gameplay.java
+++ src/main/java/baseball/Gameplay.java
@@ -2,4 +2,17 @@ package baseball;
 
 public class Gameplay {
 
+    Player player;
+    Computer computer;
+
+    //player와 computer 생성하는 함수
+    public void GameSetting(){
+        player = new Player();
+        computer = new Computer();
+    }
+
+    //게임 플레이하는 함수
+    public void Play() {
+
+    }
 }
