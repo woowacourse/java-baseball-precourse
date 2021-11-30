b1db79afbc357b3faf70bc0c1f905117f14e7dee
Huicheol Moon <huicheol7@gmail.com>
2021-11-30 23:16:41 +0900

    feat(Application): 숫자 야구 게임 어플리케이션 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import controller.BaseballGame;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		BaseballGame baseballgame = new BaseballGame();
+		baseballgame.startGame();
+	}
 }
