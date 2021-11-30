a36f4650368d1a2109bb24dc64120ac9e6dff70b
YJ <soltwood6862@naver.com>
2021-11-30 23:12:41 +0900

    feat(Application.java) : 게임 실행부분 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,19 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game(Game.getRandomValue());
+
+        while (true) {
+            String userInput = Input.inputUserNumber();
+
+            if (!game.checkNumber(userInput)) {
+                continue;
+            }
+
+            if (Input.inputEndNumber()) {
+                break;
+            }
+
+            game = new Game(Game.getRandomValue());
+        }
     }
 }
