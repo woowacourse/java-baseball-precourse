20f3f914ae694e46f7c4dadca4e02dac8f9cd0a2
mac_daehwan2yo <daehwan7989@naver.com>
2021-11-30 19:42:39 +0900

    [fix] Computer : 난수 생성시 각 자리수 별로 생성
    ## 목적
     100~999의 범위로 난수를 생성했는데, 이는 테스트에서 오류를 발생시킨다.
    따라서 각 자리수 별로 난수 생성을 받기로 한다.

--- src/main/java/baseball/computer/Computer.java
+++ src/main/java/baseball/computer/Computer.java
@@ -1,29 +1,44 @@
 package baseball.computer;
 
-import baseball.error.ErrorCode;
 import baseball.pitch.Pitch;
 import baseball.valid.Valid;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class Computer implements Valid {
-    private final int num;
-    private final int RAN_MIN = 100;
-    private final int RAN_MAX = 999;
+    private final List<Integer> numberList;
+    private final boolean[] dup;
+    private final int RAN_MIN = 1;
+    private final int RAN_MAX = 9;
     private Set<Pitch> pitchSet;
 
     public Computer() {
-        num = Randoms.pickNumberInRange(RAN_MIN, RAN_MAX);
+        numberList = new ArrayList<>();
+        dup = new boolean[10];
+        for(int i=0; i<3; i++)
+            fillList(Randoms.pickNumberInRange(RAN_MIN, RAN_MAX));
     }
+
+    private void fillList(int i) {
+        // 기존에 입력받은 값이면 다시 난수 생성
+        while(dup[i]) {
+            i = Randoms.pickNumberInRange(RAN_MIN, RAN_MAX);
+        }
+        numberList.add(i);
+        dup[i] = true;
+    }
+
     public void setPitchSet(Set<Pitch> pitchSet) {
         this.pitchSet = pitchSet;
     }
     public Set<Pitch> getPitchSet() {
         return pitchSet;
     }
-    public int getNumber() {
-        return num;
+    public List<Integer> getNumberList() {
+        return numberList;
     }
 
 }
