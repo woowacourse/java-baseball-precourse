b4c6be57f6131c599b46118a6e201b3546ae230f
yxxnghwan <younghwan960@gmail.com>
2021-11-30 11:37:47 +0900

    chore(GAME): 불필요한 import문 제거

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,6 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
 
