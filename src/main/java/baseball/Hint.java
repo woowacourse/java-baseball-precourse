d717562f72b32a759b48732908b6937209209f2c
audgnssweet <audgnssweet@naver.com>
2021-11-30 18:18:07 +0900

    refactor(Hint): 필요없는 한글 메시지 삭제

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -1,15 +1,9 @@
 package baseball;
 
 public enum Hint implements Comparable<Hint> {
-	BALL("볼"),
-	STRIKE("스트라이크"),
-	NOTHING("낫싱");
-
-	private final String name;
-
-	Hint(String name) {
-		this.name = name;
-	}
+	BALL,
+	STRIKE,
+	NOTHING;
 
 	public boolean isBall() {
 		return this == BALL;
