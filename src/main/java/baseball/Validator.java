f5a1677af48583ece2fb9847f1d497d79cdbb1a8
eonju97 <eonju97.dev@gmail.com>
2021-11-30 23:49:40 +0900

    refactor: 반복되는 출력 메세지 상수로 분리

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -9,6 +9,7 @@ public class Validator {
     private final static int END = 2;
     private final static int DIGIT = 3;
     private final static String NUMBER_PATTERN = "[+-]?\\d*(\\.\\d+)?";
+    private final static String WRONG_INPUT_MESSAGE = "입력값이 올바르지 않습니다.";
 
     private String input;
 
@@ -18,7 +19,7 @@ public class Validator {
         boolean isNumber = this.input.matches(NUMBER_PATTERN);
 
         if (!isNumber || isContainsZero() || isContainsDuplicatedNumber() || !isCorrectDigit()) {
-            throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+            throw new IllegalArgumentException(WRONG_INPUT_MESSAGE);
         }
     }
 
@@ -26,7 +27,7 @@ public class Validator {
         boolean isNumber = this.input.matches(NUMBER_PATTERN);
 
         if (!isNumber && (Integer.parseInt(input) != START || Integer.parseInt(input) != END)) {
-            throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+            throw new IllegalArgumentException(WRONG_INPUT_MESSAGE);
         }
     }
 
