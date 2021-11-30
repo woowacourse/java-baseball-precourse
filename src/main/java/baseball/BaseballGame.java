66d33a9f6c9cb741561f6ad7d00cff1607e8b4c9
SeongBinC <tph5268@naver.com>
2021-11-30 22:24:50 +0900

    feat: 게임 시작/종료 & 입력값 비교/힌트 반환

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,73 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+import static baseball.Message.*;
+import static baseball.Message.NOTHING;
+
+public class BaseballGame {
+
+    private Computer computer;
+
+    public void gameStart() {
+        this.computer = new Computer();
+        User user;
+        do {
+            user = input();
+        } while (!numberCheck(computer.getAnswer(), user.getUserNumbers()));
+    }
+
+
+    public User input() {
+        System.out.println(INPUT_NUMBERS);
+        String userInput = Console.readLine();
+        return new User(userInput);
+
+    }
+
+    boolean numberCheck(List<Integer> answer, List<Integer> User) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < answer.size(); i++) {
+            if (answer.get(i).equals(User.get(i)))
+                strike++;
+
+            ball += ballCheck(answer, User.get(i), i);
+        }
+
+        printScore(strike, ball);
+
+        return strike == 3;
+    }
+
+    void printScore(int strike, int ball) {
+
+        if (ball != 0)
+            System.out.print(ball + BALL + " ");
+        if (strike != 0)
+            System.out.print(strike + STRIKE);
+        if (strike == 3) {
+            System.out.print(FINISH_MESSAGE);
+        } else if (strike == 0 && ball == 0) {
+            System.out.print(NOTHING);
+        }
+        System.out.print('\n');
+    }
+
+    int ballCheck(List<Integer> answer, int num, int idx) {
+        int ball = 0;
+
+        for (int i = 0; i < answer.size(); ++i) {
+            if (i == idx)
+                continue;
+            if (answer.get(i) == num)
+                ball++;
+        }
+        return ball;
+    }
+
+
+}
--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -6,4 +6,10 @@ public class Message {
     public static final int LENGTH_OF_ANSWER = 3;
 
     public static final String ERROR = "잘못된 입력입니다.";
+    public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+    public static final String FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
 }
