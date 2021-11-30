71fd2883e1bdf36cde1e2460f2b10a0d725620ec
Byeongju-Kong <bjuuuu98@gmail.com>
2021-11-30 19:35:39 +0900

    refactor : 필드변수 지역변수로 변경

--- src/main/java/controller/GameSystemController.java
+++ src/main/java/controller/GameSystemController.java
@@ -9,7 +9,6 @@ import view.OutputView;
 public class GameSystemController {
     private final InputView inputView;
     private final OutputView outputView;
-    private boolean willPlayNewGame = true;
 
     public GameSystemController() {
         inputView = new InputView();
@@ -18,16 +17,17 @@ public class GameSystemController {
 
     public void run() {
         GameController gameController = new GameController(inputView, outputView);
+        boolean willPlayNewGame = true;
         while (willPlayNewGame) {
             gameController.playNewGame();
             outputView.showGameOver();
-            checkRestart();
+            willPlayNewGame = checkRestart();
         }
     }
 
-    private void checkRestart() {
+    private boolean checkRestart() {
         inputView.alertRestartNumberInput();
         String inputRestartValue = readLine();
-        willPlayNewGame = NewGameCommand.willPlayNewGame(inputRestartValue);
+        return NewGameCommand.willPlayNewGame(inputRestartValue);
     }
 }
