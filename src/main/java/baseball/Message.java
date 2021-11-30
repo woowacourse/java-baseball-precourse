6a2b03351ae06a8a4be8f87395bd617efc28d39e
SeongBinC <tph5268@naver.com>
2021-11-30 22:12:31 +0900

    feat: 입력값 검증 & 형변환

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -4,4 +4,6 @@ public class Message {
     public static final int MIN = 1;
     public static final int MAX = 9;
     public static final int LENGTH_OF_ANSWER = 3;
+
+    public static final String ERROR = "잘못된 입력입니다.";
 }
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,81 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static baseball.Message.*;
+import static baseball.Message.ERROR;
+
+public class User {
+    private static final String ZERO = "0";
+    private static final String NUMERIC_FILTER = "[1-9]+";
+    private final ArrayList<Integer> userNumbers = new ArrayList<>();
+
+
+    public User(String input) {
+        checkInputNumber(input);
+        convertInputNumber(input);
+    }
+
+    public void checkInputNumber(String input) {
+        checkContainsZero(input);
+        checkLength(input);
+        checkNumeric(input);
+        checkDuplicate(input);
+    }
+
+    private void checkNumeric(String input) {
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException(ERROR);
+        }
+    }
+
+    private boolean isNumeric(String input) {
+        return input.matches(NUMERIC_FILTER);
+    }
+
+    private void checkContainsZero(String input) {
+        if (ContainsZERO(input)) {
+            throw new IllegalArgumentException(ERROR);
+        }
+    }
+
+    private boolean ContainsZERO(String input) {
+
+        return input.contains(ZERO);
+    }
+
+    private void checkLength(String input) {
+        if (!inputNumberLength(input)) {
+            throw new IllegalArgumentException(ERROR);
+        }
+    }
+
+    private boolean inputNumberLength(String input) {
+        return Objects.equals(input.length(), LENGTH_OF_ANSWER);
+    }
+
+    private void checkDuplicate(String input) {
+        if (inputDuplicate(input)) {
+            throw new IllegalArgumentException(ERROR);
+        }
+    }
+
+    private boolean inputDuplicate(String input) {
+        return Objects.equals(input.charAt(0), input.charAt(1)) ||
+                Objects.equals(input.charAt(0), input.charAt(2)) ||
+                Objects.equals(input.charAt(1), input.charAt(2));
+    }
+
+    public void convertInputNumber(String userInput) {
+        String[] splitStr = userInput.split("");
+        for (int i = 0; i < splitStr.length; i++) {
+            int intStr = Integer.parseInt(splitStr[i]);
+            userNumbers.add(intStr);
+        }
+    }
+
+    public ArrayList<Integer> getUserNumbers() {
+        return userNumbers;
+    }
+}
