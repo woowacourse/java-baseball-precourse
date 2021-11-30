036eeaf9b74cf8ee527b04903dbdffe8b7723f80
dongho <dongho108@naver.com>
2021-11-30 19:12:58 +0900

    refactor: java convention setting

--- src/main/java/baseball/utils/Parser.java
+++ src/main/java/baseball/utils/Parser.java
@@ -2,34 +2,34 @@ package baseball.utils;
 
 public class Parser {
 
-    public int[] parseUserInput(String input, int size) throws IllegalArgumentException {
-        checkSize(input, size);
-        return getParseInt(input, size);
-    }
+	public int[] parseUserInput(String input, int size) throws IllegalArgumentException {
+		checkSize(input, size);
+		return getParseInt(input, size);
+	}
 
-    private void checkSize(String input, int size) throws IllegalArgumentException{
-        if (input.length() != size) {
-            throw new IllegalArgumentException();
-        }
-    }
+	private void checkSize(String input, int size) throws IllegalArgumentException {
+		if (input.length() != size) {
+			throw new IllegalArgumentException();
+		}
+	}
 
-    private int[] getParseInt(String input, int size) throws IllegalArgumentException{
-        int[] parseInt = new int[size];
+	private int[] getParseInt(String input, int size) throws IllegalArgumentException {
+		int[] parseInt = new int[size];
 
-        for (int i = 0; i < input.length(); i++) {
-            if (!checkDigit(input, i)) {
-                throw new IllegalArgumentException();
-            }
-            parseInt[i] = convertCharToInt(input, i);
-        }
-        return parseInt;
-    }
+		for (int i = 0; i < input.length(); i++) {
+			if (!checkDigit(input, i)) {
+				throw new IllegalArgumentException();
+			}
+			parseInt[i] = convertCharToInt(input, i);
+		}
+		return parseInt;
+	}
 
-    private int convertCharToInt(String input, int i) {
-        return input.charAt(i) - '0';
-    }
+	private int convertCharToInt(String input, int i) {
+		return input.charAt(i) - '0';
+	}
 
-    private Boolean checkDigit(String input, int i){
-        return '0' <= input.charAt(i) && input.charAt(i) <= '9';
-    }
+	private Boolean checkDigit(String input, int i) {
+		return '0' <= input.charAt(i) && input.charAt(i) <= '9';
+	}
 }
