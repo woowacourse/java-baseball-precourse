f88537f7e36559983312174e13494eb1c8dee5a4
YJ <soltwood6862@naver.com>
2021-11-30 23:09:18 +0900

    feat(Input.java) :  입력받은수 오류 확인 메소드 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -12,4 +12,31 @@ public class Input {
         return userInput;
     }
 
+    public static void isValidateUserNumber(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        if (!userInput.replaceAll("[1-9]", "").equals("")) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        if (!isDuplicate(userInput)) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+
+    private static boolean isDuplicate(String userInput) {
+        boolean[] overlapCheck = new boolean[3];
+
+        for (int j = 0; j < 3; j++) {
+            overlapCheck[j] = true;
+            if (isSame(userInput, overlapCheck, j)) {
+                return false;
+            }
+            overlapCheck[j] = false;
+        }
+        return true;
+    }
+
 }
