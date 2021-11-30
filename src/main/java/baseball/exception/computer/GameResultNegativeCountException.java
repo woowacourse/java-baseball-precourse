df08f7963ce75de406b2be1a45574a5d3646675b
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-11-30 18:32:11 +0900

    style: Rearrange Code

--- src/main/java/baseball/exception/computer/GameResultNegativeCountException.java
+++ src/main/java/baseball/exception/computer/GameResultNegativeCountException.java
@@ -1,6 +1,6 @@
 package baseball.exception.computer;
 
-public class GameResultNegativeCountException extends IllegalArgumentException{
+public class GameResultNegativeCountException extends IllegalArgumentException {
 
     private static final String GAME_RESULT_NEGATIVE_COUNT_ERROR_MESSAGE = "count는 음수가 입력될 수 없습니다.";
 
