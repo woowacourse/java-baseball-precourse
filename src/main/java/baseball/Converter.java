8637e84c5f2e97d7102a19be5979f6f8a0d04582
qbang <vhfhddl0101@naver.com>
2021-11-30 16:46:22 +0900

    refactor: improved readability

--- src/main/java/baseball/Converter.java
+++ src/main/java/baseball/Converter.java
@@ -5,16 +5,16 @@ import java.util.List;
 
 public class Converter {
     // 입력값을 잘라 리스트에 넣는다.
-    public static List<Integer> makeInputToList(String inputString){
+    public static List<Integer> toList(String inputString){
         List<Integer> result = new ArrayList<>();
-        for(String s : inputString.split("")){
+        for (String s : inputString.split("")) {
             result.add(Integer.parseInt(s));
         }
         return result;
     }
 
     // 리스트의 요소들을 붙여 스트링 형식으로 반환한다.
-    public static String makeListToString(List<Integer> list){
+    public static String toString(List<Integer> list){
         return list.toString().replaceAll("[^0-9]", "");
     }
 }
