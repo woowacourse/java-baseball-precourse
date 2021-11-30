777b10b0378a96d8aa843d7b9642e94c71e52f2e
eonju97 <eonju97.dev@gmail.com>
2021-11-30 09:41:37 +0900

    feat: 입력값 숫자 확인 로직 추가 및 재시작 입력 확인 로직 추가

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -5,13 +5,27 @@ import java.util.stream.Collectors;
 
 public class Validator {
 
+    private final static int START = 1;
+    private final static int END = 2;
     private final static int DIGIT = 3;
+    private final static String NUMBER_PATTERN = "[+-]?\\d*(\\.\\d+)?";
+
     private String input;
 
     public void validateInputNumber(String input) {
         this.input = input;
 
-        if (isContainsZero() || isContainsDuplicatedNumber() || !isCorrectDigit()) {
+        boolean isNumber = this.input.matches(NUMBER_PATTERN);
+
+        if (!isNumber || isContainsZero() || isContainsDuplicatedNumber() || !isCorrectDigit()) {
+            throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+        }
+    }
+
+    public void validateRestartInput(String input) {
+        boolean isNumber = this.input.matches(NUMBER_PATTERN);
+
+        if (!isNumber && (Integer.parseInt(input) != START || Integer.parseInt(input) != END)) {
             throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
         }
     }
