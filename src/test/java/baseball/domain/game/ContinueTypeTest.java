ec3b4583205b7313aa3aabdcef09a1454a501aac
cjlee38 <gptpem38@gmail.com>
2021-11-30 13:28:41 +0900

    test: test input of continueType
    ContinueType 의 입력 테스트 추가

---
+++ src/test/java/baseball/domain/game/ContinueTypeTest.java
@@ -0,0 +1,38 @@
+package baseball.domain.game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ContinueTypeTest {
+
+	@Test
+	@DisplayName("1 입력은 참을 반환해야 한다.")
+	public void testContinueWIthOne() throws Exception {
+		// given
+		String input = "1";
+		// when
+		boolean b = ContinueType.toBeContinued(input);
+		// then
+		Assertions.assertTrue(b);
+	}
+
+	@Test
+	@DisplayName("2 입력은 거짓을 반환해야 한다.")
+	public void testContinueWithTwo() throws Exception {
+		String input = "2";
+		// when
+		boolean b = ContinueType.toBeContinued(input);
+		// then
+		Assertions.assertFalse(b);
+	}
+
+	@Test
+	@DisplayName("1, 2 이외의 값은 예외를 던져야 한다.")
+	public void testContinueWithElse() throws Exception {
+		// given
+		String input = "12";
+		//then
+		Assertions.assertThrows(IllegalArgumentException.class, () -> ContinueType.toBeContinued(input));
+	}
+}
