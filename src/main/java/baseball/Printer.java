7a73a4492221151f79a5a2ef8ca179e6b0c8e4a7
eonju97 <eonju97.dev@gmail.com>
2021-11-30 09:40:06 +0900

    feat: 재시작 입력 메세지 출력 추가

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -22,4 +22,8 @@ public class Printer {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public void printGameRestart(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
 }
