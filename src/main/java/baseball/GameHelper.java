8abb222a47f2c0c90e560a32588215deb0b27d00
erie0210 <eriee0210@gmail.com>
2021-11-30 15:59:22 +0900

    feat: 입력받은 세 숫자와 생성된 세 숫자를 비교한다

--- src/main/java/baseball/GameHelper.java
+++ src/main/java/baseball/GameHelper.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GameHelper {
@@ -22,6 +24,7 @@ public class GameHelper {
 
     public static void play(List<Integer> generateNumbers){
         String inputString = getUserInput();
+        compare(inputString, generateNumbers);
         return;
     };
 
@@ -45,4 +48,37 @@ public class GameHelper {
             });
     };
 
+    public static void compare(String inputString, List<Integer> generateNumber){
+        List<Integer> parsedInputList = parseStringToList(inputString);
+        int[] inputArray = getArray(parsedInputList);
+        int[] generateArray = getArray(generateNumber);
+
+        int strikes =0;
+        int balls =0;
+        for(int i=0; i<10; i++){
+            if(inputArray[i]!=-1 && generateArray[i]!=-1){
+                if(inputArray[i]==generateArray[i]){
+                    strikes++;
+                    continue;
+                }
+                balls++;
+            }
+        }
+        return;
+    };
+
+    public static List<Integer> parseStringToList(String inputString){
+        String[] parsedArray = inputString.split("");
+        List<String> parsedList = new ArrayList<String>(Arrays.asList(parsedArray));
+        IntStream parsedStream  = parsedList.stream().mapToInt(num->Integer.parseInt(num));
+        List<Integer> parsedIntList = parsedStream.boxed().collect(Collectors.toList());
+        return parsedIntList;
+    };
+
+    private static int[] getArray(List<Integer> inputList) {
+        int resultArray[]=new int[10];
+        Arrays.fill(resultArray, -1);
+        IntStream.range(0,3).forEach(idx->resultArray[inputList.get(idx)] = idx);
+        return resultArray;
+    };
 }
