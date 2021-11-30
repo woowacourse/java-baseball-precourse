e972c9fdaac8a1efd36e3f249ac2f65eef69e8b2
harinn1014 <gkfla7555@gmail.com>
2021-11-30 15:35:58 +0900

    feat(Game): add methods about validation_InputType and Range

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -14,12 +14,26 @@ public class Game {
 		int nowState= PLAY_GAME;
 		Computer computer= new Computer();
 		Round round= new Round(computer);
-		while(nowState!=PLAY_GAME) {
+		while(nowState==PLAY_GAME) {
 			round.init();
 			round.play();
 			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			nowState= Integer.parseInt(readLine());
+			String userInput= readLine();
 			//숫자 유효성 검사
+		}
+	}
+	
+	private void validateInputType(String userInput) {
+		try {
+			Integer.parseInt(userInput);
+		}catch(NumberFormatException e){
+			throw new IllegalArgumentException("Type Error: 숫자만 입력하세요.");
+		}
+	}
+	
+	private void validateRange(int userInput) {
+		if(userInput<PLAY_GAME||userInput>EXIT_GAME) {
+			throw new IllegalArgumentException("Range Error: 범위를 벗어난 숫자입니다.");
 		}
 	}
 
