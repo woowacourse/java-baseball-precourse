54f667af126f6be7548381dbcbb96c99744cc144
Tony Kang <tonykang@Tonys-MacBook-Pro.local>
2022-03-18 17:34:37 +0900

    [002-RESPONSE] separate 'output' response

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,43 @@
+package baseball;
+
+public class Output {
+
+    private final String TYPE_IN_NUMBERS = "숫자를 입력해주세요 : ";
+    private final String CONGRATES = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String TYPE_IN_FOR_MORE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String STRIKE = "스크라이크";
+    private final String BALL = "볼";
+    private final String NOTHING = "낫싱";
+    private final String SPACE = " ";
+    private StringBuilder sb;
+
+    public Output() {
+        sb = new StringBuilder();
+    }
+
+    public void printAnswerRequest() {
+        sb.append(TYPE_IN_NUMBERS);
+        System.out.print(sb);
+    }
+
+    public void printAskMore() {
+        sb.append(CONGRATES).append(TYPE_IN_FOR_MORE);
+        System.out.println(sb);
+    }
+
+    public void printScore(int strike, int ball) {
+        if (strike != 0 && ball != 0) {
+            sb.append(ball + BALL).append(SPACE).append(strike + STRIKE);
+            System.out.println(sb);
+        } else if (strike != 0 && ball == 0) {
+            sb.append(strike + STRIKE);
+            System.out.println(sb);
+        } else if (ball != 0 && strike == 0) {
+            sb.append(ball + BALL);
+            System.out.println(sb);
+        } else {
+            sb.append(NOTHING);
+            System.out.println(sb);
+        }
+    }
+}
