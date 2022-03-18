72dc000345588f067d272981d015f34b47acc08f
Tony Kang <tonykang@Tonys-MacBook-Pro.local>
2022-03-18 16:48:32 +0900

    [002-RESPONSE] refactor 'Player' class using 'Input' object

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,30 +1,20 @@
 package baseball;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Player {
-
-    private final int AGAIN = 1;
-    private final int TERMINATE = 2;
 
     public Player() {
 
     }
 
     public List<Integer> submitAnswer() {
-        Scanner in = new Scanner(System.in);
-        List<Integer> playerAnswer = new ArrayList<>();
-        for (int i = 0; i < 3; ++i) {
-            playerAnswer.add(in.nextInt());
-        }
-        return playerAnswer;
+        Input playerInput = new Input();
+        return playerInput.inputPlayerAnswer();
     }
 
     public boolean askForMore() {
-        Scanner in = new Scanner(System.in);
-        int decision = in.nextInt();
-        return decision == AGAIN;
+        Input playerInput = new Input();
+        return playerInput.anotherRound();
     }
 }
