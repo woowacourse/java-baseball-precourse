c022e335a2f597544966b56b1f4aa9a9a76434ce
Tony Kang <tonykang@Tonys-MacBook-Pro.local>
2022-03-18 15:20:08 +0900

    [002-RESPONSE] add feature make new random number list

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class BaseballGame {
+
+    private final int NUMBER_RANGE_START = 0;
+    private final int NUMBER_RANGE_END = 1;
+    private final int HOW_MANY_RANDOM_NUMBERS = 3;
 
     public BaseballGame() {
 
     }
 
+    public List<Integer> makeRandomNumber() {
+        List<Integer> randomNumList = Randoms.pickUniqueNumbersInRange(NUMBER_RANGE_START, NUMBER_RANGE_END, HOW_MANY_RANDOM_NUMBERS);
+        return randomNumList;
+    }
 }
