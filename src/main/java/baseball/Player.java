6cb125b00272753ed6036a304476a1032f6d23f1
Tony Kang <tonykang@Tonys-MacBook-Pro.local>
2022-03-18 17:35:41 +0900

    [002-RESPONSE] refactor 'Player' class using 'Output' object

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -9,11 +9,17 @@ public class Player {
     }
 
     public List<Integer> submitAnswer() {
+        Output playerOutput = new Output();
+        playerOutput.printAnswerRequest();
+
         Input playerInput = new Input();
         return playerInput.inputPlayerAnswer();
     }
 
     public boolean askForMore() {
+        Output playerOutput = new Output();
+        playerOutput.printAskMore();
+
         Input playerInput = new Input();
         return playerInput.anotherRound();
     }
