208beb0abf95284583f3b1ac45340377e596e8af
shipjh <ship_reappearance@naver.com>
2022-04-15 23:25:54 +0900

    test : 스트링 테스트 요구사항1
    요구사항 1에 맞는 테스트 코드 작성완료.

--- src/test/java/stringStudy/StringTest.java
+++ src/test/java/stringStudy/StringTest.java
@@ -1,8 +1,31 @@
 package stringStudy;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
 
+    @Test
+    void 스플릿테스트() {
+        //given
+        String string = "1,2";
 
+        //when
+        String[] splitString = string.split(",");
+
+        //then
+        assertThat(splitString).contains("1","2");
+    }
+
+    @Test
+    void 한자리_숫자_스플릿테스트() {
+        //given
+        String string = "1";
+
+        //when
+        String[] splitString = string.split(",");
+
+        //then
+        assertThat(splitString).containsExactly("1");
+    }
 }
