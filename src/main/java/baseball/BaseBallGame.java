6d76f657fa1ccf975d8168357e1c0ab889b63d6b
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 21:46:38 +0900

    feat(BaseBallGame): add user balls size exception

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -14,13 +14,35 @@ public class BaseBallGame {
 	private Balls makeUserBalls() {
 		Balls userBalls = null;
 		while (userBalls == null) {
-			// todo 사용자 볼 생성 예외처리
-			userBalls = Balls.stringToBalls(getUserInput());
+			String userInput = getUserInput();
+			validateUserInput(userInput);
+			try {
+				userBalls = Balls.stringToBalls(userInput);
+			} catch (NumberFormatException e) {
+				printFormatExceptionMessage();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
 		return userBalls;
 	}
 
+	private void validateUserInput(String userInput) {
+		if (checkUserInputLength(userInput)) {
+			throw new IllegalArgumentException("입력값의 길이가 잘못 되었습니다.");
+		}
+	}
+
 	private String getUserInput() {
 		return readLine();
+	}
+
+	private Boolean checkUserInputLength(String str) {
+		return str.length() != Balls.BALLS_COUNT;
+	}
+
+	private void printFormatExceptionMessage() {
+		System.out.printf("%d개의 1~9 사이의 중복되지 않는 수만 입력할 수 있습니다.%n", Balls.BALLS_COUNT);
 	}
 }
