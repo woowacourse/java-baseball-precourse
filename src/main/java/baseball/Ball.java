7bb54b564455dd9a5782f507c4d0732131a0ec35
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 18:52:28 +0900

    feat(ball): add ball and balls
    create ball class
    add validate number range 1~9
    create balls class
    add validate duplicate ball

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,44 @@
+package baseball;
+
+public class Ball {
+	private final Integer number;
+
+	public Ball(int number) {
+		validateRange(number);
+		this.number = number;
+	}
+
+	public Ball(char ch) {
+		this(Integer.parseInt(String.valueOf(ch)));
+	}
+
+	private void validateRange(int number) {
+		if (number < 1 || 9 < number) {
+			throw new IllegalArgumentException("공의 숫자는 1~9의 자연수만 가능합니다.");
+		}
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Ball ball;
+		if (obj instanceof Ball) {
+			ball = (Ball)obj;
+		} else {
+			throw new ClassCastException(this.getClass().getName() + " 객체 끼리만 비교가 가능합니다.");
+		}
+		return this.number.equals(ball.number);
+	}
+
+	@Override
+	public int hashCode() {
+		if (number == null) {
+			return 0;
+		}
+		return number.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return this.number.toString();
+	}
+}
---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,56 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Balls {
+	public static final int BALLS_COUNT = 3;
+
+	private final List<Ball> balls;
+
+	public Balls(List<Ball> balls) {
+		validate(balls);
+		this.balls = balls;
+	}
+
+	public static Balls makeRandomBalls() {
+		List<Ball> balls = new ArrayList<>();
+
+		while (balls.size() != BALLS_COUNT) {
+			Ball ball = new Ball(Randoms.pickNumberInRange(1, 9));
+
+			if (!balls.contains(ball)) {
+				balls.add(ball);
+			}
+		}
+		return new Balls(balls);
+	}
+
+	private void validate(List<Ball> balls) {
+		Set<Ball> ballSet = new HashSet<>(balls);
+		if (ballSet.size() != BALLS_COUNT) {
+			throw new IllegalArgumentException("공의 숫자가 중복 됩니다.");
+		}
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Balls balls;
+		if (obj instanceof Balls) {
+			balls = (Balls)obj;
+		} else {
+			throw new ClassCastException(this.getClass().getName() + " 객체 끼리만 비교가 가능합니다.");
+		}
+		return this.balls.equals(balls.balls);
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(this.balls.toArray());
+	}
+}
