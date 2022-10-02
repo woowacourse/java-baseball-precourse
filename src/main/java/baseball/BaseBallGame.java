518e5ed71ba5de5073124916535a4a25302e9a42
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 22:35:50 +0900

    feat(BaseballGame): update play method to play again

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -4,11 +4,16 @@ import static camp.nextstep.edu.missionutils.Console.*;
 
 public class BaseBallGame {
 
+	private static final String CONTINUE = "1";
+	private static final String QUIT = "2";
+
 	public BaseBallGame() {
 	}
 
 	public void play() {
-		game();
+		do {
+			game();
+		} while (reGame());
 	}
 
 	public void game() {
@@ -40,6 +45,22 @@ public class BaseBallGame {
 		return userBalls;
 	}
 
+	private Boolean reGame() {
+		String input = "";
+
+		while (invalidInput(input)) {
+			printCheckRestartMessage();
+			input = getUserInput();
+		}
+
+		printCheckRestartExceptionMessage();
+		return input.equals(CONTINUE);
+	}
+
+	private boolean invalidInput(String input) {
+		return !input.equals(CONTINUE) && !input.equals(QUIT);
+	}
+
 	private void validateUserInput(String userInput) {
 		if (checkUserInputLength(userInput)) {
 			throw new IllegalArgumentException("입력값의 길이가 잘못 되었습니다.");
@@ -64,5 +85,13 @@ public class BaseBallGame {
 
 	private void printFormatExceptionMessage() {
 		System.out.printf("%d개의 1~9 사이의 중복되지 않는 수만 입력할 수 있습니다.%n", Balls.BALLS_COUNT);
+	}
+
+	private void printCheckRestartExceptionMessage() {
+		System.out.println("1, 2만 입력 가능합니다. 다시 입력해주세요.");
+	}
+
+	private void printCheckRestartMessage() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 	}
 }
