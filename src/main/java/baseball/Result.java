4ec5b320516c13980e775687088b9d6fd2c9e52c
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 21:55:41 +0900

    feat(Result): add baseball result

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,83 @@
+package baseball;
+
+import static baseball.Balls.*;
+
+public class Result {
+
+	private static final String STRIKE = "스트라이크";
+	private static final String BALL = "볼";
+	private static final String NOTHING = "낫싱";
+
+	private final Integer strike;
+	private final Integer ball;
+
+	public Result(Integer strike, Integer ball) {
+		this.strike = strike;
+		this.ball = ball;
+	}
+
+	public static Result checkBalls(Balls gameBalls, Balls userBalls) {
+		int strike = 0;
+		int ball = 0;
+
+		for (int i = 0; i < BALLS_COUNT; i++) {
+			Ball thisBall = gameBalls.get(i);
+
+			if (thisBall.equals(userBalls.get(i))) {
+				strike++;
+				continue;
+			}
+			if (userBalls.contains(thisBall)) {
+				ball++;
+			}
+		}
+
+		return new Result(strike, ball);
+	}
+
+	public String getResultMessage() {
+		if (isNothing()) {
+			return NOTHING;
+		}
+		if (isStrike()) {
+			return strike + STRIKE;
+		}
+		if (isBall()) {
+			return ball + BALL;
+		}
+		return strike + STRIKE + " " + ball + BALL;
+	}
+
+	public boolean isAllStrikes() {
+		if (strike == BALLS_COUNT) {
+			printEndMessage();
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isNothing() {
+		return strike == 0 && ball == 0;
+	}
+
+	private boolean isStrike() {
+		return 0 < strike && ball == 0;
+	}
+
+	private boolean isBall() {
+		return strike == 0 && 0 < ball;
+	}
+
+	private boolean isStrikeAndBall() {
+		return 0 < strike && 0 < ball;
+	}
+
+	private boolean isAllStrike() {
+		return strike == BALLS_COUNT;
+	}
+
+	private void printEndMessage() {
+		System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료%n", BALLS_COUNT);
+	}
+
+}
