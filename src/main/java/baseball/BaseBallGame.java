91abccc7c7d4dae3996eaac34df7b35e5fe784eb
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-02 21:17:30 +0900

    feat(BaseBallGame): add BaseBallGame and userInput

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+public class BaseBallGame {
+
+	public BaseBallGame() {
+	}
+
+	public void play() {
+		String userInput = getUserInput();
+	}
+
+	private String getUserInput() {
+		return readLine();
+	}
+}
