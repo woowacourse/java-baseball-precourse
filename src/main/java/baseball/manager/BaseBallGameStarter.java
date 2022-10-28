195617b142d9dbdb6efc6941580c4ee20a79eeb9
doyeon <kimsk3113@naver.com>
2022-10-28 20:32:50 +0900

    feat: 게임 실행

--- src/main/java/baseball/manager/BaseBallGameStarter.java
+++ src/main/java/baseball/manager/BaseBallGameStarter.java
@@ -12,6 +12,52 @@ import java.util.stream.Collectors;
 
 public class BaseBallGameStarter {
     private static final User user = new User();
+    private static final Computer computer = new Computer();
+
+    public static void run() {
+
+        do {
+            computer.setComputerNumber();
+            startUntilThreeStrike();
+        } while (isRestart());
+
+    }
+
+    private static void startUntilThreeStrike() {
+        setUserNumber();
+        CheckTotalGameStatus checkTotalGameStatus = new CheckTotalGameStatus(user.getUserNumber(), computer.getComputerNumber());
+        printResult(checkTotalGameStatus);
+
+        if (!checkTotalGameStatus.getIsWinning()) {
+            startUntilThreeStrike();
+            return;
+        }
+
+        OutputView.printWinningMessage();
+    }
+
+    private static void printResult(CheckTotalGameStatus checkTotalGameStatus) {
+
+        if (checkTotalGameStatus.getIsNothing()) {
+            OutputView.printNothing();
+            return;
+        }
+
+        OutputView.printStrikeAndBall(checkTotalGameStatus.getStrikeNumber(), checkTotalGameStatus.getBallNumber());
+    }
+
+    private static boolean isRestart() {
+        final String NEW_GAME = "1";
+        final String EXIT_GAME = "2";
+
+        String userInput = InputView.getNewGameOrExitNumber();
+
+        if (!userInput.equals(NEW_GAME) && !userInput.equals(EXIT_GAME)) {
+            throw new IllegalArgumentException("입력 오류");
+        }
+
+        return userInput.equals(NEW_GAME);
+    }
 
     private static void setUserNumber() {
         String userInput = InputView.getUserString();
