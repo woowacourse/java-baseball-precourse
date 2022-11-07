bf9e7a24611b8fa7a7304bdeeafa354534845abc
JeonSangYong <tkddyd420@naver.com>
2022-11-07 21:31:39 +0900

    feat : 플레이어가 서로 다른 숫자 3개 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         List<Integer> computer = initComputerNumbers();
+        List<Integer> user = initUserNumbers();
     }
 
-    public static List<Integer> initComputerNumbers() {
+    private static List<Integer> initComputerNumbers() {
         List<Integer> list = new ArrayList<>();
         while (list.size() != 3) {
             int number = Randoms.pickNumberInRange(1, 9);
@@ -19,5 +22,12 @@ public class Application {
             }
         }
         return list;
+    }
+
+    private static List<Integer> initUserNumbers() {
+        return Console.readLine()
+                .chars()
+                .mapToObj(c -> c - '0')
+                .collect(Collectors.toList());
     }
 }
