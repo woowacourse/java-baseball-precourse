a84a75abe2fb651a3e5930b4a5c99a576b0592b3
JeonSangYong <tkddyd420@naver.com>
2022-11-07 21:27:04 +0900

    feat : 컴퓨터가 서로 다른 임의의 수 3개 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        List<Integer> computer = initComputerNumbers();
+    }
+
+    public static List<Integer> initComputerNumbers() {
+        List<Integer> list = new ArrayList<>();
+        while (list.size() != 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!list.contains(number)) {
+                list.add(number);
+            }
+        }
+        return list;
     }
 }
