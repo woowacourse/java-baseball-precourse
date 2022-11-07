48b86a6c198dee250f9d863ab26aee1e89c44c22
JeonSangYong <tkddyd420@naver.com>
2022-11-07 21:56:10 +0900

    fix : 컴퓨터 수와 플레이어 수 비교

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -61,7 +61,11 @@ public class Application {
         }
         if (ball == 0) {
             System.out.printf("%d스트라이크\n", strike);
-            return strike == 3;
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return true;
+            }
+            return false;
         }
         if (strike == 0) {
             System.out.printf("%d볼\n", ball);
