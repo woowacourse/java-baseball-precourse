186c193ef4b6a6104c2081e4b0d6acf29a55d13d
GeonHeeLeee <108582570+GeonHeeLeee@users.noreply.github.com>
2023-03-03 17:00:08 +0900

    added generate_num function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
+
+    private static final int generate_num(){
+        int answer_arr[] = new int[3];
+        String number = "";
+        for(int i = 0; i < 3; i++){
+            int num = Randoms.pickNumberInRange(1,9);
+            answer_arr[i] = num;
+            number += Integer.toString(num);
+        }
+        return Integer.parseInt(number);
+    }
+
+
+   
+    
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        int num = generate_num();
+        System.out.println(num);
+        
     }
 }
