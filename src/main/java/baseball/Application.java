8296ddbabdc8341578049ff609b8e81974e47c29
GeonHeeLeee <108582570+GeonHeeLeee@users.noreply.github.com>
2023-03-03 17:28:37 +0900

    edited generate_num function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,9 +5,16 @@ public class Application {
 
     private static final int generate_num(){
         int answer_arr[] = new int[3];
+        boolean false_list[] = {false,false,false,false,false,false,false,false,false};
         String number = "";
-        for(int i = 0; i < 3; i++){
-            int num = Randoms.pickNumberInRange(1,9);
+        for(int i=0; i<3;i++){
+            int num;
+            while(true){
+                num = Randoms.pickNumberInRange(1,9);
+                if(false_list[num-1]){continue;}
+                false_list[num-1] = true;
+                break;
+            }
             answer_arr[i] = num;
             number += Integer.toString(num);
         }
@@ -15,7 +22,20 @@ public class Application {
     }
 
 
-   
+    // public static void start_game(int answer){
+    //     boolean flag = true;
+
+    //     while(flag){
+    //         int strike = 0;
+    //         int ball = 0;   
+    //         System.out.print("숫자를 입력해주세요 : ");
+    //         int input = Integer.parseInt(Console.readLine());
+    //         for(int i = 0; i < 3; i++){
+                
+    //         }
+    //     }
+        
+    // }
     
     public static void main(String[] args) {
         int num = generate_num();
