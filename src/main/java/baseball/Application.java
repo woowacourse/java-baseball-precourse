ee68ef1a6118733abd2ea33a97294f4dc56e132b
JinseonYe <aatwe24@gmail.com>
2023-07-09 04:02:00 +0900

    [Feat] 야구게임 기능 요구사항, 입출력 요구사항을 충족하도록 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,81 @@
 package baseball;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+
+        //게임 전체 진행
+        while (true) {
+            //랜덤한 수 생성
+            int[] question = new int[3];
+            question[0] = rd.nextInt(9) + 1;
+            question[1] = rd.nextInt(9) + 1;
+            question[2] = rd.nextInt(9) + 1;
+
+            //답변 제시
+            while (true) {
+                System.out.print("숫자를 입력해주세요 : ");
+
+                String answer = sc.nextLine();
+
+
+                //3자리가 아닌 수를 입력하면 예외 발생
+                if (answer.length() !=3) {
+                    throw new IllegalArgumentException("3자리 수를 입력해주세요.");
+                }
+
+                //문자열 answer을 숫자배열 answerArray로 변환
+                String[] answerStrings = answer.split("");
+                int[] answerArray = new int[answerStrings.length];
+                for (int i = 0; i < answerStrings.length; i++) {
+                    answerArray[i] = Integer.parseInt(answerStrings[i]);
+                }
+
+                int strike = 0;
+                int ball = 0;
+
+                //문제와 답변의 일치 여부 판단
+                for (int i = 0; i < question.length; i++) {
+                    int j = (i + 1) % question.length;
+                    int k = (i + 2) % question.length;
+                    if (question[i] == answerArray[i]) {
+                        strike++;
+                    } else if (question[i] == answerArray[j] || question[i] == answerArray[k]) {
+                        ball++;
+                    }
+                }
+
+                //일치 여부에 따른 결과 출력
+                if (strike != 0 && ball == 0) {
+                    System.out.println(strike + "스트라이크");
+                } else if (strike != 0 && ball != 0) {
+                    System.out.print(ball + "볼");
+                    System.out.println(strike + "스트라이크");
+                } else if (strike == 0 && ball != 0) {
+                    System.out.println(ball + "볼");
+                } else if (strike == 0 && ball == 0) {
+                    System.out.println("낫싱");
+                }
+
+                if (strike == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+            }
+            //게임 재실행 여부 판단
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int num = Integer.parseInt(sc.nextLine());
+            if (num == 1) {
+
+            } else if (num == 2) {
+                break;
+            }
+        }
     }
 }
