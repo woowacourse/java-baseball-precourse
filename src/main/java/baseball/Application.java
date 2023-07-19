8b3007048ca48b76562455a5b3a5cffc49301ea5
JinseonYe <aatwe24@gmail.com>
2023-07-19 23:48:54 +0900

    [Refactor] 사용자가 입력하는 값을 Scanner가 아닌 Console을 사용하도록 변경, 답변에 대한 예외 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,6 @@
 package baseball;
 
-import java.util.Scanner;
-
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -11,8 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-
-        Scanner sc = new Scanner(System.in);
 
         //게임 전체 진행
         while (true) {
@@ -25,12 +22,16 @@ public class Application {
             //답변 제시
             while (true) {
                 System.out.print("숫자를 입력해주세요 : ");
-
-                String answer = sc.nextLine();
+                String answer = Console.readLine();
 
                 //3자리가 아닌 수를 입력하면 예외 발생
                 if (answer.length() !=3) {
                     throw new IllegalArgumentException("3자리 수를 입력해주세요.");
+                }
+
+                //답변에 0이 포함되면 예외 발생
+                if (answer.contains("0")) {
+                    throw new IllegalArgumentException("답변은 0을 포함할 수 없습니다.");
                 }
 
                 //문자열 answer을 숫자배열 answerArray로 변환
@@ -48,10 +49,10 @@ public class Application {
                 if (strike != 0 && ball == 0) {
                     System.out.println(strike + "스트라이크");
                 } else if (strike != 0 && ball != 0) {
-                    System.out.print(ball + "볼");
+                    System.out.print(ball + "볼 ");
                     System.out.println(strike + "스트라이크");
                 } else if (strike == 0 && ball != 0) {
-                    System.out.println(ball + "볼");
+                    System.out.println(ball + "볼 ");
                 } else if (strike == 0 && ball == 0) {
                     System.out.println("낫싱");
                 }
@@ -63,7 +64,7 @@ public class Application {
             }
             //게임 재실행 여부 판단
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int num = Integer.parseInt(sc.nextLine());
+            int num = Integer.parseInt(Console.readLine());
             if (num == 1) {
 
             } else if (num == 2) {
