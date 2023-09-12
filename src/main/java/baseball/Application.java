f451f74ceb85a04fd859822fb7098e5174eae9eb
Hyeonji-Jung <welloff.jj@gmail.com>
2023-09-12 18:09:12 +0900

    feat(app): GameController 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+
+        GameController gameController = new GameController();
+        gameController.run();
     }
 }
