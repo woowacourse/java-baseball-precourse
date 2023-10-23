551e64688ce714d3d24bc4635610c0661d5cb523
taewoocode <qkrxodn6035@gmail.com>
2023-10-24 02:38:35 +0900

    완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,10 +2,9 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.run();
         //TODO: 숫자 야구 게임 구현
-
-
-
 
     }
 }
