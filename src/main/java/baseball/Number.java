def06923fbabee5cdc85b9a3c513c7e0c2fd29d5
leeminjun <sohwi0305@gmail.com>
2023-10-23 20:05:25 +0900

    fix: 리스트 수정

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -3,36 +3,40 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Number {
     private static String userNumber;
-    private static List<Integer> randomNumber;
+    private static List<Integer> randomNumber = new ArrayList<>();
     private static Integer continueOrNot;
     private static Integer strike;
     private static Integer ball;
     private static Integer num;
+
     private static void getRandomNumber() {
-        while(randomNumber.size() <3) {
-            num = Randoms.pickNumberInRange(1, 9); // 1~9까지
-            numberDuplication();
+        while (randomNumber.size() < 3) {
+            num = Randoms.pickNumberInRange(1, 9);
+            numberDuplication(num);
         }
     }
 
-    public static  void numberDuplication(){
+    public static void numberDuplication(Integer num) {
         if (!randomNumber.contains(num)) {
             randomNumber.add(num);
         }
     }
+
     public static void getUserNumber() {
         userNumber = Console.readLine();
-
     }
-    public static void isValidLength(String s){
-        if(s.length() != 3) {
+
+    public static void isValidLength(String s) {
+        if (s.length() != 3) {
             throw new IllegalArgumentException("세자리 수보다 크거나 작음");
         }
     }
+
     public static void printNumber() {
         getRandomNumber();
         getUserNumber();
