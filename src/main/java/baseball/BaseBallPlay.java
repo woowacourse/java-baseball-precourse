907bf7d2e199015e71b30bd4e768414cce6a5dff
leeminjun <sohwi0305@gmail.com>
2023-10-24 10:34:56 +0900

    refactor: 게임 실행 부분으로 이름 변경

--- src/main/java/baseball/BaseBallPlay.java
+++ src/main/java/baseball/BaseBallPlay.java
@@ -3,18 +3,19 @@ package baseball;
 import static constants.BaseballMessage.*;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class BaseBallPlayer {
-    private static List<Integer> userNumber = new ArrayList<>(3);
+public class BaseBallPlay {
+
+    private static List<Integer> userNumber = new ArrayList<>();
 
     public static void getUserNumber() {
         System.out.println(USER_INPUT_MESSAGE);
         String userInput = Console.readLine();
+
+        isThreeNumbers(userInput);
         userInputToUserNumber(userInput);
         System.out.println(userNumber);
     }
@@ -24,4 +25,10 @@ public class BaseBallPlayer {
         }
     }
 
+    private static boolean isThreeNumbers(String numbers) {
+        if (numbers.length() > 3) {
+            return true;
+        }
+        return false;
+    }
 }
