b7641624911592a18de9e4df80c4159da3dad623
leeminjun <sohwi0305@gmail.com>
2023-10-24 11:36:30 +0900

    feat: 스트라이크 볼 낫싱 체크 기능 추가

--- src/main/java/baseball/BaseBallPlay.java
+++ src/main/java/baseball/BaseBallPlay.java
@@ -5,7 +5,44 @@ import static constants.BaseballMessage.*;
 import java.util.List;
 
 
+
 public class BaseBallPlay {
+
+    StringBuilder checkMessage = new StringBuilder();
+
+    private String Correctanswercheck (int strike, int ball) {
+
+        if(strike != 0) {
+            if(ball != 0) {
+                MessageBallStrikeAppend(ball,strike);
+            }
+            MessageStrikeAppend(strike);
+        }
+        if(ball != 0) {
+            MessageBallAppend(ball);
+        }
+        return MessageNothingAppend();
+    }
+
+    private String MessageBallStrikeAppend(int ball,int strike){
+        checkMessage.append(ball).append(BALL).append(strike).append(STRIKE).append("\n");
+        return checkMessage.toString();
+    }
+
+    private String MessageBallAppend(int ball){
+        checkMessage.append(ball).append(BALL).append("\n");
+        return checkMessage.toString();
+    }
+
+    private String MessageStrikeAppend(int strike){
+        checkMessage.append(strike).append(STRIKE).append("\n");
+        return checkMessage.toString();
+    }
+
+    private String MessageNothingAppend(){
+        checkMessage.append(NOTHING);
+        return checkMessage.toString();
+    }
 
     private int countStrike(List<Integer> answer, List<Integer> userinput) {
         int strike = 0;
