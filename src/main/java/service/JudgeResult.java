d75b07ce4e1da4fd1bd3cc8d14e1427ec0c01d5e
ohyeonji <y.oh@ut.ac.kr>
2023-10-25 19:46:06 +0900

    feat : 결과 출력 기능 구현(스트라이크,볼,낫싱)

---
+++ src/main/java/service/JudgeResult.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.util.List;
+
+public class JudgeResult {
+	
+	CompareNum compare = new CompareNum();
+	
+    public String judgement(List<Integer> computer, List<Integer> player){
+        int total = compare.countBall(computer, player);
+        int strike = compare.countStrike(computer, player);
+        int ball = total - strike;
+        
+        String result = null;
+        
+//        if(total == 0){
+//            return "낫싱";
+//        }else if(strike == 0){
+//            return ball + "볼";
+//        }else if(ball == 0){
+//            return strike + "스트라이크";
+//        }        
+        
+        if(total == 0){
+            result = "낫싱";
+        }else if(ball != 0 && strike == 0){
+        	result = ball + "볼";
+        }else if(strike != 0 && ball == 0){
+        	result = strike + "스트라이크";
+        }else {
+        	result = ball + "볼 " + strike + "스트라이크";
+        }
+        return result;
+    }
+}
