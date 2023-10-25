3c7930362beafa053f91cf4c1246ee6acb0afc47
oodeng98 <oodeng98@naver.com>
2023-10-25 20:14:32 +0900

    feat: main 메서드 구조 설계 및 Score 클래스 생성
    main 메서드 설계,
    Score 클래스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Objects;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        String computer_number = generateNumber();
+
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            String s = input(3);
+            Score score = check(computer_number, s);
+            print(score);
+            if (score.correct) {
+                computer_number = restart();
+            }
+        } while (!computer_number.equals("1000"));
+    }
+
+    public static class Score {
+        int ball = 0;
+        int strike = 0;
+        boolean correct = false;
     }
 }
