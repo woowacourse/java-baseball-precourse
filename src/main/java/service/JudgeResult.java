683cfb7e07be62adbdfb8386737d04ec80d113f3
ohyeonji <y.oh@ut.ac.kr>
2023-10-25 19:55:08 +0900

    style : 주석제거

--- src/main/java/service/JudgeResult.java
+++ src/main/java/service/JudgeResult.java
@@ -11,15 +11,7 @@ public class JudgeResult {
         int strike = compare.countStrike(computer, player);
         int ball = total - strike;
         
-        String result = null;
-        
-//        if(total == 0){
-//            return "낫싱";
-//        }else if(strike == 0){
-//            return ball + "볼";
-//        }else if(ball == 0){
-//            return strike + "스트라이크";
-//        }        
+        String result = null;   
         
         if(total == 0){
             result = "낫싱";
