2c20efb3ef12586a1145c91d8927af2840f49a34
oodeng98 <oodeng98@naver.com>
2023-10-25 20:18:56 +0900

    feat: restart 메서드 생성
    게임 재시작 유무를 입력받는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -81,4 +81,13 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
+
+    public static String restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String s = input(1);
+        if (Objects.equals(s, "1")) {
+            return generateNumber();
+        }
+        return "1000";
+    }
 }
