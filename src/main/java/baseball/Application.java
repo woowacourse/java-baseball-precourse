5958e8b484bdb406abf47918ca0767479c6e5703
hellozo0 <ahju0011@gmail.com>
2023-12-30 17:07:55 +0900

    [feat] 게임 실행, 재시작 class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,21 +1,21 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
 
-        //먼저 랜덤 3자리 숫자를 뽑는다. - 배열로 관리
-        List<Integer> computer = new ArrayList<>(3);
-        for (int i = 0; i < 3; i++){
-            computer.set(i, pickNumberInRange(1, 9));
+        do {
+            Game game = new Game();
+            game.playGame();
+        } while ( checkPlay() );
+    }
+
+    public static boolean checkPlay() {
+        int retryGame = new Game().retryGame();
+
+        if(retryGame == 1){
+            return true;
         }
-
-
-
+        return false;
     }
 }
