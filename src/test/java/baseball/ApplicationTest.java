80b3f0ceb74722cd1f70160a84cea3e45633f776
hellozo0 <ahju0011@gmail.com>
2023-12-30 17:53:31 +0900

    [feat] 게임 TestCode 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,6 +32,63 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 유저_숫자이외의입력_예외_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> new Game().isRange("a23"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 유저_숫자길이입력_예외_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> new Game().isLength("1243"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 유저_숫자중복입력_예외_테스트(){
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> new Game().isDuplicate("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 컴퓨터_숫자생성_테스트(){
+        List<Integer> computerNum = new Game().getComputerNum();
+
+        assertThat(computerNum.size()).isEqualTo(3);
+        assertThat(new HashSet<>(computerNum).size()).isEqualTo(3);
+    }
+
+    @Test
+    void 게임_볼_검증테스트(){
+        List<Integer> computerNum = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> userNum = new ArrayList<>(Arrays.asList(2, 1, 9));
+
+        assertThat(new Game().checkBall(computerNum, userNum)).isEqualTo(2);
+    }
+
+    @Test
+    void 게임_스트라이크_검증테스트(){
+        List<Integer> computerNum = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> userNum = new ArrayList<>(Arrays.asList(1, 2, 9));
+
+        assertThat(new Game().checkStrike(computerNum, userNum)).isEqualTo(2);
+    }
+
+    @Test
+    void 게임_볼_스트라이크_검증테스트(){
+        List<Integer> computerNum = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> userNum = new ArrayList<>(Arrays.asList(1, 3, 9));
+
+        assertThat(new Game().checkStrike(computerNum, userNum)).isEqualTo(1);
+        assertThat(new Game().checkBall(computerNum, userNum)).isEqualTo(2);
     }
 
     @Override
